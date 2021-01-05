bdcd95a9bf3a3642317dc8503cf8fad05284c99c
Ely Hess <Ely.hess@me.com>
2021-01-05 14:15:37 -0700

    Add namspace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :merchants
+    resources :invoices
+  end
+  
+  resources :merchants do
+    resources :items
+    resources :invoices
+  end
+
+  resources :admin, controller: 'admin/dashboard', only: [:index]
 end
