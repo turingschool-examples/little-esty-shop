11652179b4b937668c6825a5dac2babba5e6ef66
Adam Etzion <adametzion90@gmail.com>
2021-01-05 15:02:02 -0700

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_212832) do
+ActiveRecord::Schema.define(version: 2021_01_05_214043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,18 @@ ActiveRecord::Schema.define(version: 2021_01_05_212832) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "invoice_items", id: :serial, force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "invoice_id"
+    t.integer "quantity"
+    t.integer "unit_price"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
+    t.index ["item_id"], name: "index_invoice_items_on_item_id"
   end
 
   create_table "invoices", id: :serial, force: :cascade do |t|
@@ -48,7 +60,20 @@ ActiveRecord::Schema.define(version: 2021_01_05_212832) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "transactions", id: :serial, force: :cascade do |t|
+    t.bigint "invoice_id"
+    t.integer "credit_card_number"
+    t.datetime "credit_card_expiration_date"
+    t.integer "result"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
+  end
+
+  add_foreign_key "invoice_items", "invoices"
+  add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
+  add_foreign_key "transactions", "invoices"
 end
