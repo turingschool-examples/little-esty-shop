c3f5ee06a35f2e32e3999920de866f1421bb8c4c
Ely Hess <Ely.hess@me.com>
2021-01-06 11:20:45 -0700

    Incomplete incomplete_invoices

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :item do
     name { Faker::Beer.name }
     description { Faker::Beer.style }
-    unit_price { Faker::Number.decimal.(l_digits: 2, r_digits: 2) }
+    unit_price { Faker::Number.number(digits: 3) }
   end
 end
