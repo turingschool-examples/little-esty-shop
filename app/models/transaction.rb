dc5a4baf97a09f924b5f1cabc0006e1885d125a6
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-05 18:04:07 -0700

    Finish tests for validations and relationships

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
-
+  
+  validates_presence_of :credit_card_number, :credit_card_expiration_date, :result
   enum result: [:failed, :success]
 end
