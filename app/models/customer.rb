e7cef48e5350ae371f358df6b403848495a42677
Ely Hess <Ely.hess@me.com>
2021-01-05 17:42:49 -0700

    Add top_five_customers method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,14 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
   has_many :invoice_items, through: :invoices
   has_many :items, through: :invoice_items
+
+  def self.top_five_customers
+   joins(:transactions)
+   .select("customers.*, count('transactions.result') AS transaction_count")
+   .group(:id)
+   .where('transactions.result = ?', 1)
+   .order(transaction_count: :desc)
+   .limit(5)
+  end
+  
 end
