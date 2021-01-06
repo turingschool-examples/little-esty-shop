f35456cdb056dbf11d5e2c1716cb73e1d86f2f93
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-05 18:00:43 -0700

    Create relationships and validations test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+  it { should have_many :invoices}
+  end
+  describe 'validations' do
+    it { should validate_presence_of :first_name}
+    it { should validate_presence_of :last_name}
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :merchant}
+  end
+  describe 'validations' do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :description}
+    it { should validate_presence_of :unit_price}
+  end
 end
