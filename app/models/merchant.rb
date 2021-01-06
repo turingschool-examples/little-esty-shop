aab271dd4d7b457a9afb9b991638b214c64c1825
Ryan Barnett <ryanbarnett883@gmail.com>
2021-01-05 20:50:04 -0700

    Delete commented code in Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,2 @@
 class Merchant < ApplicationRecord
-  # require 'csv'
-
-  # def self.import(file)
-  #   CSV.foreach(file.path, headers: true) do |row|
-  #     Merchant.create! row.to_hash 
-  #   end
-  # end
 end
