ea5778d579bcece636927d64f95ea883219bee9f
Cydnee Owens <cks410@yahoo.com>
2021-01-06 14:59:18 -0700

    Add dashboard links feature test

---
+++ spec/features/admin/dashboard_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Dashboard' do
+  describe 'As an admin user' do
+    it "when I visit the admin dashboard I see a header" do 
+
+      visit "/admin"
+
+      expect(page).to have_content("Admin Dashboard")
+    end
+  
+    it 'I see a link to the admin merchants and invoices index' do
+      visit admin_path
+
+      expect(page).to have_link('Merchants')
+      expect(page).to have_link('Invoices')
+      
+      click_link 'Merchants'
+      
+      expect(current_path).to eq('/admin/merchants')
+    end
+  end 
+end 
