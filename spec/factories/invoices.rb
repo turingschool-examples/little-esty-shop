6ceeb75afa1c23a9a4394dd80d56028c3b2add2b
Phil McCarthy <philmcc2631@gmail.com>
2021-01-05 17:19:11 -0700

    Update factories to include parent relationships

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :invoice do
     status { Faker::Number.between(from: 0, to: 2) }
+    merchant
+    customer
   end
 end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     credit_card_number { Faker::Number.number(digits: 16) }
     credit_card_expiration_date { Faker::Date.between(from: Date.today, to: 1.year.from_now) }
     result { Faker::Number.between(from: 0, to: 1) }
+    invoice
   end
 end
