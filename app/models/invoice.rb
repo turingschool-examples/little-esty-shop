c9cc9e5f22d5ef625b4501ff7a7ab7a60ca891d2
Cydnee Owens <cks410@yahoo.com>
2021-01-06 16:25:08 -0700

    add invoice validations/relationships code/tests

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,6 @@
 class Invoice < ApplicationRecord
+  validates_presence_of :status, :customer_id, :merchant_id
   belongs_to :customer
   belongs_to :merchant
+  has_many :transactions, dependent: :destroy
 end
