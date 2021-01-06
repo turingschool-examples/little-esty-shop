3f8a199095a7b735235b3ae3fc573c28c3f08f20
Max Ribbans <ribbans.max@gmail.com>
2021-01-05 20:10:37 -0500

    Fix some things

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :merchant do
+  resources :merchants do
     member do
       get 'dashboard'
     end
