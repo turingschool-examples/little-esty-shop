d0e7b5a33a444d9f5e6dff886eb9f3b2433db42e
Adam Etzion <adametzion90@gmail.com>
2021-01-06 15:41:39 -0700

    Test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :admin, :only => [index]
+
+  namespace :admin do
+    resources :merchants, :only => [:index, :show] 
+    resources :invoices, :only => [:index, :show]
+
+    /admin/invoices/invoice_id
+  end
+
+  # get top 5 customers
+
+  # member do
+  #   get :delete
+  # end
+
+
 end
