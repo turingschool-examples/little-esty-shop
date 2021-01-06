a42b767158c2749c0e8c04e6c1cbb5e098f5dfb9
Miquel <foymikek@gmail.com>
2021-01-05 20:18:25 -0700

    Add passing merchant validation test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Merchant, type: :model do
   describe 'relationship' do
     it { should have_many :items}
   end
+
+  describe  'validations' do
+    it { should validate_presence_of :name }
+  end
 end
