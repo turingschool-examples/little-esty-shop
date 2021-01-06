963c2aaabbe351e21d0cc376990ec210eb3636d0
Catherine Dean <catherinemdean15@gmail.com>
2021-01-05 18:56:10 -0700

    add factorybot files and info

---
+++ spec/factories.rb
@@ -0,0 +1,37 @@
+FactoryBot.define do
+  factory :customer do
+    first_name {Faker::Name.first_name}
+    last_name {Faker::Dessert.variety}
+  end
+
+  factory :invoice do
+    status {[0,1,2].sample}
+    merchant
+    customer
+  end
+
+  factory :merchant do
+    name {Faker::Space.galaxy}
+    invoices
+    items
+  end
+
+  factory :item do
+    name {Faker::Coffee.variety}
+    description {Faker::Hipster.sentence}
+    unit_price {Faker::Number.decimal(l_digits: 2)}
+    merchant
+  end
+
+  factory :transaction do
+    result {[0,1].sample}
+    credit_card_number {Faker::Finance.credit_card}
+    invoice
+  end
+
+  factory :invoice_item do
+    status {[0,1,2].sample}
+    merchant
+    invoice
+  end
+end
