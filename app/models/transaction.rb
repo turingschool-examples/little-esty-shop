80f612d2bab214f8637d0573698a7c9c8fd6ee84
Catherine Dean <catherinemdean15@gmail.com>
2021-01-05 18:59:01 -0700

    remove credit_card_expiration_date validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,7 @@
 class Transaction < ApplicationRecord
   validates_presence_of :invoice_id,
                         :credit_card_number,
-                        :credit_card_expiration_date,
                         :result
 
-  belongs_to :invoice 
+  belongs_to :invoice
 end
