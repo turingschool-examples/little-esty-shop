3ffb5f3c2c0bac83b8b0174c4720ed4e3bd8c1a4
Saundra <saundracatalina@gmail.com>
2021-01-06 15:57:46 -0700

    Add merchant dashboard items index link test

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'merchant dashboard' do
-  before :each do 
+  before :each do
     @merchant1 = Merchant.create!(name: 'Name')
     visit merchant_dashboard_index_path(@merchant1)
   end
@@ -9,4 +9,14 @@ RSpec.describe 'merchant dashboard' do
   it 'shows the merchant name' do
     expect(page).to have_content(@merchant1.name)
   end
+
+  it 'can see a link to my merchant items index' do
+    expect(page).to have_link("Items")
+
+    click_link "Items"
+
+    expect(current_path).to eq("/merchant/#{@merchant1.id}/items")
+  end
+
+  # expect(page).to have_link("Invoices")
 end
