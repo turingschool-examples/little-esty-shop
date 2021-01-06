f4771ed1792ef49b614ca4e6590bca01c54797ac
Saundra <saundracatalina@gmail.com>
2021-01-06 15:58:55 -0700

    Add items index route, controller, and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchant, only: [:show] do
     resources :dashboard, only: [:index]
+    resources :items, only: [:index]
   end
 end
