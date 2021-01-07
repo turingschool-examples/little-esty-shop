cf5787940b9a27a52180ad098293843838cc531c
Isabelle Villasenor <villasenori@outlook.com>
2021-01-07 06:22:47 -0700

    Add Routes for Admin Merchants and Admin Invoices

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   namespace :admin do 
     resources :dashboard, only: [:index]
+    resources :merchants, only: [:index]
+    resources :invoices, only: [:index]
   end
 end
