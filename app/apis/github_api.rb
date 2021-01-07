81909d299e4c2488f15c11da00250f6d81b0f367
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-07 10:20:25 -0700

    Fix class method for API

--- app/apis/github_api.rb
+++ app/apis/github_api.rb
@@ -19,5 +19,8 @@ class GithubApi
     response = Faraday.get 'https://api.github.com/repos/aidenmendez/little-esty-shop/commits?per_page=1&sha=2a81ea51cafe120b43d07cc5108581149c61168e'
     body = response.body
     parsed = JSON.parse(response.body, symbolize_names: true)
+    parsed.count do |response|
+      response[:commit]
+    end
   end
 end
