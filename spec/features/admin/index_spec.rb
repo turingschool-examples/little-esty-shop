5a442d2a34560be1f2a8e8362c1301cce8da52be
Grayson Myers <graymyers10@gmail.com>
2021-01-06 18:03:39 -0700

    Add test for incomplete invoices section of admin dashboard page

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,32 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "Admin Dashboard" do 
-  describe "visit admin dashboard" do 
-    it "can see a header indicating admin dashboard" do 
+RSpec.describe "Admin Dashboard" do
+  describe "visit admin dashboard" do
+    it "can see a header indicating admin dashboard" do
       visit "/admin"
 
       expect(page).to have_content("admin dashboard")
     end
   end
 
-  describe "admin dashboard links" do 
-    it "can see links to admin merchants index" do 
+  describe "admin dashboard links" do
+    it "can see links to admin merchants index" do
       visit "/admin"
       click_on 'merchants'
       expect(current_path).to eq(admin_merchants_path)
     end
   end
 
-  describe "admin dashboard links" do 
-    it "can see links to admin invoices index" do 
+  describe "admin dashboard links" do
+    it "can see links to admin invoices index" do
       visit "/admin"
       click_on 'invoices'
       expect(current_path).to eq(admin_invoices_path)
     end
   end
 
-  describe "dashboard top customers" do 
-    before (:each) do 
+  describe "dashboard top customers" do
+    before (:each) do
       @customers = []
       10.times { @customers << create(:customer) }
       @customers.each do |customer|
@@ -39,7 +39,7 @@ RSpec.describe "Admin Dashboard" do
       fifth_customer = @customers[4].invoices[0].id
       sixth_customer = @customers[5].invoices[0].id
       seventh_customer = @customers[6].invoices[0].id
-      
+
       10.times { create(:transaction, invoice_id:first_customer, result: "success")}
       9.times { create(:transaction, invoice_id:second_customer, result: "success")}
       8.times { create(:transaction, invoice_id:third_customer, result: "success")}
@@ -48,7 +48,7 @@ RSpec.describe "Admin Dashboard" do
       5.times { create(:transaction, invoice_id:sixth_customer, result: "success")}
       13.times { create(:transaction, invoice_id:seventh_customer, result: "failed")}
       visit "/admin"
-      
+
       @customer1 = page.find("#customer-#{@customers[0].id}")
       @customer2 = page.find("#customer-#{@customers[1].id}")
       @customer3 = page.find("#customer-#{@customers[2].id}")
@@ -56,9 +56,9 @@ RSpec.describe "Admin Dashboard" do
       @customer5 = page.find("#customer-#{@customers[4].id}")
     end
 
-    it "can see the names of top 5 customers and the number of successful transactions" do 
+    it "can see the names of top 5 customers and the number of successful transactions" do
       @customers[0..4].each do |customer|
-        within("#customer-#{customer.id}") do 
+        within("#customer-#{customer.id}") do
           expect(page).to have_content(customer.first_name)
           expect(page).to have_content(customer.last_name)
           expect(page).to have_content(customer.number_of_successful_transactions)
@@ -66,7 +66,7 @@ RSpec.describe "Admin Dashboard" do
       end
     end
 
-    it "orders customers by number of successful transactions" do 
+    it "orders customers by number of successful transactions" do
       expect(@customer1).to appear_before(@customer2)
       expect(@customer2).to appear_before(@customer3)
       expect(@customer3).to appear_before(@customer4)
@@ -74,19 +74,36 @@ RSpec.describe "Admin Dashboard" do
       expect(page).to_not have_css("#customer-#{@customers[5].id}")
       expect(page).to_not have_css("#customer-#{@customers[6].id}")
     end
+
+
   end
 
-  describe "dashboard incomplete invoices" do 
-    it "can see a list of ids of all invoices that have items that have not been shipped" do 
+  describe "dashboard incomplete invoices" do
+    before (:each) do
+      @customers = []
+      4.times { @customers << create(:customer) }
+
+      @invoice1 = create(:invoice, customer_id:@customers[0].id,status: "in progress")
+      @invoice2 = create(:invoice, customer_id:@customers[1].id,status: "in progress")
+
+      @invoice3 = create(:invoice, customer_id:@customers[2].id,status: "completed")
+      @invoice4 = create(:invoice, customer_id:@customers[3].id,status: "cancelled")
+    end
+
+    it "can see a list of ids of all invoices that have items that have not been shipped" do
       visit "/admin"
 
-      within("#Incomplete Invoices") do 
-        #ids of invoices that have status of in progress 0
-        #invoice_items that have status pending or packaged (not shipped)
+      within("#Incomplete Invoices") do
+        expect(page).to have_content(@invoice1.id)
+        expect(page).to have_content(@invoice2.id)
+
+        expect(page).to have_no_content(@invoice3.id)
+        expect(page).to have_no_content(@invoice4.id)
+
       end
     end
 
-    it "can see each invoice id links to that invoice's admin show page" do 
+    it "can see each invoice id links to that invoice's admin show page" do
 
     end
   end
