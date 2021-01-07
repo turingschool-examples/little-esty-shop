9cbe4674574e70f165edb69a433a232b871961e1
Robert Heath <robert.b.heathii@gmail.com>
2021-01-07 13:03:29 -0600

    Create show page with dynamic attributes for items

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'merchant/:merchant_id/items', to: 'merchant_items#index'
+  get '/merchant/:merchant_id/items/:item_id', to: 'merchant_items#show'
+
   resources :merchants, only: [:index, :show] do
     resources :items, only: [:index]
     resources :invoices, only: [:index]
