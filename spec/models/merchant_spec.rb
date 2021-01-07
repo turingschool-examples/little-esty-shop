c92fbddcba8306d67d766beb9927e6f40f7efdf6
Saundra <saundracatalina@gmail.com>
2021-01-06 17:41:05 -0700

    Add relationships between merchants and customers through invoices

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe Merchant, type: :model do
   describe "relationships" do
     it { should have_many :invoices }
     it { should have_many :items }
+    it { should have_many(:customers).through(:invoices) }
   end
 end
