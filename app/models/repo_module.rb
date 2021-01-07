4d2f8efda0879100a082a8f4fe56763c0b6db34f
Adam Etzion <adametzion90@gmail.com>
2021-01-07 09:20:06 -0700

    Add create repo model and module

---
+++ app/models/repo_module.rb
@@ -0,0 +1,38 @@
+module RepoSearch
+
+    def repo
+        get url("/repos/aetzion1/little-esty-shop")
+    end
+
+    def collaborators
+        get url("/repos/aetzion1/little-esty-shop/collaborators")
+    end
+    
+    def repo_info(url)
+        # response = Faraday.get("https://api.github.com#{url}"
+        response = Faraday.get("https://api.github.com/repos/aetzion1/little-esty-shop"
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        parsed.map do |data|
+            Repo.new(data)
+        end
+    end
+end
+
+# class RepoInfo
+#     def repos
+#         service.repos.map do |data|
+#             Repo.new(data)
+#         end
+#     end
+
+#     def service
+#         EstyService.new
+#     end
+# end
+
+# class EstyService
+#     def repos
+#         response = Faraday.get 'https://api.github.com/repos/aetzion1/little-esty-shop'
+#         parsed = JSON.parse(response.body, symbolize_names: true)
+#     end
+# end
