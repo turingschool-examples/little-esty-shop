0f53d996b357fdfcac0d3c8eee4b8fab1601c2a5
Grayson Cunningham <gecun289@gmail.com>
2021-01-06 20:56:26 -0700

    add test to ensure items to ship are sorted in order of invoice creation

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -60,30 +60,43 @@ RSpec.describe "Merchant Dashboard" do
         end
       end
 
-      it 'items ready to ship' do
-        items = create_list(:item, 6, merchant: merchant1)
-        items.first(4).each do |item|
-          invoice = create(:invoice, merchant: merchant1, id: item.id)
-          create(:invoice_item, item: item, invoice: invoice, status: 1)
+      describe 'items ready to ship' do
+        before :each do
+          @items = create_list(:item, 6, merchant: merchant1)
+          @items.first(4).each_with_index do |item, index|
+            invoice = create(:invoice, merchant: merchant1, id: item.id, created_at: (Date.today - index))
+            create(:invoice_item, item: item, invoice: invoice, status: 1)
+          end
         end
 
-        visit dashboard_merchant_path(merchant1)
+        it 'displays items that are ready to ship' do
+          visit dashboard_merchant_path(merchant1)
 
-        expect(page).to have_content("Items Ready to Ship")
-        within "#items_to_ship" do
-          ready = items.first(4)
-          not_ready = items.last(2)
-          ready.each_with_index do |item, index|
-            within "#item-#{index}" do
-              expect(page).to have_content(item.name)
-              invoice = Invoice.find(item.id)
-              expect(page).to have_link("Invoice ##{invoice.id}", href: merchant_invoice_path(merchant1.id, invoice.id))
-              expect(page).to have_content(invoice.created_at.strftime("%A, %B %-d, %Y"))
+          expect(page).to have_content("Items Ready to Ship")
+          within "#items_to_ship" do
+            ready = @items.first(4).reverse
+            not_ready = @items.last(2)
+            ready.each_with_index do |item, index|
+              within "#item-#{index}" do
+                expect(page).to have_content(item.name)
+                invoice = Invoice.find(item.id)
+                expect(page).to have_link("Invoice ##{invoice.id}", href: merchant_invoice_path(merchant1.id, invoice.id))
+                expect(page).to have_content(invoice.created_at.strftime("%A, %B %-d, %Y"))
+              end
+            end
+            not_ready.each do |item|
+              expect(page).not_to have_content(item.name)
             end
           end
-          not_ready.each do |item|
-            expect(page).not_to have_content(item.name)
-          end
+        end
+
+        it 'displays items ordered by invoice creation date' do
+          visit dashboard_merchant_path(merchant1)
+
+          invoices = Invoice.order(:created_at)
+          expect("Invoice ##{invoices[0].id}").to appear_before("Invoice ##{invoices[1].id}")
+          expect("Invoice ##{invoices[1].id}").to appear_before("Invoice ##{invoices[2].id}")
+          expect("Invoice ##{invoices[2].id}").to appear_before("Invoice ##{invoices[3].id}")
         end
       end
     end
