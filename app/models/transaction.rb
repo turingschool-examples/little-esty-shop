ee3eec91aed9ba01fb2f707cc90b4b3f35f67eb7
Isabelle Villasenor <villasenori@outlook.com>
2021-01-07 07:11:12 -0700

    Add enum to Transaction Model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,7 @@ class Transaction < ApplicationRecord
   validates_presence_of :invoice_id,
                         :credit_card_number,
                         :result
+  enum result: [:failed, :success]
 
   belongs_to :invoice
 end
