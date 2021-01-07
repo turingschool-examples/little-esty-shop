52420016780bff45ebaf7cf8bf2881f940c87140
Adam Etzion <adametzion90@gmail.com>
2021-01-06 20:53:01 -0700

    Add attempt to test layout

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "the Admin Dashboard" do
+    # before { visit '/admin' }
+    # page.should render_template("layouts/admin")
+
     it "should display a header indicating you are on the admin dashboard" do
         visit '/admin'
 
-        within("#header") do
+        within('header') do
             expect(page).to have_content("Admin Dashboard")
         end
     end
@@ -12,7 +15,8 @@ RSpec.describe "the Admin Dashboard" do
     it "header should have links to the admin, admin merchants, and admin invoices indexes" do
         visit '/admin'
 
-        within("#header") do
+        # find_link('Admin').visible?
+        within('header') do
             expect(page).to have_link("Admin")
             expect(page).to have_link("Merchants")
             expect(page).to have_link("Invoices")
