3bafc8bc5b88b81df6c5d1e6c88b615fd17ba268
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-07 12:22:39 -0700

    Merge issue

--- spec/features/admin/dashboard/dashboard_spec.rb
+++ spec/features/admin/dashboard/dashboard_spec.rb
@@ -11,6 +11,11 @@ describe 'As an Admin' do
       @customer_4 = Customer.create!(first_name: 'Tran', last_name: 'Smith')
       @customer_5 = Customer.create!(first_name: 'Mary', last_name: 'Dumas')
       @customer_6 = Customer.create!(first_name: 'James', last_name: 'Core')
+
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, merchant_id: @merchant.id, status: 0)
+      @invoice_2 = Invoice.create!(customer_id: @customer_1.id, merchant_id: @merchant.id, status: 1)
+      @invoice_3 = Invoice.create!(customer_id: @customer_1.id, merchant_id: @merchant.id, status: 2)
+      @invoice_4 = Invoice.create!(customer_id: @customer_1.id, merchant_id: @merchant.id, status: 0)
       end
 
     it 'I see Im in the admins dashboard with links to mechants and invoices' do
@@ -19,6 +24,16 @@ describe 'As an Admin' do
       expect(page).to have_content('Admin Dashboard')
       expect(page).to have_link("Admin Merchants")
       expect(page).to have_link("Admin Invoices")
+    end
+
+    it 'I see a section for Incomplete Invoices' do
+
+    visit admin_index_path
+
+    expect(page).to have_content(@invoice_1.id)
+    expect(page).to have_content(@invoice_4.id)
+    expect(page).to_not have_content(@invoice_2.id)
+    expect(page).to_not have_content(@invoice_3.id)
     end
   end
 end
