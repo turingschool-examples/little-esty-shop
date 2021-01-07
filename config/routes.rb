2e25ec3a02a9752e195ff30bcc398d26a3e45922
Isabelle Villasenor <villasenori@outlook.com>
2021-01-07 06:09:48 -0700

    Add Admin Dashboard Route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
     resources :items, only: [:index]
     resources :invoices, only: [:index]
   end
+
+  namespace :admin do 
+    resources :dashboard, only: [:index]
+  end
 end
