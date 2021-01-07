52fcdd660cf182caf5d6264580efdb45d8aa791e
Saundra <saundracatalina@gmail.com>
2021-01-06 17:41:52 -0700

    Add relationships between customers and merchants through invoices

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,6 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name,
                         :last_name
 
-  has_many :invoices 
+  has_many :invoices
+  has_many :merchants, through: :invoices
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,10 @@ class Merchant < ApplicationRecord
 
   has_many :invoices
   has_many :items
+  has_many :customers, through: :invoices 
+
+  def favorite_customers
+    require "pry"; binding.pry
+    customers
+  end
 end
