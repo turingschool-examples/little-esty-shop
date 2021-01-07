ad052a884fe10555a1fad9b19b6fab22b8e5a0d6
Liam Cusack <LCusack8108@gmail.com>
2021-01-06 17:11:45 -0700

    Fix/finish merchant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :merchants, only: [:index, :show]
+  resources :merchants, only: [:index, :show] do
+    resources :items, only: [:index]
+    resources :invoices, only: [:index]
+    resources :dashboard, only: [:index]
+  end
 end
