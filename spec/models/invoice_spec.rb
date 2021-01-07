8bd3d2149b4d715823b1946c10a398d7dbbbccd8
Grayson Myers <graymyers10@gmail.com>
2021-01-06 20:35:13 -0700

    Add test for ordering by recency to incomplete_invoices method

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,12 +7,17 @@ describe Invoice, type: :model do
 
   describe "class methods" do
     it "incomplete_invoices" do
+      require "date"
       complete_invoices = []
       2.times { complete_invoices << create(:invoice, status:"completed") }
       cancelled_invoices = []
       2.times { cancelled_invoices << create(:invoice, status:"cancelled") }
       in_progress_invoices = []
       2.times { in_progress_invoices << create(:invoice, status:"in progress") }
+      expect(Invoice.incomplete_invoices.to_a).to eq(in_progress_invoices)
+
+      tommorow = (Date.today + 1).to_datetime + Rational(11, 24) #this is horrible but it works
+      2.times { in_progress_invoices << create(:invoice, status:"in progress", created_at: tommorow) }
       expect(Invoice.incomplete_invoices.to_a).to eq(in_progress_invoices)
     end
   end
