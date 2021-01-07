3139f108fb16a8b840e17a025423b32dfe06621f
Grayson Myers <graymyers10@gmail.com>
2021-01-06 18:15:05 -0700

    Add incomplete_invoices class method to Invoice model

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -5,6 +5,15 @@ describe Invoice, type: :model do
     it {should define_enum_for(:status).with ['in progress', 'completed', 'cancelled'] }
   end
 
+  describe "class methods" do
+    it "incomplete_invoices" do
+      complete_invoices = create(:invoice, status:"completed")
+      cancelled_invoices = create(:invoice, status:"cancelled")
+      in_progress_invoices = create(:invoice, status:"in progress")
+      expect(Invoice.incomplete_invoices.to_a).to eq(in_progress_invoices)
+    end
+  end
+
   describe "relations" do
     it {should belong_to :customer}
     it {should belong_to :merchant}
