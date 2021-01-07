5687d2327a07eaa32c4296871fbcd23dc05c8b4a
Isabelle Villasenor <villasenori@outlook.com>
2021-01-07 06:22:23 -0700

    Add Links to Admin Merchants/Invoices to Features Spec

--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 describe 'Admin Dashboard Index Page' do
-  it 'should display a header indicating the admin dashboard' do
+  before :each do 
     visit admin_dashboard_index_path
+  end
 
+  it 'should display a header indicating the admin dashboard' do
     expect(page).to have_content('Admin Dashboard')
+  end
+
+  it 'should have link to admin merchant index' do
+    expect(page).to have_link('Merchants')
+
+    click_link 'Merchants'
+    expect(current_path).to eq(admin_merchants_path)
+  end
+
+  it 'should have link to admin invoice index' do
+    expect(page).to have_link('Invoices')
+
+    click_link 'Invoices'
+    expect(current_path).to eq(admin_invoices_path)
   end
 end
