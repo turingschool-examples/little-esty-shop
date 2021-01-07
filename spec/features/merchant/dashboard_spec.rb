f44551da3473d8f651b2edb6189b493dce90dfde
RyanDBarnett <conner2468@gmail.com>
2021-01-06 17:48:05 -0700

    Add merchant dash links user story 2

--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant Dashboard' do
   describe 'As a merchant' do
-    describe 'When I visit my merchant dashboard (/merchants/merchant_id/dashboard)'
-    it "Then I see the name of my merchant" do
+    describe 'When I visit my merchant dashboard (/merchants/merchant_id/dashboard)' do
+      before(:each) do
+        @max = Merchant.create!(name: 'Merch Max')
+      end
 
-      max = Merchant.create!(name: 'Merch Max')
+      it "Then I see the name of my merchant" do
+        visit "/merchants/#{@max.id}/dashboard"
 
-      visit "/merchants/#{max.id}/dashboard"
+        expect(page).to have_content(@max.name)
+      end
 
-      expect(page).to have_content(max.name)
+      it "Then I see links to my merchant items and invoices indexes (/merchants/merchant_id/items)" do
+        visit "/merchants/#{@max.id}/dashboard"
+
+        expect(page).to have_link('Items', href: "/merchants/#{@max.id}/items")
+        expect(page).to have_link('Invoices', href: "/merchants/#{@max.id}/invoices")
+      end
     end
   end
 end
