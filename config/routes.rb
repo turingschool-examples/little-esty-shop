4a835c53a1471efb48c4c1317f494d689866da48
Isabelle Villasenor <villasenori@outlook.com>
2021-01-08 14:51:48 -0700

    Add Admin Invoice Show Route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   namespace :admin do 
     resources :dashboard, only: [:index]
     resources :merchants, only: [:index]
-    resources :invoices, only: [:index]
+    resources :invoices, only: [:index, :show]
   end
 end
