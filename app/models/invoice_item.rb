0f96ebcf79056f0e29fc583ae1cf576f796ea8fd
Isabelle Villasenor <villasenori@outlook.com>
2021-01-08 15:22:02 -0700

    Add Ordering to Invoices

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -10,6 +10,6 @@ class InvoiceItem < ApplicationRecord
 
   def self.incomplete_invoices
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
-    Invoice.find(invoice_ids)
+    Invoice.order(created_at: :desc).find(invoice_ids)
   end
 end
