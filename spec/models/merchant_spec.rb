dbf6d78df434bf402a55e4976b8c0bbbe2b36084
Saundra <saundracatalina@gmail.com>
2021-01-07 18:22:16 -0700

    Add test for items_to_ship method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,37 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items }
     it { should have_many(:customers).through(:invoices) }
   end
+
+  describe "instance methods" do
+    before :each do
+      @merchant1 = Merchant.create!(name: 'Hair Care')
+
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @customer_2 = Customer.create!(first_name: 'Cecilia', last_name: 'Jones')
+      @customer_3 = Customer.create!(first_name: 'Mariah', last_name: 'Carrey')
+      @customer_4 = Customer.create!(first_name: 'Leigh Ann', last_name: 'Bron')
+      @customer_5 = Customer.create!(first_name: 'Sylvester', last_name: 'Nader')
+      @customer_6 = Customer.create!(first_name: 'Herber', last_name: 'Coon')
+
+      @invoice_1 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_1.id, status: 2)
+      @invoice_2 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_1.id, status: 2)
+      @invoice_3 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_2.id, status: 2)
+      @invoice_4 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_3.id, status: 2)
+      @invoice_5 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_4.id, status: 2)
+      @invoice_6 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_5.id, status: 2)
+      @invoice_7 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_6.id, status: 1)
+
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id)
+      @item_2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: @merchant1.id)
+      @item_3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: @merchant1.id)
+
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 0)
+      @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 1, unit_price: 8, status: 0)
+      @ii_3 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
+      @ii_4 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 1)
+    end
+    it "can list items ready to ship" do
+      expect(@merchant1.items_to_ship).to eq([@item_1, @item_2])
+    end
+  end
 end
