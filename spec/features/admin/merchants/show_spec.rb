13213473b0478d271430fc2cb8f913da8316ea25
Grayson Myers <graymyers10@gmail.com>
2021-01-07 17:02:09 -0700

    Add test for update merchant link

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -9,4 +9,9 @@ describe "Admin Merchants Show Page" do
   it "has the merchant's name displayed on the page" do
     expect(page).to have_content(@merchant.name)
   end
+
+  it "has a link to update merchant information" do 
+    click_on "Update Merchant Information"
+    expect(current_path).to eq(edit_admin_merchant_path(@merchant.id))
+  end
 end
