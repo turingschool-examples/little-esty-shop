69aadca1061a6901d99b1cd643411c1be23ac7c9
Isabelle Villasenor <villasenori@outlook.com>
2021-01-07 18:20:41 -0700

    Add enum

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,4 +8,6 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :transactions
+
+  enum status: [:cancelled, :in_progress, :complete]
 end
