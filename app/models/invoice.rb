eda23b66b34a25207ef0dcd3571e58076af32cdf
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-08 14:58:47 -0700

    Add has many transactions to Invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,9 +1,10 @@
 class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
+  has_many :transactions
   belongs_to :customer
   belongs_to :merchant
-  
+
   validates_presence_of :status
 
   enum status: ['in progress', :completed, :cancelled]
