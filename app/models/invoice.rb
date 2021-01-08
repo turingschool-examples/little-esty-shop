418b85546949457d993d77751133786039e638c3
Isabelle Villasenor <villasenori@outlook.com>
2021-01-08 08:18:00 -0700

    Add incomplete_invoices To Admin Dashboard Controller

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -12,7 +12,7 @@ class Invoice < ApplicationRecord
   enum status: [:cancelled, :in_progress, :complete]
 
   def incomplete_invoices
-    item_ids = InvoiceItem.where(status: 0).pluck(:item_id)
+    item_ids = InvoiceItem.where(status: 0 || status: 1).pluck(:item_id)
     Item.find(item_ids)
   end
 
