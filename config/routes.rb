5e8d92cae44ffa74adb997ce399ee6adc6c1af56
Ely Hess <Ely.hess@me.com>
2021-01-07 19:54:37 -0700

    add only/except routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :merchants
-    resources :invoices
+    resources :merchants, except: [:destoy]
+    resources :invoices, only: [:index, :show]
     resources :invoice_items, only: [:update]
   end
 
