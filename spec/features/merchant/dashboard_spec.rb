9bbd466553c077a20763cce25239d6ddf70a3c69
RyanDBarnett <conner2468@gmail.com>
2021-01-07 21:38:54 -0700

    Add merchant dashboard top 5 customers feature test

--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -21,15 +21,51 @@ RSpec.describe 'Merchant Dashboard' do
       end
 
       it "merchant dashboard displays top 5 customers with most succesful transactions" do
-        # As a merchant,
-        # When I visit my merchant dashboard
-        # Then I see the names of the top 5 customers
+        sally    = Customer.create!(first_name: 'Sally', last_name: 'Smith')
+        joel     = Customer.create!(first_name: 'Joel', last_name: 'Hansen')
+        john     = Customer.create!(first_name: 'John', last_name: 'Hansen')
+        travolta = Customer.create!(first_name: 'Travolta', last_name: 'Hansen')
+        sal      = Customer.create!(first_name: 'Sal', last_name: 'Hansen')
+        tim      = Customer.create!(first_name: 'Tim', last_name: 'Hansen')
+        amazon   = Merchant.create!(name: 'Amazon')
+        alibaba  = Merchant.create!(name: 'Alibaba')
+        invoice1 = Invoice.create!(status: 1, customer_id: sally.id, merchant_id: amazon.id)
+        invoice2 = Invoice.create!(status: 1, customer_id: sally.id, merchant_id: amazon.id)
+        invoice3 = Invoice.create!(status: 1, customer_id: joel.id, merchant_id: amazon.id)
+        invoice4 = Invoice.create!(status: 1, customer_id: joel.id, merchant_id: amazon.id)
+        invoice5 = Invoice.create!(status: 1, customer_id: john.id, merchant_id: amazon.id)
+        invoice6 = Invoice.create!(status: 1, customer_id: john.id, merchant_id: amazon.id)
+        invoice7 = Invoice.create!(status: 1, customer_id: travolta.id, merchant_id: amazon.id)
+        invoice8 = Invoice.create!(status: 1, customer_id: travolta.id, merchant_id: amazon.id)
+        invoice9 = Invoice.create!(status: 1, customer_id: sal.id, merchant_id: amazon.id)
+        invoice10 = Invoice.create!(status: 1, customer_id: sal.id, merchant_id: amazon.id)
+        invoice11 = Invoice.create!(status: 1, customer_id: tim.id, merchant_id: amazon.id)
+        invoice13 = Invoice.create!(status: 0, customer_id: sally.id, merchant_id: amazon.id)
+        invoice14 = Invoice.create!(status: 0, customer_id: sally.id, merchant_id: alibaba.id)
+        tx1      = Transaction.create!(result: "success", credit_card_number: 010001001022, credit_card_expiration_date: 20251001, invoice_id: invoice2.id,)
+        tx2      = Transaction.create!(result: "success", credit_card_number: 010001005555, credit_card_expiration_date: 20220101, invoice_id: invoice1.id,)
+        tx3      = Transaction.create!(result: "success", credit_card_number: 010001005551, credit_card_expiration_date: 20220101, invoice_id: invoice3.id,)
+        tx4      = Transaction.create!(result: "success", credit_card_number: 010001005552, credit_card_expiration_date: 20220101, invoice_id: invoice4.id,)
+        tx5      = Transaction.create!(result: "success", credit_card_number: 010001005553, credit_card_expiration_date: 20220101, invoice_id: invoice5.id,)
+        tx6      = Transaction.create!(result: "success", credit_card_number: 010001005554, credit_card_expiration_date: 20220101, invoice_id: invoice6.id,)
+        tx7      = Transaction.create!(result: "success", credit_card_number: 010001005550, credit_card_expiration_date: 20220101, invoice_id: invoice7.id,)
+        tx8      = Transaction.create!(result: "success", credit_card_number: 010001005556, credit_card_expiration_date: 20220101, invoice_id: invoice8.id,)
+        tx9      = Transaction.create!(result: "success", credit_card_number: 010001005557, credit_card_expiration_date: 20220101, invoice_id: invoice9.id,)
+        tx10     = Transaction.create!(result: "success", credit_card_number: 010001005523, credit_card_expiration_date: 20220101, invoice_id: invoice10.id,)
+        tx11     = Transaction.create!(result: "success", credit_card_number: 0100010055, credit_card_expiration_date: 20220101, invoice_id: invoice11.id,)
+        tx12     = Transaction.create!(result: "failure", credit_card_number: 0100010055, credit_card_expiration_date: 20220101, invoice_id: invoice14.id,)
 
-        # who have conducted the largest number of successful transactions with my merchant
-        # And next to each customer name I see the number of successful transactions they have
-        # conducted with my merchant
+        visit "/merchants/#{amazon.id}/dashboard"
+        
+        within('.merchant-top-5-customers-list') do
+          expect(page).to have_content("#{sally.first_name}: 2")
+          expect(page).to have_content("#{joel.first_name}: 2")
+          expect(page).to have_content("#{john.first_name}: 2")
+          expect(page).to have_content("#{travolta.first_name}: 2")
+          expect(page).to have_content("#{sal.first_name}: 2")
+          expect(page).to_not have_content("#{tim.first_name}: 2")
+        end
       end
-
     end
   end
 end
