e0ff0afc3354c8026b2ce8d4c320eae0719da95e
Isabelle Villasenor <villasenori@outlook.com>
2021-01-08 14:53:05 -0700

    Move Method to InvoiceItem

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -11,9 +11,5 @@ class Invoice < ApplicationRecord
 
   enum status: [:cancelled, :in_progress, :complete]
 
-  def incomplete_invoices
-    item_ids = InvoiceItem.where(status: 0 || status: 1).pluck(:item_id)
-    Item.find(item_ids)
-  end
 
 end
