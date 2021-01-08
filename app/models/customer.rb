11194d3479fe1c2d4c5c5c0f272bebd6c72c0456
Liam Cusack <LCusack8108@gmail.com>
2021-01-07 20:04:28 -0700

    Add working user story 3, new model method for customer and merchant. Testing comes next

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
     has_many :invoices
     has_many :merchants, through: :invoices
+
+    def self.find_name_for_merchant(id)
+      where(id: id).pluck(:first_name, :last_name).flatten.join(" ")
+    end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,7 @@ class Merchant < ApplicationRecord
     has_many :customers, through: :invoices
     has_many :items
 
+    def best_customers
+      self.invoices.group(:customer_id).where(status: 1).count.sort_by{|k, v| v}.reverse.first(5)
+    end
 end
