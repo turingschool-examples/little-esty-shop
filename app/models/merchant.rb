9ab2d992c9f1ffa89dd761a938589db799f27608
Grayson Myers <graymyers10@gmail.com>
2021-01-07 20:47:56 -0700

    Add name presence validation for merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,8 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :customers,-> {distinct}, through: :invoices
 
+  validates :name, presence: true
+
   delegate :favorite_customers, to: :customers
   delegate :items_to_ship, to: :items
 end
