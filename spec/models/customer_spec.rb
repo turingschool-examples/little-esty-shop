5bb751f654b0ddd75a4f6b60ad7d137804b6ebfc
Adam Etzion <adametzion90@gmail.com>
2021-01-07 21:13:31 -0700

    Add customer model class and instance method tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,5 +4,22 @@ RSpec.describe Customer, type: :model do
   describe "relationships" do
     it { should have_many :invoices }
     it { should have_many :merchants }
+    it { should have_many :transactions }
+    it { should have_many :invoice_items }
+    it { should have_many :items }
   end
+
+  describe "class methods" do
+    it "can get top 5 customers by successful transactions" do
+      expect(Customer.top_five.first).to eq(Customer.find(507))
+      expect(Customer.top_five.last).to eq(Customer.find(948))
+    end
+  end
+
+  describe "instance methods" do
+    it "can get count of successful transactions" do
+      expect(Customer.first.successful_count).to eq(7)
+    end
+  end
+
 end
