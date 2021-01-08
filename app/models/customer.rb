150132eb150b34ad72a47a5299a19ad4a40cff5a
Isabelle Villasenor <villasenori@outlook.com>
2021-01-07 18:19:09 -0700

    Add Top Customer Method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,14 @@ class Customer < ApplicationRecord
   has_many :invoices 
   has_many :transactions, through: :invoices
 
+
+
+  def self.top_customers
+    joins(:transactions)
+      .where('result = ?', 1)
+      .group(:id)
+      .select("customers.*, count('transactions.result') as top_result")
+      .order(top_result: :desc)
+      .limit(5)
+  end
 end
