58bbf57858e698c5f70a107b7d9360d0bf096c24
Grayson Cunningham <gecun289@gmail.com>
2021-01-07 20:21:29 -0700

    add form to edit items

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -1,9 +1,22 @@
 class Merchants::ItemsController < ApplicationController
+  before_action :get_item, only: [:show, :edit]
+
   def index
     @items = Merchant.find(params[:merchant_id]).items
   end
 
-  def show
+  def update
+    get_item.update(item_params)
+    flash.notice = "Item has been updated!"
+    redirect_to item_path(params[:id])
+  end
+
+  private
+  def get_item
     @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price)
   end
 end
