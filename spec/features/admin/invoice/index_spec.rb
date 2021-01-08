790973796a6941da325c47db455b8e4913434023
ninesky00 <jiangzho@gmail.com>
2021-01-08 17:53:52 -0600

    add test each id has a link that redirects to the invoice show page

--- spec/features/admin/invoice/index_spec.rb
+++ spec/features/admin/invoice/index_spec.rb
@@ -3,13 +3,25 @@ require 'rails_helper'
 RSpec.describe "admin invoices index" do
   before :each do 
     @invoices = FactoryBot.create_list(:invoice, 3)
+    visit admin_invoices_path
   end
 
   it "displays a list of all invoice ids in the system" do 
-    visit admin_invoices_path
+    within("#all-invoices") do 
+      @invoices.each do |invoice|
+        expect(page).to have_content(invoice.id)
+      end
+    end
+  end
 
+  it "has a link for each id that redirects to the invoice show page" do 
     @invoices.each do |invoice|
-      expect(page).to have_content(invoice.id)
+      within("#invoice-#{invoice.id}") do 
+        expect(page).to have_link("#{invoice.id}")
+        click_link "#{invoice.id}"
+        expect(current_path).to eq(admin_invoice_path(invoice.id))
+        visit admin_invoices_path
+      end
     end
   end
 end
