31937e9309f17ece7403b1129807618bbfb87770
Isabelle Villasenor <villasenori@outlook.com>
2021-01-08 21:34:35 -0700

    Add Update and Private Method to Admin Merchant Controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,9 +1,27 @@
 class Admin::MerchantsController < ApplicationController
+  before_action :set_merchant, only: [:show, :edit, :update]
   def index
     @merchants = Merchant.all
   end
 
   def show
+  end
+  
+  def edit
+  end
+
+  def update
+    @merchant.update(merchant_params)
+    flash.notice = 'Merchant Has Been Updated!'
+    redirect_to admin_merchant_path(@merchant)
+  end
+  
+  private
+  def set_merchant
     @merchant = Merchant.find(params[:id])
+  end
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
   end
 end
