b1fa0899a61fe4f03fb4067ac881f02170de86b2
Saundra <saundracatalina@gmail.com>
2021-01-09 13:40:37 -0700

    Add test to fill in form, submit, redirect, and see flash message

--- spec/features/items/edit_spec.rb
+++ spec/features/items/edit_spec.rb
@@ -17,4 +17,22 @@ describe "merchant items edit page" do
 
     expect(find_field('Name').value).to_not eq(@item_2.name)
   end
+
+  it "can fill in form, click submit, and redirect to that item's show page and see updated info and flash message" do
+    visit edit_merchant_item_path(@merchant1, @item_1)
+
+    fill_in "Name", with: "Bar Shampoo"
+    fill_in "Description", with: "Eco friendly shampoo"
+    fill_in "Unit price", with: "15"
+
+    click_button "Submit"
+
+    expect(current_path).to eq(merchant_item_path(@merchant1, @item_1))
+
+    expect(page).to have_content("Bar Shampoo")
+    expect(page).to have_content("Eco friendly shampoo")
+    expect(page).to have_content("15")
+    expect(page).to have_no_content("This washes your hair")
+    expect(page).to have_content("Succesfully Updated Item Info!")
+  end
 end
