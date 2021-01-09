ac624f21a6f54f0fe6f1b25750ca0b4b8f3d7c15
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-08 17:18:51 -0700

    Add sucessful transactions count model test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -36,5 +36,36 @@ RSpec.describe Customer, type: :model do
 
     expect(Customer.top_five).to eq([customer_1, customer_2, customer_3, customer_4, customer_5])
     end
+    describe 'instance methods' do
+      it 'shows number of sucessful transactions' do
+        merchant = Merchant.create!(name: 'House of thingys')
+
+        customer_1 = Customer.create!(first_name: 'John', last_name: 'Doe')
+        customer_2 = Customer.create!(first_name: 'Meg', last_name: 'Clain')
+        customer_3 = Customer.create!(first_name: 'Bob', last_name: 'Alves')
+        customer_4 = Customer.create!(first_name: 'Tran', last_name: 'Smith')
+        customer_5 = Customer.create!(first_name: 'Mary', last_name: 'Dumas')
+        customer_6 = Customer.create!(first_name: 'James', last_name: 'Core')
+
+        invoice_1 = Invoice.create!(customer_id: customer_1.id, merchant_id: merchant.id, status: 1)
+        invoice_2 = Invoice.create!(customer_id: customer_2.id, merchant_id: merchant.id, status: 1)
+        invoice_3 = Invoice.create!(customer_id: customer_3.id, merchant_id: merchant.id, status: 1)
+        invoice_4 = Invoice.create!(customer_id: customer_4.id, merchant_id: merchant.id, status: 1)
+        invoice_5 = Invoice.create!(customer_id: customer_5.id, merchant_id: merchant.id, status: 1)
+
+        transaction_1 = invoice_1.transactions.create!(credit_card_number: "315235234562", credit_card_expiration_date: "05/26/2023", result: 1)
+        transaction_2 = invoice_1.transactions.create!(credit_card_number: "315235234562", credit_card_expiration_date: "05/26/2023", result: 1)
+        transaction_3 = invoice_2.transactions.create!(credit_card_number: "315235234562", credit_card_expiration_date: "05/26/2023", result: 1)
+        transaction_4 = invoice_3.transactions.create!(credit_card_number: "315235234562", credit_card_expiration_date: "05/26/2023", result: 1)
+        transaction_5 = invoice_4.transactions.create!(credit_card_number: "315235234562", credit_card_expiration_date: "05/26/2023", result: 1)
+        transaction_6 = invoice_5.transactions.create!(credit_card_number: "315235234562", credit_card_expiration_date: "05/26/2023", result: 1)
+
+        expect(customer_1.sucessful_transactions_count).to eq(2)
+        expect(customer_2.sucessful_transactions_count).to eq(1)
+        expect(customer_3.sucessful_transactions_count).to eq(1)
+        expect(customer_4.sucessful_transactions_count).to eq(1)
+        expect(customer_5.sucessful_transactions_count).to eq(1)
+      end
+    end
   end
 end
