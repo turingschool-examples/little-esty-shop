ba1f83d84b5a81ce42bc22d15f858c7af83bf34b
Saundra <saundracatalina@gmail.com>
2021-01-09 13:41:39 -0700

    Add :update route and action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,4 +13,18 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def update
+    @item = Item.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+    @item.update(item_params)
+    flash.notice = "Succesfully Updated Item Info!"
+    redirect_to merchant_item_path(@merchant, @item)
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchant, only: [:show] do
     resources :dashboard, only: [:index]
-    resources :items, only: [:index, :show, :edit]
+    resources :items, only: [:index, :show, :edit, :update]
     resources :invoices, only: [:index, :show]
   end
 
