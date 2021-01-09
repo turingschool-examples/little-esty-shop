c6b3d6d0251b2824366353b747ff3c673226dd52
Robert Heath <robert.b.heathii@gmail.com>
2021-01-09 16:35:27 -0600

    Test for enable disable boxes on index page

--- spec/features/merchant_items/index_spec.rb
+++ spec/features/merchant_items/index_spec.rb
@@ -23,4 +23,34 @@ RSpec.describe "Merchant Items Index" do
       expect(page).not_to have_content(@item4.name)
     end
   end
+
+  describe 'each item should have an enable or a disable button' do
+    before :each do
+      @merchant1 = Merchant.create!(name: "Robert Heath")
+
+      @item1 = Item.create!(name: "magic pen", description: "special", unit_price: 9.10, enabled: true, merchant_id: @merchant1.id)
+      @item2 = Item.create!(name: "preposterous pencil", description: "big", unit_price: 8.50, enabled: true, merchant_id: @merchant1.id)
+      @item3 = Item.create!(name: "fantastic fountain pen", description: "small", unit_price: 55.00, merchant_id: @merchant1.id)
+    end
+
+    it 'has an enabled button for disabled items' do
+      visit "merchant/#{@merchant1.id}/items"
+
+      within("#item-#{@item3.id}") do
+        expect(page).to have_button('Enable')
+      end
+    end
+
+    it 'has a disable button for enabled items' do
+      visit "merchant/#{@merchant1.id}/items"
+      
+      within("#item-#{@item1.id}") do
+        expect(page).to have_button('Disable')
+      end
+
+      within("#item-#{@item2.id}") do
+        expect(page).to have_button('Disable')
+      end
+    end
+  end
 end
