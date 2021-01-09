9ebcebadbc79c1cc471c4f5d69bbf0b88f883de1
Saundra <saundracatalina@gmail.com>
2021-01-09 12:29:04 -0700

    Add item edit route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchant, only: [:show] do
     resources :dashboard, only: [:index]
-    resources :items, only: [:index, :show]
+    resources :items, only: [:index, :show, :edit]
     resources :invoices, only: [:index, :show]
   end
 
