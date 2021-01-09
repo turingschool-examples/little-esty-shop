714eda58235acc73d7d0fdcdedb2cc6cce31ba26
Robert Heath <robert.b.heathii@gmail.com>
2021-01-08 18:25:13 -0600

    Create tests and code for validations in item

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :unit_price, presence: true
+  validates :unit_price, numericality: { greater_than: 0}
+
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Item, type: :model do
     it { should have_many :invoices }
     it { should belong_to :merchant }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :unit_price }
+    it { should validate_numericality_of(:unit_price).is_greater_than(0) }
+  end
+
 end
