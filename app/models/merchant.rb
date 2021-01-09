fc22ca14ee9b4f074b2cfec8008ed4e91e946457
Catherine Dean <catherinemdean15@gmail.com>
2021-01-08 17:56:54 -0700

    update order of created_at

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,13 +5,21 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :customers, through: :invoices
 
-  # def favorite_customers
-  #
-  # end
+  def self.favorite_customers
+    joins(:transactions)
+    .where('result = ?', 1)
+    .select("customers.*, count('transactions.result') as top_result")
+    .order(top_result: :desc)
+    .limit(5)
+  end
+
+  def number_of_transactions(customer)
+    customer.transactions.where('result = ?', 1).count
+  end
 
   def ordered_items_to_ship
     item_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:item_id)
-    Item.order(created_at: :desc).find(item_ids)
+    Item.order(:created_at).find(item_ids)
   end
 
 end
--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe 'merchant dashboard' do
     expect(current_path).to eq("/merchant/#{@merchant1.id}/invoices")
   end
 
-  xit 'shows the names of the top 5 customers with successful transactions' do
+  it 'shows the names of the top 5 customers with successful transactions' do
     within("#customer-#{@customer_1.id}") do
       expect(page).to have_content(@customer_1.first_name)
       expect(page).to have_content(@customer_1.last_name)
