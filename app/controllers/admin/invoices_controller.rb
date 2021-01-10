157a01b8375544576ef4fb7136f1f39e29240d4e
ninesky00 <jiangzho@gmail.com>
2021-01-09 20:31:53 -0600

    add selector form for invoice status to view and update action to controller

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -1,9 +1,20 @@
 class Admin::InvoicesController < ApplicationController
   def index
-    @invoices = Invoice.all
+    @invoices = Invoice.all.order(:id)
   end
 
   def show
     @invoice = Invoice.find(params[:id])
+  end
+
+  def update
+    invoice = Invoice.find(params[:id])
+    invoice.update(invoice_items_params)
+    redirect_to admin_invoice_path(invoice)
+  end
+
+  private
+  def invoice_items_params
+    params.permit(:status)
   end
 end
