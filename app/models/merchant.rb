d634e9db679e747257e1b241159ab067ad6e0aef
Isabelle Villasenor <villasenori@outlook.com>
2021-01-10 09:27:37 -0700

    Remove Status Validation from Merchant Model and Spec

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,5 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
-  validates_numericality_of :status
 
   has_many :invoices
   has_many :items
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe Merchant do
   describe "validations" do
     it { should validate_presence_of :name }
-    it { should validate_numericality_of :status }
   end
   describe "relationships" do
     it { should have_many :invoices }
