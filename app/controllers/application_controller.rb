ddefb4011c014ab72d7c91d2fb5ee84f33bb92f8
ninesky00 <jiangzho@gmail.com>
2021-01-10 13:29:04 -0600

    extract call_github to poro singleton class API Info with method repo_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :call_github
 
   def call_github
-    conn = Faraday.new(url: 'https://api.github.com/repos/cunninghamge/little-esty-shop')
-    response = conn.get
-    body = JSON.parse(response.body, symbolize_names: true)
-    repo_name = body[:name]
-    binding.pry
+    @api_info = ApiInfo.instance
   end
 end
--- app/poros/api_info.rb
+++ app/poros/api_info.rb
@@ -1,3 +1,17 @@
+require 'singleton'
+
 class ApiInfo
-  
+  include Singleton
+
+  def initialize
+    @conn = Faraday.new(url: 'https://api.github.com/repos/cunninghamge/little-esty-shop')
+  end
+
+  def repo_name
+    response = @conn.get
+    body = JSON.parse(response.body, symbolize_names: true)
+    repo_name = body[:name]
+  end
+
+
 end
