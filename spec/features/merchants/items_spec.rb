c96700623a77acb296470ed038afea8ca15fb235
Grayson Cunningham <gecun289@gmail.com>
2021-01-09 22:25:21 -0700

    add test for top item's best day

--- spec/features/merchants/items_spec.rb
+++ spec/features/merchants/items_spec.rb
@@ -19,28 +19,50 @@ RSpec.describe "Items Index" do
       end
     end
 
-    it "the five most popular items, and their revenue, in order" do
-      merchant1 = create(:merchant)
-      item = create(:item, merchant: merchant1)
-      items = create_list(:item, 5, merchant: merchant1, unit_price: 1)
+    describe "the five most popular items" do
+      it "ranked by total revenue" do
+        merchant1 = create(:merchant)
+        item = create(:item, merchant: merchant1)
+        items = create_list(:item, 5, merchant: merchant1, unit_price: 1)
 
-      items.each_with_index do |item, index|
-        (5 - index).times do
-          invoice = create(:invoice)
-          create(:invoice_item, item_id: item.id, invoice_id: invoice.id, quantity: 1, unit_price: item.unit_price)
-          create(:transaction, invoice_id: invoice.id, result: 0)
+        items.each_with_index do |item, index|
+          (5 - index).times do
+            invoice = create(:invoice)
+            create(:invoice_item, item_id: item.id, invoice_id: invoice.id, quantity: 1, unit_price: item.unit_price)
+            create(:transaction, invoice_id: invoice.id, result: 0)
+          end
+        end
+
+        visit merchant_items_path(merchant1)
+
+        within '#top_items' do
+          expect(page).not_to have_content(item.name)
+          items.each_with_index do |item, index|
+            within "#top-#{index + 1}" do
+              expect(page).to have_link(item.name, href: item_path(item))
+              expect(page).to have_content("$ #{5 - index}")
+            end
+          end
         end
       end
 
-      visit merchant_items_path(merchant1)
+      it "with their best sales day" do
+        merchant1 = create(:merchant)
+        items = create_list(:item, 5, merchant: merchant1, unit_price: 1)
 
-      within '#top_items' do
-        expect(page).not_to have_content(item.name)
-        items.each_with_index do |item, index|
-          within "#top-#{index + 1}" do
-            expect(page).to have_link(item.name, href: item_path(item))
-            expect(page).to have_content("$ #{5 - index}")
+        5.times do |n|
+          invoice = create(:invoice, created_at: Date.today - n)
+          items[0..n].each do |item|
+            create(:invoice_item, item: item, invoice: invoice, quantity: (5 - n), unit_price: 1)
           end
+          create(:transaction, invoice: invoice, result: 0)
+        end
+
+        visit merchant_items_path(merchant1)
+
+        items.each_with_index do |item, index|
+          top_day = "Top day for #{item.name} was #{(Date.today-index).strftime("%m/%d/%y")}"
+          expect(page).to have_content(top_day)
         end
       end
     end
@@ -52,18 +74,19 @@ RSpec.describe "Items Index" do
       expect(page).to have_link("New Item", href: new_merchant_item_path(merchant1))
     end
 
-    it "items in the right order" do
+    it "enabled/disabled items in their own sections" do
       merchant1 = create(:merchant)
       enabled_item = create(:item, merchant: merchant1)
       disabled_item = create(:item, merchant: merchant1, enabled: false)
       visit merchant_items_path(merchant1)
 
-      expect(enabled_item.name).to appear_before(disabled_item.name)
+      within("#enabled") {expect(page).to have_content(enabled_item.name)}
+      within("#disabled") {expect(page).to have_content(disabled_item.name)}
     end
   end
 
   describe 'can disable/enable' do
-    it " items" do
+    it "items" do
       merchant1 = create(:merchant)
       item = create(:item, merchant: merchant1)
       visit merchant_items_path(merchant1)
