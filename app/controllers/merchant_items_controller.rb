29ad0765fc5284daab3e31409876063e1354ff4a
RyanDBarnett <conner2468@gmail.com>
2021-01-10 16:26:10 -0700

    Add merchant item edit form

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -8,4 +8,22 @@ class MerchantItemsController < ApplicationController
     merchant = Merchant.find(params[:merchant_id])
     @item = merchant.items.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update!(item_params)
+    flash[:success] = 'Item Updated Successfully'
+    render :show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price)
+  end
+
 end
--- spec/features/merchant/items/show_spec.rb
+++ spec/features/merchant/items/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'As a merchant' do
-  describe "When I visit a merchant's item's show page (/merchant/merchant_id/items/item_id)" do
+  describe "When I visit a merchant's item's show page (/merchants/merchant_id/items/item_id)" do
     before :each do
       @max = Merchant.create!(name: 'Merch Max')
       @item_1 = @max.items.create!(name: 'Beans', description: 'Tasty', unit_price: 5)
@@ -13,6 +13,18 @@ describe 'As a merchant' do
       expect(page).to have_content(@item_1.name)
       expect(page).to have_content("Description: #{@item_1.description}")
       expect(page).to have_content("Price: #{@item_1.unit_price}")
+    end
+
+    it 'I see a link to update the item information.' do
+      expect(page).to have_link('Edit Item', href: edit_merchant_item_path(@max.id, @item_1.id))
+    end
+
+    describe "When I click the 'Edit Item' link" do
+      it 'Then I am taken to a page to edit this item' do
+        click_link('Edit Item')
+
+        expect(current_path).to eq("/merchants/#{@max.id}/items/#{@item_1.id}/edit")
+      end
     end
   end
 end
