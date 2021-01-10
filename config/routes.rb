0e256712771244b4817a3e6370461118596af02a
Yesi Meza <yesi.meza10@gmail.com>
2021-01-09 18:17:33 -0700

    updated routes for admin merchant index and show

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   namespace :admin do
     root :to => 'dashboard#index'
-    resources :merchants, only: [:index]
+    resources :merchants, only: [:index, :show]
     resources :invoices, only: [:index, :show]
   end
   get '/merchants/:id/dashboard', to: 'merchants#dashboard'
