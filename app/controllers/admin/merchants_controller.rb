34bdb839490711d2cc82594d93d392c71f871c1f
Isabelle Villasenor <villasenori@outlook.com>
2021-01-10 16:02:13 -0700

    Add Enabled/Disabled to Admin Merchant Index Controller Method

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,6 +2,8 @@ class Admin::MerchantsController < ApplicationController
   before_action :set_merchant, only: [:show, :edit, :update]
   def index
     @merchants = Merchant.all
+    @enabled_merchants = Merchant.enabled
+    @disabled_merchants = Merchant.disabled
   end
 
   def show
