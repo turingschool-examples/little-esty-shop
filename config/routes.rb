5f410223533a78fd20827169a46c74a2ef454b72
Grayson Myers <graymyers10@gmail.com>
2021-01-09 20:44:35 -0700

    Remove unnecessary shallow routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,8 @@ Rails.application.routes.draw do
   end
 
   get '/admin', to: 'admins#dashboard'
-  shallow do
-    namespace :admin do
-      resources :merchants, :invoices
+  namespace :admin do
+    resources :merchants, :invoices
 
-    end
   end
 end
