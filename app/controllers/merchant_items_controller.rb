4428c3184f6ebc75cf3eb5c0448c086b8b57d915
Robert Heath <robert.b.heathii@gmail.com>
2021-01-09 18:43:08 -0600

    Remove show action because it is unused

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -4,10 +4,6 @@ class MerchantItemsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
   end
 
-  def show
-    @item = Item.find(params[:item_id])
-  end
-
   def update
     @merchant = Merchant.find(params[:merchant_id])
     @item = Item.find(params[:item_id])
