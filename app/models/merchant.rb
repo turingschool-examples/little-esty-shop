83d08214a2b59c9a3bef8ccfec37dd596c94837e
Robert Heath <robert.b.heathii@gmail.com>
2021-01-09 18:28:44 -0600

    Add tests and methods for enabled and disabled items for a merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,13 @@
 class Merchant < ApplicationRecord
-    has_many :invoices
-    has_many :customers, through: :invoices
-    has_many :items
+  has_many :invoices
+  has_many :customers, through: :invoices
+  has_many :items
+
+  def enabled_items
+    Item.all_enabled_items.where(merchant_id: self.id)
+  end
+
+  def disabled_items
+    Item.all_disabled_items.where(merchant_id: self.id)
+  end
 end
