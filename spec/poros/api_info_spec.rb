b4faa3057f3d01d823d37515c77b757f04b33fdf
ninesky00 <jiangzho@gmail.com>
2021-01-10 13:26:50 -0600

    add poro test to find repo name

--- spec/poros/api_info_spec.rb
+++ spec/poros/api_info_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ApiInfo do
-  it "it exists" do 
-    api_info = ApiInfo.new
-    expect(api_info).to be_a(ApiInfo)
+  before :each do 
+    @api_info = ApiInfo.instance
   end
+
+  it "it exists" do 
+    expect(@api_info).to be_a(ApiInfo)
+  end
+
+  it "finds repo name" do 
+    expect(@api_info.repo_name).to eq("little-esty-shop")  
+  end
+
 end
