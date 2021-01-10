563e53e868a8236060355af48f2dc86a0a6df6c2
Grayson Myers <graymyers10@gmail.com>
2021-01-09 20:43:49 -0700

    Add variable redirects based on params

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -13,13 +13,24 @@ class Admin::MerchantsController < ApplicationController
 
   def update
     merchant = Merchant.find(params[:id])
-    if result = merchant.update(name: params[:name])
+    if merchant.update(merchant_params)
       flash.notice = "Successfully Updated Info"
-      redirect_to admin_merchant_path(params[:id])
+      if params[:name]
+        destination = admin_merchant_path(params[:id])
+      else
+        destination = admin_merchants_path
+      end
+      redirect_to destination
     else
       flash.alert = merchant.errors.full_messages
       @merchant = merchant
       render "edit"
     end
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name, :enabled)
   end
 end
