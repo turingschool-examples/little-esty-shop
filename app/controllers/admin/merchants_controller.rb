4ff1e1f6902f13d256ef8fd673437b525838155e
Yesi Meza <yesi.meza10@gmail.com>
2021-01-09 18:46:25 -0700

    added update and edit actions to controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -7,5 +7,22 @@ class Admin::MerchantsController < ApplicationController
   def show 
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit 
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update 
+    merchant = Merchant.find(params[:id])
+    merchant.update(merchant_params) 
+    merchant.save 
+      redirect_to admin_merchant_path(merchant.id)
+      flash[:notice] = "Information has been updated"
+  end
+
+  private 
+  def merchant_params
+    params.permit(:name)
+  end
 end
 
