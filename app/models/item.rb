09ece6c2c59272f16106bb968e16d1f3b1491a34
Robert Heath <robert.b.heathii@gmail.com>
2021-01-09 18:19:26 -0600

    create the enabled and disabled methods in Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,12 @@ class Item < ApplicationRecord
   validates :unit_price, presence: true
   validates :unit_price, numericality: { greater_than: 0}
 
+  def self.all_enabled_items
+    where(enabled: true)
+  end
+
+  def self.all_disabled_items
+    where(enabled: false)
+  end
+
 end
