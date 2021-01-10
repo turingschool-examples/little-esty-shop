b14c441b161e6d6782203d9c62ffc98658458144
Yesi Meza <yesi.meza10@gmail.com>
2021-01-09 18:18:05 -0700

    created merchants conrtoller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,4 +1,11 @@
 class Admin::MerchantsController < ApplicationController
   def index
+    # require 'pry'; binding.pry
+    @merchants = Merchant.all
   end 
+
+  def show 
+    @merchant = Merchant.find(params[:id])
+  end
 end
+
