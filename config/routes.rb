7c0dab1562064e8c6a180991f3e739f227bfd86a
Saundra <saundracatalina@gmail.com>
2021-01-10 16:02:52 -0700

    Add enable/disable buttons-part way done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :merchant, only: [:show] do
     resources :dashboard, only: [:index]
     resources :items, only: [:index, :show, :edit, :update]
+    resources :item_status, only: [:update]
     resources :invoices, only: [:index, :show]
   end
 
