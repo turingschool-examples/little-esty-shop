8e2f9584105ceefca1fbeed28aac45cdeac84fbe
Saundra <saundracatalina@gmail.com>
2021-01-10 20:07:26 -0700

    Add AR find to test can make a button to disable item to look at db for status

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -37,13 +37,15 @@ describe "merchant items index" do
     expect(current_path).to eq("/merchant/#{@merchant1.id}/items/#{@item_1.id}")
   end
 
-  xit "can make a button to disable items" do
+  it "can make a button to disable items" do
     within("#item-#{@item_1.id}") do
       expect(page).to have_button("Enable")
       expect(page).to have_button("Disable")
 
       click_button "Disable"
-      expect(@item_1.status).to eq("disabled")
+
+      item = Item.find(@item_1.id)
+      expect(item.status).to eq("disabled")
     end
     within("#item-#{@item_2.id}") do
       expect(page).to have_button("Enable")
