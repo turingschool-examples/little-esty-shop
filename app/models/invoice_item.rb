749256f2567f9c0cc7609ec148508993df8901aa
Isabelle Villasenor <villasenori@outlook.com>
2021-01-10 18:50:32 -0700

    Add enum to invoice_items

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -8,6 +8,8 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
 
+  enum status: [:pending, :packaged, :shipped]
+
   def self.incomplete_invoices
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
