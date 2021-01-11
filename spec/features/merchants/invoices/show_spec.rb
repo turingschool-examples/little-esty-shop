250785ec85f9cde95ffc356f9bd3bd2ff2060e85
Max Ribbans <ribbans.max@gmail.com>
2021-01-11 18:56:41 -0500

    Add customer info to invoice show page- and updoot db

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -18,5 +18,23 @@ RSpec.describe "Merchant Invoices show" do
       expect(page).to have_content(invoice.created_at.strftime("%A, %B %-d, %Y"))
       expect(page).to have_content(invoice.status)
     end
+    it "customer info" do
+      merchant1 = create(:merchant)
+      items = create_list(:item, 5, merchant: merchant1, unit_price: 1)
+
+      customer = create(:customer, first_name: "Linda", last_name: "Mayhew")
+
+      invoice = create(:invoice, merchant: merchant1, customer: customer)
+
+      items.each do |item|
+        create(:invoice_item, item: item, invoice: invoice, quantity: 5, unit_price: 1)
+      end
+      # create(:transaction, invoice: invoice, result: 0)
+
+      visit merchant_invoice_path(merchant1, invoice)
+
+      expect(page).to have_content("Linda Mayhew")
+      expect(page).to have_content(customer.address)
+    end
   end
 end
