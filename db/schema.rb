e312eaa7e6bb21c3269d07c3618fd228c3d420b1
Isabelle Villasenor <villasenori@outlook.com>
2021-01-10 19:35:54 -0700

    Change Default Value to Disabled for Merchants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_160146) do
+ActiveRecord::Schema.define(version: 2021_01_11_023359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2021_01_10_160146) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status", default: 0
+    t.integer "status", default: 1
   end
 
   create_table "transactions", force: :cascade do |t|
