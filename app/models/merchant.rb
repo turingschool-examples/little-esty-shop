fdabd8a0f5f7ec3e5959d0b383d61cd6640177d4
Grayson Myers <graymyers10@gmail.com>
2021-01-11 16:12:27 -0700

    Add top_merchants class method to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,10 +1,21 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
   has_many :customers,-> {distinct}, through: :invoices
 
   validates :name, presence: true
 
   delegate :favorite_customers, to: :customers
   delegate :items_to_ship, to: :items
+
+  def self.top_merchants(number)
+    Merchant.joins(invoices: [:transactions, :invoice_items])
+    .where("result = 0")
+    .group(:id)
+    .select('merchants.*, sum(quantity * unit_price) as total_revenue')
+    .order("total_revenue" => :desc)
+    .limit(number)
+  end
 end
