1aa8fdfba29975211c380dda22d01c0f01e0a18d
Catherine Dean <catherinemdean15@gmail.com>
2021-01-10 17:18:59 -0700

    create new and create action in items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @enabled_items = @merchant.items.where(status: 0)
-    @disabled_items = @merchant.items.where(status: 1)
+    @enabled_items = @merchant.items.where(status: 1)
+    @disabled_items = @merchant.items.where(status: 0)
   end
 
   def show
@@ -20,11 +20,23 @@ class ItemsController < ApplicationController
     redirect_to merchant_item_path(@merchant, @item)
   end
 
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+
+    #require "pry"; binding.pry
+    Item.create!(item_params)
+    redirect_to merchant_items_path(@merchant)
+  end
+
 
   private
 
   def item_params
-    params.permit(:name, :description, :unit_price)
+    params.permit(:name, :description, :unit_price, :merchant_id)
   end
 
   def find_item_and_merchant
