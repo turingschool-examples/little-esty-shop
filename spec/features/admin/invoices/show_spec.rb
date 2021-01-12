db738880fe7a79ee54eb6798d7ea40bf957e08c8
Isabelle Villasenor <villasenori@outlook.com>
2021-01-11 20:04:37 -0700

    Add Customer Info To Admin Invoice Show View and Spec

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -4,7 +4,7 @@ describe 'Admin Invoices Index Page' do
   before :each do
     @m1 = Merchant.create!(name: 'Merchant 1')
 
-    @c1 = Customer.create!(first_name: 'Yo', last_name: 'Yoz')
+    @c1 = Customer.create!(first_name: 'Yo', last_name: 'Yoz', address: '123 Heyyo', city: 'Whoville', state: 'CO', zip: 12345)
     @c2 = Customer.create!(first_name: 'Hey', last_name: 'Heyz')
 
     @i1 = Invoice.create!(merchant_id: @m1.id, customer_id: @c1.id, status: 2, created_at: '2012-03-25 09:54:09')
@@ -15,6 +15,12 @@ describe 'Admin Invoices Index Page' do
     expect(page).to have_content("Invoice ##{@i1.id}")
     expect(page).to have_content("Status: #{@i1.status}")
     expect(page).to have_content("Created on: #{@i1.created_at.strftime("%A, %B %d, %Y")}")
+  end
+
+  it 'should display the customers name and shipping address' do
+    expect(page).to have_content("#{@c1.first_name} #{@c1.last_name}")
+    expect(page).to have_content(@c1.address)
+    expect(page).to have_content("#{@c1.city}, #{@c1.state} #{@c1.zip}")
   end
 end
 
