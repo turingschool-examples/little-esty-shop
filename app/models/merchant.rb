510da23d805dd8c84bccced12c05d53b485b17b2
Catherine Dean <catherinemdean15@gmail.com>
2021-01-11 19:02:01 -0700

    app/views/items/index.html.erb

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,7 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
   has_many :items
+  has_many :invoice_items, through: :items
 
   enum status: [:enabled, :disabled]
 
@@ -18,7 +19,19 @@ class Merchant < ApplicationRecord
   end
 
   def ordered_items_to_ship
-    item_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:item_id)
-    Item.order(:created_at).find(item_ids)
+    item_ids = InvoiceItem.where("status = 0 OR status = 1").order(:created_at).pluck(:item_id)
+    item_ids.map do |id|
+      Item.find(id)
+    end
+  end
+
+  def top_5_items
+     items
+     .joins(invoices: :transactions)
+     .where('transactions.result = 1')
+     .select("items.*, sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue")
+     .group(:id)
+     .order('total_revenue desc')
+     .limit(5)
   end
 end
