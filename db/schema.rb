8a246a240e35f6cb3b849ae092f9ce1aa9ed26ab
Yesi Meza <yesi.meza10@gmail.com>
2021-01-11 22:06:57 -0700

    updated status default to disbaled

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_003642) do
+ActiveRecord::Schema.define(version: 2021_01_12_045706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2021_01_11_003642) do
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status", default: 0
+    t.integer "status", default: 1
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
