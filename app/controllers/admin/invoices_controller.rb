f4747cd38e17b46e123979669d039cb33d8ae7be
Isabelle Villasenor <villasenori@outlook.com>
2021-01-11 21:45:42 -0700

    Add Update Status Method in Admin Invoices Controller

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -1,9 +1,27 @@
 class Admin::InvoicesController < ApplicationController
+  before_action :set_invoice, only: [:show, :edit, :update]
   def index
     @invoices = Invoice.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @invoice.update(invoice_params)
+    flash.notice = 'Invoice Has Been Updated!'
+    redirect_to admin_invoice_path(@invoice)
+  end
+
+  private
+  def set_invoice
     @invoice = Invoice.find(params[:id])
+  end
+
+  def invoice_params
+    params.require(:invoice).permit(:status)
   end
 end
