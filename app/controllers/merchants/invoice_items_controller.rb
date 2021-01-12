1cba658f465111746dc30939f276f8db441a4ec7
Grayson Cunningham <gecun289@gmail.com>
2021-01-12 16:59:25 -0700

    remove :previously param for redirecting view paths

--- app/controllers/merchants/invoice_items_controller.rb
+++ app/controllers/merchants/invoice_items_controller.rb
@@ -2,7 +2,7 @@ class Merchants::InvoiceItemsController < ApplicationController
   def update
     invoice_item = InvoiceItem.find(params[:id])
     invoice_item.update!(invoice_item_params)
-    redirect_to params[:invoice_item][:previously]
+    redirect_to merchant_invoice_path(params[:merchant_id], invoice_item.invoice_id)
   end
 
   private
