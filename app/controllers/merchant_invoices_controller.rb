501a193969a3622de7a8f31c982e948c5249799b
Yesi Meza <yesi.meza10@gmail.com>
2021-01-11 19:47:27 -0700

    added show action to controller

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -1,6 +1,10 @@
 class MerchantInvoicesController < ApplicationController
   def index
-    @merchant = Merchant.find(params[:id]) 
+    @merchant = Merchant.find(params[:merchant_id]) 
     @invoices = @merchant.invoices
+  end
+
+  def show 
+   @invoice = Invoice.find(params[:invoice_id])
   end
 end 
