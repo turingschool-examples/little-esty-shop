a9574cbad05b7a005c2a890840be1ae0ea135d29
Yesi Meza <yesi.meza10@gmail.com>
2021-01-12 23:11:42 -0700

    removed the item spec test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -103,46 +103,5 @@ RSpec.describe Item, type: :model do
 
       expect(Item.top_5_popular_items).to eq(actual)
     end
-
-    it "Can return the date with most sales for each item" do 
-      all_birds = Merchant.create!(name: 'All Birds', status: 0)
-      walmart   = Merchant.create!(name: 'Walmart', status: 0)
-      overstock = Merchant.create!(name: 'Overstock', status: 0)
-      big_lots  = Merchant.create!(name: 'Big Lots', status: 0)
-      amazon    = Merchant.create!(name: 'Amazon', status: 0)
-      alibaba   = Merchant.create!(name: 'Alibaba', status: 1)
-      # Invoices:
-      invoice1 = Invoice.create!(status: 0, customer_id: @sally.id, merchant_id: amazon.id)
-      invoice2 = Invoice.create!(status: 0, customer_id: @joel.id, merchant_id: alibaba.id)
-      invoice3 = Invoice.create!(status: 0, customer_id: @billy.id, merchant_id: all_birds.id)
-      invoice4 = Invoice.create!(status: 0, customer_id: @steve.id, merchant_id: overstock.id)
-      invoice5 = Invoice.create!(status: 0, customer_id: @frank.id, merchant_id: big_lots.id)
-      invoice6 = Invoice.create!(status: 0, customer_id: @joel.id, merchant_id: walmart.id)
-      # Transactions:
-      tx1      = Transaction.create!(credit_card_number: 010001001022, credit_card_expiration_date: 20251001, result: 'success', invoice_id: invoice2.id)
-      tx2      = Transaction.create!(credit_card_number: 010001005555, credit_card_expiration_date: 20220101, result: 'failed', invoice_id: invoice1.id)
-      tx3      = Transaction.create!(credit_card_number: 010001001022, credit_card_expiration_date: 20251001, result: 'success', invoice_id: invoice3.id)
-      tx4      = Transaction.create!(credit_card_number: 010001001022, credit_card_expiration_date: 20251001, result: 'success', invoice_id: invoice4.id)
-      tx5      = Transaction.create!(credit_card_number: 010001005555, credit_card_expiration_date: 20220101, result: 'success', invoice_id: invoice5.id)
-      tx6      = Transaction.create!(credit_card_number: 010001005555, credit_card_expiration_date: 20220101, result: 'success', invoice_id: invoice5.id)
-      tx7      = Transaction.create!(credit_card_number: 010001005555, credit_card_expiration_date: 20220101, result: 'success', invoice_id: invoice6.id)
-      # Items:
-      candle   = Item.create!(name: 'Lavender Candle', description: '8oz Soy Candle', unit_price: 7.0, merchant_id: amazon.id)
-      backpack = Item.create!(name: 'Camo Backpack', description: 'Double Zip Backpack', unit_price: 15.5, merchant_id: alibaba.id)
-      radio1   = Item.create!(name: 'Retro Radio', description: 'Twist and Turn to your fav jams', unit_price: 9.75, merchant_id: all_birds.id)
-      radio2   = Item.create!(name: 'Retro Radio', description: 'Twist and Turn to your fav jams', unit_price: 9.75, merchant_id: walmart.id)
-      radio3   = Item.create!(name: 'Retro Radio', description: 'Twist and Turn to your fav jams', unit_price: 9.75, merchant_id: big_lots.id)
-      radio4   = Item.create!(name: 'Retro Radio', description: 'Twist and Turn to your fav jams', unit_price: 9.75, merchant_id: overstock.id)
-      # InvoiceItems:
-      invitm1  = InvoiceItem.create!(status: 0, quantity: 25, unit_price: 7.0, invoice_id: invoice1.id, item_id: candle.id)
-      invitm2  = InvoiceItem.create!(status: 2, quantity: 10, unit_price: 15.5, invoice_id: invoice2.id, item_id: backpack.id)
-      invitm3  = InvoiceItem.create!(status: 1, quantity: 100, unit_price: 9.75, invoice_id: invoice3.id, item_id: radio1.id)
-      invitm4  = InvoiceItem.create!(status: 1, quantity: 50, unit_price: 9.75, invoice_id: invoice4.id, item_id: radio2.id)
-      invitm5  = InvoiceItem.create!(status: 1, quantity: 65, unit_price: 9.75, invoice_id: invoice5.id, item_id: radio3.id)
-      invitm6  = InvoiceItem.create!(status: 1, quantity: 30, unit_price: 9.75, invoice_id: invoice6.id, item_id: radio4.id)
-       
-
-      expect(radio1.best_sales_day).to eq(radio1.created_at.strftime("%A, %B %d, %Y")) 
-    end
   end
 end
