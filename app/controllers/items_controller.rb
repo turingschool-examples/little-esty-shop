9f98b299f7d1f93a20de81668aaef3a39c1c8743
Isabelle Villasenor <villasenori@outlook.com>
2021-01-13 06:20:16 -0700

    Refactor Item Create Controller Method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,42 +1,39 @@
 class ItemsController < ApplicationController
+  before_action :find_item_and_merchant, only: [:show, :edit, :update]
+  before_action :set_merchant, only: [:index, :new, :create]
   def index
-    @merchant = Merchant.find(params[:merchant_id])
     @enabled_items = @merchant.items.where(status: 1)
     @disabled_items = @merchant.items.where(status: 0)
   end
 
   def show
-    find_item_and_merchant
   end
 
   def edit
-    find_item_and_merchant
   end
 
   def update
-    find_item_and_merchant
     @item.update(item_params)
     flash.notice = "Succesfully Updated Item Info!"
     redirect_to merchant_item_path(@merchant, @item)
   end
 
   def new
-    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def create
-    @merchant = Merchant.find(params[:merchant_id])
-
-    #require "pry"; binding.pry
     Item.create!(item_params)
     redirect_to merchant_items_path(@merchant)
   end
 
 
   private
-
   def item_params
     params.permit(:name, :description, :unit_price, :merchant_id)
+  end
+
+  def set_merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def find_item_and_merchant
