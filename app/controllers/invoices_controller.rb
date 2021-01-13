8d5a9d934e08514cb3d523f4af9833bf32f350de
Catherine Dean <catherinemdean15@gmail.com>
2021-01-12 22:19:49 -0700

    create update action in invoices controller

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -10,4 +10,16 @@ class InvoicesController < ApplicationController
     @customer = @invoice.customer
     @invoice_item = InvoiceItem.where(invoice_id: params[:id]).first
   end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoice = Invoice.find(params[:id])
+    @invoice.update(invoice_params)
+    redirect_to merchant_invoice_path(@merchant, @invoice)
+  end
+
+  private
+  def invoice_params
+    params.require(:invoice).permit(:status)
+  end
 end
