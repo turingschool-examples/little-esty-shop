a53dc820902c7682809cc69774ba4af522bae1e8
Yesi Meza <yesi.meza10@gmail.com>
2021-01-12 17:07:23 -0700

    created test for top popular items

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
 
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
 
   def quantity_ordered(itm_id, invc_id)
     invoice_items.where(item_id: itm_id, invoice_id: invc_id).pluck(:quantity)
@@ -19,4 +20,18 @@ class Item < ApplicationRecord
   def status(itm_id, invc_id)
     invoice_items.where(item_id: itm_id, invoice_id: invc_id).pluck(:status)
   end
+
+  def self.top_5_popular_items
+  
+    Item.joins(:transactions)
+    .where("transactions.result = 'success'")
+    .select('items.name, SUM(invoice_items.quantity * invoice_items.unit_price) AS highest_in_revenue')
+    .group(:id)
+    .order('highest_in_revenue desc')
+    .limit(5)
+  end 
+
+  # def best_sales_day(itm_id, invc_id)
+  #   invoice_items.where(item_id: itm_id, invoice_id: invc_id).order(invoice.created_at).sum('unit_price * quantity').pluck(:created_at).limit(1)
+  # end
 end
--- spec/features/merchant/items/index_spec.rb
+++ spec/features/merchant/items/index_spec.rb
@@ -8,7 +8,7 @@ describe 'As a merchant' do
       @item_1 = @max.items.create!(name: 'Beans', description: 'Tasty', unit_price: 5, status: 0)
       @item_2 = @max.items.create!(name: 'Item 2', description: 'Blah', unit_price: 10, status: 0)
       @item_3 = @bob.items.create!(name: 'Item 3', description: 'Test', unit_price: 15, status: 0)
-
+     
       visit "merchants/#{@max.id}/items"
     end
 
@@ -91,5 +91,68 @@ describe 'As a merchant' do
 
       expect(current_path).to eq("/merchants/#{@max.id}/items/new")
     end
+    it "can see the top 5 most popular items ranked by total revenue" do
+      sally    = Customer.create!(first_name: 'Sally', last_name: 'Smith')
+      joel     = Customer.create!(first_name: 'Joel', last_name: 'Hansen')
+      billy    = Customer.create!(first_name: 'Billy', last_name: 'Joel')
+      steve    = Customer.create!(first_name: 'Steve', last_name: 'Carrell')
+      frank    = Customer.create!(first_name: 'Frank', last_name: 'Sinatra')
+      Jon      = Customer.create!(first_name: 'Jon', last_name: 'Travolta')
+
+      all_birds = Merchant.create!(name: 'All Birds', status: 0)
+      walmart   = Merchant.create!(name: 'Walmart', status: 0)
+      overstock = Merchant.create!(name: 'Overstock', status: 0)
+      big_lots  = Merchant.create!(name: 'Big Lots', status: 0)
+      amazon    = Merchant.create!(name: 'Amazon', status: 0)
+      alibaba   = Merchant.create!(name: 'Alibaba', status: 1)
+      # Invoices:
+      invoice1 = Invoice.create!(status: 0, customer_id: sally.id, merchant_id: all_birds.id)
+      invoice2 = Invoice.create!(status: 0, customer_id: joel.id, merchant_id: all_birds.id)
+      invoice3 = Invoice.create!(status: 0, customer_id: billy.id, merchant_id: all_birds.id)
+      invoice4 = Invoice.create!(status: 0, customer_id: steve.id, merchant_id: all_birds.id)
+      invoice5 = Invoice.create!(status: 0, customer_id: frank.id, merchant_id: all_birds.id)
+      invoice6 = Invoice.create!(status: 0, customer_id: joel.id, merchant_id: all_birds.id)
+      # Transactions:
+      tx1      = Transaction.create!(credit_card_number: 010001001022, credit_card_expiration_date: 20251001, result: 'success', invoice_id: invoice2.id,)
+      tx2      = Transaction.create!(credit_card_number: 010001005555, credit_card_expiration_date: 20220101, result: 'failed', invoice_id: invoice1.id,)
+      tx3      = Transaction.create!(credit_card_number: 010001001022, credit_card_expiration_date: 20251001, result: 'success', invoice_id: invoice3.id,)
+      tx4      = Transaction.create!(credit_card_number: 010001001022, credit_card_expiration_date: 20251001, result: 'success', invoice_id: invoice4.id,)
+      tx5      = Transaction.create!(credit_card_number: 010001005555, credit_card_expiration_date: 20220101, result: 'success', invoice_id: invoice5.id,)
+      tx6      = Transaction.create!(credit_card_number: 010001005555, credit_card_expiration_date: 20220101, result: 'success', invoice_id: invoice5.id,)
+      tx7      = Transaction.create!(credit_card_number: 010001005555, credit_card_expiration_date: 20220101, result: 'success', invoice_id: invoice6.id,)
+      # Items:
+      candle   = Item.create!(name: 'Lavender Candle', description: '8oz Soy Candle', unit_price: 7.0, merchant_id: all_birds.id)
+      backpack = Item.create!(name: 'Camo Backpack', description: 'Double Zip Backpack', unit_price: 15.5, merchant_id: all_birds.id)
+      radio1   = Item.create!(name: 'Retro Radio', description: 'Twist and Turn to your fav jams', unit_price: 9.75, merchant_id: all_birds.id)
+      radio2   = Item.create!(name: 'Retro', description: 'Twist and Turn to your fav jams', unit_price: 9.75, merchant_id: all_birds.id)
+      radio3   = Item.create!(name: 'Name', description: 'Twist and Turn to your fav jams', unit_price: 9.75, merchant_id: all_birds.id)
+      radio4   = Item.create!(name: 'I Heart Radio', description: 'Twist and Turn to your fav jams', unit_price: 9.75, merchant_id: all_birds.id)
+      # InvoiceItems:
+      invitm1  = InvoiceItem.create!(status: 0, quantity: 25, unit_price: 7.0, invoice_id: invoice1.id, item_id: candle.id)
+      invitm2  = InvoiceItem.create!(status: 2, quantity: 10, unit_price: 15.5, invoice_id: invoice2.id, item_id: backpack.id)
+      invitm3  = InvoiceItem.create!(status: 1, quantity: 100, unit_price: 9.75, invoice_id: invoice3.id, item_id: radio1.id)
+      invitm4  = InvoiceItem.create!(status: 1, quantity: 50, unit_price: 9.75, invoice_id: invoice4.id, item_id: radio2.id)
+      invitm5  = InvoiceItem.create!(status: 1, quantity: 65, unit_price: 9.75, invoice_id: invoice5.id, item_id: radio3.id)
+      invitm6  = InvoiceItem.create!(status: 1, quantity: 30, unit_price: 9.75, invoice_id: invoice6.id, item_id: radio4.id)
+      
+      visit merchant_items_path(all_birds.id)
+      within(".item-top5") do
+        expect(page).to have_link(radio3.name)
+        expect(page).to have_link(radio1.name)
+        expect(page).to have_link(radio2.name)
+        expect(page).to have_link(radio4.name)
+        expect(page).to have_link(backpack.name)
+      
+        expect(radio3.name).to appear_before(radio1.name)
+        expect(radio2.name).to appear_before(radio4.name)
+        expect(radio4.name).to appear_before(backpack.name)
+
+        expect(page).to have_content("Item:Name $1267.5")
+        expect(page).to have_content("Item:Retro Radio $975.0")
+        expect(page).to have_content("Item:Retro $487.5")
+        expect(page).to have_content("Item:I Heart Radio $292.5")
+        expect(page).to have_content("Item:Camo Backpack $155.0")
+      end 
+    end 
   end
 end
