6fd0ee2b3899c14846a7a01caf06b0cf35089eb4
RyanDBarnett <conner2468@gmail.com>
2021-01-12 21:04:48 -0700

    Add invoice item status selector WIP

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -1,10 +1,22 @@
 class MerchantInvoicesController < ApplicationController
   def index
-    @merchant = Merchant.find(params[:merchant_id]) 
+    @merchant = Merchant.find(params[:merchant_id])
     @invoices = @merchant.invoices
   end
 
-  def show 
+  def show
     @invoice = Invoice.find(params[:invoice_id])
   end
-end 
+
+  def update
+    @invoice = Invoice.find(params[:id])
+    @invoice.update!(invoice_params)
+    render :show
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:status)
+  end
+end
