cec75627d8165e3af79a0c52d74467d546de7731
Catherine Dean <catherinemdean15@gmail.com>
2021-01-12 18:01:13 -0700

    create best_day instance method

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,17 @@ class Item < ApplicationRecord
   belongs_to :merchant
 
   enum status: [:disabled, :enabled]
+
+  def best_day
+    invoices
+    .joins(:invoice_items)
+    .where('invoices.status = 2')
+    .select('invoices.*, sum(invoice_items.unit_price * invoice_items.quantity) as money')
+    .group(:id)
+    .order("money desc", "created_at desc")
+    .first
+    .created_at
+    .to_date
+  end
+
 end
