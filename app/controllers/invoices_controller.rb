bbcec504b90e45c4486bb2d7b2f50b7dd3a1501b
Liam Cusack <LCusack8108@gmail.com>
2021-01-13 18:32:06 -0700

    Start merchant invoices user story 1, edit controller, make views

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,6 +1,11 @@
 class InvoicesController < ApplicationController
+  before_action :invoices_merchant, only: [:index, :show]
 
   def index
     @invoice = Invoice.all
+  end
+
+  def invoices_merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
