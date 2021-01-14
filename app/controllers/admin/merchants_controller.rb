d6861ee51d57499b201b19bce3a0eb6e799958b5
Saundra <saundracatalina@gmail.com>
2021-01-13 20:07:32 -0700

    Add find_new_id method to admin merchant to increment new instances ids

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -10,8 +10,8 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def create
-    @merchant = Merchant.new(m_params)
-    @merchant.save
+    Merchant.create!(name: params[:name],
+                     id: find_new_id)
     flash.notice = 'Merchant Has Been Created!'
     redirect_to admin_merchants_path
   end
@@ -40,5 +40,9 @@ class Admin::MerchantsController < ApplicationController
 
   def m_params
     params.permit(:name)
+  end
+
+  def find_new_id
+    Merchant.last.id + 1
   end
 end
