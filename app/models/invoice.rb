dd522e8db1e98e9f17273b10faab75bb94886e26
Khoa Nguyen <KhoaNg323@hotmail.com>
2021-02-23 09:11:27 -0700

    css changes

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,8 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+
+  validates :customer_id, :status
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,6 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+
+  validates :unit_price, :status
 end
