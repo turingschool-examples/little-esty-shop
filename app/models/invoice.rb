0e58611d6a45e76e09b8ec9eabf5bd8d3c32e391
Katy La Tour <klatour324@gmail.com>
2021-02-23 17:26:37 -0600

    Add associations for items and invoices for invoice items

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,7 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
   enum status: {"in progress" => 0, completed: 1, cancelled: 2}
 end
