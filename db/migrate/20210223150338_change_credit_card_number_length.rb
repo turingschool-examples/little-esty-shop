be0d6ebab7c137f10f4edcb05859ce78c5bec9f0
Jenny Branham <branhamjenny@gmail.com>
2021-02-23 09:17:36 -0600

    Add validations for transaction model

---
+++ db/migrate/20210223150338_change_credit_card_number_length.rb
@@ -0,0 +1,5 @@
+class ChangeCreditCardNumberLength < ActiveRecord::Migration[5.2]
+  def change
+    change_column :transactions, :credit_card_number, :bigint
+  end
+end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :invoice }
+  end
+  describe 'validations' do
+    it { should validate_numericality_of(:credit_card_number).is_greater_than_or_equal_to(0)}
+
+    it 'result can be nil by default' do
+      transaction = create(:transaction, result: nil)
+      expect(transaction.result).to eq(nil)
+      expect(transaction.success?).to eq(false)
+      expect(transaction.failed?).to eq(false)
+    end
+    it 'result can be success' do
+      transaction = create(:transaction, result: :success)
+      expect(transaction.result).to eq("success")
+      expect(transaction.success?).to eq(true)
+      expect(transaction.failed?).to eq(false)
+    end
+    it 'result can be failed' do
+      transaction = create(:transaction, result: :failed)
+      expect(transaction.result).to eq('failed')
+      expect(transaction.success?).to eq(false)
+      expect(transaction.failed?).to eq(true)
+    end
+  end
 end
