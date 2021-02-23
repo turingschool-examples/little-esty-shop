a124b5593d652ea0407ee2349a57c6aa5136b973
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-02-23 14:30:37 -0700

    Get our rake tasks working

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,3 @@
 class Invoice < ApplicationRecord
+  enum status: {"in progress" => 0, completed: 1, cancelled: 2}
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,4 @@
 class Transaction < ApplicationRecord
+
+  enum result: {success: 0, failed: 1}
 end
