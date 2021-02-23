14b435e621048f7f6a678c7cb4f871e61335a4ab
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-02-23 14:00:33 -0700

    Finalized relationship tests in cust, ii, and item model

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe InvoiceItem do
   describe "relationships" do
-    it { should belongs_to :invoice }
+    it { should belong_to :invoice }
     # it { should belongs_to :item }
   end
 end
