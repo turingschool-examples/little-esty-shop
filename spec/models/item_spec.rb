460360fab35f27c7ff69fc65b3ad342a4a2f908e
Dominic Padula <domopadula2192@gmail.com>
2021-02-23 14:16:30 -0700

    relationship and validations tests for item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,6 +11,6 @@ RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to :merchant }
     it { should have_many :invoice_items }
-    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:invoices).through(:invoice_items) } 
   end
-end 
+end
