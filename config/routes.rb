4ff3d0b5e2e8bc171c3eea2f78acff121230a162
Diana Buffone <diana.pamelaet@gmail.com>
2021-02-24 14:59:42 -0500

    Create routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :admin 
+  resources :admin, only: :index
+
+  namespace :admin do
+    resources :merchants, only: [ :show, :update, :create ]
+    resources :invoices, only: [ :index, :show ]
+  end
 end
