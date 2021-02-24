0fb982524866c69b388cc721b932b5398c45e034
Joseph Budina <joebud3@gmail.com>
2021-02-23 19:17:13 -0700

    Add index test

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -13,4 +13,10 @@ describe 'Admin Dashboard' do
     expect(page).to have_link("Merchant Index")
     expect(page).to have_link("Invoice Index")
   end
+
+  it 'Shows names of top 5 customers with largest number of transactions' do
+    visit '/admin'
+
+    expect(page).to have_content()
+  end
 end
