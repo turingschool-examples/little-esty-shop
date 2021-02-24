edee41be79200f0aebc902bf3fa3f7b5e70b2f9d
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-02-23 18:32:40 -0700

    Update controller to be RESTful. Need to update application.html.erb for 'admin dashboard' heading

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	get "/admin", to:"admins#dashboard" 
+	# get "/admin", to:"admins#dashboard"
+	get "/admin", to:"admin/dashboard#index"
 	namespace :admin do
 		resources :merchants, only: [:index, :show, :destroy]
 		resources :invoices, only: [:index, :show, :destroy]
