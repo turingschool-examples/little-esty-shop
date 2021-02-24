c8182ebf23b02026becadc0391a0632d71f186ca
DougWelchons <doug.welchons@gmail.com>
2021-02-24 16:03:29 -0700

    Add tests for top 5 customers in admin dashboard

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe "admin dashboard" do
     @customer2 = Customer.create!(first_name: "First2", last_name: "Last2")
     @customer3 = Customer.create!(first_name: "First3", last_name: "Last3")
     @customer4 = Customer.create!(first_name: "First4", last_name: "Last4")
-    @customer5 = Customer.create!(first_name: "First4", last_name: "Last4")
-    @customer6 = Customer.create!(first_name: "First4", last_name: "Last4")
+    @customer5 = Customer.create!(first_name: "First5", last_name: "Last5")
+    @customer6 = Customer.create!(first_name: "First6", last_name: "Last6")
 
     @invoice1 = @customer2.invoices.create!(status: 1)
     @transaction1 = @invoice1.transactions.create!(result: 0)
@@ -40,7 +40,7 @@ RSpec.describe "admin dashboard" do
     it "shows a header indicating that I am on the admin dashboard" do
       visit '/admin'
 
-      within(".header") do
+      within("header") do
         expect(page).to have_content("Admin Dashboard")
       end
     end
@@ -48,7 +48,7 @@ RSpec.describe "admin dashboard" do
     it "has links to the admin merchant index page and admin invoice index page" do
       visit "/admin"
 
-      within(".header") do
+      within("header") do
         expect(page).to have_link("Merchants")
         expect(page).to have_link("Invoices")
       end
@@ -56,23 +56,37 @@ RSpec.describe "admin dashboard" do
 
     it "shows the top 5 customers, and the number of transactions they have completed" do
       visit "/admin"
-
+      save_and_open_page
       within(".top_customers") do
-        expect(@customer5.name).to appear_before(@customer6.name)
-        expect(@customer6.name).to appear_before(@customer4.name)
-        expect(@customer4.name).to appear_before(@customer3.name)
-        expect(@customer3.name).to appear_before(@customer2.name)
+        expect(@customer5.first_name).to appear_before(@customer6.first_name)
+        expect(@customer6.first_name).to appear_before(@customer2.first_name)
+        expect(@customer2.first_name).to appear_before(@customer4.first_name)
+        expect(@customer4.first_name).to appear_before(@customer3.first_name)
 
-        expect(page).to have_content(@customer5.name)
-        expect(page).to have_content(@customer5.sucsessful_transaction_count)
-        expect(page).to have_content(@customer6.name)
-        expect(page).to have_content(@customer6.sucsessful_transaction_count)
-        expect(page).to have_content(@customer4.name)
-        expect(page).to have_content(@customer4.sucsessful_transaction_count)
-        expect(page).to have_content(@customer3.name)
-        expect(page).to have_content(@customer3.sucsessful_transaction_count)
-        expect(page).to have_content(@customer2.name)
-        expect(page).to have_content(@customer2.sucsessful_transaction_count)
+        within("#customer_id_#{@customer2.id}") do
+          expect(page).to have_content("#{@customer2.first_name} #{@customer2.last_name}")
+          expect(page).to have_content(2)
+        end
+
+        within("#customer_id_#{@customer3.id}") do
+          expect(page).to have_content("#{@customer3.first_name} #{@customer3.last_name}")
+          expect(page).to have_content(1)
+        end
+
+        within("#customer_id_#{@customer4.id}") do
+          expect(page).to have_content("#{@customer4.first_name} #{@customer4.last_name}")
+          expect(page).to have_content(2)
+        end
+
+        within("#customer_id_#{@customer5.id}") do
+          expect(page).to have_content("#{@customer5.first_name} #{@customer5.last_name}")
+          expect(page).to have_content(4)
+        end
+
+        within("#customer_id_#{@customer6.id}") do
+          expect(page).to have_content("#{@customer6.first_name} #{@customer6.last_name}")
+          expect(page).to have_content(3)
+        end
       end
     end
   end
