360799a7a8ea24237e3c8124769bb303aff35766
Joseph Budina <joebud3@gmail.com>
2021-02-23 17:58:07 -0700

    Add index link test

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -6,4 +6,11 @@ describe 'Admin Dashboard' do
 
     expect(page).to have_content("Admin Dashboard")
   end
+
+  it 'Shows links to merchant and invoice index pages' do
+    visit '/admin'
+    
+    expect(page).to have_link("Merchant Index")
+    expect(page).to have_link("Invoice Index")
+  end
 end
