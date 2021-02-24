87116513b8a4383990ea5f4c5f49c9d27f0d0885
Harrison Blake <harrison.r.blake.gmail.com>
2021-02-24 14:52:21 -0700

    Refactor merchant features test

--- spec/features/merchant_items/show_spec.rb
+++ spec/features/merchant_items/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Merchant Item Index Page" do
+RSpec.describe "Merchant Item's Show Page" do
   before(:each) do
     @merchant = Merchant.create!(name: "Harrison")
     @item_1 = @merchant.items.create!(name: "apple",
@@ -11,22 +11,19 @@ RSpec.describe "Merchant Item Index Page" do
                                       unit_price: 2.09)
   end
 
-  describe "when I visit the merchant index page" do
-    it "Displays the merchant's name" do
-
+  describe "When I click the name of an item from the merchant items index page" do
+    it "takes me to the merchant's item's show page" do
       visit "/merchants/#{@merchant.id}/items"
 
-      expect(page).to have_content("Harrison")
-    end
+      expect(page).to have_link('apple')
+      first(:link, "apple").click
+      expect(current_path).to eq("/merchants/#{@merchant.id}/items/#{@item_1.id}")
 
-    it "Displays a list of all the names of this merchant's items" do
-      visit "/merchants/#{@merchant.id}/items"
+      expect(page).to have_content("apple")
 
-      expect(page).to have_content("Items")
-
-      within(".items") do
-        expect(page).to have_content("apple")
-        expect(page).to have_content("orange")
+      within(".item_attributes") do
+        expect(page).to have_content("one a day keeps the doctor away!")
+        expect(page).to have_content("22.09")
       end
     end
   end
