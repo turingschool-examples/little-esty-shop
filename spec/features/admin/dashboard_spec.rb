c78db591e202ab8a6170e44a35166337ad6659b0
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-23 17:45:33 -0700

    Write tests for admin merchant && invoices links

---
+++ spec/features/admin/dashboard_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe"As an admin" do
+	describe "when I visit the admin dashboard" do
+		it "renders an admin header" do
+			visit "/admin"
+			within("#admin-header")do
+				expect(page).to have_content("Admin Dashboard")
+			end
+		end
+
+		it"has links to merchant and invoices indexes"do
+			visit "/admin"
+			expect(page).to have_link "Admin Merchants Index"
+			expect(page).to have_link "Admin Invoices Index"
+		end
+
+	end
+	describe"when I click on the index links"do
+
+		it"takes you to the admin merchants index"do
+			visit "/admin"
+			click_link "Admin Merchants Index"
+			expect(current_path).to eq("/admin/merchants")
+		end
+
+		it"takes you to the admin invoices index"do
+			visit "/admin"
+			click_link "Admin Invoices Index"
+			expect(current_path).to eq("/admin/invoices")
+		end
+
+	end
+end
