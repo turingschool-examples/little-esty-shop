65368e9a19a952e10575716b807e7f8d15fac7a7
Jacob Volpe <volpe.jacob@gmail.com>
2021-02-24 14:57:04 -0700

    working on merchant story 3

--- spec/features/merchants/dashboard/index_spec.rb
+++ spec/features/merchants/dashboard/index_spec.rb
@@ -13,6 +13,17 @@ RSpec.describe "Merchant Dashboard" do
       expect(page).to have_link("My Items")
       expect(page).to have_link("My Invoices")
     end
+      it "shows the top 5 customers" do 
+        visit "/merchant/#{@merchant.id}/dashboard"
+
+        within "topFive" do 
+          expect(page).to have_cotent(@cust1.name)
+          expect(page).to have_cotent(@cust2.name)
+          expect(page).to have_cotent(@cust3.name)
+          expect(page).to have_cotent(@cust4.name)
+          expect(page).to have_cotent(@cust5.name)
+        end
+      end
     xit "and I click on the My Items link it takes me to that merchants items page" do 
       visit "/merchant/#{@merchant.id}/dashboard"
 
