b803a1a9f1b48b872cd52869b32b54ef63fd72e5
Katy La Tour <klatour324@gmail.com>
2021-02-24 16:47:44 -0600

    Add show action to merchant items controller

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -3,4 +3,9 @@ class MerchantItemsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @items = @merchant.items
   end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:item_id])
+  end
 end
