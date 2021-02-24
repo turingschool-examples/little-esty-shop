7b725e9722100317efa4349186b47d43f8b1bcea
DougWelchons <doug.welchons@gmail.com>
2021-02-24 09:50:48 -0700

    Add sucsessful_transaction_count instance method to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ApplicationRecord
   has_many :invoices, dependent: :destroy
+
+  def sucsessful_transaction_count
+    invoices.joins(:transactions).where('transactions.result = ?', 0).count
+  end
 end
