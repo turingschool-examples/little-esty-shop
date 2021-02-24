ee267a4d5a67ef19be143f1dd92eddaeefe3359d
Katy La Tour <klatour324@gmail.com>
2021-02-23 20:41:03 -0600

    Create scope for merchant/:merchant_id/dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :merchants, only: [:dashobard]
+
+  # get  "/merchant/:merchant_id/dashboard", to: "merchants#dashboard"
+  scope :merchant do
+    get "/:merchant_id/dashboard", to: "merchants#dashboard"
+  end
 end
