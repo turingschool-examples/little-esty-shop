95d4d7637161fce60ad73dc82cfc226e3d1a5577
Wil McCauley <wmccauley724@gmail.com>
2021-02-25 12:15:08 -0700

    Add tests to merchjant and customer model tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -14,5 +14,4 @@ RSpec.describe Customer, type: :model do
     it { should validate_presence_of :last_name }
   end
 
-
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -24,12 +24,12 @@ RSpec.describe Merchant, type: :model do
       @item5 = create(:item, merchant_id: @merchant1.id)
       @item6 = create(:item, merchant_id: @merchant1.id)
 
-      @invoice_item = create(:invoice_item_with_invoices, item_id: @item.id)
-      @invoice_item2 = create(:invoice_item_with_invoices, item_id: @item2.id)
-      @invoice_item3 = create(:invoice_item_with_invoices, item_id: @item3.id)
-      @invoice_item4 = create(:invoice_item_with_invoices, item_id: @item4.id)
-      @invoice_item5 = create(:invoice_item_with_invoices, item_id: @item5.id)
-      @invoice_item6 = create(:invoice_item_with_invoices, item_id: @item6.id)
+      @invoice_item = create(:invoice_item_with_invoices, item_id: @item.id, status: 2)
+      @invoice_item2 = create(:invoice_item_with_invoices, item_id: @item2.id, status: 2)
+      @invoice_item3 = create(:invoice_item_with_invoices, item_id: @item3.id, status: 1)
+      @invoice_item4 = create(:invoice_item_with_invoices, item_id: @item4.id, status: 1)
+      @invoice_item5 = create(:invoice_item_with_invoices, item_id: @item5.id, status: 0)
+      @invoice_item6 = create(:invoice_item_with_invoices, item_id: @item6.id, status: 0)
 
       @transactions = create_list(:transaction, 6, invoice_id: @invoice_item.invoice.id, result: "success")
       @transactions2 = create_list(:transaction, 7, invoice_id: @invoice_item2.invoice.id, result: "success")
@@ -51,6 +51,14 @@ RSpec.describe Merchant, type: :model do
       end
       it "Shows the number of successful transactions" do
         expect(@merchant1.find_top_customers.first.transaction_count).to eq(10)
+      end
+    end
+    describe "#items_not_shipped" do
+      it "Should return items not shipped" do
+        expect(@merchant1.items_not_shipped).to eq([@item3, @item4, @item5, @item6])
+      end
+      it 'should return item invoice ids' do
+        expect(@merchant1.items_not_shipped.first.invoice_id).to eq(@invoice_item3.invoice.id)
       end
     end
   end
