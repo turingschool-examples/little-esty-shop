a6b0bc0dbde8a5151fb8929f239ac2e4846649c9
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-02-24 18:52:47 -0700

    Create incomplete invoices class method for Invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,9 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def self.incomplete_invoices
+    joins(:invoice_items).select('invoices.id', 'invoices.created_at').where('invoice_items.status != ?', 2).distinct.order(created_at: :asc)
+  end
+
 end
