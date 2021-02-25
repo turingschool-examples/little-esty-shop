84b5b403c1c5363681c0bdcf6e25f06313c9fd37
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-02-24 18:53:22 -0700

    Formatting for admin user stories 4 and 5

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class Admin::DashboardController < ApplicationController
 
-def index
+	def index
 		@top_customers = Customer.top_customers
+		@incomplete_invoices = Invoice.incomplete_invoices
 	end
 
 end
