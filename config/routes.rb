2119987b5c193965788dcb2d17ca128d83f2964e
Joseph Budina <joebud3@gmail.com>
2021-02-24 17:16:12 -0700

    Add show

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :admin, only: [:index]
 
   namespace :admin do
-    resources :merchants, only: [:index]
+    resources :merchants, only: [:index, :show]
     resources :invoices
   end
 end
