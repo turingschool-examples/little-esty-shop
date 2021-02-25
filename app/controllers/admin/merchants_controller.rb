77e6c3640bdc404ab1670b550755cf60b80eeef8
kylejschulz <kylschulz@gmail.com>
2021-02-24 19:14:05 -0500

    Finish admin/merchant show page

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,6 +1,10 @@
 class Admin::MerchantsController < ApplicationController
   def index
-    @merchants = Merchant.all 
+    @merchants = Merchant.all
+  end
+
+  def show
+    @merchant = Merchant.find(params[:id])
   end
 
 
