ba8e0646a105dc18da404564b15db33df36e94fb
Katy La Tour <klatour324@gmail.com>
2021-02-24 20:43:32 -0600

    Add edit route for merchant_items and patch for update to merchant_items

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   # get  "/merchant/:merchant_id/dashboard", to: "merchants#dashboard"
   scope :merchant do
     get "/:merchant_id/dashboard", to: "merchants#dashboard"
+
     get "/:merchant_id/items", to: "merchant_items#index", as: "merchant_items"
     get "/:merchant_id/items/:item_id", to: "merchant_items#show", as: "merchant_item"
+    get "/:merchant_id/items/:item_id/edit", to: "merchant_items#edit", as: "merchant_item_edit"
+    patch "/:merchant_id/items/:item_id", to: "merchant_items#update"
+
     get "/:merchant_id/invoices", to: "invoices#index", as: "merchant_invoices"
   end
 
