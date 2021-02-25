890d50b6047638e665eee7a00da148730d658285
Dominic Padula <domopadula2192@gmail.com>
2021-02-25 09:50:18 -0700

    fix schema after conflicts

--- db/migrate/20210224230126_remove_merchant_id_from_invoices.rb
+++
@@ -1,5 +0,0 @@
-class RemoveMerchantIdFromInvoices < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :invoices, :merchant, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -18,27 +18,27 @@ ActiveRecord::Schema.define(version: 2021_02_23_222236) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "invoice_items", force: :cascade do |t|
     t.integer "quantity"
     t.integer "unit_price"
     t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "item_id"
     t.bigint "invoice_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
     t.index ["item_id"], name: "index_invoice_items_on_item_id"
   end
 
   create_table "invoices", force: :cascade do |t|
     t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "customer_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
   end
 
@@ -46,25 +46,25 @@ ActiveRecord::Schema.define(version: 2021_02_23_222236) do
     t.string "name"
     t.string "description"
     t.integer "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "merchant_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
   create_table "merchants", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "transactions", force: :cascade do |t|
     t.string "credit_card_number"
     t.date "credit_card_expiration_date"
     t.string "result"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "invoice_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
