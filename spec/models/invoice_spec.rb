1aa2382f4775a1fd90d010310bf296d4ef599b9f
DougWelchons <doug.welchons@gmail.com>
2021-02-24 21:42:41 -0700

    Add Model test for invoice.incomplete

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:items).through(:invoice_items) }
     it { should belong_to(:customer) }
   end
+
+  describe 'class methods' do
+    it '.incomplete' do
+      @customer = Customer.create!(first_name: "First1", last_name: "Last1")
+
+      @invoice1 = @customer.invoices.create!(status: 0)
+      @invoice2 = @customer.invoices.create!(status: 1)
+      @invoice3 = @customer.invoices.create!(status: 2)
+      @invoice4 = @customer.invoices.create!(status: 1)
+
+      expect(@customer.invoices.incomplete).to eq([@invoice1, @invoice2, @invoice4])
+    end
+  end
 end
