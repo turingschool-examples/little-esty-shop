ad8229420ddeb6952a58a1cd926b7dd4f50bf5d1
DougWelchons <doug.welchons@gmail.com>
2021-02-25 19:33:37 -0700

    Add test for item info on the admin invice show page

--- spec/features/admin/invoice/show_spec.rb
+++ spec/features/admin/invoice/show_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe "admin invoice show page" do
   before :each do
     @customer = Customer.create!(first_name: "First1", last_name: "Last1")
     @invoice = @customer.invoices.create!(status: 1)
+    @merchant = Merchant.create!(name: "Merchant1")
+    @item1 = @merchant.items.create!(name: "Item1", description: "Description1", unit_price: 2)
+    @item2 = @merchant.items.create!(name: "Item2", description: "Description2", unit_price: 3)
 
+    @invoice_item1 = InvoiceItem.create!(item: @item1, invoice: @invoice, quantity: 1, unit_price: 1, status: 0)
+    @invoice_item2 = InvoiceItem.create!(item: @item2, invoice: @invoice, quantity: 2, unit_price: 2, status: 1)
   end
 
   describe "when I visit an invoice show page as an admin" do
@@ -21,6 +26,26 @@ RSpec.describe "admin invoice show page" do
 
       within(".customer_info") do
         expect(page).to have_content("Customer: #{@customer.first_name} #{@customer.last_name}")
+      end
+    end
+
+    it "shows all items info" do
+      visit "admin/invoices/#{@invoice.id}"
+
+      within(".item_info") do
+        within("#item_id_#{@item1.id}") do
+          expect(page).to have_content("Item: #{@item1.name}")
+          expect(page).to have_content("Quanity ordered:  #{@invoice_item1.quanity}")
+          expect(page).to have_content("Price: #{@invoice_item1.unit_price}")
+          expect(page).to have_content("Item status: #{@invoice_item1.status}")
+        end
+
+        within("#item_id_#{@item2.id}") do
+          expect(page).to have_content("Item: #{@item2.name}")
+          expect(page).to have_content("Quanity ordered:  #{@invoice_item2.quanity}")
+          expect(page).to have_content("Price: #{@invoice_item2.unit_price}")
+          expect(page).to have_content("Item status: #{@invoice_item2.status}")
+        end
       end
     end
   end
