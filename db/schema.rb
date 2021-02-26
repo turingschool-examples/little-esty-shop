9921811f53a6b4190942dfcd7b44608c852d0b8c
Harrison Blake <harrison.r.blake.gmail.com>
2021-02-25 18:24:40 -0700

    Add item status column and finish enable/disable button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_203939) do
+ActiveRecord::Schema.define(version: 2021_02_26_003438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_203939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.boolean "status", default: true
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
