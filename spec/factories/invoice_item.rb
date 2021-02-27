680aac7582b03fb5b3f20fe6a92058623b53341e
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-26 17:18:43 -0700

    Update testing for admin dashboard stories

--- spec/factories/invoice_item.rb
+++ spec/factories/invoice_item.rb
@@ -3,6 +3,5 @@ FactoryBot.define do
   factory :invoice_item do
     quantity { 10 }
     unit_price {200}
-    status{2}
   end
 end
--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -3,37 +3,44 @@ require "rails_helper"
 RSpec.describe"As an admin" do
 		before :each do
 		@mer_1 = create(:merchant)
-
-		@customers = []
-		@customers << create(:customer, first_name: "A", last_name: "1")
-		@customers << create(:customer, first_name: "B", last_name: "1")
-		@customers << create(:customer, first_name: "C", last_name: "1")
-		@customers << create(:customer, first_name: "D", last_name: "1")
-		@customers << create(:customer, first_name: "E", last_name: "1")
-		@customers << create(:customer, first_name: "A", last_name: "2")
-		@customers << create(:customer, first_name: "B", last_name: "2")
-		@customers << create(:customer, first_name: "C", last_name: "2")
-		@customers << create(:customer, first_name: "D", last_name: "2")
-		@customers << create(:customer, first_name: "E", last_name: "2")
-
-
-		#@customers = create_list(:customer, 10)
-		@items = create_list(:item, 20, merchant_id: @mer_1.id )
-
-		invoices = @customers.map do |customer|
-			create(:invoice,status: 2,customer_id: customer.id)
-		end
-
-		invoice_items = invoices.map do |invoice|
-			@items.map do |item|
-				create(:invoice_item, item_id: item.id, invoice_id: invoice.id)
-			end
-		end
-
-		results = ["success", "failed"]
-		transactions = invoices.map do |invoice|
-			create(:transaction, result: "success", invoice_id: invoice.id)
-		end
+		@cust_1 = create(:customer, first_name: "A", last_name: "1")
+		@cust_2 = create(:customer, first_name: "B", last_name: "1")
+		@cust_3 = create(:customer, first_name: "C", last_name: "1")
+		@cust_4 = create(:customer, first_name: "D", last_name: "1")
+		@cust_5 = create(:customer, first_name: "E", last_name: "1")
+		@cust_6 = create(:customer, first_name: "F", last_name: "1")
+		@cust_7 = create(:customer, first_name: "G", last_name: "1")
+		@cust_8 = create(:customer, first_name: "H", last_name: "1")
+		@cust_9 = create(:customer, first_name: "I", last_name: "1")
+		@cust_10 = create(:customer, first_name: "J", last_name: "1")
+		@item_1 = create(:item, merchant_id: @mer_1.id)
+		@item_2 = create(:item, merchant_id: @mer_1.id)
+		@item_3 = create(:item, merchant_id: @mer_1.id)
+		@item_4 = create(:item, merchant_id: @mer_1.id)
+		@item_5 = create(:item, merchant_id: @mer_1.id)
+		@item_6 = create(:item, merchant_id: @mer_1.id)
+		@invoice1 = create(:invoice, customer_id: @cust_1.id)
+		@invoice2 = create(:invoice, customer_id: @cust_2.id)
+		@invoice3 = create(:invoice, customer_id: @cust_3.id)
+		@invoice4 = create(:invoice, customer_id: @cust_4.id)
+		@invoice5 = create(:invoice, customer_id: @cust_5.id)
+		@invoice6 = create(:invoice, customer_id: @cust_6.id)
+		@invoice_item1 = create(:invoice_item, status: 0 ,item_id:@item_1.id, invoice_id:@invoice1.id)
+		@invoice_item2 = create(:invoice_item, status: 0 , item_id:@item_2.id, invoice_id:@invoice2.id)
+		@invoice_item3 = create(:invoice_item, status: 0 , item_id:@item_3.id, invoice_id:@invoice3.id)
+		@invoice_item4 = create(:invoice_item, item_id:@item_4.id, invoice_id:@invoice4.id)
+		@invoice_item5 = create(:invoice_item, item_id:@item_5.id, invoice_id:@invoice6.id)
+		@invoice_item6 = create(:invoice_item, item_id:@item_6.id, invoice_id:@invoice6.id)
+		@transaction1 = create(:transaction, result: "success", invoice_id: @invoice1.id)
+		@transaction2 = create(:transaction, result: "failed", invoice_id: @invoice1.id)
+		@transaction2 = create(:transaction, result: "success", invoice_id: @invoice2.id)
+		@transaction2 = create(:transaction, result: "success", invoice_id: @invoice2.id)
+		@transaction3 = create(:transaction, result: "success", invoice_id: @invoice1.id)
+		@transaction4 = create(:transaction, result: "success", invoice_id: @invoice4.id)
+		@transaction5 = create(:transaction, result: "success", invoice_id: @invoice5.id)
+		@transaction6 = create(:transaction, result: "success", invoice_id: @invoice3.id)
+		@transaction7 = create(:transaction, result: "success", invoice_id: @invoice3.id)
+		@transaction8 = create(:transaction, result: "success", invoice_id: @invoice6.id)
 	end
 
 	describe "when I visit the admin dashboard" do
@@ -52,26 +59,20 @@ RSpec.describe"As an admin" do
 
 		it"shows the top 5 customers and their number of transactions"do
 			visit "/admin"
-			expect(page).to have_content("Name: A 1 -- Total Transactions: 1")
-			expect(page).to have_content("Name: B 1 -- Total Transactions: 1")
-			expect(page).to have_content("Name: C 1 -- Total Transactions: 1")
+			expect(page).to have_content("Name: A 1 -- Total Transactions: 2")
+			expect(page).to have_content("Name: B 1 -- Total Transactions: 2")
+			expect(page).to have_content("Name: C 1 -- Total Transactions: 2")
 			expect(page).to have_content("Name: D 1 -- Total Transactions: 1")
 			expect(page).to have_content("Name: E 1 -- Total Transactions: 1")
 		end
 
 		it "displays a section for incomplete invoices" do
 			visit "/admin"
-
-			invoices = @customers.map do |customer|
-				create(:invoice,status: 0,customer_id: customer.id)
+			within("#incomplete-#{@invoice1.id}") do
+				expect(page).to have_link("#{@invoice1.id}")
 			end
-			invoice_items = invoices.flat_map do |invoice|
-				@items.map do |item|
-					create(:invoice_item,status: 0 ,item_id: item.id, invoice_id: invoice.id)
-				end
-			end
-			
-
+			expect("#{@invoice1.id}").to appear_before("#{@invoice2.id}")
+			expect("#{@invoice2.id}").to appear_before("#{@invoice3.id}")
 		end
 
 	end
