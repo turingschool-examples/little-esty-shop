765904b61adff60697528936861333f9b0607f4f
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-27 13:31:23 -0600

    Add tests for edge case and modify view. all test pass.

--- spec/features/merchants/merchant_invoice_show_page_spec.rb
+++ spec/features/merchants/merchant_invoice_show_page_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "Merchant Invoice Show Page" do
   before :each do
     @merchant = Merchant.create(name: "John's Jewelry")
+    @not_merchant = Merchant.create(name: "Mikey Mouse Rings")
+    @not_item = @not_merchant.items.create(name: "Mouse Ring", description: "Oh Toodles", unit_price: 12.99)
     @item1 = @merchant.items.create(name: "Gold Ring", description: "14K Wedding Band",
                                   unit_price: 599.95)
     @item2 = @merchant.items.create(name: "Pearl Necklace", description: "Beautiful White Pearls",
@@ -17,9 +19,9 @@ RSpec.describe "Merchant Invoice Show Page" do
     @invoice_item2 = InvoiceItem.create!(invoice_id: @invoice1.id,
                                        item_id: @item2.id, quantity: 2,
                                        unit_price: 250.00, status: 0)
-    @invoice_item3 = InvoiceItem.create!(invoice_id: @invoice2.id,
-                                       item_id: @item2.id, quantity: 1000,
-                                       unit_price: 200.00, status: 1)
+    @not_invoice_item = InvoiceItem.create!(invoice_id: @invoice2.id,
+                                       item_id: @not_item.id, quantity: 976,
+                                       unit_price: 10.00, status: 1)
   end
   describe "When I visit my merchant's invoice show page(/merchants/merchant_id/invoices/invoice_id)" do
     it "I see the invoice attributes listed" do
@@ -35,6 +37,26 @@ RSpec.describe "Merchant Invoice Show Page" do
         within "#invoice-customer-info" do
           expect(page).to have_content(@customer.name)
           expect(page).to_not have_content(@not_customer.name)
+        end
+    end
+    it "Then I see all of my items on the invoice(item name, quantity, price sold, invoice item status)" do
+      visit "/merchant/#{@merchant.id}/invoices/#{@invoice1.id}"
+        expect(page).to_not have_content(@not_item.name)
+        expect(page).to_not have_content(@not_invoice_item.quantity)
+        expect(page).to_not have_content(@not_invoice_item.unit_price)
+        expect(page).to_not have_content(@not_invoice_item.status)
+
+        within "#invoice-items-#{@invoice_item1.id}-info" do
+          expect(page).to have_content(@item1.name)
+          expect(page).to have_content(@invoice_item1.quantity)
+          expect(page).to have_content(@invoice_item1.unit_price)
+          expect(page).to have_content(@invoice_item1.status)
+        end
+        within "#invoice-items-#{@invoice_item2.id}-info" do
+          expect(page).to have_content(@item2.name)
+          expect(page).to have_content(@invoice_item2.quantity)
+          expect(page).to have_content(@invoice_item2.unit_price)
+          expect(page).to have_content(@invoice_item2.status)
         end
     end
   end
