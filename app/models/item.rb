c6a9a9a03d9968728506755fa4055359299b452f
Dominic Padula <domopadula2192@gmail.com>
2021-02-27 16:27:53 -0700

    crazy query for best day method

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
 
   def change_status(status)
     self.item_status = status
@@ -16,5 +17,10 @@ class Item < ApplicationRecord
 
   def self.disabled_items
     where(item_status: false)
+  end
+
+  def best_day
+    invoices.joins(:invoice_items).joins(:transactions).select('invoices.*, Sum(invoice_items.quantity * invoice_items.unit_price) AS Revenue').group("invoices.id, DATE_TRUNC('day',invoices.created_at)").max.created_at
+
   end
 end
