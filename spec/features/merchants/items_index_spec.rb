86f7daed22a7d625b5e3857c50a15ab015c7551f
Dominic Padula <domopadula2192@gmail.com>
2021-02-27 09:50:12 -0700

    feature and model testing for user story 8

--- spec/features/merchants/items_index_spec.rb
+++ spec/features/merchants/items_index_spec.rb
@@ -34,28 +34,54 @@ RSpec.describe 'Merchant Items index page' do
 
     visit merchant_items_path(mer_1)
 
-    within("#item-#{item_1.id}") do
+    within("#enabled_items-#{item_1.id}") do
       expect(page).to have_button("Disable")
     end
 
-    within("#item-#{item_2.id}") do
+    within("#disabled_items-#{item_2.id}") do
       expect(page).to have_button("Enable")
     end
   end
 
   it "when a button is clicked the status changes" do
     mer_1 = create(:merchant)
-    item_1 = create(:item, merchant_id: mer_1.id, item_status: true)
+    item_1 = create(:item, name:"item_1", merchant_id: mer_1.id, item_status: true)
     item_2 = create(:item, merchant_id: mer_1.id, item_status: false)
 
     visit merchant_items_path(mer_1)
 
-    within("#item-#{item_1.id}") do
+    within("#enabled_items-#{item_1.id}") do
       click_button("Disable")
     end
-
-    within("#item-#{item_1.id}") do
+    within("#disabled_items-#{item_1.id}") do
       expect(page).to have_button("Enable")
     end
+  end
+
+  it "will group by status for enable and disable" do
+    mer_1 = create(:merchant)
+    item_1 = create(:item, merchant_id: mer_1.id, item_status: true)
+    item_2 = create(:item, merchant_id: mer_1.id, item_status: false)
+    item_3 = create(:item, merchant_id: mer_1.id, item_status: true)
+
+    item_4 = create(:item, merchant_id: mer_1.id,item_status: false)
+    item_5 = create(:item, merchant_id: mer_1.id, item_status: true)
+    item_6 = create(:item, merchant_id: mer_1.id, item_status: false)
+    visit merchant_items_path(mer_1)
+    within("#enabled_items-#{item_1.id}") do
+      expect(page).to have_content(item_1.name)
+    end
+      within("#enabled_items-#{item_3.id}") do
+      expect(page).to have_content(item_3.name)
+    end
+    within("#disabled_items-#{item_2.id}") do
+      expect(page).to have_content(item_2.name)
+      expect(page).not_to have_content(item_4.id)
+    end
+      within("#disabled_items-#{item_4.id}") do
+      expect(page).to have_content(item_4.name)
+      expect(page).not_to have_content(item_1.name)
+    end
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,4 +13,19 @@ RSpec.describe Item, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:invoices).through(:invoice_items) }
   end
+
+  describe "class methods " do
+    it "should filter by enabled and disabled buttons" do
+      mer_1 = create(:merchant)
+      item_1 = create(:item, merchant_id: mer_1.id, item_status: true)
+      item_2 = create(:item, merchant_id: mer_1.id, item_status: false)
+      item_3 = create(:item, merchant_id: mer_1.id, item_status: true)
+
+      item_4 = create(:item, merchant_id: mer_1.id,item_status: false)
+      item_5 = create(:item, merchant_id: mer_1.id, item_status: true)
+      item_6 = create(:item, merchant_id: mer_1.id, item_status: false)
+      expect(Item.enabled_items).to eq([item_1, item_3, item_5])
+      expect(Item.disabled_items).to eq([item_2, item_4, item_6])
+    end
+  end
 end
