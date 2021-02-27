9d655dccc63b52d1cdfcf5b24259ddbf7da5dcad
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-26 19:00:31 -0700

    Add tests for enable and disable buttons

--- spec/features/merchants/items_index_spec.rb
+++ spec/features/merchants/items_index_spec.rb
@@ -26,4 +26,36 @@ RSpec.describe 'Merchant Items index page' do
       click_link(item_1.name)
       expect(page).to have_current_path(merchant_item_path(mer_1, item_1))
   end
+
+  it "has a button to disable or enable an item" do
+    mer_1 = create(:merchant)
+    item_1 = create(:item, merchant_id: mer_1.id, item_status: true)
+    item_2 = create(:item, merchant_id: mer_1.id, item_status: false)
+
+    visit merchant_items_path(mer_1)
+
+    within("#item-#{item_1.id}") do
+      expect(page).to have_button("Disable")
+    end
+
+    within("#item-#{item_2.id}") do
+      expect(page).to have_button("Enable")
+    end
+  end
+
+  it "when a button is clicked the status changes" do
+    mer_1 = create(:merchant)
+    item_1 = create(:item, merchant_id: mer_1.id, item_status: true)
+    item_2 = create(:item, merchant_id: mer_1.id, item_status: false)
+
+    visit merchant_items_path(mer_1)
+
+    within("#item-#{item_1.id}") do
+      click_button("Disable")
+    end
+
+    within("#item-#{item_1.id}") do
+      expect(page).to have_button("Enable")
+    end
+  end
 end
