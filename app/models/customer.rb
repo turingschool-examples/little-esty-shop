8211d7c15e87c50e76ab69f85322edf0bb696b62
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-27 12:09:55 -0700

    Add relationships in customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,6 @@ class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
   has_many :invoice_items, through: :invoices
+  has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
 end
