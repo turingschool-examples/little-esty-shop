7f8d41fca5eaf43622ebf31477ea138284e956a4
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-02-27 10:50:24 -0700

    Add total revenue feature to admin invoice show page

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -8,8 +8,7 @@ class Admin::InvoicesController < ApplicationController
 		@customer = Customer.find(@invoice.customer_id)
 		invoice_id = @invoice.id
 		@items = InvoiceItem.items_by_invoice(invoice_id)
-
-
+		@revenue = @items.sum('invoice_items.quantity * invoice_items.unit_price')
 	end
 
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -5,7 +5,7 @@ class InvoiceItem < ApplicationRecord
   enum status: [:pending, :packaged, :shipped]
 
   def self.items_by_invoice(invoice_id)
-    joins(:invoice, :item).select('items.name', 'invoice_items.quantity', 'invoice_items.unit_price', 'invoice_items.status', 'invoices.id', '(invoice_items.quantity * invoice_items.unit_price) as revenue').where("invoices.id = #{invoice_id}")
+    joins(:invoice, :item).select('items.name', 'invoice_items.quantity', 'invoice_items.unit_price', 'invoice_items.status', 'invoices.id').where("invoices.id = #{invoice_id}")
   end
 
 end
