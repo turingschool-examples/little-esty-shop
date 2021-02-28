48e4cc6ae443bc6210c3994b1a8cb2224672b65a
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-28 16:48:03 -0600

    Add functionality to display items that are ready to ship on each merchant dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @merchant_top_five_customers = @merchant.top_five_customers
+    @items_ready_to_ship = @merchant.items_ready_to_ship
   end
 end
