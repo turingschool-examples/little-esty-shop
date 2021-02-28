5f4355db70e8dd4909c6e9f0ed522379357737d2
Jordan Beck <jordanfbeck@gmail.com>
2021-02-28 14:27:05 -0700

    Add passing feature and model tests for admin user story four

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :dashboard, only: [:index]
+    resources :invoices, only: [:show]
     # resources :admins
     resources :merchants
     resources :customers
