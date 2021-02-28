9f3c5fda354f395df39a74c058480a33a6a82569
Trevor Suter <Trevorsuter@icloud.com>
2021-02-28 14:20:36 -0700

    new merchant finished, added changes to the csv_load rake and a migration to the schema

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -14,16 +14,19 @@ RSpec.describe "When I visit the Admin Merchants index page", type: :feature do
     expect(page).to have_content(@gary.name)
     expect(page).to have_content(@roger.name)
   end
-  scenario "Each merchant has a link to their show page" do
+  scenario "Each merchant has a link to their show page and their status" do
     visit "/admin/merchants"
 
     within("#merchant-#{@joe.id}") do
+      expect(page).to have_content(@joe.status)
       expect(page).to have_link("#{@joe.name}", href: "/admin/merchants/#{@joe.id}")
     end
     within("#merchant-#{@gary.id}") do
+      expect(page).to have_content(@gary.status)
       expect(page).to have_link("#{@gary.name}", href: "/admin/merchants/#{@gary.id}")
     end
     within("#merchant-#{@roger.id}") do
+      expect(page).to have_content(@roger.status)
       expect(page).to have_link("#{@roger.name}", href: "/admin/merchants/#{@roger.id}")
     end
   end
@@ -40,5 +43,20 @@ RSpec.describe "When I visit the Admin Merchants index page", type: :feature do
     click_link "Create Merchant"
 
     expect(current_path).to eq("/admin/merchants/new")
+  end
+
+  scenario "I have a button to enable and disable a merchant" do
+    visit "/admin/merchants"
+
+    within("#merchant-#{@joe.id}") do
+      expect(page).to have_content("disabled")
+      expect(page).to have_button("enable")
+      click_button "enable"
+    end
+    expect(current_path).to eq("/admin/merchants")
+    within("#merchant-#{@joe.id}") do
+      expect(page).to have_content("enabled")
+      expect(page).to have_button("disable")
+    end
   end
 end
