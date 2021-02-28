1b7e3da90bd3531214680fa90cce09fefec5b4f4
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-27 21:47:57 -0600

    Add #change_status to InvoiceItem. all tests pass.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,10 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
-  
+
   enum status: {pending: 0, packaged: 1, shipped: 2}
+
+  def change_status(status)
+    self.update(status: status.downcase)
+  end
 end
