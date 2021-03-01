1d334494e7d8fd1f462e21431dfb9321bb818891
Diana Buffone <diana.pamelaet@gmail.com>
2021-03-01 10:36:00 -0500

    Complete admin invoice customer information user story

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -1,29 +1,48 @@
 require 'rails_helper'
 
-describe 'When I visit the admin invoices (/admin/invoices)' do
-    before (:each) do
-      @invoice_1 = create(:invoice)
-      @invoice_2 = create(:invoice)
-      @invoice_3 = create(:invoice)
+RSpec.describe 'Admin Invoices Show page' do
+  describe 'When I visit the admin invoices (/admin/invoices)' do
+      before (:each) do
+        @invoice_1 = create(:invoice)
+        @invoice_2 = create(:invoice)
+        @invoice_3 = create(:invoice)
+      end
+
+    it 'When I click on the name of a invoice from the admin invoices index page' do
+      visit "/admin/invoices"
+
+      expect(page).to have_content("#{@invoice_1.id}")
+      expect(page).to have_link("#{@invoice_1.id}")
+      expect(page).to have_content("#{@invoice_2.id}")
+      expect(page).to have_link("#{@invoice_2.id}")
+      expect(page).to have_content("#{@invoice_3.id}")
+      expect(page).to have_link("#{@invoice_3.id}")
     end
 
-  it 'When I click on the name of a invoice from the admin invoices index page' do
-    visit "/admin/invoices"
+    it 'Then I am taken to that invoices admin show page (/admin/invoices/invoice_id)' do
+      visit "/admin/invoices"
 
-    expect(page).to have_content("#{@invoice_1.id}")
-    expect(page).to have_link("#{@invoice_1.id}")
-    expect(page).to have_content("#{@invoice_2.id}")
-    expect(page).to have_link("#{@invoice_2.id}")
-    expect(page).to have_content("#{@invoice_3.id}")
-    expect(page).to have_link("#{@invoice_3.id}")
+      expect(page).to have_link("#{@invoice_1.id}")
+      click_link("#{@invoice_1.id}")
+      expect(current_path).to eq("/admin/invoices/#{@invoice_1.id}")
+      expect(page).to have_content("#{@invoice_1.id}")
+    end
   end
 
-  it 'Then I am taken to that invoices admin show page (/admin/invoices/invoice_id)' do
-    visit "/admin/invoices"
+  describe 'Then I see all of the customer information related to that invoice including:' do
+    it 'displays customer first name and last name' do
 
-    expect(page).to have_link("#{@invoice_1.id}")
-    click_link("#{@invoice_1.id}")
-    expect(current_path).to eq("/admin/invoices/#{@invoice_1.id}")
-    expect(page).to have_content("#{@invoice_1.id}")
+      customer = create(:customer, first_name: "Minnie")
+
+      invoice = create(:invoice, customer_id: customer.id)
+
+
+      visit "/admin/invoices/#{invoice.id}"
+
+      within (".customer") do
+        expect(page).to have_content(customer.first_name)
+        expect(page).to have_content(customer.last_name)
+      end
+    end
   end
 end
