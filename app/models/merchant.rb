36b8faf1467ddd639eb6b2347a14f5a20dc2378b
Harrison Blake <harrison.r.blake.gmail.com>
2021-03-01 15:32:02 -0700

    Fix syntax error

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -16,6 +16,7 @@ class Merchant < ApplicationRecord
 
   def items_by_status_false
     items.where(status: false)
+  end
 
   def customers
     Customer.joins(invoices: :items).where('items.merchant_id = ?', self.id).distinct
