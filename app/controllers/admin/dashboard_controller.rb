ee3204b490652613f22e115b9cda429182cf1a1b
Arika Turing <ablenker@gmail.com>
2021-02-28 18:09:29 -0700

    Add passing code and feature/model tests for admin user story five

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,9 @@ class Admin::DashboardController < ApplicationController
     # require "pry";binding.pry
     @top_five_customers = Customer.top_five_customers
     @invoice_items = InvoiceItem.all
+    # require "pry";binding.pry
+    @invoices = Invoice.all
+    # @formatted_created_at = Invoice.find(params[:id]).format_time
   end
 
   # def show
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,14 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
   has_many :transactions
 
+
+  def format_time
+    # require "pry";binding.pry
+    created_at.strftime("%A, %B %d, %Y")
+    # InvoiceItem.where.not(status: "shipped")
+    # joins(:invoice_item).all.where.not(status: "shipped").pluck(:created_at).strftime("%A, %B %d, %Y")
+    # Invoice.where(invoice_item: :)
+    # Invoice.pluck(:created_at)
+    # Invoice.all.where(invoice.id = invoice_items.invoice_id).pluck(:created_at)
+  end
 end
