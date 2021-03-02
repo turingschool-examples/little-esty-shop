3a8cd02ccbe418fcc3a9ebf3c4e737db49bc300f
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-02 08:53:34 -0700

    Add test for selector

--- spec/features/merchants/invoices_show_spec.rb
+++ spec/features/merchants/invoices_show_spec.rb
@@ -106,4 +106,40 @@ RSpec.describe 'Merchant Invoices index page' do
 
     expect(page).to have_content("Total Revenue from Invoice: 76")
   end
+
+  it " " do
+    mer_1 = create(:merchant)
+    mer_2 = create(:merchant)
+    cust_1 = create(:customer)
+    cust_2 = create(:customer)
+    cust_3 = create(:customer)
+    cust_4 = create(:customer)
+    item_1 = create(:item, name: "item_1", merchant_id: mer_1.id)
+    item_2 = create(:item, name: "item_2", merchant_id: mer_1.id)
+    item_3 = create(:item, name: "item_3", merchant_id: mer_1.id)
+    item_4 = create(:item, name:"item_4", merchant_id: mer_1.id)
+    item_5 = create(:item, name:"item_4", merchant_id: mer_2.id)
+    item_6 = create(:item, name:"item_4", merchant_id: mer_2.id)
+
+    invoice1 = create(:invoice, customer_id: cust_1.id)
+    invoice2 = create(:invoice, customer_id: cust_2.id)
+    invoice3 = create(:invoice, customer_id: cust_3.id)
+    invoice4 = create(:invoice, customer_id: cust_4.id)
+    invoice5 = create(:invoice, customer_id: cust_4.id)
+    invoice6 = create(:invoice, customer_id: cust_4.id)
+    invoice_item1 = create(:invoice_item, item_id:item_1.id, invoice_id:invoice1.id, quantity: 8, unit_price: 2, status: "shipped")
+    invoice_item2 = create(:invoice_item, item_id:item_2.id, invoice_id:invoice1.id, quantity: 10, unit_price: 5)
+    invoice_item3 = create(:invoice_item, item_id:item_2.id, invoice_id:invoice1.id, quantity: 5, unit_price: 2)
+    invoice_item4 = create(:invoice_item, item_id:item_4.id, invoice_id:invoice4.id, quantity: 3, unit_price: 5)
+    invoice_item4 = create(:invoice_item, item_id:item_5.id, invoice_id:invoice6.id, quantity: 3, unit_price: 5)
+    invoice_item4 = create(:invoice_item, item_id:item_6.id, invoice_id:invoice5.id, quantity: 3, unit_price: 5)
+    transaction1 = create(:transaction, invoice_id: invoice1.id, result: "success")
+    visit merchant_invoice_path(mer_1, invoice1)
+    save_and_open_page
+    # expect(page).to have_select(id: "invoice_item_status", selected: "pending")
+    select "packaged", from: "invoice_item[invoice_item_status]"
+    expect(page).to have_button("Update Item Status")
+    click_button("Update Item Status")
+    expect(page).to have_select("invoice_item[invoice_item_status]", selected: "packaged")
+  end
 end
