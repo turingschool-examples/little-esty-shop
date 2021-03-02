2cff68573b00a32a857a27140bbe5e4f3a03ba73
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-02 16:54:39 -0700

    Add repo method

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,11 +3,12 @@ class Admin::DashboardController < ApplicationController
 	def index
 		@json_commits = GithubService.commits
 		@json_users = GithubService.users
-
+		@json_repo = GithubService.repo 
 
 
 		@top_customers = Customer.top_customers
 		@incomplete_invoices = Invoice.incomplete_invoices
 	end
+
 
 end
