af088e7253092cf4f1da166aaa811ca62f75d996
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-01 19:42:17 -0700

    Create a class to parse API data

---
+++ app/services/github_service.rb
@@ -0,0 +1,17 @@
+require 'faraday'
+require 'json'
+require './app/poros/github'
+
+class GithubService
+  def github_information
+    endpoint = 'https://api.github.com/repos/jordanfbeck0528/little-esty-shop'
+    get_data(endpoint)
+  end
+
+  def get_data(endpoint)
+    response = Faraday.get(endpoint)
+    parsed = JSON.parse(response.body, symbolize_names: true)
+  end
+end
+
+info = GithubService.new.github_information
