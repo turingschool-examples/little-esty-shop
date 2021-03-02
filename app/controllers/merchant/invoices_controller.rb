4666151d68a9eb978d4b4f19ccb61940d07a76d7
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-02 08:53:00 -0700

    Add action update

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -5,9 +5,15 @@ class Merchant::InvoicesController < ApplicationController
   end
 
   def show
+    @merchant = Merchant.find(params[:merchant_id])
     @invoice = Invoice.find(params[:id])
     @customer = Customer.find(@invoice.customer_id)
     @invoice_item = @invoice.invoice_items.first
-    @item_name = Item.find(@invoice_item.item_id).name 
+    @item_name = Item.find(@invoice_item.item_id).name
+  end
+
+  def update
+    invoice_item = InvoiceItem.find(params[:id])
+    invoice_item.update(status: params[:invoice_item][:invoice_item_status])
   end
 end
