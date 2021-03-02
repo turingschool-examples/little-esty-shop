f440c2fda2aaa0cea719177fdae13f8b8697d16e
Katy La Tour <klatour324@gmail.com>
2021-03-01 22:10:46 -0600

    Add AR method for best sales date for an item

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,14 @@ class Item < ApplicationRecord
   def self.active
     where('status = ?', 'Active')
   end
+
+  def best_sales_date
+    invoice_items.joins(:invoice)
+    .select("invoices.created_at, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue")
+    .group("invoices.created_at")
+    .order(total_revenue: :desc)
+    .first
+    .created_at
+    .strftime("%m/%d/%Y")
+  end
 end
