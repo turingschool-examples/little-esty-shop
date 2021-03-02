347f04d87266505d6e19e55104e7101168bee713
Harrison Blake <harrison.r.blake.gmail.com>
2021-03-02 13:52:53 -0700

    Finish top 5 item by revenue no tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,9 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
 
   def unshipped
     items.joins(invoice_items: :invoice)
@@ -15,5 +19,17 @@ class Merchant < ApplicationRecord
 
   def items_by_status_false
     items.where(status: false)
+  end
+
+  def top_5_items_by_revenue
+    items.joins(invoices: :transactions)
+    .select("items.*, sum(invoice_items.unit_price * invoice_items.quantity) as revenue")
+    .where("transactions.result = ?", 0)
+    .group("items.id")
+    .order(revenue: :desc).limit(5)
+  end
+
+  def top_days_by_item
+    
   end
 end
