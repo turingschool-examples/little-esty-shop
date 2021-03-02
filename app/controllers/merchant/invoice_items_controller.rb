0da00ce1f84fab2b31d3e0c80d98a9d4f698ce74
Dominic Padula <domopadula2192@gmail.com>
2021-03-02 16:11:26 -0700

    add update invoice item action for specific invoices

--- app/controllers/merchant/invoice_items_controller.rb
+++ app/controllers/merchant/invoice_items_controller.rb
@@ -1,8 +1,11 @@
 class Merchant::InvoiceItemsController < ApplicationController
 
   def update
+    merchant = Merchant.find(params[:invoice_item][:merchant_id])
+    invoice = Invoice.find(params[:invoice_item][:invoice_id])
     invoice_item = InvoiceItem.find(params[:id])
     invoice_item.update(status: params[:invoice_item][:invoice_item_status])
+    redirect_to merchant_invoice_path(merchant,invoice)
   end
 
   private
