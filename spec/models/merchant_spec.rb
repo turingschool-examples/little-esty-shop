05ec3a53f388810926785a85b54edce816b68894
Jake Volpe <volpe.jacob@gmail.com>
2021-03-03 14:12:50 -0700

    Add model testing top five customers and their revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -73,6 +73,67 @@ RSpec.describe Merchant, type: :model do
 
       end
     end
+
+    it "can return top five cutomers and their revenue" do
+      @customer_1 = create(:customer, first_name: "Minnie")
+      @customer_2 = create(:customer, first_name: "Lloyd")
+      @customer_3 = create(:customer, first_name: "Hector")
+      @customer_4 = create(:customer, first_name: "Andrea")
+      @customer_5 = create(:customer, first_name: "Fred")
+      @customer_6 = create(:customer, first_name: "Payton")
+      @customer_7 = create(:customer, first_name: "7")
+      @invoice_1 = create(:invoice, status: 'completed', customer_id: @customer_1.id)
+      @invoice_2 = create(:invoice, status: 'completed', customer_id: @customer_2.id)
+      @invoice_3 = create(:invoice, status: 'completed', customer_id: @customer_3.id)
+      @invoice_4 = create(:invoice, status: 'completed', customer_id: @customer_4.id)
+      @invoice_5 = create(:invoice, status: 'completed', customer_id: @customer_5.id)
+      @invoice_6 = create(:invoice, status: 'completed', customer_id: @customer_6.id)
+      @invoice_7 = create(:invoice, status: 'completed', customer_id: @customer_7.id)
+      @merchant_1 = create(:merchant)
+      @merchant_2 = create(:merchant)
+      @merchant_3 = create(:merchant)
+      @merchant_4 = create(:merchant)
+      @merchant_5 = create(:merchant)
+      @merchant_6 = create(:merchant)
+      @merchant_7 = create(:merchant)
+      @merchant_8 = create(:merchant)
+      @merchant_9 = create(:merchant)
+      @item_1 = create(:item, merchant: @merchant_1, unit_price: 1)
+      @item_2 = create(:item, merchant: @merchant_2, unit_price: 2)
+      @item_3 = create(:item, merchant: @merchant_3, unit_price: 3)
+      @item_4 = create(:item, merchant: @merchant_4, unit_price: 4)
+      @item_5 = create(:item, merchant: @merchant_5, unit_price: 5)
+      @item_6 = create(:item, merchant: @merchant_6, unit_price: 6)
+      @item_7 = create(:item, merchant: @merchant_7, unit_price: 1)
+      @invoice_item_1 = create(:invoice_item, item_id: @item_1.id, invoice_id: @invoice_1.id, unit_price: 1, status: 2)
+      @invoice_item_2 = create(:invoice_item, item_id: @item_2.id, invoice_id: @invoice_2.id, unit_price: 2, status: 2)
+      @invoice_item_3 = create(:invoice_item, item_id: @item_3.id, invoice_id: @invoice_3.id, unit_price: 3, status: 2)
+      @invoice_item_4 = create(:invoice_item, item_id: @item_4.id, invoice_id: @invoice_4.id, unit_price: 4, status: 2)
+      @invoice_item_5 = create(:invoice_item, item_id: @item_5.id, invoice_id: @invoice_5.id, unit_price: 5, status: 2)
+      @invoice_item_6 = create(:invoice_item, item_id: @item_6.id, invoice_id: @invoice_6.id, unit_price: 6, status: 2)
+      @invoice_item_7 = create(:invoice_item, item_id: @item_7.id, invoice_id: @invoice_7.id, unit_price: 1, status: 2)
+      @transaction_1 = create(:transaction, invoice_id: @invoice_1.id, result: 'success')
+      @transaction_2 = create(:transaction, invoice_id: @invoice_2.id, result: 'success')
+      @transaction_3 = create(:transaction, invoice_id: @invoice_3.id, result: 'success')
+      @transaction_4 = create(:transaction, invoice_id: @invoice_4.id, result: 'success')
+      @transaction_5 = create(:transaction, invoice_id: @invoice_5.id, result: 'success')
+      @transaction_6 = create(:transaction, invoice_id: @invoice_6.id, result: 'success')
+      @transaction_7 = create(:transaction, invoice_id: @invoice_7.id, result: 'success')
+      @transaction_7 = create(:transaction, invoice_id: @invoice_7.id, result: 'failed')
+
+      top_five = [@merchant_6,@merchant_5,@merchant_4, @merchant_3, @merchant_2]
+      
+      expect(Merchant.top_five_merchants_by_revenue).to eq(top_five)
+      expect(Merchant.top_five_merchants_by_revenue.length).to eq(5)
+      
+      within("#top-five") do
+        expect(@merchant_6.revenue).to have_content(@invoice_item_6.unit_price)
+        expect(@merchant_5.revenue).to have_content(@invoice_item_5.unit_price)
+        expect(@merchant_4.revenue).to have_content(@invoice_item_4.unit_price)
+        expect(@merchant_3.revenue).to have_content(@invoice_item_3.unit_price)
+        expect(@merchant_2.revenue).to have_content(@invoice_item_2.unit_price)
+      end
+    end
   end
 end
   
