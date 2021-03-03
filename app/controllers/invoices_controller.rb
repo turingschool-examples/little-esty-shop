c41040bc29885a0bdeee93834ba0cdea70d32abb
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-02 18:21:56 -0700

    Write code to pass user story 2

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,4 +2,8 @@ class InvoicesController < ApplicationController
   def index
     @merchant_invoices = Merchant.merchant_invoices(params[:merchant_id])
   end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -41,5 +41,4 @@ class Merchant < ApplicationRecord
   .invoices
   .distinct
  end
-
 end
