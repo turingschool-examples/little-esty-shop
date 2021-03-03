ed7f3b14a5639f26232689ac532e26a3d053d522
Arika Turing <ablenker@gmail.com>
2021-03-03 13:46:09 -0700

    Change status column for Merchant table

--- db/migrate/20210303202406_add_status_to_merchants.rb
+++ db/migrate/20210303202406_add_status_to_merchants.rb
@@ -1,5 +1,5 @@
 class AddStatusToMerchants < ActiveRecord::Migration[5.2]
   def change
-    add_column :merchants, :status, :boolean, :default => true
+    add_column :merchants, :status, :integer, default: 0
   end
 end
