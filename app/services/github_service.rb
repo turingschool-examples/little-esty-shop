ca8ea5d0bd632b81ec399034e6af96ba4ee8ec24
Brisa Garcia <brisagarciaglz@gmail.com>
2021-03-03 20:09:33 -0700

    Add service rb file

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,17 +1,14 @@
-require 'faraday'
-require 'json'
-require './app/poros/github'
-
 class GithubService
-  def github_information
-    endpoint = 'https://api.github.com/repos/jordanfbeck0528/little-esty-shop'
-    get_data(endpoint)
+  def self.connection
+    Faraday.new('https://api.github.com')
   end
 
-  def get_data(endpoint)
-    response = Faraday.get(endpoint)
-    parsed = JSON.parse(response.body, symbolize_names: true)
+  def self.end_point_data(data)
+    JSON.parse(data.body, symbolize_names: true)
+  end
+
+  def self.call_github(endpoint)
+    response = connection.get(endpoint)
+  end_point_data(response)
   end
 end
-
-info = GithubService.new.github_information
