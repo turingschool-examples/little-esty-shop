f4d69aa1571a4e460ec22badc182f4716956b2fc
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-13 15:23:05 -0600

    Add models for each class

---
+++ app/models/invoice_item.rb
@@ -0,0 +1,10 @@
+class InvoiceItem < ApplicationRecord
+  belongs_to :item
+  belongs_to :invoice
+
+  validates_presence_of :quantity,
+                        :unit_price,
+                        :status,
+                        :item_id,
+                        :invoice_id
+end 
