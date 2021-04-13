ac77d0e45f1c69dc51d5f8b8545b35dba090218f
joemecha <jwmecha@gmail.com>
2021-04-13 09:59:49 -0500

    Change data type for credit card numbers

--- db/migrate/20210413020416_create_transactions.rb
+++ db/migrate/20210413020416_create_transactions.rb
@@ -2,7 +2,7 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.integer :invoice_id
-      t.integer :credit_card_number
+      t.string :credit_card_number
       t.datetime :credit_card_expiration_date
       t.string :result
       t.datetime :created_at
