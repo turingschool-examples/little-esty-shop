23f01af7a18873a2bef5c9ec397a5d3a079e1914
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-13 15:27:38 -0600

    Update model specs with parens

--- spec/models/invoices_spec.rb
+++ spec/models/invoices_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
   describe 'relationships' do
-    it { should belong_to :customer }
-    it { should have_many :transactions }
-    it { should have_many :invoice_items }
+    it { should belong_to(:customer) }
+    it { should have_many(:transactions) }
+    it { should have_many(:invoice_items) }
     it { should have_many(:items).through(:invoice_items) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of :status }
-    it { should validate_presence_of :customer_id }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:customer_id) }
   end
 end
--- spec/models/merchants_spec.rb
+++ spec/models/merchants_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   describe 'relationships' do
-    it { should have_many :items }
+    it { should have_many(:items) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of :name }
+    it { should validate_presence_of(:name) }
   end
 end
