a7c790ba68d64eb51d076d086d0890f05374faae
Kseniya K <kseniya.kudelich@gmail.com>
2021-04-12 19:14:56 -0600

    Initial commit, setting initial schema

---
+++ db/migrate/20210413011025_create_transactions.rb
@@ -0,0 +1,12 @@
+class CreateTransactions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :transactions do |t|
+      t.references :invoice, foreign_key: true
+      t.integer :cc_number
+      t.string :cc_expiration_date
+      t.string :result
+
+      t.timestamps
+    end
+  end
+end
