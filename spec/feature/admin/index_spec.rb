b06e76e240a5cad7574d75aca81c91f041c2261c
Aidan McGuire <>
2021-04-13 17:08:31 -0600

    Add links to merchants and invoices

--- spec/feature/admin/index_spec.rb
+++ spec/feature/admin/index_spec.rb
@@ -4,9 +4,23 @@ RSpec.describe 'admin index page', type: :feature do
   it 'has a header indicating i am on the admin dashboard' do
 
     visit '/admin'
-
+    
     within("#admin_header") do
       expect(page).to have_content("Admin Dashboard")
+    end
+  end
+  
+  it 'has a link to the admin merchants index' do
+    visit '/admin'
+    within ("#links") do
+      expect(page).to have_link("Merchants")  
+    end
+  end
+  
+  it 'has a link to the admin invoices index' do
+    visit '/admin'
+    within ("#links") do
+      expect(page).to have_link("Invoices")  
     end
   end
 end
