e959334ea19bc86a43dc7a06113b4a6a9843745e
Aidan McGuire <>
2021-04-13 19:59:40 -0600

    Fix routes syntax erroe

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   get '/', to: 'merchants#welcome'
 
-  get "/merchant/:id/dashboard" to: 'merchants#show'
-  get "/merchant/:id/items" to: 'merchants#item_index'
-  get "/merchant/:merchant_id/items/:item_id" to: 'merchants#item_show'
-  get "/merchant/:id/invoices" to: 'merchants#invoice_index'
-  get "/merchant/:merchant_id/invoces/:invoice_id" to: 'merchants#invoice_show'
+  get "/merchant/:id/dashboard", to: 'merchants#show'
+  get "/merchant/:id/items", to: 'merchants#item_index'
+  get "/merchant/:merchant_id/items/:item_id", to: 'merchants#item_show'
+  get "/merchant/:id/invoices", to: 'merchants#invoice_index'
+  get "/merchant/:merchant_id/invoces/:invoice_id", to: 'merchants#invoice_show'
 
 ########## Admin routes below ############
 
