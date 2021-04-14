8ba0c4d98ead125eff24a67a24fd8e4bf4f2cec9
Kseniya K <kseniya.kudelich@gmail.com>
2021-04-14 16:19:30 -0600

    Add each Item's Show spec, controller, view

---
+++ spec/features/merchants/items/show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant items show page' do
+  it 'all of the items attributes' do
+    merchant1 = create(:merchant)
+    merchant2 = create(:merchant)
+    item1 = create(:item, merchant_id: merchant1.id)
+    item2 = create(:item, merchant_id: merchant1.id)
+    item2 = create(:item, merchant_id: merchant2.id)
+
+    visit "/merchants/#{merchant1.id}/items"
+
+    click_on "#{item1.name}"
+
+    expect(page).to have_current_path("/merchants/#{merchant1.id}/items/#{item1.id}")
+
+    expect(page).to have_content(item1.name)
+    expect(page).to have_content(item1.description)
+    expect(page).to have_content(item1.unit_price)
+    expect(page).not_to have_content(item2.name)
+    expect(page).not_to have_content(item2.description)
+  end
+end
