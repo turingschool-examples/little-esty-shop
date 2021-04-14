579ff7e7c54f7ac962641ddccace1a424ae90414
brueck1988 <brueck1988@gmail.com>
2021-04-13 18:36:12 -0600

    Created migrations

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,5 @@
 class InvoiceItem < ApplicationRecord
   enum status: [ 'packaged', 'pending', 'shipped' ]
+  belongs_to :item
+  belongs_to :invoice_items
 end
--- db/migrate/20210414000521_create_invoice_items.rb
+++ db/migrate/20210414000521_create_invoice_items.rb
@@ -1,11 +1,11 @@
 class CreateInvoiceItems < ActiveRecord::Migration[5.2]
   def change
     create_table :invoice_items do |t|
-      t.integer :item_id
-      t.integer :invoice_id
+      t.references :item, foreign_key: true
+      t.references :invoice, foreign_key: true
       t.integer :quantity
       t.integer :unit_price
-      t.integer :status
+      t.string :status
       t.datetime :created_at
       t.datetime :updated_at
     end
