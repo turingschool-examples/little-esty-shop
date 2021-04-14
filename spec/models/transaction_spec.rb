61c5186371a0ed4991d7de06fd8702f6b7871f99
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-13 18:07:46 -0600

    Change stringt o integers for enum requirements

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,7 +3,5 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   describe 'relationships' do
     it { should belong_to :invoice }
-
-    it { should have_many(:customers).through(:invoices) }
   end
 end
