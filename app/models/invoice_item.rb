07981a9f1214bab683bb9e41eef91757c4a2e5f0
Olivia Charles <ocharles92@gmail.com>
2021-04-14 13:30:35 -0400

    Added more relationships, add load:all to csv

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,10 +1,11 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  has_one :merchant, through: :item
 
   validates_presence_of :quantity,
                         :unit_price,
                         :status,
                         :item_id,
                         :invoice_id
-end 
+end
