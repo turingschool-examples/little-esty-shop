b3432b02bfed000a23055f9208576eaff0e9cae1
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-13 20:35:00 -0600

    Add migration

--- db/migrate/20210413011025_create_transactions.rb
+++ db/migrate/20210413011025_create_transactions.rb
@@ -2,9 +2,9 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.references :invoice, foreign_key: true
-      t.bigint :credit_card_number
-      t.date :credit_card_expiration_date
-      t.string :result
+      t.integer :cc_number
+      t.string :cc_expiration_date
+      t.integer :result
 
       t.timestamps
     end
