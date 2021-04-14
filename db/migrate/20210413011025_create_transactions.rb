9acc7ee27e829b9517f77ae5e675adadcf5da27d
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-13 19:50:59 -0600

    Change column names

--- db/migrate/20210413011025_create_transactions.rb
+++ db/migrate/20210413011025_create_transactions.rb
@@ -4,7 +4,7 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
       t.references :invoice, foreign_key: true
       t.integer :cc_number
       t.string :cc_expiration_date
-      t.string :result
+      t.integer :result
 
       t.timestamps
     end
