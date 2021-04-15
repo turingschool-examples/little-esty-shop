d05cd0fc4dafd8ba5ef57ead0548b414913ce9c3
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-14 18:05:06 -0600

    Create class method for top 5 customers for merchant

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,10 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
 
   validates_presence_of :first_name, :last_name
+
+  def self.top_five_customers
+    joins(:transactions).where({ transactions: { result: "success" } }).
+    group(:id).select("customers.*, count('transaction.result') as top_five").
+    order(top_five: :desc).limit(5)
+  end
 end
