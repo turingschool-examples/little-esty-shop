5eba08cb458531a21a442665ffc596a1837af9fb
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-04-14 21:05:10 -0500

    Remove comments from show_spec

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -4,21 +4,13 @@ RSpec.describe "Merchanct Invoices Index" do
   before(:each) do
     @merchant = Merchant.create!(name: 'Ice Cream Parlour')
     @item_1 = @merchant.items.create!(name: 'Ice Cream Scoop', description: 'scoops ice cream', unit_price: 13)
-    # @item_2 = @merchant.items.create!(name: 'Ice Cream Cone', description: 'holds ice cream', unit_price: 3)
-    # @item_3 = @merchant.items.create!(name: 'Sprinkles', description: 'makes your ice cream pretty', unit_price: 3)
     @customer = Customer.create!(first_name: 'Stuart', last_name: 'Little')
     @invoice_1 = Invoice.create!(status: 0, customer_id: "#{@customer.id}")
     InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 3, unit_price: 40, status: 0)
-    # InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_1.id, quantity: 10, unit_price: 20, status: 2)
-    # InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_1.id, quantity: 10, unit_price: 20, status: 2)
-    # @transaction_1 = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_1.id}")
-    # @transaction_2 = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_2.id}")
-    # @transaction_3 = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_3.id}")
     visit "/merchant/#{@merchant.id}/invoices/#{@invoice_1.id}"
   end
 
   it 'can see invoice id, status, created_at date, customer first and last name' do
-    save_and_open_page
     expect(page).to have_content(@invoice_1.id)
     expect(page).to have_content(@invoice_1.status)
     expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %d, %Y"))
@@ -26,13 +18,3 @@ RSpec.describe "Merchanct Invoices Index" do
     expect(page).to have_content(@customer.last_name)
   end
 end
-
-# Merchant Invoice Show Page
-#
-# As a merchant
-# When I visit my merchant's invoice show page(/merchants/merchant_id/invoices/invoice_id)
-# Then I see information related to that invoice including:
-# - Invoice id
-# - Invoice status
-# - Invoice created_at date in the format "Monday, July 18, 2019"
-# - Customer first and last name
