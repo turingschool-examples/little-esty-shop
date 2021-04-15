461fb6ee2912cc5f7d7838cf8b3d1f2c3d2429d9
kevn7n <kevn_nguyen@yahoo.com>
2021-04-15 00:38:06 -0600

    Create Merchant feature tests for index_spec & item_index_spec

---
+++ spec/feature/merchants/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'the merchant index' do
+  before(:each) do
+    @jerde = Merchant.create!(name: 'Schroeder-Jerde')
+    @willms = Merchant.create!(name: 'Willms and Sons')
+    @thiel = Merchant.create!(name: 'Cummings-Thiel')
+  end
+
+  it 'lists of the names of all of merchants' do
+    visit '/merchants'
+
+    expect(page).to have_content(@jerde.name)
+    expect(page).to have_content(@willms.name)
+    expect(page).to have_content(@thiel.name)
+  end     # not in a u.s.
+
+  it "has a link to each merchant" do
+    visit '/merchants'
+    expect(page).to have_link(@jerde.name)
+    expect(page).to have_link(@willms.name)
+    expect(page).to have_link(@thiel.name)
+  end     # not in a u.s.
+end
