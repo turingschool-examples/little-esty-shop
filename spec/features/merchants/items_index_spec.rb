ca89fd7248c64125964596fd9d49cd45f085c74b
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-15 10:57:44 -0600

    Add enabled and disabled section css id selectors to index view

--- spec/features/merchants/items_index_spec.rb
+++ spec/features/merchants/items_index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Merchant item index page" do
     @merchant_1 = Merchant.create!(name: 'Ice Cream Parlour')
     @merchant_2 = Merchant.create!(name: 'Ice Cream Parlour')
     @item_1 = @merchant_1.items.create!(name: 'Ice Cream Scoop', description: 'scoops ice cream', unit_price: 13)
-    @item_2 = @merchant_1.items.create!(name: 'Back scratch', description: 'skirtch back', unit_price: 5)
+    @item_2 = @merchant_1.items.create!(name: 'Back scratch', description: 'skirtch back', unit_price: 5, status: 1)
     @item_3 = @merchant_2.items.create!(name: 'Pooper Scooper', description: 'holds doge poo', unit_price: 13)
     visit "/merchant/#{@merchant_1.id}/items"
   end
@@ -20,19 +20,17 @@ RSpec.describe "Merchant item index page" do
   end
 
   it 'shows a button next to each item to enable or disable that item' do
-    within("#item-#{@item_1.id}") do
-      expect(page).to have_button("Enable")
+    within("#enabled-item-#{@item_2.id}") do
       expect(page).to have_button("Disable")
     end
 
-    within("#item-#{@item_2.id}") do
+    within("#disabled-item-#{@item_1.id}") do
       expect(page).to have_button("Enable")
-      expect(page).to have_button("Disable")
     end
   end
 
   it 'can click on enable and it updates item status to enable' do
-    within("#item-#{@item_1.id}") do
+    within("#disabled-item-#{@item_1.id}") do
       click_button "Enable"
     end
 
