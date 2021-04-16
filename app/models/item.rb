6a138bfed2590618a4c73e981a72cd76c34a166c
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-16 16:37:35 -0600

    Add model and feature tests for merchant items top five items by revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,20 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
 
   validates_presence_of :name, :description, :unit_price, :merchant_id
 
   enum status: [:disabled, :enabled]
+
+  def self.most_popular_items
+    select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue").
+    joins(:invoice_items, :transactions).
+    where(transactions: {result: "success"}).
+    order(revenue: :desc).
+    limit(5).
+    group(:id)
+  end
 
   def self.ordered_items_no_ship
     joins(:invoice_items).where('status != ?', 2)
