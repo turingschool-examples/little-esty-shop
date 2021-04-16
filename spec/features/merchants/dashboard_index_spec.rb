73067b362f9e6c9dc51f27f37186e9c88df435b5
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-15 18:47:48 -0600

    WIP fix text for test

--- spec/features/merchants/dashboard_index_spec.rb
+++ spec/features/merchants/dashboard_index_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant DashBoard Index' do
   before :each do
+    InvoiceItem.destroy_all
+    Transaction.destroy_all
+    Item.destroy_all
+    Invoice.destroy_all
+    Customer.destroy_all
+
     @merchant1 = create(:merchant)
 
     @item1 = create(:item, merchant_id: @merchant1.id)
@@ -11,9 +17,9 @@ RSpec.describe 'Merchant DashBoard Index' do
     @item5 = create(:item, merchant_id: @merchant1.id)
 
     @customer1 = create(:customer)
-    @invoice1 = create(:invoice, customer_id: @customer1.id)
-    @invoice2 = create(:invoice, customer_id: @customer1.id)
-    @invoice3 = create(:invoice, customer_id: @customer1.id)
+    @invoice1 = create(:invoice, customer_id: @customer1.id, created_at: "2019-03-20 09:54:09 UTC")
+    @invoice2 = create(:invoice, customer_id: @customer1.id, created_at: "2011-04-25 09:54:09 UTC")
+    @invoice3 = create(:invoice, customer_id: @customer1.id, created_at: "2018-08-01 09:54:09 UTC")
     @invoice4 = create(:invoice, customer_id: @customer1.id)
 
     @invoice_items1 = create(:invoice_item, item_id: @item1.id, invoice_id: @invoice1.id)
@@ -30,7 +36,7 @@ RSpec.describe 'Merchant DashBoard Index' do
     @invoice_items11 = create(:invoice_item, item_id: @item1.id, invoice_id: @invoice11.id)
     @invoice_items12 = create(:invoice_item, item_id: @item2.id, invoice_id: @invoice12.id)
     @invoice_items13 = create(:invoice_item, item_id: @item3.id, invoice_id: @invoice13.id)
-    # visit "/merchants/#{@merchant1.id}/dashboard"
+
     visit merchant_dashboard_index_path(@merchant1)
   end
 
@@ -49,26 +55,29 @@ RSpec.describe 'Merchant DashBoard Index' do
         end
       end
 
-    it 'Shows a link to my invoice index' do
+      it 'Shows a link to my invoice index' do
         within(".navbar") do
           expect(page).to have_link('My Invoices')
           click_link('My Invoices')
           expect(current_path).to eq(merchant_invoices_path(@merchant1))
         end
       end
-    end
 
-    it 'I see a section for "Items Ready to Ship"' do
-      within '.items-to-ship' do
-        expect(page).to have_content("Items Ready to Ship")
+      it 'I see a section for "Items Ready to Ship"' do
+        within '.items-to-ship' do
+          expect(page).to have_content("Items Ready to Ship")
+        end
+      end
+
+      it 'Shows a list of the link names that have not been shipped, and date, that route to merchant show page' do
+          expect(page).to have_link(href: merchant_invoice_path(@merchant1, @invoice_items1.invoice_id))
+          expect(page).to have_content(@invoice_items1.invoice.format_time)
+      end
+
+
+      it "shows the list is ordered from oldest to newest" do
+        save_and_open_page
       end
     end
-
-    # it 'And i see a list of the link names of all of my items ordered, that have not been shipped, that route to merchant show page' do
-    #   within '.items' do
-    #     expect(page).to have_link("Invoice ##{@invoice1.id}")
-    #     expect(page).to_not have_link("Invoice ##{@invoice2.id}")
-    #   end
-    # end
   end
 end
