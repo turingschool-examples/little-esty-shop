3cdce7ec0ea34b6a0601ca18508afa600db7980c
Olivia Charles <ocharles92@gmail.com>
2021-04-15 22:04:01 -0400

    Admin Merchant Update working

--- app/controllers/admin/merchant_controller.rb
+++ app/controllers/admin/merchant_controller.rb
@@ -7,4 +7,24 @@ class Admin::MerchantController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+      if @merchant.update!(merchant_params)
+          flash[:message] = "Information has been successfully updated. #{error_message(@merchant.errors)}"
+        redirect_to "/admin/merchant/#{@merchant.id}"
+      else
+        flash[:error] = "Please fill in all fields. #{error_message(@merchant.errors)}."
+        redirect_to "/admin/merchant/#{@merchant.id}/edit"
+      end
+    end
+
+  private
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
