ee882cdd97b764e376e40ee90b8f5da1dc5b72c5
ignored-comment <info.konham@gmail.com>
2021-04-16 09:50:23 -0400

    Complete #6

---
+++ spec/features/admin/invoices/show_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe 'Admin Invoice Index' do
+  before :each do
+    @merchant1 = create(:merchant)
+    @item1 = create(:item, merchant_id: @merchant1.id)
+    @customer1 = create(:customer)
+    @invoice1 = create(:invoice, customer_id: @customer1.id)
+    @invoice_items1 = create(:invoice_item, item_id: @item1.id, invoice_id: @invoice1.id, quantity: 4, unit_price: 1200)
+    @item2 = create(:item, merchant_id: @merchant1.id)
+    @invoice2 = create(:invoice, customer_id: @customer1.id)
+    @invoice_items2 = create(:invoice_item, item_id: @item2.id,invoice_id: @invoice1.id, quantity: 2, unit_price: 73000)
+  end
+
+  describe "when I visit my merchant's invoices show page" do
+    it 'shows me the total revenue for each invoice show page' do
+      visit "/admin/invoices/#{@invoice1.id}"
+      expect(page).to have_content("150800")
+    end
+  end
+end
