e981ec72c2043b68bb87625a54df5e3c7c9cb3de
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-17 12:55:22 -0600

    Add model, view, and control actions to change status of merchants

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -13,16 +13,28 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def update
-    # binding.pry
     @merchant = Merchant.find(params[:id])
-    @merchant.update(merchant_params)
-    if @merchant.save
+    if params[:status]
+      enable_disable_logic
+    elsif @merchant.update(merchant_params) && !params[:status]
+      flash[:notice] = "Merchant Updated Successfully"
       redirect_to admin_merchant_path(@merchant)
-      flash[:success] = 'Merchant successfully updated!'
     end
   end
 
   private
+
+  def enable_disable_logic
+    if params[:status] == "enable"
+      @merchant.enable_merchant
+      flash[:notice] = "#{@merchant.name} Enabled"
+      redirect_to admin_merchants_path
+    else
+      @merchant.disable_merchant
+      flash[:notice] = "#{@merchant.name} Disabled"
+      redirect_to admin_merchants_path
+    end
+  end
 
    def merchant_params
      params.require(:merchant).permit(:name)
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,6 +5,24 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
 
+  # enum status: [:disabled, :enabled]
+  def self.enable
+    where(status: true)
+  end
+
+  def self.disable
+    where(status: false)
+  end
+
+  def enable_merchant
+    update(status: true)
+  end
+
+  def disable_merchant
+    update(status: false)
+  end
+
+
   def invoice_items_ready_to_ship
     invoice_items.where.not(status: :shipped)
    .joins(:invoice)
