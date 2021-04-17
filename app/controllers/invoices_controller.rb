f52d61c926eac7d880a3a4787443a78bde79e29a
joemecha <jwmecha@gmail.com>
2021-04-16 22:55:35 -0500

    Write spec controller action model methods and view Story 27

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,11 @@
 class InvoicesController < ApplicationController
   def index
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoices = @merchant.invoices
+  end
 
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoice = Invoice.find(params[:id])
   end
 end
