095014b46ea2920824d2b74937947be88fc422d8
Aidan McGuire <>
2021-04-16 19:11:55 -0600

    Add test for merchant status presence in admin index

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,6 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
+  validates :status, presence: true
 
   has_many :items, dependent: :destroy
 
--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'admin merchants index page', type: :feature do
     merchant3 = Merchant.create!(name: "Cat")
     
     visit '/admin/merchants'
-
+    
     within "#merchant-#{merchant1.id}" do
       expect(page).to have_link(merchant1.name)
     end
@@ -16,6 +16,24 @@ RSpec.describe 'admin merchants index page', type: :feature do
     end
     within "#merchant-#{merchant3.id}" do
       expect(page).to have_link(merchant3.name)
+    end
+  end
+  
+  it 'displays each merchants status' do
+    merchant1 = Merchant.create!(name: "Abe")
+    merchant2 = Merchant.create!(name: "Bel")
+    merchant3 = Merchant.create!(name: "Cat")
+    
+    visit '/admin/merchants'
+    
+    within "#merchant-#{merchant1.id}" do
+      expect(page).to have_content(merchant1.status)
+    end
+    within "#merchant-#{merchant2.id}" do
+      expect(page).to have_content(merchant2.status)
+    end
+    within "#merchant-#{merchant3.id}" do
+      expect(page).to have_content(merchant3.status)
     end
   end
 end
