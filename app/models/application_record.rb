63791881248459b65ac8d130e0f06386b6e8a24f
joemecha <jwmecha@gmail.com>
2021-04-16 20:57:12 -0500

    Add method to format unit price

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def formatted_unit_price
+    price = (unit_price.to_f / 100).round(2)
+    sprintf("%.2f", price)
+  end
 end
