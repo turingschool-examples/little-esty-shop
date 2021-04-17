ea34c530909c62561b369752893a83b47c736207
joemecha <jwmecha@gmail.com>
2021-04-16 20:58:33 -0500

    Add placeholder for top five method in spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,9 +8,18 @@ RSpec.describe Merchant, type: :model do
 
   describe 'relationships' do
     it { should have_many(:items) }
+    it { should have_many(:invoice_items).through :items }
+    it { should have_many(:invoices).through :invoice_items }
+    it { should have_many(:customers).through :invoices }
+    it { should have_many(:transactions).through :invoices }
   end
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
+  end
+
+  describe 'class methods' do
+    it '#top_five_by_successful_transaction_count' do
+    end
   end
 end
