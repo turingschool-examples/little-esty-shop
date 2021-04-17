09fba445ca5033df8eae0a5bad88a9ff4dc8b243
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-04-16 20:08:52 -0500

    Add invoice item name, quantity, unit price, and item status to show page

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -5,5 +5,6 @@ class Admin::InvoicesController < ApplicationController
 
   def show
     @invoice = Invoice.find(params[:id])
+    @invoice_items = @invoice.invoice_items
   end
 end
--- spec/features/admin/invoices_show_spec.rb
+++ spec/features/admin/invoices_show_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe "Merchanct Invoices Index" do
     @item_2 = create(:item, merchant: @merchant)
     @item_3 = create(:item, merchant: @merchant)
     @customer = create(:customer)
-    @invoice = Invoice.create!(status: 1, customer_id: @customer.id)
+    @invoice = Invoice.create!(status: 0, customer_id: @customer.id)
+    @invoice_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice.id, quantity: 3, unit_price: 13, status: 0)
     visit "/admin/invoices/#{@invoice.id}"
   end
 
@@ -17,5 +18,12 @@ RSpec.describe "Merchanct Invoices Index" do
     expect(page).to have_content(@invoice.created_at.strftime("%A, %B %d, %Y"))
     expect(page).to have_content(@customer.first_name)
     expect(page).to have_content(@customer.last_name)
+  end
+
+  it 'can see all items and their names, quantity ordered, price items sold for, and invoice item status' do
+    expect(page).to have_content(@item_1.name)
+    expect(page).to have_content(@invoice_item_1.quantity)
+    expect(page).to have_content(@invoice_item_1.unit_price)
+    expect(page).to have_content(@invoice_item_1.status)
   end
 end
