e74ad1bf05374b051145f7cda920dc6453450691
Aidan McGuire <>
2021-04-16 19:22:42 -0600

    Add admin merchants index enable/disable merchant buttons

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -36,4 +36,24 @@ RSpec.describe 'admin merchants index page', type: :feature do
       expect(page).to have_content(merchant3.status)
     end
   end
+
+  it 'has a button to disable the merchant if it is enabled, vise versa' do
+    merchant1 = Merchant.create!(name: "Abe")
+    merchant2 = Merchant.create!(name: "Bel")
+    merchant3 = Merchant.create!(name: "Cat", status: 1)
+    
+    visit '/admin/merchants'
+    
+    within "#merchant-#{merchant1.id}" do
+      expect(page).to have_button("Disable")
+    end
+
+    within "#merchant-#{merchant2.id}" do
+      expect(page).to have_button("Disable")
+    end
+    
+    within "#merchant-#{merchant3.id}" do
+      expect(page).to have_button("Enable")
+    end
+  end
 end
