312fa60696886dc67403f910845bd8b2dad08725
koshea9 <keegan.oshea9@gmail.com>
2021-04-16 18:58:56 -0600

    Test WIP

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe 'when I visit the admin merchant index page' do
     @transaction_15 = FactoryBot.create(:transaction, result: 1)
     @invoice_5.transactions << [@transaction_15]
 
-    @merchant_1 = create(:merchant)
+    @merchant_1 = create(:merchant, status: 0)
     @merchant_2 = create(:merchant)
     @merchant_3 = create(:merchant)
     @merchant_4 = create(:merchant)
@@ -71,15 +71,26 @@ RSpec.describe 'when I visit the admin merchant index page' do
     expect(current_path).to eq ("/admin/merchants/#{@merchant_1.id}")
   end
 
-  xdescribe "Admin Merchant Enable/Disable" do
+  describe "Admin Merchant Enable/Disable" do
     it 'has a button next to each merchant to disable it' do
       visit '/admin/merchants'
 
-      button_to "disbale"
+      within"#enabled-merchants" do
+        expect(page).to have_content(@merchant_1.name)
+        expect(page).to have_content(@merchant_3.name)
+        expect(page).to have_content(@merchant_5.name)
+        button_to "Disable"
+        expect(current_path).to eq ('/admin/merchants')
     end
 
-    xit 'has a button next to each merchant to enable it' do
+    it 'has a button next to each merchant to enable it' do
       visit '/admin/merchants'
+      within"#disabled-merchants"
+        expect(page).to have_content(@merchant_1.name)
+        expect(page).to have_content(@merchant_3.name)
+        expect(page).to have_content(@merchant_5.name)
+        button_to "Enable"
+        expect(current_path).to eq ('/admin/merchants')
     end
 #
 # As an admin,
