5799a43350ddcab47ff99e3fc43e3930ffcfedbd
joemecha <jwmecha@gmail.com>
2021-04-16 19:15:55 -0500

    Fix looping routing error with path arguments

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,11 @@
 class ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @items = @merchant.items 
+    @items = @merchant.items
   end
 
   def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
   end
 end
