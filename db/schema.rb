45c21bef7a0d92ee4f7c8acd66dd03799846a2f7
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-17 12:52:00 -0600

    Add status

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2021_04_17_152950) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "enabled", default: true
+    t.boolean "status", default: true
   end
 
   create_table "transactions", force: :cascade do |t|
