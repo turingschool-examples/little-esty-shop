8711569bbde8462b1996caabd2d54ea89833421f
Aidan McGuire <>
2021-04-16 18:17:22 -0600

    Add feature admin merchant show update merchant

--- spec/features/admin/merchants/edit_spec.rb
+++ spec/features/admin/merchants/edit_spec.rb
@@ -5,8 +5,18 @@ RSpec.describe 'admin merchant edit page', type: :feature do
     merchant1 = Merchant.create!(name: "Abe")
 
     visit "/admin/merchants/#{merchant1.id}/edit"
-
+    
     expect(find('form')).to have_content('Name')
-    expect(find('form')).to have_content('Update Merchant')
+    expect(find('form')).to have_button('Update Merchant')
+  end
+  
+  it 'updates and redirects to admin merchant show page' do
+    merchant1 = Merchant.create!(name: "Abe")
+  
+    visit "/admin/merchants/#{merchant1.id}/edit"
+
+    fill_in "Name", with: "Abraham"
+    click_button "Update Merchant"
+    expect(current_path).to eq("/admin/merchants/#{merchant1.id}")
   end
 end
