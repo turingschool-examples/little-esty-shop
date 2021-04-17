da1dc63dc86ef68ec73adae9cc9f76a4fa8883ca
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-16 20:05:55 -0600

    Add the actions to admin merchants

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -7,4 +7,25 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    # binding.pry
+    @merchant = Merchant.find(params[:id])
+    @merchant.update(merchant_params)
+    if @merchant.save
+      redirect_to admin_merchant_path(@merchant)
+      flash[:success] = 'Merchant successfully updated!'
+    end
+  end
+
+  private
+
+   def merchant_params
+     params.require(:merchant).permit(:name)
+   end
+
 end
