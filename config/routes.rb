04db983f06dcfe5ec481fe749598857dda82cf47
Andrew Smith <andrewpatrick138@gmail.com>
2021-04-17 16:09:44 -0700

    add view

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,6 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/', to: "dashboard#index"
     resources :merchants, only: [:index, :new, :create, :show, :update]
-    resources :invoices, only: [:index]
+    resources :invoices, only: [:index, :show]
   end
 end
