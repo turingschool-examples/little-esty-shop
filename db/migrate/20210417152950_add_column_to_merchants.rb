e3ecd2576911e7eb8ec9516362c5a896a1bca32f
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-18 11:50:47 -0600

    Change schema to false boolean

--- db/migrate/20210417152950_add_column_to_merchants.rb
+++ db/migrate/20210417152950_add_column_to_merchants.rb
@@ -1,5 +1,5 @@
 class AddColumnToMerchants < ActiveRecord::Migration[5.2]
   def change
-    add_column :merchants, :status, :boolean, default: true
+    add_column :merchants, :status, :boolean, default: false
   end
 end
