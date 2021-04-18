99fe37f801561097425feffe3b1cc60f39f3e531
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-18 15:22:41 -0600

    Add stories for Top Merchs (model, features) and update factory

--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-   factory :transcation, class: Transaction do
-      cc_number { Faker::Business.credit_card_number }
-      cc_expiration_date { Faker::Business.credit_card_expiry_date }
+   factory :transaction, class: Transaction do
+      credit_card_number { Faker::Business.credit_card_number }
+      credit_card_expiration_date { Faker::Business.credit_card_expiry_date }
       result { ["failed", "success"].sample }
 
       invoice
