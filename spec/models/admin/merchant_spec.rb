3c560c4bf98a43881594ebcbf6b1f65e2b2d61a8
Andrew Smith <andrewpatrick138@gmail.com>
2021-04-18 11:13:11 -0700

    passing tests

--- spec/models/admin/merchant_spec.rb
+++ spec/models/admin/merchant_spec.rb
@@ -46,25 +46,12 @@ RSpec.describe Merchant, type: :model do
   @transaction_3 = FactoryBot.create(:transaction, result: 1)
   @transaction_4 = FactoryBot.create(:transaction, result: 1)
   @transaction_5 = FactoryBot.create(:transaction, result: 1)
-  @invoice_1.transactions << [@transaction_1, @transaction_2, @transaction_3, @transaction_4, @transaction_5]
 
-  @transaction_6 = FactoryBot.create(:transaction, result: 1)
-  @transaction_7 = FactoryBot.create(:transaction, result: 1)
-  @transaction_8 = FactoryBot.create(:transaction, result: 1)
-  @transaction_9 = FactoryBot.create(:transaction, result: 1)
-  @invoice_2.transactions << [@transaction_6, @transaction_7, @transaction_8, @transaction_9]
-
-  @transaction_10 = FactoryBot.create(:transaction, result: 1)
-  @transaction_11 = FactoryBot.create(:transaction, result: 1)
-  @transaction_12 = FactoryBot.create(:transaction, result: 1)
-  @invoice_3.transactions << [@transaction_10, @transaction_11, @transaction_12]
-
-  @transaction_13 = FactoryBot.create(:transaction, result: 1)
-  @transaction_14 = FactoryBot.create(:transaction, result: 1)
-  @invoice_4.transactions << [@transaction_13, @transaction_14]
-
-  @transaction_15 = FactoryBot.create(:transaction, result: 1)
-  @invoice_5.transactions << [@transaction_15]
+  @invoice_1.transactions << @transaction_1
+  @invoice_2.transactions << @transaction_2 
+  @invoice_3.transactions << @transaction_3
+  @invoice_4.transactions << @transaction_4
+  @invoice_5.transactions << @transaction_5
   end
 
   describe 'relationships' do
@@ -85,7 +72,7 @@ RSpec.describe Merchant, type: :model do
 
     describe '::top_five_by_merchant_revenue' do
       it 'returns top five merchants by total revenue' do
-        require 'pry';binding.pry
+        # require 'pry';binding.pry
         expect(Merchant.top_five_by_merchant_revenue).to eq([@merchant_5, @merchant_4, @merchant_2, @merchant_3, @merchant_1])
       end
     end
