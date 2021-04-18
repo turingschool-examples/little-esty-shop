29720e72a78b50c375758efec27bf4b1dc6287ab
Aidan McGuire <>
2021-04-17 21:54:35 -0600

    Fix test file

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'admin index page', type: :feature do
   it 'has a header indicating i am on the admin dashboard' do
 
     visit '/admin'
-
+    
     within("#admin_header") do
       expect(page).to have_content("Admin Dashboard")
     end
   end
-
+  
   it 'has a link to the admin merchants index' do
     visit '/admin'
     within ("#links") do
@@ -18,11 +18,11 @@ RSpec.describe 'admin index page', type: :feature do
       expect(current_path).to eq("/admin/merchants")
     end
   end
-
+  
   it 'has a link to the admin invoices index' do
     visit '/admin'
     within ("#links") do
-      expect(page).to have_link("Invoices")
+      expect(page).to have_link("Invoices")  
       click_link("Invoices")
       expect(current_path).to eq("/admin/invoices")
     end
@@ -30,19 +30,185 @@ RSpec.describe 'admin index page', type: :feature do
 
   describe 'it has an incomplete invoices section' do
     it 'lists all incomplete invoices' do
+      merchant = Merchant.create!(name: "mel")
       customer = Customer.create!(first_name: "Abe", last_name: "Oldman")
+
+      item1 = merchant.items.create!(name: "thing", description: "thingy", unit_price: 10)
+      invoice2 = customer.invoices.create!(status: 0)
       invoice1 = customer.invoices.create!(status: 0)
-      invoice2 = customer.invoices.create!(status: 1)
-      invoice3 = customer.invoices.create!(status: 2)
-      invoice4 = customer.invoices.create!(status: 0)
+      invoice3 = customer.invoices.create!(status: 0)
+
+      invoice_item1 = InvoiceItem.create!(item: item1, invoice: invoice1, quantity: 2, unit_price: 5, status: 2)
+      invoice_item2 = InvoiceItem.create!(item: item1, invoice: invoice1, quantity: 2, unit_price: 5, status: 0)
+      invoice_item3 = InvoiceItem.create!(item: item1, invoice: invoice2, quantity: 2, unit_price: 5, status: 1)
+      invoice_item4 = InvoiceItem.create!(item: item1, invoice: invoice2, quantity: 2, unit_price: 5, status: 2)
+      invoice_item4 = InvoiceItem.create!(item: item1, invoice: invoice3, quantity: 2, unit_price: 5, status: 2)
+      invoice_item4 = InvoiceItem.create!(item: item1, invoice: invoice3, quantity: 2, unit_price: 5, status: 2)
 
       visit '/admin'
 
       within("#incomplete_invoices") do
         expect(page).to have_content("Incomplete Invoices")
         expect(page).to have_content(invoice1.id)
-        expect(page).to have_content(invoice4.id)
+        expect(page).to have_content(invoice2.id)
       end
+    end
+
+    it 'orders the invoices from oldest to newest
+        and displays the date the invoice was created "Monday, July 18,2019"' do
+      merchant = Merchant.create!(name: "mel")
+      customer = Customer.create!(first_name: "Abe", last_name: "Oldman")
+
+      item1 = merchant.items.create!(name: "thing", description: "thingy", unit_price: 10)
+      invoice2 = customer.invoices.create!(status: 0)
+      invoice1 = customer.invoices.create!(status: 0)
+      invoice3 = customer.invoices.create!(status: 0)
+
+      invoice_item1 = InvoiceItem.create!(item: item1, invoice: invoice1, quantity: 2, unit_price: 5, status: 2)
+      invoice_item2 = InvoiceItem.create!(item: item1, invoice: invoice1, quantity: 2, unit_price: 5, status: 0)
+      invoice_item3 = InvoiceItem.create!(item: item1, invoice: invoice2, quantity: 2, unit_price: 5, status: 1)
+      invoice_item4 = InvoiceItem.create!(item: item1, invoice: invoice2, quantity: 2, unit_price: 5, status: 2)
+      invoice_item4 = InvoiceItem.create!(item: item1, invoice: invoice3, quantity: 2, unit_price: 5, status: 2)
+      invoice_item4 = InvoiceItem.create!(item: item1, invoice: invoice3, quantity: 2, unit_price: 5, status: 2)
+
+      visit '/admin'
+
+      within("#incomplete_invoices") do
+        expect(page).to have_content("Incomplete Invoices")
+
+        expect("#{invoice1.id}").to appear_before("#{invoice2.id}")
+        
+        within("#incomplete_invoice-#{invoice1.id}") do
+          expect(page).to have_content(invoice1.id)
+          expect(page).to have_content("#{invoice1.created_at.strftime("%A, %B %d, %Y")}") #"Monday, July 18, 2019"
+        end
+    
+        within("#incomplete_invoice-#{invoice2.id}") do
+          expect(page).to have_content(invoice2.id)
+          expect(page).to have_content("#{invoice2.created_at.strftime("%A, %B %d, %Y")}")
+        end
+      end
+    end
+
+    it 'each incomplete invoice is a link to that invoices show page' do
+      merchant = Merchant.create!(name: "mel")
+      customer = Customer.create!(first_name: "Abe", last_name: "Oldman")
+
+      item1 = merchant.items.create!(name: "thing", description: "thingy", unit_price: 10)
+      invoice2 = customer.invoices.create!(status: 0)
+      invoice1 = customer.invoices.create!(status: 0)
+      invoice3 = customer.invoices.create!(status: 0)
+
+      invoice_item1 = InvoiceItem.create!(item: item1, invoice: invoice1, quantity: 2, unit_price: 5, status: 2)
+      invoice_item2 = InvoiceItem.create!(item: item1, invoice: invoice1, quantity: 2, unit_price: 5, status: 0)
+      invoice_item3 = InvoiceItem.create!(item: item1, invoice: invoice2, quantity: 2, unit_price: 5, status: 1)
+      invoice_item4 = InvoiceItem.create!(item: item1, invoice: invoice2, quantity: 2, unit_price: 5, status: 2)
+      invoice_item4 = InvoiceItem.create!(item: item1, invoice: invoice3, quantity: 2, unit_price: 5, status: 2)
+      invoice_item4 = InvoiceItem.create!(item: item1, invoice: invoice3, quantity: 2, unit_price: 5, status: 2)
+
+      visit '/admin'
+
+      within("#incomplete_invoice-#{invoice1.id}") do
+        expect(page).to have_link("#{invoice1.id}")
+        click_link("#{invoice1.id}")
+        expect(current_path).to eq("/admin/invoices/#{invoice1.id}")
+      end
+
+      visit '/admin'
+      within("#incomplete_invoice-#{invoice2.id}") do
+        expect(page).to have_link("#{invoice2.id}")
+        click_link("#{invoice2.id}")
+        expect(current_path).to eq("/admin/invoices/#{invoice2.id}")
+      end
+    end
+  end
+
+  it 'displays the names of the top 5 customers who
+    have conducted the most successful transactions' do
+    
+    customer1 = Customer.create!(first_name: "Ayy", last_name: "All")
+    invoice1 = customer1.invoices.create!(status: 0)
+    transaction1 = invoice1.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 0)
+    
+    customer2 = Customer.create!(first_name: "Bee", last_name: "Bold")
+    invoice2 = customer2.invoices.create!(status: 1)
+    invoice7 = customer2.invoices.create!(status: 1)
+    transaction2 = invoice2.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction3 = invoice2.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction4 = invoice2.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction5 = invoice2.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction14 = invoice7.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    
+    customer3 = Customer.create!(first_name: "Cee", last_name: "Cold")
+    invoice3 = customer3.invoices.create!(status: 2)
+    transaction6 = invoice3.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction7 = invoice3.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction8 = invoice3.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    
+    customer4 = Customer.create!(first_name: "Dee", last_name: "Droll")
+    invoice4 = customer4.invoices.create!(status: 0)
+    transaction9 = invoice4.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction10 = invoice4.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    
+    customer5 = Customer.create!(first_name: "Eyy", last_name: "Erk")
+    invoice5 = customer5.invoices.create!(status: 1)
+    transaction11 = invoice5.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    
+    customer6 = Customer.create!(first_name: "Gee", last_name: "Golly")
+    invoice6 = customer6.invoices.create!(status: 2)
+    transaction12 = invoice6.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 0)
+    transaction13 = invoice6.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+
+    visit '/admin'
+    
+    expect(customer2.first_name).to appear_before(customer3.first_name)
+    expect(customer3.first_name).to appear_before(customer4.first_name)
+    expect(customer4.first_name).to appear_before(customer5.first_name)
+    expect(page).to_not have_content(customer1.first_name)
+  end
+
+  it 'displays the number of successful transactions next to each customer' do
+    customer1 = Customer.create!(first_name: "Ayy", last_name: "All")
+    invoice1 = customer1.invoices.create!(status: 0)
+    transaction1 = invoice1.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 0)
+    
+    customer2 = Customer.create!(first_name: "Bee", last_name: "Bold")
+    invoice2 = customer2.invoices.create!(status: 1)
+    invoice7 = customer2.invoices.create!(status: 1)
+    transaction2 = invoice2.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction3 = invoice2.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction4 = invoice2.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction5 = invoice2.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction14 = invoice2.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    
+    customer3 = Customer.create!(first_name: "Cee", last_name: "Cold")
+    invoice3 = customer3.invoices.create!(status: 2)
+    transaction6 = invoice3.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction7 = invoice3.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction8 = invoice3.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    
+    customer4 = Customer.create!(first_name: "Dee", last_name: "Droll")
+    invoice4 = customer4.invoices.create!(status: 0)
+    transaction9 = invoice4.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    transaction10 = invoice4.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    
+    customer5 = Customer.create!(first_name: "Eyy", last_name: "Erk")
+    invoice5 = customer5.invoices.create!(status: 1)
+    transaction11 = invoice5.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+    
+    customer6 = Customer.create!(first_name: "Gee", last_name: "Golly")
+    invoice6 = customer6.invoices.create!(status: 2)
+    transaction12 = invoice6.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 0)
+    transaction13 = invoice6.transactions.create!(credit_card_number: 1111222233334444, credit_card_expiration_date: '', result: 1)
+
+    visit '/admin'
+    
+    within("#customer-#{customer2.id}") do
+      expect(page).to have_content(5)
+    end
+
+    within("#customer-#{customer3.id}") do
+      expect(page).to have_content(3)
     end
   end
 end
