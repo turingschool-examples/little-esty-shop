5ec038dbe489ffb98de1c3f68c6ddd76f0f8777b
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-18 15:21:56 -0600

    Fix update buttons and add Top Merchs

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,12 +1,13 @@
 class Admin::MerchantsController < ApplicationController
-
   def index
     @merchants = Merchant.all
   end
 
-
-
   def show
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def edit
     @merchant = Merchant.find(params[:id])
   end
 
