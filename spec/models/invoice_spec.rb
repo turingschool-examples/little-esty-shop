48df72e92cc7df127a9f512ed7620c248fddf7d1
Kseniya K <kseniya.kudelich@gmail.com>
2021-04-18 23:03:06 -0600

    Test correct date format and desc order

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,9 +12,13 @@ RSpec.describe Invoice, type: :model do
 
   describe 'methods' do
     it "can return incomplete invoices" do
-      invoice1 = create(:invoice)
-      invoice2 = create(:invoice)
-      invoice3 = create(:invoice)
+      time1 = Time.new(2008,6,21, 13,30,0, "+09:00").utc
+      time2 = Time.new(2006,6,21, 13,30,0, "+09:00").utc
+      time3 = Time.new(2015,6,21, 13,30,0, "+09:00").utc
+
+      invoice1 = create(:invoice, created_at: time1)
+      invoice2 = create(:invoice, created_at: time2)
+      invoice3 = create(:invoice, created_at: time3)
 
       item1 = create(:item)
       item2 = create(:item)
@@ -23,7 +27,15 @@ RSpec.describe Invoice, type: :model do
       invoice_item2 = create(:invoice_item, invoice: invoice2, item: item1, status: 1)
       invoice_item3 = create(:invoice_item, invoice: invoice3, item: item2, status: 2)
 
-      expect(Invoice.incomplete_invoices).to eq([invoice1, invoice2])
+      expect(Invoice.incomplete_invoices).to eq([invoice2, invoice1])
+    end
+
+    it "can format date correctly" do
+      time = Time.new(2008,6,21, 13,30,0, "+09:00").utc
+
+      invoice1 = create(:invoice, created_at: time)
+
+      expect(invoice1.format_time).to eq("Saturday, June 21, 2008")
     end
   end
 end
