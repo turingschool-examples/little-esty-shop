b6b2c5d4fbd208ae507d839c76fd63b43ebd1407
Kseniya K <kseniya.kudelich@gmail.com>
2021-04-18 23:05:39 -0600

    Add top 5 items view and model method

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
 
   validates :name, presence: true
 
@@ -11,5 +12,14 @@ class Item < ApplicationRecord
 
   def self.disable
     where(enabled: false)
+  end
+
+  def self.top_5_items
+    select("items.*, sum(invoice_items.unit_price * invoice_items.quantity) AS total_revenue")
+    .joins(:transactions)
+    .where(transactions: {result: :success})
+    .group(:id)
+    .order(total_revenue: :desc)
+    .limit(5)
   end
 end
