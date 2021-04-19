80fd51c6f4b515aa519b32f4c257b871cccc8772
joemecha <jwmecha@gmail.com>
2021-04-19 08:54:16 -0500

    Create show action

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -7,5 +7,6 @@ class InvoicesController < ApplicationController
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @invoice = Invoice.find(params[:id])
+    @invoice_items = @invoice.invoice_items
   end
 end
