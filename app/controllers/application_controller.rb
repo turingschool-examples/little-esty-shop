d743890969ca84c6bc3c8555f20764e63977a887
Olivia Charles <ocharles92@gmail.com>
2021-04-19 22:10:10 -0400

    Api information is working we think

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  def index
-    token = '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'
-    service = GitHubService.new(token)
-    name = service.get_name[:name]
-  end
+  # def index
+  #   token = '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'
+  #   service = GitHubService.new(token)
+  #   name = service.get_name[:name]
+  # end
 
   private
 
