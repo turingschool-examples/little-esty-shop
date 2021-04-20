a99453f61342f98b818764f90fd322937beced99
kevn7n <kevn_nguyen@yahoo.com>
2021-04-20 01:04:51 -0600

    Move item nested to items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,24 +1,50 @@
 class ItemsController < ApplicationController
 
+  def index
+    @enabled_items = Item.all.enabled
+    @disabled_items = Item.all.disabled
+  end
+
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @item = @merchant.items.find(params[:item_id])
-    redirect
-  end
-
-  # def item_index
-  #   @merchant = Merchant.find(params[:id])
-  #   @items = @merchant.items.all
-  # end
-
-  def new
   end
 
   def create
+    @merchant = Merchant.find(params[:id])
+    # item = @merchant.items.new()
+    item = Item.new({name: params[:name], description: params[:description], unit_price: params[:unit_price], able: params[:able], merchant_id: @merchant.id})
+    item.save
+    redirect_to "/merchants/#{@merchant.id}/items"
   end
 
+  def new
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items.find(params[:item_id])
+  end
 
   def update
+    @merchant = Merchant.find(params[:merchant_id])
+    # item = @merchant.items.find(params[:item_id])
+    item = Item.find(params[:item_id])
+    binding.pry
+    if params[:name]
+      item.update({
+        name: params[:name],
+        description: params[:description],
+        unit_price: params[:unit_price],
+        able: params[:able]
+        })
+    else
+      item.update({able: params[:able]})
+    end
+    item.save
+    flash[:alert] = "Item was successfully updated."
+    redirect_to "/merchants/#{@merchant.id}/items"
   end
 
 end
--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -23,60 +23,54 @@ class MerchantsController < ApplicationController
     @merchants = Merchant.all
   end
 
-  def item_index
-    @merchant = Merchant.find(params[:id])
-    # @items = @merchant.items.all
-    @enabled_items = (@merchant.items.all).enabled
-    @disabled_items = (@merchant.items.all).disabled
-  end
-
-  def item_create
-    @merchant = Merchant.find(params[:id])
-    # item = @merchant.items.new()
-    item = Item.new({name: params[:name], description: params[:description], unit_price: params[:unit_price], able: params[:able], merchant_id: @merchant.id})
-    item.save
-    redirect_to "/merchants/#{@merchant.id}/items"
-  end
-
-  def item_new
-    @merchant = Merchant.find(params[:id])
-  end
-
-  def item_edit
-    @merchant = Merchant.find(params[:merchant_id])
-    @item = @merchant.items.find(params[:item_id])
-  end
-
-  def item_update
-    @merchant = Merchant.find(params[:merchant_id])
-    # item = @merchant.items.find(params[:item_id])
-    item = Item.find(params[:item_id])
-    binding.pry
-    if params[:name]
-      item.update({
-        name: params[:name],
-        description: params[:description],
-        unit_price: params[:unit_price],
-        able: params[:able]
-        })
-    else
-      item.update({able: params[:able]})
-    end
-    item.save
-    flash[:alert] = "Item was successfully updated."
-    redirect_to "/merchants/#{@merchant.id}/items"
-  end
-
-
-  def item_show
-    @merchant = Merchant.find(params[:merchant_id])
-    @item = @merchant.items.find(params[:item_id])
-  end
-
-  def invoice_index
-  end
-
-  def invoice_show
-  end
+  # def item_index
+  #   @merchant = Merchant.find(params[:id])
+  #   # @items = @merchant.items.all
+  #   @enabled_items = (@merchant.items.all).enabled
+  #   @disabled_items = (@merchant.items.all).disabled
+  # end
+  #
+  # def item_create
+  #   @merchant = Merchant.find(params[:id])
+  #   # item = @merchant.items.new()
+  #   item = Item.new({name: params[:name], description: params[:description], unit_price: params[:unit_price], able: params[:able], merchant_id: @merchant.id})
+  #   item.save
+  #   redirect_to "/merchants/#{@merchant.id}/items"
+  # end
+  #
+  # def item_new
+  #   @merchant = Merchant.find(params[:id])
+  # end
+  #
+  # def item_edit
+  #   @merchant = Merchant.find(params[:merchant_id])
+  #   @item = @merchant.items.find(params[:item_id])
+  # end
+  #
+  # def item_update
+  #   @merchant = Merchant.find(params[:merchant_id])
+  #   # item = @merchant.items.find(params[:item_id])
+  #   item = Item.find(params[:item_id])
+  #   binding.pry
+  #   if params[:name]
+  #     item.update({
+  #       name: params[:name],
+  #       description: params[:description],
+  #       unit_price: params[:unit_price],
+  #       able: params[:able]
+  #       })
+  #   else
+  #     item.update({able: params[:able]})
+  #   end
+  #   item.save
+  #   flash[:alert] = "Item was successfully updated."
+  #   redirect_to "/merchants/#{@merchant.id}/items"
+  # end
+  #
+  #
+  # def item_show
+  #   @merchant = Merchant.find(params[:merchant_id])
+  #   @item = @merchant.items.find(params[:item_id])
+  # end
 
 end
