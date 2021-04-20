bfe240f5310b04b91c9680c1063ac7e25cbfdd15
Aidan McGuire <>
2021-04-19 18:00:55 -0600

    Add github services

---
+++ app/services/git_hub_service.rb
@@ -0,0 +1,23 @@
+class GitHubService 
+
+  def conn
+    conn = Faraday.new(
+      url: 'https://api.github.com',
+      headers: {'Accept' => 'application/vnd.github.v3+json'}
+    )
+  end
+  
+  def repo_name
+    resp = conn.get('/repos/A-McGuire/little-esty-shop')
+    json = JSON.parse(resp.body, symbolize_names: true)
+    json[:name] #repo name
+  end
+  
+  def repo_contributors
+    resp = conn.get('/repos/A-McGuire/little-esty-shop/contributors')
+    json = JSON.parse(resp.body, symbolize_names: true)
+    json.map do |user|
+      user[:login]
+    end
+  end
+end
