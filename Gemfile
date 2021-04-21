c241e0f86a3eaeb74190f2d773c4d22e2fe34036
ignored-comment <info.konham@gmail.com>
2021-04-21 17:52:19 -0400

    In process of API experimentation

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.5.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.4'
+# faraday gem
+gem 'faraday'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
---
+++ app/services/github_services.rb
@@ -0,0 +1,35 @@
+require 'faraday'
+require 'json'
+
+class GithubServices
+  def get_repo_name
+    get_url("https://api.github.com/repos/ignored-comment/little-esty-shop")[:name]
+  end
+
+  def get_collaborators
+    get_url("https://api.github.com/repos/ignored-comment/little-esty-shop/collaborators")
+  end
+
+  def get_commits
+    get_url("https://api.github.com/repos/ignored-comment/little-esty-shop/commits?")
+  end
+
+  def get_usernames
+    get_collaborators.each do |collaborator|
+      collaborator[:login]
+    end
+  end
+
+  token = ENV['GITHUB_TOKEN']
+
+  def get_url(url)
+    response = Faraday.get(url) do |req|
+      req.headers['Authorization'] = "token #{ENV['GITHUB_TOKEN']}"
+    end
+    data = response.body
+    JSON.parse(data, symbolize_names: true)
+  end
+end
+
+
+require "pry"; binding.pry
