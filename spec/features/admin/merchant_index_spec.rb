306818e09af00f087b9174d959fa5fc43c83da0d
Olivia Charles <ocharles92@gmail.com>
2021-04-21 18:18:24 -0400

    Added tests for merchants top5 rev and best sale day

--- spec/features/admin/merchant_index_spec.rb
+++ spec/features/admin/merchant_index_spec.rb
@@ -1,74 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe "Admin Merchant index page" do
-  before(:each) do
-    @parlour = Merchant.create!(name: 'Ice Cream Parlour', status: 0)
-    @tattoo = Merchant.create!(name: 'Tattoo Shop', status: 1)
-    @item_1 = @parlour.items.create!(name: 'Ice Cream Scoop', description: 'scoops ice cream', unit_price: 13)
-    @item_2 = @tattoo.items.create!(name: 'Back scratch', description: 'skirtch back', unit_price: 5, status: 1)
-    @item_3 = @tattoo.items.create!(name: 'Pooper Scooper', description: 'holds doge poo', unit_price: 13)
+  before(:each)do
+    @merchant1 = Merchant.create!(name: 'Ice Cream Parlour', status: "disabled")
+    @merchant2 = Merchant.create!(name: 'Inked', status: "disabled")
+    @merchant3 = Merchant.create!(name: 'Plants', status: "enabled")
+    @merchant4 = Merchant.create!(name: 'Water Corp', status: "enabled")
+    @merchant5 = Merchant.create!(name: 'Chocolates', status: "disabled")
+
+
+    @item_1 = create(:item, merchant: @merchant1)
+    @item_2 = create(:item, merchant: @merchant2)
+    @item_3 = create(:item, merchant: @merchant3)
+    @item_4 = create(:item, merchant: @merchant4)
+    @item_5 = create(:item, merchant: @merchant5)
+
+    @customer = create(:customer)
+    @customer_1 = create(:customer)
+    @customer_2 = create(:customer)
+    @customer_3 = create(:customer)
+    @customer_4 = create(:customer)
+    @customer_5 = create(:customer)
+
+    @invoice_1 = Invoice.create!(status: 0, customer_id: @customer.id)
+    @invoice_2 = Invoice.create!(status: 0, customer_id: @customer_1.id)
+    @invoice_3 = Invoice.create!(status: 0, customer_id: @customer_2.id)
+    @invoice_4 = Invoice.create!(status: 0, customer_id: @customer_3.id)
+    @invoice_5 = Invoice.create!(status: 0, customer_id: @customer_4.id)
+    @invoice_6 = Invoice.create!(status: 0, customer_id: @customer_5.id)
+
+    InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 3, unit_price: 4, status: 0)
+    InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_2.id, quantity: 10, unit_price: 2, status: 2)
+    InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_3.id, quantity: 2, unit_price: 40, status: 2)
+    InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_4.id, quantity: 10, unit_price: 5, status: 2)
+    InvoiceItem.create!(item_id: @item_5.id, invoice_id: @invoice_5.id, quantity: 1, unit_price: 2, status: 2)
+    InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_6.id, quantity: 3, unit_price: 5, status: 2)
+
+    @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_1.id}")
+    @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_2.id}")
+    @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_3.id}")
+    @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_4.id}")
+    @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_5.id}")
+    @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_6.id}")
+
     visit "admin/merchant"
   end
 
   it "had a list of all merchants names" do
     expect(page).to have_content("Admin Dashboard")
-    expect(page).to have_content(@parlour.name)
-    expect(page).to have_content(@tattoo.name)
-  end
-
-  it "click on merchant name takes me to that merchants show page" do
-    click_link "Ice Cream Parlour"
-
-    expect(current_path).to eq ("/admin/merchant/#{@parlour.id}")
-    expect(page).to have_content("Ice Cream Parlour")
+    expect(page).to have_link(@merchant1.name)
+    expect(page).to have_link(@merchant2.name)
+    expect(page).to have_link(@merchant3.name)
+    expect(page).to have_link(@merchant4.name)
+    expect(page).to have_link(@merchant5.name)
   end
 
   it 'shows a button next to each merchant to enable or disable that merchant' do
-    within("#disabled-merchant-#{@parlour.id}") do
+    within("#disabled-merchant-#{@merchant2.id}") do
       expect(page).to have_button("Enable")
+      click_button "Enable"
     end
 
-    within("#enabled-merchant-#{@tattoo.id}") do
+    within("#enabled-merchant-#{@merchant2.id}") do
       expect(page).to have_button("Disable")
     end
   end
 
   it 'can click on enable and it updates item status to enable' do
-    within("#disabled-merchant-#{@parlour.id}") do
+    within("#disabled-merchant-#{@merchant1.id}") do
       click_button "Enable"
     end
 
     expect(current_path).to eq("/admin/merchant")
 
-    within("#enabled-merchant-#{@parlour.id}") do
+    within("#enabled-merchant-#{@merchant1.id}") do
       expect(page).to have_button("Disable")
-    end
-  end
-
-  it 'can click on disable and it updates item status to disabled' do
-    within("#enabled-merchant-#{@tattoo.id}") do
-      click_button "Disable"
+        click_button "Disable"
     end
 
-    expect(current_path).to eq("/admin/merchant")
-
-    within("#disabled-merchant-#{@tattoo.id}") do
+    within("#disabled-merchant-#{@merchant1.id}") do
       expect(page).to have_button("Enable")
     end
   end
 
   it 'shows section for top 5 merchants by total revenue' do
-    expect(page).to have_content("Top Merchants")
-    #update to include revenue and names, need to add
-    #invoice_items and transactions to the before each
-    #want to use factory bot for the sake of time
+    expect(page).to have_content("Top 5 Merchants")
+    expect(@merchant3.name).to appear_before(@merchant2.name)
+    expect(@merchant2.name).to appear_before(@merchant4.name)
+    expect(@merchant4.name).to appear_before(@merchant1.name)
+    expect(@merchant1.name).to appear_before(@merchant5.name)
+
+    expect(page).to have_link(@merchant1.name)
+    expect(page).to have_link(@merchant2.name)
+    expect(page).to have_link(@merchant3.name)
+    expect(page).to have_link(@merchant4.name)
+    expect(page).to have_link(@merchant5.name)
+    expect(page).to have_content("Top selling date for #{@merchant1.name} was #{@invoice_1.created_at.strftime("%A, %B %d, %Y")}")
   end
-
-# 
-# Notes on Revenue Calculation:
-#
-# Only invoices with at least one successful transaction should count towards revenue
-# Revenue for an invoice should be calculated as the sum of the revenue of all invoice items
-# Revenue for an invoice item should be calculated as the invoice item unit price multiplied by the quantity (do not use the item unit price)
-
 end
--- spec/models/merchants_spec.rb
+++ spec/models/merchants_spec.rb
@@ -15,22 +15,66 @@ RSpec.describe Merchant, type: :model do
 
   describe 'class methods' do
     before(:each) do
-      @scream = Merchant.create!(name: 'Ice Scream', status: 0)
-      @tattoo = Merchant.create!(name: 'Tattood Inc', status: 1)
-      @plants = Merchant.create!(name: 'Growing Up', status:1)
+      @merchant1 = Merchant.create!(name: 'Ice Cream Parlour', status: "disabled")
+      @merchant2 = Merchant.create!(name: 'Inked', status: "disabled")
+      @merchant3 = Merchant.create!(name: 'Plants', status: "enabled")
+      @merchant4 = Merchant.create!(name: 'Water Corp', status: "enabled")
+      @merchant5 = Merchant.create!(name: 'Chocolates', status: "disabled")
+
+
+      @item_1 = create(:item, merchant: @merchant1)
+      @item_2 = create(:item, merchant: @merchant2)
+      @item_3 = create(:item, merchant: @merchant3)
+      @item_4 = create(:item, merchant: @merchant4)
+      @item_5 = create(:item, merchant: @merchant5)
+
+      @customer = create(:customer)
+      @customer_1 = create(:customer)
+      @customer_2 = create(:customer)
+      @customer_3 = create(:customer)
+      @customer_4 = create(:customer)
+      @customer_5 = create(:customer)
+
+      @invoice_1 = Invoice.create!(status: 0, customer_id: @customer.id)
+      @invoice_2 = Invoice.create!(status: 0, customer_id: @customer_1.id)
+      @invoice_3 = Invoice.create!(status: 0, customer_id: @customer_2.id)
+      @invoice_4 = Invoice.create!(status: 0, customer_id: @customer_3.id)
+      @invoice_5 = Invoice.create!(status: 0, customer_id: @customer_4.id)
+      @invoice_6 = Invoice.create!(status: 0, customer_id: @customer_5.id)
+
+      InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 3, unit_price: 4, status: 0)
+      InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_2.id, quantity: 10, unit_price: 2, status: 2)
+      InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_3.id, quantity: 2, unit_price: 40, status: 2)
+      InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_4.id, quantity: 10, unit_price: 5, status: 2)
+      InvoiceItem.create!(item_id: @item_5.id, invoice_id: @invoice_5.id, quantity: 1, unit_price: 2, status: 2)
+      InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_6.id, quantity: 3, unit_price: 5, status: 2)
+
+      @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_1.id}")
+      @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_2.id}")
+      @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_3.id}")
+      @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_4.id}")
+      @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_5.id}")
+      @transaction = Transaction.create!(credit_card_number: "123456789", credit_card_expiration_date: 1021, result: "success", invoice_id: "#{@invoice_6.id}")
+
     end
 
     describe '::disabled_merchants' do
       it 'finds rows where merchants are disabled' do
-        expect(Merchant.disabled_merchants).to eq([@scream])
-        expect(Merchant.disabled_merchants).to_not eq([@tattoo, @plants])
+        expect(Merchant.disabled_merchants).to eq([@merchant1, @merchant2, @merchant5])
+        expect(Merchant.disabled_merchants).to_not eq([@merchant3, @merchant4])
       end
     end
 
     describe '::enabled_merchants' do
       it 'finds rows where merchants are enabled' do
-        expect(Merchant.enabled_merchants).to eq([@tattoo, @plants])
-        expect(Merchant.enabled_merchants).to_not eq([@scream])
+        expect(Merchant.enabled_merchants).to eq([@merchant3, @merchant4])
+        expect(Merchant.enabled_merchants).to_not eq([@merchant1, @merchant2, @merchant5])
+      end
+    end
+
+    describe '::top_merchants_revenue' do
+      it 'shows merchants who created the most revenue' do
+        expect(Merchant.top_merchants_revenue).to eq([@merchant3, @merchant2, @merchant4, @merchant1, @merchant5])
       end
     end
   end
