2f245ca7f1d4b179583b23dede89d9a8afc6a29a
Kseniya K <kseniya.kudelich@gmail.com>
2021-04-21 19:46:13 -0600

    Add PR count calculator

--- app/services/github_services.rb
+++ app/services/github_services.rb
@@ -19,6 +19,25 @@ class GithubServices
     end
   end
 
+  def pull_requests
+    first_page = get_url("https://api.github.com/repos/ignored-comment/little-esty-shop/pulls?state=all&page=1")
+
+    second_page = get_url("https://api.github.com/repos/ignored-comment/little-esty-shop/pulls?state=all&page=2")
+
+    all_pages = first_page + second_page
+  end
+
+  def get_prs
+    bucket = 0
+
+    pull_requests.each do |element|
+      if element[:merged_at].is_a?(String)
+        bucket += 1
+      end
+    end
+    bucket
+  end
+
   token = ENV['GITHUB_TOKEN']
 
   def get_url(url)
