782432290db3e6f60090a970529d2edf1f97bc91
Zach Green <zachjamesgreen@gmail.com>
2021-06-01 17:38:44 -0600

    add relationships

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,8 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+
+  enum status: {pending: 0, packaged: 1, shipped: 2}
+  # enum status: [:pending, :packaged, :shipped]
+
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+
+  enum result: {failed: 0, success: 1, pending: 2}
+
 end
