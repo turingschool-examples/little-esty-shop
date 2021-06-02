1df21e9745b41d1bcfc2d2fd03064d39305ff9d3
JoePeterson51 <>
2021-06-01 21:42:04 -0600

    Merchant invoices

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
       get :dashboard
     end
     resources :items, only: :index
-    resources :invoices, only: :index
+    resources :invoices, only: [:index, :show]
   end
 end
---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'index' do
+  before(:each) do
+    @merchant = FactoryBot.create(:merchant)
+    @item_1 = FactoryBot.create(:item, merchant: @merchant)
+    @item_2 = FactoryBot.create(:item, merchant: @merchant)
+    @item_3 = FactoryBot.create(:item, merchant: @merchant)
+    @customer = FactoryBot.create(:customer)
+    @invoice_1 = FactoryBot.create(:invoice, customer: @customer)
+    @invoice_2 = FactoryBot.create(:invoice, customer: @customer)
+    @invoice_item = FactoryBot.create(:invoice_item, item: @item_1, invoice: @invoice_1)
+  end
+
+  it 'shows all the invoices with at least one of the merchants items' do
+    require 'pry'; binding.pry
+  end
+end
