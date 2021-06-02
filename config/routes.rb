8ef9602c85e9142f3f4c0152826a098181a0cfe3
Suzie K <suziekim.dev@gmail.com>
2021-06-02 09:40:12 -0400

    Add routes for merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/merchants/:merchant_id/dashboard', to: 'merchants#show', as: 'merchant_dashboard'
+
+  resources :merchants
+    resources :items, only: [:index, :show]
+    resources :invoices, only: [:index, :show]
+  end
 end
