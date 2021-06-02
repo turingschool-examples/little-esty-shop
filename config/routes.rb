0938cfa6e6360896238527ae2eb4856a4ca70e8d
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-02 14:56:03 -0600

    Revise admin routes to conform to user stories'

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :admin do
-    resources :customers,
-              :invoices,
-              :merchants,
-              :items,
-              :invoice_items,
-              :transactions
+  get '/admins', to: 'admins#index' # handrolled this route for admin dashboard 
+  namespace :admins do
+    resources :invoices, only: [:index, :show, :update]
+    resources :merchants, except: [:destroy]
   end
+
 end
