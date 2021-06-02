ced589c2edfe2908965d69fd8f297efc5118d7e5
Suzie K <suziekim.dev@gmail.com>
2021-06-02 10:26:27 -0400

    Green test shows name of merchant

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get '/merchants/:merchant_id/dashboard', to: 'merchants#show', as: 'merchant_dashboard'
-
-  resources :merchants
+  get 'merchants/:merchant_id/dashboard', to: 'merchants#show', as: 'merchant_dashboard'
+  
+  resources :merchants do
     resources :items
     resources :invoices
   end
