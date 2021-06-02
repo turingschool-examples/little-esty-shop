3419da480c2984eae5cc622ac36649df77b515f5
Zach Green <zachjamesgreen@gmail.com>
2021-06-01 20:41:05 -0600

    Add links to admin merchant and admin invoice page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/admin', to: 'admin#index'
+  namespace :admin do
+    get '/', to: 'dashboard#index'
+    get '/merchants', to: 'merchant#index'
+    get '/invoices', to: 'invoice#index'
+  end
 end
