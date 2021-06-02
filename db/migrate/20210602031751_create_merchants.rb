864df0a2268380470f3dfae7258644d4c7732020
Joe Ray <jray100.jr@gmail.com>
2021-06-01 21:38:37 -0600

    Add migrations

---
+++ db/migrate/20210602031751_create_merchants.rb
@@ -0,0 +1,10 @@
+class CreateMerchants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :merchants do |t|
+      t.string :name
+      t.integer :status, default: 1
+
+      t.timestamps
+    end
+  end
+end
