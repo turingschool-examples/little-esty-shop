2418489bf2ef9623ef67409716f775a7ed5a8c2a
Alex Klick <alexmac@Alexs-MacBook-Air.local>
2021-06-02 17:21:16 -0600

    routes

--- app/controllers/merchant_controller.rb
+++ app/controllers/merchant_controller.rb
@@ -2,13 +2,4 @@ class MerchantController < ApplicationController
   def index
     #render component: "HelloWorld", props: { greeting: "Hello from React-Rails." }
   end
-  def dashboard
-    @merchant = Merchant.find(params[:id])
-  end
-  def items
-    
-  end
-  def invoices
-    
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'merchant#index'
-  get '/merchants/:id/dashboard', to: 'merchant#dashboard'
-  get '/merchants/:id/invoices', to: 'merchant#invoices'
-  get '/merchants/:id/items', to: 'merchant#items'
+
+
+
+
+
+
+  namespace :merchants do
+    get '/:id/dashboard', to: 'dashboard#index'
+    get '/:id/items', to: 'items#index'
+    get '/:id/invoices', to: 'invoices#index'
+  end 
 end
