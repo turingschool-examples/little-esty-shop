12128494a95a2b39d94a50c25dfdf5344ecc4b6a
Joe Ray <jray100.jr@gmail.com>
2021-06-02 01:40:21 -0600

    Update admin-merch controller with edit & update actions.Add merchparams

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -8,7 +8,28 @@ class Admin::MerchantsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @merchant.update!(merchant_params)
+      flash[:notice] = "Merchant was successfully update!"
+
+      redirect_to admin_merchant_path(@merchant)
+    # *** Waiting on validations to be completed ***
+    # else
+    #   flash[:notice] = "Error: #{@merchant.errors.full_message.to_senetence}"
+    #           ORRRRRRRR
+    #   flash[:notice] = "Error: #{error_message(@merchant.errors)}"
+    #   render :edit
+    end
+  end
+
   private
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
+
   def find_merchant
     @merchant = Merchant.find(params[:id])
   end
