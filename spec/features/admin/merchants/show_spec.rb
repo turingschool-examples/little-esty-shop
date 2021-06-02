5a7b05bfe24ec177aef04f2b44d28f1eb3fe00c0
Joe Ray <jray100.jr@gmail.com>
2021-06-02 01:37:30 -0600

    Add test for admin-merch-update story

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -6,11 +6,26 @@ RSpec.describe 'Admin Merchants Show Page' do
     @merchant2 = Merchant.create!(name: "Jill", status: 1)
     @merchant3 = Merchant.create!(name: "Bob", status: 1)
 
-    visit admin_merchants_path(@merchant1)
+    visit admin_merchant_path(@merchant1)
   end
 
-  it "I see the name of the merchant whos page I'm on" do
-    expect(current_path).to eq(admin_merchants_path(@merchant1))
+  it "I see the name of the merchant whos page I'm on." do
+    expect(current_path).to eq(admin_merchant_path(@merchant1))
     expect(page).to have_content(@merchant1.name)
+  end
+
+  it "I see a link to update the merchant's information." do
+    expect(current_path).to eq(admin_merchant_path(@merchant1))
+    expect(page).to have_link('Update Merchant Information', href: edit_admin_merchant_path(@merchant1))
+  end
+
+  it "I can click link to update and get sent to edit page" do
+    expect(current_path).to eq(admin_merchant_path(@merchant1))
+    expect(page).to have_content(@merchant1.name)
+
+    click_link 'Update Merchant Information'
+
+
+    expect(current_path).to eq(edit_admin_merchant_path(@merchant1))
   end
 end
