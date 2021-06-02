18800e6507e4b304e186276c9a70742123f30394
Zach Green <zachjamesgreen@gmail.com>
2021-06-02 16:41:07 -0600

    Change routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     get '/', to: 'dashboard#index'
-    get '/merchants', to: 'merchant#index'
-    get '/invoices', to: 'invoice#index'
+    resources :merchants, only: [:index]
+    resources :invoices, only: [:index]
   end
 end
