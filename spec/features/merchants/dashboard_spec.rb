1f9eee7dd8ef48e214c77f24ac67699ded8e583a
Alex Klick <alexmac@Alexs-MacBook-Air.local>
2021-06-01 20:57:18 -0600

    created test structure for merch dashboard

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -21,4 +21,42 @@ describe 'Merchant Dashboard Page' do
     expect(page).to have_content("Items")
     expect(page).to have_content("Invoices")
   end
+
+  #   As a merchant,
+  # When I visit my merchant dashboard
+  # Then I see the names of the top 5 customers
+  # who have conducted the largest number of successful transactions with my merchant
+  # And next to each customer name I see the number of successful transactions they have
+  # conducted with my merchant
+  it 'has the top 5 customers by number successful transactions' do
+
+  end
+
+  it 'shows number of successful transactions with merchant for the top 5 customers' do
+  
+  end
+  #   As a merchant
+  # When I visit my merchant dashboard
+  # Then I see a section for "Items Ready to Ship"
+  # In that section I see a list of the names of all of my items that
+  # have been ordered and have not yet been shipped,
+  # And next to each Item I see the id of the invoice that ordered my item
+  # And each invoice id is a link to my merchant's invoice show page
+  it 'has a section for items ready to ship, with list of names of items to ship' do
+
+  end
+
+  it 'each item has the id of the invoice, which is a link to the invoice show page' do
+
+  end
+  # As a merchant
+  # When I visit my merchant dashboard
+  # In the section for "Items Ready to Ship",
+  # Next to each Item name I see the date that the invoice was created
+  # And I see the date formatted like "Monday, July 18, 2019"
+  # And I see that the list is ordered from oldest to newest  
+
+  it 'has the date of the invoice formatted, list ordered from oldest to newest' do
+
+  end
 end
