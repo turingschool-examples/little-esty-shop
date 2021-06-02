fcccd4ed79fca2725edcad4d76fbd966f6f322d8
Zach Green <zachjamesgreen@gmail.com>
2021-06-01 23:02:55 -0600

    Add class method to find top 5 customers by transactions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,19 @@
 class Customer < ApplicationRecord
   has_many :invoices
+
+  def self.top_5_customers_by_transactions
+    select('customers.*, count(transactions.id) as transaction_count')
+      .joins(invoices: :transactions)
+      .where('transactions.result': 2)
+      .group('customers.id')
+      .order(transaction_count: :desc)
+      .limit(5)
+    # select c.*, count(t.id) as transaction_count from customers as c
+    # join invoices as i on c.id = i.customer_id
+    # join transactions as t on i.id = t.invoice_id
+    # where t.result = 2
+    # group by c.id
+    # order by transaction_count desc
+    # limit 5;
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'class methods' do
+    describe '#top_5_customers_by_transactions' do
+      it 'gives the top 5 customers by transactions' do
+        data = Customer.top_5_customers_by_transactions
+        expect(data.map(&:first_name)).to match_array ["Otis", "Leo", "Ben", "Liliana", "Estel"]
+        expect(data.map(&:transaction_count)).to match_array [10, 10, 10, 10, 10]
+      end
+    end
+  end
 end
