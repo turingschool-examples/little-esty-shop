60db96644875e760eab57c9a34f2ad36bcf68429
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-06-03 10:22:07 -0700

    Add show to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,8 @@ class ItemsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @items = @merchant.items
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 end
