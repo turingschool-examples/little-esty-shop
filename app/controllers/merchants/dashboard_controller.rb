06e68847519125972c0bd4818564dcce3a357138
Suzie K <suziekim.dev@gmail.com>
2021-06-03 14:07:53 -0400

    Update file structure

--- app/controllers/merchants/dashboard_controller.rb
+++ app/controllers/merchants/dashboard_controller.rb
@@ -1,8 +1,7 @@
-class MerchantsController < ApplicationController
+class Merchants::DashboardController < ApplicationController
 
   def show 
     @merchant = Merchant.find(params[:merchant_id])
     @shippable_items = @merchant.items.not_yet_shipped
-    # @item = Item.not_yet_shipped(merchant_id)
   end
 end
