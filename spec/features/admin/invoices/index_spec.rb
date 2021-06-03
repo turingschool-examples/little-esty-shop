88c786f18714c33a5c7c60885d22f8b15b12fbd6
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-02 21:16:15 -0600

    Implements prototype of working with Rails fixtures

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -1,28 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'invoices index page', type: :feature do
-  before(:each) do
-    @jack_daniels = Customer.create!(first_name: "Jack", last_name: "Daniels")
-    @isabella = Customer.create!(first_name: "Isabella", last_name: "Cocoran")
-    @invoice1 = @jack_daniels.invoices.create!(status: "Pending") # will need to implement enum in invoice model
-    @invoice2 = @jack_daniels.invoices.create!(status: "Pending") # will need to implement enum in invoice model
-    @invoice3 = @isabella.invoices.create!(status: "Pending") # will need to implement enum in invoice model
-    @invoice4 = @isabella.invoices.create!(status: "Pending") # will need to implement enum in invoice model
-  end
+  # This loads the fixture files in test/fixtures
+  # Rails 5.2.x is dumb and can't load fixtures into
+  # test db unless they're in that specifc path
+  fixtures :customers
+  fixtures :invoices
 
   describe 'page appearance' do
     it 'has links to every invoice' do
       visit '/admin/invoices'
 
-      expect(page).to have_link "invoice-#{@invoice1.id}"
-      expect(page).to have_link "invoice-#{@invoice2.id}"
-      expect(page).to have_link "invoice-#{@invoice3.id}"
-      expect(page).to have_link "invoice-#{@invoice4.id}"
-      click_link "invoice-#{@invoice4.id}"
-      expect(current_path).to eq "/admin/invoices/#{@invoice4.id}"
+      expect(page).to have_link "invoice-#{invoices(:invoice_1).id}"
+      expect(page).to have_link "invoice-#{invoices(:invoice_2).id}"
+      expect(page).to have_link "invoice-#{invoices(:invoice_3).id}"
+      expect(page).to have_link "invoice-#{invoices(:invoice_4).id}"
+      click_link "invoice-#{invoices(:invoice_4).id}"
+      expect(current_path).to eq "/admin/invoices/#{invoices(:invoice_4).id}"
     end
   end
 
   describe 'page functionality' do
   end
+
+  #after :all do
+    #@jack_daniels.destroy
+    #@isabella.destroy
+    #@invoice1.destroy
+    #@invoice2.destroy
+    #@invoice3.destroy
+    #@invoice4.destroy
+  #end
 end
