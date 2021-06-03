9cb1025dfb94ef92bffc59bbae9ccf93e69507ec
Suzie K <suziekim.dev@gmail.com>
2021-06-03 09:13:30 -0400

    Red test ::find_invoice_created_at method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -5,6 +5,10 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
 
   def self.find_invoice_id(item_id)
-    invoice = where("#{item_id} = invoice_items.item_id").pluck(:invoice_id).first
+    joins(:invoice).where("#{item_id} = invoice_items.item_id").pluck(:invoice_id).first
+  end
+
+  def self.find_invoice_created_at(item_id)
+    joins(:invoice).select("invoices.created_at").where("#{item_id} = invoice_items.item_id").pluck(:created_at).first
   end
 end
