ef59bdf8edf06247396ed026ca66cebbc9d00367
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-06-03 13:02:12 +0200

    Creates merchant item index page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @merchant = Merchant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/merchants/:merchant_id/dashboard', to: 'merchants#show', as: 'merchant_dashboard'
-
-  resources :merchants
-    resources :items
-    resources :invoices
-  end
+  get '/merchants/:id/items', to: 'items#index'
 end
