85381b1b6464945975ee38a3c13a4d72d517d512
Joe Ray <jray100.jr@gmail.com>
2021-06-03 11:51:58 -0600

    Add admin merch new test

---
+++ spec/features/admin/merchants/new_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Merchants New Page' do
+  before :each do
+    # enabled
+    @merchant1 = Merchant.create!(name: "Tyler", status: 1)
+    @merchant2 = Merchant.create!(name: "Jill", status: 1)
+    # disabled
+    @merchant3 = Merchant.create!(name: "Bob", status: 0)
+
+    visit new_admin_merchant_path
+  end
+
+  it 'I can create a new merchant' do
+    expect(current_path).to eq(new_admin_merchant_path)
+    expect(page).to have_field('Name')
+    expect(page).to_not have_link('The pope')
+
+    fill_in 'Name', with: 'The pope'
+    click_button 'Submit'
+
+    expect(current_path).to eq(admin_merchants_path)
+
+    within ".disbaled-merchants" do
+      expect(page).to have_link('The pope')
+      expect(page).to have_button('Enable')
+    end
+  end
+end
