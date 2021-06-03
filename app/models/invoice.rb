4bcbd966cc1b077ea33c30de1cb2d340c238a2c8
Suzie K <suziekim.dev@gmail.com>
2021-06-03 13:37:07 -0400

    Setup for invoice index page

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,8 @@ class Invoice < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
   has_many :transactions, dependent: :destroy
+  
+  def self.find_invoice_created_at(item_id)
+    joins(:items).where("#{item_id} = invoice.id").pluck(:created_at).first
+  end
 end
--- spec/features/merchants/invoices/index_spec.rb
+++ spec/features/merchants/invoices/index_spec.rb
@@ -1,7 +1,14 @@
+# spec/features/merchants/invoices/index_spec
+
 require 'rails_helper'
 
-RSpec.describe Invoice do
-  before(:each) do
+# When I visit my merchant's invoices index (/merchants/merchant_id/invoices)
+# Then I see all of the invoices that include at least one of my merchant's items
+# And for each invoice I see its id
+# And each id links to the merchant invoice show page
+
+RSpec.describe 'Invoice index page' do
+  before :each do 
     @merchant = Merchant.create!(name: 'Sally Handmade')
     @merchant_2 = Merchant.create!(name: 'Billy Mandmade')
     @item =  @merchant.items.create!(name: 'Qui Essie', description: 'Lorem ipsim', unit_price: 75107)
@@ -15,16 +22,10 @@ RSpec.describe Invoice do
     InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice.id, quantity: 539, unit_price: 13635, status: 'packaged')
   end
 
-  describe 'relationships' do
-    it {should belong_to :customer}
-    it {should have_many :invoice_items}
-    it {should have_many :transactions}
-    it {should have_many(:items).through(:invoice_items)}
-  end
-
-  describe 'class methods' do
-  end
-
-  describe 'instance methods' do
+  it 'lists all invoices that has at least on  ' do
+    visit "merchants/#{@merchant.id}/invoices"
+    
+    expect(page).to have_content("#{@invoice.id}")
+    expect(page).to_not have_content("#{@invoice_2.id}")
   end
 end
