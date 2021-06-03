a8f2627534455b1d84e9ee0d0e07435473de2cd1
Gunnar <dunkcrunk.com>
2021-06-02 19:29:21 -0600

    Made tests pass for validations for item model.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,10 +7,12 @@ RSpec.describe Item, type: :model do
     it { should have_many(:invoices).through(:invoice_items) }
   end
 
-#   describe 'validations' do
-#     it { should validate_presence_of(:) }
-#   end
-#
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:unit_price) }
+  end
+
 #   before :each do
 #
 #   end
