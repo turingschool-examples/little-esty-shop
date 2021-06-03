be284fd6b0bd3d3ead7261bcd6af9ba356ace32d
Joe Ray <jray100.jr@gmail.com>
2021-06-03 16:57:15 -0600

    stash commit - undo

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,6 @@ class Transaction < ApplicationRecord
 
   validates :credit_card_number, presence: true, numericality: true
   validates :credit_card_expiration_date, presence: true, numericality: true
+
+  # enum result: [:success, :failed]
 end
