99b744917142f4b1d611688790dcc61496be522b
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-02 18:11:43 -0600

    Fix routes locally to admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/admins', to: 'admins#index' # handrolled this route for admin dashboard 
-  namespace :admins do
+  get '/admin', to: 'admins#index' # handrolled this route for admin dashboard 
+  namespace :admin do
     resources :invoices, only: [:index, :show, :update]
     resources :merchants, except: [:destroy]
   end
