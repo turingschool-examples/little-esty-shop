e80a5eb2e83fa22c73296bbcb9f9183a9e97ecfd
Suzie K <suziekim.dev@gmail.com>
2021-06-03 13:37:56 -0400

    Update routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'merchants/:merchant_id/dashboard', to: 'merchants#show', as: 'merchant_dashboard'
-  
-  resources :merchants do
+
+  resources :merchants, module: :merchants do
+    get '/dashboard', to: 'dashboard#show', as: 'merchant_dashboard'
     resources :items
     resources :invoices
   end
