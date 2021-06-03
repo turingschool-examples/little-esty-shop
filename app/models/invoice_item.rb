287b29dbd5e0ca32d9ee0dabdbfbcd2ee55b8402
Suzie K <suziekim.dev@gmail.com>
2021-06-02 22:07:09 -0400

    Red test ::find_invoice_id method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,4 +3,8 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
+
+  def self.find_invoice_id(item_id)
+    invoice = where("#{item_id} = invoice_items.item_id").pluck(:invoice_id).first
+  end
 end
