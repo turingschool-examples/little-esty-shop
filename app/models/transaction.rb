53d812cade695a747d2f796ed32ac7869a16b58d
Joe Ray <jray100.jr@gmail.com>
2021-06-03 23:14:32 -0600

    Comment out validation for CCED, update enums to match csv rake file

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
 
   validates :credit_card_number, presence: true, numericality: true
-  validates :credit_card_expiration_date, presence: true, numericality: true
+  # validates :credit_card_expiration_date, presence: true, numericality: true
 
-  enum result: { success: 0, failed: 1 } _prefix: :status
+  enum result: { failed: 0, success: 1 }, _prefix: :status
 end
