ded43be41f1a8fbb2c144804f83cd3001f369afd
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-06-03 19:53:36 -0700

    Add update action and private method to item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,4 +12,22 @@ class ItemsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @item = Item.find(params[:id])
   end
+
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    item = Item.find(params[:id])
+    item.update(item_params)
+    if item.save
+      redirect_to "/merchants/#{merchant.id}/items/#{item.id}"
+      flash[:notice] = "Item information has been successfully updated!"
+    else
+      redirect_to "/merchants/#{merchant.id}/items/#{item.id}/edit"
+      flash[:alert] = "Error: #{item.errors.full_messages.to_sentence}"
+    end
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :unit_price)
+  end
 end
