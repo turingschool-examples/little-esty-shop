7b49b456f23c9dda9643a1e04a204526efef7366
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-06-03 21:13:54 -0600

    Add to views and controller for merchant dashboard items/invoices links.

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -2,5 +2,6 @@ class Merchant::InvoicesController < ApplicationController
   def index
     @merchant = Merchant.find(params[:id])
     @items = @merchant.items
+    @invoices = Invoice.joins(:invoice_items).where("item.id = ?", @items)
   end
 end
