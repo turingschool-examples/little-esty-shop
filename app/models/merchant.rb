26d35bef112225b4f29a59cc2148eb3aa042761a
Joe Ray <jray100.jr@gmail.com>
2021-06-04 00:50:53 -0600

    Add merchant_best_day method to Merchant model file

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -22,8 +22,24 @@ class Merchant < ApplicationRecord
   end
 
   def self.top_merchants_by_revenue
-    joins(invoices: [:invoice_items, :transactions]).where('result >= ?', 1).select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue').group(:id)
+    joins(invoices: [:invoice_items, :transactions])
+    .where('result >= ?', 1)
+    .select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue')
+    .group(:id)
     .order('total_revenue desc')
     .limit(5)
+  end
+
+  def merchant_best_day
+    invoices
+    .joins(:invoice_items)
+    .select('invoices.created_at, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue')
+    .where(status: 2)
+    .group(:created_at)
+    .order('total_revenue desc', 'created_at desc')
+    .first
+    .created_at
+    .to_date
+    .strftime("%m/%d/%Y")
   end
 end
