6b58008201fcab26c0d9dad786e20a05673436ce
Joe Ray <jray100.jr@gmail.com>
2021-06-03 23:10:04 -0600

    Comment out validation for credit_card_expiration_date

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe Transaction, type: :model do
   describe 'relationships' do
     it { should belong_to(:invoice) }
   end
- 
+
   describe 'validations' do
     it { should validate_presence_of(:credit_card_number) }
-    it { should validate_presence_of(:credit_card_expiration_date) }
+    # it { should validate_presence_of(:credit_card_expiration_date) }
   end
 end
-
