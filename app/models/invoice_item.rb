12da7f8d0e980bda7e02b6671dbbdf0e64f7dbc4
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-06-03 21:22:50 -0600

    Add validations for models.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,7 @@
 class InvoiceItem < ApplicationRecord
+  validates :quantity, presence: true, numericality: true
+  validates :unit_price, presence: true, numericality: true
+  validates_presence_of :status
   belongs_to :invoice
   belongs_to :item
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   validates_presence_of :credit_card_number
-  validates_presence_of :credit_card_expiration_date
+  validates :credit_card_expiration_date, presence: true, numericality: true
   validates_presence_of :result
-  
+
   belongs_to :invoice
 end
