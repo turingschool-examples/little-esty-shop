41e166802a25f50fef8da0349b1a9e847d752b61
Emmy Morris <emmymorris1102@gmail.com>
2021-06-03 18:43:10 -0600

    Pass Admin Dashboard Links test

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,13 +1,31 @@
 require 'rails_helper'
-# Admin Dashboard
 
-# As an admin,
-# When I visit the admin dashboard (/admin)
-# Then I see a header indicating that I am on the admin dashboard
 RSpec.describe 'Admin' do
   it 'it shows admin dashboard' do
-    visit '/admin' 
+    # Admin Dashboard
+
+    # As an admin,
+    # When I visit the admin dashboard (/admin)
+    # Then I see a header indicating that I am on the admin dashboard
+    visit '/admin'
 
     expect(page).to have_content('Admin Dashboard')
+  end
+
+  it 'has links' do
+    # Admin Dashboard Links
+    # As an admin,
+    # When I visit the admin dashboard (/admin)
+    # Then I see a link to the admin merchants index (/admin/merchants)
+    # And I see a link to the admin invoices index (/admin/invoices)
+    visit '/admin'
+    expect(page).to have_link('Merchants')
+    click_link 'Merchants'
+    expect(current_path).to eq('/admin/merchants')
+
+    visit '/admin'
+    expect(page).to have_link('Invoices')
+    click_link 'Invoices'
+    expect(current_path).to eq('/admin/invoices')
   end
 end
