7dad5806421a3de68b320b1578ef47fc05e34a31
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-06-04 08:54:13 -0600

    Initial commit to heroku.

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Merchant::DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:id])
+    @items = @merchant.items
+    @ready_to_ship = Item.joins(:invoice_items).where("invoice_items.status != ?", 2)
+
+    binding.pry
+
   end
 end
