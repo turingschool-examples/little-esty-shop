9bbeb83f3504e253776c38916b9ef2cc8084c4f6
Joe Ray <jray100.jr@gmail.com>
2021-06-03 23:11:46 -0600

    Add dependent destroy for invoices relationship

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  has_many :invoices
+  has_many :invoices, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
