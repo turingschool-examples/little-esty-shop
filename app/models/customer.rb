3fc71739d2ee7b98ef210a071fba26cf3dbd7a47
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-03 23:49:04 -0600

    Add method to capture top five customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,11 @@
 class Customer < ApplicationRecord
   has_many :invoices
+
+  def self.top_five_by_transaction_success
+    joins(invoices: :transactions)
+    .group(:id).where('transactions.result = ?', 0)
+    .select('customers.*, count(transactions.id) as number_of_transactions')
+    .order(number_of_transactions: :desc)
+    .limit(5)
+  end
 end
