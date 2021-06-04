6a4ac7a38235f7a0c555a5a7d9c294929c9eca2e
Caleb Wittman <calebjwittman1991@gmail.com>
2021-06-04 15:32:00 -0600

    create merchant invoices controller and mercahnt items controller as well as their views and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#show'
   get '/merchants/:merchant_id/items/:item_id/edit', to: 'merchant_items#edit'
-  patch '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#update'
+  patch '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#update', as: 'item'
+  get '/merchants/:merchant_id/invoices/:invoice_id', to: 'merchant/invoices#show'
+  patch '/merchants/:merchant_id/invoices/:invoice_id', to: 'merchant/invoices#show'
 end
