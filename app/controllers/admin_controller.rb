7bbfa9d48b7f0d84791d2488d3928df9e0a28cba
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-04 19:44:38 -0600

    Implement methods and create view for incomplete invoices

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,6 @@
 class AdminController < ApplicationController
   def index
     @top_five_customers = Customer.top_five_by_transaction_success
+    @incomplete_invoices = Invoice.ordered_invoices_not_shipped
   end
 end
