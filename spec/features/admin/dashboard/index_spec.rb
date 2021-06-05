58ec7bfafc2de818759d2a2b88e14c97bb501f54
Joe Ray <jray100.jr@gmail.com>
2021-06-05 15:29:26 -0600

    Add test for AD links

--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe 'Admin Dashboard Index Page' do
   it 'I see a header indicating that I am on the admin dashboard' do
     expect(page).to have_content('Admin Dashboard')
   end
+
+  it "Then I see a link to the admin merchants & invoices index page" do
+    expect(page).to have_link('Admin Merchant Index Page', href: admin_merchants_path)
+    expect(page).to have_link('Admin Invoice Index Page', href: admin_invoices_path)
+  end
 end
