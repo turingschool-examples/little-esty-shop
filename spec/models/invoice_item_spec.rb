32f36f6fd36df50863f42a971cff8f1a1ed174b0
Suzie K <suziekim.dev@gmail.com>
2021-06-05 11:54:09 -0400

    Red test returns expected total revenue

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -2,15 +2,6 @@ require 'rails_helper'
 
 RSpec.describe InvoiceItem do
   before(:each) do
-    @merchant = Merchant.create!(name: 'Sally Handmade')
-    @item =  @merchant.items.create!(name: 'Qui Esse', description: 'Lorem ipsim', unit_price: 75107)
-    @item_2 =  @merchant.items.create!(name: 'Essie', description: 'Lorem ipsim', unit_price: 20112)
-    @item_3 = @merchant.items.create!(name: 'Glowfish Markdown', description: 'Lorem ipsim', unit_price: 55542)
-    @customer = Customer.create!(first_name: 'Joey', last_name: 'Ondricka') 
-    @invoice = Invoice.create!(customer_id: @customer.id, status: 'completed')
-    InvoiceItem.create!(item_id: @item.id, invoice_id: @invoice.id, quantity: 539, unit_price: 13635, status: 'packaged')
-    InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice.id, quantity: 539, unit_price: 13635, status: 'packaged')
-    InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice.id, quantity: 539, unit_price: 13635, status: 'shipped')
 
   end
 
@@ -20,7 +11,12 @@ RSpec.describe InvoiceItem do
   end
 
   describe 'class methods' do
+    describe '.total_revenue' do
+      it 'returns expected total revenue of all invoice items' do
 
+        expect(InvoiceItem.total_revenue).to eq(60481323)
+      end
+    end
   end
 
   describe 'instance methods' do
