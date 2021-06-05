70cc630e91fcd8b8642709f1b30d9152117a5458
Suzie K <suziekim.dev@gmail.com>
2021-06-04 23:20:33 -0400

    Green test ::top_customers_by_merchant

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,6 +11,10 @@ class Customer < ApplicationRecord
     joins(:transactions).select("customers.*, CONCAT(customers.first_name, ' ', customers.last_name) as name, count(transactions.*) as num_trans").where("transactions.result = 'success'").group(:id).order("num_trans desc").limit(5).order(:first_name)
   end
 
+  def self.top_five_by_merchant(merchant_id)
+    joins(:transactions, :merchants).select("customers.*, CONCAT(customers.first_name, ' ', customers.last_name) as name, count(transactions.*) as num_trans").where("transactions.result = 'success' and items.merchant_id = #{merchant_id}").group(:id).order("num_trans desc").limit(5).order(:first_name)
+  end
+
   def full_name
     first_name.concat(" ", last_name)
   end
