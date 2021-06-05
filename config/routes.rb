8767bc5da12d9e0db34b7e983ff646bafdb3cc74
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-04 23:10:06 -0600

    Add routes for admin invoices index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/admin', to: 'admin#index'
+  get '/admin/invoices', to: 'admin/invoices#index'
+  get '/admin/invoices/:invoice_id', to: 'admin/invoices#show'
 
   get '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#show'
   get '/merchants/:merchant_id/items/:item_id/edit', to: 'merchant_items#edit'
