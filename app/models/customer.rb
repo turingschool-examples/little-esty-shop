03471edb7224bedc3b6755db2e1d70bba2e4c293
Suzie K <suziekim.dev@gmail.com>
2021-06-04 22:43:28 -0400

    Green test .full_name method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,4 +10,8 @@ class Customer < ApplicationRecord
   def self.top_five
     joins(:transactions).select("customers.*, CONCAT(customers.first_name, ' ', customers.last_name) as name, count(transactions.*) as num_trans").where("transactions.result = 'success'").group(:id).order("num_trans desc").limit(5).order(:first_name)
   end
+
+  def full_name
+    first_name.concat(" ", last_name)
+  end
 end
