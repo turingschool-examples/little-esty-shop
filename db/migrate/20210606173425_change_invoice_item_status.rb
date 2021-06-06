40d6a4faa5ea5a13b70a77bd7b6742042de77e4e
Suzie K <suziekim.dev@gmail.com>
2021-06-06 13:53:46 -0400

    Remove for heroku push

--- db/migrate/20210606173425_change_invoice_item_status.rb
+++
@@ -1,9 +0,0 @@
-class ChangeInvoiceItemStatus < ActiveRecord::Migration[5.2]
-  def up
-    change_column :invoice_items, :status, 'integer USING CAST(status AS integer)'
-  end
-
-  def down 
-    change_column :invoice_items, :status, :integer
-  end
-end
