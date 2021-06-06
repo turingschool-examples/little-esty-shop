418dadcb5d9f7db5992d90033b809fdd1df02f7b
Suzie K <suziekim.dev@gmail.com>
2021-06-06 14:14:20 -0400

    Update Invoice show page

--- app/controllers/merchants/invoices_controller.rb
+++ app/controllers/merchants/invoices_controller.rb
@@ -16,6 +16,7 @@ class Merchants::InvoicesController < ApplicationController
     invoice = Invoice.find(params[:id])
     invoice_item = InvoiceItem.find(params[:ii_id])
     invoice_item.update(:status => params[:status])
+    invoice_item.save
     
     redirect_to "/merchants/#{merchant.id}/invoices/#{invoice.id}"
     flash[:notice] = "Item status successfully updated!"
