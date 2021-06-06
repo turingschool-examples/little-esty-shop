7d663ea53f77413455aac7f51dc895649348cd3d
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-06-06 10:03:35 -0700

    Add transaction relation to Item model, move belongs_to up to organize code differently

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,29 @@
 class Item < ApplicationRecord
+  belongs_to :merchant
+
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
-  belongs_to :merchant
+  has_many :transactions, through: :invoices
+
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true, numericality: true
 
   enum status: { disable: 0, enable: 1 }
 
-  ###TEST###
   def self.enable_items
     where(status: 1)
   end
 
   def self.disable_items
     where(status: 0)
+  end
+
+  def self.top_popular_items
+    joins(:invoice_items, :transactions)
+    .where('result = 1')
+    .group(:id)
+    .order("sum(invoice_items.quantity * invoice_items.unit_price)" 'desc')
+    .limit(5)
   end
 end
