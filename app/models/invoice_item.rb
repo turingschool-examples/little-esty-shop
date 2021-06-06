fb659ca9bb28dd67e26d06f7e676934acca4493d
Suzie K <suziekim.dev@gmail.com>
2021-06-06 14:07:55 -0400

    Add hash for status

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,7 +3,7 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
-  enum status: [ :pending, :packaged, :shipped ]
+  enum status: { pending: 0, packaged: 1, shipped: 2 }
 
   def self.total_revenue 
     sum("invoice_items.unit_price * invoice_items.quantity")
