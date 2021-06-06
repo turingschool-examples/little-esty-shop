a9c086e084c949d597ac1b599f928217a4f0375a
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-06-06 14:18:56 -0600

    Add test for merchant index and show

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe 'merchants index page', type: :feature do
-  before(:each) do
-  end
+RSpec.describe 'merchants index page', type: :feature do\
 
   describe 'page appearance' do
+    it 'has a section that shows the names of each merchant' do
+      visit '/admin/merchants'
+
+      merchants = Merchant.all
+
+      merchants.each do |merchant|
+        expect(page).to have_content(merchant.name)
+      end
+    end
   end
 
   describe 'page functionality' do
+    it 'each merchant name is a link to each merchants show page' do
+      visit '/admin/merchants'
+
+      click_on 'Schroeder-Jerde'
+
+      expect(current_path).to eq '/admin/merchants/1'
+    end
   end
 end
