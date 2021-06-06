08a4e295a36d1de234319fb050facf25ceaea482
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-06-06 16:30:37 -0600

    Add test for admin merchant create new merchant

--- spec/features/admin/merchants/new_spec.rb
+++ spec/features/admin/merchants/new_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'merchants new page', type: :feature do
-  before(:each) do
-  end
-
-  describe 'page appearance' do
-  end
-
   describe 'page functionality' do
+    it 'allows me to enter details for a new merchant and redirects to admin/merchants/index on creation' do
+      visit '/admin/merchants/new'
+
+      expect(page).to have_content('Name')
+
+      fill_in 'Name', with: 'Big Bird'
+
+      click_button 'Save changes'
+
+      expect(current_path).to eq('/admin/merchants')
+
+      expect(page).to have_content('Big Bird')
+    end
   end
 end
