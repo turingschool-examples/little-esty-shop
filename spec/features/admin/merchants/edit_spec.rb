bd74c8b9ff946ee0492803c182c67c5639107570
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-06-06 15:31:14 -0600

    Add feature test for admin merchant update

--- spec/features/admin/merchants/edit_spec.rb
+++ spec/features/admin/merchants/edit_spec.rb
@@ -5,8 +5,28 @@ RSpec.describe 'merchants edit page', type: :feature do
   end
 
   describe 'page appearance' do
+    it 'shows the current information for the record' do
+      visit 'admin/merchants/1/edit'
+
+      expect(page).to have_content('Name')
+      expect(page).to have_content('Schroeder-Jerde')
+      expect(page).to have_content('Enabled')
+      expect(page).to have_content('Yes')
+    end
   end
 
   describe 'page functionality' do
+    it 'allows me to update the info, and redirects me to the show page with a success flash message' do
+      new_merchant = Merchant.create(name: 'Big Bird')
+      visit "admin/merchants/#{new_merchant.id}/edit"
+
+      fill_in 'Name', with: 'TEST'
+
+      click_on 'Save changes'
+
+      expect(current_path).to eq("/admin/merchants/#{new_merchant.id}")
+      expect(page).to have_content('TEST')
+      expect(page).to have_content('You have successfully updated this merchant!')
+    end
   end
 end
