d7731331e91f563ea3c6e20dcbb63926f1a89244
Suzie K <suziekim.dev@gmail.com>
2021-06-06 13:42:22 -0400

    Update for heroku

---
+++ db/migrate/20210606173425_change_invoice_item_status.rb
@@ -0,0 +1,9 @@
+class ChangeInvoiceItemStatus < ActiveRecord::Migration[5.2]
+  def up
+    change_column :invoice_items, :status, 'integer USING CAST(status AS integer)'
+  end
+
+  def down 
+    change_column :invoice_items, :status, :integer
+  end
+end
