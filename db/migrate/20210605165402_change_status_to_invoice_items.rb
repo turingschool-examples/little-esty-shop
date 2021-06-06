d1ba4404ce1bc796d4fea9d15ca61d026d665ecb
Suzie K <suziekim.dev@gmail.com>
2021-06-06 10:13:07 -0400

    Update migration for heroku push

--- db/migrate/20210605165402_change_status_to_invoice_items.rb
+++ db/migrate/20210605165402_change_status_to_invoice_items.rb
@@ -1,5 +1,8 @@
 class ChangeStatusToInvoiceItems < ActiveRecord::Migration[5.2]
   def change
+    InvoiceItem.where(status: 'pending').update_all(status: 0)
+    InvoiceItem.where(status: 'packaged').update_all(status: 1)
+    InvoiceItem.where(status: 'shipped').update_all(status: 2)
     change_column :invoice_items, :status, 'integer USING CAST("status" AS integer)'
   end
 end
