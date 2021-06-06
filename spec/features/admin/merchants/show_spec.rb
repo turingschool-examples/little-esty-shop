e067d148e15346a8fd956a9442349e1ad72ee6f9
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-06-06 14:23:54 -0600

    Add test for admin merchant show page

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'merchants show page', type: :feature do
-  before(:each) do
-  end
-
   describe 'page appearance' do
+    it 'has the merchants name' do
+      visit '/admin/merchants/1'
+
+      expect(page).to have_content('Schroeder-Jerde')
+    end
   end
 
   describe 'page functionality' do
+    it 'has a link that allows you to update the merchant' do
+      visit '/admin/merchants/1'
+
+      expect(page).to have_link('Edit')
+
+      click_link 'Edit'
+
+      expect(current_path).to eq('/admin/merchants/1/edit')
+    end
   end
 end
