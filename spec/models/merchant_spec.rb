31b79052a62e9690089841d3f5420e66382e96e0
Joe Ray <jray100.jr@gmail.com>
2021-06-06 14:43:59 -0600

    Fix date issue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -87,7 +87,7 @@ RSpec.describe Merchant, type: :model do
   describe 'instance methods' do
     describe '#merchant_best_day' do
       it "shows top merchants best day" do
-        expect(@merchant6.merchant_best_day).to eq("06/04/2021")
+        expect(@merchant6.merchant_best_day).to eq("06/06/2021")
 
         expect(@merchant6.merchant_best_day).to_not eq("06/03/2021")
         expect(@merchant6.merchant_best_day).to_not eq(@merchant6.created_at)
