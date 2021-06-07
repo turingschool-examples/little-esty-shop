53313fcd811632216f5ebefd42dc90a5cf1869f7
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-06 20:59:06 -0600

    Refactor admin index and invoices show pages

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -10,7 +10,7 @@ class Invoice < ApplicationRecord
 
   def self.ordered_invoices_not_shipped
     joins(:invoice_items)
-      .where.not('invoice_items.status = ?', 2)
+      .where('invoice_items.status <> ?', 2)
       .order(created_at: :desc)
       .distinct
   end
@@ -24,3 +24,4 @@ class Invoice < ApplicationRecord
 end
 
 # Invoice.joins(:invoice_items).group(:id).select('invoices.*,sum(invoice_items.quantity*invoice_items.unit_price) as invoice_revenue').where('invoice_items.invoice_id = ?', params)
+# Invoice.joins(:invoice_items).where('invoice_items.status <> ?', 2).order(created_at: :desc).distinct
