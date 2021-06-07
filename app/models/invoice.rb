94c8c63aaf2a189ed9140ecd71cca7028518d595
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-06 18:29:36 -0600

    Refactor models on capitalization

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,7 +6,7 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
   has_many :transactions, dependent: :destroy
 
-  enum status: ['in progress', 'completed', 'cancelled']
+  enum status: ['In Progress', 'Completed', 'Cancelled']
 
   def self.ordered_invoices_not_shipped
     joins(:invoice_items)
