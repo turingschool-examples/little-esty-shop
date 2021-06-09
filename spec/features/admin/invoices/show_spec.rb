feec7812e3c323492fbf78ec31a0146dbd83e615
Joe Ray <jray100.jr@gmail.com>
2021-06-09 14:34:26 -0600

    Clean up test

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe 'Admin Invoice Show Page' do
 
     visit "/admin/invoices/#{@invoice1.id}"
   end
-  
+
   it 'can show information related to that specific invoice' do
 
     expect(current_path).to eq("/admin/invoices/#{@invoice1.id}")
     expect(page).to have_content(@invoice1.id)
-    expect(page).to have_content(@invoice1.status)    
-    expect(page).to have_content(@invoice1.created_at.strftime("%A, %B %d, %Y"))    
+    expect(page).to have_content(@invoice1.status)
+    expect(page).to have_content(@invoice1.created_at.strftime("%A, %B %d, %Y"))
     expect(page).to have_content(@customer1.first_name)
     expect(page).to have_content(@customer1.last_name)
   end
@@ -28,13 +28,13 @@ RSpec.describe 'Admin Invoice Show Page' do
     within "#invoices-items-#{@item1.id}" do
       expect(current_path).to have_content("/admin/invoices/#{@invoice1.id}")
       expect(page).to have_content(@item1.name)
-      expect(page).to have_content(@item1.unit_price)  
+      expect(page).to have_content(@item1.unit_price)
     end
- 
+
     within "#invoices-invoice-items-#{@invoice_item1.id}" do
       expect(current_path).to have_content("/admin/invoices/#{@invoice1.id}")
-      expect(page).to have_content(@invoice_item1.quantity)    
-      expect(page).to have_content(@invoice_item1.status) 
+      expect(page).to have_content(@invoice_item1.quantity)
+      expect(page).to have_content(@invoice_item1.status)
     end
   end
 
@@ -43,21 +43,11 @@ RSpec.describe 'Admin Invoice Show Page' do
     expect(page).to have_content("$50,000")
   end
 
-
-  # When I visit an admin invoice show page
-  # I see the invoice status is a select field
-  # And I see that the invoice's current status is selected
-  # When I click this select field,
-  # Then I can select a new status for the Invoice,
-  # And next to the select field I see a button to "Update Invoice Status"
-  # When I click this button
-  # I am taken back to the admin invoice show page
-  # And I see that my Invoice's status has now been updated
   it 'can click on and change the status with a select field' do
 
     page.select('Completed', from: 'status')
     click_button('Update Invoice Status')
-    
+
     expect(page).to have_select('status', selected: 'Completed')
   end
 end
