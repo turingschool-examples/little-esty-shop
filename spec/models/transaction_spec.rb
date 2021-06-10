7b28e770f7c0897f4cf8ecbbc35cf5ee7b6086fd
Leigh <lcpulzone@gmail.com>
2021-06-09 19:52:58 -0700

    Remove unused test

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -7,6 +7,5 @@ RSpec.describe Transaction, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:credit_card_number) }
-    # it { should validate_presence_of(:credit_card_expiration_date) }
   end
 end
