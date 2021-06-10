c9b6a18958e94328539b7e019fefd5ef352af937
Joe Ray <jray100.jr@gmail.com>
2021-06-09 18:18:16 -0600

    Add ghs test file

---
+++ spec/services/github_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe GithubService, type: :model do
+  it "returns the users data" do
+    # mock_response = '{}'
+
+    # allow(GithubService).to receive().and_return(mock_response)
+    # allow_any_instance_of(Faraday::Response).to receive(:body).and_return(mock_response)
+    # json = GithubService.user_info('user_name')
+
+    # expect(json).to be_a(Hash)
+    # expect(json).to have_key(:repo_name)
+    # expect(json).to have_key(:user_name_1)
+    # expect(json).to have_key(:user_name_2)
+    # expect(json).to have_key(:user_name_3)
+    # expect(json).to have_key(:user_name_4)
+    # expect(json).to have_key(:commits)
+    # expect(json).to have_key(:pull_requests)
+  end
+end
