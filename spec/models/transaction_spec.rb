b7fd23abb9759ab695ef39e3ac4e01f4254c4285
Leigh <lcpulzone@gmail.com>
2021-06-09 20:04:30 -0700

    Add enum test

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Transaction, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:credit_card_number) }
   end
+
+  describe 'enum' do
+    it { should define_enum_for(:result) }
+  end
 end
