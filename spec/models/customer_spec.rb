d613f09f249242017184cd0230c728bc9fc4a826
Leigh <lcpulzone@gmail.com>
2021-06-09 19:50:53 -0700

    Add relationship test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   describe 'relationships' do
     it { should have_many(:merchants_customers) }
+    it { should have_many(:merchants).through(:merchants_customers) }
     it { should have_many(:invoices) }
     it { should have_many(:transactions).through(:invoices) }
   end
