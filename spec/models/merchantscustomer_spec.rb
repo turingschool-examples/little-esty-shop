7dae0b67ba810e98322261616e84566825b73af6
Leigh <lcpulzone@gmail.com>
2021-06-09 19:48:54 -0700

    Add relationship test

--- spec/models/merchantscustomer_spec.rb
+++ spec/models/merchantscustomer_spec.rb
@@ -58,5 +58,7 @@ RSpec.describe MerchantsCustomer, type: :model do
   describe 'relationships' do
     it { should belong_to(:customer) }
     it { should belong_to(:merchant) }
+    it { should have_many(:invoices).through(:customer) }
+    it { should have_many(:transactions).through(:invoices) }
   end
 end
