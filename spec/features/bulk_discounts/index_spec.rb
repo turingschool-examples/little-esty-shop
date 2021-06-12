96641c5eea75da49eb4a725706bf9312bea11913
Leigh <lcpulzone@gmail.com>
2021-06-12 11:40:50 -0700

    Comment out paths until needed

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'bulk discounts index page' do
   before :each do
 
-    visit "/merchants/#{@merchant___.id}/bulk_discounts/"
-    visit merchant_bulk_discounts_path(@merchant___.id)
+    # visit "/merchants/#{@merchant___.id}/bulk_discounts/"
+    # visit merchant_bulk_discounts_path(@merchant___.id)
   end
 
   it '' do
--- spec/features/bulk_discounts/new_spec.rb
+++ spec/features/bulk_discounts/new_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'bulk discounts new page' do
   before :each do
 
-    visit "/merchants/#{@merchant___.id}/bulk_discounts/new"
-    visit new_merchant_bulk_discount_path(@merchant___.id)
+    # visit "/merchants/#{@merchant___.id}/bulk_discounts/new"
+    # visit new_merchant_bulk_discount_path(@merchant___.id)
   end
 
   it '' do
