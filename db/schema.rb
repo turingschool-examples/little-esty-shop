833220fd8e2c28e8933688c1699e78f58fbe3e24
Leigh <lcpulzone@gmail.com>
2021-06-12 15:28:32 -0700

    Change percentage datatype to an integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_12_215614) do
+ActiveRecord::Schema.define(version: 2021_06_12_222411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bulk_discounts", force: :cascade do |t|
-    t.float "percentage"
+    t.integer "percentage"
     t.integer "quantity_threshold"
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
