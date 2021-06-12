3995017064ba3fe2d2088beec51f5ab3f7f97caf
Leigh <lcpulzone@gmail.com>
2021-06-12 09:52:27 -0700

    Remove underscore from test names for consistency

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -5,62 +5,62 @@ RSpec.describe 'merchant dashboard' do
     @merchant = Merchant.create!(name: 'AnnaSellsStuff')
     @antimerchant = Merchant.create!(name: 'TheOtherOne')
 
-    @customer_1 = Customer.create!(first_name: 'John', last_name: 'Smith')
-    @customer_2 = Customer.create!(first_name: 'Julie', last_name: 'Baker')
-    @customer_3 = Customer.create!(first_name: 'Jared', last_name: 'Lanata')
-    @customer_4 = Customer.create!(first_name: 'Jira', last_name: 'Mutiu')
-    @customer_5 = Customer.create!(first_name: 'Josephina', last_name: 'Cortez')
-    @customer_6 = Customer.create!(first_name: 'Jemma', last_name: 'Henry')
+    @customer1 = Customer.create!(first_name: 'John', last_name: 'Smith')
+    @customer2 = Customer.create!(first_name: 'Julie', last_name: 'Baker')
+    @customer3 = Customer.create!(first_name: 'Jared', last_name: 'Lanata')
+    @customer4 = Customer.create!(first_name: 'Jira', last_name: 'Mutiu')
+    @customer5 = Customer.create!(first_name: 'Josephina', last_name: 'Cortez')
+    @customer6 = Customer.create!(first_name: 'Jemma', last_name: 'Henry')
 
-    @item_1 = Item.create!(name: 'Thing 1', description: 'This is the first thing.', unit_price: 14.9, merchant_id: @merchant.id)
-    @item_2 = Item.create!(name: 'Thing 2', description: 'This is the second thing.', unit_price: 16.3, merchant_id: @merchant.id)
-    @item_3 = Item.create!(name: 'Thing 3', description: 'This is the third thing.', unit_price: 19.4, merchant_id: @merchant.id)
-    @item_4 = Item.create!(name: 'Thing 4', description: 'This is the fourth thing.', unit_price: 12.2, merchant_id: @merchant.id)
-    @item_5 = Item.create!(name: 'Thing 5', description: 'This is the fifth thing.', unit_price: 15.3, merchant_id: @merchant.id)
-    @item_6 = Item.create!(name: 'Thing 6', description: 'This is the sixth thing.', unit_price: 10.4, merchant_id: @antimerchant.id)
+    @item1 = Item.create!(name: 'Thing 1', description: 'This is the first thing.', unit_price: 14.9, merchant_id: @merchant.id)
+    @item2 = Item.create!(name: 'Thing 2', description: 'This is the second thing.', unit_price: 16.3, merchant_id: @merchant.id)
+    @item3 = Item.create!(name: 'Thing 3', description: 'This is the third thing.', unit_price: 19.4, merchant_id: @merchant.id)
+    @item4 = Item.create!(name: 'Thing 4', description: 'This is the fourth thing.', unit_price: 12.2, merchant_id: @merchant.id)
+    @item5 = Item.create!(name: 'Thing 5', description: 'This is the fifth thing.', unit_price: 15.3, merchant_id: @merchant.id)
+    @item6 = Item.create!(name: 'Thing 6', description: 'This is the sixth thing.', unit_price: 10.4, merchant_id: @antimerchant.id)
 
-    @invoice_1 = Invoice.create!(status: 1, customer_id: @customer_1.id, created_at: "2021-06-05 20:11:38.553871")
-    @invoice_2 = Invoice.create!(status: 1, customer_id: @customer_2.id, created_at: "2021-06-07 20:11:38.553871")
-    @invoice_3 = Invoice.create!(status: 1, customer_id: @customer_3.id, created_at: "2021-06-06 20:11:38.553871")
-    @invoice_4 = Invoice.create!(status: 1, customer_id: @customer_4.id, created_at: "2021-06-01 20:11:38.553871")
-    @invoice_5 = Invoice.create!(status: 1, customer_id: @customer_5.id, created_at: "2021-06-02 20:11:38.553871")
-    @invoice_6 = Invoice.create!(status: 1, customer_id: @customer_6.id, created_at: "2021-06-03 20:11:38.553871")
+    @invoice1 = Invoice.create!(status: 1, customer_id: @customer1.id, created_at: "2021-06-05 20:11:38.553871")
+    @invoice2 = Invoice.create!(status: 1, customer_id: @customer2.id, created_at: "2021-06-07 20:11:38.553871")
+    @invoice3 = Invoice.create!(status: 1, customer_id: @customer3.id, created_at: "2021-06-06 20:11:38.553871")
+    @invoice4 = Invoice.create!(status: 1, customer_id: @customer4.id, created_at: "2021-06-01 20:11:38.553871")
+    @invoice5 = Invoice.create!(status: 1, customer_id: @customer5.id, created_at: "2021-06-02 20:11:38.553871")
+    @invoice6 = Invoice.create!(status: 1, customer_id: @customer6.id, created_at: "2021-06-03 20:11:38.553871")
 
-    @invoice_item_1 = InvoiceItem.create!(quantity: 2, unit_price: 14.9, status: 2, invoice_id: @invoice_1.id, item_id: @item_1.id)
-    @invoice_item_2 = InvoiceItem.create!(quantity: 5, unit_price: 16.3, status: 1, invoice_id: @invoice_1.id, item_id: @item_2.id, created_at: "2021-06-05 20:11:38.553871")
-    @invoice_item_3 = InvoiceItem.create!(quantity: 4, unit_price: 19.4, status: 2, invoice_id: @invoice_2.id, item_id: @item_3.id)
-    @invoice_item_4 = InvoiceItem.create!(quantity: 1, unit_price: 12.2, status: 2, invoice_id: @invoice_2.id, item_id: @item_4.id)
-    @invoice_item_5 = InvoiceItem.create!(quantity: 2, unit_price: 10.4, status: 2, invoice_id: @invoice_2.id, item_id: @item_2.id)
-    @invoice_item_6 = InvoiceItem.create!(quantity: 7, unit_price: 15.3, status: 1, invoice_id: @invoice_3.id, item_id: @item_5.id, created_at: "2021-06-06 20:11:38.553871")
-    @invoice_item_7 = InvoiceItem.create!(quantity: 6, unit_price: 10.4, status: 2, invoice_id: @invoice_3.id, item_id: @item_3.id)
-    @invoice_item_8 = InvoiceItem.create!(quantity: 3, unit_price: 19.4, status: 1, invoice_id: @invoice_4.id, item_id: @item_3.id, created_at: "2021-06-01 20:11:38.553871")
-    @invoice_item_9 = InvoiceItem.create!(quantity: 5, unit_price: 15.3, status: 1, invoice_id: @invoice_5.id, item_id: @item_5.id, created_at: "2021-06-01 20:11:38.553871")
-    @invoice_item_10 = InvoiceItem.create!(quantity: 5, unit_price: 15.3, status: 2, invoice_id: @invoice_6.id, item_id: @item_6.id)
-    @invoice_item_11 = InvoiceItem.create!(quantity: 5, unit_price: 15.3, status: 1, invoice_id: @invoice_4.id, item_id: @item_1.id, created_at: "2021-06-01 20:11:38.553871")
-    @invoice_item_12 = InvoiceItem.create!(quantity: 5, unit_price: 15.3, status: 2, invoice_id: @invoice_4.id, item_id: @item_2.id)
+    @invoice_item1 = InvoiceItem.create!(quantity: 2, unit_price: 14.9, status: 2, invoice_id: @invoice1.id, item_id: @item1.id)
+    @invoice_item2 = InvoiceItem.create!(quantity: 5, unit_price: 16.3, status: 1, invoice_id: @invoice1.id, item_id: @item2.id, created_at: "2021-06-05 20:11:38.553871")
+    @invoice_item3 = InvoiceItem.create!(quantity: 4, unit_price: 19.4, status: 2, invoice_id: @invoice2.id, item_id: @item3.id)
+    @invoice_item4 = InvoiceItem.create!(quantity: 1, unit_price: 12.2, status: 2, invoice_id: @invoice2.id, item_id: @item4.id)
+    @invoice_item5 = InvoiceItem.create!(quantity: 2, unit_price: 10.4, status: 2, invoice_id: @invoice2.id, item_id: @item2.id)
+    @invoice_item6 = InvoiceItem.create!(quantity: 7, unit_price: 15.3, status: 1, invoice_id: @invoice3.id, item_id: @item5.id, created_at: "2021-06-06 20:11:38.553871")
+    @invoice_item7 = InvoiceItem.create!(quantity: 6, unit_price: 10.4, status: 2, invoice_id: @invoice3.id, item_id: @item3.id)
+    @invoice_item8 = InvoiceItem.create!(quantity: 3, unit_price: 19.4, status: 1, invoice_id: @invoice4.id, item_id: @item3.id, created_at: "2021-06-01 20:11:38.553871")
+    @invoice_item9 = InvoiceItem.create!(quantity: 5, unit_price: 15.3, status: 1, invoice_id: @invoice5.id, item_id: @item5.id, created_at: "2021-06-01 20:11:38.553871")
+    @invoice_item10 = InvoiceItem.create!(quantity: 5, unit_price: 15.3, status: 2, invoice_id: @invoice6.id, item_id: @item6.id)
+    @invoice_item11 = InvoiceItem.create!(quantity: 5, unit_price: 15.3, status: 1, invoice_id: @invoice4.id, item_id: @item1.id, created_at: "2021-06-01 20:11:38.553871")
+    @invoice_item12 = InvoiceItem.create!(quantity: 5, unit_price: 15.3, status: 2, invoice_id: @invoice4.id, item_id: @item2.id)
 
-    @transaction1 = @invoice_1.transactions.create!(result: 1, credit_card_number: 4654405418249632)
-    @transaction2 = @invoice_1.transactions.create!(result: 1, credit_card_number: 4580251236515201)
-    @transaction3 = @invoice_1.transactions.create!(result: 1, credit_card_number: 4580251236515201)
+    @transaction1 = @invoice1.transactions.create!(result: 1, credit_card_number: 4654405418249632)
+    @transaction2 = @invoice1.transactions.create!(result: 1, credit_card_number: 4580251236515201)
+    @transaction3 = @invoice1.transactions.create!(result: 1, credit_card_number: 4580251236515201)
 
-    @transaction4 = @invoice_2.transactions.create!(result: 1, credit_card_number: 4923661117104166)
-    @transaction5 = @invoice_2.transactions.create!(result: 1, credit_card_number: 4515551623735617)
-    @transaction6 = @invoice_2.transactions.create!(result: 1, credit_card_number: 4515551623735617)
-    @transaction7 = @invoice_2.transactions.create!(result: 1, credit_card_number: 4515551623735607)
+    @transaction4 = @invoice2.transactions.create!(result: 1, credit_card_number: 4923661117104166)
+    @transaction5 = @invoice2.transactions.create!(result: 1, credit_card_number: 4515551623735617)
+    @transaction6 = @invoice2.transactions.create!(result: 1, credit_card_number: 4515551623735617)
+    @transaction7 = @invoice2.transactions.create!(result: 1, credit_card_number: 4515551623735607)
 
-    @transaction8 = @invoice_3.transactions.create!(result: 1, credit_card_number: 4203696133194408)
+    @transaction8 = @invoice3.transactions.create!(result: 1, credit_card_number: 4203696133194408)
 
-    @transaction9 = @invoice_4.transactions.create!(result: 1, credit_card_number: 4203696133194408)
-    @transaction10 = @invoice_4.transactions.create!(result: 1, credit_card_number: 4540842003561938)
+    @transaction9 = @invoice4.transactions.create!(result: 1, credit_card_number: 4203696133194408)
+    @transaction10 = @invoice4.transactions.create!(result: 1, credit_card_number: 4540842003561938)
 
-    @transaction11 = @invoice_5.transactions.create!(result: 1, credit_card_number: 4203696133194408)
-    @transaction12 = @invoice_5.transactions.create!(result: 1, credit_card_number: 4540842003561938)
-    @transaction13 = @invoice_5.transactions.create!(result: 1, credit_card_number: 4203696133194408)
+    @transaction11 = @invoice5.transactions.create!(result: 1, credit_card_number: 4203696133194408)
+    @transaction12 = @invoice5.transactions.create!(result: 1, credit_card_number: 4540842003561938)
+    @transaction13 = @invoice5.transactions.create!(result: 1, credit_card_number: 4203696133194408)
 
-    @transaction14 = @invoice_6.transactions.create!(result: 1, credit_card_number: 4540842003561938)
-    @transaction15 = @invoice_6.transactions.create!(result: 0, credit_card_number: 4540842003561938)
-    @transaction16 = @invoice_6.transactions.create!(result: 0, credit_card_number: 4540842003561938)
-    @transaction17 = @invoice_6.transactions.create!(result: 0, credit_card_number: 4540842003561938)
+    @transaction14 = @invoice6.transactions.create!(result: 1, credit_card_number: 4540842003561938)
+    @transaction15 = @invoice6.transactions.create!(result: 0, credit_card_number: 4540842003561938)
+    @transaction16 = @invoice6.transactions.create!(result: 0, credit_card_number: 4540842003561938)
+    @transaction17 = @invoice6.transactions.create!(result: 0, credit_card_number: 4540842003561938)
   end
 
   it 'shows the name of the merchant in question' do
@@ -86,31 +86,31 @@ RSpec.describe 'merchant dashboard' do
     visit "/merchants/#{@merchant.id}/dashboard"
 
     expect(page).to have_content("Favorite Customers:")
-    expect(page).to have_content("#{@customer_1.first_name} #{@customer_1.last_name}")
-    expect(page).to have_content("#{@customer_2.first_name} #{@customer_2.last_name}")
-    expect(page).to have_content("#{@customer_3.first_name} #{@customer_3.last_name}")
-    expect(page).to have_content("#{@customer_4.first_name} #{@customer_4.last_name}")
-    expect(page).to have_content("#{@customer_5.first_name} #{@customer_5.last_name}")
-    expect(page).to have_no_content("#{@customer_6.first_name} #{@customer_6.last_name}")
+    expect(page).to have_content("#{@customer1.first_name} #{@customer1.last_name}")
+    expect(page).to have_content("#{@customer2.first_name} #{@customer2.last_name}")
+    expect(page).to have_content("#{@customer3.first_name} #{@customer3.last_name}")
+    expect(page).to have_content("#{@customer4.first_name} #{@customer4.last_name}")
+    expect(page).to have_content("#{@customer5.first_name} #{@customer5.last_name}")
+    expect(page).to have_no_content("#{@customer6.first_name} #{@customer6.last_name}")
 
-    # within("##{@customer_1.id}") do
-      expect(page).to have_content(@customer_1.top_successful_transactions(@merchant.id))
+    # within("##{@customer1.id}") do
+      expect(page).to have_content(@customer1.top_successful_transactions(@merchant.id))
     # end
 
-    # within("##{@customer_2.id}") do
-      expect(page).to have_content(@customer_2.top_successful_transactions(@merchant.id))
+    # within("##{@customer2.id}") do
+      expect(page).to have_content(@customer2.top_successful_transactions(@merchant.id))
     # end
 
-    # within("##{@customer_3.id}") do
-      expect(page).to have_content(@customer_3.top_successful_transactions(@merchant.id))
+    # within("##{@customer3.id}") do
+      expect(page).to have_content(@customer3.top_successful_transactions(@merchant.id))
     # end
 
-    # within("##{@customer_4.id}") do
-      expect(page).to have_content(@customer_4.top_successful_transactions(@merchant.id))
+    # within("##{@customer4.id}") do
+      expect(page).to have_content(@customer4.top_successful_transactions(@merchant.id))
     # end
 
-    # within("##{@customer_5.id}") do
-      expect(page).to have_content(@customer_5.top_successful_transactions(@merchant.id))
+    # within("##{@customer5.id}") do
+      expect(page).to have_content(@customer5.top_successful_transactions(@merchant.id))
     # end
   end
 
@@ -118,26 +118,26 @@ RSpec.describe 'merchant dashboard' do
     visit "/merchants/#{@merchant.id}/dashboard"
 
     expect(page).to have_content("Items Ready To Ship:")
-    expect(page).to have_content("#{@invoice_item_2.item_name}")
-    expect(page).to have_content("#{@invoice_item_6.item_name}")
-    expect(page).to have_content("#{@invoice_item_8.item_name}")
-    expect(page).to have_content("#{@invoice_item_9.item_name}")
-    expect(page).to have_content("#{@invoice_item_11.item_name}")
+    expect(page).to have_content("#{@invoice_item2.item_name}")
+    expect(page).to have_content("#{@invoice_item6.item_name}")
+    expect(page).to have_content("#{@invoice_item8.item_name}")
+    expect(page).to have_content("#{@invoice_item9.item_name}")
+    expect(page).to have_content("#{@invoice_item11.item_name}")
 
   end
 
   it 'does not show items that have been shipped' do
     visit "/merchants/#{@merchant.id}/dashboard"
 
-    expect(page).to have_no_content("#{@invoice_item_4.item_name}")
+    expect(page).to have_no_content("#{@invoice_item4.item_name}")
 
   end
 
   it 'has the invoice id of the item listed and it is a link to the invoice show page' do
     visit "/merchants/#{@merchant.id}/dashboard"
 
-    expect(page).to have_link("#{@invoice_1.id}", href: "/merchants/#{@merchant.id}/invoices/#{@invoice_1.id}")
-    expect(page).to have_no_link("#{@invoice_6.id}")
+    expect(page).to have_link("#{@invoice1.id}", href: "/merchants/#{@merchant.id}/invoices/#{@invoice1.id}")
+    expect(page).to have_no_link("#{@invoice6.id}")
   end
 
   it 'has the creation date listed by each invoice in oldest to newest order' do
@@ -151,7 +151,7 @@ RSpec.describe 'merchant dashboard' do
 
       expect(page).to have_no_content("Thursday, June 03, 2021")
 
-    expect("#{@invoice_4.convert_create_date}").to appear_before("#{@invoice_1.convert_create_date}")
-    expect("#{@invoice_1.convert_create_date}").to appear_before("#{@invoice_3.convert_create_date}")
+    expect("#{@invoice4.convert_create_date}").to appear_before("#{@invoice1.convert_create_date}")
+    expect("#{@invoice1.convert_create_date}").to appear_before("#{@invoice3.convert_create_date}")
   end
 end
