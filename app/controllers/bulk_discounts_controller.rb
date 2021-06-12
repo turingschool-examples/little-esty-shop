9e6c23627c540196a8d43203019cd65cc46def2c
Leigh <lcpulzone@gmail.com>
2021-06-12 16:27:58 -0700

    Add discount views

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,6 +1,7 @@
 class BulkDiscountsController < ApplicationController
   def index
-
+    @merchant = Merchant.find(params[:merchant_id])
+    @discounts = @merchant.bulk_discounts
   end
 
   def show
