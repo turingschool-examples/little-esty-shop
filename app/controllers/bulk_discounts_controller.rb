ecd7f8a732cc49ec7a7e93e8de901e353884cc11
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-06-13 14:45:31 -0400

    Add feature tests for new page, controller methods, and new view

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,4 +6,21 @@ class BulkDiscountsController < ApplicationController
   def show
     @bulk_discount = BulkDiscount.find(params[:id])
   end
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount= BulkDiscount.new({
+      percentage: params[:percentage],
+      quantity_threshold: params[:quantity_threshold],
+      merchant_id: @merchant.id
+    })
+    if @bulk_discount.save
+      redirect_to "/merchants/#{@merchant.id}/bulk_discounts"
+    else
+      redirect_to "/merchants/#{@merchant.id}/bulk_discounts/new"
+      flash[:notice] = "Please fill in valid information."
+    end
+  end
 end
