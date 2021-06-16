14016d759a4c145b0045ce6e4791d38360f3b379
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-06-16 13:12:23 -0400

    temp remove github api for more efficient presentation time

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,34 +1,34 @@
-class GithubService
-  attr_reader :name,
-              :commits_and_names,
-              :pulls
-
-  def initialize
-    @name ||= repo_name
-    @commits_and_names ||= repo_commits_and_names
-    @pulls ||= repo_pull_requests
-  end
-  
-  def repo_name
-    response = Faraday.get 'https://api.github.com/repos/jrwhitmer/little-esty-shop'
-    parsed = JSON.parse(response.body, symbolize_names: true)
-    parsed[:name]
-  end
-
-  def repo_commits_and_names
-    response = Faraday.get 'https://api.github.com/repos/jrwhitmer/little-esty-shop/stats/contributors'
-    parsed = JSON.parse(response.body, symbolize_names: true)
-
-    parsed.each_with_object({}) do |login, total|
-      total[login[:author][:login]] = login[:total]
-    end
-  end
-
-  def repo_pull_requests
-    response = Faraday.get 'https://api.github.com/repos/jrwhitmer/little-esty-shop/pulls?state=all'
-    JSON.parse(response.body, symbolize_names: true).count
-  end
-end
+# class GithubService
+#   attr_reader :name,
+#               :commits_and_names,
+#               :pulls
+#
+#   def initialize
+#     @name ||= repo_name
+#     @commits_and_names ||= repo_commits_and_names
+#     @pulls ||= repo_pull_requests
+#   end
+#
+#   def repo_name
+#     response = Faraday.get 'https://api.github.com/repos/jrwhitmer/little-esty-shop'
+#     parsed = JSON.parse(response.body, symbolize_names: true)
+#     parsed[:name]
+#   end
+#
+#   def repo_commits_and_names
+#     response = Faraday.get 'https://api.github.com/repos/jrwhitmer/little-esty-shop/stats/contributors'
+#     parsed = JSON.parse(response.body, symbolize_names: true)
+#
+#     parsed.each_with_object({}) do |login, total|
+#       total[login[:author][:login]] = login[:total]
+#     end
+#   end
+#
+#   def repo_pull_requests
+#     response = Faraday.get 'https://api.github.com/repos/jrwhitmer/little-esty-shop/pulls?state=all'
+#     JSON.parse(response.body, symbolize_names: true).count
+#   end
+# end
 
 # GitHub API: Repo Name
 
