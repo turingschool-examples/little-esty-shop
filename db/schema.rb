2495dfc8f5fe61af758fc6004af659b80611ca97
Amanda McGuire <aruwart001@gmail.com>
2021-07-26 20:56:23 -0600

    Update columns to strings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_004719) do
+ActiveRecord::Schema.define(version: 2021_07_27_022809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(version: 2021_07_27_004719) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.integer "credit_card_number"
-    t.datetime "credit_card_expiration_date"
+    t.string "credit_card_number"
+    t.string "credit_card_expiration_date"
     t.string "result"
     t.bigint "invoice_id"
     t.datetime "created_at", null: false
