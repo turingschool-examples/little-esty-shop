b618b86dc3879840dbd099b6d9d497ca180fa2db
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-27 16:44:49 -0600

    Add coverage

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it {should belong_to :merchant}
+  end
 end
