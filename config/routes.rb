c721caff61fd47b602f1b7d637374333f46c4d04
Brett Jones <bhjones4545@gmail.com>
2021-07-27 16:52:35 -0600

    Add resourses for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :merchants, shallow: true do
+    # get '/dashboard', to: 'dashboard#show', as: 'merchant_dashboard'
+    resources :dashboard
+  end
 end
