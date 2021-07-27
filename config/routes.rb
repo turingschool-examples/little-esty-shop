e36fd20b2dea0711c8a512be39a34b14661c7964
Elliot Olbright <elliotolbright@gmail.com>
2021-07-27 17:06:40 -0600

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
+  
+  resources :merchants, only: [:show, :index] do
+    resources :items, :invoices
+  end
+
+  namespace :admin do 
+    resources :merchants, :invoices
+  end 
+
 end
