dff1d3c8ff45bb7fb5861a09c3d9b5286d7cd81f
deebot10 <dee1jhill@gmail.com>
2021-07-27 15:55:22 -0600

    Pass test for Admin Story2

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -4,9 +4,18 @@ RSpec.describe 'Admin Index' do
 
   describe 'visit' do
     it 'Visits the Admin Dash' do
-      visit '/admin'  
+      visit admin_index_path  
 
       expect(page).to have_content('Admin Dashboard')
+    end
+  end
+
+  describe 'links' do
+    it 'has links to merchant/invoice index' do
+      visit admin_index_path
+
+      expect(page).to have_link('Admin Invoices')
+      expect(page).to have_link('Admin Merchants')
     end
   end
 end
