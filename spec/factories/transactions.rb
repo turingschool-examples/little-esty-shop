7595ed5aad288d1f9fcf2842c61dc2c559a92951
Matt Kragen <mattdkragen@gmail.com>
2021-07-27 16:11:35 -0700

    Write factory for transaction

---
+++ spec/factories/transactions.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :transaction do
+    credit_card_number { Faker::Finance.credit_card(:mastercard) }
+    credit_card_expiration_date { Faker::String.random(length: 5) }
+    result { 'In Progress' }
+    invoice_id
+  end
+end
