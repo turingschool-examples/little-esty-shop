415971aa249e3ed1111ed700ad864c738bac87ed
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-27 17:13:04 -0600

    Add many to many relationship

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it {should belong_to :customer}
+    it {should have_many :transactions}
+    it {should have_many :invoice_items}
+    it {should have_many(:items).through(:invoice_items)}
+  end
 end
