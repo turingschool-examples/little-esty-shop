a3803546a7aedc32292bdfcd149f4e9a42a21245
Matt Kragen <mattdkragen@gmail.com>
2021-07-27 16:48:43 -0700

    Fix typo

--- spec/factories/invoice_item.rb
+++ spec/factories/invoice_item.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :invoice_item do
     quantity { Faker::Number.decimal_part(digits: 2) }
     sequence(:unit_price) { |n| n + 150 }
-    status { Faker::Number.between(from: 0, to: 2) #=> 7 }
+    status { Faker::Number.between(from: 0, to: 2) }
     invoice
     item
   end
