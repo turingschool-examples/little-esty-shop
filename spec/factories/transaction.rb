d074406899c78bad42562d93a3b895d2b429308d
Matt Kragen <mattdkragen@gmail.com>
2021-07-27 17:12:21 -0700

    Fixed Faker setup to produce better exp dates

--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :transaction do
     credit_card_number { Faker::Finance.credit_card(:mastercard) }
-    credit_card_expiration_date { Faker::String.random(length: 5) }
+    credit_card_expiration_date { "#{Faker::Number.between(from: 1, to: 12)}/#{Faker::Number.between(from: 21, to: 25)}" }
     result { 'In Progress' }
     invoice
   end
