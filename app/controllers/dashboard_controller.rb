d237b1c5e30e2aaa05b6684b37922bffd2ec8753
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-28 16:49:59 -0700

    Add items not shipped to dashboard index view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
+    @items_not_shipped = @merchant.items.not_yet_shipped
   end
 end
