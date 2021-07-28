d824e6ab88c584477c85739b01c9c6ce58829795
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-28 15:55:31 -0600

    Change validation to length

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,8 +2,6 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
 
   validates_presence_of :credit_card_number
-
-  validates :credit_card_number, numericality: { only_integer: true }
-
+  validates :credit_card_number, numericality: { only_integer: true }, length: { is: 16 }
   validates :result, inclusion: { in: [ true, false ] }
 end
