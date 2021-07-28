df3c0be1a03a5739130eb5e3a57710b710c5bea1
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-28 13:59:30 -0400

    Update routes configuration with merchant dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 
   get '/', to: 'welcome#index'
-  
+
   resources :merchants, only: [:show, :index] do
-    resources :items, :invoices
+    resources :items
+    resources :invoices
+    resources :dashboard, only: [:index]
   end
 
-  namespace :admin do 
-    resources :merchants, :invoices
-  end 
+  namespace :admin do
+      resources :merchants
+    resources :invoices
+  end
 
 end
