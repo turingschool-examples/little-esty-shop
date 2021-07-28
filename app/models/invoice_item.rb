ee9de8c62441a3ceae55e502636380e4c8878d8e
Matt Toensing <matthew.toensing@gmail.com>
2021-07-28 11:06:05 -0700

    WIP need to drop db and reseed

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,5 +2,5 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
 
-  enum status: {pending: 0, packeged: 1, shipped: 2}
+  enum status: {pending: 0, packaged: 1, shipped: 2}
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Transaction do
   describe 'relationships' do
     it {should belong_to(:invoice)}
   end
-  
+
   describe 'validations' do
     it { should define_enum_for(:result).with([:success, :failed]) }
   end
