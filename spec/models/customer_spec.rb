cd21ddde1bcb7f745a2a21e0ecd374354fbf5d51
deebot10 <dee1jhill@gmail.com>
2021-07-27 20:42:16 -0600

    Update relationships

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Customer do
   describe 'relationships' do
     it {should have_many :invoices}
+    it {should have_many(:transactions).through(:invoices)}
   end
 
   describe 'class methods' do
