779ec7ec68f94aa5568d436d8410b1bb901e6e59
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-28 11:51:38 -0600

    Add missing t to  description

--- db/migrate/20210728023432_create_items.rb
+++ db/migrate/20210728023432_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name
-      t.string :descripion
+      t.string :description
       t.integer :unit_price
       t.references :merchant, foreign_key: true
 
