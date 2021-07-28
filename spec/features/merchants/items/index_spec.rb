7a8e6bb84ca47d9b5609a40999c41bee07b766c8
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-27 19:29:00 -0700

    Create index show page tests to display item attributes

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe 'Merchants Items Index page' do
     visit "/merchants/#{@merchant_1.id}/items"
   end
 
-  it "Lists the names of all this merchants items" do
+  it "lists the names of all this merchants items" do
     expect(page).to have_content(Item.first.name)
     expect(page).to have_content(Item.second.name)
     expect(page).to have_content(Item.third.name)
   end
 
-  it "Does not have items from other merchants" do
+  it "does not have items from other merchants" do
     merchant_2 = create(:merchant)
     items = create(:item, name: "Please Not This", merchant_id: merchant_2.id)
 
---
+++ spec/features/merchants/items/show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants Items Show page' do
+  before(:each) do
+    @merchant_1 = create(:merchant)
+    @item = create(:item, merchant_id: @merchant_1.id)
+
+    visit "/merchants/#{@merchant_1.id}/items/#{item.id}"
+  end
+
+  it "lists all the items attriutes" do
+    expect(page).to have_content(item.name)
+    expect(page).to have_content(item.description)
+    expect(page).to have_content(item.unit_price)
+  end
+end
