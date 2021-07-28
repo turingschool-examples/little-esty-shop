fb118ee3867517d4a1e9dc548823be991df49e01
Matt Toensing <matthew.toensing@gmail.com>
2021-07-28 11:42:58 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :admin, only: [:index]
-  
+
   namespace :admin do
-    resources :merchants, only: [:index] 
+    resources :merchants 
   end
 end
