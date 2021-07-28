f00a9a3c21fac501583f5d9e0639b2d1cd977364
Amanda McGuire <aruwart001@gmail.com>
2021-07-28 10:34:59 -0600

    Add status to item database and add destroy dependents

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,7 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
-  has_many :invoice_items
+  has_many :invoice_items, :dependent => :destroy
   has_many :items, through: :invoice_items
-  has_many :transactions
+  has_many :transactions, :dependent => :destroy
   enum status: [ 'in progress', :completed, :cancelled ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_022809) do
+ActiveRecord::Schema.define(version: 2021_07_28_161452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_07_27_022809) do
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "disabled"
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
