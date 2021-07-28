d6ef628371df423d280d957d4c1f658eb450ce25
Brett Jones <bhjones4545@gmail.com>
2021-07-27 22:03:29 -0600

    Add relationships to models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,6 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :invoice_items
   has_many :items, through: :invoice_items
+  has_many :transactions
+  has_many :merchants, through: :items
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,5 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+  has_many :customers, through: :invoice
   enum result: { success: 0, failed: 1 }
 end
