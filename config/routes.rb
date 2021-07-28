35df0e29a16bb78ef3ece0af4611153b00623726
danembb <dbrophy720@gmail.com>
2021-07-28 16:44:40 -0600

    Add items index link to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/merchants/:id/dashboard', to: 'merchants#dashboard'
+  get '/merchants/:id/items', to: 'merchant_items#index'
 end
--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -18,5 +18,15 @@ RSpec.describe 'merchant dashboard page' do
       expect(page).to have_content(@merchant1.name)
       expect(page).to_not have_content(@merchant2.name)
     end
+
+    it 'link to items' do
+      visit "/merchants/#{@merchant1.id}/dashboard"
+
+      expect(page).to have_content('My Items')
+
+      click_on 'My Items'
+      save_and_open_page
+      expect(current_path).to eq("/merchants/#{@merchant1.id}/items")
+    end
   end
 end
