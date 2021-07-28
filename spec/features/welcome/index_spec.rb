aefb6cb716c324c2cddff56e78c3d372131acb9d
Elliot Olbright <elliotolbright@gmail.com>
2021-07-28 10:57:10 -0600

    fix ambigous match test for link

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -10,28 +10,27 @@ RSpec.describe 'Welcome Page' do
     expect(page).to have_content('Welcome to our project!')
   end
 
-  it 'can take user to merchans index page' do 
-    click_link 'Merchants' 
+  it 'can take user to merchans index page' do
+    first(:link, 'Merchants').click
 
     expect(current_path).to eq('/merchants')
   end
 
-  it 'can take user to admin merchants index page' do 
-    click_link 'Admin' 
-    click_link 'AdminMerchants' 
+  it 'can take user to admin merchants index page' do
+    within("ul#dropdownmenu") do
+      click_link 'Merchants' 
 
-    expect(current_path).to eq('/admin/merchants')
+      expect(current_path).to eq('/admin/merchants')
+    end 
   end
 
-  it 'can take user to admin invoices index page' do 
-    click_link 'Admin' 
-    click_link 'AdminInvoices' 
+  it 'can take user to admin invoices index page' do
+    click_link 'Invoices' 
 
     expect(current_path).to eq('/admin/invoices')
   end
 
   it 'can take user to admin feeling lucky index page' do 
-    click_link 'Admin' 
     click_link 'Feeling Lucky 😎' 
 
     expect(current_path).to eq('/')
