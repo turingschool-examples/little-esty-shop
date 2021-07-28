bc0fd165dee9a7c7014af71bb755da314e13ed24
Matt Toensing <matthew.toensing@gmail.com>
2021-07-28 11:43:38 -0700

    Add show action

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,5 +1,9 @@
 class Admin::MerchantsController < ApplicationController
   def index
-    @merchants = Merchant.all  
-  end  
+    @merchants = Merchant.all
+  end
+
+  def show
+    @merchant = Merchant.find(params[:id])
+  end
 end
