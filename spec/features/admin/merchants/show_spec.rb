fcda5f25a86e3edf0ff12f1ff0a96d7881b26367
Matt Toensing <matthew.toensing@gmail.com>
2021-07-28 15:31:08 -0700

    Add show tests for admin merchant

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -3,16 +3,13 @@ require 'rails_helper'
 RSpec.describe 'admin merchant show page' do
   before(:each) do
     @merchant = create(:merchant)
-  end
-  describe '' do
-    it '' do
-#       Admin Merchant Show
-#
-# As an admin,
-# When I click on the name of a merchant from the admin merchants index page,
-# Then I am taken to that merchant's admin show page (/admin/merchants/merchant_id)
-# And I see the name of that merchant
 
+    visit admin_merchant_path(@merchant.id)
+  end
+
+  describe 'contents' do
+    it 'displays the name of the merchant' do
+      expect(page).to have_content(@merchant.name)
     end
   end
 end
