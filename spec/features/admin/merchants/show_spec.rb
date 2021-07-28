9d5e591b60ce6fc20f3afffd3c1cca99eeebf029
Matt Toensing <matthew.toensing@gmail.com>
2021-07-28 11:42:53 -0700

    Create file and write first test for page contents

---
+++ spec/features/admin/merchants/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'admin merchant show page' do
+  before(:each) do
+    @merchant = create(:merchant)
+  end
+  describe '' do
+    it '' do
+#       Admin Merchant Show
+#
+# As an admin,
+# When I click on the name of a merchant from the admin merchants index page,
+# Then I am taken to that merchant's admin show page (/admin/merchants/merchant_id)
+# And I see the name of that merchant
+
+    end
+  end
+end
