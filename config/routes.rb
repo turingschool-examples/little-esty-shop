aee7bb596fa51d878030363cb4ff82fa812daf95
Gunnar <dunkcrunk.com>
2021-07-28 20:22:18 -0600

    Added admin namespace along with resource paths for merchants and invoices.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+  # Admin routes
+  namespace :admin do
+    resources :merchants
+    resources :invoices
+  end
 end
