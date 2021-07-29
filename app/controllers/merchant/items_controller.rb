77c3472fe6aee0682a986a840b9d876921fdaf8b
Michael Abbott <michael@mjabbottdesign.com>
2021-07-28 21:41:42 -0700

    Complete Merchant Item Update feature; add flash message render

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,21 +1,32 @@
 class Merchant::ItemsController < ApplicationController
   def index
-    @merchant_items = Item.all    #helper method for item specific to merchant
+    @merchant = Merchant.find(params[:merchant_id])
+    @merchant_items = @merchant.merchant_items
   end
 
   def show
-    #merchant specific item
+    @item = Item.find(params[:id])
   end
 
   def new
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def create
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to merchant_item_path(item.merchant_id, item.id), notice: "Item successfully updated."
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price)
   end
 end
