8a5e750f149b4e25edd1f328935b4787e4ce1010
Elliot Olbright <elliotolbright@gmail.com>
2021-07-28 18:15:11 -0600

    add merchant item idex MVC

--- app/controllers/invoices_controller.rb
+++
@@ -1,5 +0,0 @@
-class InvoicesController < ApplicationController 
-  def index 
-    @merchant = Merchant.find(params[:merchant_id])
-  end 
-end 
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe Merchant do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
+
+  describe 'Methods' do 
+    before :each do 
+      @merchant = Merchant.create!(name: 'Tom Holland')
+      
+      @item1 = Item.create!(name: 'spider suit', description: 'saves lives', unit_price: '10000', merchant_id: @merchant.id)
+      @item2 = Item.create!(name: 'web shooter', description: 'shoots webs', unit_price: '5000', merchant_id: @merchant.id)
+      @item3 = Item.create!(name: 'upside down kiss', description: 'That Mary jane Swag', unit_price: '15000', merchant_id: @merchant.id)
+    end 
+    describe '::merchant_items' do 
+      it 'returns all of the merchants items' do 
+        expect(@merchant.merchant_items).to eq([@item1, @item2, @item3])
+      end 
+    end
+  end 
+
 end
