3b0594370d9589f3ec5286f223aa55781061ba7f
Taylor Varoglu <tava8398@colorado.edu>
2021-07-29 17:58:04 -0600

    Refactor Item#items_ready_to_ship_by_ordered_date

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,13 +8,13 @@ class Item < ApplicationRecord
   validates :unit_price, presence: true, numericality: { only_integer: true }
 
 
-  def self.items_ready_to_ship(merchant_id)
-    select("items.id, items.name, invoice_items.invoice_id AS invoice_id")
+  def self.items_ready_to_ship_by_ordered_date(merchant_id)
+    select("items.id, items.name, invoice_items.invoice_id AS invoice_id, invoice_items.created_at AS ordered_date")
       .joins(:invoice_items)
       .where("invoice_items.status <> 2")
       .where("items.merchant_id = ?", merchant_id)
       .distinct
-      .order(:id)
+      .order("ordered_date ASC")
   end
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,33 +16,35 @@ RSpec.describe Item do
   describe 'class methods' do
     before :each do
       @merchant = Merchant.create!(name: 'Lydia Rodarte-Quayle')
-      @item = Item.create!(name: 'P2P', description: 'secret...', unit_price: 1000, merchant_id: @merchant.id)
+      @item_1 = Item.create!(name: 'P2P', description: 'secret...', unit_price: 1000, merchant_id: @merchant.id)
+      @item_2 = Item.create!(name: '10 Gallon Drum', description: 'for storage', unit_price: 100, merchant_id: @merchant.id)
+      @item_3 = Item.create!(name: 'Dolley', description: 'for transportation', unit_price: 10, merchant_id: @merchant.id)
 
       @customer_1 = Customer.create!(first_name: 'Tuco', last_name: 'Salamanca')
       @customer_1.invoices.create!(status: 2)
-      InvoiceItem.create!(invoice: @customer_1.invoices[0], item: @item, quantity: 1, unit_price: @item.unit_price, status: 1)
+      InvoiceItem.create!(invoice: @customer_1.invoices[0], item: @item_1, quantity: 1, unit_price: @item_1.unit_price, status: 1)
       @customer_1.invoices[0].transactions.create!(credit_card_number: '1234', credit_card_expiration_date: '', result: 0)
       @customer_1.invoices.create!(status: 2)
-      InvoiceItem.create!(invoice: @customer_1.invoices[1], item: @item, quantity: 1, unit_price: @item.unit_price, status: 1)
+      InvoiceItem.create!(invoice: @customer_1.invoices[1], item: @item_2, quantity: 1, unit_price: @item_2.unit_price, status: 1)
       @customer_1.invoices[1].transactions.create!(credit_card_number: '1234', credit_card_expiration_date: '', result: 0)
 
       @customer_2 = Customer.create!(first_name: 'Gustavo', last_name: 'Fring')
       @customer_2.invoices.create!(status: 2)
-      InvoiceItem.create!(invoice: @customer_2.invoices[0], item: @item, quantity: 1, unit_price: @item.unit_price, status: 1)
+      InvoiceItem.create!(invoice: @customer_2.invoices[0], item: @item_1, quantity: 2, unit_price: @item_1.unit_price, status: 1)
       @customer_2.invoices[0].transactions.create!(credit_card_number: '9012', credit_card_expiration_date: '', result: 0)
       @customer_2.invoices.create!(status: 2)
-      InvoiceItem.create!(invoice: @customer_2.invoices[1], item: @item, quantity: 1, unit_price: @item.unit_price, status: 2)
+      InvoiceItem.create!(invoice: @customer_2.invoices[1], item: @item_2, quantity: 4, unit_price: @item_2.unit_price, status: 2)
       @customer_2.invoices[1].transactions.create!(credit_card_number: '9012', credit_card_expiration_date: '', result: 0)
       @customer_2.invoices.create!(status: 2)
-      InvoiceItem.create!(invoice: @customer_2.invoices[2], item: @item, quantity: 1, unit_price: @item.unit_price, status: 1)
+      InvoiceItem.create!(invoice: @customer_2.invoices[2], item: @item_3, quantity: 1, unit_price: @item_3.unit_price, status: 1)
       @customer_2.invoices[2].transactions.create!(credit_card_number: '9012', credit_card_expiration_date: '', result: 0)
     end
 
-    it 'can return #items_ready_to_ship for a given merchant' do
-      expected = Item.items_ready_to_ship(@merchant.id)
+    it 'can return #items_ready_to_ship_by_ordered_date for a given merchant' do
+      expected = Item.items_ready_to_ship_by_ordered_date(@merchant.id)
 
       expect(expected.length).to eq(4)
-      expect(expected.all? { |item| item == @item } ).to be true
+      expect(expected.all? { |item| item.class == Item } ).to be true
       expect(expected[0].invoice_id).to eq(@customer_1.invoices[0].id)
       expect(expected[1].invoice_id).to eq(@customer_1.invoices[1].id)
       expect(expected[2].invoice_id).to eq(@customer_2.invoices[0].id)
