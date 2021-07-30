743e80470b47c6bab0f5df877a140931ec32ac76
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-30 16:31:53 -0600

    Add test for presence and use of update link

--- spec/features/merchant_items/show_spec.rb
+++ spec/features/merchant_items/show_spec.rb
@@ -13,5 +13,19 @@ RSpec.describe 'the merchant items show' do
       expect(page).to have_content('A large quantity of whole milk')
       expect(page).to have_content('500')
     end
+
+    it 'has an update link' do
+      visit merchant_item_path(@merchant1, @item1)
+
+      expect(page).to have_link("Update #{@item1.name}")
+    end
+
+    it 'can click update link and be taken to update page' do
+      visit merchant_item_path(@merchant1, @item1)
+
+      click_link("Update #{@item1.name}")
+
+      expect(current_path).to eq(edit_merchant_item_path(@merchant1, @item1))
+    end
   end
 end
