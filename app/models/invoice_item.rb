f939562fb853a20a78b09c1f4f41eb3c61be311a
Brett Jones <bhjones4545@gmail.com>
2021-07-30 16:29:32 -0600

    Add total revenue method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,4 +2,8 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
   enum status: {pending: 0, packaged: 1, shipped: 2}
+
+  def self.total_revenue
+    sum("invoice_items.unit_price * invoice_items.quantity")
+  end
 end
