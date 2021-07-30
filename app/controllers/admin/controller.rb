4b79a05f8301679d83f4ba0cbf14221c20db9665
Jacob Piland <piland.jacob@gmail.com>
2021-07-29 22:07:51 -0500

    reconfigure controller and view setup to add admin module

---
+++ app/controllers/admin/controller.rb
@@ -0,0 +1,5 @@
+class Admin::Controller < ApplicationController
+
+  def index
+  end
+end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
   has_many :invoices
+
+  def self.admin_top_five_customers
+    Customer.select("customers.*, count(transactions.invoice_id) as transaction_count")
+    .joins(invoices: :transactions)
+    .where('transactions.result = 0')
+    .group(:id)
+    .order('transaction_count DESC')
+    .limit(5)
+  end
 end
