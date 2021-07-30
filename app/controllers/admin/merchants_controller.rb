31b2b6ab5e398b222b101e15a9cca6a1257f9550
sami-p <sami.peterson14@gmail.com>
2021-07-30 17:02:38 -0500

    build update admin merchant functionality

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -7,4 +7,20 @@ class Admin:: MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    merchant.update(merchant_params)
+    redirect_to admin_merchant_path(merchant)
+    flash[:alert] = "YOUR MERCHANT HAS BEEN UPDATED."
+  end
+
+  private
+  def merchant_params
+    params.permit(:name)
+  end
 end
