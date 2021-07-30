bbaea5f6486cf172f043fa877177d6c9e7646582
Taylor Varoglu <tava8398@colorado.edu>
2021-07-30 15:37:13 -0600

    Fix failed tests that resulted from merge conflict

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -2,12 +2,8 @@ class Merchant::DashboardController < ApplicationController
 
   def index
     @merchant = Merchant.find(params[:merchant_id])
-<<<<<<< HEAD
     @top_customers = Customer.top_customers_for_merchant(@merchant.id)
     @pending_items = Item.items_ready_to_ship_by_ordered_date(@merchant.id)
   end
 
-=======
-  end
->>>>>>> f29153cf2cbeb386fd0aeb10ebf7f3968d87e0e8
 end
