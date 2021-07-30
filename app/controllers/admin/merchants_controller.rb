8644a8161ac3bf03ae7f38646db6214c5992cfc1
Elliot Olbright <elliotolbright@gmail.com>
2021-07-30 15:47:36 -0600

    add merchant edit story finished

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -12,12 +12,22 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def edit
+    @merchant = Merchant.find(params[:id])
   end
 
   def create
   end
 
   def update
+    merchant = Merchant.find(params[:id])
+    merchant.update(merchant_params)
+    redirect_to admin_merchant_path(merchant.id), notice: "Item successfully updated."
+  end
+
+  private 
+
+  def merchant_params 
+    params.require(:merchant).permit(:name)
   end
 
 end
