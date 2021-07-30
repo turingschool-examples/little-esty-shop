214fbe705ef3151a09a9da464ce9781794e98e2b
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-30 16:09:20 -0600

    Test that there is a link to indiv items

--- spec/features/merchant_items/index_spec.rb
+++ spec/features/merchant_items/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'the mechant items index' do
+RSpec.describe 'the merchant items index' do
   describe 'display' do
     it 'visit' do
       visit merchant_items_path(@merchant1)
@@ -15,6 +15,17 @@ RSpec.describe 'the mechant items index' do
       expect(page).to have_content('Hot Dog')
       expect(page).to have_content('Steaks')
       expect(page).to_not have_content('Frying Pan')
+    end
+
+    it 'can click on item link and be taken to its show page' do
+      visit merchant_items_path(@merchant1)
+
+      click_link('Milk')
+
+      expect(current_path).to eq(merchant_item_path(@merchant1, @item1))
+      expect(page).to have_content('Milk')
+      expect(page).to have_content('A large quantity of whole milk')
+      expect(page).to have_content('500')
     end
   end
 end
