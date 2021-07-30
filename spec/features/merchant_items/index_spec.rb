ed2035a244c195d5cdb6a13a67b0e3ba83823f08
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-30 15:44:43 -0600

    Add test for having content, complete user story

--- spec/features/merchant_items/index_spec.rb
+++ spec/features/merchant_items/index_spec.rb
@@ -4,7 +4,17 @@ RSpec.describe 'the mechant items index' do
   describe 'display' do
     it 'visit' do
       visit merchant_items_path(@merchant1)
-      save_and_open_page
+    end
+
+    it 'displays merchant name and items' do
+      visit merchant_items_path(@merchant1)
+
+      expect(page).to have_content('Costco')
+      expect(page).to have_content('Milk')
+      expect(page).to have_content('Potato Chips')
+      expect(page).to have_content('Hot Dog')
+      expect(page).to have_content('Steaks')
+      expect(page).to_not have_content('Frying Pan')
     end
   end
 end
