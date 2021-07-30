b35091947733a92a647a32daf24c47811f4ec62e
Gunnar <dunkcrunk.com>
2021-07-30 15:13:38 -0600

    Made a admin merchant index page through TDD. Made a test with within blocks and all tests pass.

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -5,12 +5,32 @@ require 'rails_helper'
    end
    it 'can show the names of the of all the merchants' do
      
-    expect(page).to have_content(@merchant1.name)
-    expect(page).to have_content(@merchant2.name)
-    expect(page).to have_content(@merchant3.name)
-    expect(page).to have_content(@merchant4.name)
-    expect(page).to have_content(@merchant5.name)
-    expect(page).to have_content(@merchant6.name)
-    expect(page).to have_content(@merchant7.name)
+    within "#all-admin-merchants-#{@merchant1.id}" do
+      expect(page).to have_content(@merchant1.name)
+    end
+
+    within "#all-admin-merchants-#{@merchant2.id}" do
+      expect(page).to have_content(@merchant2.name)
+    end
+
+    within "#all-admin-merchants-#{@merchant3.id}" do
+      expect(page).to have_content(@merchant3.name)
+    end
+
+    within "#all-admin-merchants-#{@merchant4.id}" do
+      expect(page).to have_content(@merchant4.name)
+    end
+
+    within "#all-admin-merchants-#{@merchant5.id}" do
+      expect(page).to have_content(@merchant5.name)
+    end
+
+    within "#all-admin-merchants-#{@merchant6.id}" do
+      expect(page).to have_content(@merchant6.name)
+    end
+
+    within "#all-admin-merchants-#{@merchant7.id}" do
+      expect(page).to have_content(@merchant7.name)
+    end
    end
  end
