a0934346d8e9c86a568720192fc325b3284b8ada
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-30 09:59:15 -0600

    Create file and setup rspec structure

---
+++ spec/features/merchants/invoices/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'the merchant invoices index' do
+  before(:each) do
+    @merchant = Merchant.create!(name: 'Bilbo')
+  end
+
+  describe 'display' do
+    it 'visit' do
+    end
+  end
+end
