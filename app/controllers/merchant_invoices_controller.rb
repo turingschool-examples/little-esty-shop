d8503fab4b17db2b8807cf6fdce7059123ec53ce
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-31 11:08:53 -0600

    Build test and display for hyperlink on each invoice id to invoice show page

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -2,4 +2,9 @@ class MerchantInvoicesController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoice = Invoice.find(params[:id])
+  end
 end
--- spec/features/merchant_invoices/index_spec.rb
+++ spec/features/merchant_invoices/index_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'the merchant invoices index' do
       visit merchant_invoices_path(@merchant1.id)
 
       within('#header') do
+        expect(page).to have_content(@merchant1.name)
         expect(page).to have_content('My Invoices')
       end
     end
@@ -50,6 +51,37 @@ RSpec.describe 'the merchant invoices index' do
       expect(page).to_not have_content(@invoice10.id)
       expect(page).to_not have_content(@invoice11.id)
       expect(page).to_not have_content(@invoice12.id)
+    end
+  end
+
+  describe 'hyperlinks' do
+    it 'links to the show page for each invoice' do
+      visit merchant_invoices_path(@merchant1.id)
+
+      within("#invoice-#{@invoice1.id}") do
+        expect(page).to have_link("#{@invoice1.id}")
+        click_link("#{@invoice1.id}")
+
+        expect(page).to have_current_path(merchant_invoice_path(@merchant1.id, @invoice1.id))
+      end
+
+      visit merchant_invoices_path(@merchant1.id)
+
+      within("#invoice-#{@invoice2.id}") do
+        expect(page).to have_link("#{@invoice2.id}")
+        click_link("#{@invoice2.id}")
+
+        expect(page).to have_current_path(merchant_invoice_path(@merchant1.id, @invoice2.id))
+      end
+
+      visit merchant_invoices_path(@merchant1.id)
+
+      within("#invoice-#{@invoice14.id}") do
+        expect(page).to have_link("#{@invoice14.id}")
+        click_link("#{@invoice14.id}")
+
+        expect(page).to have_current_path(merchant_invoice_path(@merchant1.id, @invoice14.id))
+      end
     end
   end
 end
