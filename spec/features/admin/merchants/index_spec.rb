947d029cb1fdf17f371552e2e5ddeb9a3d81f14a
Gunnar <dunkcrunk.com>
2021-07-31 12:47:33 -0600

    Made passing feature tests that show that the index page is cut into sections of enabled and disabled merchants but shows all merchants on the page.

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -4,33 +4,46 @@ require 'rails_helper'
      visit "/admin/merchants"
    end
    it 'can show the names of the of all the merchants' do
-     
-    within "#all-admin-merchants-#{@merchant1.id}" do
-      expect(page).to have_content(@merchant1.name)
-    end
-
-    within "#all-admin-merchants-#{@merchant2.id}" do
-      expect(page).to have_content(@merchant2.name)
-    end
-
-    within "#all-admin-merchants-#{@merchant3.id}" do
-      expect(page).to have_content(@merchant3.name)
-    end
-
-    within "#all-admin-merchants-#{@merchant4.id}" do
-      expect(page).to have_content(@merchant4.name)
-    end
-
-    within "#all-admin-merchants-#{@merchant5.id}" do
-      expect(page).to have_content(@merchant5.name)
-    end
-
-    within "#all-admin-merchants-#{@merchant6.id}" do
-      expect(page).to have_content(@merchant6.name)
-    end
-
-    within "#all-admin-merchants-#{@merchant7.id}" do
-      expect(page).to have_content(@merchant7.name)
-    end
+     expect(page).to have_content(@merchant1.name)
+     expect(page).to have_content(@merchant2.name)
+     expect(page).to have_content(@merchant3.name)
+     expect(page).to have_content(@merchant4.name)
+     expect(page).to have_content(@merchant5.name)
+     expect(page).to have_content(@merchant6.name)
+     expect(page).to have_content(@merchant7.name)
    end
- end
+
+   it 'classifies the merchant names in labeled sections, shows the enabled merchants section' do
+
+    within "#enabled-admin-merchants-#{@merchant5.id}" do
+        expect(page).to have_content(@merchant5.name)
+      end
+
+      within "#enabled-admin-merchants-#{@merchant6.id}" do
+        expect(page).to have_content(@merchant6.name)
+      end
+   end
+
+   it 'classifies the merchant names in labeled sections, shows the disabled merchants section' do
+
+      within "#disabled-admin-merchants-#{@merchant1.id}" do
+        expect(page).to have_content(@merchant1.name)
+      end
+
+      within "#disabled-admin-merchants-#{@merchant2.id}" do
+        expect(page).to have_content(@merchant2.name)
+      end
+
+      within "#disabled-admin-merchants-#{@merchant3.id}" do
+        expect(page).to have_content(@merchant3.name)
+      end
+
+      within "#disabled-admin-merchants-#{@merchant4.id}" do
+        expect(page).to have_content(@merchant4.name)
+      end
+
+      within "#disabled-admin-merchants-#{@merchant7.id}" do
+        expect(page).to have_content(@merchant7.name)
+      end
+   end
+  end
