3af2deb901db1c199064232d95cc8c516ecbb81e
Taylor Varoglu <tava8398@colorado.edu>
2021-07-31 15:19:35 -0600

    Refactor Item#items_ready_to_ship_by_ordered date to run with or without arg

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,16 +8,16 @@ class Item < ApplicationRecord
   validates :unit_price, presence: true, numericality: { only_integer: true }
 
 
-  def self.items_ready_to_ship_by_ordered_date(merchant_id)
-    select("items.id, items.name, invoice_items.invoice_id AS invoice_id, invoices.status,
+  def self.items_ready_to_ship_by_ordered_date(merchant_id = nil)
+    query = select("items.id, items.name, invoice_items.invoice_id AS invoice_id, invoices.status,
       invoices.created_at AS invoiced_date")
       .joins("INNER JOIN invoice_items ON items.id = invoice_items.item_id")
       .joins("INNER JOIN invoices ON invoice_items.invoice_id = invoices.id")
       .where("invoice_items.status <> 2")
       .where("invoices.status <> 0")
-      .where("items.merchant_id = ?", merchant_id)
       .order("invoiced_date ASC")
       .distinct
+    merchant_id == nil ? query : query.where("items.merchant_id = ?", merchant_id)
   end
 
 end
