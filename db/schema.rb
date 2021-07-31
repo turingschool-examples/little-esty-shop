7257549e1acf25b6cdf779b8d0501656c01c8bc1
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-31 12:50:18 -0600

    Add new status column to item table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_025545) do
+ActiveRecord::Schema.define(version: 2021_07_31_024150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_07_28_025545) do
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "Disabled"
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
