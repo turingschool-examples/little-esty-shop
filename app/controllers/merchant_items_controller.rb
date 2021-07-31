4ce79cd1ec45857b1c590f5990d1937286e36c11
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-30 20:15:42 -0600

    Add update action, strong params, add to edit

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -9,6 +9,21 @@ class MerchantItemsController < ApplicationController
   end
 
   def edit
-    
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
+    @item.update!(item_params)
+    flash[:update_status] = 'Item was successfully updated!'
+    redirect_to merchant_item_path(@merchant, @item)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price)
   end
 end
