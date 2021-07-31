4b6e18233d455704fc8f74698924591174ed83ee
Antonio King <antonio.j.king@gmail.com>
2021-07-30 22:07:07 -0600

    Update admin inv show test and add to show method

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -5,5 +5,6 @@ class Admin::InvoicesController < ApplicationController
 
   def show
     @invoice = Invoice.find(params[:id])
+    @customer = Customer.find(@invoice.customer_id)
   end
 end
--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -9,18 +9,18 @@ RSpec.describe 'Admin Invoice Show Page' do
   # - Invoice created_at date in the format "Monday, July 18, 2019"
   # - Customer first and last name
   it 'shows an invoice with attributes' do
-    visit admin_invoice_path("#{@invoice1}")
+    visit admin_invoice_path("#{@invoice1.id}")
 
     expect(page).to have_content("#{@invoice1.id}")
     expect(page).to have_content("#{@invoice1.status}")
-    expect(page).to have_content("#{@invoice1.created_at}")
-    expect(page).to have_content("#{@invoice1.customer.first_name}")
-    expect(page).to have_content("#{@invoice1.customer.last_name}")
+    expect(page).to have_content('Created At: Sunday, March 25, 2012')
+    expect(page).to have_content("#{@customer1.first_name}")
+    expect(page).to have_content("#{@customer1.last_name}")
 
-    expect(page).to_not have_content("#{@invoice2.id}")
-    expect(page).to_not have_content("#{@invoice2.status}")
-    expect(page).to_not have_content("#{@invoice2.created_at}")
-    expect(page).to_not have_content("#{@invoice2.customer.first_name}")
-    expect(page).to_not have_content("#{@invoice2.customer.last_name}")
+    expect(page).to_not have_content("#{@invoice7.id}")
+    expect(page).to_not have_content("#{@invoice7.status}")
+    expect(page).to_not have_content('Created At: Wednesday, March 7, 2012')
+    expect(page).to_not have_content("#{@customer2.first_name}")
+    expect(page).to_not have_content("#{@customer2.last_name}")
   end
 end
