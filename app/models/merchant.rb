f21987b53ab959d4806d150c58c0f066db6fd581
Gunnar <dunkcrunk.com>
2021-07-31 11:41:00 -0600

    Made model methods that pass and can classify merchanst by their status of either enabled or disabled.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,12 @@ class Merchant < ApplicationRecord
   has_many :invoices, through: :items
 
   validates :name, presence: true
+
+  def self.enabled_merchants
+    where("status = ?", "enabled")
+  end
+
+  def self.disabled_merchants
+    where("status = ?", "disabled")
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,14 +10,19 @@ RSpec.describe Merchant, type: :model do
     it { should validate_presence_of(:name) }
   end
 
-  # before :each do
+  describe 'class methods' do
+   describe '.enabled_merchants' do
+      it 'can get all the merchants that are enabled' do
+        expect(Merchant.enabled_merchants).to eq([@merchant5, @merchant6])  
+      end
+    end
 
-  # end
-
-  # describe 'class methods' do
-  #  describe '.' do
-  #   end
-  # end
+    describe '.disabled_merchants' do
+      it 'can get all the merchants that are disabled' do
+        expect(Merchant.disabled_merchants).to eq([@merchant1, @merchant2, @merchant3, @merchant4, @merchant7])  
+      end
+    end
+  end
 
   # describe 'instance methods' do
   #   describe '#' do
