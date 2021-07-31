c1a1faf833a6e51498a146f088c6c5378c188726
Michael Abbott <michael@mjabbottdesign.com>
2021-07-31 12:05:44 -0700

    Add enum enable; Item #enable_opposite

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  enum enable: { enable: 0, disable: 1 }
+
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
@@ -6,4 +8,8 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true, numericality: { only_integer: true }
+
+  def enable_opposite
+    enable == 'enable' ? 'disable' : 'enable' 
+  end
 end
