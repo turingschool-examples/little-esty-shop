365b65e13e3595ab3016ecca2057f0221792c1e8
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-01 15:33:34 -0600

    Add tests for proving enable/disable sections

--- spec/features/merchant_items/index_spec.rb
+++ spec/features/merchant_items/index_spec.rb
@@ -26,12 +26,15 @@ RSpec.describe 'the merchant items index' do
     it 'has an enable button for items that are disabled' do
       visit merchant_items_path(@merchant1)
 
-      expect(page).to have_content('Status: Disabled')
       expect(page).to have_button('Enable')
     end
 
-    xit 'has a disable link for items that are enabled' do
+    it 'has a disable link for items that are enabled' do
+      visit merchant_items_path(@merchant1)
+      expect(page).to_not have_button('Disable Milk')
+      click_button('Enable Milk')
 
+      expect(page).to have_button('Disable Milk')
     end
   end
 
@@ -49,12 +52,12 @@ RSpec.describe 'the merchant items index' do
 
     it 'can click on enable button and enable disabled item' do
       visit merchant_items_path(@merchant1)
-      expect(page).to_not have_content('Status: Enabled')
 
       click_button('Enable Milk')
 
       expect(current_path).to eq(merchant_items_path(@merchant1))
-      expect(page).to have_content('Status: Enabled')
+      expect('Enabled Items').to appear_before('Milk')
+      expect('Milk').to appear_before('Disabled Items')
       save_and_open_page
     end
   end
