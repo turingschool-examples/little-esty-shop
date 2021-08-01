92356e2e0aabbc5e89e95accd6d4ab8d7dccb08d
deebot10 <dee1jhill@gmail.com>
2021-08-01 16:39:10 -0600

    Tweaks to Total Revenue Story

--- spec/factories/invoice_item.rb
+++ spec/factories/invoice_item.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :invoice_item do
     quantity { Faker::Number.between(from: 1, to: 50) }
-    unit_price { Faker::Number.between(from: 20, to: 2000) }
+    unit_price { '$372.08' }
     status { 0 }
   end
 end
