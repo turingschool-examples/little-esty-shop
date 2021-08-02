7b627c31b82a5a24943cf69cf0f1b1aa261fd224
Taylor Varoglu <tava8398@colorado.edu>
2021-08-01 18:14:24 -0600

    Add class variables to ApplicationController for API calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
 class ApplicationController < ActionController::Base
-  
+
+  def welcome
+  end
+
+  @@class_commits = API.aggregate_by_author(:commits)
+  @@class_pulls = API.aggregate_by_author(:pulls)
+
+  def self.commits
+    @@class_commits
+  end
+
+  def self.pulls
+    @@class_pulls
+  end
+
+  def self.default_stats
+    {
+      commits: API.contributions[:defaults][:commits],
+      pulls: API.contributions[:defaults][:pulls]
+    }
+  end
+
+  def self.reset_commits
+    @@class_commits = API.aggregate_by_author(:commits)
+  end
+
+  def self.reset_pulls
+    @@class_pulls = API.aggregate_by_author(:pulls)
+  end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,8 @@
 class WelcomeController < ApplicationController
 
   def refresh
-    @default_commits = API.contributions[:defaults][:commits]
-    @default_pulls = API.contributions[:defaults][:pulls]
-    # @refreshed_commits = API.aggregate_by_author(:commits)
-    # @refreshed_pulls = API.aggregate_by_author(:pulls)
-    # if @refreshed_commits.values.sum { |val| val } == 0
-    #   @refreshed_commits = @default_commits
-    # end
-    # if @refreshed_pulls.values.sum { |val| val } == 0
-    #   @refreshed_pulls = @default_pulls
-    # end
+    ApplicationController.reset_commits
+    ApplicationController.reset_pulls
     redirect_back(fallback_location: root_path)
   end
 
