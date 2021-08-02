fa33fb5e298c062e3bfb70645e63563f458d08ce
Matt Toensing <matthew.toensing@gmail.com>
2021-08-02 11:33:57 -0700

    Make organization changes to query

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -40,7 +40,7 @@ class Merchant < ApplicationRecord
     .joins(:invoices)
     .joins(:transactions)
     .group('invoices.id, items.id')
-    .select('invoices.created_at')
+    .select('invoices.created_at, SUM(invoice_items.quantity * invoice_items.unit_price) desc')
     .where('invoices.status = ?', 2)
     .where('transactions.result = ?', 0)
     .order(Arel.sql('SUM(invoice_items.quantity * invoice_items.unit_price) desc'))
