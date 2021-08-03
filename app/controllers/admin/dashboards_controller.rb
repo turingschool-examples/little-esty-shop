237273953a41a389376d564ff1263364bbfc3367
Gunnar <dunkcrunk.com>
2021-08-02 21:54:11 -0600

    Made the admin dashboard first two stories by making a admin dashboard by handrolling my routes and creating a dashboard controller and views.

---
+++ app/controllers/admin/dashboards_controller.rb
@@ -0,0 +1,10 @@
+class Admin::DashboardsController < ApplicationController
+  def index
+    
+  end
+
+# private
+#   def _params
+#     params.permit(:)
+#   end
+end
--- spec/features/admin/dashboards/index_spec.rb
+++ spec/features/admin/dashboards/index_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
-RSpec.describe 'it can ' do
+RSpec.describe 'it can display the dashboards index page' do
   before :each do
-    
-    visit "/"
+    visit "/admin"
   end
-  it '' do
+
+  it 'can show the Header of the index page' do
+    expect(page).to have_content("Admin Dashboard")
+  end
+
+  it 'shows the links to the admin merchants and the admin invoices' do
+    expect(page).to have_link("Merchants: Index page")
+    expect(page).to have_link("Invoices: Index page")
+  end
+
+  it 'can click on the index links and be on that specified index page' do
     
+    click_link("Merchants: Index page")
+    expect(current_path).to eq("/admin/merchants")
+
+    visit "/admin"
+
+    click_link("Invoices: Index page")
+    expect(current_path).to eq("/admin/invoices")
   end
 end
