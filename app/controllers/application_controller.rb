427db4e3b185fc2a22492e7b0cd0c114c39f90c1
Matt Toensing <matthew.toensing@gmail.com>
2021-08-03 16:02:29 -0700

    Change action name to service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :api
+  before_action :service
 
-  def api
+  def service
     service = GithubService.new
     @contributors = service.contributors
     @commits = service.commits
+    @pull_requests = service.pull_requests
   end
 end
