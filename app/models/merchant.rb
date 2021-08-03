5f29feed6689d410253752ed77582faeba1d2bd7
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-02 20:31:22 -0600

    Add method to return top five items for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,14 @@
 class Merchant < ApplicationRecord
   has_many :items
-  has_many :invoices, through: :items
 
   validates :name, presence: true
+
+  def top_five_items
+    items.joins(invoices: :transactions)
+         .where(transactions: {result: true})
+         .select("items.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+         .group("items.id")
+         .order(revenue: :desc)
+         .limit(5)
+  end
 end
