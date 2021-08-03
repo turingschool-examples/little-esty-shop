b342b2a891faa98ea4930dfaad8daf4ebf19f8b2
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-03 16:43:05 -0600

    Make method for finding individual item top day

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,14 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
+
+  def best_sales_day
+    invoices.joins(:transactions)
+    .where(transactions: {result: true})
+    .select("invoices.created_at as best_day, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+    .group("invoices.id")
+    .order(revenue: :desc)
+    .first
+    .best_day
+  end
 end
