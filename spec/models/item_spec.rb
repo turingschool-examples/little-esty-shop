435aab82efd8372e0622ae5da5442e739334d8c4
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-04 14:52:35 -0600

    Make test for best sales day more robust

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,7 +16,23 @@ RSpec.describe Item, type: :model do
   describe 'instance methods' do
     describe '#best_sales_day' do
       it 'can return date of highest sales earned' do
-        expect(@item1.best_sales_day.strftime("%m/%d/%Y")).to eq("08/03/2021")
+        @merchant1 = Merchant.create!(name: 'Costco', status: "disabled")
+        @customer1 = Customer.create!(first_name: 'Gunner', last_name: 'Runkle')
+        @item30 = @merchant1.items.create!(name: 'Milk', description: 'A large quantity of whole milk', unit_price: 500)
+        @invoice1 = @customer1.invoices.create!(status: 'completed', created_at: '2018-02-13 14:53:59 UTC', updated_at: '2018-02-13 14:53:59 UTC')
+        @invoice2 = @customer1.invoices.create!(status: 'completed', created_at: '2015-05-25 14:53:59 UTC', updated_at: '2015-05-25 14:53:59 UTC')
+        @invoice3 = @customer1.invoices.create!(status: 'completed', created_at: '2010-01-24 14:53:59 UTC', updated_at: '2010-01-24 14:53:59 UTC')
+        @invoice4 = @customer1.invoices.create!(status: 'completed', created_at: '2015-05-25 14:53:59 UTC', updated_at: '2015-05-25 14:53:59 UTC')
+        @invoice_item1 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, quantity: 125, unit_price: @item1.unit_price, status: 'shipped')
+        @invoice_item2 = InvoiceItem.create!(invoice_id: @invoice2.id, item_id: @item1.id, quantity: 250, unit_price: @item1.unit_price, status: 'shipped')
+        @invoice_item3 = InvoiceItem.create!(invoice_id: @invoice3.id, item_id: @item1.id, quantity: 1000, unit_price: @item1.unit_price, status: 'shipped')
+        @invoice_item4 = InvoiceItem.create!(invoice_id: @invoice4.id, item_id: @item1.id, quantity: 500, unit_price: @item1.unit_price, status: 'shipped')
+        @transaction1 = @invoice1.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: true)
+        @transaction2 = @invoice2.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: true)
+        @transaction3 = @invoice3.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: false)
+        @transaction4 = @invoice4.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: true)
+
+        expect(@item1.best_sales_day.strftime("%m/%d/%Y")).to eq("05/25/2015")
       end
     end
   end
