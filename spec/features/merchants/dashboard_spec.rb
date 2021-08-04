ee3222b4cc40a1c2e369ef91f36f931f40ca0d4e
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-04 16:33:39 -0600

    Test for dashboard ready to ship items

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -12,16 +12,32 @@ RSpec.describe 'the merchant dashboard' do
       end
     end
 
-    it 'show the items that are ready to ship with their invoice and date' do
+    it 'show the items that are ready to ship with their invoice and date ordered by oldest first' do
+      @merchant1 = Merchant.create!(name: 'Costco', status: "disabled")
+      @customer1 = Customer.create!(first_name: 'Gunner', last_name: 'Runkle')
+      @item1 = @merchant1.items.create!(name: 'Milk', description: 'A large quantity of whole milk', unit_price: 500)
+      @invoice1 = @customer1.invoices.create!(status: 'completed', created_at: '2018-02-13 14:53:59 UTC', updated_at: '2018-02-13 14:53:59 UTC')
+      @invoice2 = @customer1.invoices.create!(status: 'completed', created_at: '2015-05-25 14:53:59 UTC', updated_at: '2015-05-25 14:53:59 UTC')
+      @invoice3 = @customer1.invoices.create!(status: 'completed', created_at: '2010-01-24 14:53:59 UTC', updated_at: '2010-01-24 14:53:59 UTC')
+      @invoice4 = @customer1.invoices.create!(status: 'completed', created_at: '2015-05-25 14:53:59 UTC', updated_at: '2015-05-25 14:53:59 UTC')
+      @invoice_item1 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, quantity: 125, unit_price: @item1.unit_price, status: 'packaged')
+      @invoice_item2 = InvoiceItem.create!(invoice_id: @invoice2.id, item_id: @item1.id, quantity: 250, unit_price: @item1.unit_price, status: 'pending')
+      @invoice_item3 = InvoiceItem.create!(invoice_id: @invoice3.id, item_id: @item1.id, quantity: 1000, unit_price: @item1.unit_price, status: 'packaged')
+      @invoice_item4 = InvoiceItem.create!(invoice_id: @invoice4.id, item_id: @item1.id, quantity: 500, unit_price: @item1.unit_price, status: 'shipped')
+      @transaction1 = @invoice1.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: true)
+      @transaction2 = @invoice2.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: true)
+      @transaction3 = @invoice3.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: false)
+      @transaction4 = @invoice4.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: true)
+
       visit merchant_path(@merchant1.id)
 
       within('#ready_to_ship') do
         expect(page).to have_content('Items Ready to Ship')
-        expect(page).to have_content('Potato Chips')
-        expect(page).to have_content('Spinach')
-        expect(page).to have_content('Red Bull')
-        expect(page).to have_content('Hot Cheetos')
-        expect(page).to have_content('Hot Dog')
+        expect(page).to have_content(@item1.name)
+        expect(page).to have_content(@invoice1.id)
+        expect(page).to have_content(@invoice2.id)
+        expect(page).to have_content(@invoice3.id)
+        expect("Sunday, January 24, 2010").to appear_before("Tuesday, February 13, 2018")
       end
     end
   end
