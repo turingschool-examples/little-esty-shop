a06ac9ea9adb2cffc2a8dacd67ce2fd27e1cffe7
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-04 16:34:24 -0600

    Add tests for ready to ship method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -45,5 +45,34 @@ RSpec.describe Merchant, type: :model do
         expect(@merchant1.top_five_items).to eq(expected)
       end
     end
+    describe '#ready_to_ship' do
+      it 'can give all items that are ready to ship ordered by oldest first' do
+        @merchant1 = Merchant.create!(name: 'Costco', status: "disabled")
+        @customer1 = Customer.create!(first_name: 'Gunner', last_name: 'Runkle')
+        @item1 = @merchant1.items.create!(name: 'Milk', description: 'A large quantity of whole milk', unit_price: 500)
+        @invoice1 = @customer1.invoices.create!(status: 'completed', created_at: '2018-02-13 14:53:59 UTC', updated_at: '2018-02-13 14:53:59 UTC')
+        @invoice2 = @customer1.invoices.create!(status: 'completed', created_at: '2015-05-25 14:53:59 UTC', updated_at: '2015-05-25 14:53:59 UTC')
+        @invoice3 = @customer1.invoices.create!(status: 'completed', created_at: '2010-01-24 14:53:59 UTC', updated_at: '2010-01-24 14:53:59 UTC')
+        @invoice4 = @customer1.invoices.create!(status: 'completed', created_at: '2015-05-25 14:53:59 UTC', updated_at: '2015-05-25 14:53:59 UTC')
+        @invoice_item1 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, quantity: 125, unit_price: @item1.unit_price, status: 'packaged')
+        @invoice_item2 = InvoiceItem.create!(invoice_id: @invoice2.id, item_id: @item1.id, quantity: 250, unit_price: @item1.unit_price, status: 'pending')
+        @invoice_item3 = InvoiceItem.create!(invoice_id: @invoice3.id, item_id: @item1.id, quantity: 1000, unit_price: @item1.unit_price, status: 'packaged')
+        @invoice_item4 = InvoiceItem.create!(invoice_id: @invoice4.id, item_id: @item1.id, quantity: 500, unit_price: @item1.unit_price, status: 'shipped')
+        @transaction1 = @invoice1.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: true)
+        @transaction2 = @invoice2.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: true)
+        @transaction3 = @invoice3.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: false)
+        @transaction4 = @invoice4.transactions.create!(credit_card_number: '1234234534564567', credit_card_expiration_date: nil, result: true)
+
+        actual = @merchant1.ready_to_ship.map do |item|
+          item.invoice_date
+        end
+
+        expected = [@invoice3, @invoice2, @invoice1].map do |invoice|
+          invoice.created_at
+        end
+
+        expect(actual).to eq(expected)
+      end
+    end
   end
 end
