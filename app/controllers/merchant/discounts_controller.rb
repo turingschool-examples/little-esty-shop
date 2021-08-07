11d91a1f2b36f12fe42db2bc921e13722b10d132
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-07 00:20:18 -0400

    Update discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,69 +1,74 @@
-class DiscountsController < ApplicationController
+class Merchant::DiscountsController < ApplicationController
+  before_action :set_merchant, only: [ :index, :show, :edit, :update, :destroy ]
   before_action :set_discount, only: [ :show, :edit, :update, :destroy ]
 
   # GET /discounts or /discounts.json
   def index
-    @discounts = Discount.all
+    # @merchant = Merchant.find(params[:merchant_id])
   end
 
-  # GET /discounts/1 or /discounts/1.json
-  def show
-  end
-
-  # GET /discounts/new
-  def new
-    @discount = Discount.new
-  end
-
-  # GET /discounts/1/edit
-  def edit
-  end
-
-  # POST /discounts or /discounts.json
-  def create
-    @discount = Discount.new(discount_params)
-
-    respond_to do |format|
-      if @discount.save
-        format.html { redirect_to @discount, notice: "Discount was successfully created." }
-        format.json { render :show, status: :created, location: @discount }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @discount.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /discounts/1 or /discounts/1.json
-  def update
-
-      if @discount.update(discount_params)
-        format.html { redirect_to @discount, notice: "Discount was successfully updated." }
-        format.json { render :show, status: :ok, location: @discount }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @discount.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /discounts/1 or /discounts/1.json
-  def destroy
-    @discount.destroy
-    respond_to do |format|
-      format.html { redirect_to discounts_url, notice: "Discount was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
+  # # GET /discounts/1 or /discounts/1.json
+  # def show
+  # end
+  #
+  # # GET /discounts/new
+  # def new
+  #   @discount = Discount.new
+  # end
+  #
+  # # GET /discounts/1/edit
+  # def edit
+  # end
+  #
+  # # POST /discounts or /discounts.json
+  # def create
+  #   @discount = Discount.new(discount_params)
+  #
+  #   respond_to do |format|
+  #     if @discount.save
+  #       format.html { redirect_to @discount, notice: "Discount was successfully created." }
+  #       format.json { render :show, status: :created, location: @discount }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @discount.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  #
+  # # PATCH/PUT /discounts/1 or /discounts/1.json
+  # def update
+  #
+  #     if @discount.update(discount_params)
+  #       format.html { redirect_to @discount, notice: "Discount was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @discount }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @discount.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  #
+  # # DELETE /discounts/1 or /discounts/1.json
+  # def destroy
+  #   @discount.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to discounts_url, notice: "Discount was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
+  #
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_discount
       @discount = Discount.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    def discount_params
-      params.require(:discount).permit(:name, :threshold, :percentage, :merchant_id)
+    def set_merchant
+      @merchant = Merchant.find(params[:merchant_id])
     end
+  #
+  #   # Only allow a list of trusted parameters through.
+  #   def discount_params
+  #     params.require(:discount).permit(:name, :threshold, :percentage, :merchant_id)
+  #   end
 end
