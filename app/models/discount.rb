a9587e25209f8f0a216e17ddd886546bf3cb2543
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-06 20:52:04 -0400

    Create Discount model with associations

---
+++ app/models/discount.rb
@@ -0,0 +1,6 @@
+class Discount < ApplicationRecord
+  belongs_to :merchant
+  has_many :items, through: :merchant
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items
+end
