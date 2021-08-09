563ce869af5b93551c07a3eb4f14e447adc528af
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-09 00:08:20 -0400

    Create show page and controller actions

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,10 +1,11 @@
 class Merchant::DiscountsController < ApplicationController
-  before_action :set_merchant, only: [ :index, :show, :edit, :update, :destroy ]
+  before_action :set_merchant, only: [ :index, :show, :edit, :update ]
   before_action :set_discount, only: [ :show, :edit, :update, :destroy ]
 
   # GET /discounts or /discounts.json
   def index
     # @merchant = Merchant.find(params[:merchant_id])
+    # @discounts = @merchant.discounts.all
   end
 
   # # GET /discounts/1 or /discounts/1.json
@@ -12,50 +13,54 @@ class Merchant::DiscountsController < ApplicationController
   # end
   #
   # # GET /discounts/new
-  # def new
-  #   @discount = Discount.new
-  # end
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.new
+  end
   #
   # # GET /discounts/1/edit
-  # def edit
-  # end
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.find(params[:id])
+  end
   #
   # # POST /discounts or /discounts.json
-  # def create
-  #   @discount = Discount.new(discount_params)
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.discounts.create(discount_params)
   #
   #   respond_to do |format|
-  #     if @discount.save
-  #       format.html { redirect_to @discount, notice: "Discount was successfully created." }
+      if @discount.save
+        redirect_to merchant_discounts_path(@merchant), flash: {notice: "Discount was successfully created." }
   #       format.json { render :show, status: :created, location: @discount }
-  #     else
+      else
+        redirect_to new_merchant_discount_path(@merchant), flash: {notice: "Discount was not created." }
   #       format.html { render :new, status: :unprocessable_entity }
   #       format.json { render json: @discount.errors, status: :unprocessable_entity }
-  #     end
+      end
   #   end
-  # end
+  end
+
+  # PATCH/PUT /discounts/1 or /discounts/1.json
+  def update
+    if @discount.update(discount_params)
+      redirect_to merchant_discount_path(@merchant, @discount), flash: {notice: "Successfully Updated Discount"}
+    #   # format.json { render :show, status: :ok, location: @discount }
+    else
+      redirect_to merchant_discount_path(@merchant, @discount), flash: {notice: "Discount not updated"}
+      # format.html { render :edit, status: :unprocessable_entity }
+      # format.json { render json: @discount.errors, status: :unprocessable_entity }
+    end
+  end
   #
-  # # PATCH/PUT /discounts/1 or /discounts/1.json
-  # def update
-  #
-  #     if @discount.update(discount_params)
-  #       format.html { redirect_to @discount, notice: "Discount was successfully updated." }
-  #       format.json { render :show, status: :ok, location: @discount }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @discount.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  #
-  # # DELETE /discounts/1 or /discounts/1.json
-  # def destroy
-  #   @discount.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to discounts_url, notice: "Discount was successfully destroyed." }
-  #     format.json { head :no_content }
-  #   end
-  # end
+  # DELETE /discounts/1 or /discounts/1.json
+  def destroy
+    @merchant = Merchant.find(params[:merchant_id])
+    # @merchant = Merchant.find(params[:])
+    @discount = Discount.find(params[:id]).destroy
+    redirect_to merchant_discounts_path(@merchant), flash: {notice: "Discount successfully destroyed"}
+    # end
+  end
   #
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -68,7 +73,7 @@ class Merchant::DiscountsController < ApplicationController
     end
   #
   #   # Only allow a list of trusted parameters through.
-  #   def discount_params
-  #     params.require(:discount).permit(:name, :threshold, :percentage, :merchant_id)
-  #   end
+    def discount_params
+      params.require(:discount).permit(:name, :threshold, :percentage, :merchant_id)
+    end
 end
