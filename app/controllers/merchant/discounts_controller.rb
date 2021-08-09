aeb17289eda782dbf284ac2873e62717408a322c
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-09 10:59:57 -0400

    Clean up discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -4,8 +4,6 @@ class Merchant::DiscountsController < ApplicationController
 
 
   def index
-    # @merchant = Merchant.find(params[:merchant_id])
-    # @discounts = @merchant.discounts.all
   end
 
   def show
@@ -31,27 +29,21 @@ class Merchant::DiscountsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /discounts/1 or /discounts/1.json
+
   def update
     if @discount.update(discount_params)
       redirect_to merchant_discount_path(@merchant, @discount), flash: {notice: "Successfully Updated Discount"}
-    #   # format.json { render :show, status: :ok, location: @discount }
     else
       redirect_to merchant_discount_path(@merchant, @discount), flash: {notice: "Discount not updated"}
-      # format.html { render :edit, status: :unprocessable_entity }
-      # format.json { render json: @discount.errors, status: :unprocessable_entity }
     end
   end
-  #
-  # DELETE /discounts/1 or /discounts/1.json
+
   def destroy
     @merchant = Merchant.find(params[:merchant_id])
-    # @merchant = Merchant.find(params[:])
     @discount = Discount.find(params[:id]).destroy
     redirect_to merchant_discounts_path(@merchant), flash: {notice: "Discount successfully destroyed"}
-    # end
   end
-  #
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_discount
@@ -61,8 +53,7 @@ class Merchant::DiscountsController < ApplicationController
     def set_merchant
       @merchant = Merchant.find(params[:merchant_id])
     end
-  #
-  #   # Only allow a list of trusted parameters through.
+
     def discount_params
       params.require(:discount).permit(:name, :threshold, :percentage, :merchant_id)
     end
