bf335153a0a077b0f71d5ff8870f12b449e6ea6e
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-09 22:27:39 -0400

    Create invoice item methods to determine discount

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -10,4 +10,19 @@ class InvoiceItem < ApplicationRecord
   has_many :merchants, through: :item
   has_many :discounts, through: :merchants
 
+  def full_amount
+    (unit_price * quantity).div(100.0)
+  end
+
+  def invoice_item_discount
+    item.merchant.discounts.where('discounts.threshold <= ?', quantity).order(percentage: :desc).first
+  end
+
+  def revenue_after_discount
+    if invoice_item_discount.present?
+      (full_amount * (1 - (invoice_item_discount.percentage / 100.0))).round(2)
+    else
+      full_amount
+    end
+  end
 end
