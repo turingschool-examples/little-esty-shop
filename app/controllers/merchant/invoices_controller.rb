dc3a86fcb569874a7bfa24251311d48da65067ec
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-10 18:52:59 -0400

    Clean up merchant items controller with callbacks

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -1,12 +1,12 @@
 class Merchant::InvoicesController < ApplicationController
+  before_action :set_merchant, only: [ :show, :update ]
+  before_action :set_invoice, only: [ :show, :update, :destroy ]
 
   def index
     @invoices = Merchant.merchant_invoices(params[:merchant_id])
   end
 
   def show
-    @merchant = Merchant.find(params[:merchant_id])
-    @invoice = Invoice.find(params[:id])
   end
 
   def new
@@ -19,14 +19,21 @@ class Merchant::InvoicesController < ApplicationController
   end
 
   def update
-    @merchant = Merchant.find(params[:merchant_id])
-    @invoice = Invoice.find(params[:id])
     @invoice_item = InvoiceItem.find(params[:ii_id])
-    @invoice_item.update(status: params[:status] )
+    @invoice_item.update(status: params[:status])
     @invoice_item.save
 
     redirect_to "/merchants/#{@merchant.id}/invoices/#{@invoice.id}"
     flash[:notice] = "Item status successfully updated!"
   end
 
+  private
+
+    def set_invoice
+      @invoice = Invoice.find(params[:id])
+    end
+
+    def set_merchant
+      @merchant = Merchant.find(params[:merchant_id])
+    end
 end
