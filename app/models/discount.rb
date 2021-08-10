43030eec17c7730368609dedfa9ad85f2deba945
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-10 17:52:08 -0400

    Prevent multiple holiday discounts of the same holiday with validation

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,7 +4,7 @@ class Discount < ApplicationRecord
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :percentage, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 100 }
   validates :threshold, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
