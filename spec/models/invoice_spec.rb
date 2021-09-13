ac527c2b11fa980de7768672973100ea0062a691
Laura Court <laura.mcourt@gmail.com>
2021-09-13 19:58:37 -0400

    Creates model relationships

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Invoice do
+  describe 'relationships' do
+    it { should belong_to(:customer) }
+    it { should have_many(:transactions) }
+    it { should have_many(:items).through(:invoice_items) }
+  end
+end
