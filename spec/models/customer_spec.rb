dad254a37bbe92feb25ef1aa25a2a7208023106a
Ida Olson <idaolson@gmail.com>
2021-09-13 19:07:26 -0600

    Create customer model spec

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+  describe 'relationships' do
+    it { should have_many(:invoices) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:created_at) }
+    it { should validate_presence_of(:updated_at) }
+  end
+end
