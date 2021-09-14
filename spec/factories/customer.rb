7c345761f08834311bece97add10ee6aedd650d1
Khoi Nguyen <khoinguyen311@gmail.com>
2021-09-14 14:35:35 -0700

    Add uinque characters

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :customer, class: Customer do
-    first_name { Faker::TvShows::RickAndMorty.character }
-    last_name { Faker::TvShows::Simpsons.character }
+    first_name { Faker::TvShows::RickAndMorty.unique.character }
+    last_name { Faker::TvShows::Simpsons.unique.character }
     created_at { "2012-03-27 14:53:59 UTC" }
     updated_at { "2012-03-27 14:53:59 UTC" }
   end
