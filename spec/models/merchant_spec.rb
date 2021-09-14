0a1070c6a5c5749e86051cf553d409db92ed17fa
Tanner Dale <tanner@tannerdale.com>
2021-09-13 21:09:49 -0500

    Add merchant relationship test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    let(:merchant) { create :merchant }
+
+    it 'should respond to items' do
+      expect(merchant).to respond_to(:items)
+    end
+  end
 end
