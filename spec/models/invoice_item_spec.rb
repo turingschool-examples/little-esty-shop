3e8b5143649e2fbbdb2c2a9f8cc1381a91c7259b
Ida Olson <idaolson@gmail.com>
2021-09-13 19:08:11 -0600

    Create invoice-item model spec

---
+++ spec/models/invoice_item_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe InvoiceItem do
+  describe 'relationships' do
+    it { should belong_to(:item) }
+    it { should belong_to(:invoice) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:quantity) }
+    it { should validate_presence_of(:unit_price) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:created_at) }
+    it { should validate_presence_of(:updated_at) }
+  end
+end
