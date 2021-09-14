25b31b9bd16db6f05620d4458cce114465061169
Ozzie <mikeosmonson@gmail.com>
2021-09-14 17:02:02 -0600

    Add Admin Merchants Index, Admin Merchant Show MVC's, routes & tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'admin/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'application#welcome'
 
   resources :merchant, only: [:show]
-  resources :admin, only: [:show]
+  resources :admin
 
   namespace :merchants do
     resources :items
---
+++ spec/features/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Merchant Index' do
+  before :each do
+    @customer1 = Customer.create!(first_name: 'John', last_name: 'Smith')
+    @customer2 = Customer.create!(first_name: 'Steve', last_name: 'Dobson')
+    @customer3 = Customer.create!(first_name: 'Melenie', last_name: 'Kelly')
+    @customer4 = Customer.create!(first_name: 'Rachel', last_name: 'Jonas')
+    @customer5 = Customer.create!(first_name: 'Adam', last_name: 'Tally')
+    @customer6 = Customer.create!(first_name: 'George', last_name: 'Bratz')
+  end
+
+  describe 'Admin Dashboard' do
+    it 'indicates the page is admin dashboard' do
+      visit '/admin'
+
+      expect(page).to have_content("Admin Dashboard")
+    end
+
+    it 'shows links to merchants index & invoices index' do
+      visit '/admin'
+
+      expect(page).to have_link('List of Merchants')
+      expect(page).to have_link('List of Invoices')
+    end
+
+    xit 'can display the names of the top 5 customers' do
+      visit '/admin'
+    end
+  end
+end
