3fb35c335629cd4a431bebdd97c060199418face
Khoi Nguyen <khoinguyen311@gmail.com>
2021-09-14 12:01:59 -0700

    Change Faker category for names

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :customer, class: Customer do
     first_name { Faker::TvShows::RickAndMorty.character }
-    last_name { Faker::TvShows::RickAndMorty.character }
+    last_name { Faker::TvShows::Simpsons.character }
     created_at { "2012-03-27 14:53:59 UTC" }
     updated_at { "2012-03-27 14:53:59 UTC" }
   end
