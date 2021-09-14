146a1dc21edd23a7e38c3bda01352a1476de3924
Tanner Dale <tanner@tannerdale.com>
2021-09-13 23:31:19 -0500

    Add new has_many, has_many through relationships

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,7 @@ class Merchant < ApplicationRecord
   self.primary_key = :id
 
   has_many :items, dependent: :destroy
+
+  has_many :merchant_invoices
+  has_many :invoices, through: :merchant_invoices
 end
