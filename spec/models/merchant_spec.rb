eeed3e2b0622e1912952e19736023dd4811cd9b7
Ida Olson <idaolson@gmail.com>
2021-09-13 19:07:50 -0600

    Create merchant model spec

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Merchant do
+  describe 'relationships' do
+    it { should have_many(:items) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:created_at) }
+    it { should validate_presence_of(:updated_at) }
+  end
+end
