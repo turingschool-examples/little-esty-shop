b8b2860be5510a5185f5c39a2435ebba74b351bc
Ida Olson <idaolson@gmail.com>
2021-09-14 14:19:59 -0600

    Add enum status

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,4 +2,6 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
   validates :quantity, :unit_price, :status, :created_at, :updated_at, presence: true
+
+  enum status: [:pending, :packaged, :shipped]
 end
