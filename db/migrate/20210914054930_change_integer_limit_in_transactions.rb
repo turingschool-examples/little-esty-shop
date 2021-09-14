1cc51e79c1d7c3afa9f31b212858ab52bbc9b1ae
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-09-13 23:54:51 -0600

    degugged transaction table issue

---
+++ db/migrate/20210914054930_change_integer_limit_in_transactions.rb
@@ -0,0 +1,5 @@
+class ChangeIntegerLimitInTransactions < ActiveRecord::Migration[5.2]
+  def change
+    change_column :transactions, :credit_card_number, :integer, limit: 8
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_020148) do
+ActiveRecord::Schema.define(version: 2021_09_14_054930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2021_09_14_020148) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.integer "credit_card_number"
+    t.bigint "credit_card_number"
     t.integer "credit_card_expiration_date"
     t.string "result"
     t.bigint "invoice_id"
