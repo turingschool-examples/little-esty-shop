4c6f9df1be3da4ca4895b1eb550766bbbfb7dd4c
Henry Schmid <henry.schmid1@gmail.com>
2021-09-13 21:36:08 -0600

    Add feature for favorite customers

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,6 @@
 class MerchantsController < ApplicationController
   def dashboard
     @merchant = Merchant.find(params[:merchant_id])
+    @favorite_customers = @merchant.favorite_customers
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,11 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
+
+  def favorite_customers
+    items.select('customers.*, COUNT(transactions.result = 0) as success')
+         .joins(invoices: [:customer, :transactions])
+         .group('customers.id')
+         .order('success desc')
+         .limit(5)
+  end
 end
