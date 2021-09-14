196fa336354805209e959eb6a346b9e56c793dc2
Laura Court <laura.mcourt@gmail.com>
2021-09-14 19:08:19 -0400

    Change integer to string for cc number

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_09_13_233252) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.integer "credit_card_number"
+    t.string "credit_card_number"
     t.string "credit_card_expiration_date"
     t.string "result"
     t.bigint "invoice_id"
