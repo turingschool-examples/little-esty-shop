d0a49bd19d024aadff3a513f5a527784e97e037d
Andrew Massey <acmassey@ncsu.edu>
2021-09-14 18:34:02 -0400

    Create test for the top five items and begin workshopping method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -14,4 +14,16 @@ class Merchant < ApplicationRecord
     #   .joins(invoices: [:items, :transactions])
     #   .group(:id)
   end
+
+  def top_five_items
+
+    items.joins(invoices: :invoice_items)
+         .joins(transactions: :invoices)
+require "pry"; binding.pry
+         #.where("transaction.result = ?", 0)
+         #.select(item.*,sum(invoice_items.unit_price*invoice_items.quantity)as revenue)
+         #.group(:id)
+         #.order(revenue: :desc)
+         #.limit(5)
+  end
 end
