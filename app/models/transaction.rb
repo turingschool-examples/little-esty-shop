2ab13de93eb6434795a441e3826c036d7684180f
Laura Court <laura.mcourt@gmail.com>
2021-09-14 19:08:49 -0400

    Remove numericality validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,5 @@ class Transaction < ApplicationRecord
   belongs_to  :invoice
 
   validates :credit_card_number, numericality: { only_integer: true }, presence: true
-  validates :result, numericality: { only_integer: true }, presence: true
+  validates :result, presence: true
 end
