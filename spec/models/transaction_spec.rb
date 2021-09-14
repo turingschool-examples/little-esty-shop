ca30bf284008f15ba0b9b9ed1526c0a4bb9244ea
Jamie Pace <jamiejpace@gmail.com>
2021-09-13 20:14:35 -0600

    Add model test for transaction relationships

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    let(:transaction) {create :transaction}
+    let(:status) { ['success', 'failed'] }
+
+    it 'should respond to invoices' do
+      expect(transaction).to respond_to(:invoice)
+    end
+
+    it 'has the right index' do
+      status.each_with_index do |item, index|
+        expect(Transaction.statuses[item]).to eq(index)
+      end
+    end
+  end
 end
