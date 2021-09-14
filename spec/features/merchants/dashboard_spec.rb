63d27c2de96f3f37f36741497efa767a11f107de
Tanner Dale <tanner@tannerdale.com>
2021-09-13 23:30:54 -0500

    Add tests, view updates for links to invoices, items

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -2,11 +2,29 @@ require 'rails_helper'
 # rspec spec/features/merchants/dashboard_spec.rb
 RSpec.describe 'Merchant Dashboard Show Page' do
   describe 'Merchant Dashboard Show Page' do
-    it 'shows merchant dashbaord attributes' do
-      merchant1 = create(:merchant)
+    let!(:merchant1) { create(:merchant) }
+
+    before :each do
       visit merchant_dashboard_path(merchant1.id)
+    end
+
+    it 'shows merchant dashbaord attributes' do
 
       expect(page).to have_content(merchant1.name)
+    end
+
+    describe 'links' do
+      it 'has links to the merchants items' do
+        click_on 'Items'
+
+        expect(current_path).to eq(merchant_items_path(merchant1))
+      end
+
+      it 'has links to the merchants Invoices' do
+        click_on 'Invoices'
+
+        expect(current_path).to eq(merchant_invoices_path(merchant1))
+      end
     end
   end
 end
