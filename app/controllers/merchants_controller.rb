f4871deca02a0b14ac9a74163994b0e7e36e8490
Henry Schmid <henry.schmid1@gmail.com>
2021-09-14 18:29:02 -0600

    Add items ready to ship to dashboard view

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,7 +1,5 @@
 class MerchantsController < ApplicationController
   def dashboard
     @merchant = Merchant.find(params[:merchant_id])
-    @favorite_customers = @merchant.favorite_customers
-    # @items_ready_to_ship = @merchant.ready_to_ship
   end
 end
