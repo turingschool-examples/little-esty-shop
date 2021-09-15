e2dc0754f0932a23039287500140e832d2e94a60
Andrew Massey <acmassey@ncsu.edu>
2021-09-15 15:00:20 -0400

    Create method and view for the top five customers on the admin dashboard.

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,5 @@
+class Admin::DashboardController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
   has_many :invoices, dependent: :destroy
+
+  def self.top_five
+    Customer.joins(invoices: :transactions)
+            .select('COUNT(transactions.result) as successful, customers.id, customers.first_name as f_name, customers.last_name as l_name')
+            .group('customers.id')
+            .where('transactions.result = ?', 0)
+            .order(successful: :desc, f_name: :asc, l_name: :asc)
+            .limit(5)
+  end
 end
