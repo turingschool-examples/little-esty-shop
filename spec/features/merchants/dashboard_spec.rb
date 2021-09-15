5a0ba778802b28a74266554f69f985e1a5f0a5cf
Jessica Jones <daddehjones@gmail.com>
2021-09-15 14:09:06 -0400

    Reviewed with Jamison.

---
+++ spec/features/merchants/dashboard_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant#dashboard' do
+  describe 'dashboard' do
+    before(:each) do
+      @merchant_1 = Merchant.create!(name: 'JJ')
+    end
+#     As a merchant,
+# When I visit my merchant dashboard (/merchants/merchant_id/dashboard)
+# Then I see the name of my merchant
+    it "has a merchant name" do
+      visit
+      expect(page).to have_content(@merchant_1.name)
+    end
+  end
+end
