cf570ef8a3d9e2dca0300ea05b5a1dfcc9893711
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-09-15 17:28:01 -0600

    merchant items show page IP

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,10 @@
 class ItemsController < ApplicationController
     def index
       @merchant = Merchant.find(params[:merchant_id])
+      @merchant_items = @merchant.items
+    end
+
+    def show
+      @item = Item.find_by(params[:item_id])
     end
 end
--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe 'Merchant Items Index Page' do
     expect(page).to have_content(item_1.name)
     expect(page).to have_content(item_2.name)
   end
+
+  it 'has a link to take you to the merchant items show page' do
+    merchant = Merchant.create!(name: "Tony")
+    item_1 = merchant.items.create!(name: "Shirt", description: "A blue shirt", unit_price: 30)
+    item_2 = merchant.items.create!(name: "Pants", description: "Black pants", unit_price: 50)
+
+    visit (merchant_items_path(merchant))
+    click_on "Shirt"
+
+    expect(current_path).to eq(merchant_item_path(item_1))
+  end
 end
