a147ad61c3170b49f707d8093e40eb8a8a969893
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-09-15 15:07:57 -0600

    Merchant items index complete for US1

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Items Index Page' do
-    describe 'display' do
-        
-    end
+
+  it 'shows the names of all items of that merchant' do
+    merchant = Merchant.create!(name: "Tony")
+    item_1 = merchant.items.create!(name: "Shirt", description: "A blue shirt", unit_price: 30)
+    item_2 = merchant.items.create!(name: "Pants", description: "Black pants", unit_price: 50)
+
+    visit (merchant_items_path(merchant))
+
+    expect(current_path).to eq("/merchants/#{merchant.id}/items")
+    expect(page).to have_content(item_1.name)
+    expect(page).to have_content(item_2.name)
+  end
 end
