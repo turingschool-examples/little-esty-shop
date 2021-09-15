829f6be1eaefaba6ba05a70df9444ae776e2b670
Ted S <tstaros23@gmail.com>
2021-09-15 10:01:34 -0600

    Add dashboard links test

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -1,16 +1,34 @@
 require 'rails_helper'
 
-# Admin Dashboard
-#
-# As an admin,
-# When I visit the admin dashboard (/admin)
-# Then I see a header indicating that I am on the admin dashboard
-
 RSpec.describe 'admin dashboard page' do
   it "has a header" do
 
     visit '/admin'
 
     expect(page).to have_content('Admin Dashboard')
+  end
+
+#   Admin Dashboard Links
+#
+# As an admin,
+# When I visit the admin dashboard (/admin)
+# Then I see a link to the admin merchants index (/admin/merchants)
+# And I see a link to the admin invoices index (/admin/invoices)
+
+  it "has links to merchants and invoices" do
+    visit '/admin'
+
+    expect(page).to have_link('Merchants')
+    expect(page).to have_link('Invoices')
+
+    click_on 'Merchants'
+
+    expect(current_path).to eq('/admin/merchants')
+
+    visit '/admin'
+
+    click_on 'Invoices'
+    
+    expect(current_path).to eq('/admin/invoices')
   end
 end
