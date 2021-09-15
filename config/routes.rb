1188b11591debafaeb2496e04fd5eea252c7e177
Jessica Jones <daddehjones@gmail.com>
2021-09-15 17:40:11 -0400

    Change temporary routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :merchants do
-
-    resources :dashboard, controller: 'merchants/dashboards'
-  end
+  get "/merchants/:merchant_id/dashboard", to: "merchants#dashboard"
+  # resources :merchants do
+  #
+  #   resources :dashboard, only: [:index]
+  # end
 # resources :merchants
 end
