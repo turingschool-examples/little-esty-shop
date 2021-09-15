fbf1ae6c94d0e8239fb050bd60c858e7736ad36b
Jessica Jones <daddehjones@gmail.com>
2021-09-15 18:20:27 -0400

    Add new route for item index.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/merchants/:merchant_id/dashboard", to: "merchants#dashboard"
+  get "/merchants/:id/items", to: "merchants_items#index"
   # resources :merchants do
   #
   #   resources :dashboard, only: [:index]
