99e91e9e453cb2e0bbc91c6efaf5189b5de76833
Weston Ellis <ellisweston112@gmail.com>
2021-09-15 11:49:55 -0600

    Tweak routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/admin', to: 'dashboard#index'
+  # get '/admin', to: 'dashboard#index'
+
+  resources :admin #only syntax
   namespace :admin do
     resources :merchants
     resources :invoices
