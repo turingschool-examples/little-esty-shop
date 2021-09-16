c4eb61a587a2b754478501dfea38c5ee1235fd7b
Laura Court <laura.mcourt@gmail.com>
2021-09-16 02:47:28 -0400

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/merchants/:merchant_id/dashboard", to: "merchants#dashboard"
+  # get "/merchants/:merchant_id/dashboard", to: "merchants#dashboard"
 
-  resources :merchants do
+
+  resources :merchants, only: :index do
+    get "/dashboard", controller: :merchants, action: :show, as: "dashboard"
     resources :items, controller: :merchant_items
     resources :invoices, controller: :merchant_invoices
   end
@@ -15,6 +17,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :dashboard, only: [:index]
+    #get "/", to: 'dashboard#index', as: "dashboard"
     resources :merchants
     resources :invoices
   end
