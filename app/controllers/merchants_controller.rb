18431b85bdd7b788c5cacc02934c836d7c23047c
Jessica Jones <daddehjones@gmail.com>
2021-09-15 21:32:55 -0400

    Comment out.

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,8 +1,8 @@
 class MerchantsController < ApplicationController
-  def index
-    @merchants = Merchant.all
-  end
-  
+  # def index
+  #   @merchants = Merchant.all
+  # end
+  #
   def dashboard#maybeshow
     @merchant = Merchant.find(params[:merchant_id])
   end
