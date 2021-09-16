8926f3cdbb3d6b3cded9c59ad6613b34202661ed
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-09-16 11:23:01 -0600

    Complete edit merchant item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,25 @@ class ItemsController < ApplicationController
     end
 
     def show
+      @merchant = Merchant.find(params[:merchant_id])
       @item = Item.find_by(params[:item_id])
+    end
+
+    def edit
+      @merchant = Merchant.find(params[:merchant_id])
+      @item = Item.find_by(params[:item_id])
+    end
+
+    def update
+      require "pry"; binding.pry
+      merchant = Merchant.find(params[:merchant_id])
+      item = Item.find_by(params[:item_id])
+      item.update(item_params)
+      redirect_to merchant_item_path(merchant, item)
+    end
+
+    private
+    def item_params
+      params.require(:item).permit(:name, :description, :unit_price)
     end
 end
---
+++ spec/features/items/edit_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Item edit page' do
+
+  it 'edits an item' do
+    merchant = Merchant.create!(name: "John")
+    item = merchant.items.create!(name: "Shirt", description: "A blue shirt", unit_price: 30)
+
+    visit edit_merchant_item_path(merchant, item)
+    fill_in "Name", with: "Blue Shirt"
+    click_button "Submit"
+
+    expect(current_path).to eq(merchant_item_path(merchant, item))
+    expect(page).to have_content("Blue Shirt")
+  end
+end
