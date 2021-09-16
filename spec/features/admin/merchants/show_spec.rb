6a9ea76561dd984ed0c6e5270cd1ebd46d210964
Weston Ellis <ellisweston112@gmail.com>
2021-09-16 11:42:38 -0600

    Cleanup

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -15,7 +15,6 @@ RSpec.describe 'admin merchants show page' do
     expect(page).to have_content(merchant_1.name)
   end
 
-
   it 'links to edit page' do
     merchant = create(:merchant)
 
@@ -27,17 +26,4 @@ RSpec.describe 'admin merchants show page' do
 
     expect(current_path).to eq(admin_merchant_path(merchant))
   end
-
-
-
-
 end
-
-
-# Then I see a link to update the merchant's information.
-# When I click the link
-# Then I am taken to a page to edit this merchant
-# And I see a form filled in with the existing merchant attribute information
-# When I update the information in the form and I click ‘submit’
-# Then I am redirected back to the merchant's admin show page where I see the updated information
-# And I see a flash message stating that the information has been successfully updated
