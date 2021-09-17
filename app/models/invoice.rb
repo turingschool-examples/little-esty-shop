438e91db9e2c42a4697e6ce5e86f02775f46bc15
Weston Ellis <ellisweston112@gmail.com>
2021-09-17 15:46:43 -0600

    Write instance method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -16,6 +16,7 @@ class Invoice < ApplicationRecord
     "completed": 2
   }
 
-# Invoice.joins(:invoice_items).where("NOT invoice_items.status = 3")
-
+  def date
+    created_at.strftime("%A, %B %d, %Y")
+  end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -16,20 +16,28 @@ RSpec.describe Invoice do
     @customer = create(:customer)
     @invoice_1 = create(:invoice, customer: @customer)
     @invoice_2 = create(:invoice, customer: @customer)
-    @invoice_3 = create(:invoice, customer: @customer)
+    @invoice_3 = create(:invoice, customer: @customer, created_at: Date.new(2021, 9, 17))
     @merchant = create(:merchant)
     @item_1 = create(:item, merchant: @merchant)
     @item_2 = create(:item, merchant: @merchant)
     @invoice_item_1 = create(:invoice_item, item: @item_1, status: 'shipped', invoice: @invoice_1)
     @invoice_item_2 = create(:invoice_item, item: @item_1, invoice: @invoice_2)
     @invoice_item_3 = create(:invoice_item, item: @item_1, invoice: @invoice_3)
-    # require "pry"; binding.pry
-
   end
 
   describe 'class methods' do
     it "gets all incomplete invoices" do
       expect(Invoice.incomplete_invoices).to eq([@invoice_2, @invoice_3])
+    end
+  end
+
+  describe 'instance methods' do
+    it 'formats date' do
+      expect(@invoice_3.date).to eq('Friday, September 17, 2021')
+    end
+
+    it 'formats customer name' do
+      expect(@invoice_3.customer_name).to eq("#{customer.first_name} #{customer.last_name}")
     end
   end
 end
