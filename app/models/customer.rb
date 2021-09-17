dda9ea6f1ee153e276376985eebac4cdfac957cd
Erin Quinn <equinn125@gmail.com>
2021-09-17 09:56:04 -0600

    fixing some heroku issues

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
   validates_presence_of :first_name
   validates_presence_of :last_name
-  has_many :invoices
+  has_many :invoices, dependent: :destroy
 
   def create
   end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,5 @@
 class Invoice < ApplicationRecord
   validates_presence_of :status
   belongs_to :customer
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
 end
