2caa55a156a84aa31b718654247eaf1ef5ae2073
Michael Mattson <michael.p.mattson@gmail.com>
2021-09-17 15:23:39 -0600

    Remove launchy line, comments

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -58,13 +58,3 @@ RSpec.describe 'Merchant Items Index page' do
     end
   end
 end
-
-
-# Merchant Item Disable/Enable
-#
-# As a merchant
-# When I visit my items index page
-# Next to each item name I see a button to disable or enable that item.
-# When I click this button
-# Then I am redirected back to the items index
-# And I see that the items status has changed
