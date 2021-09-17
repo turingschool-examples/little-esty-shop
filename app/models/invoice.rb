f1a99b990f780622fc5b3fd0bbfce3d9da7d8390
Erin Quinn <equinn125@gmail.com>
2021-09-17 10:16:10 -0600

    update many to many relationship

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,6 @@ class Invoice < ApplicationRecord
   validates_presence_of :status
   belongs_to :customer
   has_many :transactions, dependent: :destroy
+  has_many :invoice_items, dependent: :destroy
+  has_many :items, through: :invoice_items
 end
