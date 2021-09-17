b30fdc89fa206be78ee3627e48689140d9b6f6f6
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-16 18:53:46 -0600

    Add test for update link on show page

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe 'admin merchants show page', type: :feature do
     expect(page).to_not have_content(@merchant_2.name)
     expect(page).to_not have_content(@merchant_3.name)
   end
+
+  it 'has update link' do
+    visit "/admin/merchants/#{@merchant_1.id}"
+
+    expect(page).to have_button("Update #{@merchant_1.name}")
+    expect(page).to_not have_button("Update #{@merchant_2.name}")
+    expect(page).to_not have_button("Update #{@merchant_3.name}")
+
+    click_on "Update #{@merchant_1.name}"
+    expect(current_path).to eq("/admin/merchants/#{@merchant_1.id}/edit")
+  end
 end
