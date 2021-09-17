b46258766688cf430b4349ff171690c213813512
Weston Ellis <ellisweston112@gmail.com>
2021-09-17 15:38:19 -0600

    Tweak tests

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'admin invoice index page' do
       expect(page).to have_link(@invoice_4.id)
     end
 
-    click_link @invoice_1.id
+    click_link "#{@invoice_1.id}"
 
     expect(current_path).to eq(admin_invoice_path(@invoice_1))
   end
--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -3,17 +3,16 @@ require "rails_helper"
 RSpec.describe 'admin show page' do
   before(:each) do
     @customer = create(:customer)
-    @invoice_1 = create(:invoice, customer: @customer, created_at: 'Monday, July 18, 2019')
+    @invoice_1 = create(:invoice, customer: @customer, created_at: Date.new(2021, 9, 17))
   end
 
   it 'shows relevant invoice information' do
-    require "pry"; binding.pry
     visit admin_invoice_path(@invoice_1)
 
     within('#invoice-information') do
       expect(page).to have_content(@invoice_1.id)
       expect(page).to have_content(@invoice_1.status)
-      expect(page).to have_content(@invoice_1.created_at)
+      expect(page).to have_content("Friday, September 17, 2021")
       expect(page).to have_content("#{customer.first_name} #{customer.last_name}")
     end
 
