1fa13af4005626441bdbae96a9974d862a5e93c5
Laura Court <laura.mcourt@gmail.com>
2021-09-16 23:06:30 -0400

    Adds merch items show page

--- spec/features/merchant/items/show_spec.rb
+++ spec/features/merchant/items/show_spec.rb
@@ -10,17 +10,6 @@ RSpec.describe "merchant items show page" do
     @item_4 = create(:item, merchant: @merchant_2)
   end
 
-#   Merchant Items Show Page
-#
-# As a merchant,
-# When I click on the name of an item from the merchant items index page,
-# Then I am taken to that merchant's item's show page (/merchants/merchant_id/items/item_id)
-# And I see all of the item's attributes including:
-#
-# - Name
-# - Description
-# - Current Selling Price
-
   it "links to merchant item's show page" do
 
     visit merchant_items_path(@merchant_1)
@@ -30,7 +19,23 @@ RSpec.describe "merchant items show page" do
     expect(page).to have_content(@item_1.name)
     expect(page).to have_content(@item_1.description)
     expect(page).to have_content(@item_1.unit_price)
+  end
+#
+#   As a merchant,
+# When I visit the merchant show page of an item
+# I see a link to update the item information.
+# When I click the link
+# Then I am taken to a page to edit this item
+# And I see a form filled in with the existing item attribute information
+# When I update the information in the form and I click ‘submit’
+# Then I am redirected back to the item show page where I see the updated information
+# And I see a flash message stating that the information has been successfully updated.
 
+  it 'shows a link to update item information' do
+    visit merchant_item_path(@merchant_1, @item_1)
 
+    expect(page).to have_content("Update Item")
+    click_link "Update Item"
+    expect(current_path).to eq(edit_item_path(@item_1))
   end
 end
