a3637b1481475db42e542c0ac595f95b79e46aab
Erin Quinn <equinn125@gmail.com>
2021-09-18 15:58:31 -0600

    complete user story 14

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'admin merchants index page' do
   before :each do
-    @merchant_1 = Merchant.create!(name: "Bob", id: 1, created_at:" 2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC")
-    @merchant_2 = Merchant.create!(name: "Sara", id: 2, created_at:" 2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC")
+    @merchant_1 = Merchant.create!(name: "Bob",created_at:" 2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC")
+    @merchant_2 = Merchant.create!(name: "Sara",created_at:" 2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC", status: 'disabled')
   end
 
   it 'lists all merchants' do
@@ -27,5 +27,29 @@ RSpec.describe 'admin merchants index page' do
     visit '/admin/merchants'
     click_on("New Merchant")
     expect(current_path).to eq("/admin/merchants/new")
+  end
+
+  it 'has a button to disable the merchant' do
+    visit '/admin/merchants'
+    expect(page).to have_button('Disable Merchant')
+    within "#merchant-#{@merchant_1.id}" do
+      click_button('Disable Merchant')
+      expect(current_path).to eq("/admin/merchants")
+      expect(page).to have_content('disabled')
+    end
+  end
+
+  it 'has a button to enable the merchant' do
+    visit '/admin/merchants'
+    expect(page).to have_button('Disable Merchant')
+    within "#merchant-#{@merchant_1.id}" do
+      click_button('Enable Merchant')
+      expect(current_path).to eq("/admin/merchants")
+      expect(page).to have_content('enabled')
+    end
+  end
+
+  it 'has a section for enabled merchants' do
+    
   end
 end
