45561194227aa097853de0a0dad3ffb7419f829e
Michael Mattson <michael.p.mattson@gmail.com>
2021-09-17 19:42:06 -0600

    Add dash links

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -5,10 +5,21 @@ RSpec.describe 'Merchant Dashboard page' do
     before(:each) do
       @sprouts = create(:merchant)
       visit "/merchants/#{@sprouts.id}/dashboard"
+      save_and_open_page
     end
 
     it 'shows the name of my merchant' do
       expect(page).to have_content('Sprouts')
+    end
+
+    it 'has a link to my items page' do
+      click_link('My Items')
+      expect(current_path).to eq("/merchants/#{@sprouts.id}/items")
+    end
+
+    it 'has a link to my invoices index' do
+      click_link('My Invoices')
+      expect(current_path).to eq("/merchants/#{@sprouts.id}/invoices")
     end
   end
 end
