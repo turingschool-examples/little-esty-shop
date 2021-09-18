8e0672a0f6b896be6633df9a989a57a6b78254c8
Weston Ellis <ellisweston112@gmail.com>
2021-09-18 10:10:55 -0600

    Refactor

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -1,7 +1,4 @@
 require 'rails_helper'
-# As an admin,
-# When I visit the admin merchants index (/admin/merchants)
-# Then I see the name of each merchant in the system
 RSpec.describe 'admin merchants index page' do
   it "shows every merchant" do
     merchant_1 = Merchant.create!(name: 'Weston')
@@ -37,15 +34,8 @@ RSpec.describe 'admin merchants index page' do
     end
 
     expect(current_path).to eq(admin_merchants_path)
-
     within('#enabled-merchants') do
       expect(page).to have_content("Disable: #{merchant_1.name}")
     end
   end
-#   As an admin,
-# When I visit the admin merchants index
-# Then next to each merchant name I see a button to disable or enable that merchant.
-# When I click this button
-# Then I am redirected back to the admin merchants index
-# And I see that the merchant's status has changed
 end
