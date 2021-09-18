f436f88f771a3becd404f31226ddeb8165619a9a
Michael Mattson <michael.p.mattson@gmail.com>
2021-09-18 11:12:14 -0600

    Add full_name method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ApplicationRecord
   has_many :invoices, dependent: :destroy
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe Customer, type: :model do
   describe 'relationships' do
     it { should have_many(:invoices).dependent(:destroy) }
   end
+
+  before(:each) do
+    @customer = create(:customer)
+  end
+
+  describe '#instance methods' do
+    describe '#full_name' do
+      it 'returns customers full name' do
+        expect(@customer.full_name).to eq("John Doe")
+      end
+    end
+  end
 end
