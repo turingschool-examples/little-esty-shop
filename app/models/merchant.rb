e4209f912cc2d09164b7a70913e19775fc94a0a4
Laura Court <laura.mcourt@gmail.com>
2021-09-19 18:17:38 -0400

    Adds packaged items method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,14 @@ class Merchant < ApplicationRecord
 
   validates :name, presence: true
 
+  def self.enabled_merchants
+    where(status: true)
+  end
+
+  def self.disabled_merchants
+    where(status: false)
+  end
+
   def merchant_invoices
     Invoice.joins(:items).where("items.merchant_id = ?", id)
   end
@@ -14,12 +22,8 @@ class Merchant < ApplicationRecord
   def disabled
     items.where(status: 0)
   end
-  
-  def self.enabled_merchants
-    Merchant.where(status: true)
-  end
 
-  def self.disabled_merchants
-    Merchant.where(status: false)
+  def packaged_items
+    items.joins(:invoice_items).where('invoice_items.status = ?', 0)
   end
 end
