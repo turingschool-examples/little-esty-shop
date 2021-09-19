41b0f079af4ac10c93a0f3fbe569a235cd72eff2
Jessica Jones <daddehjones@gmail.com>
2021-09-18 20:08:14 -0400

    Define create action. Include flash message.

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -6,11 +6,19 @@ class MerchantItemsController < ApplicationController
 
   def new
     @merchant = Merchant.find(params[:merchant_id])
-    item = Item.new
-    item.save
-    redirect_to merchant_items_path(@merchant)
   end
-  
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    item = merchant.items.new(item_params)
+    if item.save
+      redirect_to merchant_items_path(merchant)
+    else
+      flash[:alert] = "Item still not created. Please investigate."
+      redirect_to new_merchant_item_path(merchant)
+    end
+  end
+
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @item = Item.find(params[:id])
@@ -29,5 +37,11 @@ class MerchantItemsController < ApplicationController
       item.update(status: 0)
     end
     redirect_to merchant_items_path(@merchant)
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price)
   end
 end
