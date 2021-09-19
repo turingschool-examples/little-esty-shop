c73a8ec24c0dff943ee32d735d561cdbaa264889
Blake Dempsey <bdempsey864@gmail.com>
2021-09-19 17:05:08 -0600

    Refactor merchant invoices index

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -1,6 +1,5 @@
 class MerchantInvoicesController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @merchant_invoices = Invoice.merchant_invoices(params[:merchant_id])
   end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,8 +3,4 @@ class Invoice < ApplicationRecord
   has_many   :transactions,  dependent: :destroy
   has_many   :invoice_items, dependent: :destroy
   has_many   :items, through: :invoice_items
-
-  def self.merchant_invoices(merchant_id)
-    joins(items: :invoice_items).where(items:{merchant_id: merchant_id.to_i})
-  end
 end
