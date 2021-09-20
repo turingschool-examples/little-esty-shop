f7c1991813e9b803788f0054fea30b9f9494b04c
Weston Ellis <ellisweston112@gmail.com>
2021-09-20 15:20:28 -0600

    Add update action

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -7,4 +7,16 @@ class Admin::InvoicesController < ApplicationController
     @invoice = Invoice.find(params[:id])
     @items = @invoice.items
   end
+
+  def update
+    invoice = Invoice.find(params[:id])
+
+    invoice.update(invoice_params)
+    redirect_to(admin_invoice_path(invoice))
+  end
+
+  private
+  def invoice_params
+    params.require(:invoice).permit(:status)
+  end
 end
