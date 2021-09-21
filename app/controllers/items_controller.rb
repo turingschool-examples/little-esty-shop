34014d3c0bb0037bb310eeadaefd502229c9856b
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-09-21 16:02:01 -0600

    Story complete for creating new merchant item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,23 @@ class ItemsController < ApplicationController
       @item = Item.find_by(params[:item_id])
     end
 
+    def new
+      @merchant = Merchant.find(params[:merchant_id])
+      @item = Item.new
+    end
+
+    def create
+      merchant = Merchant.find(params[:merchant_id])
+      item = merchant.items.create(item_params)
+      if item.save
+        redirect_to merchant_items_path(merchant)
+        flash[:alert] = "New item for #{merchant.name} has been created."
+      else
+        redirect_to new_merchant_item_path(merchant)
+        flash[:alert] = "Error creating item. Please try again."
+      end
+    end
+
     def edit
       @merchant = Merchant.find(params[:merchant_id])
       @item = Item.find_by(params[:item_id])
@@ -28,12 +45,12 @@ class ItemsController < ApplicationController
     end
 
     private
-    
+
     def item_params
       if not params[:item]
         params.permit(:status ,:name, :description, :unit_price)
       else
-        params.require(:item).permit(:name, :description, :unit_price)
+        params.require(:item).permit(:name, :description, :unit_price, :status)
       end
     end
 end
