7c22c53d92b70368814f08ea5864c7bf6beb18c1
Weston Ellis <ellisweston112@gmail.com>
2021-09-21 11:56:43 -0600

    Add AR relations test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Merchant do
   describe 'relationships' do
     it { should have_many(:items) }
+    it { should have_many(:invoices).through(:items)}
+    it { should have_many(:customers).through(:invoices)}
   end
 
   describe 'validations' do
