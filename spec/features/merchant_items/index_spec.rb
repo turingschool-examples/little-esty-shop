ead1d4f69324aaa63c1044df19dec0a9b67a44ce
Philip Medlin <phmedlin@gmail.com>
2021-11-02 14:04:18 -0600

    Feat/tests: MerchantItemsIndex

---
+++ spec/features/merchant_items/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Items Index' do
+  before :each do
+    @merchant = Merchant.create!(name: "Angela's Shop")
+    @item_1 = @merchant.items.create!(name: "Jade Rabbit", description: "25mmx25mm hand carved jade rabbit", unit_price: 2500)
+    @item_2 = @merchant.items.create!(name: "Amazonite Rabbit", description: "25mmx25mm hand carved Amazonite rabbit", unit_price: 3500)
+  end
+
+  it 'lists the names of all the items' do
+
+    visit "merchants/#{@merchant.id}/items"
+
+    expect(page).to have_content(@item_1.name)
+    expect(page).to have_content(@item_2.name)
+
+
+  end
+
+end
