c9f9a0681d0f1e3fc0fe64424f024306177d7ad6
Alex McConnell <alexmmcconnell@gmail.com>
2021-11-02 15:34:58 -0600

    Feat - Created WIP helper methods for top customer method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
   has_many :invoices
+
+  def successful_transactions_count#(merchant_id)
+    invoice_ids = self.invoices.pluck(:id)
+    transaction_ids = Transaction.where(invoice_id: invoice_ids).where(result: 'success').count
+  end
 end
+
+# find all customers related to merchant
+# number of successful transactions for each customer (done)
+# return top 5 customers with highest # of successful transactions
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,15 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def top_customers
+    item_ids = self.items.pluck(:id)
+    invoice_ids = InvoiceItem.where(item_id: item_ids).distinct.pluck(:invoice_id)
+    transaction_ids = Transaction.where(invoice_id: invoice_ids).pluck(:id)
+
+    customer_ids = Invoice.where(id: invoice_ids).pluck(:customer_id)
+    customers = Customer.where(id: customer_ids)
+
+    binding.pry
+  end
 end
+# find all customers related to merchant
