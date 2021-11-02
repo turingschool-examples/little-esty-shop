193a942198aeca60dd28cdf50530f5d92e701741
Philip Medlin <phmedlin@gmail.com>
2021-11-02 15:19:55 -0600

    Feat/tests: MerchantItem Update

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -7,4 +7,18 @@ class MerchantItemsController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def edit
+    @item = Item.find(params[:item_id])
+  end
+
+  def update
+    item = Item.find(params[:item_id])
+    item.update!(item_params)
+    redirect_to "/merchants/#{item.merchant_id}/items/#{item.id}"
+  end
+
+  private
+    def item_params
+      params.permit(:name, :description, :unit_price)
+    end
 end
--- spec/features/merchant_items/show_spec.rb
+++ spec/features/merchant_items/show_spec.rb
@@ -14,4 +14,24 @@ RSpec.describe 'Merchant Items Show' do
     expect(page).to have_content(@item_1.description)
     expect(page).to have_content("$25.00")
   end
+
+  it 'can edit the item' do
+
+    visit "/merchants/#{@merchant.id}/items/#{@item_1.id}"
+
+    within('#update_link') do
+      click_link
+    end
+
+    fill_in 'Name', with: "Moazinite Rabbit"
+    fill_in 'Description', with: "25mmx25mm Moazinite Rabbit"
+    fill_in 'Unit price', with: "4500"
+
+    click_button
+
+    expect(current_path).to eq("/merchants/#{@merchant.id}/items/#{@item_1.id}")
+    expect(page).to have_content "Moazinite Rabbit"
+    expect(page).not_to have_content "Jade Rabbit"
+  end
+
 end
