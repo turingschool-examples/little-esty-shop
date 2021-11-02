c74837a7713cb5d624fc4e0a28e0d644cdfc6409
Alex McConnell <alexmmcconnell@gmail.com>
2021-11-02 15:34:35 -0600

    Test - Create initial tests for top customers and helper methods

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it {should have_many :items}
   end
+
+  describe '#top_customers' do
+    before(:each) do
+      @merchant = Merchant.first
+    end
+    it 'returns the top 5 customers for the given merchant' do
+
+      expect(@merchant.top_customers.length).to eq(5)
+    end
+  end
 end
