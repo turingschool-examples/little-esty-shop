1d19e9f9867159d4c5ead648b4c23f689891c353
Nathan Brown <nathan.brown263@gmail.com>
2021-11-03 10:49:43 -0600

    feat: Add last two stories in merchant dashboard

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,18 +2,8 @@ class Merchant < ApplicationRecord
   has_many :items
 
   def top_customers
-    # item_ids = self.items.pluck(:id)
-    # invoice_ids = InvoiceItem.where(item_id: item_ids).distinct.pluck(:invoice_id)
-    # transaction_ids = Transaction.where(invoice_id: invoice_ids).pluck(:id)
-    # require "pry"; binding.pry
-    # customer_ids = Invoice.where(id: invoice_ids).pluck(:customer_id)
-    # customers = Customer.where(id: customer_ids)
-    # select("customers.*, COUNT(transactions.id)")
-    #     .joins("INNER JOIN invoice_items ON items.id = invoice_items.item_id INNER JOIN transactions ON invoice_items.invoice_id = transactions.invoice_id")
-    #     .where("transactions.result = 'success'").joins("INNER JOIN invoices ON transactions.invoice_id = invoices.id INNER JOIN customers ON invoices.customer_id = customers.id")
-    # #     .group("customers.id")
-
-    result = ActiveRecord::Base.connection.exec_query("SELECT CONCAT(customers.first_name, ' ', customers.last_name) AS name, COUNT(transactions.*) AS transaction_count
+    ActiveRecord::Base.connection.exec_query(
+                         "SELECT CONCAT(customers.first_name, ' ', customers.last_name) AS name, COUNT(transactions.*) AS transaction_count
                           FROM merchants
                           INNER JOIN items ON merchants.id = items.merchant_id
                           INNER JOIN invoice_items ON items.id = invoice_items.item_id
@@ -24,6 +14,18 @@ class Merchant < ApplicationRecord
                           GROUP BY customers.id
                           ORDER BY transaction_count
                           DESC
-                          LIMIT 5").rows
+                          LIMIT 5"
+                        ).rows
+  end
+
+  def shippable_items
+    ActiveRecord::Base.connection.exec_query(
+                         "SELECT items.name, invoice_items.id, invoice_items.created_at
+                          FROM items
+                          INNER JOIN invoice_items ON items.id = invoice_items.item_id
+                          INNER JOIN merchants ON merchants.id = items.merchant_id
+                          WHERE invoice_items.status = '0' AND merchants.id = '#{self.id}'
+                          ORDER BY invoice_items.created_at"
+                        ).rows
   end
 end
--- spec/features/merchant_dashboard/show_spec.rb
+++ spec/features/merchant_dashboard/show_spec.rb
@@ -21,4 +21,18 @@ RSpec.describe 'merchant dashboard show page' do
     expect(@merchant.top_customers[2][0]).to appear_before(@merchant.top_customers[3][0])
     expect(@merchant.top_customers[3][0]).to appear_before(@merchant.top_customers[4][0])
   end
+
+  it 'should have top customers names in correct order' do
+    expect(@merchant.top_customers[0][0]).to appear_before(@merchant.top_customers[1][0])
+    expect(@merchant.top_customers[1][0]).to appear_before(@merchant.top_customers[2][0])
+    expect(@merchant.top_customers[2][0]).to appear_before(@merchant.top_customers[3][0])
+    expect(@merchant.top_customers[3][0]).to appear_before(@merchant.top_customers[4][0])
+  end
+
+  it 'should have items ready to ship in order from oldest to newest' do
+    expect(page).to have_content("Items Ready to Ship")
+    expect(page).to have_content(@merchant.shippable_items[0][0])
+    expect(page).to have_content(@merchant.shippable_items[0][1])
+    expect(@merchant.shippable_items[0][0]).to appear_before(@merchant.shippable_items[10][0])
+  end
 end
