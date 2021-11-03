e1135e1715fb7cd331cb9795b7084c5a3c6e8d97
Nathan Brown <nathan.brown263@gmail.com>
2021-11-03 00:05:35 -0600

    feat: Add top customers to merchant dashboard

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -13,18 +13,6 @@ class Merchant < ApplicationRecord
     #     .where("transactions.result = 'success'").joins("INNER JOIN invoices ON transactions.invoice_id = invoices.id INNER JOIN customers ON invoices.customer_id = customers.id")
     # #     .group("customers.id")
     
-    # customers = Customer.find_by_sql("SELECT customers.id, customers.first_name, customers.last_name, COUNT(transactions.*) AS transaction_count
-    #                       FROM merchants
-    #                       INNER JOIN items ON merchants.id = items.merchant_id
-    #                       INNER JOIN invoice_items ON items.id = invoice_items.item_id
-    #                       INNER JOIN transactions ON invoice_items.invoice_id = transactions.invoice_id
-    #                       INNER JOIN invoices ON transactions.invoice_id = invoices.id
-    #                       INNER JOIN customers ON invoices.customer_id = customers.id
-    #                       WHERE transactions.result = 'success' AND merchants.id = '#{self.id}'
-    #                       GROUP BY customers.id
-    #                       ORDER BY transaction_count
-    #                       DESC
-    #                       LIMIT 5")
     result = ActiveRecord::Base.connection.exec_query("SELECT CONCAT(customers.first_name, ' ', customers.last_name) AS name, COUNT(transactions.*) AS transaction_count
                           FROM merchants
                           INNER JOIN items ON merchants.id = items.merchant_id
