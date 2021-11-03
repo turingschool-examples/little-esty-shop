59895c664c487e478c13a8cdd3254de3902f2692
Lesley Sanders <lesleyasanders@gmail.com>
2021-11-03 13:57:48 -0400

    Update relationships in models

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -1,6 +1,8 @@
 class MerchantInvoicesController < ApplicationController
 
   def index
-    @invoice = Invoice.find(params[:invoice_id])
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoices = @merchant.invoices
+    # @invoice = Invoice.find(params[:invoice_id])
   end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,6 +3,6 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   belongs_to :customer
-  
+
   enum status: [:cancelled, :completed, 'in progress']
 end
