3e50b47d505104b879d8aa60b4e2ec2e12431d47
Ted S <tstaros23@gmail.com>
2021-11-03 15:25:34 -0600

    Finish model testing

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,5 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
-  has_one :merchant, through: :item
   enum status: [:packaged, :pending, :shipped]
 end
---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Merchant, type: :model do
+  describe 'relationships' do
+    it {should have_many :items}
+    it {should have_many(:invoice_items).through(:items)}
+    it {should have_many(:invoices).through(:invoice_items)}
+  end
+end
