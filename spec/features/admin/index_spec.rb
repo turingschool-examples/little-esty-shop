fc58191769abf9f272724de5be8dcfb6d27909c9
Micha Bernhard <mib70@pitt.edu>
2021-11-03 15:31:55 -0600

    Test admin user story 3

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -19,7 +19,35 @@ RSpec.describe 'Admin Index' do
     visit admin_index_path
     
     click_link "Invoices"
-    
+
     expect(current_path).to eq('/admin/invoices')
+  end
+
+#   Admin Dashboard Statistics - Top Customers
+
+  # As an admin,
+  # When I visit the admin dashboard
+  # Then I see the names of the top 5 customers
+  # who have conducted the largest number of successful transactions
+  # And next to each customer name I see the number of successful transactions they have
+  # conducted
+  it 'has statistics about the best customers' do
+    customer1 = Customer.create!(first_name: "Bob", last_name: "Dylan")
+    customer2 = Customer.create!(first_name: "Micha", last_name: "B")
+    customer3 = Customer.create!(first_name: "Christian", last_name: "V")
+    invoice1 = Invoice.create!(customer_id: customer1.id, status: 'completed')
+    invoice2 = Invoice.create!(customer_id: customer1.id, status: 'completed')
+    invoice3 = Invoice.create!(customer_id: customer1.id, status: 'completed')
+    invoice4 = Invoice.create!(customer_id: customer2.id, status: 'completed')
+    invoice5 = Invoice.create!(customer_id: customer2.id, status: 'completed')
+    invoice6 = Invoice.create!(customer_id: customer3.id, status: 'completed')
+    invoice7 = Invoice.create!(customer_id: customer3.id, status: 'cancelled')
+    transaction1 = Transaction.create!(invoice_id: invoice1.id, credit_card_number: 123, credit_card_expiration_date: nil, result: 'success')
+    transaction2 = Transaction.create!(invoice_id: invoice2.id, credit_card_number: 123, credit_card_expiration_date: nil, result: 'success')
+    transaction3 = Transaction.create!(invoice_id: invoice3.id, credit_card_number: 123, credit_card_expiration_date: nil, result: 'success')
+    transaction4 = Transaction.create!(invoice_id: invoice4.id, credit_card_number: 123, credit_card_expiration_date: nil, result: 'success')
+    transaction5 = Transaction.create!(invoice_id: invoice5.id, credit_card_number: 123, credit_card_expiration_date: nil, result: 'success')
+    transaction6 = Transaction.create!(invoice_id: invoice6.id, credit_card_number: 123, credit_card_expiration_date: nil, result: 'success')
+    transaction7 = Transaction.create!(invoice_id: invoice7.id, credit_card_number: 123, credit_card_expiration_date: nil, result: 'success')
   end
 end
