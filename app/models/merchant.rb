5c44ff5fb9401585d6864e8d70c8a0675996b483
Lesley Sanders <lesleyasanders@gmail.com>
2021-11-03 18:30:57 -0400

    Work on top 5 method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,14 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
+
+  def top_5
+    x = items
+    .joins(invoices: :transactions)
+    require "pry"; binding.pry
+    .where(transactions.result = 'success')
+
+    # .limit(5)
+
+  end
 end
