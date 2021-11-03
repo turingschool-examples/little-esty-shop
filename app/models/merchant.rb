29ea4e284ac9b39e7a19e8c78ebc557e940a3cd0
Nathan Brown <nathan.brown263@gmail.com>
2021-11-03 09:24:47 -0600

    test: Add rspec for top merchants

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -12,7 +12,7 @@ class Merchant < ApplicationRecord
     #     .joins("INNER JOIN invoice_items ON items.id = invoice_items.item_id INNER JOIN transactions ON invoice_items.invoice_id = transactions.invoice_id")
     #     .where("transactions.result = 'success'").joins("INNER JOIN invoices ON transactions.invoice_id = invoices.id INNER JOIN customers ON invoices.customer_id = customers.id")
     # #     .group("customers.id")
-    
+
     result = ActiveRecord::Base.connection.exec_query("SELECT CONCAT(customers.first_name, ' ', customers.last_name) AS name, COUNT(transactions.*) AS transaction_count
                           FROM merchants
                           INNER JOIN items ON merchants.id = items.merchant_id
--- spec/features/merchant_dashboard/show_spec.rb
+++ spec/features/merchant_dashboard/show_spec.rb
@@ -14,4 +14,11 @@ RSpec.describe 'merchant dashboard show page' do
     has_link?("My Items")
     has_link?("My Invoices")
   end
+
+  it 'should have top customers names in correct order' do
+    expect(@merchant.top_customers[0][0]).to appear_before(@merchant.top_customers[1][0])
+    expect(@merchant.top_customers[1][0]).to appear_before(@merchant.top_customers[2][0])
+    expect(@merchant.top_customers[2][0]).to appear_before(@merchant.top_customers[3][0])
+    expect(@merchant.top_customers[3][0]).to appear_before(@merchant.top_customers[4][0])
+  end
 end
