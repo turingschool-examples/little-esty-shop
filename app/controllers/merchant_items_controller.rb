f1de450952e5ba4b95fe782674196eb6b51aa9a2
Ted S <tstaros23@gmail.com>
2021-11-03 10:41:50 -0600

    Create merchant show page that shows item attributes

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -1,6 +1,9 @@
 class MerchantItemsController < ApplicationController
   def index
-    # require "pry"; binding.pry
     @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 end
