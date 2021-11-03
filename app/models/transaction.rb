cb2495af970473b402717ce6ec029f78b334188d
Chaz Simons <chaz.simons@gmail.com>
2021-11-03 11:12:08 -0700

    Play around with AR querying

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,14 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
   enum result: [:success, :failed]
+
+  def top_5
+    vip = Transaction.where(result: 0).joins(:invoice, :customers)
+
+    # vip.map do |transactioninvoice|
+    #   transactioninvoice.invoice.customer
+    # end
+    #
+    # vip.order()
+  end
 end
---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Transaction do
+
+  it 'can help us solve this AR stuff' do
+  end
+
+end
