693bcfdce91f067143f0f98eb6161a071fb86bfc
Micha Bernhard <mib70@pitt.edu>
2021-11-03 18:25:28 -0600

    Work on admin user story 3

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,13 +1,13 @@
 class Customer < ApplicationRecord
   has_many :invoices
 
-  # def self.top_customers
-  #   @customers = joins(invoices: :transactions).where(transactions: {result: 'success'})
-  #   binding.pry
-  #   @customers.each do |customer|
-  #     customer.invoices.each do |invoice|
-  #       invoice.transactions.count
-  #     end
-  #   end
-  #  
+  def self.top_customers
+    customers = joins(invoices: :transactions).where(transactions: {result: 'success'})
+    first_name_hash = customers.group(:first_name).count
+    customer_hash = Hash.new
+    first_name_hash.each do |k, v|
+      customer_hash[Customer.find_by(first_name: k)] = v
+    end
+    result = customer_hash.sort_by { |name, age| age }.reverse
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,3 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
-
-  def self.total_transactions(customer)
-    transactions = joins(invoices: :customers).where(customer_id: customer.id).count
-    binding.pry
-  end
 end
