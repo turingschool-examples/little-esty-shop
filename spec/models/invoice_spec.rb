18e645d16adb08edb0e18b3b3a3621456c3492fc
Micha Bernhard <mib70@pitt.edu>
2021-11-04 17:54:52 -0600

    Model test for admin user story 4

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Invoice, type: :model do
     it {should belong_to :customer}
     it {should have_many :transactions}
   end
+
+  describe '::pending_invoices' do
+    it 'returns an array of pending invoices' do
+      customer1 = Customer.create!(first_name: "Bob", last_name: "Dylan")
+      invoice3 = Invoice.create!(customer_id: customer1.id, status: 'in progress')
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 'in progress')
+      invoice2 = Invoice.create!(customer_id: customer1.id, status: 'completed')
+
+      expect(Invoice.pending_invoices).to eq([invoice3, invoice1])
+    end
+  end
 end
