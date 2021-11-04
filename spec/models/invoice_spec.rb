7e8dade1f8e53a2bc8fcf9f4a8491dde75ff9251
Micha Bernhard <mib70@pitt.edu>
2021-11-03 18:24:39 -0600

    add describe block for relationship

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  it {should belong_to :customer}
-  it {should have_many :transactions}
+  describe 'relationships' do
+    it {should belong_to :customer}
+    it {should have_many :transactions}
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
- it { should belong_to :invoice }
+  describe 'relationships' do
+    it { should belong_to :invoice }
+  end
 end
