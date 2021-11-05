0005270298936cba39396c6c87453300feb9ac0e
Jacob Yarborough <jayarborough95@gmail.com>
2021-11-05 17:24:47 -0600

    Add test for top five merchant items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -51,4 +51,44 @@ RSpec.describe Merchant, type: :model do
       end 
     end 
   end
+
+  describe 'instance methods' do 
+    before(:each) do 
+      @merchant = create(:merchant)
+
+      @customer1 = create :customer
+      @customer2 = create :customer
+
+      @item1 = create :item, { merchant_id: @merchant.id }
+      @item2 = create :item, { merchant_id: @merchant.id }
+      @item3 = create :item, { merchant_id: @merchant.id }
+      @item4 = create :item, { merchant_id: @merchant.id }
+      @item5 = create :item, { merchant_id: @merchant.id }
+      @item6 = create :item, { merchant_id: @merchant.id }
+
+      @invoice1 = create :invoice, { customer_id: @customer1.id }
+      @invoice2 = create :invoice, { customer_id: @customer2.id }
+      @invoice3 = create :invoice, { customer_id: @customer2.id }
+
+      @transaction1 = create :transaction, { invoice_id: @invoice1.id, result: 'success' }
+      @transaction2 = create :transaction, { invoice_id: @invoice1.id, result: 'success' }
+      @transaction3 = create :transaction, { invoice_id: @invoice1.id, result: 'success' }
+      @transaction4 = create :transaction, { invoice_id: @invoice2.id, result: 'success' }
+      @transaction5 = create :transaction, { invoice_id: @invoice2.id, result: 'success' }
+      @transaction6 = create :transaction, { invoice_id: @invoice3.id, result: 'failed' }
+
+      @inv_item1 = create :invoice_item, { item_id: @item1.id, invoice_id: @invoice1.id, unit_price: 10000, quantity: 1}
+      @inv_item2 = create :invoice_item, { item_id: @item2.id, invoice_id: @invoice1.id, unit_price: 9000, quantity: 1}
+      @inv_item3 = create :invoice_item, { item_id: @item3.id, invoice_id: @invoice2.id, unit_price: 8000, quantity: 1}
+      @inv_item4 = create :invoice_item, { item_id: @item4.id, invoice_id: @invoice2.id, unit_price: 7000, quantity: 1}
+      @inv_item5 = create :invoice_item, { item_id: @item5.id, invoice_id: @invoice3.id, unit_price: 6000, quantity: 1}
+      @inv_item6 = create :invoice_item, { item_id: @item6.id, invoice_id: @invoice3.id, unit_price: 10000000000000, quantity: 1}
+    end 
+
+    describe '#top_five_items' do 
+      it 'retruns the top five items by total revenue' do 
+        expect(@merchant.top_five_items).to eq([@item1,@item2,@item3,@item4,@item5])
+      end 
+    end 
+  end 
 end
