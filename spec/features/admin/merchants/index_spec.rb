41db8b28ab3769a831b926769b901bcc7814927f
Stephanie Helm <stephaniemhelm@gmail.com>
2021-11-05 15:15:57 -0600

    user story 1

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "admin dashboard" do
+RSpec.describe "admin merchants" do
   before do
     @merchant = create(:merchant)
+    @merchant1 = create(:merchant)
+    @merchant2 = create(:merchant)
+    @merchant3 = create(:merchant)
+    @merchant4 = create(:merchant)
 
     @customer1 = create :customer
     @customer2 = create :customer
@@ -34,45 +38,19 @@ RSpec.describe "admin dashboard" do
     @inv_item5 = create :invoice_item, { item_id: @item.id, invoice_id: @invoice5.id, status: "packaged"}
     @inv_item6 = create :invoice_item, { item_id: @item.id, invoice_id: @invoice6.id, status: "shipped"}
 
-    visit admin_dashboard_path
+    visit admin_merchants_path
+
   end
 
-  it 'shows that you are on the admin dashboard' do
-    expect(page).to have_content("Admin Dashboard")
-  end
-
-  it 'can link to admin merchants index' do
-    click_link("My Merchants")
+  it 'can visit admin merchants index and see the name of each merchant' do
     expect(current_path).to eq(admin_merchants_path)
+
+    expect(page).to have_content(@merchant.name)
+    expect(page).to have_content(@merchant1.name)
+    expect(page).to have_content(@merchant2.name)
+    expect(page).to have_content(@merchant3.name)
+    expect(page).to have_content(@merchant4.name)
+
   end
 
-  it 'can link to admin invoices index' do
-    click_link("My Invoices")
-    expect(current_path).to eq(admin_invoices_path)
-  end
-
-  it 'i see the names of the top 5 cust (>success trans) with success trans count' do
-    within("#top5") do
-      within("#cust-#{@customer1.id}") do
-        expect(page).to have_content(@customer1.full_name)
-        expect(page).to have_content(1)
-      end
-    end
-  end
-
-  it 'I see a section for incomplete invoices, with a link to each invoice id, and the date it was created' do
-    within("#incomplete_invoices") do
-      within("#invoice-#{@invoice1.id}") do
-        expect(page).to have_content(@invoice1.id)
-        expect(page).to have_content(@invoice1.created_at.strftime('%A, %B%e, %Y'))
-        click_link(@invoice1.id)
-        expect(current_path).to eq(admin_invoice_path(@invoice1))
-      end
-    end
-  end
-
-  it 'the incomplete invoices list is sorted from oldest to newest' do
-    expect("(Invoice #{@invoice4.id})").to appear_before("(Invoice #{@invoice3.id})")
-    expect("(Invoice #{@invoice2.id})").to appear_before("(Invoice #{@invoice1.id})")
-  end
 end
