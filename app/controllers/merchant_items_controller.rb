d5f160170c9dd15af5852b74e7ae71f2a2fc4527
Sam Devine <samuel.will.devine@gmail.com>
2021-11-05 19:03:46 -0600

    Merchant Items Index create a new item

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -1,8 +1,27 @@
 class MerchantItemsController < ApplicationController
   def index
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def show
     @item = Item.find(params[:item_id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.new
+  end
+
+  def create
+    binding.pry
+    params[:unit_price] = params[:price].to_i * 100
+    merchant = Merchant.find(params[:merchant_id])
+    item = merchant.items.create(item_params)
+    redirect_to "/merchants/#{params[:merchant_id]}/items"
+  end
+
+  private
+    def item_params
+      params.permit(:name, :description, :unit_price)
+    end
 end
--- spec/features/merchant_items/index_spec.rb
+++ spec/features/merchant_items/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.desribe 'the merchants items index' do
-  describe 'as a merchant'
+RSpec.describe 'the merchants items index' do
+  describe 'as a merchant' do
 
     xit 'each item on my items index is a link to the show page' do
       merchant = Merchant.create(name: 'Toys and Stuff')
@@ -15,6 +15,35 @@ RSpec.desribe 'the merchants items index' do
       click_on item.name
 
       expect(current_path).to eq "/merchants/#{merchant.id}/items/#{item.id}"
+    end
+
+    describe 'item create' do
+      before :each do
+        @merchant = Merchant.create(name: 'Toys and Stuff')
+      end
+
+      it 'I see a link to create a new item' do
+        visit "/merchants/#{@merchant.id}/items"
+
+        expect(page).to have_link "Create a new item"
+
+        click_link "Create a new item"
+
+        expect(current_path).to eq "/merchants/#{@merchant.id}/items/new"
+      end
+
+      it 'the page has a form that creates a new item when submitted' do
+        visit "/merchants/#{@merchant.id}/items/new"
+        fill_in :name, with: 'fidget spinner'
+        fill_in :description, with: 'it spins'
+        fill_in :price, with: '15.00'
+        click_on 'Save'
+
+        expect(current_path).to eq "/merchants/#{@merchant.id}/items"
+        expect(page).to have_content 'fidget spinner'
+        # user story says the item's status should be disabled by default?
+        # items don't have a status so I'm not sure about this one...
+      end
     end
   end
 end
