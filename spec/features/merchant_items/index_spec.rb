84fc0ca941bd6aee0c6b213e6d9d8b0e835797eb
Sam Devine <samuel.will.devine@gmail.com>
2021-11-06 11:47:02 -0600

    more giant test setups

--- spec/features/merchant_items/index_spec.rb
+++ spec/features/merchant_items/index_spec.rb
@@ -61,5 +61,45 @@ RSpec.describe 'the merchants items index' do
         # items don't have a status so I'm not sure about this one...
       end
     end
+
+    describe 'top items by revenue section' do
+      it 'displays the top 5 items by revenue (ii.quantity * ii.unit_price)' do
+        merchant = Merchant.create(name: "Friendly Traveling Merchant")
+        item_1 = merchant.items.create(name: 'YoYo', description: 'its on a string', unit_price: 1000)
+        item_2 = merchant.items.create(name: 'raisin', description: 'dried grape', unit_price: 100)
+        item_3 = merchant.items.create(name: 'apple', description: 'nice and crisp', unit_price: 500)
+        item_4 = merchant.items.create(name: 'banana', description: 'mushy but good', unit_price: 200)
+        item_5 = merchant.items.create(name: 'pear', description: 'refreshing treat', unit_price: 600)
+        customer_1 = Customer.create(first_name: 'George', last_name: 'Washington')
+        invoice_1 = customer_1.invoices.create(status: 'Completed')
+        invoice_2 = customer_1.invoices.create(status: 'Completed')
+        invoice_3 = customer_1.invoices.create(status: 'Completed')
+        invoice_item_1 = invoice_1.invoice_items.create(item_id: item_1.id, quantity: 3, unit_price: 1000, status: 'shipped')
+        invoice_item_2 = invoice_1.invoice_items.create(item_id: item_2.id, quantity: 1, unit_price: 100, status: 'shipped')
+        invoice_item_3 = invoice_1.invoice_items.create(item_id: item_3.id, quantity: 1, unit_price: 400, status: 'shipped')
+        invoice_item_4 = invoice_2.invoice_items.create(item_id: item_4.id, quantity: 1, unit_price: 200, status: 'shipped')
+        invoice_item_5 = invoice_2.invoice_items.create(item_id: item_5.id, quantity: 1, unit_price: 600, status: 'shipped')
+        invoice_item_6 = invoice_3.invoice_items.create(item_id: item_5.id, quantity: 2, unit_price: 500, status: 'shipped')
+        invoice_item_7 = invoice_3.invoice_items.create(item_id: item_5.id, quantity: 1, unit_price: 600, status: 'shipped')
+        transaction_1 = invoice_1.transactions.create(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'failed')
+        transaction_2 = invoice_1.transactions.create(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+        transaction_3 = invoice_2.transactions.create(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+        transaction_4 = invoice_3.transactions.create(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'failed')
+        transaction_4 = invoice_3.transactions.create(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+
+        visit "/merchants/#{merchant.id}/items"
+        save_and_open_page
+        expect(page).to have_content 'Top Items'
+
+        within 'div#top-items' do
+          expect(item_1.name).to appear_before item_5.name
+          expect(item_5.name).to appear_before item_3.name
+          expect(item_3.name).to appear_before item_4.name
+          expect(item_4.name).to appear_before item_2.name
+
+          expect(page).to have_content "#{item_5.name} - $22.00 in sales"
+        end
+      end
+    end
   end
 end
