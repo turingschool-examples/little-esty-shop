2a485d9a51847308727173db1b843a10567e254c
Gregory Fischer <gwhoisj@gmail.com>
2021-11-06 17:03:40 -0400

    hopefully fixed

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,18 +8,4 @@ RSpec.describe Invoice, type: :model do
     it {should have_many :invoice_items}
     it {should have_many(:items).through(:invoice_items)}
   end
-
-  describe 'class methods' do
-    before :each do
-      @invoice1 = create(:invoice, created_at: "2012-03-25 09:54:09 UTC")
-      @invoice2 = create(:invoice, created_at: "2012-03-26 06:54:10 UTC")
-      @invoice3 = create(:invoice, created_at: "2012-03-26 06:54:10 UTC")
-    end
-
-    describe '#highest_date' do
-      it 'should return the date with the highest number of invoices' do
-        expect(Invoice.highest_date).to eq(@invoice2.created_at)
-      end
-    end
-  end
 end
