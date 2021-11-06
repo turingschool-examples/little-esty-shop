a96c5cdb1df5d5bf3542540353a42092a6428e3b
Billy Frey <bfrey16@gmail.com>
2021-11-06 12:31:03 -0600

    test: added model testing for US20 and updated method to reflect edge cases

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,11 +4,18 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
 
   def number_of_transactions
-    Customer.select('customers.id as id, count(transactions) as transaction_count').where("transactions.result = '0'").joins(:invoices).group("customers.id").joins(:transactions).group("invoices.id").find_by(id: self.id).transaction_count
+    if self.invoices.count != 0
+      join_table = Customer.select("customers.id as id, customers.first_name as first_name, customers.last_name as last_name, count(transactions) as transaction_count").where("transactions.result = '0'").group("transactions.result").joins(:invoices).group("customers.id").joins(:transactions).group('invoices').find_by(id: self.id)
+      if join_table != nil
+        return join_table.transaction_count
+      else
+        return 0
+      end
+    else
+      return 0
+    end
   end
   def self.top_5
-    Customer.select("customers.id as id, customers.first_name as first_name, customers.last_name as last_name, count(transactions) as transaction_count").where("transactions.result = '0'").group("transactions.result").joins(:invoices).group("customers.id").joins(:transactions).order('transaction_count DESC').distinct.limit(10)
+    Customer.select("customers.id as id, customers.first_name as first_name, customers.last_name as last_name, count(transactions) as transaction_count").where("transactions.result = '0'").group("transactions.result").joins(:invoices).group("customers.id").joins(:transactions).group('invoices').order('transaction_count DESC').distinct.limit(5)
   end
-
-
 end
