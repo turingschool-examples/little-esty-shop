764486fe78b0c91388dba898c51a3c32db53adb3
Sam Devine <samuel.will.devine@gmail.com>
2021-11-06 16:01:34 -0600

    Admin Invoices Index (US9)

--- app/controllers/admin_invoices_controller.rb
+++ app/controllers/admin_invoices_controller.rb
@@ -1,7 +1,8 @@
 class AdminInvoicesController < ApplicationController
   def index
+    @invoices = Invoice.all
   end
-  
+
   def show
   end
 end
---
+++ spec/features/admin_invoices/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'the admin invoice index' do
+  describe 'as an admin' do
+    before :each do
+      customer = Customer.create!(first_name: 'Taylor', last_name: 'Swift')
+      @invoice_1 = customer.invoices.create!(status: 'in progress')
+      @invoice_2 = customer.invoices.create(status: 'cancelled')
+      @invoice_3 = customer.invoices.create(status: 'completed')
+    end
+
+    it 'I see a list of all Invoice ids in the system' do
+      visit '/admin/invoices'
+
+      expect(page).to have_content @invoice_1.id
+      expect(page).to have_content @invoice_2.id
+      expect(page).to have_content @invoice_3.id
+    end
+
+    it 'each id links to the admin invoice show page' do
+      visit '/admin/invoices'
+
+      click_on @invoice_1.id
+
+      expect(current_path).to eq "/admin/invoices/#{@invoice_1.id}"
+    end
+  end
+end
