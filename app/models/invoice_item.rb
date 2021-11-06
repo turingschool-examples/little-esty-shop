9154bf87f9243b323e85bc0db6ec87c6ab702a6b
Chaz Simons <chaz.simons@gmail.com>
2021-11-06 10:32:14 -0700

    Add: item_revenue method to invoice show page

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,4 +3,9 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   has_one :merchant, through: :item
   enum status: [:packaged, :pending, :shipped]
+
+  def item_revenue
+    # InvoiceItem.group(:id).sum(quantity * unit_price)
+    quantity * unit_price
+  end
 end
