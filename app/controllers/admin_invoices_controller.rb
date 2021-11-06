711d8a4b9184ba090f2f6a2e92fc2d7d36668e68
Lesley Sanders <lesleyasanders@gmail.com>
2021-11-06 15:25:37 -0400

    Update show for admin invoices

--- app/controllers/admin_invoices_controller.rb
+++ app/controllers/admin_invoices_controller.rb
@@ -7,14 +7,13 @@ class AdminInvoicesController < ApplicationController
   def show
     @invoice = Invoice.find(params[:invoice_id])
     @customer = Customer.find(@invoice.customer_id)
-    @invoice_items = InvoiceItem.find(params[:invoice_id])
+    @invoice_items = InvoiceItem.find_by(invoice_id: @invoice.id)
     @item = Item.find(@invoice_items.item_id)
   end
 
   def update
     invoice = Invoice.find(params[:invoice_id])
     invoice.update(invoice_params)
-    require "pry"; binding.pry
     redirect_to "/admin/invoices/#{invoice.id}"
   end
 
