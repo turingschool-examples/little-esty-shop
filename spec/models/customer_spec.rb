99a98d3c5973360dd01cdcb10ce3d376b96dc888
Nathan Brown <nathan.brown263@gmail.com>
2021-11-06 19:16:54 -0600

    test: Implement FactoryBot in merchant model spec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,8 +6,17 @@ RSpec.describe Customer, type: :model do
   end
 
   it 'counts how many successful transactions a customer has' do
-    customer = Customer
+    customer = create(:customer)
+    invoice1 = create(:invoice, customer: customer)
+    invoice2 = create(:invoice, customer: customer)
+    invoice3 = create(:invoice, customer: customer)
 
-    expect(customer.successful_transactions_count).to eq(7)
+    create(:transaction, result: 'success', invoice: invoice1)
+    create(:transaction, result: 'success', invoice: invoice1)
+    create(:transaction, result: 'success', invoice: invoice2)
+    create(:transaction, result: 'failed', invoice: invoice2)
+    create(:transaction, result: 'failed', invoice: invoice3)
+
+    expect(customer.successful_transactions_count).to eq(3)
   end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+FactoryBot.find_definitions
 
 RSpec.describe Merchant, type: :model do
   describe 'relationships' do
@@ -6,29 +7,66 @@ RSpec.describe Merchant, type: :model do
   end
 
   before(:each) do
-    @merchant = Merchant.first
+    @merchant = create(:merchant)
+
+    @customer1 = create(:customer)
+    @customer2 = create(:customer)
+    @customer3 = create(:customer)
+    @customer4 = create(:customer)
+    @customer5 = create(:customer)
+    @customer6 = create(:customer)
+
+    @invoice1 = create(:invoice, customer: @customer1)
+    @invoice2 = create(:invoice, customer: @customer2)
+    @invoice3 = create(:invoice, customer: @customer3)
+    @invoice4 = create(:invoice, customer: @customer4)
+    @invoice5 = create(:invoice, customer: @customer5)
+    @invoice6 = create(:invoice, customer: @customer6)
+
+    @item1 = create(:item, merchant: @merchant)
+    @item2 = create(:item, merchant: @merchant)
+    @item3 = create(:item, merchant: @merchant)
+    @item4 = create(:item, merchant: @merchant)
+    @item5 = create(:item, merchant: @merchant)
+    @item6 = create(:item, merchant: @merchant)
+
+    @invitem1 = create(:invoice_item, invoice: @invoice1, status:'packaged', item: @item1)
+    @invitem2 = create(:invoice_item, invoice: @invoice2, status:'packaged',item: @item2)
+    @invitem3 = create(:invoice_item, invoice: @invoice3, status:'packaged',item: @item3)
+    @invitem4 = create(:invoice_item, invoice: @invoice4, status:'pending', item: @item4)
+    @invitem5 = create(:invoice_item, invoice: @invoice5, status:'pending',item: @item5)
+    @invitem6 = create(:invoice_item, invoice: @invoice6, status:'shipped', item: @item6)
+
+    @transaction1 = create(:transaction, result: 'success', invoice: @invoice1)
+    @transaction2 = create(:transaction, result: 'success', invoice: @invoice1)
+    @transaction3 = create(:transaction, result: 'success', invoice: @invoice2)
+    @transaction4 = create(:transaction, result: 'success', invoice: @invoice2)
+    @transaction5 = create(:transaction, result: 'success', invoice: @invoice3)
+    @transaction6 = create(:transaction, result: 'success', invoice: @invoice3)
+    @transaction7 = create(:transaction, result: 'success', invoice: @invoice4)
+    @transaction8 = create(:transaction, result: 'success', invoice: @invoice4)
+    @transaction9 = create(:transaction, result: 'success', invoice: @invoice5)
+    @transaction10 = create(:transaction, result: 'success', invoice: @invoice5)
   end
 
   describe '#top_customers' do
     it 'returns the top 5 customers for the given merchant' do
-      expect(@merchant.top_customers.length).to eq(5)
+      expect(@merchant.top_customers).to eq([@customer1, @customer2, @customer3, @customer4, @customer5])
     end
   end
 
   describe '#shippable_items' do
     it 'returns the items that are ready to ship in order from oldest to newest' do
 
-      expect(@merchant.shippable_items.length).to eq(21)
-      expect(@merchant.shippable_items.first.name).to eq("Item Expedita Aliquam")
+      expect(@merchant.shippable_items.length).to eq(3)
+      expect(@merchant.shippable_items).to eq([@item1, @item2, @item3])
       expect(@merchant.shippable_items.first.invoice_created_at).to be < @merchant.shippable_items.last.invoice_created_at
     end
   end
 
   describe '#invoices' do
     it "returns all invoices related to that merchant's items" do
-      invoice = Invoice.find(29)
-
-      expect(@merchant.invoice_ids).to include(invoice.id)
+      expect(@merchant.invoice_ids).to include(@invoice1.id, @invoice2.id, @invoice3.id)
     end
   end
 end
