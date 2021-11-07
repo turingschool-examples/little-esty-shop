5c2ef38233afb9c3f23db2804d5d1c0498d89ad2
Devin Pile <devinpile@gmail.com>
2021-11-06 20:18:25 -0600

    Add validations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,6 +5,8 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices, source: :invoice_items
   has_many :transactions, through: :invoices
 
+  validates_presence_of :name 
+
   def top_customers
     Transaction.joins(invoice: :customer)
     .where("result =?", 0)
