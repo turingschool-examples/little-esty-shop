c96af95d135eb45af38956bebb04c937bf1ca351
Gregory Fischer <gwhoisj@gmail.com>
2021-11-07 13:46:43 -0500

    Feat: add basic page for admin merchant show

---
+++ spec/features/admin/merchants/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+FactoryBot.find_definitions
+
+RSpec.describe "merchant admin show page" do
+  before :each do
+    @merchant = create(:merchant)
+  end
+
+  it 'displays the merchants name' do
+    visit "/admin/merchants/#{@merchant.id}"
+
+    expect(page).to have_content(@merchant.name)
+  end
+end
