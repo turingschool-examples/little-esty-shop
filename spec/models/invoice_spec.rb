d4bd05eef12cd0242e15a782a35bc85e5eeec021
Gregory Fischer <gwhoisj@gmail.com>
2021-11-08 18:38:09 -0500

    Refactor: remove unsused methods

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-FactoryBot.find_definitions
+# FactoryBot.find_definitions
 
 RSpec.describe Invoice, type: :model do
   describe 'relationships' do
@@ -14,6 +14,13 @@ RSpec.describe Invoice, type: :model do
       @invoice1 = create(:invoice, created_at: "2012-03-25 09:54:09 UTC")
       @invoice2 = create(:invoice, created_at: "2012-03-26 06:54:10 UTC")
       @invoice3 = create(:invoice, created_at: "2012-03-26 06:54:10 UTC")
+
+      @transaction1 = create(:transaction, invoice: @invoice1, result: "success")
+      @transaction1 = create(:transaction, invoice: @invoice2, result: "success")
+      @transaction1 = create(:transaction, invoice: @invoice3, result: "failed")
+
+      @invoice_item1 = create(:invoice_item, invoice: @invoice1, unit_price: 1, quantity: 10)
+      @invoice_item2 = create(:invoice_item, invoice: @invoice1, unit_price: 2, quantity: 10)
     end
 
     describe '#highest_date' do
