a709ca0cae0144ab5df0a0031f257adfb4d8a078
ChloeLL <chloell@protonmail.ch>
2021-11-08 12:17:05 -0700

    Add create merchant item functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,26 @@ class ItemsController < ApplicationController
     @items = @merchant.items
     @item = @items.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    item = Item.new(item_params)
+
+    if item.save
+      redirect_to "/merchants/#{@merchant.id}/items"
+    else
+      redirect_to "/merchants/#{@merchant.id}/items/new"
+      #put a flash here
+    end
+  end
+end
+
+private
+
+def item_params
+  params.permit(:id, :name, :description, :unit_price, :merchant_id)
 end
