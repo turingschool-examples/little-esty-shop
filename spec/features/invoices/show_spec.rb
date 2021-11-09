9caa3d0bcb9294ca5ed3cf0b93268a7df47e8f20
Matthew Kimball <mekimba6@gmail.com>
2021-11-08 18:15:00 -0700

    sorts ready to ship in descending order by created_at

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -26,12 +26,3 @@ RSpec.describe 'Show page', type: :feature do
     end
   end
 end
-
-# As a merchant
-# When I visit my merchant's invoice show page(/merchants/merchant_id/invoices/invoice_id)
-# Then I see information related to that invoice including:
-
-# Invoice id
-# Invoice status
-# Invoice created_at date in the format "Monday, July 18, 2019"
-# Customer first and last name
