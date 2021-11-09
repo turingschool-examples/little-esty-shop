f810b8124fd8fdce8267c287ea534583e6417339
Haewon Jeon <haewonito@gmail.com>
2021-11-09 16:27:38 -0700

    incorporating comments. before changing status to boolean

--- spec/features/merchant_invoices/index_spec.rb
+++ spec/features/merchant_invoices/index_spec.rb
@@ -1,9 +1,3 @@
-# As a merchant,
-# When I visit my merchant's invoices index (/merchants/merchant_id/invoices)
-# Then I see all of the invoices that include at least one of my merchant's items
-# And for each invoice I see its id
-# And each id links to the merchant invoice show page
-
 
 require "rails_helper"
 
