834c1683f01b5555f6977c8df3ba925123afea6c
Micha Bernhard <mib70@pitt.edu>
2021-11-08 17:29:09 -0700

    Test top 5 merchants by revenue

--- spec/features/admin/merchant/index_spec.rb
+++ spec/features/admin/merchant/index_spec.rb
@@ -40,6 +40,36 @@ RSpec.describe 'Admin Merchant Index page' do
       click_button 'Enable/Disable'
       expect(page).to have_content("Status: Disabled")
     end
+  end
 
+  it 'displays the top 5 merchants based on revenue generated' do
+    merchant1 = Merchant.create(name: "Bob")
+    merchant2 = Merchant.create(name: "Kevin")
+    merchant3 = Merchant.create(name: "Zach")
+
+    item = merchant1.items.create(name: 'Mug', description: 'You can drink with it', unit_price: 5)
+    item = merchant2.items.create(name: 'GPU', description: 'Its too expensive', unit_price: 1500)
+    item = merchant3.items.create(name: 'Compressed Air', description: 'Its compressed', unit_price: 2)
+
+    customer_1 = Customer.create(first_name: 'Jen', last_name: 'R')
+    customer_2 = Customer.create(first_name: 'Micha', last_name: 'B')
+    customer_3 = Customer.create(first_name: 'Richard', last_name: 'A')
+
+    invoice_1 = customer_1.invoices.create(status: 'completed')
+    invoice_2 = customer_2.invoices.create(status: 'completed')
+    invoice_3 = customer_3.invoices.create(status: 'in progress')
+
+    invoice_item_1 = invoice_1.invoice_items.create(item_id: item.id, quantity: 1, unit_price: 1000, status: 'shipped')
+    invoice_item_2 = invoice_2.invoice_items.create(item_id: item.id, quantity: 1, unit_price: 1000, status: 'shipped')
+    invoice_item_3 = invoice_3.invoice_items.create(item_id: item.id, quantity: 1, unit_price: 1000, status: 'shipped')
+
+    transaction_1 = invoice_1.transactions.create(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+    transaction_2 = invoice_2.transactions.create(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+    transaction_3 = invoice_3.transactions.create(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'failed')
+
+    visit admin_merchants_path
+
+    expect(page).to have_content('Top 5 Merchants:')
+    expect('Kevin - 1500').to appear_before('Bob - 5')
   end
 end
