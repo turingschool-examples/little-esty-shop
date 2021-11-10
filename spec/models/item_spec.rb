4f8b93153baf5eecc147c636022a1ad2882bb28e
Karan Mehta <karanm645@gmail.com>
2021-11-10 15:39:20 -0600

    Work on creating a model test for disable/enable

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of :description }
     it { should validate_presence_of :unit_price }
   end 
+
+  before(:each) do
+      @merchant1 = Merchant.create!(name: 'Willms and Sons')
+      @merchant2 = Merchant.create!(name: 'John Jacob J')
+      @item1 = @merchant1.items.create!(name: "Item 1", description: "An item", unit_price: 1300, status:0)
+      @item2 = @merchant1.items.create!(name: "Item 2", description: "Another item", unit_price: 1200, status: 1)
+      @item3 = @merchant2.items.create!(name: "Item 3", description: "Another other item", unit_price: 1240, status:1)
+    end
+
+    it ".disabled_items" do 
+      
+      expect(Item.disabled_items).to eq([@item2])  
+    end 
 end
