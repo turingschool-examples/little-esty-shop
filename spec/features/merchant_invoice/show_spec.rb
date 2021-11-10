7ae5b4cfeb088e00527f47f08848e582fd78e019
Christian Valesares <cvalesares@gmail.com>
2021-11-09 20:18:54 -0500

    add half-baked select field for invoice item status

--- spec/features/merchant_invoice/show_spec.rb
+++ spec/features/merchant_invoice/show_spec.rb
@@ -32,4 +32,32 @@ RSpec.describe "Merchant Invoice Show" do
 
     expect(page).to have_content("Total Revenue: 13")
   end
+
+  it "can update the invoice item's status" do
+      merchant1 = Merchant.create!(name: "Bob's Burgers")
+      customer1 = Customer.create!(first_name: "Teddy", last_name: "Lastname")
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 1)
+      item1 = Item.create!(name: "Burger", description: "Burger with fries", unit_price: 3, merchant_id: merchant1.id)
+      item2 = Item.create!(name: "Shake", description: "Shake without fries", unit_price: 2, merchant_id: merchant1.id)
+      invoiceitem1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 3, unit_price: item1.unit_price, status: 0)
+      invoiceitem2 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice1.id, quantity: 2, unit_price: item2.unit_price, status: 0)
+
+      visit "merchants/#{merchant1.id}/invoices/#{invoice1.id}"
+      save_and_open_page
+      click_button "Update Item Status"
+
+      reload item1
+      expect(item1.status).to eq(1)
+  end
 end
+
+# As a merchant
+# When I visit my merchant invoice show page
+# I see that each invoice item status is a select field
+# And I see that the invoice item's current status is selected
+# When I click this select field,
+# Then I can select a new status for the Item,
+# And next to the select field I see a button to "Update Item Status"
+# When I click this button
+# I am taken back to the merchant invoice show page
+# And I see that my Item's status has now been updated
