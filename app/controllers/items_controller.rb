a2b7cd8a90e3a727fd4aea6bb04f3704dccc4a70
Karan Mehta <karanm645@gmail.com>
2021-11-10 15:38:52 -0600

    Create update action for disable/enable

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,21 @@ class ItemsController < ApplicationController
     @items = @merchant.items
     @item = @items.find(params[:id])
   end
+
+  def update 
+    item = Item.find(params[:item_id])
+    merchant = item.merchant
+    
+    item.update(status: params[:disable].to_i) if params[:disable] 
+    item.update(status: params[:enable].to_i) if params[:enable] 
+    redirect_to merchant_items_path(merchant)
+    end
+  end
+
+  def create  
+  end
+private
+
+def item_params
+  params.permit(:id, :name, :description, :unit_price, :merchant_id, :status)
 end
