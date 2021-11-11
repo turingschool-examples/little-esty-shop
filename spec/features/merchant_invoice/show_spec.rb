1461867beb49adaa13d22f613e90545dd4cfaed0
Christian Valesares <cvalesares@gmail.com>
2021-11-10 19:41:00 -0500

    refactor to merchant invoice show spec

--- spec/features/merchant_invoice/show_spec.rb
+++ spec/features/merchant_invoice/show_spec.rb
@@ -6,9 +6,7 @@ RSpec.describe "Merchant Invoice Show" do
     customer1 = Customer.create!(first_name: "Teddy", last_name: "Lastname")
     invoice1 = Invoice.create!(customer_id: customer1.id, status: 1)
     item1 = Item.create!(name: "Burger", description: "Burger with fries", unit_price: 3, merchant_id: merchant1.id)
-    item2 = Item.create!(name: "Shake", description: "Shake without fries", unit_price: 2, merchant_id: merchant1.id)
     invoiceitem1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 3, unit_price: item1.unit_price, status: "Packaged")
-    invoiceitem2 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice1.id, quantity: 2, unit_price: item2.unit_price, status: "Pending")
 
     visit "merchants/#{merchant1.id}/invoices/#{invoice1.id}"
 
