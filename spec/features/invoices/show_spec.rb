2aec0de69663ab444d57dca81772f31acfc0c62e
Matthew Kimball <mekimba6@gmail.com>
2021-11-10 17:37:25 -0700

    removes user story

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -65,13 +65,3 @@ RSpec.describe 'Show page', type: :feature do
     end
   end
 end
-# As a merchant
-# When I visit my merchant invoice show page
-# I see that each invoice item status is a select field
-# And I see that the invoice item's current status is selected
-# When I click this select field,
-# Then I can select a new status for the Item,
-# And next to the select field I see a button to "Update Item Status"
-# When I click this button
-# I am taken back to the merchant invoice show page
-# And I see that my Item's status has now been updated
