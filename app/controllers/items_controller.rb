c78b0bc49f2a95677272cb44c27b703c5e088dbe
Devin Pile <devinpile@gmail.com>
2021-11-10 17:43:24 -0700

    Add changes for merchant item update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,37 @@ class ItemsController < ApplicationController
 
   def show
     @merchant = Merchant.find(params[:merchant_id])
-    @items = @merchant.items
-    @item = @items.find(params[:id])
+    @item = Item.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:success] = 'Successfully Updated'
+      redirection
+    else
+      flash[:alert] = 'Try Again'
+      redirect_to edit_merchant_item_path(@merchant, @item)
+    end
+  end
+
+  private
+
+    def item_params
+      params.permit(:name, :description, :unit_price, :status)
+    end
+
+    def redirection
+      if params[:disable] || params[:enable]
+        redirect_to merchant_items_path(@merchant)
+     else
+      redirect_to merchant_item_path(@merchant, @item)
+     end
+   end
 end
