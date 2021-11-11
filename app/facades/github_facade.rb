51c2584792459bf6fcb27888522d66b824846e3d
Micha Bernhard <mib70@pitt.edu>
2021-11-11 09:16:55 -0700

    Add github users to the layouts page

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -4,5 +4,10 @@ class GithubFacade
       repository = GithubService.repository
       GithubRepo.new(repository)
     end
+
+    def users
+      users = GithubService.users
+      GithubUsers.new(users)
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_11_08_161256) do
     t.bigint "invoice_id"
     t.integer "quantity"
     t.integer "unit_price"
-    t.integer "status", default: 0
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2021_11_08_161256) do
 
   create_table "invoices", force: :cascade do |t|
     t.bigint "customer_id"
-    t.integer "status", default: 1
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
