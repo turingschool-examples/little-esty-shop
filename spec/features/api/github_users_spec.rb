22fb5f7524ef502a911713a8a6a816dd27de7b46
Haewon Jeon <haewonito@gmail.com>
2021-11-11 10:58:15 -0700

    API for github user login and commit numbers

--- spec/features/api/github_users_spec.rb
+++ spec/features/api/github_users_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe GithubUsers do
   it 'exists' do
-    users = GithubUsers.new([{login: "Micha"}, {login: "Bob"}])
+    users = GithubUsers.new([{login: "Micha", contributions: 10}, {login: "Bob", contributions: 20}])
 
     expect(users).to be_an_instance_of GithubUsers
   end
 
   it 'gets the names of the users' do
-    users = GithubUsers.new([{login: "Micha"}, {login: "Bob"}])
+    users = GithubUsers.new([{login: "Micha", contributions: 10}, {login: "Bob", contributions: 20}])
 
-    expect(users.get_users).to eq(['Micha', 'Bob'])
+    expect(users.user_and_contributions).to eq({"Micha" => 10, "Bob" => 20})
   end
 end
