b7e4aa5c38b281c87d5695b64fad4d0ed2a1ff8c
ChloeLL <chloell@protonmail.ch>
2021-11-11 12:44:44 -0700

    Add admin invoice item information

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,17 +4,28 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   validates_presence_of :name
-  validates_presence_of :description 
+  validates_presence_of :description
   validates_presence_of :unit_price
 
   enum status: {enabled: 0, disabled: 1}
 
-  def self.enabled_items 
+  def self.enabled_items
     where(status: 0)
   end
 
   def self.disabled_items
-    where(status: 1)    
+    where(status: 1)
   end
 
+  def amount(invoice_id)
+    invoice_items.find_by(invoice_id: invoice_id, item_id: id).quantity
+  end
+  
+  def order_status(invoice_id)
+    invoice_items.find_by(invoice_id: invoice_id, item_id: id).status
+  end
+
+  def sold_price(invoice_id)
+    invoice_items.find_by(invoice_id: invoice_id, item_id: id).unit_price
+  end
 end
--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe 'Admin invoice show page' do
   describe 'page layout' do
     before(:each) do
       @customer = Customer.create!(first_name: 'John', last_name: 'Wick')
+      @merchant = Merchant.create(name: 'Bob')
       @invoice = Invoice.create!(customer_id: @customer.id, status: 0)
+      @item = Item.create!(name: 'Test Item', description: 'Just a thing', unit_price: 1300, merchant_id: @merchant.id)
+      @invoice_item = InvoiceItem.create!(item_id: @item.id, invoice_id: @invoice.id, quantity: 50, unit_price: 2000)
 
       visit "/admin/invoices/#{@invoice.id}"
     end
@@ -18,6 +21,15 @@ RSpec.describe 'Admin invoice show page' do
       expect(page).to have_content('November')
       expect(page).to have_content(@customer.first_name)
       expect(page).to have_content(@customer.last_name)
+    end
+
+    it 'shows invoice item info' do
+      visit(admin_invoice_path(@invoice))
+
+      expect(page).to have_content('Test Item')
+      expect(page).to have_content(@invoice_item.quantity)
+      expect(page).to have_content(@invoice_item.unit_price)
+      expect(page).to have_content(@invoice_item.status)
     end
   end
 end
