bf9ac6e27a4cd33b0562de468d094b3855074506
Christian Valesares <cvalesares@gmail.com>
2021-11-11 15:04:22 -0500

    add top merchant rev by day

--- spec/features/admin/merchant/index_spec.rb
+++ spec/features/admin/merchant/index_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'Admin Merchant Index page' do
   it 'has a button to change the status of a merchant' do
     merchant1 = Merchant.create!(name: 'Alan Apple')
     merchant2 = Merchant.create!(name: 'Bob Burger')
-    
+
     visit admin_merchants_path
 
     within "#id-#{merchant1.id}" do
@@ -33,7 +33,7 @@ RSpec.describe 'Admin Merchant Index page' do
 
   it 'when the button is clicked it changes the status of the merchant' do
     merchant1 = Merchant.create!(name: 'Alan Apple', status: 'Enabled')
-    
+
     visit admin_merchants_path
 
     within "#id-#{merchant1.id}" do
@@ -43,37 +43,78 @@ RSpec.describe 'Admin Merchant Index page' do
   end
 
   it 'displays the top 5 merchants based on revenue generated' do
-    merchant1 = Merchant.create(name: "Bob")
-    merchant2 = Merchant.create(name: "Kevin")
-    merchant3 = Merchant.create(name: "Zach")
+    merchant1 = Merchant.create!(name: "Bob", status: "Enabled")
+    merchant2 = Merchant.create!(name: "Kevin")
+    merchant3 = Merchant.create!(name: "Zach")
 
-    item1 = merchant1.items.create(name: 'Mug', description: 'You can drink with it', unit_price: 5)
-    item2 = merchant2.items.create(name: 'GPU', description: 'Its too expensive', unit_price: 1500)
-    item3 = merchant3.items.create(name: 'Compressed Air', description: 'Its compressed', unit_price: 2)
+    item1 = merchant1.items.create!(name: 'Mug', description: 'You can drink with it', unit_price: 5)
+    item2 = merchant2.items.create!(name: 'GPU', description: 'Its too expensive', unit_price: 1500)
+    item3 = merchant3.items.create!(name: 'Compressed Air', description: 'Its compressed', unit_price: 2)
 
-    customer_1 = Customer.create(first_name: 'Jen', last_name: 'R')
-    customer_2 = Customer.create(first_name: 'Micha', last_name: 'B')
-    customer_3 = Customer.create(first_name: 'Richard', last_name: 'A')
+    customer_1 = Customer.create!(first_name: 'Jen', last_name: 'R')
+    customer_2 = Customer.create!(first_name: 'Micha', last_name: 'B')
+    customer_3 = Customer.create!(first_name: 'Richard', last_name: 'A')
 
-    invoice_1 = customer_1.invoices.create(status: 'completed')
-    invoice_2 = customer_2.invoices.create(status: 'completed')
-    invoice_3 = customer_3.invoices.create(status: 'in progress')
+    invoice_1 = customer_1.invoices.create!(status: 2)
+    invoice_2 = customer_2.invoices.create!(status: 2)
+    invoice_3 = customer_3.invoices.create!(status: 1)
 
-    invoice_item_1 = invoice_1.invoice_items.create(item_id: item1.id, quantity: 2, unit_price: 5, status: 'shipped')
-    invoice_item_2 = invoice_2.invoice_items.create(item_id: item2.id, quantity: 2, unit_price: 1500, status: 'shipped')
-    invoice_item_3 = invoice_3.invoice_items.create(item_id: item3.id, quantity: 2, unit_price: 2, status: 'shipped')
-    invoice_item_4 = invoice_1.invoice_items.create(item_id: item1.id, quantity: 2, unit_price: 5, status: 'shipped')
-    invoice_item_5 = invoice_2.invoice_items.create(item_id: item2.id, quantity: 2, unit_price: 1500, status: 'shipped')
-    invoice_item_6 = invoice_3.invoice_items.create(item_id: item3.id, quantity: 2, unit_price: 2, status: 'shipped')
-    
-    transaction_1 = invoice_1.transactions.create(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
-    transaction_2 = invoice_2.transactions.create(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
-    transaction_3 = invoice_3.transactions.create(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'failed')
+    invoice_item_1 = invoice_1.invoice_items.create!(item_id: item1.id, quantity: 2, unit_price: 5, status: 2)
+    invoice_item_2 = invoice_2.invoice_items.create!(item_id: item2.id, quantity: 2, unit_price: 1500, status: 2)
+    invoice_item_3 = invoice_3.invoice_items.create!(item_id: item3.id, quantity: 2, unit_price: 2, status: 2)
+    invoice_item_4 = invoice_1.invoice_items.create!(item_id: item1.id, quantity: 2, unit_price: 5, status: 2)
+    invoice_item_5 = invoice_2.invoice_items.create!(item_id: item2.id, quantity: 2, unit_price: 1500, status: 2)
+    invoice_item_6 = invoice_3.invoice_items.create!(item_id: item3.id, quantity: 2, unit_price: 2, status: 2)
 
-    visit admin_merchants_path
+    transaction_1 = invoice_1.transactions.create!(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+    transaction_2 = invoice_2.transactions.create!(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+    transaction_3 = invoice_3.transactions.create!(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'failed')
+
+    visit "/admin/merchants"
 
     expect(page).to have_content('Top 5 Merchants:')
-    expect('Kevin - 6000').to appear_before('Bob - 20')
-    expect(page).to_not have_content('Zach - 10')
+    expect('Kevin - $60.00').to appear_before('Bob - $0.20')
+    expect(page).to_not have_content('Zach - $0.10')
+  end
+
+  it 'shows the top revenue day per merchant' do
+    merchant1 = Merchant.create!(name: "Bob", status: "Enabled")
+    merchant2 = Merchant.create!(name: "Kevin")
+    merchant3 = Merchant.create!(name: "Zach")
+
+    item1 = merchant1.items.create!(name: 'Mug', description: 'You can drink with it', unit_price: 5)
+    item2 = merchant2.items.create!(name: 'GPU', description: 'Its too expensive', unit_price: 1500)
+    item3 = merchant3.items.create!(name: 'Compressed Air', description: 'Its compressed', unit_price: 2)
+
+    customer_1 = Customer.create!(first_name: 'Jen', last_name: 'R')
+    customer_2 = Customer.create!(first_name: 'Micha', last_name: 'B')
+    customer_3 = Customer.create!(first_name: 'Richard', last_name: 'A')
+
+    invoice_1 = customer_1.invoices.create!(status: 2, created_at: "2021-11-11")
+    invoice_2 = customer_2.invoices.create!(status: 2, created_at: "2021-11-11")
+    invoice_3 = customer_3.invoices.create!(status: 2, created_at: "2021-11-11")
+    invoice_4 = customer_1.invoices.create!(status: 2, created_at: "2019-03-06")
+    invoice_5 = customer_2.invoices.create!(status: 2, created_at: "2019-03-06")
+    invoice_6 = customer_3.invoices.create!(status: 2, created_at: "2019-03-06")
+
+    invoice_item_1 = invoice_1.invoice_items.create!(item_id: item1.id, quantity: 2, unit_price: 5, status: 2)
+    invoice_item_2 = invoice_2.invoice_items.create!(item_id: item2.id, quantity: 10, unit_price: 1500, status: 2)
+    invoice_item_3 = invoice_3.invoice_items.create!(item_id: item3.id, quantity: 2, unit_price: 2, status: 2)
+    invoice_item_4 = invoice_4.invoice_items.create!(item_id: item1.id, quantity: 10, unit_price: 5, status: 2)
+    invoice_item_5 = invoice_5.invoice_items.create!(item_id: item2.id, quantity: 8, unit_price: 1500, status: 2)
+    invoice_item_6 = invoice_6.invoice_items.create!(item_id: item3.id, quantity: 6, unit_price: 2, status: 2)
+
+    transaction_1 = invoice_1.transactions.create!(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+    transaction_2 = invoice_2.transactions.create!(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+    transaction_3 = invoice_3.transactions.create!(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+    transaction_4 = invoice_4.transactions.create!(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+    transaction_5 = invoice_5.transactions.create!(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+    transaction_6 = invoice_6.transactions.create!(credit_card_number: 1234123412341234, credit_card_expiration_date: '2012-03-27', result: 'success')
+
+    visit "/admin/merchants"
+
+    expect(page).to have_content("Top selling date for #{merchant2.name} was: #{invoice_2.created_at.strftime("%A, %B %-d, %Y")}")
+    expect(page).to have_content("Top selling date for #{merchant1.name} was: #{invoice_4.created_at.strftime("%A, %B %-d, %Y")}")
+    expect(page).to have_content("Top selling date for #{merchant3.name} was: #{invoice_6.created_at.strftime("%A, %B %-d, %Y")}")
   end
 end
