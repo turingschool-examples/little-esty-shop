743a8fcecfe43a5b98f7204363bd437dd61d4493
Christian Valesares <cvalesares@gmail.com>
2021-11-10 23:56:40 -0500

    add enabled/disabled merchants to admin merchant index

--- spec/features/merchant_admin/index_spec.rb
+++ spec/features/merchant_admin/index_spec.rb
@@ -2,24 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Admin Merchant Index' do
   it 'has sections for enabled and disabled merchants' do
-    merchant1 = Merchant.create!(name: 'Jimmy Pesto')
-    merchant1 = Merchant.create!(name: 'Linda Belcher')
-    merchant1 = Merchant.create!(name: 'Louis Belcher')
+    merchant1 = Merchant.create!(name: 'Jimmy Pesto', status: "Enabled")
+    merchant2 = Merchant.create!(name: 'Linda Belcher', status: "Disabled")
+    merchant3 = Merchant.create!(name: 'Louis Belcher', status: "Disabled")
 
     visit '/admin/merchants'
 
     expect(page).to have_content("Enabled Merchants")
     expect(page).to have_content("Disabled Merchants")
 
-    # within "Enabled" do
-    #   expect(page).to have_content(merchant1.name)
-    # end
-    #
-    # within "Disabled" do
-    #   expect(page). to have_content(merchant2.name)
-    #   expect(page). to have_content(merchant3.name)
-    # end
+    within "#Enabled" do
+      save_and_open_page
+      expect(page).to have_content(merchant1.name)
+    end
+
+    within "#Disabled" do
+      expect(page). to have_content(merchant2.name)
+      expect(page). to have_content(merchant3.name)
+    end
   end
 end
-
-#This needs to be synced up with Micha's user story for disabling merchants
