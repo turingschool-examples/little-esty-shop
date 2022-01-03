1d22a507bf521ce01513aaeffca7620bd608a9a2
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-03 17:26:35 -0500

    Modify items factory.

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     sequence(:name) { |n| "Default Item Name #{n}" }
     description { "Default Description" }
-    sequence(:unit_price) { |n| 500 + n }
+    sequence(:unit_price) { |n| 10000 + (n * 1000) }
     merchant
   end
 end
