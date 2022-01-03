5ac6346f354d9f0fba51ea51c1657cf1e1742b0c
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-03 16:40:19 -0500

    Add config to rails_helper for factory_bot_rails, shoulda-matchers, simplecov.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start
+
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -61,4 +65,33 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
+  config.include FactoryBot::Syntax::Methods
+  
+  # config.include Capybara::DSL
+  #
+  # config.before(:suite) do
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
+  #
+  # config.before :each do
+  #   DatabaseCleaner.clean
+  # end
+  #
+  # config.after :each do
+  #   DatabaseCleaner.clean
+  # end
+  #
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.use_transactional_fixtures = true
+  # config.infer_spec_type_from_file_location!
+  # config.filter_rails_from_backtrace!
+
+
 end
