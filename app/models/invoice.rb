b05fc3389e19d80fa238add3ad3d78ebbb189f2e
Kevin Gloss <kgloss89@gmail.com>
2022-01-04 14:11:18 -0700

    Add relationships and validations to models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,7 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
-  # validates :customer_id, presence: true
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+  validates :status, presence: true
+  # enum status: { pending: 0, approved: 1}
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ApplicationRecord
-
+  belongs_to :invoice
+  validates :credit_card_number, :credit_card_expiration_date, :result, presence: true
 end
