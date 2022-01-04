9ddcad14c995c56d33ab0dbbf5f440a9199cb05a
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-01-03 18:10:02 -0700

    Complete database setup

--- db/migrate/20220103212748_create_items.rb
+++ db/migrate/20220103212748_create_items.rb
@@ -1,12 +1,10 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
+      t.references :merchant, foreign_key: true
       t.string :name
       t.string :description
       t.string :unit_price
-      t.string :merchant_id
-      t.string :created_at
-      t.string :updated_at
 
       t.timestamps
     end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:invoice) }
+    it { should belong_to(:item) }
+  end
 end
