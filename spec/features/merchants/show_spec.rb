4a3b77ff8da31e96e09e2b6f1985e59d0d0608d3
Mallory Vining <mallorygallery12@gmail.com>
2022-01-04 16:51:44 -0700

    Magic commit

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Merchant Dashboad Show Page" do
-#   Merchant Dashboard
-#
-# As a merchant,
-# When I visit my merchant dashboard (/merchants/merchant_id/dashboard)
-# Then I see the name of my merchant
   it 'shows merchant name' do
     merch_1 = Merchant.create!(name: "Shop Here")
 
