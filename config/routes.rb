9292954035875ac7d39c8692acc113775ab6f6c0
Katy Harrod <m.catherine.harrod@gmail.com>
2022-01-04 14:45:49 -0700

    Add routes to corresponding pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # resources :merchants, only:[:show] do
-    get '/merchants/:id/dashboard', to: 'dashboard#index'
+  resources :merchants, only:[:show] do
+    resources :items, only:[:index]
+    resources :invoices, only:[:index]
+    resources :dashboard, only:[:index]
+    # get '/merchants/:id/dashboard', to: 'dashboard#index'
+  end
 end
