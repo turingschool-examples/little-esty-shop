81556249bad88b06ac8e90962709482aa3f5f49a
Tim Roeder <timothyjroeder@gmail.com>
2022-01-04 13:44:51 -0700

    Fix: rename view folder to feature folder

---
+++ spec/features/items/new.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "items/new", type: :view do
+  before(:each) do
+    assign(:item, Item.new(
+      name: "MyString",
+      merchants: nil,
+      description: "MyText",
+      unit_price: 1
+    ))
+  end
+
+  it "renders new item form" do
+    render
+
+    assert_select "form[action=?][method=?]", items_path, "post" do
+
+      assert_select "input[name=?]", "item[name]"
+
+      assert_select "input[name=?]", "item[merchants_id]"
+
+      assert_select "textarea[name=?]", "item[description]"
+
+      assert_select "input[name=?]", "item[unit_price]"
+    end
+  end
+end
---
+++ spec/features/transactions/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "transactions/new", type: :view do
+  before(:each) do
+    assign(:transaction, Transaction.new(
+      invoices: nil,
+      credit_card_number: 1,
+      result: 1
+    ))
+  end
+
+  it "renders new transaction form" do
+    render
+
+    assert_select "form[action=?][method=?]", transactions_path, "post" do
+
+      assert_select "input[name=?]", "transaction[invoices_id]"
+
+      assert_select "input[name=?]", "transaction[credit_card_number]"
+
+      assert_select "input[name=?]", "transaction[result]"
+    end
+  end
+end
