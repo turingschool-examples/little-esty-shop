bbed231b0a677cd7b1688826a2cc129038c17342
Eldridge Turambi <eturambi95@gmail.com>
2022-01-04 14:34:12 -0700

    Finish trasaction rake task

--- db/migrate/20220104033925_create_transactions.rb
+++ db/migrate/20220104033925_create_transactions.rb
@@ -2,7 +2,7 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.string :credit_card_number
-      t.string :credit_card_expiriation_date
+      t.string :credit_card_expiration_date
       t.string :result
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_01_04_025823) do
+ActiveRecord::Schema.define(version: 2022_01_04_033925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +20,14 @@ ActiveRecord::Schema.define(version: 2022_01_04_025823) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.bigint "customer_id"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_invoices_on_customer_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -33,20 +40,23 @@ ActiveRecord::Schema.define(version: 2022_01_04_025823) do
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
-  create_table "invoices", force: :cascade do |t|
-    t.bigint "customer_id"
-    t.integer "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["customer_id"], name: "index_invoices_on_customer_id"
-  end
-
   create_table "merchants", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "items", "merchants"
+  create_table "transactions", force: :cascade do |t|
+    t.string "credit_card_number"
+    t.string "credit_card_expiration_date"
+    t.string "result"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "invoice_id"
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
+  end
+
   add_foreign_key "invoices", "customers"
+  add_foreign_key "items", "merchants"
+  add_foreign_key "transactions", "invoices"
 end
