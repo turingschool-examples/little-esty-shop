7b3e77223d942b8b6c38def25ebc425291839022
Katy Harrod <m.catherine.harrod@gmail.com>
2022-01-04 14:50:46 -0700

    Add association for merchant + invoice items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 describe Merchant, type: :model do
   describe 'relationships' do
-    it {should have_many :items}
+    it { should have_many :items }
+    it { should have_many(:invoice_items).through :items }
+    it { should have_many(:invoices).through :invoice_items }
   end
 end
