fd77680157f6af99807cf0ea9bab86dc9fbc4d2c
Dylan Harper <dylanharper4@icloud.com>
2022-01-04 13:11:15 -0800

    Add enum to InvoiceItem and Invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,6 @@ class Invoice < ApplicationRecord
 
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  enum status: { "cancelled" => 0, "completed" => 1, "in progress" => 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2022_01_03_232628) do
     t.bigint "invoice_id"
     t.integer "quantity"
     t.integer "unit_price"
-    t.string "status"
+    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2022_01_03_232628) do
   end
 
   create_table "invoices", force: :cascade do |t|
-    t.string "status"
+    t.integer "status"
     t.bigint "customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
