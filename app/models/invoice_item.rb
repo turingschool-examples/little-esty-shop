8b4fd71847d1fcf1b988d3c4a326fd7ce5130a1e
Karan Mehta <karanm645@gmail.com>
2022-01-03 20:40:25 -0600

    Create all relations

---
+++ app/models/invoice_item.rb
@@ -0,0 +1,6 @@
+class InvoiceItem < ApplicationRecord 
+  enum status: ["packaged", "pending", "shipped"]
+  
+  belongs_to :invoice
+  belongs_to :item 
+end 
---
+++ spec/models/invoice_item_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe InvoiceItem do 
+  describe 'validation' do 
+    it { should define_enum_for(:status).with([:packaged, :pending, :shipped]) }
+  end 
+  describe 'relations' do 
+    it { should belong_to :invoice }
+    it { should belong_to :item }
+  end 
+end 
