d0dd311172739febf113effc476ff99cfd70b73b
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-04 22:21:55 -0500

    Add merchants_with_invoices factory.

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :merchant do
     sequence(:name) { |n| "Default Merchant Name #{n}" }
+
+    factory :merchant_with_invoices do
+      transient do
+        invoice_count {2}
+      end
+      after(:create) do |merchant, evaluator|
+        evaluator.invoice_count.times do
+          item = create(:item, merchant: merchant)
+          invoice = create(:invoice)
+          invoice_item = (:invoice_item, item: item, invoice: invoice )
+        end
+      end
+    end
   end
 end
