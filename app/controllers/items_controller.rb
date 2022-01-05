10d42cfdb131badb7e36019bd6338ff1613f0439
Kelly Anderson <kean9528@colorado.edu>
2022-01-04 17:39:03 -0700

    add merchant items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,3 @@
 class ItemsController < ApplicationController
-  def index
-    @merchant = Merchant.find(params[:merchant_id])
-    @items = Item.all.where(merchant: params[:merchant_id])
-  end
 
-  def show
-    @item = Item.find(params[:id])
-  end
 end
