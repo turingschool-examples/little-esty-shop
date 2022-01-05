12d8631129c517e71bc7276aed4ceabecd2faf75
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-04 17:31:54 -0700

    Test: merchant items index

---
+++ spec/features/merchants/invoices/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Invoices Index page' do
+  invoice_item1 = FactoryBot.create(:invoice_item)
+  invoice_item2 = FactoryBot.create(:invoice_item)
+  invoice_item3 = FactoryBot.create(:invoice_item)
+  it 'shows all of the invoices that include at least one of the merchants items with a link to that invoice' do
+    item = Item.find(invoice_item1.item_id)
+    visit "/merchants/#{item.merchant_id}/invoices"
+    save_page_and_open
+    expect(page).to have_content(invoice_item1.invoice_id)
+    click_on(invoice_item1.invoice_id)
+    expect(current_path).to eq("/merchants/#{item.merchant_id}/invoices/#{invoice_item1.invoice_id}")
+  end
+end
