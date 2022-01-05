cd50d53a2db58425baa8e16f04b0dbd7052ee394
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-04 20:05:12 -0700

    Test: refactor FactoryBot on model test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe Item, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
   describe 'class methods' do
-    let!(:merchant){Merchant.create!(name: "Bill's Flowers")}
-    let!(:item1){merchant.items.create!(name: "rose", description: "red", unit_price: 5)}
-    invoice1 = FactoryBot.create(:invoice)
-    let!(:invoice_item1){InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 12, unit_price: 5, status: 0)}
     it '#invoice_finder' do
-      expect(Item.invoice_finder(merchant.id)).to eq [invoice1]
+      merchant1 = create(:merchant)
+      invoice1 = create(:invoice)
+      item1 = create(:item, merchant: merchant1)
+      invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id)
+      expect(Item.invoice_finder(item1.merchant_id)).to eq [invoice1]
     end
   end
 end
