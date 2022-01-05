3886e7b7989dc6014570c9d5dae62dcd1b3ecb2b
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-01-05 15:20:17 -0500

    Add tests to display item names

--- spec/features/items/index.html.erb_spec.rb
+++ spec/features/items/index.html.erb_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "items/index", type: :view do
+# Merchant Items Index Page
+#
+# As a merchant,
+# When I visit my merchant items index page ("merchants/merchant_id/items")
+# I see a list of the names of all of my items
+# And I do not see items for any other merchant
+
+RSpec.describe "items/index", type: :feature do
   before(:each) do
-    assign(:items, [
-      Item.create!(
-        name: "Name",
-        merchants: nil,
-        description: "MyText",
-        unit_price: 2
-      ),
-      Item.create!(
-        name: "Name",
-        merchants: nil,
-        description: "MyText",
-        unit_price: 2
-      )
-    ])
+    @merchant_1 = Merchant.create!(name: "Joe Shmoe")
+    @merchant_2 = Merchant.create!(name: "Jane Shmoe")
+
+    @item_1 = @merchant_1.items.create!(name: "Light Machine", description: "Revolutionary Device", unit_price: 999)
+    @item_2 = @merchant_1.items.create!(name: "Shiny Stone", description: "It's shiny", unit_price: 9)
+    @item_3 = @merchant_2.items.create!(name: "Molly Doll", description: "What is it", unit_price: 500)
+    visit merchant_items_path(@merchant_1)
   end
 
-
+it 'has the names of merchant items' do
+  expect(page).to have_content(@item_1.name)
+  expect(page).to have_content(@item_2.name)
+  expect(page).to_not have_content(@item_3.name)
+  end
 end
