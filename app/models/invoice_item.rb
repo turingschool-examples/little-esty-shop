b81a4d9b2e1f53ae68a4e64a3c986a566627612c
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-04 19:25:51 -0700

    Tests passing

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,5 +2,9 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
 
-  enum status: {'pending' => 0, 'packaged' => 1, 'shipped' => 2 }
+  validates :quantity, presence: true, numericality: {only_integer: true}
+  validates :unit_price, presence: true, numericality: {only_integer: true}
+
+
+  enum status: { "pending" => 0, :packaged => 1, "shipped" =>2 }
 end
