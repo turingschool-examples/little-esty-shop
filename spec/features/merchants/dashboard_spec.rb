fbac1cddb4ea47d93d8a858b51957278bceb61ea
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-04 21:15:58 -0500

    Add test for merchant dash user story.

---
+++ spec/features/merchants/dashboard_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant dashboard' do
+    it "When I visit a merchant dashboard I see the name of my merchant" do
+      merchant = create(:merchant, name: 'Bob')
+
+      visit "/merchants/#{merchant.id}/dashboard"
+
+      expect(page).to have content('Bob')
+    end
+end
