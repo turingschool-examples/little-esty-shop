9799be6716918f58e2495e6754224754886cfb8f
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-04 19:17:06 -0700

    Test: item model, #invoice_finder

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  
   def self.invoice_finder(merchant_id)
     Invoice.joins(:invoice_items => :item).where(:items => {:merchant_id => merchant_id})
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'class methods' do
+    let!(:merchant){Merchant.create!(name: "Bill's Flowers")}
+    let!(:item1){merchant.items.create!(name: "rose", description: "red", unit_price: 5)}
+    invoice1 = FactoryBot.create(:invoice)
+    let!(:invoice_item1){InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 12, unit_price: 5, status: 0)}
+    it '#invoice_finder' do
+      expect(Item.invoice_finder(merchant.id)).to eq [invoice1]
+    end
+  end
 end
