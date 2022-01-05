6c35145c7ef61604d2acc755d21670cecc5735cd
Hannah Warren <hannahkwarren@gmail.com>
2022-01-04 22:03:05 -0500

    Feature: merchant invoices index page

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,11 @@ class Invoice < ApplicationRecord
   enum status: ["in progress", "completed", "cancelled"]
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def self.merchants_invoices(merchant)
+
+    joins(:invoice_items, :items).where(items: {merchant_id: merchant.id})
+
+  end
+
 end
