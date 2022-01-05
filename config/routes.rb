25a7ed886263b3e92447fd8793282cb42b13b480
Kevin Gloss <kgloss89@gmail.com>
2022-01-04 22:10:11 -0700

    Add welcome page for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#heroku welcome page
+root 'welcome#index'
+
 get '/merchants/:merchant_id/dashboard', to: 'merchants#dashboard'
 # get '/merchants/:merchant_id/items', to: 'merchant_items#index'
 get '/merchants/:merchant_id/invoices', to: 'merchant_invoices#index'
