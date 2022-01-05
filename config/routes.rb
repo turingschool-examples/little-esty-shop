9b02acd1444d018bc82c47357cf1465ab11baf10
Mallory Vining <mallorygallery12@gmail.com>
2022-01-04 17:13:08 -0700

    Add invoices/items links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/merchants/:id/dashboard", to: "merchants#show"
-  # resources :merchants do
-  #   resources :dashboard, controller: :merchants
-  # end
+  # get "/merchants/:id/items", to: "items#show"
+  resources :merchants do
+    resources :items
+    resources :invoices
+  end
 end
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe "Merchant Dashboad Show Page" do
       expect(page).to have_content("Merchant Name: #{merch_1.name}")
     end
   end
+
+  it "links to items index" do
+    merch_1 = Merchant.create!(name: "Shop Here")
+    visit "/merchants/#{merch_1.id}/dashboard"
+
+    expect(page).to have_content("Items Index")
+    click_link "Items Index"
+    expect(current_path).to eq("/merchants/#{merch_1.id}/items")
+  end
+
+  it "links to invoices index" do
+    merch_1 = Merchant.create!(name: "Shop Here")
+    visit "/merchants/#{merch_1.id}/dashboard"
+
+    expect(page).to have_content("Invoices Index")
+    click_link "Invoices Index"
+    expect(current_path).to eq("/merchants/#{merch_1.id}/invoices")
+  end
 end
