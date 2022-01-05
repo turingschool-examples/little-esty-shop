18861296275305f2b90824e5eadd541ca3aedabe
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-05 14:37:03 -0700

    Test: merchant item update

--- spec/features/merchants/items/show_spec.rb
+++ spec/features/merchants/items/show_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe 'Merchant Items Show page' do
     expect(page).to have_content(item1.description)
     expect(page).to have_content(item1.unit_price)
   end
+
+  it 'allows the merchant to update their items' do
+    merchant1 = create(:merchant)
+    item1 = create(:item, merchant: merchant1)
+    visit "/merchants/#{merchant1.id}/items/#{item1.id}"
+
+    expect(page).to have_content("Update #{item1.name}")
+    click_on("Update #{item1.name}")
+    expect(current_path).to eq("/merchants/#{merchant1.id}/items/#{item1.id}/edit")
+
+    within("#update_item") do
+      fill_in 'unit_price', with: 11111
+    end
+    click_on 'Submit'
+    expect(current_path).to eq("/merchants/#{merchant1.id}/items")
+    #checking for flash message
+    expect(page).to have_content("Item Succeefully Updated")
+  end 
 end
