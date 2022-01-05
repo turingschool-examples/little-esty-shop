3d31e42627461a6bc357e025eba5529a455949af
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-01-05 15:12:30 -0500

    Add tests for displaying item names

--- spec/features/items/edit.html.erb_spec.rb
+++ spec/features/items/edit.html.erb_spec.rb
@@ -1,27 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "items/edit", type: :view do
+RSpec.describe 'items/edit', type: :feature do
   before(:each) do
-    @item = assign(:item, Item.create!(
-      name: "MyString",
-      merchants: nil,
-      description: "MyText",
-      unit_price: 1
-    ))
+    @merchant_1 = Merchant.create!(name: 'Joe Shmoe')
+    @item_1 = @merchant_1.items.create!(name: 'Light Machine', description: 'Revolutionary Device', unit_price: 999)
+    visit "/merchants/#{@merchant_1.id}/items/#{@item_1.id}/edit"
+  end
+  it 'has existing info' do
+    expect(page).to have_field(:name, with: 'Light Machine')
+    expect(page).to have_field(:description, with: 'Revolutionary Device')
+    expect(page).to have_field(:unit_price, with: 999)
   end
 
-  xit "renders the edit item form" do
-    render
-
-    assert_select "form[action=?][method=?]", item_path(@item), "post" do
-
-      assert_select "input[name=?]", "item[name]"
-
-      assert_select "input[name=?]", "item[merchants_id]"
-
-      assert_select "textarea[name=?]", "item[description]"
-
-      assert_select "input[name=?]", "item[unit_price]"
-    end
+  it 'fills in with new info' do
+    fill_in(:name, with: 'Flash Light')
+    fill_in(:description, with: 'Ordinary device')
+    fill_in(:unit_price, with: 99)
+    expect(page).to have_field(:name, with: 'Flash Light')
+    expect(page).to have_field(:description, with: 'Ordinary device')
+    expect(page).to have_field(:unit_price, with: 99)
+    click_button "Save Changes"
+    expect(current_path).to eq("/merchants/#{@merchant_1.id}/items/#{@item_1.id}")
+    expect(page).to have_content("Flash Light")
+    expect(page).to have_content("Ordinary device")
+    expect(page).to have_content("99")
   end
 end
