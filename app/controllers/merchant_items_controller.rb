ecba0b9d4b980d472acb242e16208a1552c55bc8
Eldridge Turambi <eturambi95@gmail.com>
2022-01-04 21:32:40 -0700

    Finish Merchant Item Update story; needs extra tests and flash message

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -7,5 +7,23 @@ class MerchantItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    ## below: params[:merchant_id] >> looks for object with that merchant id
+    redirect_to merchant_item_path(params[:merchant_id], item)
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :description, :unit_price)
+    end
 end
--- spec/features/merchants/items/show_spec.rb
+++ spec/features/merchants/items/show_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe 'Merchant Items Show page' do
       expect(page).to have_content("#{item1.unit_price}")
     end
   end
+
+  it 'sees link to update item info. link takes user to edit page of that item' do
+    merchant1 = Merchant.create!(name: 'merchant1')
+    item1 = merchant1.items.create!(name: 'item1', description: 'coolest item ever1', unit_price: 10000)
+
+    visit merchant_item_path(merchant1, item1)
+
+    within ".item" do
+      click_link "Update #{item1.name}"
+      expect(current_path).to eq(edit_merchant_item_path(merchant1, item1))
+    end
+  end
 end
