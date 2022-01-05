3adfe001eaf6b33227f3de3f4e13f9d7ebe64661
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-04 21:38:22 -0700

    Feat: #invoice_finder passing tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,8 @@ class Merchant < ApplicationRecord
   has_many :items
 
   validates :name, presence: true
+
+  def invoice_finder
+    Invoice.joins(:invoice_items => :item).where(:items => {:merchant_id => self.id})
+  end
 end
