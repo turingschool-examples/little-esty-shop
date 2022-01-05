1b068748e83bfdf629b2e432d740be8f09d775c8
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-04 22:39:14 -0500

    cleanup test

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -3,25 +3,17 @@ require 'rails_helper'
 RSpec.describe 'merchant dashboard' do
     it "When I visit a merchant dashboard I see the name of my merchant" do
       merchant = create(:merchant, name: 'Bob')
-
       visit "/merchants/#{merchant.id}/dashboard"
-
       expect(page).to have_content('Bob')
     end
 
     it 'has a link to merchant invoices index' do
-      merchant = create(:merchant_with_invoices, invoice_count: 3)
-
       visit "/merchants/#{merchant.id}/dashboard"
-
       expect(page).to have_link("/merchants/#{merchant.id}/invoices")
     end
 
     it 'has a link to merchant items index' do
-      merchant = create(:merchant_with_items, item_count: 3)
-
       visit "/merchants/#{merchant.id}/dashboard"
-
       expect(page).to have_link("/merchants/#{merchant.id}/items")
     end
 end
