30285b50f41286f4e9951e004a68a9a238a80854
Hannah Warren <hannahkwarren@gmail.com>
2022-01-05 18:03:57 -0500

    Uncomment root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # root 'welcome#index'
+  root 'welcome#index'
   get '/welcome', to: 'welcome#index'
   get '/merchants/:id/dashboard', to: 'merchants#show'
   # get "/merchants/:id/items", to: "items#show"
