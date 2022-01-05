45834167e4f365d816cf56a05de7efefae9d1f0d
David Kassin <david.j.kassin@gmail.com>
2022-01-04 17:32:55 -0700

    finish before each setup

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,4 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
-  validates :credit_card_number, :credit_card_expiration_date, :result, presence: true
+  validates :credit_card_number, :result, presence: true
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,34 +67,61 @@ RSpec.configure do |config|
     @customer_6 = Customer.create!(first_name: "ECustomer", last_name: "6")
     #invoices
     @invoice_1 = @customer_1.invoices.create!
-    @invoice_2 = @customer_2.invoices.create!
-    @invoice_3 = @customer_3.invoices.create!
-    @invoice_4 = @customer_4.invoices.create!
-    @invoice_5 = @customer_5.invoices.create!
-    @invoice_6 = @customer_6.invoices.create!
+    @invoice_2 = @customer_1.invoices.create!
+    @invoice_3 = @customer_1.invoices.create!
+    @invoice_4 = @customer_2.invoices.create!
+    @invoice_5 = @customer_2.invoices.create!
+    @invoice_6 = @customer_3.invoices.create!
+    @invoice_7 = @customer_4.invoices.create!
+    @invoice_8 = @customer_4.invoices.create!
+    @invoice_9 = @customer_4.invoices.create!
+    @invoice_10 = @customer_4.invoices.create!
+    @invoice_11 = @customer_5.invoices.create!
+    @invoice_12 = @customer_5.invoices.create!
+    @invoice_13 = @customer_6.invoices.create!
+    @invoice_14 = @customer_6.invoices.create!
+    @invoice_15 = @customer_6.invoices.create!
+    @invoice_16 = @customer_6.invoices.create!
+    @invoice_17 = @customer_6.invoices.create!
+    @invoice_18 = @customer_6.invoices.create!
     #invoice items
     @invoice_1.invoice_items.create!(item_id: @item_1.id, quantity: 1, unit_price: @item_1.unit_price)
-    @invoice_1.invoice_items.create!(item_id: @item_2.id, quantity: 1, unit_price: @item_2.unit_price)
-    @invoice_1.invoice_items.create!(item_id: @item_3.id, quantity: 1, unit_price: @item_3.unit_price)
-    @invoice_1.invoice_items.create!(item_id: @item_4.id, quantity: 1, unit_price: @item_4.unit_price)
-    @invoice_2.invoice_items.create!(item_id: @item_5.id, quantity: 1, unit_price: @item_5.unit_price)
-    @invoice_2.invoice_items.create!(item_id: @item_6.id, quantity: 1, unit_price: @item_6.unit_price)
-    @invoice_2.invoice_items.create!(item_id: @item_7.id, quantity: 1, unit_price: @item_7.unit_price)
-    @invoice_3.invoice_items.create!(item_id: @item_8.id, quantity: 1, unit_price: @item_8.unit_price)
-    @invoice_3.invoice_items.create!(item_id: @item_9.id, quantity: 1, unit_price: @item_9.unit_price)
-    @invoice_3.invoice_items.create!(item_id: @item_10.id, quantity: 1, unit_price: @item_10.unit_price)
-    @invoice_3.invoice_items.create!(item_id: @item_1.id, quantity: 1, unit_price: @item_1.unit_price)
-    @invoice_3.invoice_items.create!(item_id: @item_1.id, quantity: 1, unit_price: @item_1.unit_price)
-    @invoice_4.invoice_items.create!(item_id: @item_2.id, quantity: 1, unit_price: @item_2.unit_price)
-    @invoice_5.invoice_items.create!(item_id: @item_7.id, quantity: 1, unit_price: @item_7.unit_price)
-    @invoice_5.invoice_items.create!(item_id: @item_8.id, quantity: 1, unit_price: @item_8.unit_price)
-    @invoice_5.invoice_items.create!(item_id: @item_9.id, quantity: 1, unit_price: @item_9.unit_price)
-    @invoice_6.invoice_items.create!(item_id: @item_2.id, quantity: 1, unit_price: @item_2.unit_price)
-    @invoice_6.invoice_items.create!(item_id: @item_3.id, quantity: 1, unit_price: @item_3.unit_price)
-    @invoice_6.invoice_items.create!(item_id: @item_4.id, quantity: 1, unit_price: @item_4.unit_price)
-    @invoice_6.invoice_items.create!(item_id: @item_5.id, quantity: 1, unit_price: @item_5.unit_price)
+    @invoice_2.invoice_items.create!(item_id: @item_2.id, quantity: 1, unit_price: @item_2.unit_price)
+    @invoice_3.invoice_items.create!(item_id: @item_3.id, quantity: 1, unit_price: @item_3.unit_price)
+    @invoice_4.invoice_items.create!(item_id: @item_4.id, quantity: 1, unit_price: @item_4.unit_price)
+    @invoice_5.invoice_items.create!(item_id: @item_5.id, quantity: 1, unit_price: @item_5.unit_price)
     @invoice_6.invoice_items.create!(item_id: @item_6.id, quantity: 1, unit_price: @item_6.unit_price)
-    @invoice_6.invoice_items.create!(item_id: @item_7.id, quantity: 1, unit_price: @item_7.unit_price)
+    @invoice_7.invoice_items.create!(item_id: @item_7.id, quantity: 1, unit_price: @item_7.unit_price)
+    @invoice_8.invoice_items.create!(item_id: @item_8.id, quantity: 1, unit_price: @item_8.unit_price)
+    @invoice_9.invoice_items.create!(item_id: @item_9.id, quantity: 1, unit_price: @item_9.unit_price)
+    @invoice_10.invoice_items.create!(item_id: @item_10.id, quantity: 1, unit_price: @item_10.unit_price)
+    @invoice_11.invoice_items.create!(item_id: @item_1.id, quantity: 1, unit_price: @item_1.unit_price)
+    @invoice_12.invoice_items.create!(item_id: @item_1.id, quantity: 1, unit_price: @item_1.unit_price)
+    @invoice_13.invoice_items.create!(item_id: @item_2.id, quantity: 1, unit_price: @item_2.unit_price)
+    @invoice_14.invoice_items.create!(item_id: @item_7.id, quantity: 1, unit_price: @item_7.unit_price)
+    @invoice_15.invoice_items.create!(item_id: @item_8.id, quantity: 1, unit_price: @item_8.unit_price)
+    @invoice_16.invoice_items.create!(item_id: @item_9.id, quantity: 1, unit_price: @item_9.unit_price)
+    @invoice_17.invoice_items.create!(item_id: @item_2.id, quantity: 1, unit_price: @item_2.unit_price)
+    @invoice_18.invoice_items.create!(item_id: @item_3.id, quantity: 1, unit_price: @item_3.unit_price)
+    #transaction
+    @invoice_1.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_2.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_3.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_4.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_5.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_6.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_7.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_8.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_9.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_10.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_11.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_12.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_13.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_14.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_15.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_16.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_17.transactions.create!(credit_card_number: "1", result: "success")
+    @invoice_18.transactions.create!(credit_card_number: "1", result: "success")
   end
 
   config.after(:each) do
