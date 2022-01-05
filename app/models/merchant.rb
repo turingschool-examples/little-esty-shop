d919fca65015cc1b237e40fc0bece8b7f86f30ca
Robin Dittrich <robinodittrich@gmail.com>
2022-01-04 20:01:33 -0800

    Complete User Story 4 Items Ready to Ship.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,12 @@ class Merchant < ApplicationRecord
   has_many :items
   validates :name, presence: true
 
-  def self.test
+  # def top_five_customers
+  #   customers = Item.where(merchant_id: self.id)
+  #   Transaction.find(merchant_id: :id).limit(5)
+  # end
+
+  def items_ready_to_ship
+    Item.joins(:invoice_items).where(invoice_items: {status: 1})
   end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe Merchant do
   describe 'relationships' do
     it { should have_many(:items) }
   end
+
+  describe 'class methods' do
+  #   it '#top_five_customers' do
+  #     expect(@merchant_1.top_five_customers).to eq([@customer_2, @customer_3, @customer_4, @customer_5, @customer_6])
+  #   end
+    it '#items_ready_to_ship' do
+      expect(@merchant_1.items_ready_to_ship).to eq([@item_1, @item_2, @item_3, @item_4])
+    end
+  end
 end
