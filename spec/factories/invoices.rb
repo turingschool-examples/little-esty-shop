653c69ff3d457e87983131caaa49413b24bc2d6b
Tim Roeder <timothyjroeder@gmail.com>
2022-01-05 00:18:22 -0700

    Feat: created model factories

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,6 +1,17 @@
 FactoryBot.define do
   factory :invoice do
-    customers { nil }
-    status { "" }
+    trait :in_progress do
+      status { 'in_progress' }
+    end
+
+    trait :cancelled do
+      status { 'cancelled' }
+    end
+
+    trait :completed do
+      status { 'completed' }
+    end
+    
+    customer
   end
 end
--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,24 @@
 FactoryBot.define do
   factory :merchant do
-    name { "MyString" }
+    sequence(:name) { |n| "merch_#{n}" }
+
+    factory :merch_w_all do
+      transient do
+        item_invoice_count { 10 }
+      end
+
+      after(:create) do |merchant, evaluator|
+        evaluator.item_invoice_count.times do
+          item = create(:item, merchant: merchant)
+          invoice = create(:invoice)
+          transaction = create(:transaction, invoice: invoice)
+          invoice_item = create(:invoice_item, item: item, invoice: invoice)
+          merchant.reload
+          item.reload
+          invoice.reload
+          invoice_item.reload
+        end
+      end
+    end
   end
 end
