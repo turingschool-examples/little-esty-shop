5e31ac03d107c4e2cb13b12f4e2bac019bca8908
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-04 18:59:34 -0700

    Feat: merchant invoices refactor

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,7 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 
   enum status: {'in progress' => 0, 'cancelled' => 1, 'completed' => 2}
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
   def self.invoice_finder(merchant_id)
-    invoice_items = []
-    invoices = []
-    items = Item.where(merchant_id: merchant_id)
-    items.each do |item|
-      invoice_items.push(InvoiceItem.where(item_id: item.id))
-    end
-    invoice_items.flatten.each do |invoice_item|
-      invoices.push(Invoice.where(id: invoice_item.invoice_id))
-    end
-    invoices.flatten
+    Invoice.joins(:invoice_items => :item).where(:items => {:merchant_id => merchant_id})
   end
 end
