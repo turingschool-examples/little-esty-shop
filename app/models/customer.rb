5d8d0d1612a91a3f5475bd48a6039c11772719aa
Kevin Gloss <kgloss89@gmail.com>
2022-01-05 18:29:35 -0700

    Add relationships and top five method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
   has_many :invoice_items, through: :invoices
   has_many :items, through: :invoice_items
-  has_one :merchant, through: :items
+  has_many :merchants, through: :items
   validates :first_name, presence: true
   validates :last_name, presence: true
 
