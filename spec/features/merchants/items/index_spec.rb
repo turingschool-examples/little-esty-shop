111a03fb06959e5577b41faf14ad361928097396
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-06 11:02:26 -0700

    Test: add test for page not to have content for another merchant

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -2,15 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'merchant item index page' do
   before(:each) do
-    @merchant = Merchant.create!(name: "Parker")
-    @item = @merchant.items.create!(name: "Small Thing", description: "Its a thing that is small.", unit_price: 400)
-    @item2 = @merchant.items.create!(name: "Large Thing", description: "Its a thing that is large.", unit_price: 800)
-    visit "/merchants/#{@merchant.id}/items"
+    @merchant_1 = Merchant.create!(name: "Parker")
+    @merchant_2 = Merchant.create!(name: "Kerri")
+    @item = @merchant_1.items.create!(name: "Small Thing", description: "Its a thing that is small.", unit_price: 400)
+    @item2 = @merchant_1.items.create!(name: "Large Thing", description: "Its a thing that is large.", unit_price: 800)
+    @item3 = @merchant_2.items.create!(name: "Medium Thing", description: "Its a thing that is medium.", unit_price: 600)
+    visit "/merchants/#{@merchant_1.id}/items"
   end
 
   it 'shows each item name' do
     expect(page).to have_content("Small Thing")
     expect(page).to have_content("Large Thing")
-  end  
+  end
 
+  it 'does not show items for any other merchant' do
+    expect(page).to_not have_content("Medium Thing")
+  end
 end
