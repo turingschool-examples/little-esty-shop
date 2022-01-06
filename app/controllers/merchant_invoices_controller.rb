c1d2e3e69eb38390ec154e19894c2dbd61f3dba1
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-06 12:09:04 -0700

    Feat: All tests passing for merchant_invoice#show

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -7,6 +7,6 @@ class MerchantInvoicesController < ApplicationController
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @invoice = Invoice.find(params[:invoice_id])
-    @invoiced_item = InvoiceItem.where(invoice_id: params[:invoice_id])
+    #require "pry"; binding.pry
   end
 end
