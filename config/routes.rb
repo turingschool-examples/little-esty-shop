29e30acd5da99adaed433c39c872bd0e303f6472
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-06 08:23:02 -0500

    Add route to #new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/merchants/:id/items', to: 'merchant_items#index'
+  get '/merchants/:id/items/new', to: 'merchant_items#new'
   get '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#show'
   get '/merchants/:merchant_id/items/:item_id/edit', to: 'merchant_items#edit'
   patch '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#update'
