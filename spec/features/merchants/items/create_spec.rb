24f9d80ba5ab6f8ce1bc6976fab9ac4ac761474e
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-06 08:54:47 -0500

    Add missing / to link in test

--- spec/features/merchants/items/create_spec.rb
+++ spec/features/merchants/items/create_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "Merchant Item Create page" do
 
   it "has a form for new item, and redirects to merchant items index with new item listed" do
-    @merchant1 = create(:merchant)
-    visit "merchants/#{@merchant1.id}/items/new"
+    merchant1 = create(:merchant)
+    visit "merchants/#{merchant1.id}/items/new"
 
     within('#create_item') do
       fill_in "Name:", with: "Paul's Item"
@@ -12,7 +12,7 @@ RSpec.describe "Merchant Item Create page" do
       fill_in "Unit Price:", with: 1000
       click_button "Submit"
 
-      expect(current_path).to eq("merchants/#{merchant1.id}/items")
+      expect(current_path).to eq("/merchants/#{merchant1.id}/items")
       expect(page).to have_content("Paul's Item")
       expect(page).to have_content("An item from Paul")
       expect(page).to have_content(1000)
