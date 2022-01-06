eaa7bd7f46563913ad08da2cb565219db906bc33
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-06 13:54:29 -0500

    add enum status to model.  needs to be tested and set to disabled by default

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,13 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+  enum status: { disabled: 0, enabled: 1 }
+  #need to make this default to disabled 
+
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true, numericality: {only_integer: true}
+
 
   def self.invoice_finder(merchant_id)
     Invoice.joins(:invoice_items => :item).where(:items => {:merchant_id => merchant_id})
