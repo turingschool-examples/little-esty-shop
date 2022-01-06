237aa3bd4f6dcd0aa9e15599311513ca3f4ce05f
Kevin Gloss <kgloss89@gmail.com>
2022-01-06 14:54:15 -0700

    Add default value for item status

--- db/migrate/20220106174749_add_item_status_to_items.rb
+++ db/migrate/20220106174749_add_item_status_to_items.rb
@@ -1,5 +1,5 @@
 class AddItemStatusToItems < ActiveRecord::Migration[5.2]
   def change
-    add_column :items, :item_status, :integer
+    add_column :items, :item_status, :integer, default: 0
   end
 end
