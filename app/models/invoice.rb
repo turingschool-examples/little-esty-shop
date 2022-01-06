06de5ffb4835ec2877554d8d4af9e77ee63c53e8
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-05 21:10:24 -0500

    Add transaction association to invoice model.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,7 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :invoice_items
   has_many :items, through: :invoice_items
+  has_a :transaction
 
   enum status: { in_progress: 0, cancelled: 1, completed: 2}
 
