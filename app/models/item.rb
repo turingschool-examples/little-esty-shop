a4b95b8c2a9c8d5ef03a2aab52e220eb777025cd
Karan Mehta <karanm645@gmail.com>
2022-01-06 17:03:05 -0600

    Create enable/disable model methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord 
+  enum status: ["enabled", "disabled"]
+
   belongs_to :merchant 
   has_many :invoice_items 
   has_many :invoices, through: :invoice_items 
+
+  def self.enabled_items
+    where(status: 0)
+  end
+
+  def self.disabled_items
+    where(status: 1)
+  end
 end 
