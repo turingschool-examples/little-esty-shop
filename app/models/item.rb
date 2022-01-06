e70f417b0f5d6346034e8d58ca8d0b41ada6c42f
Kevin Gloss <kgloss89@gmail.com>
2022-01-05 17:08:04 -0700

    Add relationships and merchant top_five method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :customers, through: :invoices
   belongs_to :merchant
 
   validates :name, :description, :unit_price, presence: true
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,11 +1,13 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items
+  has_many :customers, through: :invoices
   validates :name, presence: true
 
-  # def top_five_customers
-  #   customers = Item.where(merchant_id: self.id)
-  #   Transaction.find(merchant_id: :id).limit(5)
-  # end
+  def top_five_customers
+    customers.joins(:transactions).where(transactions: {result: "success"}).group(:id).select("customers.*, count(transactions) as good_transactions").order(good_transactions: :desc).limit(5)
+  end
 
   def items_ready_to_ship
     Item.joins(:invoice_items).where(invoice_items: {status: 1})
