8e3de23980c16e63babb9d41cc6ec97178667e71
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-06 12:03:18 -0700

    Create test for invoice#merchant_invoice_items, #merchant_items

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Invoice, type: :model do
       end
     end
 
-    describe '#merchant_invoice_items' do
+    describe '#merchant_items' do
       it 'organizes an invoices items by a given merchant' do
         merchant_1 = create(:merchant, name: 'Bob')
         invoice_1 = create(:invoice)
@@ -32,7 +32,19 @@ RSpec.describe Invoice, type: :model do
         item_2 = create(:item_with_invoices, name: 'Car', merchant: merchant_1, invoice: invoice_1)
         item_3 = create(:item_with_invoices, name: 'Candy', invoice: invoice_1)
 
-        expect(invoice_1.merchant_invoice_items(merchant_1)).to eq([item_1, item_1, item_2, item_2])
+        expect(invoice_1.merchant_items(merchant_1)).to eq([item_1, item_1, item_2, item_2])
+      end
+    end
+
+    describe '#merhcant_invoice_items' do
+      it 'organizes invoice items by a given merchant' do
+        merchant_1 = create(:merchant, name: 'Bob')
+        invoice_1 = create(:invoice)
+        item_1 = create(:item_with_invoices, name: 'Toy', merchant: merchant_1, invoice: invoice_1)
+        item_3 = create(:item_with_invoices, name: 'Candy', invoice: invoice_1)
+
+
+        expect(invoice_1.merchant_invoice_items(merchant_1)).to eq(item_1.invoice_items)
       end
     end
   end
