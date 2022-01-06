b7240da77bf0f5f7cea87d4d82670be9625b33d2
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2022-01-05 22:09:18 -0600

    Routing for merchant dashboard, items index, invoice index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get '/merchants/:merchant_id/dashboard', to: 'merchants#show'
-
   resources :merchants do
     resources :dashboard, only: [:index]
+    get '/items', to: 'merchant_items#index'
+    get '/invoices', to: 'merchant_invoices#index'
   end
 end
