5ad8ca9767ea82faf007c609f2554c1f89259076
Eric Chon <echon92@gmail.com>
2022-01-06 10:51:15 -0700

    Item names and invoice numbers showing

--- spec/features/transactions/new.html.erb_spec.rb
+++ spec/features/transactions/new.html.erb_spec.rb
@@ -9,16 +9,4 @@ RSpec.describe "transactions/new", type: :view do
     ))
   end
 
-  xit "renders new transaction form" do
-    render
-
-    assert_select "form[action=?][method=?]", transactions_path, "post" do
-
-      assert_select "input[name=?]", "transaction[invoices_id]"
-
-      assert_select "input[name=?]", "transaction[credit_card_number]"
-
-      assert_select "input[name=?]", "transaction[result]"
-    end
-  end
 end
