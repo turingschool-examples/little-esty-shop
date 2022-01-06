aa94629dc91cb525e41036eabcf99c1f2f051a12
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-06 09:25:52 -0700

    Create tests for Invoice#merchant_invoice_items

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -23,5 +23,17 @@ RSpec.describe Invoice, type: :model do
         expect(invoice1.customer_name).to eq("Default First Name 1 Default Last Name 1")
       end
     end
+
+    describe '#merchant_invoice_items' do
+      it 'organizes an invoices items by a given merchant' do
+        merchant_1 = create(:merchant, name: 'Bob')
+        invoice_1 = create(:invoice)
+        item_1 = create(:item_with_invoices, name: 'Toy', merchant: merchant_1, invoice: invoice_1)
+        item_2 = create(:item_with_invoices, name: 'Car', merchant: merchant_1, invoice: invoice_1)
+        item_3 = create(:item_with_invoices, name: 'Candy', invoice: invoice_1)
+
+        expect(invoice_1.merchant_invoice_items(merchant_1)).to eq([item_1, item_1, item_2, item_2])
+      end
+    end
   end
 end
