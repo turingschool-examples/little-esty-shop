4c3cdf8242c20876bcfabd2447665fbefcb711b2
Robin Dittrich <robinodittrich@gmail.com>
2022-01-06 14:42:38 -0800

    Add create new item test.

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -1,24 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Merchant item index" do
-  before :each do
-    @merchant1 = Merchant.create!(name: "Kelly")
-    @merchant2 = Merchant.create!(name: "Craig")
-    @item1 = @merchant1.items.create!(name: "Mixing Bowl", description: "xyz", unit_price: 500)
-    @item2 = @merchant1.items.create!(name: "Gumball", description: "abc", unit_price: 25)
-    @item3 = @merchant2.items.create!(name: "Hat", description: "sdasasdsd", unit_price: 88)
-  end
-
   it 'I see a list of the names of all of my items' do
-    visit merchant_items_path(@merchant1)
-    expect(page).to have_content(@item1.name)
-    expect(page).to have_content(@item2.name)
-    expect(page).to_not have_content(@item3.name)
+    visit "/merchants/#{@merchant_1.id}/items"
+
+    expect(page).to have_content(@item_1.name)
+    expect(page).to have_content(@item_2.name)
   end
 
   it 'each items name is a link to the show page' do
-    visit merchant_items_path(@merchant1)
-    click_link "#{@item1.name}"
-    expect(current_path).to eq("/merchants/#{@merchant1.id}/items/#{@item1.id}")
+    visit "/merchants/#{@merchant_1.id}/items"
+
+    click_link "#{@item_1.name}"
+    expect(current_path).to eq("/merchants/#{@merchant_1.id}/items/#{@item_1.id}")
+  end
+
+  # it 'displays two sections, one for Enabled Items and one for Disabled Items' do
+  #   visit "/merchants/#{@merchant_1.id}/items"
+  #
+  #   expect(page).to have_content("Enabled Items")
+  #   expect(page).to have_content("Disabled Items")
+  # end
+
+  # it 'has enabled items in the enabled section and disable items in disabled section' do
+  #   visit "/merchants/#{@merchant_1.id}/items"
+  #
+  #   within "Enabled Items" do
+  # end
+
+  it 'has a link to create a new item' do
+    visit "/merchants/#{@merchant_1.id}/items"
+
+    expect(page).to have_link("Create a New Item")
+    click_link "Create a New Item"
+
   end
 end
