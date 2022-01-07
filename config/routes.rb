605f55ede5e8ca5e866e31f1b0d91027e3bf8d05
Darren Kulback <dkulback@gmail.com>
2022-01-07 12:00:15 -0700

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-
-  # get "merchants/:id/items", to "merchant_items#index"
-  resources :merchants do
-    resources :items, only: [:index, :show, :edit, :update], controller: :merchant_items
-  end
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   root 'welcome#index'
   get '/welcome', to: 'welcome#index'
   get '/merchants/:id/dashboard', to: 'merchants#show'
-  # get "/merchants/:id/items", to: "items#show"
   resources :merchants, except: [:show] do
-    resources :items
+    resources :items, controller: :merchant_items
     resources :invoices
   end
 end
