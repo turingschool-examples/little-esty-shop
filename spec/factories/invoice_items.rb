198d6f948b9c831004e3f88d343deaa7a562ad1b
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2022-01-07 00:54:08 -0600

    Remove unneeded hardcoded enum traits

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
   factory :invoice_item do
     association :invoice, factory: :invoice
     association :item, factory: :item
-    traits_for_enum(:status)
     quantity { Faker::Number.number(digits: 2) }
     unit_price { Faker::Number.number(digits: 4) }
   end
