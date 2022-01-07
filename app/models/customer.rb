8340261cccef72b4e9d963386b998f6435c324c5
Dylan Harper <dylanharper4@icloud.com>
2022-01-07 10:36:00 -0800

    Finish functionality for us20

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,12 @@ class Customer < ApplicationRecord
   has_many :items, through: :invoices
   has_many :transactions, through: :invoices
   has_many :merchants, through: :items
+
+  def self.top_five_customers
+    all
+    .joins(invoices: :transactions)
+    .where('transactions.result = ?', 'success')
+    .group('id')
+    .order('transactions.count DESC')
+  end
 end
