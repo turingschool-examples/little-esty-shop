d5541389ef828fdd609177c873aa618bbef13d29
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2022-01-07 00:54:51 -0600

    Test: admin invoices index

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -1,67 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'admin invoices index page' do
-  let!(:merchant) {Merchant.create!(name: "Parker")}
-  let!(:merchant2) {Merchant.create!(name: "Joel")}
-  let!(:merchant3) {Merchant.create!(name: "Kerri")}
-  let!(:merchant4) {Merchant.create!(name: "John")}
+  before(:each) do
+    @invoice_item = FactoryBot.create(:invoice_item)
+    @invoice_item2 = FactoryBot.create(:invoice_item)
+    @invoice_item3 = FactoryBot.create(:invoice_item)
 
-  let!(:item) {merchant.items.create!(name: "Small Thing", description: "Its a thing that is small.", unit_price: 400)}
-  let!(:item2) {merchant.items.create!(name: "Large Thing", description: "Its a thing that is large.", unit_price: 800)}
-  let!(:item3) {merchant2.items.create!(name: "Medium Thing", description: "Its a thing that is medium.", unit_price: 600)}
-  let!(:item4) {merchant2.items.create!(name: "Extra Large Thing", description: "Its a thing that is extra large.", unit_price: 1000)}
-  let!(:item5) {merchant3.items.create!(name: "Tiny Thing", description: "Its a thing that is tiny.", unit_price: 200)}
-  let!(:item6) {merchant3.items.create!(name: "Huge Thing", description: "Its a thing that is huge.", unit_price: 1200)}
-  let!(:item7) {merchant4.items.create!(name: "Teensy Thing", description: "Its a thing that is teensy.", unit_price: 100)}
-  let!(:item8) {merchant4.items.create!(name: "Gargantuan Thing", description: "Its a thing that is gargantuan.", unit_price: 1400)}
-
-  let!(:customer) {Customer.create!(first_name: "Fred", last_name: "Rogers")}
-  let!(:customer2) {Customer.create!(first_name: "Big", last_name: "Bird")}
-  let!(:customer3) {Customer.create!(first_name: "Cookie", last_name: "Monster")}
-  let!(:customer4) {Customer.create!(first_name: "George", last_name: "Jetson")}
-
-  let!(:invoice) {customer.invoices.create!(status: "in progress")}
-  let!(:invoice2) {customer2.invoices.create!(status: "in progress")}
-  let!(:invoice3) {customer3.invoices.create!(status: "in progress")}
-  let!(:invoice4) {customer4.invoices.create!(status: "in progress")}
-
-  item.invoices.create!()
-
-  # before(:each) do
-  #   InvoiceItem.create!(invoice_id: invoice.id, item_id: item.id, quantity: 20, unit_price: 400, status: "pending")
-  #   InvoiceItem.create!(invoice_id: invoice.id, item_id: item6.id, quantity: 6, unit_price: 1200, status: "pending")
-  #   InvoiceItem.create!(invoice_id: invoice2.id, item_id: item2.id, quantity: 10, unit_price: 800, status: "pending")
-  #   InvoiceItem.create!(invoice_id: invoice2.id, item_id: item3.id, quantity: 7, unit_price: 600, status: "pending")
-  #   InvoiceItem.create!(invoice_id: invoice3.id, item_id: item4.id, quantity: 4, unit_price: 1000, status: "pending")
-  #   InvoiceItem.create!(invoice_id: invoice4.id, item_id: item7.id, quantity: 50, unit_price: 100, status: "pending")
-  #
-  # end
-
-  it 'shows all transaction ids' do
-    visit "/admin/invoices/"
-    expect(page).to have_content(invoice.id)
-    expect(page).to have_content(invoice2.id)
-    expect(page).to have_content(invoice3.id)
-    expect(page).to have_content(invoice4.id)
+    visit "/admin/invoices"
   end
 
-  it 'links to each transaction show page' do
-    visit "/admin/invoices/"
-    within("#admin-invoice-#{invoice.id}") do
-      click_link("#{invoice.id}")
-      expect(current_path).to eq("/admin/invoices/#{invoice.id}")
+  it 'shows invoice id and links to invoice show page' do
+    within("#admin-invoice-#{@invoice_item.invoice_id}") do
+      expect(page).to have_content(@invoice_item.invoice_id)
+      click_link("#{@invoice_item.invoice_id}")
+      expect(current_path).to eq("/admin/invoices/#{@invoice_item.invoice_id}")
     end
-    within("#admin-invoice-#{invoice2.id}") do
-      click_link("#{invoice2.id}")
-      expect(current_path).to eq("/admin/invoices/#{invoice2.id}")
+  end
+  it 'shows invoice2 id and links to show page' do
+    within("#admin-invoice-#{@invoice_item2.invoice_id}") do
+      expect(page).to have_content(@invoice_item2.invoice_id)
+      click_link("#{@invoice_item2.invoice_id}")
+      expect(current_path).to eq("/admin/invoices/#{@invoice_item2.invoice_id}")
     end
-    # within("#admin-invoice-#{@invoice3.id}") do
-    #   click_link("#{@invoice3.id}")
-    #   expect(current_path).to eq("/admin/invoices/#{@invoice3.id}")
-    # end
-    # within("#admin-invoice-#{@invoice4.id}") do
-    #   click_link("#{@invoice4.id}")
-    #   expect(current_path).to eq("/admin/invoices/#{@invoice4.id}")
-    # end
+  end
+  it 'shows invoice3 id and links to show page' do
+    within("#admin-invoice-#{@invoice_item3.invoice_id}") do
+      expect(page).to have_content(@invoice_item3.invoice_id)
+      click_link("#{@invoice_item3.invoice_id}")
+      expect(current_path).to eq("/admin/invoices/#{@invoice_item3.invoice_id}")
+    end
   end
 end
