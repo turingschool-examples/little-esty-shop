1b73260a43faa59ee17d1a3f8c53c4bbef664580
Kevin Gloss <kgloss89@gmail.com>
2022-01-06 18:27:14 -0700

    Add invoice transaction relationship

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,7 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
+  has_many :transactions, through: :invoice
   validates :quantity, :unit_price, :status, presence: true
   enum status: { pending: 0, packaged: 1, shipped: 2 }
 
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,5 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+  has_many :invoice_items, through: :invoice
   validates :credit_card_number, :result, presence: true
 end
