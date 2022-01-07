b514acea736621879fe76f7f49d2e06ce4820895
Kevin Gloss <kgloss89@gmail.com>
2022-01-07 16:13:37 -0700

    Update for new directory

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,6 +1,7 @@
 class InvoicesController < ApplicationController
   def index
+    # require 'pry'; binding.pry
     @merchant = Merchant.find(params[:merchant_id])
-    @invoices = Invoice.all.where(merchant_id: params[:merchant_id])
+    @invoices = @merchant.invoices
   end
 end
