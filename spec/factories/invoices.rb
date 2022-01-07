cca9601d97e745714887af476cea7c871e05b2ba
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-07 18:10:10 -0500

    Create invoice_with_transactions factory.

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -7,6 +7,15 @@ FactoryBot.define do
       status { 2 }
     end
 
+    factory :invoice_with_transactions do
+      transient do
+        transaction_count {2}
+        transaction_result {0}
+      end
+      create_list(:transaction, evaluator.transaction_count, invoice: invoice, status: evaluator.transaction_result)
+      end
+    end
+
     factory :completed_invoice, traits:[:completed]
   end
 end
