7c377510d5a54c16bb0d674a7e82b46dde4b0656
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-07 17:09:16 -0500

    Add status to schema for item

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2022_01_06_181004) do
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status"
+    t.integer "status", default: 0
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
