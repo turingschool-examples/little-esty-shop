39d4b408fa4c13756b6b868a88ca6b7b30568647
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2022-01-07 16:17:35 -0600

    Test: Add invoice instance method tests

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe Invoice, type: :model do
   describe "validations" do
     it { should validate_presence_of(:customer) }
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      @customer = FactoryBot.create(:customer, first_name: "Cookie", last_name: "Monster")
+      @invoice = FactoryBot.create(:invoice, customer: @customer)
+    end
+
+    describe 'pretty_created_at' do
+      it 'formats created_at datetime' do
+        expect(@invoice.pretty_created_at).to eq(Date.today.strftime("%A, %B%-d, %Y"))
+      end
+    end
+
+    describe 'customer_name' do
+      it 'outputs customer full name' do
+        expect(@invoice.customer_name).to eq("Cookie Monster")
+      end
+    end
+  end
 end
