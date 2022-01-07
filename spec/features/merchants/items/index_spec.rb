ad7eff18f340b3b3bcfdb8a6ee8c95029db4a42a
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-07 18:11:03 -0500

    Create test for enabled and disabled for merchant items index

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -56,11 +56,26 @@ RSpec.describe 'Merchant Items Index page' do
     expect(current_path).to eq("/merchants/#{merchant.id}/items/new")
   end
 
-  it "has two sections, one for enabled items, and one for disabled items" do
-    visit "/merchants/#{item1.merchant_id}/items"
+  it "has two sections, one for enabled items, and one for disabled items. Items are separated to these sections based on status" do
+    merchant = create(:merchant)
+    item1 = create(:item, merchant: merchant, name: "Paul")
+    item2 = create(:item, merchant: merchant, name: "Leland")
+    item3 = create(:item, merchant: merchant, name: "Josh", status: 1)
+    visit "/merchants/#{merchant.id}/items"
 
     expect(page).to have_content("Enabled Items:")
     expect(page).to have_content("Disabled Items:")
-    #Not sure what this means so I am just going to add this ^ to the view for now
+
+    within("div.Enabled_Items") do
+      expect(page).to have_content(item1.name)
+      expect(page).to have_content(item2.name)
+      expect(page).to_not have_content(item3.name)
+    end
+
+    within("div.Disabled_Items") do
+      expect(page).to_not have_content(item1.name)
+      expect(page).to_not have_content(item2.name)
+      expect(page).to have_content(item3.name)
+    end
   end
 end
