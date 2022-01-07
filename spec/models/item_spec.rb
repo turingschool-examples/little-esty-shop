d8f61bb0f5f807f425b93adbbf0fe3caf81aa54c
Sierra Tucker <sierra.tucker121@gmail.com>
2022-01-07 11:15:44 -0700

    Test: change test data to have different quantity values in each invoice_item and different invoices

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,35 +7,35 @@ RSpec.describe Item do
     it { should have_many(:invoices).through(:invoice_items) }
   end
 
-  
+
   describe 'instance methods' do
     before(:each) do
       @merchant_1 = Merchant.create!(name: 'Ron Swanson')
-      
+
       @item_1 = @merchant_1.items.create!(name: "Necklace", description: "A thing around your neck", unit_price: 1000)
-      
+
       @customer_1 = Customer.create!(first_name: "Billy", last_name: "Joel")
       @customer_2 = Customer.create!(first_name: "Britney", last_name: "Spears")
       @customer_3 = Customer.create!(first_name: "Prince", last_name: "Mononym")
-  
+
       @invoice_1 = @customer_1.invoices.create!(status: 1, created_at: '2012-03-25 09:54:09')
       @invoice_2 = @customer_2.invoices.create!(status: 1, created_at: '2012-04-25 08:54:09')
       @invoice_3 = @customer_3.invoices.create!(status: 1, created_at: '2012-10-25 04:54:09')
       @invoice_5 = @customer_1.invoices.create!(status: 1, created_at: '2012-03-28 12:54:09')
       @invoice_6 = @customer_1.invoices.create!(status: 1, created_at: '2012-03-29 12:54:09')
       @invoice_7 = @customer_1.invoices.create!(status: 1, created_at: '2012-05-28 12:54:09')
-  
-      @invoice_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, unit_price: 1000, quantity: 1, status: 0)
-      @invoice_item_2 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, unit_price: 900, quantity: 1, status: 0)
-      @invoice_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, unit_price: 1000, quantity: 1, status: 0)
-      @invoice_item_2 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, unit_price: 900, quantity: 1, status: 0)
-      @invoice_item_3 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_3.id, unit_price: 1500, quantity: 3, status: 0)
+
+      @invoice_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, unit_price: 1000, quantity: 3, status: 0)
+      @invoice_item_2 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_2.id, unit_price: 1000, quantity: 4, status: 0)
+      @invoice_item_3 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_3.id, unit_price: 1000, quantity: 1, status: 0)
+      @invoice_item_4 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_5.id, unit_price: 1000, quantity: 9, status: 0)
+      @invoice_item_5 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_6.id, unit_price: 1000, quantity: 2, status: 0)
     end
 
     describe '#date_with_most_sales' do
       it 'shows date' do
-        expect(@item_1.date_with_most_sales).to eq("03/25/2012")
+        expect(@item_1.date_with_most_sales).to eq("03/28/2012")
       end
     end
   end
-end 
+end
