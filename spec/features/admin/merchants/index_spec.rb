ff624d74083a25b183e408cadc97e279a2ba9ce9
Sierra Tucker <sierra.tucker121@gmail.com>
2022-01-08 10:50:54 -0700

    Update: add let blocks

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin Merchants Index' do
+  let!(:merchant_1) {Merchant.create!(name: 'Ron Swanson')}
+  let!(:merchant_2) {Merchant.create!(name: 'Leslie Knope')}
+  let!(:merchant_3) {Merchant.create!(name: 'Oprah Winfrey')}
+  let!(:merchant_4) {Merchant.create!(name: 'Leonardo Dicaprio')}
+  
   scenario 'visitor sees the names of each merchant in the system' do
-    merchant_1 = Merchant.create!(name: 'Ron Swanson')
-    merchant_2 = Merchant.create!(name: 'Leslie Knope')
-    merchant_3 = Merchant.create!(name: 'Tom Haverford')
-    merchant_4 = Merchant.create!(name: 'April Ludgate')
-
     visit admin_merchants_path
 
     expect(page).to have_content(merchant_1.name)
