0799f1c2245f8e094d2c90c4e7d124e25434ee53
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-07 17:44:27 -0700

    add functionality to mechant items index page to have a button to  enable or disable an item

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,11 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
   validates :name, :description, :merchant, presence: true
   validates :unit_price, numericality: { only_integer: true }
+
+  # enum result: {
+  #   disabled: 0,
+  #   enabled: 1
+  # }
+
+  enum status: [:disabled, :enabled]
 end
--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -1,27 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe 'merchant item index page' do
-  before(:each) do
-    @merchant_1 = Merchant.create!(name: "Parker")
-    @merchant_2 = Merchant.create!(name: "Kerri")
-    @item1 = @merchant_1.items.create!(name: "Small Thing", description: "Its a thing that is small.", unit_price: 400)
-    @item2 = @merchant_1.items.create!(name: "Large Thing", description: "Its a thing that is large.", unit_price: 800)
-    @item3 = @merchant_2.items.create!(name: "Medium Thing", description: "Its a thing that is medium.", unit_price: 600)
-    visit "/merchants/#{@merchant_1.id}/items"
+  describe 'merchant show page' do
+    before(:each) do
+      @merchant_1 = Merchant.create!(name: "Parker")
+      @merchant_2 = Merchant.create!(name: "Kerri")
+      @item1 = @merchant_1.items.create!(name: "Small Thing", description: "Its a thing that is small.", unit_price: 400)
+      @item2 = @merchant_1.items.create!(name: "Large Thing", description: "Its a thing that is large.", unit_price: 800)
+      @item3 = @merchant_2.items.create!(name: "Medium Thing", description: "Its a thing that is medium.", unit_price: 600)
+      visit "/merchants/#{@merchant_1.id}/items"
+    end
+
+    it 'shows each item name' do
+      expect(page).to have_content("Small Thing")
+      expect(page).to have_content("Large Thing")
+    end
+
+    it 'does not show items for any other merchant' do
+      expect(page).to_not have_content("Medium Thing")
+    end
+
+    it 'the item name is a link to the merchant items show page' do
+      click_link("#{@item1.name}")
+
+      expect(current_path).to eq("/merchants/#{@merchant_1.id}/items/#{@item1.id}")
+    end
   end
 
-  it 'shows each item name' do
-    expect(page).to have_content("Small Thing")
-    expect(page).to have_content("Large Thing")
-  end
+  describe 'enable/disable button' do
+    it 'has a button to enable/disable an item' do
+      merch_1 = Merchant.create!(name: "Clothing Store")
+      item_1 = merch_1.items.create!(name: "Sweater", description: "Red Sweater", unit_price: 40)
+      item_2 = merch_1.items.create!(name: "Hat", description: "Beanie", unit_price: 20, status: 1)
+      item_3 = merch_1.items.create!(name: "Shoes", description: "Running Shoes", unit_price: 80)
 
-  it 'does not show items for any other merchant' do
-    expect(page).to_not have_content("Medium Thing")
-  end
+      visit "/merchants/#{merch_1.id}/items"
 
-  it 'the item name is a link to the merchant items show page' do
-    click_link("#{@item1.name}")
+      within("#item-#{item_1.id}") do
+        expect(item_1.status).to eq("disabled")
 
-    expect(current_path).to eq("/merchants/#{@merchant_1.id}/items/#{@item1.id}")
+        first(:button, "Enable").click
+
+        expect(page).to have_button("Disable")
+        expect(current_path).to eq("/merchants/#{merch_1.id}/items")
+        updated_item_1 = Item.find(item_1.id)
+        expect(updated_item_1.status).to eq("enabled")
+      end
+
+      within("#item-#{item_2.id}") do
+        expect(item_2.status).to eq("enabled")
+        first(:button, "Disable").click
+
+        expect(page).to have_button("Enable")
+        expect(current_path).to eq("/merchants/#{merch_1.id}/items")
+        updated_item_2 = Item.find(item_2.id)
+        expect(updated_item_2.status).to eq("disabled")
+      end
+    end
   end
 end
