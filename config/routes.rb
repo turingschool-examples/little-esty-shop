73f95c65a1d1ed9b1f3ef5599c868df6a591a61e
Sierra Tucker <sierra.tucker121@gmail.com>
2022-01-07 19:13:27 -0700

    Update: add admin routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # get '/merchants/:merchant_id/dashboard', to: 'dashboard#index'
-
-  resources :merchants do 
+  namespace :admin do
+    resources :merchants
+  end
+  
+  resources :merchants do
     resources :items, :invoices
     resources :dashboard, only: [:index]
   end
