2357d1d335e705402bae644cd7828f5a02ce2d8f
Robin Dittrich <robinodittrich@gmail.com>
2022-01-07 17:25:24 -0800

    Add show method to Invoice controller.

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 RSpec.describe "Merchant invoice show" do
   it 'shows all the information relation to the invoice' do
     visit merchant_invoice_path(@merchant_1, @invoice_1)
+
     expect(page).to have_content(@invoice_1.id)
     expect(page).to have_content(@invoice_1.status)
     expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %d, %Y") )
     expect(page).to have_content(@invoice_1.customer.first_name)
     expect(page).to have_content(@invoice_1.customer.last_name)
+  end
+
+  it 'shows all items and information' do
+    visit merchant_invoice_path(@merchant_1, @invoice_1)
+
+    expect(page).to have_content(@Item_1.name)
+    expect(page).to have_content(@Item_1.quantity)
   end
 end
