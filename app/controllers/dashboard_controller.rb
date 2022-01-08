8251a005ea4ca6fd99f96f0605af3579d56a6c1f
Joel Grant <joelmgrant@me.com>
2022-01-08 09:29:49 -0700

    Remove extra instance variables from controller, moved to view.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,5 @@
 class DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @customers = @merchant.top_customers
-    @items = @merchant.ready_items
   end
 end
