1b2cc18361e147e388eb9d6065a8090ed8a3ecdc
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-08 08:48:29 -0700

    add filter_item_status instance method and test for Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :items
-  validates :name, presence: true 
+  validates :name, presence: true
+
+  def filter_item_status(status_enum)
+    items.where(status: status_enum)
+  end
 end
