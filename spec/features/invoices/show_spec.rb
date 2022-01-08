79d25f16957cbd09545cc1880ffe4340a2ce1661
Kevin Gloss <kgloss89@gmail.com>
2022-01-08 08:53:05 -0700

    Update testing to be for invoice_items and have quantity

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe "Merchant invoice show" do
   it 'shows all items and information' do
     visit merchant_invoice_path(@merchant_1, @invoice_1)
 
-    @invoice_1.items.each do |item|
-      expect(page).to have_content(item.name)
-      #ADD QUANTITY METHOD THAT ACTUALLY COUNTS THIS<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-      expect(page).to have_content(item.unit_price)
-      expect(page).to have_content(item.item_status)
+    @invoice_1.invoice_items.each do |invoice_item|
+      expect(page).to have_content(invoice_item.item.name)
+      expect(page).to have_content(invoice_item.quantity)
+      expect(page).to have_content(h.number_to_currency(invoice_item.unit_price))
+      expect(page).to have_content(invoice_item.status)
     end
   end
 
