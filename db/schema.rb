fe011384d183d16a719151452404be73001acce5
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2022-01-08 14:50:38 -0600

    Migration: add status to merchants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_210910) do
+ActiveRecord::Schema.define(version: 2022_01_08_023103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2022_01_04_210910) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "transactions", force: :cascade do |t|
