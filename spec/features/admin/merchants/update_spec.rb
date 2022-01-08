f7e6178ba1e96f3938b176f6fc0f5a5ba0dd7c46
Kevin Gloss <kgloss89@gmail.com>
2022-01-08 14:58:34 -0700

    Pass in merchant to expected path

--- spec/features/admin/merchants/update_spec.rb
+++ spec/features/admin/merchants/update_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Admin merchant edit feature" do
   describe 'edit page' do
-    visit admin_merchant_path(@merchant_1)
+    it 'has a link to the edit page' do
+      visit admin_merchant_path(@merchant_1)
 
-    click_link "Update #{@merchant_1.name}"
+      click_link "Update #{@merchant_1.name}"
 
-    expect(current_path).to eq(edit_admin_merchant_path)
+      expect(current_path).to eq(edit_admin_merchant_path(@merchant_1))
+    end
   end
 end
