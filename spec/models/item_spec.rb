0b36fe343fee4b251e0244e364112a8f4eacaee9
Kevin Gloss <kgloss89@gmail.com>
2022-01-07 21:34:35 -0700

    Test top_item_best_day class method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,4 +13,9 @@ RSpec.describe Item, type: :model do
     it { should have_many(:invoices).through(:invoice_items) }
     it { should have_many(:customers).through(:invoices) }
   end
+
+  it '#top_item_best_day' do
+    expect(@item_7.invoice_items.first.quantity).to eq(@item_7.invoice_items.second.quantity)
+    expect(@item_7.top_item_best_day).to eq(@item_7.invoices.first)
+  end
 end
