8a00b086e21ccc6c8d0e3406af89511599843017
Robin Dittrich <robinodittrich@gmail.com>
2022-01-07 18:17:18 -0800

    Add tests for item info.

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -14,7 +14,20 @@ RSpec.describe "Merchant invoice show" do
   it 'shows all items and information' do
     visit merchant_invoice_path(@merchant_1, @invoice_1)
 
-    expect(page).to have_content(@Item_1.name)
-    expect(page).to have_content(@Item_1.quantity)
+    @invoice_1.items.each do |item|
+      expect(page).to have_content(item.name)
+      #>>>>>>>WE NEED TO INCLUDE QUANTITY<<<<<<<<<<<<
+      expect(page).to have_content(item.unit_price)
+      expect(page).to have_content(item.item_status)
+    end
+  end
+
+  it 'does not show other merchants items info' do
+    other_merchant = Merchant.create!(name: "Other Merchant")
+    other_merchant_item = other_merchant.items.create!(name: "Other Merchant Item", description: "Description of other merchants item", unit_price: 33)
+
+    visit merchant_invoice_path(@merchant_1, @invoice_1)
+
+    expect(page).to_not have_content("#{other_merchant_item.name}")
   end
 end
