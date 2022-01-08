787f6e78ffcf6e90602abcca6feb907104662c1a
Mallory Vining <mallorygallery12@gmail.com>
2022-01-07 18:13:02 -0700

    Add admin invoice ids

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -1,5 +1,5 @@
 class Admin::InvoicesController < ApplicationController
   def index
-
+    @invoices = Invoice.all
   end
 end
---
+++ spec/features/admin/invoices/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Invoice Index page' do
+  before(:each) do
+    @merch_1 = Merchant.create!(name: "Shop Here")
+
+    @item_1 = Item.create!(name:"jumprope", description:"Pink and sparkly.", unit_price:600, merchant_id:"#{@merch_1.id}")
+    @item_2 = Item.create!(name:"hula hoop", description:"Get your groove on!", unit_price:700, merchant_id:"#{@merch_1.id}")
+
+    @cust_1 = Customer.create!(first_name:"Hannah", last_name:"Warner")
+
+    @invoice_1 = Invoice.create!(customer_id:"#{@cust_1.id}", status:1)
+    @invoice_2 = Invoice.create!(customer_id:"#{@cust_1.id}", status:1)
+    @invoice_3 = Invoice.create!(customer_id:"#{@cust_1.id}", status:1)
+
+    @invoice_item_1 = InvoiceItem.create!(invoice_id:"#{@invoice_1.id}", item_id:"#{@item_1.id}", status: 2, quantity:1, unit_price:600)
+    @invoice_item_2 = InvoiceItem.create!(invoice_id:"#{@invoice_2.id}", item_id:"#{@item_2.id}", status: 2, quantity:1, unit_price:700)
+    @invoice_item_3 = InvoiceItem.create!(invoice_id:"#{@invoice_3.id}", item_id:"#{@item_2.id}", status: 2, quantity:1, unit_price:700)
+  end
+
+  it 'displays a list of all Invoice ids in the system' do
+    visit admin_invoices_path
+
+    expect(page).to have_content(@invoice_1.id)
+    expect(page).to have_content(@invoice_2.id)
+    expect(page).to have_content(@invoice_3.id)
+  end
+
+  it 'links to admin invoice show page' do
+  end
+end
