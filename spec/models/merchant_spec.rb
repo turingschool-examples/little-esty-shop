19682a49f8cc20cb34a875639c7165674bd7a217
Seth Perna <gseth26@gmail.com>
2022-01-08 16:15:22 -0500

    model test for diabled_merchants and enbled_merchants

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,4 +10,24 @@ describe Merchant, type: :model do
     it { should define_enum_for(:status).with_values({enable: 0, disable: 1})}
   end
 
+  describe 'instance and class methods' do
+    it '.enabled_merchants' do
+      merchant_1 = Merchant.create!(name: 'merchant_1')
+      merchant_2 = Merchant.create!(name: 'merchant_2', status: 0)
+      merchant_3 = Merchant.create!(name: 'merchant_3')
+      merchant_4 = Merchant.create!(name: 'merchant_4', status: 0)
+
+      expect(Merchant.enabled_merchants).to eq([merchant_2, merchant_4])
+    end
+
+    it '.disabled_merchants' do
+      merchant_1 = Merchant.create!(name: 'merchant_1')
+      merchant_2 = Merchant.create!(name: 'merchant_2', status: 0)
+      merchant_3 = Merchant.create!(name: 'merchant_3')
+      merchant_4 = Merchant.create!(name: 'merchant_4', status: 0)
+
+      expect(Merchant.disabled_merchants).to eq([merchant_1, merchant_3])
+    end
+  end
+
 end
