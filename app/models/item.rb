d98f8e8b54591ca809fc28a17266cd38e6dfcc87
Kevin Gloss <kgloss89@gmail.com>
2022-01-07 22:16:23 -0700

    Remove redundant methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,14 +7,6 @@ class Item < ApplicationRecord
   validates :name, :description, :unit_price, presence: true
   enum item_status: { disabled: 0, enabled: 1}
 
-  def enabled
-    items.where(item_status: 1)
-  end
-
-  def disabled
-    items.where(item_status: 0)
-  end
-
   # are the successful transactions redundant here after doing it in top_five_items
   # good or bad to include the updated_at on the end?
   def top_item_best_day
