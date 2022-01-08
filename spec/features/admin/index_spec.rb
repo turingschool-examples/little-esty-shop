c30b012d47293c5dc956a783ff27b1883ee71340
Robin Dittrich <robinodittrich@gmail.com>
2022-01-08 13:05:12 -0800

    Create test for incomplete invoice links.

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -19,6 +19,20 @@ RSpec.describe 'Admin Dashboard Index' do
       visit "/admin"
       click_link "Invoices"
       expect(current_path).to eq('/admin/invoices')
-    end 
+    end
+
+    it 'I see a section for Incomplete Invoices with IDs of all invoices' do
+      visit "/admin"
+
+      expect(page).to have_content("Incomplete Invoices")
+      within "#incompleteinvoices" do
+        Invoice.incomplete.each do |invoice|
+          expect(page).to have_content(invoice.id)
+        end
+        click_link("#{@invoice_4.id}")
+      end
+
+      expect(current_path).to eq(admin_invoice_path(@invoice_4.id))
+    end
   end
 end
