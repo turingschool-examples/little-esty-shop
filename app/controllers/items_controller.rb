f92ac3e046394627b411ae5d5668ae7d6687c6c0
Karan Mehta <karanm645@gmail.com>
2022-01-07 19:46:56 -0600

    Create new and create method for items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,8 +15,18 @@ class ItemsController < ApplicationController
       @item.update(status: params[:disable].to_i)
       redirect_to merchant_items_path(@merchant.id) 
      elsif params[:enable] 
-      @item.update(status: params[:enable].to_i)
       redirect_to merchant_items_path(@merchant.id)
     end 
+  end
+
+  def new 
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create 
+    item = Item.create(name: params[:name], description: params[:description], unit_price: params[:unit_price], merchant_id: params[:merchant_id], status: params[:status])
+    merchant = Merchant.find(params[:merchant_id])
+    item.save 
+    redirect_to merchant_items_path(merchant.id)
   end
 end
