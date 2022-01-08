4c24ec4fd5ec2e37478818b6c5d5a5e26c52a5aa
Sierra Tucker <sierra.tucker121@gmail.com>
2022-01-08 13:04:07 -0700

    Update: add edit, update and strong params

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -4,6 +4,23 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def show
-    @merchant = Merchant.find(params[:id]) 
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    merchant.update(merch_params)
+    redirect_to admin_merchant_path(merchant.id)
+    flash[:notice] = "Successfully Updated Merchant Information"
+  end
+
+  private
+
+  def merch_params
+    params.require(:merchant).permit(:name)
   end
 end
