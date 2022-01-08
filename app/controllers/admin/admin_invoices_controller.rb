e5f0a9a43952a111151d36b86089c22593eac9a3
Dylan Harper <dylanharper4@icloud.com>
2022-01-08 14:58:07 -0800

    Finish user story 5

--- app/controllers/admin/admin_invoices_controller.rb
+++ app/controllers/admin/admin_invoices_controller.rb
@@ -1,11 +1,26 @@
 class Admin::AdminInvoicesController < ApplicationController
 
-def index
-  @invoices = Invoice.all
-end
+  def index
+    @invoices = Invoice.all
+  end
 
-def show
-  @invoice = Invoice.find(params[:id])
-end
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def update
+    invoice = Invoice.find(params[:id])
+    invoice.update(invoice_params)
+
+    redirect_to action: :show
+  end
+
+  private
+
+  def invoice_params
+    params.permit(:status)
+  end
+
+
 
 end
