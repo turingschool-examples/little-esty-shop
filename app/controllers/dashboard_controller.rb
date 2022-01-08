35b80f1e6a5f104e7dad7243abc2f26c3f85e978
Joel Grant <joelmgrant@me.com>
2022-01-07 22:44:12 -0700

    Feat: Create merchant dashboard with top 5 customers and items

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
+    @customers = @merchant.top_customers
+    @items = @merchant.ready_items
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2022_01_04_210910) do
   create_table "transactions", force: :cascade do |t|
     t.bigint "invoice_id"
     t.string "credit_card_number"
-    t.datetime "credit_card_expiration_date"
+    t.date "credit_card_expiration_date"
     t.integer "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
