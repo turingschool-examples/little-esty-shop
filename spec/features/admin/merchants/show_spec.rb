717316214a88d799199f7a3f82c6a59b81e3e23f
Hannah Warren <hannahkwarren@gmail.com>
2022-01-08 13:27:04 -0500

    Clarify show page test description

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Admin Merchant Show page" do 
 
-  it "" do
+  it "show page provides only one merchant's data" do
     merch_1 = Merchant.create!(name: "Easily Amused Studio")
     merch_2 = Merchant.create!(name: "Retro Furniture")
     
-    visit "admin/merchants/#{merch_1.id}"
+    visit "/admin/merchants/#{merch_1.id}"
 
     expect(page).to have_content(merch_1.name)
     expect(page).to_not have_content(merch_2.name)
