346a60b57baac4f0f68cf3a4cc203c370de60ace
Hannah Warren <hannahkwarren@gmail.com>
2022-01-08 13:20:01 -0500

    Feature: Admin Merchant Show page

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,5 +1,17 @@
 class Admin::MerchantsController < ApplicationController
+
   def index
     @merchants = Merchant.all
   end
+
+  def show
+    @merchant = Merchant.find(admin_merchant_params[:id])
+  end
+
+private 
+
+  def admin_merchant_params
+    params.permit(:id)
+  end
+
 end
