78b190836a545a2bc71caf856772eba951842623
Kevin Gloss <kgloss89@gmail.com>
2022-01-08 10:53:53 -0700

    Remove empty method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -4,8 +4,4 @@ class InvoiceItem < ApplicationRecord
   has_many :transactions, through: :invoice
   validates :quantity, :unit_price, :status, presence: true
   enum status: { pending: 0, packaged: 1, shipped: 2 }
-
-  def self.ready_to_ship
-
-  end
 end
