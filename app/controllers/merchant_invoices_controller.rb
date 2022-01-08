773f8dfb51dab240d4fff69dafd68fe6e195da72
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-07 17:41:44 -0700

    Removed erroneous pry

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -6,6 +6,6 @@ class MerchantInvoicesController < ApplicationController
 
   def show
     @merchant = Merchant.find(params[:merchant_id])
-    @invoice = Invoice.find(params[:invoice_id]
+    @invoice = Invoice.find(params[:invoice_id])
   end
 end
