57cd51098726b568315e062cd4767f182affaf54
Devin Pile <devinpile@gmail.com>
2022-01-09 14:25:01 -0700

    Add merchant invoices index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :merchants, only:[:show] do
     resources :items, except:[:destroy]
     resources :item_status, only:[:update]
-    resources :invoices, only:[:index]
+    resources :invoices, only:[:index, :show]
     resources :dashboard, only:[:index]
   end
 
