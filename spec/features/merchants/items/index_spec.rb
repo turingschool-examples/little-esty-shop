7ca58e1a147eee010d654dde80e8f456e0c735f4
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2022-01-08 23:10:15 -0600

    typo fix

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -21,10 +21,11 @@ RSpec.describe 'merchant item index page' do
     end
 
     it 'the item name is a link to the merchant items show page' do
-      click_link("#{@item.name}")
+      click_link("#{@item1.name}")
 
-      expect(current_path).to eq("/merchants/#{@merchant_1.id}/items/#{@item.id}")
+      expect(current_path).to eq("/merchants/#{@merchant_1.id}/items/#{@item1.id}")
     end
+  end
 
   describe 'enable/disable button' do
     it 'has a button to enable/disable an item' do
