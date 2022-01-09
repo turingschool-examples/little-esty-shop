d461ad386b2be403744b105b74e4b4f4b41ca8b5
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-08 20:40:54 -0700

    Create tests for admin_invoice_show

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -12,4 +12,42 @@ RSpec.describe 'Admin_Invoices Show Page' do
     expect(page).to have_content("Thursday, January 06, 2022")
     expect(page).to have_content(invoice.customer_name)
   end
+
+  it 'shows the invoiced items name' do
+    merchant = create(:merchant)
+    invoice = create(:invoice)
+    item = create(:item_with_invoices, merchant: merchant, invoices: [invoice], name: 'Toy')
+
+    visit "/admin/invoices/#{invoice.id}"
+
+    expect(page).to have_content("Item: Toy")
+  end
+
+  it 'shows the quantity of the item ordered' do
+    merchant = create(:merchant)
+    invoice = create(:invoice)
+    item = create(:item_with_invoices, merchant: merchant, invoices: [invoice], quantity: 12)
+    visit "/admin/invoices/#{invoice.id}"
+    expect(page).to have_content("Quantity Ordered: 12")
+  end
+
+  xit 'shows the price the item sold for' do
+    merchant = create(:merchant)
+    invoice = create(:invoice)
+    item = create(:item_with_invoices, merchant: merchant, invoices: [invoice], unit_price: 13000)
+
+    visit "/admin/invoices/#{invoice.id}"
+
+    expect(page).to have_content("Unit Price: $130.00")
+  end
+
+  xit 'shows the invoice items status' do
+    merchant = create(:merchant)
+    invoice = create(:invoice)
+    item = create(:item_with_invoices, merchant: merchant, invoices: [invoice], name: 'Toy', status: "shipped")
+
+    visit "/admin/invoices/#{invoice.id}"
+
+    expect(page).to have_content("Status: shipped")
+  end
 end
