563ff7792e9b2d2152bb9e91e975b14889b2fc04
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2022-01-09 17:16:46 -0600

    Create top 5 method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,13 @@ class Customer < ApplicationRecord
   def transaction_count
     transactions.count
   end
+
+  def self.top_5
+    joins(invoices: :transactions)
+      .select("customers.*, count(transactions.id) as transaction_count")
+      .limit(5)
+      .group(:id)
+      .order(transaction_count: :desc)
+      .where(transactions: { result: 1 })
+  end
 end
