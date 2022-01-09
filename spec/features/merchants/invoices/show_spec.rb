d536f91840686b33c9f9b9602ae7c97a2fd79722
Brad Breiten <jonathan@breiten.us>
2022-01-09 15:33:27 -0700

    Fix: remove save_and_open_page and remove commented out user story

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -85,8 +85,6 @@ RSpec.describe 'Merchant Invoice Show Page', type: :feature do
     end
 
     scenario 'the item select field is populated by item status and changes when updated' do
-      save_and_open_page
-
       within "#item#{item_1.id}" do
         expect(page).to have_field('Status', with: 'disabled')
         expect(page).to have_button("Update Item Status")
@@ -98,21 +96,6 @@ RSpec.describe 'Merchant Invoice Show Page', type: :feature do
         expect(page).to have_field('Status', with: 'enabled')
         expect(page).to have_button("Update Item Status")
       end
-
-      save_and_open_page
-
     end
   end
-  #   As a merchant
-  # When I visit my merchant invoice show page
-  # I see that each invoice item status is a select field
-  # And I see that the invoice item's current status is selected
-  # When I click this select field,
-  # Then I can select a new status for the Item,
-  # And next to the select field I see a button to "Update Item Status"
-  # When I click this button
-  # I am taken back to the merchant invoice show page
-  # And I see that my Item's status has now been updated
-
-
 end
