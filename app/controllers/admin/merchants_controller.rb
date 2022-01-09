9e9c046796e77ebd1292a43359b7ce5b165f50de
Hannah Warren <hannahkwarren@gmail.com>
2022-01-08 20:23:14 -0500

    Feature: Create a Merchant

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -9,7 +9,17 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def new 
-    
+    merchant = Merchant.new
+  end
+
+  def create 
+    merchant = Merchant.new(admin_merchant_params)
+    if merchant.save
+      redirect_to "/admin/merchants"
+    else
+      redirect_to "/admin/merchants/new"
+      flash[:alert] = "Error: #{error_message(merchant.errors)}"
+    end
   end
 
   def edit
