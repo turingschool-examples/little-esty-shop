6cc5a124b6e58c91eda92f1ebfbcd0233373a27a
Sierra Tucker <sierra.tucker121@gmail.com>
2022-01-09 12:58:42 -0700

    Test: top_five class method and new set of data

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -141,3 +141,50 @@ RSpec.describe Merchant do
       end
   end
 end
+
+RSpec.describe Merchant do
+  let!(:merchant_1) {Merchant.create!(name: 'Ron Swanson', status: 0)}
+  let!(:merchant_2) {Merchant.create!(name: 'Leslie Knope', status: 0)}
+  let!(:merchant_3) {Merchant.create!(name: 'Oprah Winfrey', status: 0)}
+  let!(:merchant_4) {Merchant.create!(name: 'Leonardo Dicaprio')}
+  let!(:merchant_5) {Merchant.create!(name: 'Steve Carell')}
+  let!(:merchant_6) {Merchant.create!(name: 'Jenna Fischer')}
+
+  let!(:item_1) {merchant_1.items.create!(name: "Necklace", description: "A thing around your neck", unit_price: 1000, status: 0)}
+  let!(:item_2) {merchant_2.items.create!(name: "Bracelet", description: "A thing around your wrist", unit_price: 900, status: 0)}
+  let!(:item_3) {merchant_3.items.create!(name: "Earrings", description: "These go through your ears", unit_price: 1500, status: 1)}
+  let!(:item_4) {merchant_4.items.create!(name: "Ring", description: "A thing around your finger", unit_price: 1000)}
+  let!(:item_5) {merchant_5.items.create!(name: "Toe Ring", description: "A thing around your neck", unit_price: 800)}
+  let!(:item_6) {merchant_6.items.create!(name: "Pendant", description: "A thing to put somewhere", unit_price: 1500)}
+
+  let!(:customer_1) {Customer.create!(first_name: "Billy", last_name: "Joel")}
+
+  let!(:invoice_1) {customer_1.invoices.create!(status: 1, created_at: '2012-03-25 09:54:09')}
+  let!(:invoice_2) {customer_1.invoices.create!(status: 1, created_at: '2012-04-25 08:54:09')}
+  let!(:invoice_3) {customer_1.invoices.create!(status: 1, created_at: '2012-10-25 04:54:09')}
+  let!(:invoice_4) {customer_1.invoices.create!(status: 1, created_at: '2012-03-26 01:54:09')}
+  let!(:invoice_5) {customer_1.invoices.create!(status: 1, created_at: '2012-03-28 12:54:09')}
+  let!(:invoice_6) {customer_1.invoices.create!(status: 1, created_at: '2012-03-29 07:54:09')}
+
+  let!(:invoice_item_1)  {InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, unit_price: 1000, quantity: 1, status: 0)}
+  let!(:invoice_item_2)  {InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_2.id, unit_price: 1000, quantity: 13, status: 0)}
+  let!(:invoice_item_3)  {InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_3.id, unit_price: 1000, quantity: 2, status: 0)}
+  let!(:invoice_item_4)  {InvoiceItem.create!(item_id: item_4.id, invoice_id: invoice_4.id, unit_price: 1000, quantity: 4, status: 1)}
+  let!(:invoice_item_5)  {InvoiceItem.create!(item_id: item_5.id, invoice_id: invoice_5.id, unit_price: 1000, quantity: 20, status: 1)}
+  let!(:invoice_item_6)  {InvoiceItem.create!(item_id: item_6.id, invoice_id: invoice_6.id, unit_price: 1000, quantity: 7, status: 2)}
+
+  let!(:transaction_1) {invoice_1.transactions.create!(result: 'success')}
+  let!(:transaction_2) {invoice_2.transactions.create!(result: 'success')}
+  let!(:transaction_3) {invoice_3.transactions.create!(result: 'success')}
+  let!(:transaction_4) {invoice_4.transactions.create!(result: 'success')}
+  let!(:transaction_5) {invoice_5.transactions.create!(result: 'success')}
+  let!(:transaction_6) {invoice_6.transactions.create!(result: 'success')}
+
+  describe 'class methods' do
+    describe '::top_five' do
+      it 'lists the top five merchants ordered by total revenue' do
+        expect(Merchant.top_five).to eq([merchant_5, merchant_2, merchant_6, merchant_4, merchant_3])
+      end
+    end
+  end
+end
