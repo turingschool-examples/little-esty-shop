8ffa24db6188ef06babd93ca43f17805f79fe9e1
Brad Breiten <jonathan@breiten.us>
2022-01-08 18:15:08 -0700

    Feat: invoice index page items status can be seen and updated

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -6,8 +6,16 @@ class InvoicesController < ApplicationController
   end
 
   def show
-    @invoice = Invoice.find(params[:id])
+    @invoice        = Invoice.find(params[:id])
     @merchant_items = @invoice.items_by_merchant(params[:merchant_id])
-    @total_revenue = @invoice.total_revenue_by_merchant(params[:merchant_id])
+    @total_revenue  = @invoice.total_revenue_by_merchant(params[:merchant_id])
+  end
+
+  def update
+    @item = Item.find(params[:item_id])
+
+    @item.update(status: params[:status].to_i)
+
+    redirect_to merchant_invoice_path(params[:merchant_id], params[:id])
   end
 end
