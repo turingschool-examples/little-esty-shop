0d502cd62c8bd199229a60f7c14bfce68e0ade57
Kelly Anderson <kean9528@colorado.edu>
2022-01-09 11:44:52 -0700

    finish feature test for story 14

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -16,5 +16,19 @@ RSpec.describe 'Admin Merchant Index' do
       click_link "#{@merchant_1.name}"
       expect(current_path).to eq("/admin/merchants/#{@merchant_1.id}")
     end
+
+    it 'next to each merchant name I see a button to disable or enable that merchant.' do
+      visit "/admin/merchants"
+      within("#merchant-#{@merchant_1.id}") do
+        click_button("Enable")
+
+        expect(current_path).to eq('/admin/merchants')
+        expect(page).to have_content(@merchant_1.status)
+        click_button("Disable")
+
+        expect(current_path).to eq('admin/merchants')
+        expect(page).to have_content(@merchant_1.status)
+      end
+    end
   end
 end
