069b353af305ee1fc211152d80e58e498060b64b
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-10 15:21:42 -0700

    had to commit complete work

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -2,4 +2,10 @@ class MerchantInvoicesController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    @invoice = Invoice.find(params[:invoice_id])
+    binding.pry
+    @customer = Customer.find(@invoice.customer_id)
+  end
 end
