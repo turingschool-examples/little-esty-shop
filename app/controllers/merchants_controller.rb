cd4fd8b6cb259f508fe1d02077a77fe515671bff
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-10 10:00:10 -0500

    create update method for admin merchants

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -7,4 +7,17 @@ class MerchantsController < ApplicationController
   def index
     redirect_to "/admin/merchants"
   end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    merchant.update(merchant_params)
+    redirect_to "/admin/merchants"
+  end
+
+
+  private
+
+  def merchant_params
+    params.permit(:name, :status)
+  end
 end
