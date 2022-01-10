ccb98f209b869b555803f8eb8cf3e18f0a80214c
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-10 09:00:22 -0500

    create test

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -8,6 +8,21 @@ RSpec.describe 'Admin_Merchants Index Page' do
     expect(page).to have_content("Willy's Bakery")
   end
 
+  it "groups merchants by either diabled or enabled" do
+    merchant1 = create(:merchant, name: "Matthew", status: 1)
+    merchant2 = create(:merchant, name: "Mark", status: 1)
+    merchant3 = create(:merchant, name: "Luke")
+    merchant4 = create(:merchant, name: "John")
+    visit "/admin/merchants"
+
+    expect("Enabled Merchants:").to appear_before("Disabled Merchants:")
+    expect("Enabled Merchants:").to appear_before(merchant1)
+    expect(merchant1).to appear_before("Disabled Merchants:")
+    expect(merchant2).to appear_before("Disabled Merchants:")
+    expect("Disabled Merchants").to appear_before(merchant3)
+    expect("Disabled Merchants").to appear_before(merchant4)
+  end
+
   it "has a link to create a new Admin_Merchant" do
     visit "/admin/merchants"
 
