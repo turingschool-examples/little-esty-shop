346f2f36c3d89ae63a213c8902406fc69fd0b4ac
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-10 08:42:05 -0500

    Add status to default to disabled

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2022_01_10_131231) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status"
+    t.integer "status", default: 0
   end
 
   create_table "transactions", force: :cascade do |t|
