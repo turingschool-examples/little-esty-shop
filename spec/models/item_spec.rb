9f2971c85d7f710328ed631a915a8558d3c85311
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-09 23:16:29 -0500

    Add item.transaction associateion and validation.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Item, type: :model do
     it { should belong_to(:merchant)}
     it { should have_many(:invoice_items)}
     it { should have_many(:invoices).through(:invoice_items)}
+    it { should have_many(:transactions).through(:invoices)}
   end
 
   describe 'validations' do
