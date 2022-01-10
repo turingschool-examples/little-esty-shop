36286bc7e6ff168fcb01b80ab0e27cc8bf1e75fc
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-10 09:40:54 -0500

    write test for disable

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -8,6 +8,32 @@ RSpec.describe 'Admin_Merchants Index Page' do
     expect(page).to have_content("Willy's Bakery")
   end
 
+  it "groups merchants by either diabled or enabled" do
+    merchant1 = create(:merchant, name: "Matthew", status: 1)
+    merchant2 = create(:merchant, name: "Mark", status: 1)
+    merchant3 = create(:merchant, name: "Luke")
+    merchant4 = create(:merchant, name: "John")
+    visit "/admin/merchants"
+
+    expect("Enabled Merchants:").to appear_before("Disabled Merchants:")
+    expect("Enabled Merchants:").to appear_before(merchant1.name)
+    expect(merchant1.name).to appear_before("Disabled Merchants:")
+    expect(merchant2.name).to appear_before("Disabled Merchants:")
+    expect("Disabled Merchants").to appear_before(merchant3.name)
+    expect("Disabled Merchants").to appear_before(merchant4.name)
+  end
+
+  it "has a button to disable next to each enabled merchant, button refreshes page and changes merchant status" do
+    merchant2 = create(:merchant, name: "Mark", status: 1)
+    visit "/admin/merchants"
+
+    click_button "Enable #{merchant2.name}"
+
+    expect(current_path).to eq("/admin/merchants")
+    merchant2.reload
+    expect(merchant2.status).to eq("Disabled")
+  end
+
   it "has a link to create a new Admin_Merchant" do
     visit "/admin/merchants"
 
