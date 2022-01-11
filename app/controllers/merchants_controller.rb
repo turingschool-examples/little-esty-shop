324e483e02765d872687a2ac8363834327d234a9
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-11 12:14:51 -0500

    remove update method from merchants_controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,18 +3,4 @@ class MerchantsController < ApplicationController
   def dashboard
     @merchant = Merchant.find(params[:id])
   end
-
-  def update
-    merchant = Merchant.find(params[:id])
-    merchant.update(merchant_params)
-    require "pry"; binding.pry
-    redirect_to "/admin/merchants"
-  end
-
-
-  private
-
-  def merchant_params
-    params.permit(:name, :status)
-  end
 end
