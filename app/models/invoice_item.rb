7629ecb6d7be7e3ce200bf268f52b60381f1cce7
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-10 23:48:02 -0500

    Add transaction association to invoice_item model.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,7 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  has_many :transactions, through: :invoice
 
   validates :quantity, presence: true, numericality: {only_integer: true}
   validates :unit_price, presence: true, numericality: {only_integer: true}
