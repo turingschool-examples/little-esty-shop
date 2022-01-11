d952e0a1980f1e010be8f743755f9b9137951535
Dylan Harper <dylanharper4@icloud.com>
2022-01-10 16:55:11 -0800

    push

--- app/services/github_info.rb
+++ app/services/github_info.rb
@@ -15,20 +15,20 @@ user_names = user_name_parsed.map do |user|
   user[:login]
 end
 
-commits_response = HTTParty.get("https://api.github.com/repos/croixk/little-esty-shop/commits")
+commits_response = HTTParty.get("https://api.github.com/repos/croixk/little-esty-shop/commits?page=1&per_page=1000")
 commits = JSON.parse(commits_response.body, symbolize_names: true)
 
-binding.pry
 commit_hash = Hash.new(0)
 
-  # user_names.each do |user|
-  #   commits.each do |commit|
-  #     if user == commit[:author][:login]
-  #       commit_hash[user] += 1
-  #     end
-  #   end
-  #   commit_hash
-  # end
+
+  user_names.each do |user|
+    commits.each do |commit|
+      if user == commit[:author][:login]
+        commit_hash[user] += 1
+      end
+    end
+    commit_hash
+  end
 
 
 #repo name
@@ -41,12 +41,16 @@ repo_name = parsed[:name]
 
 class GithubInfo
 
-  attr_reader :repo_name, :contributor
+  attr_reader :repo_name, :contributor, :commits_per_person
 
   def initialize(parsed, user_names)
     @repo_name = parsed[:name]
     @contributors = user_names
+    @commits_per_person = commit_hash
+    binding.pry
   end
+
+
 
 
 end
