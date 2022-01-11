bd05a8eaf4a306cf304f3074bf0ae799dfbfbf32
Chris Hewitt <agop5134@gmail.com>
2022-01-11 13:40:33 -0700

    API work

--- app/services/github_info.rb
+++ app/services/github_info.rb
@@ -3,24 +3,23 @@ require 'pry'
 require 'json'
 
 
-response = HTTParty.get("https://api.github.com/repos/croixk/little-esty-shop")
-
-
-parsed = JSON.parse(response.body, symbolize_names: true)
+repo_response = HTTParty.get("https://api.github.com/repos/croixk/little-esty-shop")
+repo_name = JSON.parse(repo_response.body, symbolize_names: true)
 
 user_name_response = HTTParty.get("https://api.github.com/repos/croixk/little-esty-shop/contributors")
-user_name_parsed = JSON.parse(user_name_response.body, symbolize_names: true)
+user_names = JSON.parse(user_name_response.body, symbolize_names: true).map {|user| user[:login]}
 
-user_names = user_name_parsed.map do |user|
-  user[:login]
-end
-
-commits_response = HTTParty.get("https://api.github.com/repos/croixk/little-esty-shop/commits?page=1&per_page=1000")
+commits_response = HTTParty.get("https://api.github.com/repos/croixk/little-esty-shop/commits?page=1&per_page=100")
 commits = JSON.parse(commits_response.body, symbolize_names: true)
 
+pr_response = HTTParty.get("https://api.github.com/repos/croixk/little-esty-shop/pulls?state=closed&page=1&per_page=100")
+prs = JSON.parse(pr_response.body, symbolize_names: true)
+#does closed count as merged?
+#what about testing?
+#we will need to combine the commits response hash from pages 1 and 2 unless we can find away around limiting.
+#mocking api request stuff
+#webmock,VCR
 commit_hash = Hash.new(0)
-
-
   user_names.each do |user|
     commits.each do |commit|
       if user == commit[:author][:login]
@@ -30,27 +29,14 @@ commit_hash = Hash.new(0)
     commit_hash
   end
 
-
-#repo name
-#user names
-#commits
-#pull requests
-
-repo_name = parsed[:name]
-
-
 class GithubInfo
+  attr_reader :repo_name, :contributor, :commits_per_person, :all_merged
 
-  attr_reader :repo_name, :contributor, :commits_per_person
-
-  def initialize(parsed, user_names)
-    @repo_name = parsed[:name]
+  def initialize(parsed, user_names, commit_hash)
+    @repo_name = repo_name[:name]
     @contributors = user_names
     @commits_per_person = commit_hash
-    binding.pry
+    @all_merged = prs.count
   end
-
-
-
-
 end
+
