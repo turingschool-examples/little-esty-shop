4f30beda469520be3256410d83ce7ac6ba94eb39
Brad (he/him) <88853324+jbreit88@users.noreply.github.com>
2022-01-11 13:07:12 -0500

    Remove commented out user story

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -60,14 +60,4 @@ RSpec.describe 'Admin Invoice Show page' do
     expect(current_path).to eq(admin_invoice_path(invoice_1.id))
     expect(page).to have_field('Status', with: 'completed')
   end
-#   As an admin
-# When I visit an admin invoice show page
-# I see the invoice status is a select field
-# And I see that the invoice's current status is selected
-# When I click this select field,
-# Then I can select a new status for the Invoice,
-# And next to the select field I see a button to "Update Invoice Status"
-# When I click this button
-# I am taken back to the admin invoice show page
-# And I see that my Invoice's status has now been updated
 end
