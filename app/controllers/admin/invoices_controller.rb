e13d248a3b526b42c033d5b00c9f23376c1e5755
Brad Breiten <jonathan@breiten.us>
2022-01-10 21:54:22 -0700

    Feature: Admin invoice show page displays invoice status as select menu and can be updated through the same

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -9,4 +9,18 @@ class Admin::InvoicesController < ApplicationController
     @customer = @invoice.customer
     @items = @invoice.items
   end
+
+  def update
+    @invoice = Invoice.find(params[:id])
+
+    if params[:status]['cancelled']
+      @invoice.update(status: 0)
+    elsif params[:status]['completed']
+      @invoice.update(status: 1)
+    elsif params[:status]['in progress']
+      @invoice.update(status: 2)
+    end
+
+    redirect_to admin_invoice_path(params[:id])
+  end
 end
