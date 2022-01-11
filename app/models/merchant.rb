62d818fd95d48f12e1a6c2d1ece1e05c6a24d464
Russell Rockwood <russell.rockwood@gmail.com>
2022-01-11 15:17:58 -0500

    add merchant favorite customers

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,8 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
 
   enum status: {enable: 0, disable: 1}
 
@@ -23,6 +25,15 @@ class Merchant < ApplicationRecord
 
   def ready_to_ship
     invoice_items.joins(:invoice).where(status: [0, 1]).order('invoices.created_at')
+  end
+
+  def top_five_customers
+    customers.joins(invoices: :transactions)
+              .where(transactions: { result: 0 })
+              .select('customers.*, count(transactions.*) as total_transactions')
+              .order(total_transactions: :desc)
+              .group('customers.id')
+              .limit(5)
   end
 
   def self.top_five_merchants
