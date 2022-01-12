9a9fad5eeaaea03aaf166c173abfb0c06fc637ca
Chris Hewitt <agop5134@gmail.com>
2022-01-12 13:37:35 -0700

    Complete spec coverage with API in layouts/refactor GHservice

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe GithubService do
   it 'gets commits per person', :vcr, js: true do
     github_service = GithubService.new
     VCR.use_cassette("gets_commits_per_person") do
-    expect(github_service.get_commits_per_person).to eq({"Henchworm"=>35, "croixk"=>19, "dylan-harper"=>42, "jacksonvaldez"=>4})
+    expect(github_service.get_commits_per_person).to eq({"BrianZanti"=>51, "Henchworm"=>42, "croixk"=>22, "dylan-harper"=>49, "jacksonvaldez"=>10, "jamisonordway"=>1, "scottalexandra"=>3, "timomitchel"=>9})
     end
   end
 
@@ -28,7 +28,7 @@ RSpec.describe GithubService do
     github_service = GithubService.new
     VCR.use_cassette("gets_all_merged_commits") do
     expect(github_service.get_all_merged).to be_a(Integer)
-    expect(33).to be >= (github_service.get_all_merged)
+    expect(80).to be <= (github_service.get_all_merged)
     end
   end
 end
