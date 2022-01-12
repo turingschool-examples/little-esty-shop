bf38e7ce207d0f18d897b3c7271c89007a14d928
Dylan Harper <dylanharper4@icloud.com>
2022-01-12 14:52:42 -0800

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/merchants/:id/dashboard', to: 'merchants#show'
---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,9 @@
+# require 'rails_helper'
+#
+# RSpec.describe "welcome/index.html.erb", type: :view do
+#
+#   it 'visits welcome page'
+#     visit "welcome/index"
+#     expect(current_path).to eq("welcome/index")
+#   end
+#
