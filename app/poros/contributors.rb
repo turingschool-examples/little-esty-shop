c398d2bcc33e3afb5bd74c5bf43f93aa9f5e2d6d
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-12 17:41:08 -0700

    Update poros with message incase rate limit exceeded

--- app/poros/contributors.rb
+++ app/poros/contributors.rb
@@ -2,7 +2,12 @@ class Contributors
   attr_reader :commits, :username
 
   def initialize(data)
-    @commits = data[:contributions]
-    @username = data[:login]
-  end
+     if data.class == Array
+        @commits = data[1]
+        @username = data[1]
+     else
+        @commits = data[:contributions]
+        @username = data[:login]
+     end
+   end
 end
--- app/poros/repo.rb
+++ app/poros/repo.rb
@@ -2,6 +2,10 @@ class Repo
   attr_reader :name
 
   def initialize(data)
-    @name = data[:name]
+    if data.class == Array
+       @name = data[1]
+    else
+       @name = data[:name]
+    end
   end
 end
