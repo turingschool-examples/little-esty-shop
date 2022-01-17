4edfb8ab2c000fefa4c4271e75099b3895ddc160
Croix Kimmel <croixkimmel@gmail.com>
2022-01-17 14:03:52 -0700

    Add functional total revenue and discounted revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -16,4 +16,49 @@ class Invoice < ApplicationRecord
     invoice_items
     .sum('quantity*unit_price')
   end
+
+
+  def discounted_total_revenue
+
+    merchant = Merchant.first
+
+    discounted_total = 0
+
+    discounts = Discount.order(:min_quantity)
+
+    items = InvoiceItem
+      .joins(item: :merchant)
+
+
+    items.each do |item|
+      item_total = item.quantity * item.unit_price
+      discounted_total = item_total + discounted_total
+
+      discounts.each do |discount|
+        if item.quantity >= discount.min_quantity
+          discounted_total = discounted_total - item_total
+          item_total = item.quantity * item.unit_price * (100 - discount.percent_off) / 100
+          discounted_total = discounted_total + item_total
+        end
+      end
+    end
+
+    return discounted_total
+
+    # discount_items_total = InvoiceItem
+    #   .joins(item: {merchant: :discounts})
+    #   .distinct
+    #   .where('quantity >= min_quantity')
+    #   .sum('invoice_items.unit_price * invoice_items.quantity * (100 - percent_off) / 100')
+    #
+    #
+    #
+    # full_price_items_total = InvoiceItem
+    #     .joins(item: {merchant: :discounts})
+    #     .distinct
+    #     .where('invoice_items.quantity < discounts.min_quantity')
+    #     .sum('invoice_items.unit_price * invoice_items.quantity')
+    
+  end
+
 end
--- spec/features/merchant_invoices/show_spec.rb
+++ spec/features/merchant_invoices/show_spec.rb
@@ -6,17 +6,24 @@ RSpec.describe 'the merchant invoice show page' do
 
   let!(:merchant_1) {Merchant.create!(name: 'Billys Pet Rocks')}
 
-  let!(:item_1) {merchant_1.items.create!(name: 'Obsidian Nobice', description: 'A beautiful obsidian', unit_price: 50)}
-  let!(:item_2) {merchant_1.items.create!(name: 'Pleasure Geode', description: 'Glamourous Geode', unit_price: 100)}
-  let!(:item_3) {merchant_1.items.create!(name: 'Brown Pebble', description: 'GClassic rock', unit_price: 50)}
+  let!(:item_1) {merchant_1.items.create!(name: 'Obsidian Nobice', description: 'A beautiful obsidian', unit_price: 1)}
+  let!(:item_2) {merchant_1.items.create!(name: 'Pleasure Geode', description: 'Glamourous Geode', unit_price: 1)}
+  let!(:item_3) {merchant_1.items.create!(name: 'Brown Pebble', description: 'Classic rock', unit_price: 1)}
+  # let!(:item_4) {merchant_1.items.create!(name: 'Green Pebble', description: 'Shiny rock', unit_price: 1)}
 
   let!(:customer_1) {Customer.create!(first_name: 'Billy', last_name: 'Carruthers')}
 
   let!(:invoice_1) {customer_1.invoices.create!(status: 'completed' )}
 
-  let!(:invoice_item_1) {InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 2, unit_price: 50, status: 'shipped')}
-  let!(:invoice_item_2) {InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_2.id, quantity: 1, unit_price: 50, status: 'pending')}
-  let!(:invoice_item_3) {InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_3.id, quantity: 3, unit_price: 50, status: 'pending', created_at: Time.new(2021))}
+  let!(:invoice_item_1) {InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 10, unit_price: 1, status: 'shipped', created_at: Time.new(2021))}
+  let!(:invoice_item_2) {InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_2.id, quantity: 20, unit_price: 1, status: 'pending', created_at: Time.new(2021))}
+  let!(:invoice_item_3) {InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_3.id, quantity: 30, unit_price: 1, status: 'pending', created_at: Time.new(2021))}
+  # let!(:invoice_item_4) {InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_4.id, quantity: 30, unit_price: 1, status: 'pending', created_at: Time.new(2021))}
+
+  let!(:discount_1) {Discount.create!(percent_off: 10, min_quantity: 10, merchant_id: merchant_1.id)}
+  let!(:discount_2) {Discount.create!(percent_off: 20, min_quantity: 20, merchant_id: merchant_1.id)}
+  let!(:discount_3) {Discount.create!(percent_off: 30, min_quantity: 30, merchant_id: merchant_1.id)}
+
 
   it 'displays invoice attributes' do
     visit "/merchants/#{merchant_1.id}/invoices/#{invoice_1.id}"
@@ -66,7 +73,14 @@ RSpec.describe 'the merchant invoice show page' do
   it 'displays total revenue for merchants invoices' do
     visit "/merchants/#{merchant_1.id}/invoices/#{invoice_1.id}"
 
-    expect(page).to have_content('Total Revenue: 300')
+    expect(page).to have_content('Total Revenue: 60')
+  end
+
+  it 'displays discounted total revenue for merchants invoices' do
+    visit "/merchants/#{merchant_1.id}/invoices/#{invoice_1.id}"
+
+    expect(page).to have_content('Total Discounted Revenue: 46')
+    save_and_open_page
   end
 
 end
