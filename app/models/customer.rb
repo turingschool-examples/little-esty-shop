834f958ffbd593677090220e4a04b7c18ac12187
Sully Birashk <sully.birashk@gmail.com>
2022-02-17 14:18:09 -0800

    Model Setup and Model Testing

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
   has_many :invoices
-  has_many :transactions, through: :invoices
-  
+  has_many :transactions,through: :invoices
+
 end
---
+++ db/migrate/20220217220028_create_invoices.rb
@@ -0,0 +1,10 @@
+class CreateInvoices < ActiveRecord::Migration[5.2]
+  def change
+    create_table :invoices do |t|
+      t.references :customer, foreign_key: true
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
