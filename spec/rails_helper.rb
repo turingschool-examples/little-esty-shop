6e8a4c14b9d6fae4cf01065acde34d8a83a5a096
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-17 13:42:03 -0700

    Initial commit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'simplecov'
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
@@ -38,6 +41,14 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end 
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
