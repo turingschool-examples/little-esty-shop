1dce19016b3a1ba9e63e98d4fa29c9e30a34f863
Richard LaBrecque <richardlabreque@gmail.com>
2022-02-17 20:20:57 -0500

    feat: merchant dashboard links us39

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe 'the merchant dashboard' do
     expect(current_path).to eq("/merchant/#{merchant.id}/dashboard")
     expect(page).to have_content(merchant.name)
   end
+  # As a merchant,
+  # When I visit my merchant dashboard
+  # Then I see link to my merchant items index (/merchant/merchant_id/items)
+  # And I see a link to my merchant invoices index (/merchant/merchant_id/invoices)
+  it 'has links to the merchant items index' do
+    merchant = Merchant.create!(name: "Steve")
+    visit "/merchant/#{merchant.id}/dashboard"
+
+    expect(page).to have_content("Item Index")
+    expect(page).to have_content("Invoice Index")
+  end
 end
