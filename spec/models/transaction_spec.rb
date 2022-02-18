e9b33692dbaadb38636977eb3481dd950f8454c1
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-17 22:39:03 -0700

    Remove validation for credit card expiration (no info in CSV to validate)

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   describe 'attributes' do
     it { should validate_presence_of :credit_card_number }
-    it { should validate_presence_of :credit_card_expiration }
     it { should validate_presence_of :result }
   end
 
