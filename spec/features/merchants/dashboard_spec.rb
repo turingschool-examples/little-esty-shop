941c6bb2d6817f9e15ea9df28af290647fdbaae3
stevenjames-turing <stevenjamesstl@gmail.com>
2022-02-18 11:49:09 -0600

    User story 2 complete

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe 'merchant dashboard' do
         merchant = Merchant.create!(name: "Schroeder-Jerde")
 
         visit "/merchants/#{merchant.id}/dashboard"
-
+        
         expect(page).to have_content(merchant.name)
+    end
+    
+    it 'has links to items index and invoices index' do 
+        merchant = Merchant.create!(name: "Schroeder-Jerde")
+    
+        visit "/merchants/#{merchant.id}/dashboard"
+
+        expect(page).to have_link("My Items")
+        expect(page).to have_link("My Invoices")
+        
     end
 end
