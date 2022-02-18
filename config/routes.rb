e3e72319feed0c450db7ad10e48c79a7e303f0d3
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-18 17:00:47 -0500

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/merchant/:id/dashboard', to: 'merchants#show'
   get '/merchant/:id/items', to: 'merchant_items#index'
   get '/merchant/:id/invoices', to: 'merchant_invoices#index'
+
+  get '/merchants/:id/items/:id', to: 'merchant_items#show'
 end
