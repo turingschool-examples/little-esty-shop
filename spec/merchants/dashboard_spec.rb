2fedd97ef4ef0e85adea4a9f796ef6a599add7a9
Michael Hicks <hicks.michael04@gmail.com>
2022-02-19 00:23:14 -0700

    test: Add link to merchant invoice index page

--- spec/merchants/dashboard_spec.rb
+++ spec/merchants/dashboard_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
  RSpec.describe 'Merchant Dashboard' do
-   it "when visiting merchant dashboard I see name of merchant" do
-     merchant_1 = Merchant.create!(name: 'Circuit City Jr')
+  describe 'user story #40' do
+    it "when visiting merchant dashboard I see name of merchant" do
+      merchant_1 = Merchant.create!(name: 'Circuit City Jr')
 
-     visit "/merchants/#{merchant_1.id}/dashboard"
-     save_and_open_page
+      visit "/merchants/#{merchant_1.id}/dashboard"
 
-     expect(page).to have_content('Circuit City Jr')
-   end
+      expect(page).to have_content('Circuit City Jr')
+    end
+  end
+
+  describe 'user story #39' do
+    it "when visiting merchant dashboard I see a link to merchant invoices index" do
+      merchant_1 = Merchant.create!(name: 'Circuit City Jr')
+
+      visit "/merchants/#{merchant_1.id}/dashboard"
+      click_link 'Invoices'
+
+      expect(page).to have_content('Circuit City Jr')
+    end
+  end
+
+
  end
