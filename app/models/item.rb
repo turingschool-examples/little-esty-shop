5fc1508c4b61ca72c5b8d1dd10fa058812d5e22c
Tamara Dowis <tamara.dowis@gmail.com>
2022-02-19 16:57:38 -0600

    feat: add status attribute to items table for enable/ disable feature

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,11 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+  attribute :status, :integer, default: 2
+
   def display_price
     cents = self.unit_price
     '%.2f' % (cents /100.0)
   end
+
 end
