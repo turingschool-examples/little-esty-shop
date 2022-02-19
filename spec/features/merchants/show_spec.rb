5d2f0078fd45b150dcd7c0f10d53bd11e03f2362
Richard LaBrecque <richardlabreque@gmail.com>
2022-02-18 20:04:57 -0500

    feat: build out seeds file to use in testing

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -3,21 +3,29 @@ require 'rails_helper'
 # When I visit my merchant dashboard (/merchant/merchant_id/dashboard)
 # Then I see the name of my merchant
 RSpec.describe 'the merchant dashboard' do
-  it 'has a dashboard page' do
-    merchant = Merchant.create!(name: "Steve")
-    visit "/merchant/#{merchant.id}/dashboard"
-    expect(current_path).to eq("/merchant/#{merchant.id}/dashboard")
-    expect(page).to have_content(merchant.name)
-  end
-  # As a merchant,
-  # When I visit my merchant dashboard
-  # Then I see link to my merchant items index (/merchant/merchant_id/items)
-  # And I see a link to my merchant invoices index (/merchant/merchant_id/invoices)
-  it 'has links to the merchant items index' do
-    merchant = Merchant.create!(name: "Steve")
-    visit "/merchant/#{merchant.id}/dashboard"
+  describe 'existance and links' do
+    it 'has a dashboard page' do
+      merchant = Merchant.create!(name: "Steve")
+      visit "/merchant/#{merchant.id}/dashboard"
+      expect(current_path).to eq("/merchant/#{merchant.id}/dashboard")
+      expect(page).to have_content(merchant.name)
+    end
+    # As a merchant,
+    # When I visit my merchant dashboard
+    # Then I see link to my merchant items index (/merchant/merchant_id/items)
+    # And I see a link to my merchant invoices index (/merchant/merchant_id/invoices)
+    it 'has links to the merchant items index' do
+      merchant = Merchant.create!(name: "Steve")
+      visit "/merchant/#{merchant.id}/dashboard"
 
-    expect(page).to have_link("Item Index")
-    expect(page).to have_link("Invoice Index")
+      expect(page).to have_link("Item Index")
+      expect(page).to have_link("Invoice Index")
+    end
+  end
+
+  describe 'top customers section' do
+    it 'is able to list top 5 customers for this merchant' do
+      merchant = Merchant.create!(name: "Steve")
+    end
   end
 end
