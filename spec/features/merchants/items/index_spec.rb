949891e1b2c39fde81701f64f106b78ed810aa29
Becky Nisttahuz <benistta@hotmail.com>
2022-02-19 10:46:44 -0700

    Test: Merchant Items Index Page

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -8,13 +8,14 @@ require 'rails_helper'
 RSpec.describe 'Merchants Index' do
   it 'can see a list of the names of all of the items' do
     merchant_1 = Merchant.create!(name: "Ana Maria")
-    marchant_2 = Merchant.create!(name: "Juan Lopez")
+    merchant_2 = Merchant.create!(name: "Juan Lopez")
     item_1 = merchant_1.items.create!(name: "cheese", description: "european cheese", unit_price: 24)
-    item_2 = merchant_1.items.create!(name: "onion", description: "red onion", unit_price: 34)
+    item_2 = merchant_2.items.create!(name: "onion", description: "red onion", unit_price: 34)
 
-    visit "/merchants/#{merchant.id}/items"
+    visit "/merchants/#{merchant_1.id}/items"
 
     expect(page).to have_content(item_1.name)
+    expect(page).to_not have_content(item_2.name)
 
   end
 end
