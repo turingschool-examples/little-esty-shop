b6cbaa99aefdca041c5806c905d357f4a9795097
Richard LaBrecque <richardlabreque@gmail.com>
2022-02-18 20:50:20 -0500

    feat: customer can count successful transactions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,16 @@ class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions,through: :invoices
 
+  def name
+    first_name + " " + last_name
+  end
+
+  def transaction_count
+    transactions.count
+  end
+
+  def successful_transactions_count
+    transactions.where(result: 1).count
+  end
+  
 end
