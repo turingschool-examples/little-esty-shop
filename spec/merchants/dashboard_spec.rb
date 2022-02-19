03191571a90030ce52c8473fbc0f41a5e3baa6b5
Michael Hicks <hicks.michael04@gmail.com>
2022-02-19 11:19:11 -0700

    test: create test for user story #38

--- spec/merchants/dashboard_spec.rb
+++ spec/merchants/dashboard_spec.rb
@@ -23,4 +23,25 @@ require 'rails_helper'
   end
 
 
+describe 'user story #38' do
+  it "when visiting merchant dashboard I see a link to merchant invoices index" do
+    merchant_1 = Merchant.create!(name: 'Circuit City Jr')
+
+    # As a merchant,
+    # When I visit my merchant dashboard
+    visit "/merchants/#{merchant_1.id}/dashboard"
+    # Then I see the names of the top 5 customers
+    # who have conducted the largest number of successful transactions with my merchant
+    # And next to each customer name I see the number of successful transactions they have
+    # conducted with my merchant
+
+
+    click_link 'Invoices'
+
+    expect(page).to have_content('Circuit City Jr')
+  end
+end
+
+
+
  end
