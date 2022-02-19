c00dc94370a02af8c5e02e7953fa4230e21aec74
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-19 13:49:35 -0700

    feature: add descriptive text to merchant invoices index, as well as convert invoice_id to link to show page. Define show index in controller. Create empty show file. Test: write test displays invoices as links. Pass tests

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -4,6 +4,10 @@ class Merchant::InvoicesController < ApplicationController
     @merchant_invoices = find_merchant.merchant_invoices
   end 
 
+  def show
+    
+  end
+
 private 
   def find_merchant
     @merchant = Merchant.find(params[:merchant_id])
--- spec/merchant/invoices/index_spec.rb
+++ spec/merchant/invoices/index_spec.rb
@@ -7,13 +7,15 @@ RSpec.describe 'The Merchant Invoices Index' do
     @invoice1 =Invoice.create!(status: 0, customer_id: @customer1.id)
     @item1 = Item.create!(name: 'food', description: 'delicious', unit_price: '2000', merchant_id: @merchant.id)
     @invoice_item1 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 2, unit_price: 100, status: 1)
-    # visit merchant_invoices_path(@merchant.id)
   end 
   it 'displays all invoices that include a merchants items' do 
-    # visit isn't working 
-    # visit merchant_invoices_path(@merchant.id)
-    # visit "/merchants/#{@merchant.id}/invoices"
-    save_and_open_page
+    visit merchant_invoices_path(@merchant.id)
     expect(page).to have_content("Merchant Invoices Index")
+    expect(page).to have_content("Invoice Id: #{@invoice1.id}")
+  end 
+  it 'displays invoices as clickable links that bring the user to the merchant invoice show page' do 
+    visit merchant_invoices_path(@merchant.id)
+    click_link("#{@invoice1.id}")
+    expect(current_path).to eq(merchant_invoice_path(@merchant.id, @invoice1.id))
   end 
 end 
