77e0c6cf3f21e3802fc0c635744791278178bce2
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-19 16:24:20 -0700

    add another expect line to test to make test more robust

--- spec/features/merchant/invoices/show_spec.rb
+++ spec/features/merchant/invoices/show_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe 'The Merchant Invoice Show Page' do
     @item2 = Item.create!(name: 'literal goop', description: 'delicious', unit_price: '2000', merchant_id: @merchant2.id)
     @invoice_item1 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 2, unit_price: 100, status: 1)
     @invoice_item2 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 2, unit_price: 100, status: 1)
+    @invoice_item3 = InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice2.id, quantity: 2, unit_price: 500, status: 1)
   end
 
   it 'displays all information related to that invoice' do 
@@ -26,5 +27,6 @@ RSpec.describe 'The Merchant Invoice Show Page' do
   it 'displays the total revenue that will be generated from all items on the invoice' do 
     visit merchant_invoice_path(@merchant.id, @invoice1.id)
     expect(page).to have_content(@invoice1.invoice_revenue)
+    expect(page).to have_no_content(@invoice2.invoice_revenue)
   end 
 end
