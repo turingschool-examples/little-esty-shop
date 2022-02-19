3cfea0de982a94ed843f7ca3389f2f3148591415
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-19 16:22:06 -0500

    write show method

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -1,6 +1,9 @@
 class MerchantItemsController < ApplicationController
 
   def show
-    
+    # require "pry"; binding.pry
+    @merchant = Merchant.find(params[:id])
+    @item = @merchant.items.find(params[:id])
+
   end
 end
