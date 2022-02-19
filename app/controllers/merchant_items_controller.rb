4c711c38f6579a5bc1cde2b38851fc75f7bf5f6e
Becky Nisttahuz <benistta@hotmail.com>
2022-02-19 14:40:16 -0700

    Feat: Add method show to merchant items controller

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -4,7 +4,26 @@ class MerchantItemsController < ApplicationController
   end
 
   def show
-    merchant = Merchant.find(params[:merchant_id])
-    @item = merchant.items.find(params[:item_id])
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items.find(params[:item_id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items.find(params[:item_id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items.find(params[:item_id])
+      @item.update(item_params)
+        redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}"
+
+        flash.alert = "#{@item.name} has been updated"
+    end
+
+  private
+    def item_params
+      params.permit(:name, :description, :unit_price)
+    end
 end
