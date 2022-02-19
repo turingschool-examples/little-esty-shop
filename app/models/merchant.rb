1a46df66fd1177883098ca10a4ad0752c04b4a5c
Austin Moore <austinchristianmoore@gmail.com>
2022-02-19 13:51:15 -0500

    Fix: Prepare to split branches

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,6 @@
 class Merchant < ApplicationRecord
   has_many :items
-
-  def customers
-    
-  end
+  # has_many :invoice_items, through: :items
+  # has_many :invoices, through: :invoice_items
+  # has_many :customers, through: :invoices
 end
