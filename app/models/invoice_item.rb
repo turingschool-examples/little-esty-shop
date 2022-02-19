12b03399cbf73b01ce5bd7cae893fec0dfb8fad5
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-19 15:13:57 -0700

    define display_price method for invoice_item. Write test, pass test

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,4 +2,9 @@ class InvoiceItem < ApplicationRecord
   enum status: { packaged: 0, pending: 1, shipped: 2 }
   belongs_to :invoice
   belongs_to :item
+
+  def display_price
+    cents = self.unit_price
+    '%.2f' % (cents /100.0)
+  end
 end 
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -9,4 +9,33 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'validations' do 
     it { should define_enum_for(:status).with_values([:packaged, :pending, :shipped]) }
   end 
+
+  before :each do 
+    @merchant = Merchant.create!(name: 'BuyMyThings')
+    @merchant2 = Merchant.create!(name: 'BuyMyThings')
+    @customer1 = Customer.create!(first_name: 'Tired', last_name: 'Person')
+    @customer2 = Customer.create!(first_name: 'Tired', last_name: 'Person')
+    
+    @invoice1 =Invoice.create!(status: 0, customer_id: @customer1.id)
+    @invoice2 =Invoice.create!(status: 0, customer_id: @customer1.id)
+    @invoice3 =Invoice.create!(status: 0, customer_id: @customer2.id)
+
+    @item1 = Item.create!(name: 'food', description: 'delicious', unit_price: '2000', merchant_id: @merchant.id)
+    @item2 = Item.create!(name: 'more food', description: 'even more delicious', unit_price: '3000', merchant_id: @merchant.id)
+    @item3 = Item.create!(name: 'not food at all', description: 'definitely not food', unit_price: '1500', merchant_id: @merchant2.id)
+
+    @invoice_item1 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 2, unit_price: 100, status: 1)
+    @invoice_item2 = InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice2.id, quantity: 2, unit_price: 400, status: 1)
+    @invoice_item3 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice3.id, quantity: 2, unit_price: 200, status: 1)
+  end 
+
+  describe 'instance methods' do
+    describe '#display_price' do
+        it 'displays dollars in cents' do
+          expect(@invoice_item1.display_price).to eq("1.00")
+          expect(@invoice_item2.display_price).to eq("4.00")
+        
+        end
+    end 
+  end 
 end
