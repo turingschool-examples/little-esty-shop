cd59e204cb360d11c17b495e881f8feb191a07e4
Sam Iavri <sammi80134@gmail.com>
2022-02-19 15:16:42 -0700

    Test: User Story #33, Merchant can update their items

--- spec/features/merchants/items/show_spec.rb
+++ spec/features/merchants/items/show_spec.rb
@@ -13,4 +13,25 @@ RSpec.describe 'Merchant Items Show page' do
       expect(page).to have_content(item1.unit_price)
     end
   end
+
+  describe '#user story 33' do
+    it 'allows the merchant to update their items' do
+      merchant1 = Merchant.create!(name: 'Primate Privleges')
+      item1 = merchant1.items.create!(name: 'Monkey Paw', description: 'A furry mystery', unit_price: 3)
+      visit "/merchants/#{merchant1.id}/items/#{item1.id}"
+      expect(page).to have_link("Update #{item1.name}")
+      click_on("Update #{item1.name}")
+      expect(current_path).to eq("/merchants/#{merchant1.id}/items/#{item1.id}/edit")
+      expect(page).to have_field(item_name: 'Monkey Paw')
+      expect(page).to have_field(item_description: 'A furry mystery')
+      expect(page).to have_field(item_unit_price: 3)
+
+      within('#item_update') do
+        fill_in 'item_unit_price', with: 23
+        click_on 'Update Item'
+      end
+      expect(current_path).to eq("/merchants/#{merchant1.id}/items/#{item1.id}")
+      expect(page).to have_content('Item Successfully Updated')
+    end
+  end
 end
