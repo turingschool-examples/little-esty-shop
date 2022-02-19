31616eb2706a0a9486f43d47d7a995dbc2ee3716
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-19 17:38:04 -0500

    switch to advanced routing and fix controller methods to find correct ID

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -1,11 +1,11 @@
 class MerchantItemsController < ApplicationController
 
   def index
-    @merchant = Merchant.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def show
-    @merchant = Merchant.find(params[:id])
-    @item = @merchant.items.find(params[:item_id])
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :merchants do
+    resources :items, controller: 'merchant_items'
+  end
   get '/merchant/:id/dashboard', to: 'merchants#show'
-  get '/merchants/:id/items', to: 'merchant_items#index'
   get '/merchant/:id/invoices', to: 'merchant_invoices#index'
-
-  get '/merchants/:id/items/:item_id', to: 'merchant_items#show'
 end
