aa6ee91ff4c3e5000168c8b9a4e318f8c7472986
Becky Nisttahuz <benistta@hotmail.com>
2022-02-19 14:43:31 -0700

    Feat: Add routes for merchant items edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/merchants/:merchant_id/items', to: 'merchant_items#index'
   get '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#show'
+  get '/merchants/:merchant_id/items/:item_id/edit', to: 'merchant_items#edit'
+  patch '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#update'
+
 end
