40bb137f224f614f9bf80c5b5bc4e76dd546ee3b
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-19 13:28:28 -0700

    Feat: fixed US 26, confirmerd US27 still works.

--- app/controllers/merchants/invoices_controller.rb
+++ app/controllers/merchants/invoices_controller.rb
@@ -1,4 +1,11 @@
 class Merchants::InvoicesController < ApplicationController
   def index
-  end 
+    @merchant = Merchant.find(params[:id])
+    @merch_invoice = @merchant.invoices.distinct
+  end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
-  
+
   scope :merchants, module: :merchants do
     get ':id/dashboard', to: 'dashboard#index'
     get ':id/items', to: 'items#index'
     get ':id/invoices', to: 'invoices#index'
+    get ':id/invoices/:id', to: 'invoices#show'
   end
 
   scope :admin, module: :admin do
     get '', to: 'dashboard#index'
     get '/merchants', to: 'merchants#index'
+
   end
 end
