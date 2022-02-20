ab58d86562674edf3cd39b1856b0039cfcce448f
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-20 10:50:10 -0700

    Add merchant index link to admin dashboard, waiting for invoice index

--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Admin Dashboard Index Page' do
-  before :each do 
+  before :each do
 
 
   end
@@ -11,4 +11,20 @@ describe 'Admin Dashboard Index Page' do
     expect(page).to have_content('Admin Dashboard')
   end
 
+  xit 'should have links to merchants and invoices index' do
+
+    # uncomment out the first part of this test when we have
+    # the admin invoices index page merged in!
+    # It should fail since we don't have a link in our view -Katy
+
+    # visit '/admin'
+    # click_link "View invoices"
+
+    # expect(current_path).to eq("/admin/invoices")
+
+    visit '/admin'
+    click_link "View merchants"
+
+    expect(current_path).to eq("/admin/merchants")
+  end
 end
