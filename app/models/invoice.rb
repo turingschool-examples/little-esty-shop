20381ba6903e342af3e8a495abe504ba1bd5f4ce
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-20 11:15:23 -0700

    add active record relationship invoice has many merchants through items table

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,6 +4,7 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
 
   def customer_name
     customer = Customer.find(customer_id)
@@ -11,6 +12,6 @@ class Invoice < ApplicationRecord
   end 
 
   def invoice_revenue
-  (invoice_items.sum("invoice_items.unit_price * invoice_items.quantity"))/100
+    (invoice_items.sum("invoice_items.unit_price * invoice_items.quantity"))/100
   end 
 end
