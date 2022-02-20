22917f8c2b7709428595c464fc9d9065a4ab6e5e
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-20 11:17:22 -0700

    add activerecord relationship item has many transactions through invoices. Add relationship test, pass test

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
 
   def display_price
     cents = self.unit_price
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Item, type: :model do
   describe 'relationships' do
     it {should have_many(:invoice_items)}
     it {should have_many(:invoices).through(:invoice_items)}
+    it {should have_many(:transactions).through(:invoices)}
 
   end
 
