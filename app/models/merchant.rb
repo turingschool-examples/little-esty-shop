7775d4aa98e1c639b3df03e0ecb69a1d8d14f91c
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-20 11:29:06 -0700

    test/feature: add activerecord relationships to merchant model. Write tests, tests pass

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,7 +4,9 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
-
+ 
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
 
   def merchant_invoices
     (invoices.order(:id)).uniq
