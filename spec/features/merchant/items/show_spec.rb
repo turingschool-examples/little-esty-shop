2d2bd76ff234d2886af39f01dea49d974daa26e0
Ruben Pasillas <hobbiathan@gmail.com>
2022-02-20 15:50:52 -0700

    create show spec tests

---
+++ spec/features/merchant/items/show_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Items Show Page' do
+  before(:each) do
+    @merchant_1 = Merchant.create!(name: 'Schroeder-Jerde')
+    @item_1 = @merchant_1.items.create!(name: 'tem Qui Esse', description: 'nihil autem', unit_price: 75107)
+    @item_2 = @merchant_1.items.create!(name: 'Item Autem Minima', description: 'Cumque consequuntu', unit_price: 67076)
+  end
+
+  it 'contains relevant item information' do
+    visit "/mechants/#{@merchant_1.id}/items/#{@item_1.id}"
+    expect(page).to have_content(@item_1.name)
+    expect(page).to have_content(@item_1.description)
+    expect(page).to have_content(@item_1.unit_price)
+    expect(page).to_not have_content(@item_2.name)
+    expect(page).to_not have_content(@item_2.description)
+    expect(page).to_not have_content(@item_2.unit_price)
+
+    visit "/mechants/#{@merchant_1.id}/items/#{@item_2.id}"
+    expect(page).to have_content(@item_2.name)
+    expect(page).to have_content(@item_2.description)
+    expect(page).to have_content(@item_2.unit_price)
+    expect(page).to_not have_content(@item_1.name)
+    expect(page).to_not have_content(@item_1.description)
+    expect(page).to_not have_content(@item_1.unit_price)
+  end
+
+
+end
