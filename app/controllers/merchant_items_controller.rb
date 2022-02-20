bffdee1f4596f1c36a8b581f1bb4844f34379a1f
Ruben Pasillas <hobbiathan@gmail.com>
2022-02-20 16:13:48 -0700

    create #show

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -2,4 +2,9 @@ class MerchantItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:id])
   end
+
+  def show
+    @item = Item.find(params[:item_id])
+    binding.pry
+  end
 end
