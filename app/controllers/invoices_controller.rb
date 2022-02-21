7439fa7b4e978f77ac818b7fa1175196bd8ef964
Sam Smith <samlsmith424@gmail.com>
2022-02-21 15:41:21 -0800

    Feat: #update redirecting to merchant invoice show page

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -6,7 +6,20 @@ class InvoicesController < ApplicationController
   end
 
   def show
-    @invoice = Invoice.find(params[:id])
-    # require "pry"; binding.pry
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoice = Invoice.find(params[:invoice_id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoice = Invoice.find(params[:invoice_id])
+    @invoice_item = InvoiceItem.find(params[:invoice_item_id])
+    @invoice_item.update(invoice_item_params)
+    redirect_to "/merchants/#{@merchant.id}/invoices/#{@invoice.id}"
+  end
+
+  private
+  def invoice_item_params
+    params.permit(:status)
   end
 end
