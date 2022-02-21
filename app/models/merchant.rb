4b08fea83a1efeb7de27ce135d3c4b14bc21ec54
Conor Barthel <conor.barthel@gmail.com>
2022-02-21 12:11:36 -0700

    Feat: added relationships

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,7 +4,8 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
-
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :customers
 
   def merchant_invoices
     (invoices.order(:id)).uniq
