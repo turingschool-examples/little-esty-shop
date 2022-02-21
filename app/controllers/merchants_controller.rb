a2c1d20d0f35d3b7b885162ebbfdbd2b17295ebe
Austin Moore <austinchristianmoore@gmail.com>
2022-02-21 17:21:45 -0500

    Feature: Render items ready to ship

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,6 @@
 class MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
+    @shippable_invoice_items = @merchant.ship_ready_items
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,11 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
+
+  def ship_ready_items
+    self.invoice_items.where.not(status: :shipped)
+  end
 end
