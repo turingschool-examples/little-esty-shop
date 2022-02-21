4b17baff974e4be77d227a77d815da37b86dbe29
Sam Smith <samlsmith424@gmail.com>
2022-02-20 16:09:07 -0800

    Feat: invoices index and show page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   get '/merchants/:id/dashboard', to: 'merchants#show'
+
+  get '/merchants/:id/invoices', to: 'invoices#index'
+  get '/merchants/:id/invoices/:id', to: 'invoices#show'
+
 end
