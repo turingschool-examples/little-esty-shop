34b913116f5fd9fb015244b833f3730efd42dd26
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-21 11:08:00 -0700

    hash out manual routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,9 @@ Rails.application.routes.draw do
   end 
   # End routes Aedan created
 
-  get '/merchants/:merchant_id/items', to: 'merchant_items#index'
-  get '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#show'
-  get '/merchants/:merchant_id/items/:item_id/edit', to: 'merchant_items#edit'
-  patch '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#update'
-
+  # get '/merchants/:merchant_id/items', to: 'merchant_items#index'
+  # get '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#show'
+  # get '/merchants/:merchant_id/items/:item_id/edit', to: 'merchant_items#edit'
+  # patch '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#update'
 
 end
