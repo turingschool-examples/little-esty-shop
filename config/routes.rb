cb8dc60a0e2415288ad82d6c348e366f29a12c72
Tamara Dowis <tamara.dowis@gmail.com>
2022-02-21 12:51:29 -0600

    resolve routing aaawith testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@
 
     resources :merchants, only: [:show, :index], module: :merchant do
       resources :invoices
+      resources :items
     end
-
-  # get '/merchants/:merchant_id/items', to: 'merchant_items#index'
-  # get '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#show'
-  # get '/merchants/:merchant_id/items/:item_id/edit', to: 'merchant_items#edit'
-  # patch '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#update'
 
 end
