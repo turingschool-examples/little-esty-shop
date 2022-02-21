337b23785c83781361e4318af6d44f83bce5db18
stevenjames-turing <stevenjamesstl@gmail.com>
2022-02-21 11:43:49 -0600

    Refactor SQL to ActiveRecord

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,27 +4,15 @@ class Merchant < ApplicationRecord
   validates :name, presence: true
                         
     def favorite_customers(count)
-      Merchant.find_by_sql("SELECT customers.* FROM customers 
-                            JOIN invoices ON customers.id = invoices.customer_id
-                            JOIN invoice_items ON invoices.id = invoice_items.invoice_id
-                            JOIN items ON invoice_items.item_id = items.id
-                            JOIN merchants ON items.merchant_id = merchants.id
-                            JOIN transactions on transactions.invoice_id = invoices.id
-                            WHERE merchants.id = #{id} AND transactions.result = 0
-                            GROUP BY customers.id
-                            ORDER BY COUNT(transactions.id) DESC
-                            LIMIT #{count}")
+      Merchant.joins(invoice_items: [invoice: [customer: :transactions]])
+      .where(merchants: {id: id}, transactions: {result: 0})
+      .select("customers.*").group("customers.id").order(count: :desc)
+      .limit(count)
     end
 
     def items_ready_to_ship
-      Merchant.find_by_sql("SELECT items.name, invoices.id, invoices.created_at FROM merchants 
-                            JOIN items ON merchants.id = items.merchant_id
-                            JOIN invoice_items ON items.id = invoice_items.item_id
-                            JOIN invoices ON invoice_items.invoice_id = invoices.id 
-                            JOIN transactions ON invoices.id = transactions.invoice_id
-                            WHERE merchants.id = #{id} AND invoice_items.status != 2 
-                            AND transactions.result = 0 AND invoices.status != 2
-                            ORDER BY invoices.created_at ASC")
-                            
+      Merchant.joins(invoice_items: [invoice: :transactions])
+      .where(merchants: {id: id}, invoice_items: {status: [0,1]}, invoices: {status: [0,1]}, transactions: {result: 0})
+      .select("items.name, invoices.id, invoices.created_at").order("invoices.created_at ASC")                            
     end
   end
