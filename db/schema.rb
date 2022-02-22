6854a29336e2f16b4751a826198254bf83f13238
Mark Ertmer <mark.ertmer@gmail.com>
2022-02-22 08:12:24 -0700

    DB: default value added to add_status_to_merchant

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2022_02_19_221647) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status"
+    t.integer "status", default: 0
   end
 
   create_table "transactions", force: :cascade do |t|
