3f5b25bd0f36f34ec5ffbfa4e6650ae45f234a21
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-22 17:07:33 -0500

    Add edit, update, and private params

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -10,6 +10,20 @@ class MerchantItemsController < ApplicationController
   end
 
   def edit
-
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items.find(params[:id])
   end
+
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to "/merchants/#{merchant.id}/items/#{item.id}", notice: "Item Successfully Updated"
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :description, :unit_price, :status)
+    end
 end
