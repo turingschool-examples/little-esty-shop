8af618a7e115f1785c2a0d469b63e8c6a8cf2742
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-22 18:20:23 -0500

    write test for user story functionality

---
+++ spec/features/merchants/items/create_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Merchant Item Create page" do
+  describe "when I visit the merchant item create page," do
+    it "has a form for a new item, once filled out and submitted it redirect to index with item listed" do
+      merchant_1 = Merchant.create!(name: "Staples")
+      visit "merchants/#{merchant1.id}/items/new"
+
+      within('#create_item') do
+        fill_in "Name", with: "Paul's new item"
+        fill_in "Description", with: "An item made by paul"
+        fill_in "Unit Price:", with: 1000
+        click_button "Submit"
+      end
+
+      expect(current_path).to eq("/merchants/#{merchant1.id}/items")
+      expect(merchant.items.last.name).to eq("Paul's new item")
+    end
+  end
+end
