ef1e060bd3e12f107947cb37dbbe98eac782785d
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-22 18:31:08 -0500

    Fix test errors

--- spec/features/merchants/items/create_spec.rb
+++ spec/features/merchants/items/create_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe "Merchant Item Create page" do
   describe "when I visit the merchant item create page," do
     it "has a form for a new item, once filled out and submitted it redirect to index with item listed" do
       merchant_1 = Merchant.create!(name: "Staples")
-      visit "merchants/#{merchant1.id}/items/new"
+      visit "merchants/#{merchant_1.id}/items/new"
 
       within('#create_item') do
         fill_in "Name", with: "Paul's new item"
         fill_in "Description", with: "An item made by paul"
         fill_in "Unit Price:", with: 1000
-        click_button "Submit"
+        click_button "Create Item"
       end
 
-      expect(current_path).to eq("/merchants/#{merchant1.id}/items")
+      expect(current_path).to eq("/merchants/#{merchant_1.id}/items")
       expect(merchant.items.last.name).to eq("Paul's new item")
     end
   end
