9a8ca37ea047a3d9aade8e1dd9c55b973ec79940
Paul Leonard <paulpaulleonard@gmail.com>
2022-02-21 20:11:47 -0500

    Create first test

---
+++ spec/features/merchants/items/edit_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Merchant Items Show Page" do
+  describe "when I visit the merchant items edit page" do
+    it "I see form filled in with existing info" do
+      merchant = Merchant.create!(name: "Paul the Merchant")
+      item1 = merchant.items.create!(name: "Paul's Item",
+                                     description: "Paul's very popular item",
+                                     unit_price: "2500")
+      visit "/merchants/#{merchant.id}/items/#{item1.id}/edit"
+
+      expect(page).to have_field(:item_unit_price, with: 2500)
+      expect(page).to have_field(:item_name, with: item1.name)
+      expect(page).to have_field(:item_description, with: item1.description)
+    end
+
+    it "When the form is filled out and submitted, " do
+
+    end
+  end
+end
