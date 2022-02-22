87cf1c57d65f209a76c6b47ffd1ed0a91cc0227c
Austin Moore <austinchristianmoore@gmail.com>
2022-02-21 22:12:06 -0500

    Feature: Create route, controller and render

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/merchant/:id/dashboard', to: 'merchants#show'
-  get '/merchants/:id/items', to: 'merchant_items#index'
-  get '/merchant/:id/invoices', to: 'merchant_invoices#index'
 
+  get '/merchants/:id/items', to: 'merchant_items#index'
+
+  get '/merchant/:id/invoices', to: 'merchant_invoices#index'
+  get '/merchant/:merchant_id/invoices/:invoice_id', to: 'merchant_invoices#show'
 
   get '/items/:id', to: 'items#show'
   patch 'items/:id', to: 'items#update_status'
