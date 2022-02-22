7a1426aab4d01766f5ae8d763db2fdfe2858f16b
Austin Moore <austinchristianmoore@gmail.com>
2022-02-21 19:43:34 -0500

    Feature: Create functionality

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,6 +1,6 @@
 class MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
-    @shippable_invoice_items = @merchant.ship_ready_items
+    @shippable_invoice_items = @merchant.ship_ready_items.order_by_oldest
   end
 end
