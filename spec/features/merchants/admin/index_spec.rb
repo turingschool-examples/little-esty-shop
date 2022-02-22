8ae5fa37d4a4c590d04c7df530dd0737806d35b8
stevenjames-turing <stevenjamesstl@gmail.com>
2022-02-21 20:39:58 -0600

    Admin Merchants Grouped by Status

--- spec/features/merchants/admin/index_spec.rb
+++ spec/features/merchants/admin/index_spec.rb
@@ -24,6 +24,35 @@ RSpec.describe 'admin merchant index' do
     
         visit '/admin/merchants/'
 
+        expect(page).to have_content(merchant_1.name)
+        expect(page).to have_content(merchant_2.name)
+        expect(page).to have_content(merchant_3.name)
+        expect(page).to have_button("Disable", count: 3)
+        
+        expect(page).to have_content(merchant_4.name)
+        expect(merchant_4.status).to eq("disabled")
+        expect(page).to have_button("Enable", count: 1)
+        
+        click_button("Enable")
+        expect(current_path).to eq("/admin/merchants/")
+        
+        
+        expect(page).to have_content(merchant_1.name)
+        expect(page).to have_content(merchant_2.name)
+        expect(page).to have_content(merchant_3.name)
+        expect(page).to have_content(merchant_4.name)
+        expect(page).to have_button("Disable", count: 4)
+        expect(page).to_not have_button("Enable")
+    end
+
+    it 'Admin Merchants Grouped by Status' do 
+        merchant_1 = create(:merchant)
+        merchant_2 = create(:merchant)
+        merchant_3 = create(:merchant)         
+        merchant_4 = create(:merchant, status: 1)
+    
+        visit '/admin/merchants/'
+
         within ".enabled" do 
             expect(page).to have_content(merchant_1.name)
             expect(page).to have_content(merchant_2.name)
