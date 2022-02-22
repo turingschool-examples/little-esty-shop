2df933bef9ebaf211670fc349af3ecec8b3a9aed
Sully Birashk <sully.birashk@gmail.com>
2022-02-22 15:25:36 -0800

    Refactor Tests

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe 'the merchant item index page' do
     @merchant_1 = Merchant.create!(name: "Staples")
     @item_1 = @merchant_1.items.create!(name: "stapler", description: "Staples papers together", unit_price: 13)
     @item_2 = @merchant_1.items.create!(name: "paper", description: "construction", unit_price: 29)
-    @enabled_item = @merchant_1.items.create!(name: "pen", description: "red ink", unit_price: 5, status: "Enabled")
     @merchant_2 = Merchant.create!(name: "Dunder Miflin")
     @item_3 = @merchant_2.items.create!(name: "calculator", description: "TI-84", unit_price: 84)
     @item_4 = @merchant_2.items.create!(name: "paperclips", description: "24 Count", unit_price: 25)
@@ -57,6 +56,8 @@ RSpec.describe 'the merchant item index page' do
   end
 
   it "Has Enabled and Disabled section on index page" do
+    @enabled_item = @merchant_1.items.create!(name: "pen", description: "red ink", unit_price: 5, status: "Enabled")
+    
     visit "/merchants/#{@merchant_1.id}/items"
 
     expect(page).to have_content('Enabled Items')
