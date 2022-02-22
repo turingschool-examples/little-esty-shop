7694ee555ff21ea5e15d05164be819df9c2673a6
Mark Ertmer <mark.ertmer@gmail.com>
2022-02-22 07:39:28 -0700

    Add: add merchant link test, fix post /merchants path

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     get '/merchants', to: 'merchants#index'
     get '/merchants/new', to: 'merchants#new'
     get '/merchants/:id', to: 'merchants#show'
-    post '/merchants/create', to: 'merchants#create'
+    post '/merchants', to: 'merchants#create'
     get '/merchants/:id/edit', to: 'merchants#edit'
     patch '/merchants/:id', to: 'merchants#update'
   end
