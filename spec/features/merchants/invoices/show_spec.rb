bf03998dc9d74acd96da73159820dec21ca3a2d7
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-23 16:05:56 -0700

    US24 Test for merchant revenue

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -7,7 +7,11 @@ RSpec.describe 'Shows 1 invoice, and all its attributes', type: :feature do
     @merchant1 = Merchant.create!(name: "The Tornado")
     @item1 = @merchant1.items.create!(name: "SmartPants", description: "IQ + 20", unit_price: 125)
     @customer1 = Customer.create!(first_name: "Marky", last_name: "Mark" )
+    @customer2 = Customer.create!(first_name: "Larky", last_name: "Lark" )
+    @customer3 = Customer.create!(first_name: "Sparky", last_name: "Spark" )
     @invoice1 = @customer1.invoices.create!(status: 1)
+    @invoice2 = @customer2.invoices.create!(status: 1)
+    @invoice3 = @customer2.invoices.create!(status: 1)
     @invoice_item1 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, quantity: 2, unit_price: 125, status: 1)
   end
 
@@ -26,4 +30,9 @@ RSpec.describe 'Shows 1 invoice, and all its attributes', type: :feature do
     expect(page).to have_content("Customer name: #{@invoice1.customer.first_name} #{@invoice1.customer.last_name}")
     expect(page).to have_content("#{@invoice1.created_at.strftime("%A, %B, %d, %Y")}")
   end
+
+  it " test for the total amount of the invoice." do
+    visit "/merchants/#{@merchant1.id}/invoices/#{@invoice1.id}"
+
+    expect(page).to have_content("Total amount from Marky's invoice: $2125")
 end
