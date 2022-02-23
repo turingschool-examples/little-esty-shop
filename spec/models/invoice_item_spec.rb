fa55bde1ded4664310fcd3f38602c83c7e9a8a8b
Austin Moore <austinchristianmoore@gmail.com>
2022-02-23 10:47:42 -0500

    Feature: Create revenue tests

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,14 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  describe "reltionships" do
+  describe "relationships" do
     it { should belong_to :invoice }
     it { should belong_to :item }
-  end
-
-  describe "Instance Methods" do
-    it "calculates each invoice_items revenue" do
-      
-    end
   end
 end
