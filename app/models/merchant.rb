271ce72fc6ef15ee1288da36b59962c3915219dc
Tamara Dowis <tamara.dowis@gmail.com>
2022-02-23 16:51:26 -0600

    top 5 items share

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -19,4 +19,14 @@ class Merchant < ApplicationRecord
   def disabled_status
     self.items.where("item_status =?", 2)
   end
+
+  def five_most_popular_items
+    # self.joins(items: [{ invoice_items: :invoices}, :transactions])
+    items.joins(invoice_items: { invoice: :transactions})
+        .where('transactions.result =?', 0)
+        .select("merchant_id, invoice_items.item_id, items.name, sum(invoice_items.unit_price) AS total_item_sales")
+        .group("merchant_id, invoice_items.item_id, items.name")
+        .order("merchant_id, total_item_sales DESC")
+        .limit(5)
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,10 +11,10 @@ RSpec.describe Merchant, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of :name }
-    
+
   end
 
-  before :each do 
+  before :each do
     @merchant = Merchant.create!(name: 'BuyMyThings')
     @customer1 = Customer.create!(first_name: 'Tired', last_name: 'Person')
     @invoice1 =Invoice.create!(status: 0, customer_id: @customer1.id)
@@ -22,16 +22,16 @@ RSpec.describe Merchant, type: :model do
     @item2 = Item.create!(name: 'more food', description: 'even more delicious', unit_price: '3000', merchant_id: @merchant.id)
     @item3 = Item.create!(name: 'not food at all', description: 'definitely not food', unit_price: '1500', merchant_id: @merchant.id)
     @invoice_item1 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 2, unit_price: 200, status: 1)
-  end 
+  end
 
-  describe 'instance methods' do 
-    describe '.merchant_invoices' do 
-      it "returns all of the invoices that include a merchant's items" do 
+  describe 'instance methods' do
+    describe '.merchant_invoices' do
+      it "returns all of the invoices that include a merchant's items" do
         merchant = Merchant.create!(name: 'BuyMyThings')
         merchant2 = Merchant.create!(name: 'BuyMyThings')
         customer1 = Customer.create!(first_name: 'Tired', last_name: 'Person')
         customer2 = Customer.create!(first_name: 'Tired', last_name: 'Person')
-        
+
         invoice1 =Invoice.create!(status: 0, customer_id: customer1.id)
         invoice2 =Invoice.create!(status: 0, customer_id: customer1.id)
         invoice3 =Invoice.create!(status: 0, customer_id: customer2.id)
@@ -45,7 +45,76 @@ RSpec.describe Merchant, type: :model do
         invoice_item3 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice3.id, quantity: 2, unit_price: 100, status: 1)
         expect(merchant.merchant_invoices).to eq([invoice1, invoice2])
         expect(merchant2.merchant_invoices).to eq([invoice3])
-      end 
-    end 
-  end 
+      end
+    end
+  end
+end
+
+RSpec.describe Merchant, type: :model do
+  describe 'instance method' do
+  before :each do
+    @merchant2 = Merchant.create!(name: "Juan Lopez")
+
+    @item1 = @merchant2.items.create!(name: "cheese", description: "european cheese", unit_price: 2400, item_status: 1)
+    @item2 = @merchant2.items.create!(name: "onion", description: "red onion", unit_price: 3450, item_status: 1)
+    @item3 = @merchant2.items.create!(name: "earing", description: "Lotus earings", unit_price: 14500)
+    @item4 = @merchant2.items.create!(name: "bracelet", description: "Silver bracelet", unit_price: 76000, item_status: 1)
+    @item5 = @merchant2.items.create!(name: "ring", description: "lotus ring", unit_price: 2345)
+    @item6 = @merchant2.items.create!(name: "skirt", description: "Hoop skirt", unit_price: 2175, item_status: 1)
+    @item7 = @merchant2.items.create!(name: "shirt", description: "Mike's Yellow Shirt", unit_price: 5405, item_status: 1)
+    @item8 = @merchant2.items.create!(name: "socks", description: "Cat Socks", unit_price: 934)
+
+    @customer1 = Customer.create!(first_name: 'Fred', last_name: 'Dunce')
+    @customer2 = Customer.create!(first_name: 'Sal', last_name: 'Dali')
+    @customer3 = Customer.create!(first_name: 'Earny', last_name: 'Hemi')
+    @customer4 = Customer.create!(first_name: 'Jack', last_name: 'Wack')
+    @customer5 = Customer.create!(first_name: 'Mack', last_name: 'Aurther')
+    @customer6 = Customer.create!(first_name: 'Charly', last_name: 'Dicky')
+
+    @invoice1 =Invoice.create!(status: 2, customer_id: @customer1.id)
+    @invoice2 =Invoice.create!(status: 2, customer_id: @customer1.id)
+    @invoice3 =Invoice.create!(status: 2, customer_id: @customer2.id)
+    @invoice4 =Invoice.create!(status: 2, customer_id: @customer3.id)
+    @invoice5 =Invoice.create!(status: 2, customer_id: @customer4.id)
+    @invoice6 =Invoice.create!(status: 2, customer_id: @customer5.id)
+    @invoice7 =Invoice.create!(status: 2, customer_id: @customer6.id)
+
+    @invoice_item1 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 3, unit_price: 2400, status: 1)
+    @invoice_item2 = InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice2.id, quantity: 2, unit_price: 3450, status: 0)
+    # @invoice_item3 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice3.id, quantity: 1, unit_price: 14500, status: 2)
+    @invoice_item4 = InvoiceItem.create!(item_id: @item7.id, invoice_id: @invoice4.id, quantity: 2, unit_price: 5405, status: 2)
+    @invoice_item5 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice5.id, quantity: 1, unit_price: 14500, status: 2)
+    @invoice_item6 = InvoiceItem.create!(item_id: @item4.id, invoice_id: @invoice6.id, quantity: 1, unit_price: 76000, status: 2)
+    # @invoice_item7 = InvoiceItem.create!(item_id: @item6.id, invoice_id: @invoice7.id, quantity: 2, unit_price: 2175, status: 2)
+    @invoice_item8 = InvoiceItem.create!(item_id: @item8.id, invoice_id: @invoice1.id, quantity: 4, unit_price: 934, status: 2)
+    @invoice_item9 = InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice2.id, quantity: 2, unit_price: 3450, status: 2)
+    # @invoice_item10 = InvoiceItem.create!(item_id: @item4.id, invoice_id: @invoice3.id, quantity: 1, unit_price: 76000, status: 2)
+    @invoice_item11 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice4.id, quantity: 2, unit_price: 2400, status: 2)
+    @invoice_item12 = InvoiceItem.create!(item_id: @item5.id, invoice_id: @invoice5.id, quantity: 3, unit_price: 2345, status: 2)
+    # @invoice_item13 = InvoiceItem.create!(item_id: @item6.id, invoice_id: @invoice7.id, quantity: 5, unit_price: 2175, status: 2)
+    # @invoice_item14 = InvoiceItem.create!(item_id: @item8.id, invoice_id: @invoice7.id, quantity: 7, unit_price: 934, status: 2)
+    @invoice_item15 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice6.id, quantity: 2, unit_price: 2400, status: 2)
+    @invoice_item16 = InvoiceItem.create!(item_id: @item7.id, invoice_id: @invoice2.id, quantity: 2, unit_price: 5405, status: 2)
+    @invoice_item17 = InvoiceItem.create!(item_id: @item6.id, invoice_id: @invoice2.id, quantity: 2, unit_price: 2175, status: 2)
+    @invoice_item18 = InvoiceItem.create!(item_id: @item5.id, invoice_id: @invoice4.id, quantity: 2, unit_price: 2345, status: 2)
+    @invoice_item19 = InvoiceItem.create!(item_id: @item4.id, invoice_id: @invoice4.id, quantity: 1, unit_price: 76000, status: 2)
+    # @invoice_item20 = InvoiceItem.create!(item_id: @item7.id, invoice_id: @invoice7.id, quantity: 2, unit_price: 5405, status: 2)
+
+
+    @transaction1 = Transaction.create!(result: 0, invoice_id: @invoice1.id)
+    @transaction2 = Transaction.create!(result: 0, invoice_id: @invoice2.id)
+    @transaction3 = Transaction.create!(result: 0, invoice_id: @invoice3.id)
+    @transaction4 = Transaction.create!(result: 1, invoice_id: @invoice3.id)
+    @transaction5 = Transaction.create!(result: 0, invoice_id: @invoice4.id)
+    @transaction6 = Transaction.create!(result: 0, invoice_id: @invoice5.id)
+    @transaction7 = Transaction.create!(result: 0, invoice_id: @invoice6.id)
+    @transaction8 = Transaction.create!(result: 1, invoice_id: @invoice7.id)
+    end
+
+    it 'returns the names of the top 5 most popular items on the merchant index page' do
+
+        expect(@merchant2.five_most_popular_items).to eq(true)
+    end
+  end
+
 end
