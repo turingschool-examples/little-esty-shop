ba96e3997d0946ecead86e51987bd6d7a500d493
Blake Dempsey <bdempsey864@gmail.com>
2022-02-23 09:54:49 -0500

    Feat: Refactor and finish User Story 37

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,10 +7,14 @@ class Merchant < ApplicationRecord
 
   validates_presence_of :name
   
+  # def ordered_items_to_ship
+  #   item_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:item_id)
+  #   item_ids.map do |id|
+  #     Item.find(id)
+  #   end
+  # end
+
   def ordered_items_to_ship
-    item_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:item_id)
-    item_ids.map do |id|
-      Item.find(id)
-    end
+    Item.find_by_sql "SELECT items.name, invoice_items.invoice_id, invoice_items.status, invoice_items.created_at FROM items INNER JOIN invoice_items ON items.id=invoice_items.item_id WHERE status=0 OR status=1 ORDER BY invoice_items.created_at ASC"
   end
 end
