c9c800b08ceb2301e3bd47bad5de2d16272e8afd
Sam Iavri <sammi80134@gmail.com>
2022-02-22 18:46:42 -0700

    Test: testing disable feat.

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -35,5 +35,46 @@ RSpec.describe 'Merchant Items Index page' do
         expect(item1.status).to eq('disabled')
       end
     end
+
+    it 'can disable an item' do
+      merchant1 = Merchant.create!(name: 'Primate Privleges')
+      item1 = merchant1.items.create!(name: 'Monkey Paw', description: 'A furry mystery', unit_price: 3)
+      item2 = merchant1.items.create!(name: 'Gorilla Grip Glue', description: 'A sticky mystery', unit_price: 7)
+
+      visit "/merchants/#{merchant1.id}/items"
+
+      within("div.item_#{item1.id}") do
+        click_button("Disable #{item1.name}")
+        expect(page).to have_button("Enable #{item1.name}")
+        click_button("Enable #{item1.name}")
+
+        expect(current_path).to eq("/merchants/#{merchant1.id}/items")
+
+        expect(page).to have_button("Disable #{item1.name}")
+        item1.reload
+        expect(item1.status).to eq('enabled')
+      end
+    end
+  end
+
+  describe '#User story #31' do
+    xit 'can enable an item' do
+      merchant1 = Merchant.create!(name: 'Primate Privleges')
+      item1 = merchant1.items.create!(name: 'Monkey Paw', description: 'A furry mystery', unit_price: 3)
+      item2 = merchant1.items.create!(name: 'Gorilla Grip Glue', description: 'A sticky mystery', unit_price: 7)
+
+      visit "/merchants/#{merchant1.id}/items"
+
+      within("div.item_#{item1.id}") do
+        expect(page).to have_button("Disable #{item1.name}")
+        click_button("Disable #{item1.name}")
+
+        expect(current_path).to eq("/merchants/#{merchant1.id}/items")
+
+        expect(page).to have_button("Enable #{item1.name}")
+        item1.reload
+        expect(item1.status).to eq('disabled')
+      end
+    end
   end
 end
