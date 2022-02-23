c9ea8e16c4a56e7ea9116560401ab2adab6f9d33
Austin Moore <austinchristianmoore@gmail.com>
2022-02-23 09:26:03 -0500

    Fix: Reverting to symbol notation temporarily

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,6 +3,6 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  
-  enum status: {"cancelled" => 0, "in progress" => 1, "completed" => 2}
+
+  enum status: [:cancelled, :in_progress, :completed]
 end
