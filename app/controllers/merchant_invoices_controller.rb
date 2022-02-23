d73699a0b0af1bfc705add7081889e1fcfbb7734
Austin Moore <austinchristianmoore@gmail.com>
2022-02-22 19:07:30 -0500

    Feature: Create route and method for update functionality

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -5,4 +5,10 @@ class MerchantInvoicesController < ApplicationController
     @invoice = @merchant.invoices.find(params[:invoice_id])
   end
 
+  def update
+    @altered_invoice_item = InvoiceItem.find(params[:invoice_item_id])
+    @altered_invoice_item.update(status: params[:status])
+    redirect_to "/merchant/#{params[:merchant_id]}/invoices/#{params[:invoice_id]}"
+  end
+
 end
