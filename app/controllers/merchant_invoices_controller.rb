e8bb5a177f212877c9ca278bbdf8ab0f02ac618d
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-23 21:29:44 -0500

    Add: Update method for MerchantInvoice controller

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -3,4 +3,23 @@ class MerchantInvoicesController < ApplicationController
     @merchant = Merchant.find(params[:id])
     @invoice = Invoice.find(params[:invoice_id])
   end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+    @invoice = Invoice.find(params[:invoice_id])
+    if params[:status] == "pending"
+      @invoice.status = 0
+    elsif params[:status] == "packaged"
+      @invoice.status = 1
+    elsif params[:status] == "shipped"
+      @invoice.status = 2
+    end
+    redirect_to "/merchants/#{@merchant.id}/invoices/#{@invoice.id}"
+  end
+
+  private
+
+  def invoice_status_param
+    params.permit(:status)
+  end
 end
