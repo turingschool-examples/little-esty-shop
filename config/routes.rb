4a15e16bc12f45c87568730be870f4d8f1bee646
Conor Barthel <conor.barthel@gmail.com>
2022-02-24 10:43:43 -0700

    Feat: file structure for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
   Rails.application.routes.draw do
 
-
   get '/merchants', to: 'merchants#index'
     resources :merchants, only: [:show, :index, :update, :post], module: :merchant do
       resources :invoices
