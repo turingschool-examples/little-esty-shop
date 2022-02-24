ad19903a6b628af74ae4705c9a882ce3bfe08b10
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-24 10:56:40 -0700

    Add item show page links to merchant items index

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -17,5 +17,13 @@ RSpec.describe 'merchant item index', type: :feature do
     expect(page).to have_content(@item3.name)
   end
 
+  it 'has links to each items show page' do
+    visit "/merchants/#{@merchant.id}/items"
 
+    within "#item-#{@item2.id}" do
+      click_link "View item"
+    end
+
+    expect(current_path).to eq("/merchants/#{@merchant.id}/items/#{@item2.id}")
+  end
 end
