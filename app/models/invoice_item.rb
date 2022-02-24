0844c987658693705c168342d0785eee6d679099
Austin Moore <austinchristianmoore@gmail.com>
2022-02-24 11:10:15 -0500

    Refactor: Combine order by method to ship ready items

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,8 +2,4 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
   enum status: {"pending" => 0, "packaged" => 1, "shipped" => 2}
-
-  def self.order_by_oldest
-    order(:created_at)
-  end
 end
