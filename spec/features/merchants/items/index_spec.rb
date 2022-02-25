9ff418382607e1324d2506a4cfe4cb20891390f9
Mark Ertmer <mark.ertmer@gmail.com>
2022-02-24 19:13:01 -0700

    Mark adds spec setup on merchant items index

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -16,13 +16,19 @@ RSpec.describe 'merchant item index', type: :feature do
   end
 
   it 'has links to each items show page' do
-    visit "/merchants/#{@merchant.id}/items"
+    merchant = create(:merchant)
 
-    within "#item-#{@item2.id}" do
+    item1 = create(:item, merchant: merchant)
+    item2 = create(:item, merchant: merchant)
+    item3 = create(:item, merchant: merchant)
+
+    visit "/merchants/#{merchant.id}/items"
+
+    within "#item-#{item2.id}" do
       click_link "View item"
     end
 
-    expect(current_path).to eq("/merchants/#{@merchant.id}/items/#{@item2.id}")
+    expect(current_path).to eq("/merchants/#{merchant.id}/items/#{item2.id}")
   end
 
   it 'can toggle the status from the index page' do
