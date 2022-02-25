8a933d74d832948e7847502969438c6469cd9e82
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-25 08:19:29 -0700

    test: Write test for button admin can click to swap merchant status in admin merchant index view. Feature: implement button, pass test

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -3,4 +3,11 @@ class Admin::MerchantsController < ApplicationController
   def index 
     @merchants = Merchant.all
   end
+
+  def update 
+    if params[:trigger]
+      # Somehow clicking the button is changing the merchant's status before I even can do it here? Weird. Must be in the view 
+      redirect_to admin_merchants_path
+    end 
+  end 
 end
--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -28,4 +28,18 @@ RSpec.describe 'The Admin Merchants Index' do
     click_link(@merchant1.name)
     expect(current_path).to eq(admin_merchant_path(@merchant1.id))
   end 
+
+  it 'displays a button an admin can use to enable or disable a merchants status' do 
+    visit admin_merchants_path
+    within '#the-duke' do
+      expect(page).to have_button("disable merchant")
+      expect(@merchant1.status).to eq("enabled")
+      expect(page).to have_no_button("enable merchant")  
+      click_button("disable merchant")
+      expect(current_path).to eq(admin_merchants_path)
+      @merchant1.reload
+      expect(page).to have_button("enable merchant")
+    end
+    
+  end 
 end 
