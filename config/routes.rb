2265996969d3d81ce185dd81810bae583539d0b4
Richard LaBrecque <richardlabreque@gmail.com>
2022-02-24 20:59:57 -0500

    refactor admin merchants index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,32 @@
 Rails.application.routes.draw do
   resources :merchants do
-    resources :items, controller: 'merchant_items'
+    resources :items, only:[:index, :show], controller: 'merchant_items'
+    resources :invoices, only:[:index, :show, :update], controller: 'merchant_invoices'
   end
+  resources :items, only:[:show, :update]
+  resources :invoices, only:[:show]
+
+
+
+namespace :admin do
+  resources :merchants, :invoices
+end
 
   get '/', to: "welcome#index"
 
-  get '/items/:id', to: 'items#show'
-  patch 'items/:id', to: 'items#update_status'
-  get '/merchant/:id/dashboard', to: 'merchants#show'
+  #get '/items/:id', to: 'items#show'
+  #patch 'items/:id', to: 'items#update_status'
+  #get '/merchant/:id/dashboard', to: 'merchants#show'
 
-  get '/merchants/:id/invoices', to: 'merchant_invoices#index'
-  get '/merchants/:id/items', to: 'merchant_items#index'
-  get '/merchant/:id/invoices', to: 'merchant_invoices#index'
+  #get '/merchants/:id/invoices', to: 'merchant_invoices#index'
+  #get '/merchants/:id/items', to: 'merchant_items#index'
+  #get '/merchant/:id/invoices', to: 'merchant_invoices#index'
 
-  get '/merchant/:merchant_id/invoices/:invoice_id', to: 'merchant_invoices#show'
-  patch '/merchant/:merchant_id/invoices/:invoice_id', to: 'merchant_invoices#update'
-  get '/merchants/:id/items/:item_id', to: 'merchant_items#show'
-  get '/invoices/:id', to: 'invoices#show'
+  #get '/merchant/:merchant_id/invoices/:invoice_id', to: 'merchant_invoices#show'
+  #patch '/merchant/:merchant_id/invoices/:invoice_id', to: 'merchant_invoices#update'
+  #get '/merchants/:id/items/:item_id', to: 'merchant_items#show'
+  #get '/invoices/:id', to: 'invoices#show'
 
-  get '/admin', to: 'admin#show'
-  get '/admin/merchants', to: 'admin_merchants#index'
-  get '/admin/invoices', to: 'admin_invoices#index'
 
 
 
