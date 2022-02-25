986a7167a2d38f3d51dd2de10a78e7b7408af03e
Michael Brandt <msbrandt00@gmail.com>
2022-02-25 10:17:22 -0700

    refactor rake file with module

--- lib/task_helper/application_helper.rb
+++ lib/task_helper/application_helper.rb
@@ -1,16 +1,11 @@
-module ApplicationHelper
-
-  def date_formatter(string)
-      string.strftime('%A, %B %d, %Y')
-  end
-
-  def import(path, type)
-     binding.pry
+require 'csv'
+module ApplicationHelper 
+ def import_data(path, type)
     CSV.foreach(path, :headers => true) do |row| 
       type.create!(row.to_hash)
     end
     ActiveRecord::Base.connection.execute("ALTER SEQUENCE invoice_items_id_seq RESTART WITH #{type.maximum(:id) + 1}")
-   
   end
-
 end
+
+
