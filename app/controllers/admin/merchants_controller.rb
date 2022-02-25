81af60d65ac11b843110dce6c255f102dbc00c25
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-25 13:15:26 -0700

    fix: update controller to find merchant first thing

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -13,12 +13,11 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def update 
+    merchant = set_merchant
     if params[:trigger]
-      merchant = set_merchant
       merchant.change_status
       redirect_to admin_merchants_path
     elsif merchant.name != params[:name]
-      merchant = set_merchant
       merchant.update!(name: params[:name])
       redirect_to admin_merchant_path(merchant)
       flash[:notice] = "#{merchant.name} has been updated"
