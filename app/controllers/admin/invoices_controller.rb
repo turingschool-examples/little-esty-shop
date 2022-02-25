30262f989508e02e88c43876559c3c99d84b0c14
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-24 18:03:11 -0700

    Test: fixed.  Methods had doubled up in the controller somehow.

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -6,18 +6,6 @@ class Admin::InvoicesController < ApplicationController
   def show
     @invoice = Invoice.find(params[:invoice_id])
     @inv_items = @invoice.items
-
- end
-
-  def update
-    @invoice = Invoice.find(params[:invoice_id])
-    @invoice.update(status: params[:status])
-
-    redirect_to "/merchants/#{params[:merchant_id]}/invoices/#{params[:invoice_id]}"
-  end
-private
-  def invoice_params
-    params.permit(:status)
   end
 
   def update
@@ -25,7 +13,8 @@ private
     @invoice.update(status: params[:status])
     redirect_to "/admin/invoices/#{params[:id]}"
   end
-private
+
+  private
   def invoice_params
     params.permit(:status)
   end
