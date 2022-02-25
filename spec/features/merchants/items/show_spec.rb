692515ccba62077b999719c3e5183efebbd5ed4c
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-24 18:45:46 -0700

    Add status attribute to merchant item show page

--- spec/features/merchants/items/show_spec.rb
+++ spec/features/merchants/items/show_spec.rb
@@ -14,31 +14,35 @@ RSpec.describe 'Merchant Items show page', type: :feature do
   end
 
   it "can see all the items attributes including name, description, and selling price" do
- 
-    visit ("/merchants/#{@merchant.id}/items/#{@item1.id}") 
-  
+
+    visit ("/merchants/#{@merchant.id}/items/#{@item1.id}")
+
       expect(page).to have_content(@item1.name)
       expect(page).to have_content(@item1.description)
       expect(page).to have_content(@item1.unit_price)
+      expect(page).to have_content(@item1.status)
       expect(page).to have_no_content(@item2.name)
 
-    visit "/merchants/#{@merchant.id}/items/#{@item2.id}" 
+    visit "/merchants/#{@merchant.id}/items/#{@item2.id}"
         expect(page).to have_content(@item2.name)
         expect(page).to have_content(@item2.description)
         expect(page).to have_content(@item2.unit_price)
+        expect(page).to have_content(@item2.status)
         expect(page).to have_no_content(@item3.name)
 
-    visit "/merchants/#{@merchant2.id}/items/#{@item4.id}" 
+    visit "/merchants/#{@merchant2.id}/items/#{@item4.id}"
       expect(page).to have_content(@item4.name)
       expect(page).to have_content(@item4.description)
       expect(page).to have_content(@item4.unit_price)
+      expect(page).to have_content(@item4.status)
       expect(page).to have_no_content(@item5.name)
 
 
-      visit "/merchants/#{@merchant2.id}/items/#{@item5.id}" 
+      visit "/merchants/#{@merchant2.id}/items/#{@item5.id}"
       expect(page).to have_content(@item5.name)
       expect(page).to have_content(@item5.description)
       expect(page).to have_content(@item5.unit_price)
-      expect(page).to have_no_content(@item6.name)    
+      expect(page).to have_content(@item5.status)
+      expect(page).to have_no_content(@item6.name)
   end
 end
