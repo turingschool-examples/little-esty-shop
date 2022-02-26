e3c80510cf0f77692ccce44d02236e9b9b5cfcbc
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-02-26 16:50:45 -0600

    Refactor '.to_s' to be '.to_sym'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,8 @@ module ApplicationHelper
   end
 
   def sortable(data, params)
-    if params[:sort] != nil && ActiveRecord::Base.connection.column_exists?(params[:controller].to_s, params[:sort])
+    require 'pry'; binding.pry
+    if params[:sort] != nil && ActiveRecord::Base.connection.column_exists?(params[:controller].to_sym, params[:sort])
         data.order(params[:sort])
     else 
       data
