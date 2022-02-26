2ab8b4201859e89d82168fdc14679df0f001733b
Becky Nisttahuz <benistta@hotmail.com>
2022-02-26 13:33:59 -0700

    Feat: Admin Dashboard Links complete

--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -6,13 +6,6 @@ RSpec.describe 'The Admin Dashboard Index' do
     expect(page).to have_content("Admin Dashboard")
   end
 
-#   Admin Dashboard Links
-#
-# As an admin,
-# When I visit the admin dashboard (/admin)
-# Then I see a link to the admin merchants index (/admin/merchants)
-# And I see a link to the admin invoices index (/admin/invoices)
-
   it 'can see link to admin merchant index' do
     visit admin_dashboard_index_path
     expect(page).to have_link("Admin merchants index")
