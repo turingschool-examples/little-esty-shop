fa792adec73830a407b2bfcdbda55ac83b47bfe3
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-02-26 17:07:30 -0600

    Extension: Sortables added to Admin Invoice Index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   end
 
   def sortable(data, params)
-    if params[:sort] != nil && ActiveRecord::Base.connection.column_exists?(params[:controller].to_sym, params[:sort])
+    if params[:sort] != nil && ActiveRecord::Base.connection.column_exists?(params[:controller].delete_prefix("admin/").to_sym, params[:sort])
         data.order(params[:sort])
     else 
       data
