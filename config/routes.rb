b4c6d8bcda3b34697a3edb03cbd1abd3c21dc1d6
Conor Barthel <conor.barthel@gmail.com>
2022-02-26 09:49:19 -0700

    Feat:add partial for merchant new and edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@
 
   get '/merchants', to: 'merchants#index'
     resources :merchants, only: [:show, :index, :update, :post], module: :merchant do
-      resources :invoices only: [:index, :show, :update]
-      resources :dashboard only: [:index]
-      resources :items [:index, :show, :edit, :update, :new, :create]
+      resources :invoices, only: [:index, :show, :update]
+      resources :dashboard, only: [:index]
+      resources :items, only: [:index, :show, :edit, :update, :new, :create]
     end
     namespace :admin, only: [:index, :show, :edit, :update, :new, :create], module: :admin do
-      resources :dashboard [:index]
-      resources :merchants [:index, :show, :edit, :update, :new, :create]
-      resources :invoices [:index, :show, :update]
+      resources :dashboard, only: [:index]
+      resources :merchants, only: [:index, :show, :edit, :update, :new, :create]
+      resources :invoices, only: [:index, :show, :update]
     end
 end
