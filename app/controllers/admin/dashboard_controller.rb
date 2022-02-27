b2e6290daebeb825b336b36ab5194061da0f460b
Becky Nisttahuz <benistta@hotmail.com>
2022-02-26 19:19:07 -0700

    Refactor: index method in controller admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Admin::DashboardController < ApplicationController
   def index
     @customers = Customer.top_five_customers
     @invoices = Invoice.not_shipped
+    # @invoices = InvoiceItem.not_shipped
   end
 
 
