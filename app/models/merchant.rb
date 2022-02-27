1bc1b16eb4f596bdbbbccfb8cae82fab043acf27
Ruben Pasillas <hobbiathan@gmail.com>
2022-02-26 23:41:21 -0700

    Create ::group_items_by_status(bool)

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,7 +6,7 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   validates_presence_of :name
-  
+
   # def ordered_items_to_ship
   #   item_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:item_id)
   #   item_ids.map do |id|
@@ -16,5 +16,9 @@ class Merchant < ApplicationRecord
 
   def ordered_items_to_ship
     Item.find_by_sql "SELECT items.name, invoice_items.invoice_id, invoice_items.status, invoice_items.created_at FROM items INNER JOIN invoice_items ON items.id=invoice_items.item_id WHERE status=0 OR status=1 ORDER BY invoice_items.created_at ASC"
+  end
+
+  def group_items_by_status(bool)
+    items.where("status = #{bool}")
   end
 end
