52cee1a8b409b6f39d080ab65ebfa562d7be7117
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-01 22:58:23 -0500

    missed save

--- app/poros/contributor.rb
+++ app/poros/contributor.rb
@@ -1,8 +1,9 @@
 class Contributor
-  attr_reader :name, :commits, :id
+  attr_reader :name, :commits, :id #merged_pr symbol
   def initialize(data)
     @name = data[:login]
     @commits = data[:contributions]
     @id = data[:id]
+    # @merged_prs = data[:number]
   end
 end
