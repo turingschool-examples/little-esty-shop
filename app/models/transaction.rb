5358c8fcb05fc839d22ddc2de1d1bc53fcc5446b
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-02 15:06:09 -0600

    Refactor model testing

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
   validates :credit_card_number, presence: true, numericality: true
-  # validates :credit_card_expiration_date, presence: true #our data is empty!
   validates :result, presence: true
   validates :invoice_id, presence: true, numericality: true
   enum result: {'success' => 0, 'failed' => 1}
