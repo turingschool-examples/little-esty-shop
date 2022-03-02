9bd0189f4761cd2791fe60fb415b223c4126e9ac
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-02 12:37:44 -0600

    Refactor Admin Merchant Index Spec for SimpleCov coverage 100%

--- spec/features/merchants/admin/index_spec.rb
+++ spec/features/merchants/admin/index_spec.rb
@@ -15,35 +15,79 @@ RSpec.describe 'admin merchant index' do
         expect(page).to have_content(merchant_4.name)
         expect(page).to_not have_content(merchant_4.id)
     end
-        
-    it 'Admin Merchant Enable/Disable' do 
-        merchant_1 = create(:merchant)
-        merchant_2 = create(:merchant)
-        merchant_3 = create(:merchant)         
-        merchant_4 = create(:merchant, status: 1)
-    
-        visit '/admin/merchants/'
 
-        expect(page).to have_content(merchant_1.name)
-        expect(page).to have_content(merchant_2.name)
-        expect(page).to have_content(merchant_3.name)
-        expect(page).to have_button("Disable", count: 3)
+    describe 'Admin Merchant Enable/Disable' do 
+        it 'disabled merchants can be enabled with button click' do 
+            merchant_1 = create(:merchant)
+            merchant_2 = create(:merchant)
+            merchant_3 = create(:merchant)         
+            merchant_4 = create(:merchant, status: 1)
         
-        expect(page).to have_content(merchant_4.name)
-        expect(merchant_4.status).to eq("disabled")
-        expect(page).to have_button("Enable", count: 1)
+            visit '/admin/merchants/'
+            
+            within(".enabled") do 
+                expect(page).to have_content(merchant_1.name)
+                expect(page).to have_content(merchant_2.name)
+                expect(page).to have_content(merchant_3.name)
+                expect(page).to_not have_content(merchant_4.name)
+                expect(page).to have_button("Disable", count: 3)
+            end
+            within(".disabled") do 
+                expect(page).to have_content(merchant_4.name)
+                expect(page).to have_button("Enable", count: 1)
+            end
+            
+            click_button("Enable")
+            expect(current_path).to eq("/admin/merchants/")
+            
+            within(".enabled") do 
+                expect(page).to have_content(merchant_1.name)
+                expect(page).to have_content(merchant_2.name)
+                expect(page).to have_content(merchant_3.name)
+                expect(page).to have_content(merchant_4.name)
+                expect(page).to have_button("Disable", count: 4)
+            end
+            within(".disabled") do 
+                expect(page).to_not have_content(merchant_4.name)
+            end
+        end
+        it 'enabled merchants can be DISABLED with button click' do 
+            merchant_1 = create(:merchant)
+            merchant_2 = create(:merchant, status: 1)
+            merchant_3 = create(:merchant, status: 1)         
+            merchant_4 = create(:merchant, status: 1)
         
-        click_button("Enable")
-        expect(current_path).to eq("/admin/merchants/")
-        
-        
-        expect(page).to have_content(merchant_1.name)
-        expect(page).to have_content(merchant_2.name)
-        expect(page).to have_content(merchant_3.name)
-        expect(page).to have_content(merchant_4.name)
-        expect(page).to have_button("Disable", count: 4)
-        expect(page).to_not have_button("Enable")
+            visit '/admin/merchants/'
+            
+            within(".enabled") do 
+                expect(page).to have_content(merchant_1.name)
+                expect(page).to_not have_content(merchant_4.name)
+                expect(page).to have_button("Disable", count: 1)
+            end
+            within(".disabled") do 
+                expect(page).to have_content(merchant_2.name)
+                expect(page).to have_content(merchant_3.name)
+                expect(page).to have_content(merchant_4.name)
+                expect(page).to have_button("Enable", count: 3)
+            end
+            
+            click_button("Disable")
+            expect(current_path).to eq("/admin/merchants/")
+            
+            within(".enabled") do 
+                expect(page).to_not have_content(merchant_1.name)
+            end
+            within(".disabled") do 
+                expect(page).to have_content(merchant_1.name)
+                expect(page).to have_content(merchant_2.name)
+                expect(page).to have_content(merchant_3.name)
+                expect(page).to have_content(merchant_4.name)
+                expect(page).to have_button("Enable", count: 4)
+            end
+        end
+
     end
+        
 
     it 'Admin Merchants Grouped by Status' do 
         merchant_1 = create(:merchant)
