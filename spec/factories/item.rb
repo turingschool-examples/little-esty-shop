8971ec770e9b90001cf85a2a54a8148a993dfdea
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-02 13:08:32 -0600

    Add Status to Item FactoryBot to default 'disabled'

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:name) { |n| "Item#{n}" }
     sequence(:description) { |n| "Description #{n}" }
     sequence(:unit_price) { |n| n }
+    status { "disabled" }
     merchant 
   end
 end
