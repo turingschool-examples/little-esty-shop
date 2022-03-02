874a66a0081d3f7f58a6476be798d2291325e09e
Sam Smith <samlsmith424@gmail.com>
2022-03-02 13:44:56 -0800

    Test: InvoiceItem #change_status eix git add .! exit git add .!

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -14,6 +14,26 @@ RSpec.describe InvoiceItem, type: :model do
   it {should validate_numericality_of :invoice_id}
   it {should validate_numericality_of :unit_price}
   it {should validate_numericality_of :quantity}
+  
+  describe 'instance methods' do
+    it 'can change the status of an invoice_item' do
+      merchant_1 = create(:merchant)
+      item_1 = create(:item, merchant_id: merchant_1.id)
+      invoice_1 = create(:invoice)
+      invoice_item_1 = create(:invoice_item, item_id: item_1.id, invoice_id: invoice_1.id, status: "pending")
 
+      merchant_2 = create(:merchant)
+      item_2 = create(:item, merchant_id: merchant_2.id)
+      invoice_2 = create(:invoice)
+      invoice_item_2 = create(:invoice_item, item_id: item_2.id, invoice_id: invoice_2.id, status: "packaged")
 
+      invoice_item_1.change_status("packaged")
+      expect(invoice_item_1.status).to eq("packaged")
+      expect(invoice_item_1.status).to_not eq("pending")
+
+      invoice_item_2.change_status("shipped")
+      expect(invoice_item_2.status).to eq("shipped")
+      expect(invoice_item_2.status).to_not eq("packaged")
+    end
+  end
 end
