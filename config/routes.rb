7257c8831b4412c4a4d574a22dc11b1cbbab5e80
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-01 20:31:45 -0600

    heroku changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
   Rails.application.routes.draw do
 
   get '/merchants', to: 'merchants#index'
+  get '/', to: 'application#welcome'
     resources :merchants, only: [:show, :index, :update, :post], module: :merchant do
       resources :invoices, only: [:index, :show, :update]
       resources :dashboard, only: [:index]
       resources :items, only: [:index, :show, :edit, :update, :new, :create]
     end
-    
+
     namespace :admin, only: [:index, :show, :edit, :update, :new, :create], module: :admin do
       resources :dashboard, only: [:index]
       resources :merchants, only: [:index, :show, :edit, :update, :new, :create]
