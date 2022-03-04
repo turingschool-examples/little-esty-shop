6c76852dd4cb3328829830a79a1f4c718bb0f2c9
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-04 17:01:29 -0600

    -mupdate seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 BulkDiscount.destroy_all
 InvoiceItem.destroy_all
-Invoice.destroy_all
 Item.destroy_all
-Merchant.destroy_all
 Transaction.destroy_all
+Invoice.destroy_all
+Merchant.destroy_all
 Customer.destroy_all
 
 
