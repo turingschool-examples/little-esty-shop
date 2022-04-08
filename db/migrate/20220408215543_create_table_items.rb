3e0ea969475994f22875fc953aaaeaca3151e77f
Andrew Speth <speth.andrew@yahoo.com>
2022-04-08 15:59:21 -0600

    create initial migrations

---
+++ db/migrate/20220408215543_create_table_items.rb
@@ -0,0 +1,13 @@
+class CreateTableItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :table_items do |t|
+      t.bigint :id
+      t.string :name
+      t.string :description
+      t.integer :unit_price
+      t.references :merchant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20220408215840_create_table_transactions.rb
@@ -0,0 +1,13 @@
+class CreateTableTransactions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :table_transactions do |t|
+      t.bigint :id
+      t.references :invoice, foreign_key: true
+      t.string :credit_card_number
+      t.string :credit_card_expiration_date
+      t.integer :result
+
+      t.timestamps
+    end
+  end
+end
