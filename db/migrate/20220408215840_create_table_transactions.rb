e3c52adb836d95a4336b99d3757b52a719434b1c
Alex Psitos <apsitos08@gmail.com>
2022-04-08 19:08:47 -0400

    Redo Foreign keys

--- db/migrate/20220408215840_create_table_transactions.rb
+++ db/migrate/20220408215840_create_table_transactions.rb
@@ -1,7 +1,7 @@
 class CreateTableTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
-      # t.references :invoice, foreign_key: true
+      t.references :invoice, foreign_key: true
       t.string :credit_card_number
       t.string :credit_card_expiration_date
       t.integer :result
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_215840) do
+ActiveRecord::Schema.define(version: 2022_04_08_230709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,12 +28,18 @@ ActiveRecord::Schema.define(version: 2022_04_08_215840) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "item_id"
+    t.bigint "invoice_id"
+    t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
+    t.index ["item_id"], name: "index_invoice_items_on_item_id"
   end
 
   create_table "invoices", force: :cascade do |t|
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "customer_id"
+    t.index ["customer_id"], name: "index_invoices_on_customer_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -42,6 +48,8 @@ ActiveRecord::Schema.define(version: 2022_04_08_215840) do
     t.integer "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
   create_table "merchants", force: :cascade do |t|
@@ -56,6 +64,13 @@ ActiveRecord::Schema.define(version: 2022_04_08_215840) do
     t.integer "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "invoice_id"
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
+  add_foreign_key "invoice_items", "invoices"
+  add_foreign_key "invoice_items", "items"
+  add_foreign_key "invoices", "customers"
+  add_foreign_key "items", "merchants"
+  add_foreign_key "transactions", "invoices"
 end
