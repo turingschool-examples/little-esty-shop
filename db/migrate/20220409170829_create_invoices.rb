a785655567c0fb0b3cca8b0674d7939eabd16cca
Phillip Kamps <phillip.kamps@gmail.com>
2022-04-09 11:18:06 -0600

    Sets up gems/migrations

---
+++ db/migrate/20220409170829_create_invoices.rb
@@ -0,0 +1,9 @@
+class CreateInvoices < ActiveRecord::Migration[5.2]
+  def change
+    create_table :invoices do |t|
+      t.string :status
+      t.references :customer, foreign_key: true
+      t.timestamps
+    end
+  end
+end
