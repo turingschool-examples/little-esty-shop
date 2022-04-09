e4ee5ad7efb980e27424201472dbc077352d6d63
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-08 19:48:46 -0600

    ModelTest: Invoice attributes, validations, and relationships' '

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -18,4 +18,8 @@ RSpec.describe Customer do
     it { should validate_presence_of :first_name}
     it { should validate_presence_of :last_name}
   end
+
+  context 'relationships' do
+    it { should have_many :invoices }
+  end
 end
---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Invoice do
+  before :each do
+    @customer = Customer.create!(first_name: "Billy", last_name: "Jonson",
+                                 created_at: Time.parse('2012-03-27 14:54:09 UTC'),
+                                 updated_at: Time.parse('2012-03-27 14:54:09 UTC'))
+
+    @invoice = @customer.invoices.create!(status: "in progress",
+                                          created_at: Time.parse('2012-03-25 09:54:09 UTC'),
+                                          updated_at: Time.parse('2012-03-25 09:54:09 UTC'))
+  end
+
+  context 'readable attributes' do
+    it 'has a status' do
+      expect(@invoice.status).to eq("in progress")
+    end
+  end
+
+  context 'validations' do
+    it { should validate_presence_of :status}
+  end
+
+  context 'relationships' do
+    it { should belong_to :customer }
+  end
+end
