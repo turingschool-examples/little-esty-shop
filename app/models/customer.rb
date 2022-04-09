ea346aa4cb914e14ce136d4b7361a2551bc2f8a0
Phillip Kamps <phillip.kamps@gmail.com>
2022-04-09 12:20:02 -0600

    Model: adds has many relationship to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,6 @@ class Customer < ApplicationRecord
   validates :last_name, presence: true
   validates :created_at, presence: true
   validates :updated_at, presence: true
+
+  has_many :invoices
 end
