9987e4ffb4b1497800433a4c3825987bbf70a5ab
Alex Psitos <apsitos08@gmail.com>
2022-04-09 16:34:46 -0400

    item validations pass spec

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
-  has_many :invoices, through: :invoice_items, dependent: :destroy 
+  has_many :invoices, through: :invoice_items, dependent: :destroy
 
   enum status: { Disabled: 0, Enabled: 1 }
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :unit_price, presence: true
-
+  validates_presence_of :name
+  validates_presence_of :description, presence: true
+  validates_presence_of :unit_price, presence: true
+  validates_presence_of :created_at
+  validates_presence_of :updated_at
+  
 
 end
