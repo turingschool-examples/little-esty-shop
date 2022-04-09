804e9ba33e3bcde1d185ba24edafca3c3e2fe9b6
Cory Bethune <corybbethune@gmail.com>
2022-04-08 17:44:14 -0700

    Transaction model validations and associations test passes

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,9 @@
 class Transaction < ApplicationRecord
+  belongs_to :invoice
+
+  validates_presence_of :credit_card_number
+  validates_presence_of :credit_card_expiration_date
+  validates_presence_of :result
 
 
 end
