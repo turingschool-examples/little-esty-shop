30258c2e23db04e85ce0eb49fddd1ad70c3d07f4
Alex Psitos <apsitos08@gmail.com>
2022-04-09 16:37:14 -0400

    invoice item modelassociations pass spec

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,4 +3,8 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
 
   enum status: {packaged: 0, pending: 1, shipped: 2}
+  validates_presence_of :quantity
+  validates_presence_of :status
+  validates_presence_of :created_at
+  validates_presence_of :updated_at
 end
