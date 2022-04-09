b3dc1ef6ba5ca4a988fd0df22b924536d651ca9a
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-08 18:03:46 -0600

    Migration: add items table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_233327) do
+ActiveRecord::Schema.define(version: 2022_04_09_000321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,20 @@ ActiveRecord::Schema.define(version: 2022_04_08_233327) do
     t.datetime "updated_at"
   end
 
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "unit_price"
+    t.bigint "merchant_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["merchant_id"], name: "index_items_on_merchant_id"
+  end
+
   create_table "merchants", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  add_foreign_key "items", "merchants"
 end
