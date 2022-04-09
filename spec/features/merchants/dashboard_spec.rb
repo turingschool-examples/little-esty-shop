de3599864422bd528ce8781f10dccb4835255bce
Sherman Au <shermau13@gmail.com>
2022-04-09 16:05:59 -0600

    test: test links for items and invoices for merchants dashboard#index

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -2,12 +2,36 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant Dashboard' , type: :feature do
   before do
-    @merchant_1 = create :merchant
-    binding.pry
-    visit "/merchants/#{@merchant_1.id}/dashboard"
+    @merchant = create :merchant
+    @item1 = create :item, { merchant_id: @merchant.id}
+    @item2 = create :item, { merchant_id: @merchant.id}
+    @item3 = create :item, { merchant_id: @merchant.id}
+
+    @customer = create :customer
+    @invoice1 = create :invoice, { customer_id: @customer.id}
+    @invoice2 = create :invoice, { customer_id: @customer.id}
+
+
+    visit merchant_dashboard_index_path(@merchant)
   end
 
-  it' displays merchant name' do
-    expect(page).to have_content(@merchant_1.name)
+  describe 'display' do
+    it 'displays merchant name' do
+      expect(page).to have_content(@merchant.name)
+    end
+
+    it 'has a link to view all merchant items' do
+      expect(page).to have_link("Merchant Items")
+
+      click_link
+      expect(current_path).to eq(merchant_items_path(@merchant))
+    end
+
+    it 'has a link to view all merchant invoices' do
+      expect(page).to have_link("Merchant Invoices")
+
+      click_link
+      expect(current_path).to eq(merchant_invoices_path(@merchant))
+    end
   end
 end
