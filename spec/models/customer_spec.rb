1c40ce82ebfcd364631bb9ce51ac570b3454df67
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-08 18:57:39 -0600

    test pass for merchant relationships and validations

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,11 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Customer, type: :model do 
-
-  describe 'associations' do 
-	it {should have_many :invoices}
+RSpec.describe Customer, type: :model do
+  describe "associations" do
+    it { should have_many :invoices }
   end
-
-
-
-end 
+end
