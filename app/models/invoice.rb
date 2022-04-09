ae4dcce573051c090f453f037699e718e663206c
Alex Psitos <apsitos08@gmail.com>
2022-04-09 16:39:34 -0400

    invoice model associations pass spec

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,9 +2,11 @@ class Invoice < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items, dependent: :destroy
   belongs_to :customer
-  has_many :transactions, dependent: :destroy 
+  has_many :transactions, dependent: :destroy
 
   enum status: [:cancelled, 'in progress', :completed]
 
-  validates_presence_of :status, :customer_id
+  validates_presence_of :status
+  validates_presence_of :created_at
+  validates_presence_of :updated_at
 end
