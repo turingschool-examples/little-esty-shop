a44dee537d1c8d4888b9d66d154b081034c381a9
Susan Boyd <sueboyd922@gmail.com>
2022-04-08 20:39:12 -0400

    Test: Invoice Items 'add numericality validations'

--- spec/models/invoice_items_spec.rb
+++ spec/models/invoice_items_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe InvoiceItem, type: :model do
     it { should validate_presence_of(:quantity) }
     it { should validate_presence_of(:unit_price) }
     it { should validate_presence_of(:status) }
+    it { should validate_numericality_of(:quantity).is_greater_than(0) }
+    it { should validate_numericality_of(:unit_price).is_greater_than(0) }
   end
 
-end 
+end
