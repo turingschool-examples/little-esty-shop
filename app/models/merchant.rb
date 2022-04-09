676c8cef5705148cdf8b792abae0c902dd72e91a
Alex Psitos <apsitos08@gmail.com>
2022-04-09 16:31:51 -0400

    Validate merchant attributes

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,8 +3,11 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :items, dependent: :destroy
   has_many :invoices, through: :invoice_items, dependent: :destroy
   has_many :customers, through: :invoices, dependent: :destroy
-  has_many :transactions, through: :invoices, dependent: :destroy 
+  has_many :transactions, through: :invoices, dependent: :destroy
 
+  validates_presence_of :name
+  validates_presence_of :created_at
+  validates_presence_of :updated_at
   enum status: {enable: 0, disable: 1}
 
 end
