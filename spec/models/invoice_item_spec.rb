c2d641d7a848c92311c6ee50e52ea1935ed30000
Sherman Au <shermau13@gmail.com>
2022-04-08 22:02:46 -0600

    test:invoice item validations

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:quantity)}
+    it { should validate_numericality_of(:unit_price)}
+    it { should validate_presence_of(:status)}
+  end
 end
