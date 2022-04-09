d8dd9cb08028dab76b0d651dd9112629181ca299
Scott Sullivan <scottsullivanltd@gmail.com>
2022-04-08 19:44:30 -0600

    More Faker attributes added

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyString" }
-    unit_price { 1 }
+    name { Faker::Device.model_name }
+    description { Faker::Quote.yoda }
+    unit_price { Faker::Number.between(from: 1, to: 10) }
     enabled { 1 }
     merchant { nil }
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"  these are ppaceholders, delete in each
 end
