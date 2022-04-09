7cb803ac1d55c23217cbbafc2ba2afc3e42d94e0
Susan Boyd <sueboyd922@gmail.com>
2022-04-08 20:48:06 -0400

    Feat: Invoice Items: add validations and relationships to InvoiceItem model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,11 @@
 class InvoiceItem < ApplicationRecord
+  belongs_to :invoice
+  belongs_to :item
 
+  validates_presence_of :quantity, :unit_price, :status
+  validates :quantity, numericality: true
+  validates :quantity, numericality: {only_integer: true, greater_than: 0}
+  validates :unit_price, numericality: true
+  validates :unit_price, numericality: {only_integer: true, greater_than: 0}
 
 end
