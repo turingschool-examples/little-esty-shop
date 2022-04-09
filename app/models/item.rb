a47793b506448d79862881055aa0e8ff9d2349c2
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-09 14:50:21 -0600

    Model: create Item model

---
+++ app/models/item.rb
@@ -0,0 +1,11 @@
+class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :unit_price, presence: true
+  validates :created_at, presence: true
+  validates :updated_at, presence: true
+
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+  belongs_to :merchant
+end
