ec2db3d06c8e77b1959dd90318357910bb60d013
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-09 19:42:17 -0600

    Create merchant controller methods to specify view information

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,6 +1,16 @@
 class MerchantsController < ApplicationController
-
   def show
     @merchant = Merchant.find(params[:id])
+  end
+
+  def item_index
+    @merchant = Merchant.find(params[:id])
+    @items = Item.where("merchant_id = #{@merchant.id}")
+  end
+
+  def invoice_index
+    @merchant = Merchant.find(params[:id])
+    @invoices = Invoice.joins(:merchants)
+    .where("items.merchant_id = #{@merchant.id}")
   end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,8 +5,9 @@ class Invoice < ApplicationRecord
 
   has_many :invoice_items
   has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
   has_many :transactions
   belongs_to :customer
 
-  enum status: { "in progress" => "0", "completed" => "1", "cancelled" => "2" }
+  enum status: { "in_progress" => 0, "completed" => 1, "cancelled" => 2 }
 end
