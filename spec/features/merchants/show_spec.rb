642772c1c929302ecde2d869ed17ee8f26b5edaa
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-09 19:08:14 -0600

    Test: merchant dash has links to Item and Invoice index

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -12,11 +12,114 @@ describe 'merchant dashboard page' do
       created_at: Date.current,
       updated_at: Date.current
     )
+
+    @cup = @merchant_1.items.create!(
+      name: "Cup",
+      description: "What the **** is this thing?",
+      unit_price: 10000,
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+    @soccer = @merchant_1.items.create!(
+      name: "Soccer Ball",
+      description: "A ball of pure soccer.",
+      unit_price: 32000,
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+
+    @beer = @merchant_2.items.create!(
+      name: "Beer",
+      description: "Happiness <3",
+      unit_price: 100,
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+
+    @customer_1 = Customer.create!(
+      first_name: "Malcolm",
+      last_name: "Jordan",
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+    @customer_2 = Customer.create!(
+      first_name: "Jimmy",
+      last_name: "Felony",
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+
+    @invoice_1 = @customer_1.invoices.create!(
+      status: 0,
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+    @invoice_2 = @customer_1.invoices.create!(
+      status: 1,
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+    @invoice_3 = @customer_2.invoices.create!(
+      status: 0,
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+
+    @invoice_item_1 = InvoiceItem.create!(
+      item_id: @soccer.id,
+      invoice_id: @invoice_1.id,
+      quantity: 2,
+      unit_price: @soccer.unit_price * 2,
+      status: 0,
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+    @invoice_item_2 = InvoiceItem.create!(
+      item_id: @cup.id,
+      invoice_id: @invoice_1.id,
+      quantity: 4,
+      unit_price: @cup.unit_price * 4,
+      status: 0,
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+    @invoice_item_3 = InvoiceItem.create!(
+      item_id: @soccer.id,
+      invoice_id: @invoice_2.id,
+      quantity: 4,
+      unit_price: @soccer.unit_price * 4,
+      status: 2,
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+
+    @invoice_item_4 = InvoiceItem.create!(
+      item_id: @beer.id,
+      invoice_id: @invoice_3.id,
+      quantity: 2,
+      unit_price: @beer.unit_price * 2,
+      status: 0,
+      created_at: Date.current,
+      updated_at: Date.current
+    )
+    visit "/merchants/#{@merchant_1.id}/dashboard"
   end
 
   it 'displays the merchants name' do
-    visit "/merchants/#{@merchant_1.id}/dashboard"
-
     expect(page).to have_content("Store Store")
+  end
+
+  it 'has links to the merchant item index' do
+    click_link("Store Store's Items")
+    expect(page).to have_content("Cup")
+    expect(page).to have_content("Soccer Ball")
+    expect(page).not_to have_content("Beer")
+  end
+
+  it 'has links to the merchant invoice index' do
+    click_link("Invoices")
+    expect(page).to have_content("Invoice ##{@invoice_1.id}")
+    expect(page).to have_content("Invoice ##{@invoice_2.id}")
+    expect(page).not_to have_content("Invoice ##{@invoice_3.id}")
   end
 end
