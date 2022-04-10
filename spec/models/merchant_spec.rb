1701644c55e85086603c2823601bd0c05714ea1b
Sherman Au <shermau13@gmail.com>
2022-04-09 18:43:01 -0600

    refactor: test all relationships with merchants

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,6 +10,10 @@ require 'rails_helper'
  RSpec.describe Merchant, type: :model do
    describe 'relationships' do
     it { should have_many(:items)}
+    it { should have_many(:invoice_items)}
+    it { should have_many(:invoices)}
+    it { should have_many(:customers)}
+    it { should have_many(:transactions)}
    end
 
   describe 'validations' do
