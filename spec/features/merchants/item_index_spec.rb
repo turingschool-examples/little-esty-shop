609256e8851aa1a9776a92dd991835748bc25058
Andrew Speth <speth.andrew@yahoo.com>
2022-04-10 11:20:07 -0600

    test: merchant items index shows my items and not items from other merchants

---
+++ spec/features/merchants/item_index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+
+RSpec.describe 'Merchant Item Index Page' do
+
+  before do
+
+    @merch1 = Merchant.create!(name: 'Jeffs Gold Blooms', created_at: Time.now, updated_at: Time.now)
+    @merch2 = Merchant.create!(name: 'Miyazakis Dark Souls', created_at: Time.now, updated_at: Time.now)
+    @item1 = @merch1.items.create(name: "Golden Rose", description: "24k gold rose", unit_price: 100)
+    @item2 = @merch2.items.create(name: 'Dark Sole Shoes', description: "Dress shoes", unit_price: 200)
+    visit "/merchants/#{@merch1.id}/items"
+  end
+
+  describe 'As a Merchant' do
+
+    it 'items index page shows my items' do
+      expect(page).to have_content("Golden Rose")
+      expect(page).to_not have_content('Dark Sole Shoes')
+    end
+
+  end
+
+end
+
+
+# As a merchant,
+# When I visit my merchant items index page ("merchant/merchant_id/items")
+# I see a list of the names of all of my items
+# And I do not see items for any other merchant
