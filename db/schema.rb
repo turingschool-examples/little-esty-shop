3e7f245693ea2ad8f1c61f75d360fc993ec4aefc
Susan Boyd <sueboyd922@gmail.com>
2022-04-10 16:56:37 -0400

    Fix status to have default of 'enabled'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_201846) do
+ActiveRecord::Schema.define(version: 2022_04_10_205155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2022_04_10_201846) do
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "enabled"
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
