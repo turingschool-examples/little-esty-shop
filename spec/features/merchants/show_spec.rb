9616fc0ce6e285fc4dbd280671c6fcb1d37c3e3a
Kim G <xiaole.guo@yahoo.com>
2022-04-10 10:40:27 -0600

    add sad path to ticket 40

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.describe 'merchant show page' do 
 	before :each do 
 		@merchant1 = Merchant.create!(name: "Schroeder-Jerde")
+		@merchant2 = Merchant.create!(name: "Schroeder-Jerde_2")
 	end
 
 	it 'displays the merchant name' do 
 		visit "/merchants/#{@merchant1.id}/dashboard"
 
 		expect(page).to have_content(@merchant1.name)
+		expect(page).to_not have_content(@merchant2.name)
 	end
 
 end
