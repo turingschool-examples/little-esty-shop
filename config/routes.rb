7427287f4f987719a9d4919290706501ebdc486a
Luke Pascale <lpluke5@gmail.com>
2022-04-10 16:08:53 -0500

    Refactor: Add namespace for admin folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get '/admin', to: 'admin#index'
-get '/admin/merchants', to: 'admin_merchants#index'
-get '/admin/invoices', to: 'admin_invoices#index'
-
+namespace :admin do
+  get '/merchants', to: 'merchants#index'
+  get '/invoices', to: 'invoices#index'
+end
 
 
 end
