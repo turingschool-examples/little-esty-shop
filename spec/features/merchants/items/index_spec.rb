e449437b98294d1da28846d7e539ff49d282b419
Susan Boyd <sueboyd922@gmail.com>
2022-04-10 18:05:17 -0400

    Test: update test in Merchant Items Index spec to also test disable to enable

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'merchants items index page', type: :feature do
 
     @item1 = @merchant1.items.create!(name: "Item Ea Voluptatum", description: "A thing that does things", unit_price: 7654)
     @item2 = @merchant1.items.create!(name: "Item Quo Magnam", description: "A thing that does nothing", unit_price: 10099)
-    @item3 = @merchant1.items.create!(name: "Item Voluptatem Sint", description: "A thing that does everything", unit_price: 8790)
+    @item3 = @merchant1.items.create!(name: "Item Voluptatem Sint", description: "A thing that does everything", unit_price: 8790, status: "disabled")
     @item4 = @merchant2.items.create!(name: "Item Rerum Est", description: "A thing that barks", unit_price: 3455)
     @item5 = @merchant2.items.create!(name: "Item Itaque Consequatur", description: "A thing that makes noise", unit_price: 7900)
   end
@@ -23,7 +23,7 @@ RSpec.describe 'merchants items index page', type: :feature do
 
   it 'has the ability to enable or disable an item' do
     visit "merchants/#{@merchant1.id}/items"
-    # require "pry"; binding.pry
+
     within ".item-#{@item1.id}" do
       click_on "Enable"
     end
@@ -31,6 +31,17 @@ RSpec.describe 'merchants items index page', type: :feature do
     expect(Item.find(@item1.id).status).to eq("disabled")
     within ".item-#{@item1.id}" do
       expect(page).to have_button("Disable")
+    end
+
+    visit "merchants/#{@merchant1.id}/items"
+
+    within ".item-#{@item3.id}" do
+      click_on "Disable"
+    end
+    expect(current_path).to eq("/merchants/#{@merchant1.id}/items")
+    expect(Item.find(@item3.id).status).to eq("disabled")
+    within ".item-#{@item3.id}" do
+      expect(page).to have_button("Enable")
     end
   end
 end
