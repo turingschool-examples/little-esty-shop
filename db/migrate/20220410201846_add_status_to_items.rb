244b9afb1fcc2820358a0e51eb5f78c89918ed59
Susan Boyd <sueboyd922@gmail.com>
2022-04-10 16:56:57 -0400

    Test for Item status default of enabled

--- db/migrate/20220410201846_add_status_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToItems < ActiveRecord::Migration[5.2]
-  def change
-    add_column :items, :status, :string
-  end
-end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,6 +13,16 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of(:unit_price)}
     it { should validate_numericality_of(:unit_price).only_integer }
     it {should validate_numericality_of(:unit_price).is_greater_than(0)}
+    let(:status) {%i[enabled disabled]}
+  end
+
+  describe 'default' do
+    it 'has a default of enabled' do
+      merchant = Merchant.create!(name: "Sam's Toys")
+      item = merchant.items.create!(name: "Potato gun", description: "Shoots potatoes", unit_price: 1800)
+
+      expect(item.status).to eq("enabled")
+    end
   end
 
 end
