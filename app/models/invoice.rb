4a6a613abb187b4e106860c08e123467b5888304
Luke Pascale <lpluke5@gmail.com>
2022-04-10 17:56:50 -0500

    ModelFeat: Add associations to invoice, item and invoice_item

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,4 @@
 class Invoice < ApplicationRecord
+  has_many :items, through: :invoice_items
   enum status: ["in progress".to_sym, :completed, :cancelled]
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,3 @@
 class Item < ApplicationRecord
-
+  has_many :invoices, through: :invoice_items 
 end
