bfe549e887491eba5c4f4f860c9c587845fe7345
John Hennerich <john.hennerich@gmail.com>
2022-04-11 13:36:11 -0600

    updated db status columns to be integer vs string

--- db/migrate/20220410194534_create_transactions.rb
+++ db/migrate/20220410194534_create_transactions.rb
@@ -2,7 +2,7 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.bigint :credit_card_number
-      t.bigint :credit_card_expiration_date
+      t.string :credit_card_expiration_date
       t.string :result
       t.references :invoice, foreign_key: true
 
