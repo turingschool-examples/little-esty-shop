e3c3afa4438c505e734aec9a98d4d27256d59b4e
Joseph Downs <jos.w.downs@gmail.com>
2022-04-11 15:50:30 -0600

    Add path and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/admin', to: 'admin#index'
   get '/admin/merchants', to: 'admin#merchants'
   get '/admin/invoices', to: 'admin#invoices'
+  get '/admin/merchants/:id', to: 'admin#merchants_show'
 end
