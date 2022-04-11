b43b49021c32785d2f33589d92b0b320a6001053
Ian Ross <ianrossdrums@gmail.com>
2022-04-11 15:12:20 -0600

    feat: add merchant dashboard links to item and invoices index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :merchants do
     resources :dashboard, controller: 'merchant_dashboard', only: [:index]
+    resources :items, controller: 'merchant_items', only: [:index]
+    resources :invoices, controller: 'merchant_invoices', only: [:index]
   end
 end
