b67e44b8bf3406ad60a311bb53ecb7fca81d2f3c
Luke Pascale <lpluke5@gmail.com>
2022-04-11 15:43:49 -0500

    ModelRelTest: Test for the relations of all models

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe Customer, type: :model do
     it { should have_many(:invoice_items).through(:invoices)}
     it { should have_many(:items).through(:invoice_items)}
     it { should have_many(:transactions).through(:invoices)}
+    it { should have_many(:merchants).through(:items)}
   end
 end
