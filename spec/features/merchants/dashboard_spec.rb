137fdd74e8bcd0f6cae1c5c9bcf7e270e3b29a1c
Chris Romero <chrisleeromero1@gmail.com>
2022-04-11 14:20:40 -0600

    test: merchant dashboard has links to items and invoices indexes

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -1,16 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'merchant dashboard' do
-#   As a merchant,
-# When I visit my merchant dashboard (/merchant/merchant_id/dashboard)
-# Then I see the name of my merchant
+
   before(:each) do
     @merchant = Merchant.create!(name: 'Brylan')
+    visit "/merchants/#{@merchant.id}/dashboard"
   end
 
   it 'dispalys the name of the merchant' do
-    visit "/merchants/#{@merchant.id}/dashboard"
     expect(page).to have_content('Brylan')
+  end
+#   As a merchant,
+# When I visit my merchant dashboard
+# Then I see link to my merchant items index (/merchant/merchant_id/items)
+# And I see a link to my merchant invoices index (/merchant/merchant_id/invoices)
+  context 'links' do
+
+    it 'displays links to the merchant items index' do
+      click_link "Merchant's Items"
+      expect(current_path).to eq("/merchant/#{@merchant.id}/items")
+    end
+
+    it 'displays links to the merchant invoices index' do
+      click_link "Merchant's Invoices"
+      expect(current_path).to eq("/merchant/#{@merchant.id}/invoices")
+    end
   end
 
 end
