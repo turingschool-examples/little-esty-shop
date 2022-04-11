96eecb07c17b6bd06bb5f882a643bde5fab21f04
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-11 15:37:08 -0600

    refactor: call @merchant.name in MerchantItems index view

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -1,6 +1,6 @@
 class MerchantItemsController < ApplicationController
   def index
-    merchant = Merchant.find(params[:id])
-    @merchant_items = merchant.items
+    @merchant = Merchant.find(params[:id])
+    @merchant_items = @merchant.items
   end
 end
