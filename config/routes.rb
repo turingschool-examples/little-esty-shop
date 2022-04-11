9a66e1d42c00f8ab42d745b977a82b934240279d
Ian Ross <ianrossdrums@gmail.com>
2022-04-11 14:54:23 -0600

    feat: merchant dashboard crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :merchants do
+    resources :dashboard, controller: 'merchant_dashboard', only: [:index]
+  end
 end
