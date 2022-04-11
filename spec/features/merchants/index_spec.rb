830a770c11cf9f808faafaf3ee6b0fefed30fd3c
Ian Ross <ianrossdrums@gmail.com>
2022-04-11 15:11:36 -0600

    test: adds to merchant dashboard test

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe 'merchant dashboard' do
   it 'shows merchant name' do
     expect(page).to have_content(@merchant1.name)
   end
+  it 'has link to merchant item index' do
+    within("#index-buttons") do
+      click_button "Items Index"
+      expect(current_path).to eq(merchant_items_path(@merchant1))
+    end
+  end
+  it 'has link to merchant invoices index' do
+    click_button "Invoices Index"
+    expect(current_path).to eq(merchant_invoices_path(@merchant1))
+  end
 end
