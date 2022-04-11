40689046f63f983fcc8236b4fc992771f732a46c
Tyler Ross <tross020890@gmail.com>
2022-04-11 00:00:03 -0500

    Feat: item show displays name, description, price

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,6 @@
+class ItemsController < ApplicationController
+
+  def show
+    @item = Item.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/merchant/:id/items/:id', to: 'items#show'
 end
