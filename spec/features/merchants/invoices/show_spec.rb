742bff81d539389c1385299aca527b44a9ba3357
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-11 16:09:50 -0600

    Test: add test for user story 26, merchant invoice show page, seeing invoice attributes and customer name

---
+++ spec/features/merchants/invoices/show_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+
+RSpec.describe 'Merchant Invoice Show Page' do
+  before do
+    @merch1 = FactoryBot.create(:merchant)
+    @cust1 = FactoryBot.create(:customer)
+    @item1 = FactoryBot.create(:item, merchant_id: @merch1.id)
+    @item2 = FactoryBot.create(:item, merchant_id: @merch1.id)
+    @item3 = FactoryBot.create(:item, merchant_id: @merch1.id)
+    @invoice1 = FactoryBot.create(:invoice, customer_id: @cust1.id)
+    @invoice_item_1 = FactoryBot.create(:invoice_item, item_id: @item1.id, invoice_id: @invoice1.id)
+
+  end
+  describe 'As a visitor' do
+
+    it 'I see the invoices id, status, formated created at date, customer first and last name' do
+
+      visit "/merchants/#{@merch1.id}/invoices/#{@invoice1.id}"
+      expect(page).to have_content("ID: #{@invoice1.id}")
+      expect(page).to have_content("Status: #{@invoice1.status}")
+      expect(page).to have_content("Status: #{@invoice1.created_at}")
+      expect(page).to have_content("Customer Name: #{@cust1.first_name} #{@cust1.last_name}")
+
+
+    end
+
+
+
+  end
+end
