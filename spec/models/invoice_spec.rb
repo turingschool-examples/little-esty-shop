7bc25fc865a958e0c9c912c5ed3be23ce52557ea
Will M <93014155+wmedders21@users.noreply.github.com>
2022-04-10 20:57:32 -0400

    Update invoice_spec.rb

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,8 +1,5 @@
 # require 'rails_helper'
 #
 # RSpec.describe Invoice, type: :model do
-#   describe 'relationships' do
-#     # it { should belong_to(:transactions) }
-#     it { should have_many(:items).through(:invoice_items)}
-#   end
+#  
 # end
