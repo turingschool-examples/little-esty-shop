64a3682af0612002146a4164afa23d6dd807dba4
Luke Pascale <lpluke5@gmail.com>
2022-04-10 19:10:07 -0500

    ModelTest: Add model tests for relationships

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe 'relationships' do
-    # it { should belong_to(:invoice) }
-
+    it { should have_many(:invoices) }
+    it { should have_many(:invoice_items).through(:invoices)}
+    it { should have_many(:items).through(:invoice_items)}
+    it { should have_many(:transactions).through(:invoices)}
   end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,8 +1,7 @@
-# require 'rails_helper'
-#
-# RSpec.describe Transaction, type: :model do
-#   describe 'relationships' do
-#     it { should belong_to(:invoice) }
-#     it { should have_many(:items).through(:invoice_items)}
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe Transaction, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:invoice) }
+  end
+end
