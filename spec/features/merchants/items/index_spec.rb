6999de203bf1620ce4344ce862f2c0719778b13d
James Harkins <jh.harkins@protonmail.com>
2022-04-11 17:59:31 -0600

    FeatTest: create tests for Enabled Items and Disabled Items sections on merchants::items#index page

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -91,6 +91,53 @@ RSpec.describe 'merchant items index page' do
           expect(page).to have_button("Disable")
         end
       end
+
+      it 'i see two sections: one for Enabled Items and one for Disabled Items' do
+        merchant_1 = Merchant.create!(name: "Jim's Rare Guitars")
+        item_1 = merchant_1.items.create!(name: "1959 Gibson Les Paul",
+                                        description: "Tobacco Burst Finish, Rosewood Fingerboard",
+                                        unit_price: 25000000,
+                                        status: 1)
+        item_2 = merchant_1.items.create!(name: "1954 Fender Stratocaster",
+                                        description: "Seafoam Green Finish, Maple Fingerboard",
+                                        unit_price: 10000000)
+        item_3 = merchant_1.items.create!(name: "1968 Gibson SG",
+                                        description: "Cherry Red Finish, Rosewood Fingerboard",
+                                        unit_price: 400000,
+                                        status: 1)
+        item_4 = merchant_1.items.create!(name: "1982 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 600000)
+
+        visit "/merchants/#{merchant_1.id}/items"
+
+        within("#enabled_items") do
+          expect(page).to have_content("Enabled Items")
+          expect(page).to have_content(item_2.name)
+          within("#item-#{item_2.id}") do
+            expect(page).to have_button("Disable")
+          end
+
+          expect(page).to have_content(item_4.name)
+          within("#item-#{item_4.id}") do
+            expect(page).to have_button("Disable")
+          end
+        end
+
+        within("#disabled_items") do
+          expect(page).to have_content("Disabled Items")
+
+          expect(page).to have_content(item_1.name)
+          within("#item-#{item_1.id}") do
+            expect(page).to have_button("Enable")
+          end
+
+          expect(page).to have_content(item_3.name)
+          within("#item-#{item_3.id}") do
+            expect(page).to have_button("Enable")
+          end
+        end
+      end
     end
   end
 end
