a308204ebf9848ce4056a82361dc16a9d32335ca
Luke Pascale <lpluke5@gmail.com>
2022-04-10 19:21:25 -0500

    FeatTest: Write test to display all merchant names

--- spec/features/admin/merchant/index_spec.rb
+++ spec/features/admin/merchant/index_spec.rb
@@ -10,10 +10,17 @@ RSpec.describe "Admin Merchants Index" do
   end
 
   it 'displays all of the merchants' do
+    @merchant_1 = Merchant.create!(name: "Mollys")
+    @merchant_2 = Merchant.create!(name: "Berrys")
+    @merchant_3 = Merchant.create!(name: "Jimmys")
+
     visit '/admin/merchants'
 
     within(".index") do
-      expect(page).to have_content(@merchants)
+      expect(page).to have_content('Mollys')
+      expect(page).to have_content('Berrys')
+      expect(page).to have_content('Jimmys')
+      expect(page).to_not have_content('Willys')
     end
   end
 end
