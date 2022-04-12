92c669d4f1a85e429ef911f16e69d2a5e88b3e1c
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-12 09:47:21 -0600

    Model: Item validates status, create enum status: [:disabled, :enabled]

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,11 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true, numericality: true
+  validates :status, presence: true
 
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
   belongs_to :merchant
+
+  enum status: [:disabled, :enabled]
 end
