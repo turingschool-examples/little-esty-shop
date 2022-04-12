43aaf3356577b2792eefbd7bd877675f0512f651
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-12 16:32:50 -0400

    Feature: Merchant creation passes spec.

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -10,4 +10,15 @@ class Admin::MerchantsController < ApplicationController
 
   def new
   end
+
+  def create
+    Merchant.create!(merchant_params)
+    redirect_to("/admin/merchants")
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name, :enabled)
+  end
 end
