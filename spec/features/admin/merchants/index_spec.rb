e6f607bce0acd613a916056f7216005a811c41ac
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-12 15:58:23 -0400

    Feature: Admin Merchants Index links to individual show pages via name.

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -9,12 +9,36 @@ RSpec.describe 'Admin Merchants Index', type: :feature do
   end
   
   it 'Has all merchants' do
-    visit "admin/merchants"
+    visit "/admin/merchants"
 
     within("#merchants") do
-      expect(page).to have_content(@merchant_1.name)
-      expect(page).to have_content(@merchant_2.name)
-      expect(page).to have_content(@merchant_3.name)
+      within("#merchant-#{@merchant_1.id}") do
+        expect(page).to have_content(@merchant_1.name)
+        expect(page).to_not have_content(@merchant_2.name)
+        expect(page).to_not have_content(@merchant_3.name)
+      end
+      within("#merchant-#{@merchant_2.id}") do
+        expect(page).to_not have_content(@merchant_1.name)
+        expect(page).to have_content(@merchant_2.name)
+        expect(page).to_not have_content(@merchant_3.name)
+      end
+      within("#merchant-#{@merchant_3.id}") do
+        expect(page).to_not have_content(@merchant_1.name)
+        expect(page).to_not have_content(@merchant_2.name)
+        expect(page).to have_content(@merchant_3.name)
+      end
+    end
+  end
+
+  it 'Links from index to show page for each merchant' do
+    visit '/admin/merchants'
+  
+    within("#merchants") do
+      within("#merchant-#{@merchant_1.id}") do
+        expect(page).to have_link("#{@merchant_1.name}", href: "/admin/merchants/#{@merchant_1.id}")
+        expect(page).to_not have_link("#{@merchant_2.name}", href: "/admin/merchants/#{@merchant_2.id}")
+        expect(page).to_not have_link("#{@merchant_3.name}", href: "/admin/merchants/#{@merchant_3.id}")
+      end
     end
   end
 end
