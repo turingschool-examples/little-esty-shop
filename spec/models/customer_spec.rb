a540bb881e2447496344429ffe13e27f60310b8e
Will Medders <wmedders@gmail.com>
2022-04-11 22:08:27 -0400

    ModelTest: Customer #top_five

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -8,4 +8,45 @@ RSpec.describe Customer, type: :model do
     it { should have_many(:transactions).through(:invoices)}
     it { should have_many(:merchants).through(:items)}
   end
+
+  describe 'class methods' do
+    it '#top_five' do
+      bob = Customer.create!(first_name: "Bob", last_name: "Benson")
+      nate = Customer.create!(first_name: "Nate", last_name: "Chaffee")
+      barty = Customer.create!(first_name: "Barty", last_name: "Dasher")
+      zeke = Customer.create!(first_name: "Zeke", last_name: "Bristol")
+      flipper = Customer.create!(first_name: "Flipper", last_name: "McDaniel")
+      tildy = Customer.create!(first_name: "Tildy", last_name: "Lynch")
+      invoice_1 = bob.invoices.create!(status: 1)
+      invoice_2 = barty.invoices.create!(status: 1)
+      invoice_3 = nate.invoices.create!(status: 1)
+      invoice_4 = zeke.invoices.create!(status: 1)
+      invoice_5 = flipper.invoices.create!(status: 1)
+      invoice_6 = tildy.invoices.create!(status: 1)
+
+      t_1 = invoice_1.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_2 = invoice_1.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_3 = invoice_1.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_4 = invoice_1.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_5 = invoice_1.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_6 = invoice_1.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_7 = invoice_2.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_8 = invoice_2.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_9 = invoice_2.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_10 = invoice_2.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_11 = invoice_2.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_12 = invoice_3.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_13 = invoice_3.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_14 = invoice_3.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_15 = invoice_3.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_16 = invoice_4.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_17 = invoice_4.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_18 = invoice_4.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_19 = invoice_5.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_20 = invoice_5.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+      t_21 = invoice_6.transactions.create!(credit_card_number: "*", credit_card_expiration_date: "*", result: "successful")
+
+      expect(Customer.top_five).to eq([bob, barty, nate, zeke, flipper])
+    end
+  end
 end
