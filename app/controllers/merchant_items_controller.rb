fa1203c38642a8b8e54003c6c0d0d6dfb5ac3c6c
craig cardwell <wizeduvcc@gmail.com>
2022-04-11 19:55:08 -0600

    feat: add show page and edit form for merchant item show page

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -6,5 +6,29 @@ class MerchantItemsController < ApplicationController
 
   def show
     @merchant_item = Item.find(params[:id])
+    @merchant = @merchant_item.merchant
+  end
+
+  def edit
+    @merchant_item = Item.find(params[:id])
+    @merchant = @merchant_item.merchant
+  end
+
+  def update
+    merchant_item = Item.find(params[:id])
+    merchant = merchant_item.merchant
+
+    if merchant_item.update(merchant_items_params)
+      redirect_to "/merchants/#{merchant.id}/items/#{merchant_item.id}"
+      flash[:notice] = "Success: The information has been successfully updated"
+    else
+      redirect_to "/merchants/#{merchant.id}/items/#{merchant_item.id}/edit"
+    end
+  end
+
+  private
+
+  def merchant_items_params
+    params.permit(:name, :unit_price, :description)
   end
 end
