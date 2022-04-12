f356647605c4220c1758efcd0e4077c0ea8c890b
James Harkins <jh.harkins@protonmail.com>
2022-04-12 14:23:02 -0600

    FeatTest: create tests for merchants::items#index page 5 most popular items section

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -184,6 +184,92 @@ RSpec.describe 'merchant items index page' do
           expect(page).to have_content("1997 Fender Stratocaster Plus Deluxe")
         end
       end
+
+      it 'i see the names of the top 5 most popular items by total revenue,
+          which are also links to those items show pages, and i see those
+          items total revenue generated' do
+        merchant_1 = Merchant.create!(name: "Jim's Rare Guitars")
+        item_1 = merchant_1.items.create!(name: "1959 Gibson Les Paul",
+                                        description: "Tobacco Burst Finish, Rosewood Fingerboard",
+                                        unit_price: 25000000)
+        item_2 = merchant_1.items.create!(name: "1954 Fender Stratocaster",
+                                        description: "Seafoam Green Finish, Maple Fingerboard",
+                                        unit_price: 10000000)
+        item_3 = merchant_1.items.create!(name: "1968 Gibson SG",
+                                        description: "Cherry Red Finish, Rosewood Fingerboard",
+                                        unit_price: 400000)
+        item_4 = merchant_1.items.create!(name: "1984 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 600000)
+        item_5 = merchant_1.items.create!(name: "1991 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 900000)
+        item_6 = merchant_1.items.create!(name: "1993 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 700000)
+        item_7 = merchant_1.items.create!(name: "2004 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 200000)
+        item_8 = merchant_1.items.create!(name: "1997 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 100000)
+        item_9 = merchant_1.items.create!(name: "1996 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 100000)
+        item_10 = merchant_1.items.create!(name: "1975 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 400000)
+        customer_1 = Customer.create!(first_name: "Guthrie", last_name: "Govan")
+
+        invoice_1 = customer_1.invoices.create!(status: 1)
+        invoice_2 = customer_1.invoices.create!(status: 1)
+        invoice_3 = customer_1.invoices.create!(status: 1)
+        invoice_item_1 = InvoiceItem.create!(item: item_1, invoice: invoice_1, quantity: 1, unit_price: item_1.unit_price, status: 0)
+        invoice_item_2 = InvoiceItem.create!(item: item_9, invoice: invoice_1, quantity: 25, unit_price: item_9.unit_price, status: 0)
+        invoice_item_3 = InvoiceItem.create!(item: item_2, invoice: invoice_1, quantity: 1, unit_price: item_2.unit_price, status: 0)
+        invoice_item_4 = InvoiceItem.create!(item: item_4, invoice: invoice_1, quantity: 31, unit_price: item_4.unit_price, status: 0)
+        invoice_item_5 = InvoiceItem.create!(item: item_3, invoice: invoice_1, quantity: 35, unit_price: item_3.unit_price, status: 0)
+        invoice_item_6 = InvoiceItem.create!(item: item_5, invoice: invoice_1, quantity: 10, unit_price: item_5.unit_price, status: 0)
+        invoice_item_7 = InvoiceItem.create!(item: item_6, invoice: invoice_1, quantity: 17, unit_price: item_6.unit_price, status: 0)
+        invoice_item_8 = InvoiceItem.create!(item: item_8, invoice: invoice_1, quantity: 22, unit_price: item_8.unit_price, status: 0)
+        invoice_item_9 = InvoiceItem.create!(item: item_7, invoice: invoice_1, quantity: 1, unit_price: item_7.unit_price, status: 0)
+        invoice_item_10 = InvoiceItem.create!(item: item_10, invoice: invoice_1, quantity: 4, unit_price: item_10.unit_price, status: 0)
+        invoice_item_11 = InvoiceItem.create!(item: item_5, invoice: invoice_2, quantity: 10000, unit_price: item_5.unit_price, status: 0)
+        invoice_item_12 = InvoiceItem.create!(item: item_7, invoice: invoice_2, quantity: 10000, unit_price: item_7.unit_price, status: 0)
+        invoice_item_13 = InvoiceItem.create!(item: item_8, invoice: invoice_3, quantity: 10000, unit_price: item_8.unit_price, status: 0)
+        transaction_1 = invoice_1.transactions.create!(credit_card_number: 0000111122223333, result: "success")
+        transaction_1 = invoice_2.transactions.create!(credit_card_number: 0000111122223333, result: "failed")
+        transaction_2 = invoice_3.transactions.create!(credit_card_number: 0000111122223333, result: "success")
+
+        visit "/merchants/#{merchant_1.id}/items"
+
+        within "#most_popular_items" do
+          expect(item_8.name).to appear_before(item_1.name)
+          expect(item_1.name).to appear_before(item_4.name)
+          expect(item_4.name).to appear_before(item_3.name)
+          expect(item_3.name).to appear_before(item_6.name)
+        end
+
+        within "#popular_item-#{item_8.id}" do
+          expect(page).to have_content("Total Revenue: $1022000.00")
+        end
+
+        within "#popular_item-#{item_1.id}" do
+          expect(page).to have_content("Total Revenue: $250000.00")
+        end
+
+        within "#popular_item-#{item_4.id}" do
+          expect(page).to have_content("Total Revenue: $186000.00")
+        end
+
+        within "#popular_item-#{item_3.id}" do
+          expect(page).to have_content("Total Revenue: $140000.00")
+        end
+
+        within "#popular_item-#{item_8.id}" do
+          expect(page).to have_content("Total Revenue: $119000.00")
+        end
+      end
     end
   end
 end
