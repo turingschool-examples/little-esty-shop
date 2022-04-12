6c581b30d0a8729088377d170ae3c997a0b6d9de
Tyler Ross <tross020890@gmail.com>
2022-04-12 17:31:16 -0500

    Feat: link to update from show, contains update form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,17 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+    redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}"
   end
 end
---
+++ spec/features/items/update_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+describe "Merchants Items update", type: :feature do
+  before :each do
+    @merchant1 = create :merchant
+    @merchant2 = create :merchant
+    @item1 = create :item, {merchant_id: @merchant1.id}
+    @item2 = create :item, {merchant_id: @merchant1.id}
+    @item3 = create :item, {merchant_id: @merchant2.id}
+
+    @customer = create :customer
+    @invoice1 = create :invoice, {customer_id: @customer.id}
+    @invoice2 = create :invoice, {customer_id: @customer.id}
+    @invoice_item1 = create :invoice_item, {invoice_id: @invoice1.id, item_id: @item1.id, quantity: 1, unit_price: 22, status: 0}
+    @invoice_item2 = create :invoice_item, {invoice_id: @invoice1.id, item_id: @item2.id, quantity: 1, unit_price: 45, status: 1}
+    @invoice_item3 = create :invoice_item, {invoice_id: @invoice2.id, item_id: @item3.id, quantity: 1, unit_price: 72, status: 2}
+
+  end
+  it 'displays update link' do
+    visit "/merchants/#{@merchant1.id}/items/#{@item1.id}"
+    save_and_open_page
+    expect(page).to have_link("Update #{@item1.name}")
+    click_link("Update #{@item1.name}")
+
+    expect(page).to have_current_path("/merchants/#{@merchant1.id}/items/#{@item1.id}/edit")
+  end
+
+  it 'has form to update item' do
+    visit "/merchants/#{@merchant1.id}/items/#{@item1.id}"
+    click_link("Update #{@item1.name}")
+
+    expect(find('form')).to have_content('Name')
+    expect(find('form')).to have_content('Description')
+    expect(find('form')).to have_content('Unit price')
+    expect(find('form')).to have_content('Enabled')
+  end
+end
