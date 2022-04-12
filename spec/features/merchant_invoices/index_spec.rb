d63eba785dd692f5e834a05f79d426ec8d112a75
Chris Romero <chrisleeromero1@gmail.com>
2022-04-12 15:39:07 -0600

    test: displays all invoices that include at least one of my merchant's items

---
+++ spec/features/merchant_invoices/index_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant invoices index page' do
+
+  before(:each) do
+    @merchant = Merchant.create!(name: 'Brylan')
+    @item_1 = @merchant.items.create!(name: 'Bottle', unit_price: 10, description: 'H20')
+    @item_2 = @merchant.items.create!(name: 'Can', unit_price: 5, description: 'Soda')
+    @customer = Customer.create!(first_name: "Billy", last_name: "Jonson")
+    @invoice_1 = @customer.invoices.create(status: "in progress")
+    @invoice_2 = @customer.invoices.create(status: "in progress")
+    @invoice_item_1 = InvoiceItem.create(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 10, unit_price: 10, status: "packaged")
+    @invoice_item_2 = InvoiceItem.create(item_id: @item_1.id, invoice_id: @invoice_2.id, quantity: 10, unit_price: 10, status: "packaged")
+
+    @customer_2 = Customer.create!(first_name: "Illy", last_name: "Jonson")
+    @invoice_3 = @customer_2.invoices.create(status: "in progress")
+    @invoice_4 = @customer_2.invoices.create(status: "in progress")
+    @invoice_item_3 = InvoiceItem.create(item_id: @item_2.id, invoice_id: @invoice_3.id, quantity: 10, unit_price: 10, status: "packaged")
+    @invoice_item_4 = InvoiceItem.create(item_id: @item_2.id, invoice_id: @invoice_4.id, quantity: 10, unit_price: 10, status: "packaged")
+
+    @merchant_2 = Merchant.create!(name: 'Chris')
+    @item_3 = @merchant_2.items.create!(name: 'Ball', unit_price: 5, description: 'Fun')
+    @customer_3 = Customer.create!(first_name: "Illy", last_name: "Jonson")
+    @invoice_5 = @customer_2.invoices.create(status: "in progress")
+    @invoice_item_5 = InvoiceItem.create(item_id: @item_3.id, invoice_id: @invoice_5.id, quantity: 10, unit_price: 10, status: "packaged")
+
+    visit "/merchants/#{@merchant.id}/invoices"
+  end
+  # As a merchant,
+  # When I visit my merchant's invoices index (/merchants/merchant_id/invoices)
+  # Then I see all of the invoices that include at least one of my merchant's items
+  # And for each invoice I see its id
+
+  # And each id links to the merchant invoice show page
+  it "displays all invoices that include at least one of my merchant's items" do
+    expect(page).to have_content("Invoice: #{@invoice_1.id}")
+    expect(page).to have_content("Invoice: #{@invoice_2.id}")
+    expect(page).to have_content("Invoice: #{@invoice_3.id}")
+    expect(page).to have_content("Invoice: #{@invoice_4.id}")
+    expect(page).to_not have_content("Invoice: #{@invoice_5.id}")
+  end
+
+  it "each invoice_id links to the merchant invoice show page"
+end
