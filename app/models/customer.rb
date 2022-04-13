4e5314ffec387ffdebb9e23e3c1fd67bfbe4789c
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-12 22:43:52 -0600

    feat: customer.successful_transactions returns only successful transactions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
   has_many :invoices, dependent: :destroy
   has_many :transactions, through: :invoices
+
+  def successful_transactions
+    transactions.where(result: 'success')
+  end
 end
