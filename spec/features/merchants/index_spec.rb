283a24ee0b66030ff4a51ba4afc3af5b0a5b1134
Joseph Senecal <jasenecal2@gmail.com>
2022-04-13 14:10:47 -0600

    Fix: unintentional string literals and missing tag closers

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -23,9 +23,9 @@ RSpec.describe 'merchant dashboard' do
 
   it "merchant dashboard items ready to ship" do
     # comprehensively tests all possible item compartmentalizations
-    item_1 = @merchant_1.items.create!(name: "spoon", description: "stamped stainless steel, not deburred", unit_price: 80, status: 1, merchant_id: @merchant_1.id)
-    item_2 = @merchant_1.items.create!(name: "add_foreign_key", description: "stamped stainless steel, not deburred", unit_price: 90, status: 1, merchant_id: @merchant_1.id)
-    item_3 = @merchant_1.items.create!(name: "butter knife", description: "stamped stainless steel, not deburred", unit_price: 65, status: 1, merchant_id: @merchant_1.id)
+    item_1 = @merchant1.items.create!(name: "spoon", description: "stamped stainless steel, not deburred", unit_price: 80, status: 1, merchant_id: @merchant1.id)
+    item_2 = @merchant1.items.create!(name: "fork", description: "stamped stainless steel, not deburred", unit_price: 90, status: 1, merchant_id: @merchant1.id)
+    item_3 = @merchant1.items.create!(name: "butter knife", description: "stamped stainless steel, not deburred", unit_price: 65, status: 1, merchant_id: @merchant1.id)
     customer_1 = Customer.create(first_name: "Max", last_name: "Powers")
     customer_2 = Customer.create!(first_name: "Bob", last_name: "Ross")
     invoice_1 = customer_1.invoices.create!(status: 0)
@@ -37,20 +37,20 @@ RSpec.describe 'merchant dashboard' do
     visit merchant_dashboard_index_path(@merchant1)
 
     expect(page).to have_content("Items Ready to Ship")
-
-    expect(page).to have_content("item_2.name")
-    expect(page).not_to have_content("item_3.name") # Because item already shipped
+save_and_open_page
+    expect(page).to have_content(item_2.name)
+    expect(page).not_to have_content(item_3.name) # Because item already shipped
 
     within ("#unshipped_invoice_item_#{invoice_item_1.id}") do
       expect(page).to have_content(invoice_item_1.invoice_id)
-      expect(page).to have_content("item_1.name")
-      expect(page).to have_link("/invoices/#{invoice_1.id}")
+      expect(page).to have_content(item_1.name)
+      expect(page).to have_link(href: "/invoices/#{invoice_1.id}/")
     end
 
     within ("#unshipped_invoice_item_#{invoice_item_2.id}") do
       expect(page).to have_content(invoice_item_2.invoice_id)
-      expect(page).to have_content("item_2.name")
-      expect(page).to have_link("/invoices/#{invoice_2.id}")
+      expect(page).to have_content(item_2.name)
+      expect(page).to have_link(href: "/invoices/#{invoice_2.id}/")
     end
 
   end
