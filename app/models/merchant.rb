221db55f7d115a52e91186ebb291d865ad576561
Joseph Senecal <jasenecal2@gmail.com>
2022-04-13 14:09:22 -0600

    Add: Merchant#unshipped_invoice_items method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,9 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   has_many :items
+
+  def unshipped_invoice_items
+    # Item.find_by_sql("SELECT * FROM merchants INNER JOIN items ON merchants.id = items.merchant_id INNER JOIN invoice_items ON items.id = invoice_items.item_id WHERE merchant_id = ? ", id )
+    items.joins(:invoice_items).select('items.name, invoice_items.invoice_id, invoice_items.status, invoice_items.id AS invoice_item_id').where("invoice_items.status = 0 OR invoice_items.status = 1")
+  end
 end
