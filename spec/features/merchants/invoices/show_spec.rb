d40c4280124e53aad3b7485af6a7622d769174fe
Ian Ross <ianrossdrums@gmail.com>
2022-04-13 16:14:21 -0600

    test: add merchant invoice show test

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -15,30 +15,21 @@ RSpec.describe 'merchant dashboard' do
     InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, status: 1, quantity: 20, unit_price: 10)
     InvoiceItem.create!(invoice_id: @invoice2.id, item_id: @item1.id, status: 1, quantity: 30, unit_price: 10)
 
-    visit merchant_invoices_path(@merchant1)
+    visit merchant_invoice_path(@merchant1, @invoice1)
   end
 
   it 'has header' do
-    expect(page).to have_content("Invoices index")
+    expect(page).to have_content("Invoice #{@invoice1.id}")
   end
 
-  it 'shows all invoices related to merchant' do
-    within("#invoices") do
+  it 'shows all invoice attributes' do
+    within("#invoice") do
       expect(page).to have_content(@invoice1.id)
-      expect(page).to have_content(@invoice2.id)
+      expect(page).to have_content(@invoice1.status)
+      expect(page).to have_content(@invoice1.created_at.strftime("%A, %b %d, %Y"))
+      expect(page).to have_content(@customer1.first_name)
+      expect(page).to have_content(@customer1.last_name)
     end
   end
 
-  it 'has link to each invoice show page' do
-    within("#invoices") do
-      click_link @invoice1.id
-      expect(current_path).to eq(merchant_invoice_path(@merchant1, @invoice1))
-    end
-  end
-  it 'has link to seperate invoice' do
-    within("#invoices") do
-      click_link @invoice2.id
-      expect(current_path).to eq(merchant_invoice_path(@merchant1, @invoice2))
-    end
-  end
 end
