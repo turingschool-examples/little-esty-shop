0fa88d9da66757dd60c8c466ca5c349ac81624f9
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-12 20:38:37 -0600

    Create feature and model tests to display top 5 items on merchant show

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -50,17 +50,17 @@ describe 'merchant item index page' do
     )
 
     @invoice_1 = @customer_1.invoices.create!(
-      status: "1",
+      status: 1,
       created_at: Date.current,
       updated_at: Date.current
     )
     @invoice_2 = @customer_1.invoices.create!(
-      status: "2",
+      status: 2,
       created_at: Date.current,
       updated_at: Date.current
     )
     @invoice_3 = @customer_2.invoices.create!(
-      status: "0",
+      status: 0,
       created_at: Date.current,
       updated_at: Date.current
     )
@@ -68,9 +68,9 @@ describe 'merchant item index page' do
     @invoice_item_1 = InvoiceItem.create!(
       item_id: @soccer.id,
       invoice_id: @invoice_1.id,
-      quantity: 2,
+      quantity: 1,
       unit_price: @soccer.unit_price,
-      status: "1",
+      status: 1,
       created_at: Date.current,
       updated_at: Date.current
     )
@@ -79,7 +79,7 @@ describe 'merchant item index page' do
       invoice_id: @invoice_1.id,
       quantity: 50,
       unit_price: @cup.unit_price,
-      status: "1",
+      status: 1,
       created_at: Date.current,
       updated_at: Date.current
     )
@@ -88,7 +88,7 @@ describe 'merchant item index page' do
       invoice_id: @invoice_2.id,
       quantity: 500,
       unit_price: @soccer.unit_price,
-      status: "0",
+      status: 0,
       created_at: Date.current,
       updated_at: Date.current
     )
@@ -98,7 +98,7 @@ describe 'merchant item index page' do
       invoice_id: @invoice_3.id,
       quantity: 2,
       unit_price: @beer.unit_price * 2,
-      status: "0",
+      status: 0,
       created_at: Date.current,
       updated_at: Date.current
     )
@@ -109,7 +109,7 @@ describe 'merchant item index page' do
     )
     @transaction_2 = @invoice_2.transactions.create!(
       credit_card_number:"4654405418249632",
-      result: "failure"
+      result: "failed"
     )
 
     visit "/merchants/#{@merchant_1.id}/items"
@@ -126,91 +126,111 @@ describe 'merchant item index page' do
     expect(current_path).to eq("/merchants/#{@merchant_1.id}/items/new")
   end
 
-  xit 'displays the merchants top 5 items' do
-    nothing = @merchant_1.items.create!(
-      name: "nothing",
-      description: "Not anything",
-      unit_price: 9999999,
-      created_at: Date.current,
-      updated_at: Date.current
-    )
-    lotion = @merchant_1.items.create!(
-      name: "Lotion",
-      description: "Moisturize, kid.",
-      unit_price: 5000,
-      created_at: Date.current,
-      updated_at: Date.current
-    )
-    gnihton = @merchant_1.items.create!(
-      name: "gnihton",
-      description: "Seems to be word written backwards",
-      unit_price: 1111111,
-      created_at: Date.current,
-      updated_at: Date.current
-    )
-    something = @merchant_1.items.create!(
-      name: "something",
-      description: "Seems to be something",
-      unit_price: 37500,
-      created_at: Date.current,
-      updated_at: Date.current
-    )
+  describe 'displays the merchants top 5 items' do
+    before do
+      @nothing = @merchant_1.items.create!(
+        name: "nothing",
+        description: "Not anything",
+        unit_price: 999,
+        created_at: Date.current,
+        updated_at: Date.current
+      )
+      @lotion = @merchant_1.items.create!(
+        name: "lotion",
+        description: "Moisturize, kid.",
+        unit_price: 50000,
+        created_at: Date.current,
+        updated_at: Date.current
+      )
+      @gnihton = @merchant_1.items.create!(
+        name: "gnihton",
+        description: "Seems to be word written backwards",
+        unit_price: 1111111,
+        created_at: Date.current,
+        updated_at: Date.current
+      )
+      @something = @merchant_1.items.create!(
+        name: "something",
+        description: "Seems to be @something",
+        unit_price: 37500,
+        created_at: Date.current,
+        updated_at: Date.current
+      )
 
-    invoice_4 = @customer_1.invoices.create!(
-      status: "1",
-      created_at: Date.current,
-      updated_at: Date.current
-    )
+      @invoice_4 = @customer_1.invoices.create!(
+        status: 1,
+        created_at: Date.current,
+        updated_at: Date.current
+      )
 
-    invoice_item_5 = InvoiceItem.create!(
-      item_id: nothing.id,
-      invoice_id: invoice_4.id,
-      quantity: 999,
-      unit_price: nothing.unit_price,
-      status: "2",
-      created_at: Date.current,
-      updated_at: Date.current
-    )
-    invoice_item_6 = InvoiceItem.create!(
-      item_id: gnihton.id,
-      invoice_id: invoice_4.id,
-      quantity: 12,
-      unit_price: gnihton.unit_price,
-      status: "2",
-      created_at: Date.current,
-      updated_at: Date.current
-    )
-    invoice_item_7 = InvoiceItem.create!(
-      item_id: lotion.id,
-      invoice_id: invoice_4.id,
-      quantity: 5,
-      unit_price: lotion.unit_price,
-      status: "2",
-      created_at: Date.current,
-      updated_at: Date.current
-    )
-    invoice_item_8 = InvoiceItem.create!(
-      item_id: something.id,
-      invoice_id: invoice_4.id,
-      quantity: 5,
-      unit_price: something.unit_price,
-      status: "2",
-      created_at: Date.current,
-      updated_at: Date.current
-    )
-    transaction_3 = invoice_4.transactions.create!(
-      credit_card_number:"4654405418249632",
-      result: "success"
-    )
+      @invoice_item_5 = InvoiceItem.create!(
+        item_id: @nothing.id,
+        invoice_id: @invoice_4.id,
+        quantity: 90,
+        unit_price: @nothing.unit_price,
+        status: 2,
+        created_at: Date.current,
+        updated_at: Date.current
+      )
+      @invoice_item_6 = InvoiceItem.create!(
+        item_id: @gnihton.id,
+        invoice_id: @invoice_4.id,
+        quantity: 12,
+        unit_price: @gnihton.unit_price,
+        status: 2,
+        created_at: Date.current,
+        updated_at: Date.current
+      )
+      @invoice_item_7 = InvoiceItem.create!(
+        item_id: @lotion.id,
+        invoice_id: @invoice_4.id,
+        quantity: 5,
+        unit_price: @lotion.unit_price,
+        status: 2,
+        created_at: Date.current,
+        updated_at: Date.current
+      )
+      @invoice_item_8 = InvoiceItem.create!(
+        item_id: @something.id,
+        invoice_id: @invoice_4.id,
+        quantity: 5,
+        unit_price: @something.unit_price,
+        status: 2,
+        created_at: Date.current,
+        updated_at: Date.current
+      )
+      @transaction_3 = @invoice_4.transactions.create!(
+        credit_card_number:"4654405418249632",
+        result: "success"
+      )
 
-    within('#top_five_items') do
-      expect(page).to have_content("nothing")
-      expect(page).to have_content("lotion")
-      expect(page).to have_content("gnihton")
-      expect(page).to have_content("Cup")
-      expect(page).to have_content("something")
+      visit "/merchants/#{@merchant_1.id}/items"
+    end
 
-      expect(page).not_to have_content("Soccer Ball")
+    it 'displays them in order' do
+      within('#top_five_items') do
+        save_and_open_page
+        expect("gnihton").to appear_before("Cup")
+        expect("Cup").to appear_before("lotion")
+        expect("lotion").to appear_before("something")
+        expect("something").to appear_before("nothing")
+
+        expect(page).not_to have_content("Soccer Ball")
+      end
+    end
+
+    it "displays the item's total revenue" do
+      within("#top_item_#{@gnihton.id}") do
+        expect(page).to have_content("Total revenue generated: $133333.32")
+      end
+
+      within("#top_item_#{@cup.id}") do
+        expect(page).to have_content("Total revenue generated: $5000.0")
+      end
+
+      within("#top_item_#{@nothing.id}") do
+        expect(page).to have_content("Total revenue generated: $899.1")
+      end
     end
   end
 end
