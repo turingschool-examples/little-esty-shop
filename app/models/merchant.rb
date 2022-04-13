ccba9b31047269f93a0816c0b6d5b5ce443962d6
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-12 21:46:23 -0600

    Create restful Invoice Index Page

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,9 +2,9 @@ class Merchant < ApplicationRecord
   validates :name, presence: true
 
   has_many :items
+  has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
-  has_many :invoice_items, through: :items
   has_many :customers, through: :invoices
 
   def top_5_customers
