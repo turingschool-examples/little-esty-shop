8d8e268c81f6858f200119529bf34e2aac9c2400
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-12 22:42:26 -0600

    Test: customer.successful_transactions should retunr only successful transactions

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,24 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Customer do
   before :each do
-    @customer = Customer.create!(first_name: "Billy", last_name: "Jonson",
-                                 created_at: Time.parse('2012-03-27 14:54:09 UTC'),
-                                 updated_at: Time.parse('2012-03-27 14:54:09 UTC'))
+    @merchant = Merchant.create!(name: 'Brylan')
+
+    @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Ondricka')
+    @invoice_1 = @customer_1.invoices.create!(status: 'completed')
+    @item_1 = @merchant.items.create!(name: 'Pencil', unit_price: 5, description: 'Writes things.')
+    @item_1.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 3, unit_price: 4, status: 2)
+    @transaction_1 = @invoice_1.transactions.create!(credit_card_number: '4654405418249632', result: 'success')
+    @transaction_2 = @invoice_1.transactions.create!(credit_card_number: '4654405418249631', result: 'success')
+    @transaction_3 = @invoice_1.transactions.create!(credit_card_number: '4654405418249633', result: 'success')
+    @transaction_4 = @invoice_1.transactions.create!(credit_card_number: '4654405418249635', result: 'failed')
+    @transaction_5 = @invoice_1.transactions.create!(credit_card_number: '4654405418249635', result: 'failed')
+    @transaction_6 = @invoice_1.transactions.create!(credit_card_number: '4654405418249635', result: 'failed')
   end
 
   context 'readable attributes' do
     it 'has a name' do
-      expect(@customer.first_name).to eq("Billy")
-      expect(@customer.last_name).to eq("Jonson")
+      expect(@customer_1.first_name).to eq("Joey")
+      expect(@customer_1.last_name).to eq("Ondricka")
     end
   end
 
   context 'validations' do
-    it { should validate_presence_of :first_name}
-    it { should validate_presence_of :last_name}
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
   end
 
   context 'relationships' do
     it { should have_many :invoices }
+    it { should have_many(:transactions).through(:invoices) }
+  end
+
+  context 'instance methods' do
+    it '.successful_transactions should return all successful transactions' do
+      expect(@customer_1.successful_transactions).to eq([@transaction_1, @transaction_2, @transaction_3])
+    end
   end
 end
