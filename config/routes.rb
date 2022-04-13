fbfd2d7a09b9c47a5c2b0ec39efd10c956148e36
John Hennerich <john.hennerich@gmail.com>
2022-04-12 18:40:19 -0600

    Feat: User story 33 Merchant Item Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :merchants do
     resources :dashboard, controller: 'merchant_dashboard', only: [:index]
-    resources :items, controller: 'merchant_items'
+    resources :items, controller: 'merchant_items' 
     resources :invoices, controller: 'merchant_invoices', only: [:index]
   end
 
