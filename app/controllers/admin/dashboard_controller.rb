a99d06b7201d3aaea165246447b58647c0bd663c
Andrew Speth <speth.andrew@yahoo.com>
2022-04-12 19:43:15 -0600

    refactor to meet wireframe

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DashboardController < ApplicationController
     def index
-    @merchant = Merchant.find(params[:id])
+      @invoices = Invoice.all
+      @customers = Customer.all
     end
 end
