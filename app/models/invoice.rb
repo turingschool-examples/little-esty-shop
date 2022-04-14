217fb5d4565fe2195a8d54495b3a78b0720d4999
John Hennerich <john.hennerich@gmail.com>
2022-04-13 21:53:47 -0600

    Refactor: add has_many paths to models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,6 +6,7 @@ class Invoice < ApplicationRecord
   has_many :merchants, through: :items
   has_many :transactions
   has_many :invoice_items
+  has_many :items, through: :invoice_items
 
   enum status: {"in progress" => 0, "completed" => 1, "cancelled" => 2}
 
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,5 +4,15 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices, through: :items
   has_many :invoice_items, through: :invoices
-  
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
+
+  def most_popular_items
+    items.joins(invoices: :transactions)
+      .select('items.*, sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue')
+      .where('transactions.result = ?', 'success')
+      .group('items.id')
+      .order('total_revenue desc')
+      .limit(5)
+  end
 end
