6cbec8a08d7f219c911ae1863e7be0240adf5fcb
Chris Romero <chrisleeromero1@gmail.com>
2022-04-14 13:01:11 -0500

    test: merchant invoice show page should display total revenue

--- spec/features/merchant_invoices/show_spec.rb
+++ spec/features/merchant_invoices/show_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'merchant invoice show page' do
     @item_1 = @merchant.items.create!(name: 'Bottle', unit_price: 100, description: 'H20')
     @item_2 = @merchant.items.create!(name: 'Can', unit_price: 500, description: 'Soda')
     @item_3 = @merchant_2.items.create!(name: 'Jar', unit_price: 400, description: 'Jelly')
-    
+
     @customer = Customer.create!(first_name: "Billy", last_name: "Jonson")
     @invoice_1 = @customer.invoices.create!(status: "in progress", created_at: Time.parse("2022-04-12 09:54:09"))
     @invoice_1.invoice_items.create!(item_id: @item_1.id, status: "shipped", quantity: 8, unit_price: 100)
@@ -45,6 +45,14 @@ RSpec.describe 'merchant invoice show page' do
         expect(page).to have_content("Price per Item: $5.00")
         expect(page).to have_content("Status: packaged")
       end
+    end
+
+    it "displays the total revenue for all items on the invoice" do
+      @item_1.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 3, unit_price: 4, status: 2)
+      @item_2.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 3, unit_price: 4, status: 2)
+      @item_3.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 3, unit_price: 4, status: 2)
+      # @item_4.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 3, unit_price: 4, status: 2)
+      expect(page).to have_content("Total Revenue: 48")
     end
   end
 end
