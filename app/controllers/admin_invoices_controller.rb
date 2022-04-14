700be403b43cda00ce193fc8c49bf4a000c4fd8e
Joseph Downs <jos.w.downs@gmail.com>
2022-04-13 18:40:22 -0600

    Refactor to include more controllers

---
+++ app/controllers/admin_invoices_controller.rb
@@ -0,0 +1,8 @@
+class AdminInvoicesController < ApplicationController
+  def index
+  end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
 
   resources :merchants do
     resources :dashboard, controller: 'merchant_dashboard', only: [:index]
-    resources :items, controller: 'merchant_items' 
+    resources :items, controller: 'merchant_items'
     resources :invoices, controller: 'merchant_invoices', only: [:index]
   end
 
   get '/admin', to: 'admin#index'
-  get '/admin/merchants', to: 'admin#merchants'
-  get '/admin/invoices', to: 'admin#invoices'
-  get '/admin/merchants/:id', to: 'admin#merchants_show'
-  get '/admin/invoices/:id', to: 'admin#invoices_show'
+
+  get '/admin/merchants', to: 'admin_merchants#index'
+  get '/admin/merchants/:id', to: 'admin_merchants#show'
+  get '/admin/merchants/:id/edit', to: 'admin_merchants#edit'
+
+  get '/admin/invoices', to: 'admin_invoices#index'
+  get '/admin/invoices/:id', to: 'admin_invoices#show'
 end
