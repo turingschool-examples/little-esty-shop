02f17e0887a2be87325fe1da603f7fd224d47407
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-14 11:17:44 -0600

    test: write model test for #incomplete_invoices

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -29,11 +29,39 @@ RSpec.describe Invoice, type: :model do
       date = 	"2020-02-08 09:54:09 UTC".to_datetime
       cust1 = FactoryBot.create(:customer, first_name: "L'Ron", last_name: 'Hubbard')
       merch1 = FactoryBot.create(:merchant)
-      item1 = FactoryBot.create(:item, merchant_id: merch1.id)
+      item1 = FactoryBot.create(:item, id: 69, merchant_id: merch1.id)
       invoice1 = FactoryBot.create(:invoice, created_at: date, customer_id: cust1.id)
       invoice_item_1 = FactoryBot.create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id)
 
       expect(invoice1.customer_name).to eq("L'Ron Hubbard")
+    end
+  end
+
+  describe "incomplete invoices" do
+    it 'returns a list of all cancelled and in progress invoices' do
+        @merchant_1 = create(:merchant)
+        @item = create(:item, merchant_id: @merchant_1.id)
+
+        # customer_1, 6 succesful transactions and 1 failed
+        @customer_1 = create(:customer)
+        @invoice_1 = create(:invoice, status: 0, customer_id: @customer_1.id, created_at: "2012-03-25 09:54:09 UTC")
+        @invoice_item_1 = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_1.id, status: 2)
+        @transactions_list_1 = FactoryBot.create_list(:transaction, 6, invoice_id: @invoice_1.id, result: 0)
+        @failed_1 = create(:transaction, invoice_id: @invoice_1.id, result: 1)
+
+        # customer_2 5 succesful transactions
+        @customer_2 = create(:customer)
+        @invoice_2 = create(:invoice, status: 2, customer_id: @customer_2.id, created_at: "2012-03-25 09:54:09 UTC")
+        @invoice_item_2 = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_2.id, status: 1)
+        transactions_list_2 = FactoryBot.create_list(:transaction, 5, invoice_id: @invoice_2.id, result: 0)
+        #customer_3 4 succesful
+        @customer_3 = create(:customer)
+        @invoice_3 = create(:invoice, status: 1,customer_id: @customer_3.id, created_at: "2012-03-25 09:54:09 UTC")
+        @invoice_item_3 = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_3.id, status: 2)
+        @transactions_list_3 = FactoryBot.create_list(:transaction, 4, invoice_id: @invoice_3.id, result: 0)
+
+      expect(Invoice.incomplete_invoices).to eq([@invoice_1, @invoice_3])
+
     end
   end
 end
