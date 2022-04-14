6d81f8cb19f24f4cd541f58452b0de25312ecb62
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-13 19:35:13 -0600

    Feat/Refactor: add first iteration of drop down menu to the view, refactor test to align with the menu/form in the view

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -93,19 +93,17 @@ RSpec.describe 'Merchant Invoice Show Page' do
       cust1 = FactoryBot.create(:customer)
       item1 = FactoryBot.create(:item, unit_price: 75107, merchant_id: merch1.id)
       item2 = FactoryBot.create(:item, unit_price: 59999, merchant_id: merch1.id)
-      item3 = FactoryBot.create(:item, unit_price: 65734, merchant_id: merch1.id)
       invoice1 = FactoryBot.create(:invoice, customer_id: cust1.id)
       invoice_item_1 = FactoryBot.create(:invoice_item, item_id: item1.id, unit_price: item1.unit_price, quantity: 3, status: 0, invoice_id: invoice1.id)
       invoice_item_2 = FactoryBot.create(:invoice_item, item_id: item2.id, unit_price: item2.unit_price, quantity: 1, invoice_id: invoice1.id)
-      invoice_item_3 = FactoryBot.create(:invoice_item, item_id: item3.id, unit_price: item3.unit_price, quantity: 2, invoice_id: invoice1.id)
       visit "/merchants/#{merch1.id}/invoices/#{invoice1.id}"
-
+      save_and_open_page
       expect(page).to have_button("Update Item Status")
 
       within "#invoice_item-#{invoice_item_1.id}" do
-        expect(find_field('invoice_item_status').value).to eq('packaged')
+        expect(find_field('status').value).to eq('packaged')
         select 'packaged'
-        click_button 'Update Invoice'
+        click_button 'Update Item Status'
       end
       expect(current_path).to eq("/merchants/#{merch1.id}/invoices/#{invoice1.id}")
 
