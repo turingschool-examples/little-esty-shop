75d8095fa0f1f16eb6e6173f7d698af36ffbb5ea
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-14 07:59:07 -0600

    refactor: admin incomplete invoices test

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -69,52 +69,62 @@ RSpec.describe 'admin dashboad spec' do
     before (:each) do
       @merchant_1 = create(:merchant)
       @item = create(:item, merchant_id: @merchant_1.id)
+
       # customer_1, 6 succesful transactions and 1 failed
       @customer_1 = create(:customer)
-      @invoice_1 = create(:invoice, customer_id: @customer_1.id, created_at: "2012-03-25 09:54:09 UTC")
+      @invoice_1 = create(:invoice, status: 0, customer_id: @customer_1.id, created_at: "2012-03-25 09:54:09 UTC")
       @invoice_item_1 = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_1.id, status: 2)
       @transactions_list_1 = FactoryBot.create_list(:transaction, 6, invoice_id: @invoice_1.id, result: 0)
       @failed_1 = create(:transaction, invoice_id: @invoice_1.id, result: 1)
+
       # customer_2 5 succesful transactions
       @customer_2 = create(:customer)
       @invoice_2 = create(:invoice, status: 2, customer_id: @customer_2.id, created_at: "2012-03-25 09:54:09 UTC")
       @invoice_item_2 = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_2.id, status: 1)
       transactions_list_2 = FactoryBot.create_list(:transaction, 5, invoice_id: @invoice_2.id, result: 0)
+
       #customer_3 4 succesful
       @customer_3 = create(:customer)
       @invoice_3 = create(:invoice, status: 1,customer_id: @customer_3.id, created_at: "2012-03-25 09:54:09 UTC")
       @invoice_item_3 = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_3.id, status: 2)
       @transactions_list_3 = FactoryBot.create_list(:transaction, 4, invoice_id: @invoice_3.id, result: 0)
+
+      #customer_4 3 succesful
+      @customer_4 = create(:customer)
+      @invoice_4 = create(:invoice, status: 2, customer_id: @customer_4.id, created_at: "2012-03-25 09:54:09 UTC")
+      @invoice_item_4 = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_4.id, status: 2)
+      @transactions_list_4 = FactoryBot.create_list(:transaction, 3, invoice_id: @invoice_4.id, result: 0)
+
+      #customer_5 2 succesful
+      @customer_5 = create(:customer)
+      @invoice_5 = create(:invoice, status: 2, customer_id: @customer_5.id, created_at: "2012-03-25 09:54:09 UTC")
+      @transactions_list_5 = FactoryBot.create_list(:transaction, 2, invoice_id: @invoice_5.id, result: 0)
+
       #customer_6 1 succesful
       @customer_6 = create(:customer)
       @invoice_6 = create(:invoice, customer_id: @customer_6.id, created_at: "2012-03-25 09:54:09 UTC")
-
-      @invoice_item_6 = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_6.id, status: 2)
+      @invoice_item_6 = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_6.id, status: 1)
       transactions_list_6 = FactoryBot.create_list(:transaction, 1, invoice_id: @invoice_6.id, result: 0)
-      #customer_4 3 succesful
-      @customer_4 = create(:customer)
-      @invoice_4 = create(:invoice, customer_id: @customer_4.id, created_at: "2012-03-25 09:54:09 UTC")
-      @invoice_item_4 = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_4.id, status: 2)
-      @transactions_list_4 = FactoryBot.create_list(:transaction, 3, invoice_id: @invoice_4.id, result: 0)
-      #customer_5 2 succesful
-      @customer_5 = create(:customer)
-      @invoice_5 = create(:invoice, customer_id: @customer_5.id, created_at: "2012-03-25 09:54:09 UTC")
       @invoice_item_5 = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_5.id, status: 2)
-      @transactions_list_5 = FactoryBot.create_list(:transaction, 2, invoice_id: @invoice_5.id, result: 0)
-
         visit "/admin"
     end
 
     it 'has a section showing ids of invoices with unshipped items ' do
-      expect(page).to_not have_content(@invoice_2.id)
-      expect(page).to_not have_content(@invoice_3.id)
-      expect(page).to_not have_link(@invoice_1.id)
-      expect(page).to have_link(@invoice_4.id)
-      expect(page).to have_link(@invoice_5.id)
-      expect(page).to have_link(@invoice_6.id)
+      within("incomplete_invoices") do
+        expect(page).to have_content(@invoice_2.id)
+        expect(page).to have_content(@invoice_6.id)
+        expect(page).to_not have_content(@invoice_2.id)
+        expect(page).to_not have_content(@invoice_3.id)
+        expect(page).to_not have_content(@invoice_4.id)
+        expect(page).to_not have_content(@invoice_5.id)
+        expect(page).to_not have_link(@invoice_1.id)
+        expect(page).to have_link(@invoice_1.id)
+        expect(page).to have_link(@invoice_3.id)
+        expect(page).to have_link(@invoice_6.id)
+      end
 
-      click_link(@invoice_5.id)
-      expect(current_path).to eq("/admin/invoices/#{invoice_1.id}")
+      click_link(@invoice_6.id)
+      expect(current_path).to eq("/admin/invoices/#{@invoice_6.id}")
     end
   end
 end
