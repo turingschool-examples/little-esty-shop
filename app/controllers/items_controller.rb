190f0ba4b42c2286ac0ca28f81f8edba8ea1c231
Scott Sullivan <scottsullivanltd@gmail.com>
2022-04-14 11:40:52 -0600

    Test: invoice model total revenue method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,29 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+    if @item.update(item_params)
+      redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}"
+      flash[:notice] = "Item updated successfully"
+    else
+      redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}/edit"
+      flash[:alert] = "Error: #{error_message(@item.errors)}"
+    end
+  end
+
+  private
+  def item_params
+    params.permit(:id, :name, :unit_price, :description, :enabled)
+  end
+
 end
