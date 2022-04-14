291ff9b638787cb6937aa8998a0c5f14d07e9824
Phillip Kamps <56653844+phillipkamps@users.noreply.github.com>
2022-04-14 16:49:53 -0600

    Revert "Revert "Merchant index""

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,8 @@ class Item < ApplicationRecord
   belongs_to :merchant
 
   enum status: [:disabled, :enabled]
+
+  def to_dollars
+    unit_price.to_f / 100
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Item do
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:status) }
     it { should validate_numericality_of(:unit_price) }
   end
 end
