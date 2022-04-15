7e312f8207e69be8e1ee23945e44762edcd3d539
Joseph Senecal <jasenecal2@gmail.com>
2022-04-15 00:05:55 -0600

    Test: Item#best_sales_day

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'time'
 
 RSpec.describe Item, type: :model do
   describe "validations" do
@@ -9,5 +10,25 @@ RSpec.describe Item, type: :model do
   end
   describe "relationships" do
      it { should belong_to :merchant }
+  end
+  describe "instance methods" do
+    # April 15 and 12 tie, so it should return the 15th as the most recent date
+    it '#best_sales_date returns date of most ever sales' do
+      merchant_1 = create(:merchant)
+      item_1 = create(:item, merchant: merchant_1, status: 1)
+      customer_1 = create(:customer)
+      invoice_1 = create(:invoice, customer: customer_1, created_at: Time.parse("2019.04.12"))
+      invoice_2 = create(:invoice, customer: customer_1, created_at: Time.parse("2019.04.15"))
+      invoice_3 = create(:invoice, customer: customer_1, created_at: Time.parse("2019.04.15"))
+      invoice_4 = create(:invoice, customer: customer_1, created_at: Time.parse("2019.04.13"))
+      invoice_5 = create(:invoice, customer: customer_1, created_at: Time.parse("2019.04.12"))
+      invoice_item_1 = create(:invoice_item, item: item_1, invoice: invoice_1)
+      invoice_item_2 = create(:invoice_item, item: item_1, invoice: invoice_2)
+      invoice_item_3 = create(:invoice_item, item: item_1, invoice: invoice_3)
+      invoice_item_4 = create(:invoice_item, item: item_1, invoice: invoice_4)
+      invoice_item_5 = create(:invoice_item, item: item_1, invoice: invoice_5)
+
+      expect(item_1.best_sales_date).to eq("April 15, 2019")
+    end
   end
 end
