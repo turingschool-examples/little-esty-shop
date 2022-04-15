3cd1f63791713f2e7a4127947adb485d188aa48d
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-14 23:17:23 -0600

    Test: admin dashboard has link to admin merchants index

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -1,9 +1,17 @@
 require "rails_helper"
 
 RSpec.describe "admin dashboard" do
-  it "exists" do
+  before :all do
     visit "/admin"
+  end
 
+  it "exists" do
     expect(page).to have_content("Admin Dashboard")
+  end
+
+  it "has link to admin merchants index" do
+    click_link("Admin Merchants Index")
+
+    expect(current_path).to eq("/admin/merchants")
   end
 end
