93b85d0413806c94f6334d034093fd11e65f117b
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-14 20:26:36 -0600

    Add invoice total to invoice show page

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,4 +8,12 @@ class Invoice < ApplicationRecord
   belongs_to :customer
 
   enum status: { "in_progress" => 0, "completed" => 1, "cancelled" => 2 }
+
+  def invoice_total
+    total = invoice_items
+    .group(:invoice_id)
+    .sum("invoice_items.unit_price * invoice_items.quantity")
+
+    total[id]/100.0
+  end
 end
