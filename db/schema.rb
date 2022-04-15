d9df4185c4bb64fd4fd5314e0a9d89b5edc0f9f6
Andrew Speth <speth.andrew@yahoo.com>
2022-04-15 14:43:53 -0600

    added default status of disabled to merchants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_170647) do
+ActiveRecord::Schema.define(version: 2022_04_15_203938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2022_04_14_170647) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status"
+    t.integer "status", default: 1
   end
 
   create_table "transactions", force: :cascade do |t|
