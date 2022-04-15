8979d3b758a52849700279be1413d52468ab9a9b
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-14 19:30:07 -0600

    Test: add test for admin/merchant show page having a link to update the merchant, and that link takes user to admin/merchant/:id/edit

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -9,9 +9,20 @@ RSpec.describe "Admin Merchants Show Page" do
       merch1 = Merchant.create!(name: 'Cheese Company', created_at: Time.now, updated_at: Time.now, status: 1)
 
       visit "/admin/merchants/#{merch1.id}"
-      save_and_open_page
+      # save_and_open_page
       expect(page).to have_content("Merchant Name: Cheese Company")
+    end
 
+    it 'Has a link to a page to edit this merchants information, a form is filled with existing info' do
+      merch1 = Merchant.create!(name: 'Cheese Company', created_at: Time.now, updated_at: Time.now, status: 1)
+
+      visit "/admin/merchants/#{merch1.id}"
+
+      within "#update_this_merchant" do
+        expect(page).to have_link("Update This Merchant")
+      end
+      click_on "Update This Merchant"
+      expect(current_path).to eq("/admin/merchants/#{merch1.id}/edit")
     end
 
 
