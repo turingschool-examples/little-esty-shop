02ce5ed8e47aea4727365b1fedb3eb94be0a2a89
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-16 17:26:32 -0600

    test: admin invoice index test for all ids linked

---
+++ spec/features/admin/invoices/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Admin Invoice Page" do
+
+  describe 'As an Admin' do
+
+    before do
+      @customer_1 = create(:customer)
+      @invoice_1 = create(:invoice, customer_id: @customer_1.id, created_at: "2012-03-25 09:54:09 UTC")
+      @customer_2 = create(:customer)
+      @invoice_2 = create(:invoice, customer_id: @customer_2.id, created_at: "2012-03-25 09:54:09 UTC")
+      @customer_3 = create(:customer)
+      @invoice_3 = create(:invoice, customer_id: @customer_3.id, created_at: "2012-03-25 09:54:09 UTC")
+      visit '/admin/invoices'
+    end
+
+    it'shows a list of all invoice ids as links to their respective show page'do
+    expect(page).to have_content("#{@invoice_1.id}")
+    expect(page).to have_content("#{@invoice_2.id}")
+    expect(page).to have_content("#{@invoice_3.id}")
+    end
+    
+  end
+end
