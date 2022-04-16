53136cd0e490ed72a2bb4169fcda4ca7f983817f
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-04-16 16:09:31 -0700

    Test: Admin Invoice Show Page

---
+++ spec/features/admin/invoices/show_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+
+RSpec.describe 'Admin Invoice Show' do
+  describe 'Admin Invoice Show Page' do
+    it 'lists Invoice id, status, created at and customer name' do
+      customer = Customer.create!(first_name: "Billy", last_name: "Jonson")
+      invoice_1 = customer.invoices.create(status: "in progress")
+
+      visit "/admin/invoices"
+
+      click_link "#{invoice_1.id}"
+      expect(current_path).to eq("/admin/invoices/#{invoice_1.id}")
+      expect(page).to have_content("#{invoice_1.id}")
+      expect(page).to have_content("#{invoice_1.status}")
+      expect(page).to have_content("#{invoice_1.created_at.strftime("%A, %B %d, %Y")}")
+      expect(page).to have_content("Billy Jonson")
+
+    end
+
+    # it 'lists a link to update Invoice status using a select field' do
+    #   customer = Customer.create!(first_name: "Billy", last_name: "Jonson")
+    #   invoice_1 = customer.invoices.create(status: "in progress")
+    #
+    #   visit "/admin/invoices/#{invoice_1.id}"
+    #   # select_list_form("Cancelled")
+    #   assert_selector :status_id, 'Cancelled'
+    #   click_button 'Submit'
+    #
+    #
+    # end
+  end
+end
