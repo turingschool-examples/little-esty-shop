6b6a87b0c10e27dcc81ec0db7a7d455f883c2d39
John Hennerich <john.hennerich@gmail.com>
2022-04-17 09:38:03 -0600

    Test: add invoice item spec test to invoice show page

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -9,6 +9,25 @@ RSpec.describe 'Admin invoice show page' do
     @invoice1 = @customer1.invoices.create!(status: 0)
     @invoice2 = @customer1.invoices.create!(status: 1)
 
+
+    @invoiceitem1 = @invoice1.items.create!(name:	"X-wing",
+                          quantity: 1,
+                          unit_price:75107,
+                          status: 1
+                         )
+
+    @invoiceitem2 = @invoice1.items.create!(name:	"Tie-fighter",
+                          quantity: 1,
+                          unit_price:75000,
+                          status: 0
+                         )
+    @invoiceitem3 = @invoice2.items.create!(name:	"Lightsaber",
+                          quantity: 1,
+                          unit_price:7500,
+                          status: 1
+                         )
+
+
     visit "/admin/invoices/#{@invoice1.id}"
   end
 
@@ -23,6 +42,15 @@ RSpec.describe 'Admin invoice show page' do
       expect(page).to have_content(@invoice1.created_at.strftime("%A, %b %d, %Y"))
       expect(page).to have_content(@customer1.first_name)
       expect(page).to have_content(@customer1.last_name)
+    end
+  end
+
+  it 'shows all item attributes on the invoice' do
+    within("#invoice_item-#{@invoiceitem1.id}") do
+      expect(page).to have_content(@invoiceitem1.name)
+      expect(page).to have_content(@invoiceitem1.quantity)
+      expect(page).to have_content(@invoiceitem1.unit_price)
+      expect(page).to have_content(@invoiceitem1.status)
     end
   end
 
