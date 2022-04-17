53f2748674cdbd9a8cc1c7de4bcc76ab597211de
reuben <creubend@gmail.com>
2022-04-17 07:42:06 -0600

    test: Invoice show page test for id, status, date and customer name

---
+++ spec/features/admin/invoice/show_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Invoice Show Page" do
+    before :each do 
+        @invoice_1 = @customer_1.invoices.create!(
+            status: 1,
+            created_at: Date.new(2020,12,12),
+            updated_at: Date.current
+        )
+        @customer_1 = Customer.create!(
+            first_name: "Malcolm",
+            last_name: "Jordan",
+            created_at: Date.current,
+            updated_at: Date.current
+        )
+        visit "/admin/invoices/:#{@invoice_1.id}"
+    end
+
+    it "contains information about the invoice" do
+    expect(page).to have_content(@invoice_1.id)
+    expect(page).to have_content(@invoice_1.status)
+    expect(page).to have_content("Monday, July 18, 2019")
+    expect(page).to have_content("Malcolm Jordan")
+    end 
+
+end 
