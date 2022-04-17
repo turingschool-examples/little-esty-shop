04ee2cdcab1751ec64d18a2543cb545b189d463a
reuben <creubend@gmail.com>
2022-04-17 09:27:45 -0600

    :test: admin merchants test written

---
+++ spec/features/admin/merchants/index_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Admin Merchants Index" do 
+    before :each do
+      @merchant_1 = Merchant.create!(
+      name: "Wally World",
+      created_at: Date.current,
+      updated_at: Date.current)
+
+      @merchant_2 = Merchant.create!(
+      name: "Mako",
+      created_at: Date.current,
+      updated_at: Date.current)
+
+      @merchant_3 = Merchant.create!(
+      name: "Silly Stuff",
+      created_at: Date.current,
+      updated_at: Date.current)
+
+      @merchant_4 = Merchant.create!(
+      name: "The Store",
+      created_at: Date.current,
+      updated_at: Date.current)
+    
+
+        visit '/admin/merchants'
+    end
+
+    it 'has a list of merchants'  do 
+        expect(page).to have_content("Wally World")
+        expect(page).to have_content("The Store")
+        expect(page).to have_content("Soccerball")
+    end
+end
