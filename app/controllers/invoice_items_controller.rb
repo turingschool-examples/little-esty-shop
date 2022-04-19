143c50a4ff6a309f1b39f9d7734b3f9ec3277036
Sherman Au <shermau13@gmail.com>
2022-04-18 21:11:35 -0600

    refactor: change params path arguments

--- app/controllers/invoice_items_controller.rb
+++ app/controllers/invoice_items_controller.rb
@@ -2,13 +2,12 @@ class InvoiceItemsController < ApplicationController
   def update
     invoice_item = InvoiceItem.find(params[:id])
     invoice_item.update(invoice_item_params)
-    # redirect_to merchant_invoice_item_path(params[:merchant_id], params[:id])
-    redirect_to "/merchants/#{merchant.id}/invoice/#{invoice.id}"
+    redirect_to merchant_invoice_path(params[:merchant_id], invoice_item.invoice.id)
   end
 
   private
 
   def invoice_item_params
-    params.require(:invoice_item).permit(:id, :status)
+    params.require(:invoice_item).permit(:status)
   end
 end
