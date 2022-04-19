ce569556919da64da146d2d84274b903f20136f4
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-19 14:14:30 -0600

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,10 @@ Rails.application.routes.draw do
   get "/admin", to: "admin/dashboard#index"
   get "/admin/merchants", to: "admin/merchants#index"
   get "/admin/invoices", to: "admin/invoices#index"
-<<<<<<< HEAD
+
   get "/admin/merchants/new", to: "admin/merchants#new"
-=======
   get "/admin/invoices/:id", to:"admin/invoices#show"
   get "/admin/merchants/:id", to:"admin/merchants#show"
   patch "/admin/merchants/:id", to: "admin/merchants#update"
   get "admin/merchants/:id/edit", to: "admin/merchants#edit"
->>>>>>> main
 end
