fbb4d615d04add587193a03bc43cdd5868dc46c5
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-20 00:30:47 -0600

    Add ', :vcr' to all feature specs

--- spec/features/admin/merchants/new_spec.rb
+++ spec/features/admin/merchants/new_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "Admin Merchants New Page" do
-  it "has form to create new merchant" do
+  it "has form to create new merchant", :vcr do
     visit "/admin/merchants"
     expect(page).to_not have_content("Tummies McRubme")
 
     visit "/admin/merchants/new"
-  
+
     fill_in('Name', with: 'Tummies McRubme')
     click_button("Submit")
 
--- spec/features/invoices/index_spec.rb
+++ spec/features/invoices/index_spec.rb
@@ -4,75 +4,48 @@ describe 'merchant invoice index page' do
   before do
     @merchant_1 = Merchant.create!(
       name: "Store Store",
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @merchant_2 = Merchant.create!(
       name: "Erots",
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @cup = @merchant_1.items.create!(
       name: "Cup",
       description: "What the **** is this thing?",
       unit_price: 10000,
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @soccer = @merchant_1.items.create!(
       name: "Soccer Ball",
       description: "A ball of pure soccer.",
       unit_price: 32000,
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @beer = @merchant_2.items.create!(
       name: "Beer",
       description: "Happiness <3",
       unit_price: 100,
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @customer_1 = Customer.create!(
       first_name: "Malcolm",
       last_name: "Jordan",
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @customer_2 = Customer.create!(
       first_name: "Jimmy",
       last_name: "Felony",
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @invoice_1 = @customer_1.invoices.create!(
       status: 1,
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @invoice_2 = @customer_1.invoices.create!(
       status: 2,
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @invoice_3 = @customer_2.invoices.create!(
       status: 0,
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @invoice_item_1 = InvoiceItem.create!(
       item_id: @soccer.id,
       invoice_id: @invoice_1.id,
       quantity: 1,
       unit_price: @soccer.unit_price,
       status: 1,
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @invoice_item_2 = InvoiceItem.create!(
       item_id: @cup.id,
@@ -80,8 +53,6 @@ describe 'merchant invoice index page' do
       quantity: 50,
       unit_price: @cup.unit_price,
       status: 1,
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @invoice_item_3 = InvoiceItem.create!(
       item_id: @soccer.id,
@@ -89,20 +60,14 @@ describe 'merchant invoice index page' do
       quantity: 500,
       unit_price: @soccer.unit_price,
       status: 0,
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @invoice_item_4 = InvoiceItem.create!(
       item_id: @beer.id,
       invoice_id: @invoice_3.id,
       quantity: 2,
       unit_price: @beer.unit_price * 2,
       status: 0,
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @transaction_1 = @invoice_1.transactions.create!(
       credit_card_number:"4654405418249632",
       result: "success"
@@ -115,14 +80,14 @@ describe 'merchant invoice index page' do
     visit "/merchants/#{@merchant_1.id}/invoices"
   end
 
-  it 'displays invoices including an item from the merchant' do
+  it 'displays invoices including an item from the merchant', :vcr do
     expect(page).to have_content("Invoice ##{@invoice_1.id}")
     expect(page).to have_content("Invoice ##{@invoice_2.id}")
 
     expect(page).not_to have_content("Invoice ##{@invoice_3.id}")
   end
 
-  it 'has links to invoice show pages' do
+  it 'has links to invoice show pages', :vcr do
     click_link("#{@invoice_1.id}")
 
     expect(current_path).to eq("/merchants/#{@merchant_1.id}/invoices/#{@invoice_1.id}")
