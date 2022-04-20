55efb996c0fbb511a68cf0f55fdde083a87e3802
Phillip Kamps <phillip.kamps@gmail.com>
2022-04-19 18:03:42 -0600

    Adds partial for invoice item info

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -8,5 +8,7 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
 
+  has_one :merchant, through: :item
+
   enum status: {"pending" => 0, "packaged" => 1, "shipped" => 2}
 end
