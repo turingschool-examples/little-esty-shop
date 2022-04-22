83dbb51488bef03389e8a5a72d124a2fa95205cc
Sherman Au <shermau13@gmail.com>
2022-04-22 01:38:01 -0600

    disable github api

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -1,9 +1,9 @@
-class BaseService
-  def self.conn(url)
-    Faraday.new(url)
-  end
+# class BaseService
+#   def self.conn(url)
+#     Faraday.new(url)
+#   end
 
-  def self.get_json(response)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-end
+#   def self.get_json(response)
+#     JSON.parse(response.body, symbolize_names: true)
+#   end
+# end
--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -1,10 +1,10 @@
-require "rails_helper"
+# require "rails_helper"
 
-RSpec.describe GithubService do
-  describe "API repo endpoint" do
-    it "gets repo data from github endpoint", :vcr do
-      json = GithubService.get_repo_data
-      expect(json).to have_key(:name)
-    end
-  end
-end
+# RSpec.describe GithubService do
+#   describe "API repo endpoint" do
+#     it "gets repo data from github endpoint", :vcr do
+#       json = GithubService.get_repo_data
+#       expect(json).to have_key(:name)
+#     end
+#   end
+# end
