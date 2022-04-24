ccdd0942dde3210bbd51dd404e0f905910a8175f
Phillip Kamps <phillip.kamps@gmail.com>
2022-04-24 14:38:34 -0600

    Completes bulk discount show page, story 4 complete

---
+++ spec/features/bulk_discounts/show_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "Bulk discounts show page" do
+  let!(:merchant) { Merchant.create!(name: "Rebekah's Circuits") }
+  let!(:bulk_discount_a) { BulkDiscount.create!(merchant: merchant, percentage: 0.10, quantity: 10000) }
+  let!(:bulk_discount_b) { BulkDiscount.create!(merchant: merchant, percentage: 0.20, quantity: 40000) }
+
+  before :each do
+    visit "/merchants/#{merchant.id}/bulk_discounts/#{bulk_discount_a.id}"
+  end
+
+  it "shows bulk discount info" do
+    expect(page).to have_content("Percentage: 0.1")
+    expect(page).to have_content("Quantity: 10000")
+
+    expect(page).to_not have_content("Percentage: 0.2")
+    expect(page).to_not have_content("Quantity: 40000")
+  end
+end
