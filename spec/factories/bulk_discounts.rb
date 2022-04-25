4a482bc807aba105f7255f61931d18c3ebe6d20c
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-25 15:28:56 -0400

    Tests: Create Bulk Discount factory and before :each block for merchant bulk discount index.

---
+++ spec/factories/bulk_discounts.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :bulk_discount do
+    percentage_discount { Faker::Number.between {from: 1, to: 99} }
+    quantity_threshold { Faker::Number.between {from: 1, to: 5} }
+    merchant { nil }
+  end
+end
