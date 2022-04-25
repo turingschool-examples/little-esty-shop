c0f24c4f5f9fec2bfc454b560a0ab98985a80032
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-25 15:44:43 -0400

    Feature: Bulk Discount Index route complete.

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -23,20 +23,18 @@ RSpec.describe 'Merchant Bulk Discount Index', type: :feature do
   it 'Will display all bulk discounts, including links to individual show pages' do
     visit merchant_dashboard_index_path(@merchant.id)
 
-    within '#bulk-discounts' do
-      expect(page).to have_link("View Bulk Discounts", to: merchant_bulk_discounts_index_path(@merchant.id))
-    end
+    expect(page).to have_link("View Bulk Discounts", href: merchant_bulk_discounts_path(@merchant.id))
 
     click_link("View Bulk Discounts")
 
-    expect(current_path).to eq(merchant_bulk_discounts_index_path(@merchant.id))
+    expect(current_path).to eq(merchant_bulk_discounts_path(@merchant.id))
 
     within "#discounts" do
       within "#discount-#{@bulk_discount1.id}" do
         expect(page).to have_content("Discount #{@bulk_discount1.id}: #{@bulk_discount1.percentage_discount}% off if you purchase #{@bulk_discount1.quantity_threshold} or more of an item.")
         expect(page).to_not have_content("Discount #{@bulk_discount2.id}: #{@bulk_discount2.percentage_discount}% off if you purchase #{@bulk_discount2.quantity_threshold} or more of an item.")
         expect(page).to_not have_content("Discount #{@bulk_discount3.id}: #{@bulk_discount3.percentage_discount}% off if you purchase #{@bulk_discount3.quantity_threshold} or more of an item.")
-        expect(page).to have_link("View Details", to: merchant_bulk_discounts_show_path(@bulk_discount1.id))
+        expect(page).to have_link("View Details", href: merchant_bulk_discounts_show_path(@bulk_discount1.id))
       end
       within "#discount-#{@bulk_discount2.id}" do
         expect(page).to_not have_content("Discount #{@bulk_discount1.id}: #{@bulk_discount1.percentage_discount}% off if you purchase #{@bulk_discount1.quantity_threshold} or more of an item.")
