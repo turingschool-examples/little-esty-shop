ab1ca62ede3b1eac1609f68a172801af28a4d6f0
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-26 13:08:28 -0400

    Test: Edit merchant bulk discount spec created.

--- spec/features/bulk_discounts/edit_spec.rb
+++ spec/features/bulk_discounts/edit_spec.rb
@@ -36,5 +36,7 @@ RSpec.describe 'Edit Bulk Discount Page', type: :feature do
     expect(page).to have_content("Minimum # of Items: #{@bulk_discount1.quantity_threshold}")
     expect(@bulk_discount1.percentage_discount).to eq(15)
     expect(@bulk_discount1.quantity_threshold).to eq(3)
+
+    expect(page).to have_link("Edit Discount", edit_merchant_bulk_discount_path(@merchant1.id, @bulk_discount1.id))
   end
 end
