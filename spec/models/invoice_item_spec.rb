50e5f7fbcf59f9cbe7d697f448baf702cddc82cd
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-26 15:30:58 -0400

    Test: Create test for discount method (backwards, but was just isolating functionality.)

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -16,6 +16,19 @@ RSpec.describe InvoiceItem, type: :model do
     it { should belong_to(:item) }
   end
 
+  it 'Finds highest applicable discount for an invoice item' do
+    merchant = create :merchant
+    customer = create :customer
+    invoice = create :invoice, {customer: customer}
+    item = create :item, {unit_price: 10, merchant: merchant}
+    invoice_item = create :invoice_item, {item: item, invoice: invoice, quantity: 4}
+    bulk_discount1 = create :bulk_discount, {quantity_threshold: 3, percentage_discount: 10, merchant: merchant}
+    bulk_discount2 = create :bulk_discount, {quantity_threshold: 5, percentage_discount: 20, merchant: merchant}
+
+    expected = bulk_discount1
+    expect(invoice_item.discount).to eq(expected)
+  end
+
   it 'Returns discount percentage for an invoice item if any discounts are applicable' do
     merchant = create :merchant
     customer = create :customer
