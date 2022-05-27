625923e952dfa7c75c063f699021e305684b519d
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-05-27 19:44:03 -0400

    feat: add merchant csv_load task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_27_232941) do
+ActiveRecord::Schema.define(version: 2022_05_27_234306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2022_05_27_232941) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "merchants", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
