6b355407606e22893b8311fd4a5ba6f6d527db74
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-28 16:06:07 -0600

    Add validations for invoice item

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,8 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
+
+  validates_numericality_of :quantity
+  validates_numericality_of :unit_price
+  enum status:["pending", "packaged", "shipped"]
 end
