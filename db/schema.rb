8cc03df7b75a27644fd4c82065108bdbef09b478
Clay Ash <claycourtneyash@gmail.com>
2022-05-28 13:58:15 -0600

    Create invoice table and add customers to invoices table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_161118) do
+ActiveRecord::Schema.define(version: 2022_05_28_194110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2022_05_28_161118) do
     t.string "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "customer_id"
+    t.index ["customer_id"], name: "index_invoices_on_customer_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -47,5 +55,6 @@ ActiveRecord::Schema.define(version: 2022_05_28_161118) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "invoices", "customers"
   add_foreign_key "items", "merchants"
 end
