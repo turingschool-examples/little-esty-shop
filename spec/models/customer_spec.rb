c566fdfe5d11c17051135797b8a8fd7306a4ebb3
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-28 16:08:04 -0600

    Add validations for customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Customer, type: :model do
     it { should have_many(:transactions).through(:invoices) }
     it { should have_many(:invoice_items).through(:invoices) }
   end
+
+  describe 'validations' do 
+    it { should validate_presence_of :first_name}
+    it { should validate_presence_of :last_name}
+  end
 end
