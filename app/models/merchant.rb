9bc73e38ce7ffdd63ed128212977b7f02c15b7d6
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-05-27 21:05:03 -0600

    Feat: add validations and relationships to models

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,5 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  validates :name, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,9 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
 
+  validates :credit_card_number, presence: true
+  validates :credit_card_expiration_date, presence: true
+  validates :result, presence: true
+
   enum result: { 'success' => true, 'failed' => false }
 end
