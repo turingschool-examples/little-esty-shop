ed269ecc7c89d92019cdf9d3ca027760d69cbc63
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-28 12:53:10 -0600

    Add Customer model relationships to Transaction

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ApplicationRecord
   has_many :invoices
-
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
+  
   validates_presence_of :first_name, :last_name
 
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Customer do
   describe 'relationships' do
     it { should have_many(:invoices) }
+    it { should have_many(:invoice_items).through(:invoices) }
+    it { should have_many(:transactions).through(:invoices) }
   end
 
   describe 'validations' do
