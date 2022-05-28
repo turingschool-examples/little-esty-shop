f9ab34b4a40cbaa0b2417bc676ea40ff2e6fcf26
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-28 16:00:24 -0600

    Add validations for item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Item, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:invoices).through(:invoice_items) }
   end
+
+  describe 'validations' do 
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :description}
+    it {should validate_numericality_of :unit_price}
+
+  end
 end
