e067d12e40bf20a9df73ca12c3678a06dd2b0f6d
Sarah Hall <swatson1994@yahoo.com>
2022-05-28 19:45:04 -0400

    "Test: Add tests for links from merchant dashboard"

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -1,11 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Show Dash' do
+  before(:each) do
+    @billman = Merchant.create!(name: "Billman")
+  end
   it "has the name of the merchant on the page" do
-    billman = Merchant.create!(name: "Billman")
+    visit "/merchants/#{@billman.id}/dashboard"
 
-    visit "/merchants/#{billman.id}/dashboard"
+    expect(page).to have_content(@billman.name)
+  end
 
-    expect(page).to have_content(billman.name)
+  it "has a link to merchant items index" do
+    visit "/merchants/#{@billman.id}/dashboard"
+
+    expect(page).to have_link("#{@billman.name}'s Items")
+
+    click_link("#{@billman.name}'s Items")
+
+    expect(page).to have_current_path("/merchants/#{@billman.id}/items")
+  end
+
+  xit "has a link to merchant invoices index" do
+    visit "/merchants/#{@billman.id}/dashboard"
+
+    expect(page).to have_link("#{@billman.name}'s Invoices")
+
+    click_link("#{@billman.name}'s Invoices")
+
+    expect(page).to have_current_path("/merchants/#{@billman.id}/invoices")
   end
 end
