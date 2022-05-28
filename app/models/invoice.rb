ecbf2ee050488f1be579813f5815b0aa0e3b1473
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-28 16:02:56 -0600

    Add validations for invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,6 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   belongs_to :customer
+
+  enum status:["in progress", "completed", "cancelled"]
 end
