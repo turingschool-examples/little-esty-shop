4bbc8e8ae6f383c2d8273238f8586aa0a269a218
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-28 15:55:26 -0600

    Add valdiations for transaction model

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Transaction, type: :model do
   describe "relationships" do
     it { should belong_to :invoice }
   end
+
+  describe 'validations' do 
+    it { should validate_presence_of :credit_card_number}
+    it { should define_enum_for(:result).with(["success", "failed"])}
+
+  end
 end
