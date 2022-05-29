88b35b09c59b6ac42e6094d096c3909fb52dc948
Stephen Wilkens <stephenwilkens@gmail.com>
2022-05-29 13:43:14 -0600

    Add merchant_items index view, feat spec, route, and index action to controller

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -1,5 +1,9 @@
 class MerchantItemsController < ApplicationController
 
+    def index
+        @merchant = Merchant.find(params[:merchant_id])
+    end
+
     def show
         @item = Item.find(params[:item_id])
     end
@@ -7,5 +11,4 @@ class MerchantItemsController < ApplicationController
     def edit
         @item = Item.find(params[:item_id])
     end
-
 end
