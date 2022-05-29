94c260d8f0fbcb34c70f699bb6f436ab6557490e
Jim Riddle <jimriddle0@gmail.com>
2022-05-29 12:15:27 -0500

    modified views and routes to pass tests for merchant item show page

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  belongs_to :merchant
+
+  # def self.merchants_items_all
+  #   binding.pry
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/merchants/:id/dashboard', to: 'merchants#dashboard'
-  get '/merchants/:id/items', to: 'merchant_items#index'
   get '/merchants/:id/invoices', to: 'merchant_invoices#index'
+
+  get '/merchants/:id/items', to: 'merchant_items#index'
+  get '/merchants/:id/items/:item_id', to: 'merchant_items#show'
 end
