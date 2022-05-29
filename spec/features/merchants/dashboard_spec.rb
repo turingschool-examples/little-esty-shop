f85c73134bbcdf673543a9b05abc515f0f98ca56
Zachary P <zachary.s.prince@outlook.com>
2022-05-29 09:40:46 -0400

    Fix test

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'merchants dashboard' do
   it 'shows the name of the merchant' do
     merch1 = Merchant.create!(name: 'Floopy Fopperations')
 
-    visit "/merchant/#{merchant.id}/dashboard"
+    visit "/merchants/#{merch1.id}/dashboard"
 
     expect(page).to have_content(merch1.name)
   end
