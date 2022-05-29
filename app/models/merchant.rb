36fca6e3acc66b04d12282dfd1c9570af7b13b85
Colin Reinhart <colinbreinhartgmail.com>
2022-05-29 15:47:16 -0600

    User story 25 Complete

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,9 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
 
   validates_presence_of :name
+
+  def merchant_ii
+    require "pry"; binding.pry
+    Merchant.find(self.invoice_item)
+  end
 end
