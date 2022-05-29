64dcd08093b3f59e79f4ecf6ae725fcd13f06b2b
Andrew King <dev.andrewking@gmail.com>
2022-05-29 12:29:46 -0500

    add model method to convert price to dollars

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   validates_presence_of :name, :description, :unit_price
+
+  def unit_price_to_dollars
+    price = self.unit_price / 100.00
+  end
 end
