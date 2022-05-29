5504a5b77ae368d605baf12f760df420c951c2ab
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-29 15:33:59 -0600

    Feat: add items ready to ship to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,9 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
 
   validates_presence_of :name
+
+  def items_ready_to_ship
+    merchant_items = Item.where("merchant_id = #{self.id}")
+    InvoiceItem.where(item_id: merchant_items).where(status: [0,1])
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,33 +12,38 @@ RSpec.describe Merchant do
 
   describe 'instance methods' do
 
+    before :each do
+      @merch_1 = Merchant.create!(name: "Two-Legs Fashion")
+      @merch_2 = Merchant.create!(name: "One-Leg Fashion")
+
+      @item_1 = @merch_1.items.create!(name: "Two-Leg Pantaloons", description: "pants built for people with two legs", unit_price: 5000)
+      @item_2 = @merch_1.items.create!(name: "Two-Leg Shorts", description: "shorts built for people with two legs", unit_price: 3000)
+      @item_3 = @merch_1.items.create!(name: "Hat", description: "hat built for people with two legs and one head", unit_price: 6000)
+      @item_4 = @merch_2.items.create!(name: "Single Legged Pant", description: "pants built for people with one leg", unit_price: 50000)
+  
+      @cust_1 = Customer.create!(first_name: "Debbie", last_name: "Twolegs")
+      @cust_2 = Customer.create!(first_name: "Tommy", last_name: "Doubleleg")
+  
+      @invoice_1 = @cust_1.invoices.create!(status: 1)
+      @invoice_2 = @cust_1.invoices.create!(status: 1)
+      @invoice_3 = @cust_1.invoices.create!(status: 1)
+      @invoice_4 = @cust_2.invoices.create!(status: 1)
+      @invoice_5 = @cust_2.invoices.create!(status: 1)
+      @invoice_6 = @cust_2.invoices.create!(status: 1)
+  
+      @ii_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: @item_1.unit_price, status: 0)
+      @ii_2 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_1.unit_price, status: 1)
+      @ii_3 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_2.unit_price, status: 2)
+      @ii_4 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_3.unit_price, status: 2)
+      @ii_5 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_4.id, quantity: 1, unit_price: @item_1.unit_price, status: 1)
+      @ii_6 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_4.id, quantity: 1, unit_price: @item_2.unit_price, status: 1)
+      @ii_7 = InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_6.id, quantity: 1, unit_price: @item_4.unit_price, status: 1)
+    end
+
     describe '#items_ready_to_ship' do
-      before :each do
-        @merch_1 = Merchant.create!(name: "Two-Legs Fashion")
-
-        @item_1 = @merch_1.items.create!(name: "Two-Leg Pantaloons", description: "pants built for people with two legs", unit_price: 5000)
-        @item_2 = @merch_1.items.create!(name: "Two-Leg Shorts", description: "shorts built for people with two legs", unit_price: 3000)
-        @item_3 = @merch_1.items.create!(name: "Hat", description: "hat built for people with two legs and one head", unit_price: 6000)
-      
-        @cust_1 = Customer.create!(first_name: "Debbie", last_name: "Twolegs")
-        @cust_2 = Customer.create!(first_name: "Tommy", last_name: "Doubleleg")
-    
-        @invoice_1 = @cust_1.invoices.create!(status: 1)
-        @invoice_2 = @cust_1.invoices.create!(status: 1)
-        @invoice_3 = @cust_1.invoices.create!(status: 1)
-        @invoice_4 = @cust_2.invoices.create!(status: 1)
-        @invoice_5 = @cust_2.invoices.create!(status: 1)
-    
-        @ii_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: @item_1.unit_price, status: 0)
-        @ii_2 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_1.unit_price, status: 1)
-        @ii_3 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_2.unit_price, status: 2)
-        @ii_4 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_3.unit_price, status: 2)
-        @ii_5 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_4.id, quantity: 1, unit_price: @item_1.unit_price, status: 1)
-        @ii_6 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_4.id, quantity: 1, unit_price: @item_2.unit_price, status: 1)
-      end
-
       it 'returns the items that have not yet been shipped for a particular merchant' do
-        expect(@merch_1.items_ready_to_ship).to eq([@item_1, @item_2])
+        expect(@merch_1.items_ready_to_ship).to eq([@ii_1, @ii_2, @ii_5, @ii_6])
+        expect(@merch_2.items_ready_to_ship).to eq([@ii_7])
       end
     end
 
