fc56bb7e70ccb704788c74561377f447fd6a0199
Zachary P <zachary.s.prince@outlook.com>
2022-05-29 09:48:04 -0400

    Add merchant dashboard links test

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe 'merchants dashboard' do
 
     expect(page).to have_content(merch1.name)
   end
+
+  it 'shows links to the merchants items index and invoices index' do
+    merch1 = Merchant.create!(name: 'Floopy Fopperations')
+
+    visit "/merchants/#{merch1.id}/dashboard"
+
+    expect(page).to have_content('Items Index')
+
+    expect(page).to have_content('Invoices Index')
+  end
 end
