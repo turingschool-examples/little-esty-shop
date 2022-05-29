3300ef3b3271d33ce6aea5d4ceeb2b47f1b8bff4
Sarah Hall <swatson1994@yahoo.com>
2022-05-28 20:07:46 -0400

    "Add route, controller, and view for merchant Items"

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'application#welcome'
   get "/merchants/:merchant_id/dashboard", to: "merchants#show"
+  get "/merchants/:merchant_id/items", to: "merchant_items#index"
 end
