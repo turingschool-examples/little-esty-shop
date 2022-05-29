90ad38aeeaea84e5bf211c95d6f43f67c0d2791a
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-29 15:53:45 -0600

    Feat: add items ready to ship section to merchant dashboard

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -6,27 +6,27 @@ RSpec.describe "merchant dashboard", type: :feature do
     @merchant_2 = Merchant.create(name: "Klein, Rempel and Jones")
 
     @item_1 = @merchant_1.items.create!(name: "Two-Leg Pantaloons", description: "pants built for people with two legs", unit_price: 5000)
-      @item_2 = @merchant_1.items.create!(name: "Two-Leg Shorts", description: "shorts built for people with two legs", unit_price: 3000)
-      @item_3 = @merchant_1.items.create!(name: "Hat", description: "hat built for people with two legs and one head", unit_price: 6000)
-      @item_4 = @merchant_2.items.create!(name: "Pant", description: "pants built for people", unit_price: 50000)
-  
-      @cust_1 = Customer.create!(first_name: "Debbie", last_name: "Twolegs")
-      @cust_2 = Customer.create!(first_name: "Tommy", last_name: "Doubleleg")
-  
-      @invoice_1 = @cust_1.invoices.create!(status: 1)
-      @invoice_2 = @cust_1.invoices.create!(status: 1)
-      @invoice_3 = @cust_1.invoices.create!(status: 1)
-      @invoice_4 = @cust_2.invoices.create!(status: 1)
-      @invoice_5 = @cust_2.invoices.create!(status: 1)
-      @invoice_6 = @cust_2.invoices.create!(status: 1)
-  
-      @ii_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: @item_1.unit_price, status: 0)
-      @ii_2 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_1.unit_price, status: 1)
-      @ii_3 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_2.unit_price, status: 2)
-      @ii_4 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_3.unit_price, status: 2)
-      @ii_5 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_4.id, quantity: 1, unit_price: @item_1.unit_price, status: 1)
-      @ii_6 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_4.id, quantity: 1, unit_price: @item_2.unit_price, status: 1)
-      @ii_7 = InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_6.id, quantity: 1, unit_price: @item_4.unit_price, status: 1)
+    @item_2 = @merchant_1.items.create!(name: "Two-Leg Shorts", description: "shorts built for people with two legs", unit_price: 3000)
+    @item_3 = @merchant_1.items.create!(name: "Hat", description: "hat built for people with two legs and one head", unit_price: 6000)
+    @item_4 = @merchant_2.items.create!(name: "Shirt", description: "shirt for people", unit_price: 50000)
+
+    @cust_1 = Customer.create!(first_name: "Debbie", last_name: "Twolegs")
+    @cust_2 = Customer.create!(first_name: "Tommy", last_name: "Doubleleg")
+
+    @invoice_1 = @cust_1.invoices.create!(status: 1)
+    @invoice_2 = @cust_1.invoices.create!(status: 1)
+    @invoice_3 = @cust_1.invoices.create!(status: 1)
+    @invoice_4 = @cust_2.invoices.create!(status: 1)
+    @invoice_5 = @cust_2.invoices.create!(status: 1)
+    @invoice_6 = @cust_2.invoices.create!(status: 1)
+
+    @ii_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: @item_1.unit_price, status: 0)
+    @ii_2 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_1.unit_price, status: 1)
+    @ii_3 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_2.unit_price, status: 2)
+    @ii_4 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_2.id, quantity: 1, unit_price: @item_3.unit_price, status: 2)
+    @ii_5 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_4.id, quantity: 1, unit_price: @item_1.unit_price, status: 1)
+    @ii_6 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_4.id, quantity: 1, unit_price: @item_2.unit_price, status: 1)
+    @ii_7 = InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_6.id, quantity: 1, unit_price: @item_4.unit_price, status: 1)
   end
 
   it "shows name of merchant" do
@@ -49,6 +49,7 @@ RSpec.describe "merchant dashboard", type: :feature do
     visit "/merchants/#{@merchant_1.id}/dashboard"
 
     expect(page).to have_content("Items Ready To Ship")
+    save_and_open_page
     within("#item-0") do
       expect(page).to have_content(@item_1.name)
       expect(page).to have_content(@invoice_1.id)
@@ -67,7 +68,7 @@ RSpec.describe "merchant dashboard", type: :feature do
       expect(page).to_not have_content(@item_4.name)
       expect(page).to_not have_content(@invoice_1.id)
     end
-    within("#item-2") do
+    within("#item-3") do
       expect(page).to have_content(@item_2.name)
       expect(page).to have_content(@invoice_4.id)
       expect(page).to_not have_content(@item_4.name)
