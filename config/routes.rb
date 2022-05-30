eeb8883bfe261d5a06c1e27520ab4f45b65d5fef
Andrew King <dev.andrewking@gmail.com>
2022-05-30 15:29:59 -0500

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   get "/merchants/:merchant_id/invoices", to: "merchant_invoices#index"
   get "/merchants/:merchant_id/invoices/:invoice_id", to: "merchant_invoices#show"
 
+  get "/admin/merchants", to: 'admin_merchants#index'
+
 end
