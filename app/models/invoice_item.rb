e3b929bb420c8f5623f29fe26144cd3ad4b0eb7d
Chris Kjolhede <ckjolhede@gmail.com>
2022-05-30 04:24:40 -0600

    write validations for InvoiceItems

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,10 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+
+  validates_presence_of :quantity
+  validates_presence_of :unit_price
+  validates_presence_of :status
+  validates_presence_of :created_at
+  validates_presence_of :updated_at
 end
