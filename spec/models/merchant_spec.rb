33c4dd960bdf00fde4daaeb118583c765292799d
Cory Bethune <corybbethune@gmail.com>
2022-05-29 20:54:13 -0700

    update merchant model spec associations

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Merchant do
   describe 'associations' do
     it { should have_many :items}
+    it { should have_many(:invoice_items).through(:items)}
+    it { should have_many(:invoices).through(:invoice_items)}
   end
    describe 'validations' do
     it { should validate_presence_of :name}
