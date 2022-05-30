813440199e96814ae7cc2355474bb3614504ba7e
Andrew King <dev.andrewking@gmail.com>
2022-05-30 16:47:10 -0500

    add update action, add merchant_params private method

--- app/controllers/admin_merchants_controller.rb
+++ app/controllers/admin_merchants_controller.rb
@@ -10,4 +10,20 @@ class AdminMerchantsController < ApplicationController
   def edit
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    if merchant.update(merchant_params)
+        redirect_to "/admin/merchants/#{merchant.id}"
+        flash[:success] = "Merchant successfully updated!"
+    else
+        redirect_to "/admin/merchants/#{merchant.id}/edit"
+        flash[:alert] = "Error: #{error_message(merchant.errors)}"
+    end
+  end
+
+  private
+    def merchant_params
+        params.permit(:name)
+    end
 end
