1a309fadd13196a35bfccaa42c4d875e0e73ec29
Chris Kjolhede <ckjolhede@gmail.com>
2022-05-30 04:25:48 -0600

    write Validations for Invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,8 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :transactions
+
+  validates_presence_of :status
+  validates_presence_of :created_at
+  validates_presence_of :updated_at
 end
