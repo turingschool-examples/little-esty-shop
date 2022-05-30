4c280bfeecc5e79a2e5b7fad6aeb6cd386e3189f
Andrew King <dev.andrewking@gmail.com>
2022-05-30 15:24:03 -0500

    add index_spec

---
+++ spec/features/admin/merchants/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'admin merchants index', type: :feature do
+  before(:each) do
+    @merch_1 = Merchant.create!(name: "Two-Legs Fashion")
+    @merch_2 = Merchant.create!(name: "Buck-an-Ear Jewelry")
+    @merch_3 = Merchant.create!(name: "Orange You Glad")
+    @merch_4 = Merchant.create!(name: "Absolutely Authentic Autographs")
+  end
+
+  it "displays all merchants" do
+    visit "admin/merchants"
+
+    expect(page).to have_content(@merch_1.name)
+    expect(page).to have_content(@merch_2.name)
+    expect(page).to have_content(@merch_3.name)
+    expect(page).to have_content(@merch_4.name)
+  end
+end
