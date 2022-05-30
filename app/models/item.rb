848f7b14be3b03fd2f4f6c519b5309aa023fa658
Chris Kjolhede <ckjolhede@gmail.com>
2022-05-30 04:27:35 -0600

    write Validations for Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :unit_price
+  validates_presence_of :created_at
+  validates_presence_of :updated_at
 end
