4be8d082c4386a5867f178b14fd21ef0ed693ca8
Wesley Garcia <wes@alltradestech.com>
2022-05-29 18:49:56 -0700

    Add dynamic data with 'faker' gem to factories

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,8 +1,8 @@
-FactoryBot.define do 
-  factory :item do 
-    name { 'feet pics' }
-    description { 'pics of feet' }
-    unit_price { 500000 }
+FactoryBot.define do
+  factory :item do
+    name { Faker::Commerce.product_name }
+    description { Faker::Coffee.notes }
+    unit_price { Faker::Number.number(digits: 4) }
     merchant
   end
 end
