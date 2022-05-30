08831b148b8bdd3f7b111762d516a66a13e00d4e
Chris Kjolhede <ckjolhede@gmail.com>
2022-05-30 03:26:52 -0600

    correct customer-invoices foreign key association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_091649) do
+ActiveRecord::Schema.define(version: 2022_05_30_092441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,6 @@ ActiveRecord::Schema.define(version: 2022_05_30_091649) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "invoice_id"
-    t.index ["invoice_id"], name: "index_customers_on_invoice_id"
   end
 
   create_table "invoice_items", force: :cascade do |t|
@@ -40,6 +38,8 @@ ActiveRecord::Schema.define(version: 2022_05_30_091649) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "customer_id"
+    t.index ["customer_id"], name: "index_invoices_on_customer_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -68,9 +68,9 @@ ActiveRecord::Schema.define(version: 2022_05_30_091649) do
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
-  add_foreign_key "customers", "invoices"
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
+  add_foreign_key "invoices", "customers"
   add_foreign_key "items", "merchants"
   add_foreign_key "transactions", "invoices"
 end
