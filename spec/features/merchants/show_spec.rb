4ead53f6d954b3e8cd93866b7f3c419f059de125
Chris Kjolhede <ckjolhede@gmail.com>
2022-05-30 08:46:14 -0600

    refactored dashboard show tests to pass with validations in place

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant Show Dash' do
   before(:each) do
-    @billman = Merchant.create!(name: "Billman")
+    @billman = Merchant.create!(name: "Billman", created_at: Time.now, updated_at: Time.now)
 
-    bracelet = @billman.items.create!(name: "Bracelet")
-    mood = @billman.items.create!(name: "Mood Ring")
+    @bracelet = @billman.items.create!(name: "Bracelet", description: "shiny", unit_price: 1001, created_at: Time.now, updated_at: Time.now)
+    @mood = @billman.items.create!(name: "Mood Ring", description: "Moody", unit_price: 2002, created_at: Time.now, updated_at: Time.now)
 
-    brenda = Customer.create!(first_name: "Brenda", last_name: "Bhoddavista")
+    @brenda = Customer.create!(first_name: "Brenda", last_name: "Bhoddavista", created_at: Time.now, updated_at: Time.now)
 
-    invoice1 = brenda.invoices.create!(status: "In Progress")
+    @invoice1 = @brenda.invoices.create!(status: "In Progress", created_at: Time.now, updated_at: Time.now)
 
-    order1 = bracelet.invoice_items.create!(quantity: 1, unit_price: 10000, status: "Pending", invoice_id: invoice1.id)
-    order2 = mood.invoice_items.create!(quantity: 1, unit_price: 1000, status: "Pending", invoice_id: invoice1.id)
+    @order1 = @bracelet.invoice_items.create!(quantity: 1, unit_price: 10000, status: "Pending", invoice_id: @invoice1.id, created_at: Time.now, updated_at: Time.now)
+    @order2 = @mood.invoice_items.create!(quantity: 1, unit_price: 1000, status: "Pending", invoice_id: @invoice1.id, created_at: Time.now, updated_at: Time.now)
   end
+  
   it "has the name of the merchant on the page" do
     visit "/merchants/#{@billman.id}/dashboard"
 
