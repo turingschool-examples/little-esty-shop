dbbf0de652c80b6144ec1539fa4ce15bb0ca9acb
Andrew King <dev.andrewking@gmail.com>
2022-05-30 15:47:13 -0500

    add show action

--- app/controllers/admin_merchants_controller.rb
+++ app/controllers/admin_merchants_controller.rb
@@ -2,4 +2,8 @@ class AdminMerchantsController < ApplicationController
     def index
         @merchants = Merchant.all
     end
+
+    def show
+        @merchant = Merchant.find(params[:merchant_id])
+    end
 end
