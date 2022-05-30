fde6f0fed8c6837279d9d58c910ab847b79cbfa8
Cory Bethune <corybbethune@gmail.com>
2022-05-29 20:51:14 -0700

    update merchant associations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
+  has_many :invoice_items, through: :items  
+  has_many :invoices, through: :invoice_items
 
   validates_presence_of :name
 end
