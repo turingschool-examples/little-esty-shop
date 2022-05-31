b22448eeb7baf105faa52b1916633bb9dfb35b50
Bryce Wein <bryce.wein@gmail.com>
2022-05-31 17:07:21 -0400

    Test: adds test of merchants on admin merchants index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_29_001945) do
+ActiveRecord::Schema.define(version: 2022_05_31_205620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2022_05_29_001945) do
     t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "status", default: 1
   end
 
   create_table "transactions", force: :cascade do |t|
