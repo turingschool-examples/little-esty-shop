646a552708b862245ee77a22e70ab27fbb20466e
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-31 17:38:59 -0500

    Test: lists all invoice Id's and links to show page

---
+++ spec/features/admin/invoices/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Invoice Index Page' do
+  before :each do
+  @customer_1 = Customer.create!(first_name: 'Joey', last_name: "Ondricka")
+  @invoice_1 = @customer_1.invoices.create!(status: "cancelled")
+  @invoice_2 = @customer_1.invoices.create!(status: "in progress")
+  @invoice_3 = @customer_1.invoices.create!(status: "completed")
+
+  end
+
+  it "lists all invoice ID's and links to their show page" do
+    visit '/admin/invoices'
+    expect(page).to have_content("Invoice ID's:")
+    expect(page).to have_content(@invoice_1.id)
+    expect(page).to have_content(@invoice_2.id)
+    expect(page).to have_content(@invoice_3.id)
+    click_link "#{@invoice_1.id}"
+    expect(current_path).to eq("/admin/invoices/#{@invoice_1.id}")
+  end
+
+end
