02596088e61716a0574ed22443473b939c0626f9
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-05-31 14:24:51 -0700

    Add: #ready_items merchant instance method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,13 @@
 class Merchant < ApplicationRecord
   has_many :items
 
+  def ready_items
+    items.joins(:invoices).select('items.*')
+         .where("invoice_items.status = 1")
+         .group("items.id")
+         .distinct
+         .order(:created_at)
+  end
+  
+
 end
