086155bf14202be7426d04667fcb2bf1dec95653
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-31 17:32:16 -0600

    Feat: add favorite customers heading to merchatn dashboard

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -194,7 +194,7 @@ RSpec.describe "merchant dashboard", type: :feature do
     transaction_16 = invoice_17.transactions.create!(credit_card_number: 4023948573948394, credit_card_expiration_date: "1", result: "success")
   
     visit "/merchants/#{@merch_1.id}/dashboard"
-
+save_and_open_page
     expect(page).to have_content("Favorite Customers")
     expect("Debbie Twolegs").appear_before("Tommy Doubleleg")
     expect("Tommy Doubleleg").appear_before("Brian Twinlegs")
