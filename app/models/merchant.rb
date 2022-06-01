8fd042a68f791075133ce70bded1d81cf9672a1a
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-01 13:46:01 -0600

    Ref: use grouping and ordering in top 5 customers instead of ruby logic

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -16,11 +16,16 @@ class Merchant < ApplicationRecord
     merchant_invoices = Invoice.joins(:items).where(items: { id: merchant_items })
     successful_invoices = Invoice.joins(:transactions).where(id: merchant_invoices).where(transactions: { result: "success" })
     #customer_invoice_count is a hash, key: customer_id value: number of successful invoices for customer
-    customer_invoice_count = Invoice.where(id: successful_invoices).group(:customer_id).count
-    customer_sorted = customer_invoice_count.sort_by {|id, count| count}
-    top_5 = customer_sorted.last(5)
-    customer_ids = top_5.map { |customer_count| customer_count[0] }
-    customers_top_5 = Customer.where(id: customer_ids)
+    # customer_invoice_count = Invoice.where(id: successful_invoices).group(:customer_id).count
+    # invoice_customer_count_sorted = Invoice.where(id: successful_invoices).select("invoices.customer_id, count(customer_id) AS count_invoices").group(:customer_id).order(count_invoices: :desc)
+    invoice_customer_count_sorted = Invoice.where(id: successful_invoices).select("invoices.customer_id, count(customer_id)").group(:customer_id).order(count: :desc)
+    # binding.pry
+    cust_ids = invoice_customer_count_sorted.limit(5).pluck(:customer_id)
+    # customer_sorted = customer_invoice_count.sort_by {|id, count| count}
+    # top_5 = customer_sorted.last(5)
+    # customer_ids = top_5.map { |customer_count| customer_count[0] }
+    # customers_top_5 = Customer.where(id: customer_ids)
+    customers_top_5 = Customer.where(id: cust_ids)
   end
 
 end
