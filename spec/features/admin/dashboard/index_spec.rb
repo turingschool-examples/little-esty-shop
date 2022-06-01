518235e13844b5f522db8170a01aebc8eda27f01
Stirling Hostetter <stirhoss@icloud.com>
2022-06-01 00:40:52 -0500

    Test: Covers forst 2 stories of admin dashboard

--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -2,8 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'admin dashboard' do
   it 'has an admin header' do
-  visit admin_dashboard_path
+    visit admin_dashboard_path
 
-  expect(page).to have_content('Admin Dashboard')
+    within '#header' do
+      expect(page).to have_content('Admin Dashboard')
+    end
+  end
+
+  it 'has links to admin/merchants' do
+    visit admin_dashboard_path
+
+    within '#merchants' do
+      expect(page).to have_link('Merchants')
+      click_link 'Merchants'
+      expect(current_path).to eq(admin_merchants_path)
+    end
+  end
+
+  it 'has links to admin/invoices' do
+    visit admin_dashboard_path
+
+    within '#invoices' do
+      expect(page).to have_link('Invoices')
+      click_link 'Invoices'
+      expect(current_path).to eq(admin_invoices_path)
+    end
   end
 end
