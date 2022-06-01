7bebe9616f4015168fd94d0ec6f83fcc4e79f9ef
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-01 09:31:49 -0600

    Test: total revenue on admin invoice show page

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -28,10 +28,21 @@ RSpec.describe 'Admin invoices show page' do
 
   it 'displays the invoices id, status, created time, and customers first and last name' do
     visit "/admin/invoices/#{@invoice_1.id}"
-save_and_open_page
+
     expect(page).to have_content("#{@invoice_1.id}")
     expect(page).to have_content("Status: in progress")
     expect(page).to have_content("#{@invoice_1.created_at.strftime("%A %b %M %Y")}")
     expect(page).to have_content("Debbie Twolegs")
+  end
+
+  it 'displays total revenue for invoice' do
+    merch_1 = Merchant.create!(name: "Two-Legs Fashion")
+
+    item_1 = merch_1.items.create!(name: "Two-Leg Pantaloons", description: "pants built for people with two legs", unit_price: 5000)
+
+    InvoiceItem.create!(item_id: item_1.id, invoice_id: @invoice_1.id, quantity: 3, unit_price: item_1.unit_price, status: 2)
+    visit "/admin/invoices/#{@invoice_1.id}"
+
+    expect(page).to have_content("Total Revenue: $150.00")
   end
 end
