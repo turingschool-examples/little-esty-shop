5b95c02aaa5ce0df09f6038514a854b93e103dc3
Zachary P <zachary.s.prince@outlook.com>
2022-06-01 17:31:54 -0400

    Add invoice time test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,34 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Item do
-    describe "relationships" do
-        it { should belong_to :merchant } 
-        it { should have_many :invoice_items } 
-        it { should have_many(:invoices).through(:invoice_items) } 
+  describe 'relationships' do
+    it { should belong_to :merchant }
+    it { should have_many :invoice_items }
+    it { should have_many(:invoices).through(:invoice_items) }
+  end
+
+  describe 'instance methods' do
+    before :each do
+      @merch1 = Merchant.create!(name: 'Floopy Fopperations')
+      @customer1 = Customer.create!(first_name: 'Joe', last_name: 'Bob')
+      @item1 = @merch1.items.create!(name: 'Floopy Original', description: 'the best', unit_price: 450)
+      @item2 = @merch1.items.create!(name: 'Floopy Updated', description: 'the better', unit_price: 950)
+      @item3 = @merch1.items.create!(name: 'Floopy Retro', description: 'the OG', unit_price: 550)
+      @item4 = @merch1.items.create!(name: 'Floopy Geo', description: 'the OG', unit_price: 550)
+      @invoice1 = @customer1.invoices.create!(status: 2)
+      @invoice2 = @customer1.invoices.create!(status: 2)
+      InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 5, unit_price: 1000, status: 0)
+      InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice2.id, quantity: 5, unit_price: 1000, status: 1)
+      InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice1.id, quantity: 5, unit_price: 1000, status: 1)
+      InvoiceItem.create!(item_id: @item4.id, invoice_id: @invoice2.id, quantity: 5, unit_price: 1000, status: 2)
     end
-    
-    describe "instance methods" do
-        before :each do
-            @merch1 = Merchant.create!(name: 'Floopy Fopperations')
-            @customer1 = Customer.create!(first_name: 'Joe', last_name: 'Bob')
-            @item1 = @merch1.items.create!(name: 'Floopy Original', description: 'the best', unit_price: 450)
-            @item2 = @merch1.items.create!(name: 'Floopy Updated', description: 'the better', unit_price: 950)
-            @item3 = @merch1.items.create!(name: 'Floopy Retro', description: 'the OG', unit_price: 550)
-            @item4 = @merch1.items.create!(name: 'Floopy Geo', description: 'the OG', unit_price: 550)
-            @invoice1 = @customer1.invoices.create!(status: 2)
-            @invoice2 = @customer1.invoices.create!(status: 2)
-            InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 5, unit_price: 1000, status: 0)
-            InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice2.id, quantity: 5, unit_price: 1000, status: 1)
-            InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice1.id, quantity: 5, unit_price: 1000, status: 1)
-            InvoiceItem.create!(item_id: @item4.id, invoice_id: @invoice2.id, quantity: 5, unit_price: 1000, status: 2)
+    describe '#find_invoice_id' do
+      it 'finds invoice id of a specific item' do
+        expect(@item1.find_invoice_id).to eq(@invoice1.id)
+      end
+
+      describe '#invoice_time' do
+        it 'finds invoice creation date of a specific item' do
+          expect(@item1.invoice_time).to eq(@invoice1.created_at.strftime('%A %B %e %Y'))
         end
-        describe "#find_invoice_id" do
-            it "finds invoice id of a specific item" do
-                expect(@item1.find_invoice_id).to eq(@invoice1.id)
-            end
-            
-        end
+      end
     end
-    
-    
+  end
 end
+
