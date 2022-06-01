49546b34ffef0ab01b917e4f7678236bb40fae9f
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-05-31 23:07:26 -0400

    feat: merchant name appears on dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :merchants do 
+  resources :merchants do
     resources :items, only: [:index, :show, :edit, :update]
+    resources :dashboard, only: [:index]
   end
 
   namespace :admin do
