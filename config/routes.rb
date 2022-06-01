b04f5f6504e646e8b6a0f25888c7561a393e37ab
Caden Jarrett <caden.jarrett99@gmail.com>
2022-05-31 22:04:46 -0600

    deleted routes file to not have any overlap with current routes on main file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get 'merchants/:id/items', to: 'items#index'
-  get 'merchants/:id/items/:item_id', to: 'items#show'
-  get 'merchants/:id/invoices', to: 'invoices#index'
 end
