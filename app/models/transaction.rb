c80842b8d4bb92034fd4791551c983f1a5083ef8
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-01 16:43:52 -0600

    Ref: do not validate credit card expiration date

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,4 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
 
   validates_presence_of :credit_card_number, :result
-  validates_presence_of :credit_card_expiration_date, presence: false
 end
