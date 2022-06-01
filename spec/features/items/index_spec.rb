1c17e51659aa9f28d4cde17ec81b1904ef6fb5c3
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-06-01 11:10:03 -0400

    Write tests for new merchant's item page and link

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -1,19 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Merchant Items Index Page" do
+  before do
+    @merchant_1 = Merchant.create!(name: "Schroeder-Jerde")
+    @item_1 = @merchant_1.items.create!(name: "Qui Esse", description: "Nihil autem sit odio inventore deleniti", unit_price: 75107)
+    @item_2 = @merchant_1.items.create!(name: "Autem Minima", description: "Cumque consequuntur ad", unit_price: 67076)
+
+    @merchant_2 = Merchant.create!(name: "Willms and Sons")
+    @item_3 = @merchant_2.items.create!(name: "Ea Voluptatum", description: "Sunt officia", unit_price: 68723)
+
+    visit merchant_items_path(@merchant_1)
+  end
 
   it "displays only a specified merchant's items" do
-    merchant_1 = Merchant.create!(name: "Schroeder-Jerde")
-    item_1 = merchant_1.items.create!(name: "Qui Esse", description: "Nihil autem sit odio inventore deleniti", unit_price: 75107)
-    item_2 = merchant_1.items.create!(name: "Autem Minima", description: "Cumque consequuntur ad", unit_price: 67076)
-
-    merchant_2 = Merchant.create!(name: "Willms and Sons")
-    item_3 = merchant_2.items.create!(name: "Ea Voluptatum", description: "Sunt officia", unit_price: 68723)
-
-    visit merchant_items_path(merchant_1)
 
     expect(page).to have_content("Qui Esse")
     expect(page).to have_content("Autem Minima")
     expect(page).to_not have_content("Ea Voluptatum")
+  end
+
+  it "has a link to create a new item" do
+    click_link "Create A New Item"
+
+    expect(current_path).to eq(new_merchant_item_path(@merchant_1))
   end
 end
---
+++ spec/features/items/new_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "Merchant Items New Page" do
+
+  it "displays a form to add a new item, when I submit the item I am redirected to my merchants item index page where the item has been added" do
+    merchant_1 = Merchant.create!(name: "Schroeder-Jerde")
+
+    expect(new_merchant_item_path(merchant_1)).to_not have_content("Cool New Thingymabob")
+
+    visit new_merchant_item_path(merchant_1)
+
+    fill_in "Name", with: "Cool New Thingymabob"
+    fill_in "Description", with: "Exactly the thing you didn't know you needed"
+    fill_in "Unit Price", with: "10"
+    click_button "Submit"
+
+    expect(current_path).to eq(merchant_items_path(merchant_1))
+
+    # within "#disabled-items" do
+      expect(page).to have_content("Cool New Thingymabob")
+    # end
+
+    # within "#enabled-items" do
+      # expect(page).to_not have_content("Cool New Thingymabob")
+    # end
+  ##will need to test that the item has a default status of disabled when the functionality is done being created by Rue
+  end
+end
