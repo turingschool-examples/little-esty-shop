9919851e5050d9f142230c7f1aa3e12b25b5bc1e
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-06-01 17:19:53 -0400

    Update spec to use let blocks

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,17 +3,15 @@ class Item < ApplicationRecord
 
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
 
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
 
 
-  # def self.enabled_items
-  #   where(status: 1)
-  # end
-  #
-  # def self.disabled_items
-  #   where(status: 0)
-  # end
+
+  def top_five_items
+    require "pry"; binding.pry
+  end
 end
