516ea9b817d5dfd55a631948e31366b9b4cda45b
Chris Kjolhede <ckjolhede@gmail.com>
2022-06-02 12:43:28 -0600

    refactor TEST: item show update, remove unused method from item model

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe Item, type: :feature do
     end
 
     it "has an update link to an edit page form with fields prepopulated with items current information" do
-
+      visit "/merchants/#{@merchant1.id}/items/#{@item1.id}"
       click_on "Update"
       expect(current_path).to eq("/merchants/#{@merchant.id}/items/#{@item.id}/edit")
 
