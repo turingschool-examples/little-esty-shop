ace1d94f92d018864824c0ecbf74d33f48768fbb
Nick Jacobs <nickjacobsatc@gmail.com>
2022-06-02 13:10:07 -0500

    Fix: removed validation tests for timestamps

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  describe 'relationships' do 
+  describe 'relationships' do
     it {should have_many :items}
   end
-  
-  describe 'validations' do  
+
+  describe 'validations' do
     it {should validate_presence_of(:name)}
-    it {should validate_presence_of(:created_at)}
-    it {should validate_presence_of(:updated_at)}
   end
 end
