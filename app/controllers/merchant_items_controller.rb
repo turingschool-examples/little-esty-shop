c20943484fd953216ca99d987658ce1fcd6ef7ab
Chris Kjolhede <ckjolhede@gmail.com>
2022-06-02 13:49:03 -0600

    add #Update to merchant_item_controller w/ strong params

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -16,14 +16,16 @@ class MerchantItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
+    @merchant = Merchant.find(params[:merchant_id])
     @item.update(item_params)
     flash[:success] = 'You have successfully updated this item'
+    redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}"
   end
 
   private
 
   def item_params
-    params.permit(:id, :description, :unit_price, :status, :created_at, :updated_at, :merchant_id)
+    params.permit(:id, :name, :description, :unit_price, :status, :created_at, :updated_at, :merchant_id)
   end
 end
