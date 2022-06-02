b92fe0b64b6c10ff7eb2efdc339467f93700ba7d
Nick Jacobs <nickjacobsatc@gmail.com>
2022-06-01 20:19:07 -0500

    Feat: Update and invoice_params methods created

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -6,6 +6,16 @@ class Admin::InvoicesController < ApplicationController
 
   def show
     @invoice = Invoice.find(params[:id])
-  end 
+  end
 
+  def update
+    invoice = Invoice.find(params[:id])
+    invoice.update(invoice_params)
+    invoice.save
+    redirect_to "/admin/invoices/#{invoice.id}"
+  end
+
+  def invoice_params
+    params.permit(:id, :status, :created_at, :updated_at, :customer_id)
+  end
 end
