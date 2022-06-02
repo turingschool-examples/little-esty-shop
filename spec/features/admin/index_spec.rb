de57c5c9746d27b8036b296b9eec2aac90188c03
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-06-02 13:36:58 -0400

    Write tests for admin dashboard links

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -10,6 +10,13 @@ describe "Admin Dashboad" do
     expect(page).to have_content("Welcome to the Admin Dashboard")
   end
 
+  it "displays links to the admin merchants index and admin invoices index" do
+    click_link("Merchants Index")
+    expect(current_path).to eq(admin_merchants_path)
 
+    visit admin_index_path
+    click_link("Invoices Index")
+    expect(current_path).to eq(admin_invoices_path)
+  end
 
 end
