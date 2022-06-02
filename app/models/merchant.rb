8bc6aeb78b1a9bd9a08310a8200e7aa203a5a8e0
Nick Jacobs <nickjacobsatc@gmail.com>
2022-06-02 13:07:02 -0500

    Fix: removed timestamp validations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,5 @@
 class Merchant < ApplicationRecord
-  has_many :items 
+  has_many :items
 
   validates_presence_of :name
-  validates_presence_of :created_at
-  validates_presence_of :updated_at
 end
