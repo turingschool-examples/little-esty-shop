0832eee66ba0a9f4931cd42f554f4892f44777b5
Chris Kjolhede <ckjolhede@gmail.com>
2022-06-03 00:49:48 -0600

    remove item status validation test from model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,9 +12,4 @@ RSpec.describe Item, type: :model do
     it {should validate_presence_of(:description)}
     it {should validate_presence_of(:unit_price)}
   end
-
-  describe 'status validation' do
-    it {should validate_presence_of(:status)}
-  end
-
 end
