292640b8fe96f326c5188e955bdc28cec47a6d3e
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-06-03 16:55:31 -0600

    Test: Admin Merchant's show page can click a link to update this merchant and shows a flash message stating the merchant has been successfully updated

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -3,9 +3,24 @@ require 'rails_helper'
 RSpec.describe "Admin Merchant Show" do
   let!(:merchant1) { Merchant.create!(name: "REI") }
 
-  it "displays the name of a Merchant" do
+  before do
     visit admin_merchant_path(merchant1)
+  end
 
+  it "displays the name of a Merchant" do
     expect(page).to have_content("REI's Admin Page")
+  end
+
+  it "can click a link to update this merchant and shows a flash message stating the merchant has been successfully updated" do
+    click_link "Update Merchant"
+
+    expect(current_path).to eq(edit_admin_merchant_path(merchant1))
+
+    fill_in :name, with: 'Black Diamond'
+    click_on "Submit"
+    save_and_open_page
+    expect(current_path).to eq(admin_merchant_path(merchant1))
+    expect(page).to have_content('Black Diamond')
+    expect(page).to have_content('You have successfully updated this Merchant!')
   end
 end
