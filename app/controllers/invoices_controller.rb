6812b3eeb9f1fdd39274eaf748d0d7afd12fdf01
Zachary Hazelwood <zhaze001@gmail.com>
2022-06-02 18:27:26 -0600

    Feat: Update Invoice Item Status with selector

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -8,4 +8,16 @@ class InvoicesController < ApplicationController
   def show
     @invoice = Invoice.find(params[:id])
   end
+
+  def update
+    invoice = Invoice.find(params[:id])
+    invoice_item = InvoiceItem.find(params[:invoice_item_id])
+    invoice_item.update(invoice_item_params)
+    redirect_to merchant_invoice_path(@merchant, invoice)
+  end
+
+  private
+    def invoice_item_params
+      params.permit(:status)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :merchants, except: [:show] do
     resources :items, only: [:index, :show, :edit, :update, :new, :create]
-    resources :invoices, only: [:index, :show]
+    resources :invoices, only: [:index, :show, :update]
   end
 
   namespace :admin do
