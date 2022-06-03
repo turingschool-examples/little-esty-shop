2625675885f4c19b107c91495aca39b7721dc656
Sarah Hall <swatson1994@yahoo.com>
2022-06-03 19:40:51 -0400

    "Create route, view and method for merchant invoices index"

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -3,5 +3,4 @@ class MerchantInvoicesController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,16 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
 
   get "/merchants/:merchant_id/dashboard", to: "merchants#show"
-  
+
   get "/merchants/:merchant_id/items", to: "merchant_items#index"
   get "/merchants/:merchant_id/items/:item_id", to: "merchant_items#show"
 
   get "/merchants/:merchant_id/items/:item_id/edit", to: "merchant_items#edit"
-  patch "merchants/:merchant_id/items/:item_id", to: "merchant_items#update"
+  patch "/merchants/:merchant_id/items/:item_id", to: "merchant_items#update"
 
+  get "/merchants/:merchant_id/invoices/:invoice_id", to: "merchant_invoices#show"
   get "/merchants/:merchant_id/invoices", to: "merchant_invoices#index"
+
 
   get '/admin/invoices', to: 'admin/invoices#index'
   get '/admin/invoices/:id', to: 'admin/invoices#show'
