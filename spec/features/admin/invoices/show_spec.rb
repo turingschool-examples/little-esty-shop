2dd6afaaba1cca4968a2a871d11b97ad8febc38d
Cory Bethune <corybbethune@gmail.com>
2022-06-02 21:02:21 -0700

    Update routes with correct verbage

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -86,7 +86,6 @@ RSpec.describe 'Admin invoices show page' do
 
   it "has a select menu for the invoice status and you can update the status" do
     visit "/admin/invoices/#{@invoice_1.id}"
-    save_and_open_page
 
     expect(page).to have_select(:status, :selected => "in progress")
     select 'completed', from: 'status'
