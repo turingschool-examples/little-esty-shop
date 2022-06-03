165cbf6e1221d94f2efa139bfb9aba59cd4a5cda
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-06-03 07:20:23 -0600

    Validate enum status for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,6 @@ class Item < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :description
   validates_numericality_of :unit_price
+
+  enum status:["Enabled", "Disabled"]
 end
