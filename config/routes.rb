3667bc43637e5333a976b2312ab0105745b0daf7
Chris Kjolhede <ckjolhede@gmail.com>
2022-06-03 02:58:40 -0600

    write TEST for item status changing:  create Welcome page w/ controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'application#welcome'
+  get '/', to: 'welcome#index'
 
   get "/merchants/:merchant_id/dashboard", to: "merchants#show"
   
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "/merchants/:merchant_id/items/:item_id", to: "merchant_items#show"
   get "/merchants/:merchant_id/items/:item_id/edit", to: "merchant_items#edit"
   patch "merchants/:merchant_id/items/:item_id", to: "merchant_items#update"
+  patch "/merchants/:merchant_id/items/:item_id/status_button", to: "merchant_items#status_button"
 
   get "/merchants/:merchant_id/invoices", to: "merchant_invoices#index"
 end
