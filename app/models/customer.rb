831120ae4c123b45ee3f3e03bb47793d66599322
Chris Kjolhede <ckjolhede@gmail.com>
2022-06-03 00:47:09 -0600

    add #dependent: :destroy to all parents models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
-  has_many :invoices 
+  has_many :invoices, dependent: :destroy 
 
   validates_presence_of :first_name
   validates_presence_of :last_name
- end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :merchant
-  has_many :invoice_items
-  has_many :invoices, through: :invoice_items
+  has_many :invoice_items, dependent: :destroy
+  has_many :invoices, through: :invoice_items, dependent: :destroy
 
-  # enum :status [:enabled, :disabled]
+  enum status: {enabled: 0, disabled: 1}
 
   validates_presence_of :name
   validates_presence_of :description
   validates_presence_of :unit_price
-  # validates_presence_of :status
 
 end
