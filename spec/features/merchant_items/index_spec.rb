9982ca5da2f6215fc96f7fb82a5d73813e0f700e
Wesley Garcia <wes@alltradestech.com>
2022-06-04 11:50:05 -0700

    Add test for Merchant Items Index: Top Item's Best Day

--- spec/features/merchant_items/index_spec.rb
+++ spec/features/merchant_items/index_spec.rb
@@ -132,7 +132,7 @@ RSpec.describe 'Merchant Items Index Page' do
     let!(:invoice_item10) { create(:invoice_item, item: items[3], invoice: invoices[2], quantity: 1, unit_price: 4) }
     let!(:invoice_item11) { create(:invoice_item, item: items[4], invoice: invoices[1], quantity: 7, unit_price: 5) }
     let!(:invoice_item12) { create(:invoice_item, item: items[5], invoice: invoices[1], quantity: 9, unit_price: 6) }
-    
+
     # Then I see the names of the top 5 most popular items ranked by total revenue generated
     # And I see that each item name links to my merchant item show page for that item
     # And I see the total revenue generated next to each item name
@@ -140,40 +140,108 @@ RSpec.describe 'Merchant Items Index Page' do
     it "has the top 5 most popular items ranked by total revenue generated" do
       visit "/merchants/#{merchant.id}/items"
       save_and_open_page
-      within "#rightSide2" do 
+      within "#rightSide2" do
         expect(page).to have_content("Top Items")
         expect(items[3].name).to appear_before(items[5].name)
         expect(items[5].name).to appear_before(items[2].name)
         expect(items[2].name).to appear_before(items[4].name)
         expect(items[4].name).to appear_before(items[1].name)
-      end 
-      
+      end
+
       within "#topItem-#{items[1].id}" do
         expect(page).to have_link("#{items[1].name}")
         expect(page).to have_content("#{items[1].name} - $60.00 in sales")
-      end 
-      
+      end
+
       within "#topItem-#{items[2].id}" do
         expect(page).to have_link("#{items[2].name}")
         expect(page).to have_content("#{items[2].name} - $153.00 in sales")
       end
-        
+
       within "#topItem-#{items[3].id}" do
         expect(page).to have_link("#{items[3].name}")
         expect(page).to have_content("#{items[3].name} - $1,280.00 in sales")
-      end 
+      end
 
       within "#topItem-#{items[4].id}" do
         expect(page).to have_link("#{items[4].name}")
         expect(page).to have_content("#{items[4].name} - $120.00 in sales")
-      end 
+      end
 
       within "#topItem-#{items[5].id}" do
         expect(page).to have_link("#{items[5].name}")
         expect(page).to have_content("#{items[5].name} - $400.00 in sales")
         click_link "#{items[5].name}"
-      end 
+      end
       expect(current_path).to eq("/merchants/#{merchant.id}/items/#{items[5].id}")
+    end
+  end
+
+  describe "Merchant Items Index: Top Item's Best Day" do
+    let!(:merchant) { create(:merchant) }
+    let!(:customer) { create(:customer) }
+    let!(:items) { create_list(:item, 6, merchant: merchant) }
+    # let!(:invoices) { create_list(:invoice, 3, customer: customer) }
+
+    let!(:invoice1) { create(:invoice, customer: customer, created_at: "2012-03-10 00:54:09 UTC") }
+    let!(:invoice2) { create(:invoice, customer: customer, created_at: "2012-03-10 00:54:09 UTC") }
+    let!(:invoice3) { create(:invoice, customer: customer, created_at: "2022-03-10 00:54:09 UTC") }
+
+    # let!(:transaction1) { create(:transaction, invoice: invoices[0], result: 1) }
+    # let!(:transaction2) { create(:transaction, invoice: invoices[0], result: 1) }
+    # let!(:transaction3) { create(:transaction, invoice: invoices[1], result: 0) }
+    # let!(:transaction4) { create(:transaction, invoice: invoices[1], result: 1) }
+    # let!(:transaction5) { create(:transaction, invoice: invoices[2], result: 0) }
+    # let!(:transaction6) { create(:transaction, invoice: invoices[2], result: 0) }
+
+    # let!(:invoice_item1) { create(:invoice_item, item: items[0], invoice: invoices[0], quantity: 42, unit_price: 500000000) }
+    # let!(:invoice_item2) { create(:invoice_item, item: items[1], invoice: invoices[1], quantity: 3, unit_price: 2000) }
+    # let!(:invoice_item3) { create(:invoice_item, item: items[2], invoice: invoices[1], quantity: 5, unit_price: 3000) }
+    # let!(:invoice_item4) { create(:invoice_item, item: items[3], invoice: invoices[2], quantity: 16, unit_price: 4000) }
+    # let!(:invoice_item5) { create(:invoice_item, item: items[4], invoice: invoices[1], quantity: 2, unit_price: 6000) }
+    # let!(:invoice_item6) { create(:invoice_item, item: items[5], invoice: invoices[2], quantity: 4, unit_price: 5000) }
+    # let!(:invoice_item7) { create(:invoice_item, item: items[0], invoice: invoices[0], quantity: 6, unit_price: 1) }
+    # let!(:invoice_item8) { create(:invoice_item, item: items[1], invoice: invoices[2], quantity: 22, unit_price: 2) }
+    # let!(:invoice_item9) { create(:invoice_item, item: items[2], invoice: invoices[2], quantity: 63, unit_price: 3) }
+    # let!(:invoice_item10) { create(:invoice_item, item: items[3], invoice: invoices[2], quantity: 1, unit_price: 4) }
+    # let!(:invoice_item11) { create(:invoice_item, item: items[4], invoice: invoices[1], quantity: 7, unit_price: 5) }
+    # let!(:invoice_item12) { create(:invoice_item, item: items[5], invoice: invoices[1], quantity: 9, unit_price: 6) }
+
+    let!(:invoice_item1) { create(:invoice_item, item: items[0], invoice: invoice1, quantity: 1, unit_price: 500000000) }
+    let!(:invoice_item2) { create(:invoice_item, item: items[0], invoice: invoice2, quantity: 1, unit_price: 2000) }
+    let!(:invoice_item3) { create(:invoice_item, item: items[1], invoice: invoice1, quantity: 1, unit_price: 3000) }
+    let!(:invoice_item4) { create(:invoice_item, item: items[1], invoice: invoice2, quantity: 1, unit_price: 4000) }
+    let!(:invoice_item5) { create(:invoice_item, item: items[2], invoice: invoice1, quantity: 1, unit_price: 6000) }
+    let!(:invoice_item6) { create(:invoice_item, item: items[2], invoice: invoice3, quantity: 1, unit_price: 5000) }
+    let!(:invoice_item7) { create(:invoice_item, item: items[3], invoice: invoice1, quantity: 2, unit_price: 1) }
+    let!(:invoice_item8) { create(:invoice_item, item: items[3], invoice: invoice3, quantity: 1, unit_price: 2) }
+    let!(:invoice_item9) { create(:invoice_item, item: items[4], invoice: invoice1, quantity: 1, unit_price: 3) }
+    let!(:invoice_item10) { create(:invoice_item, item: items[4], invoice: invoice3, quantity: 2, unit_price: 4) }
+    let!(:invoice_item11) { create(:invoice_item, item: items[5], invoice: invoice3, quantity: 1, unit_price: 5) }
+    let!(:invoice_item12) { create(:invoice_item, item: items[5], invoice: invoice3, quantity: 1, unit_price: 6) }
+    # Note: use the invoice date. If there are multiple days with equal number of sales, return the most recent day.
+    it "has the date with the most sales for each item" do
+      visit "/merchants/#{merchant.id}/items"
+
+      within "#topItem-#{items[1].id}" do
+        expect(page).to have_content("Top selling date for #{items[1].name} was #{invoice2.created_at}")
+      end
+
+      within "#topItem-#{items[2].id}" do
+        expect(page).to have_content("Top selling date for #{items[2].name} was #{invoice3.created_at}")
+      end
+
+      within "#topItem-#{items[3].id}" do
+        expect(page).to have_content("Top selling date for #{items[3].name} was #{invoice1.created_at}")
+      end
+
+      within "#topItem-#{items[4].id}" do
+        expect(page).to have_content("Top selling date for #{items[4].name} was #{invoice3.created_at}")
+      end
+
+      within "#topItem-#{items[5].id}" do
+        expect(page).to have_content("Top selling date for #{items[5].name} was #{invoice3.created_at}")
+      end
     end
   end
 end
