36e55435e80498bcdd97703a37b586399d1f385a
Luke Swenson <LukeSwenson95@outlook.com>
2022-06-04 07:14:34 -0600

    test: created test to display Admin Dashboard User Story 1

---
+++ spec/features/admin/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Admin Dashboard", type: :feature do
+  describe "Admin User Story 1 - Admin Dashboard" do
+    it "can display a header" do
+      visit '/admin'
+
+      within '#leftSide' do
+      expect(page).to have_content("Admin Dashboard")
+
+      end
+    end
+  end
+
+  
+end
