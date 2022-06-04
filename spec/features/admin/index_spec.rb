4757fd5c1e08c7868edf211a5c8fe6273838b01c
Luke Swenson <LukeSwenson95@outlook.com>
2022-06-04 07:34:13 -0600

    test: Created tests to test links for Admin User story 2: :

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -12,5 +12,23 @@ RSpec.describe "Admin Dashboard", type: :feature do
     end
   end
 
-  
+  describe "Admin User Story 2 - Admin Dashboard Links" do
+    it "has a admin merchants index link and admin invoices index link" do
+      visit '/admin'
+
+      click_link 'Merchants'
+      visit '/admin/merchants'
+
+      expect(current_path).to eq('/admin/merchants')
+
+      visit '/admin'
+
+      click_link 'Invoices'
+      visit '/admin/invoices'
+
+      expect(current_path).to eq('/admin/invoices')
+
+
+    end
+  end
 end
