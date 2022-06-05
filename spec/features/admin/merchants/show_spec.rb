0da879984c4ce017a08bb1435e2d2fddcc5b3cc7
Zachary P <zachary.s.prince@outlook.com>
2022-06-05 12:59:49 -0400

    Refactor test

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'admin merchant show page' do
   it 'shows the name of the merchant' do
     @merch1 = Merchant.create!(name: 'Floopy Fopperations')
 
-    visit "/admin/#{@merch1.id}"
+    visit "/admin/merchants/#{@merch1.id}"
 
-    expect(page).to have_content('Floopy Flopperations')
+    expect(page).to have_content('Name: Floopy Fopperations')
   end
 end
