dbeba2b2c6695ac4d7c029c45a93acf0b1c01ede
Zachary P <zachary.s.prince@outlook.com>
2022-06-05 18:41:51 -0400

    Add incomplete invoices test

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -2,33 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'merchants dashboard' do
   before :each do
-
   end
   it 'shows admin dashboard' do
+    visit '/admin'
 
-    # As an admin,
-    # When I visit the admin dashboard (/admin)
-    # Then I see a header indicating that I am on the admin dashboard
-    visit "/admin"
-
-    expect(page).to have_content("Admin Dashboard")
+    expect(page).to have_content('Admin Dashboard')
   end
 
   it 'shows the admin dashboard with links' do
-
-    # As an admin,
-    # When I visit the admin dashboard (/admin)
-    # Then I see a link to the admin merchants index (/admin/merchants)
-    # And I see a link to the admin invoices index (/admin/invoices)
-    visit "/admin"
+    visit '/admin'
 
     click_link('Admin Merchants Index')
-    expect(current_path).to eq("/admin/merchants")
+    expect(current_path).to eq('/admin/merchants')
 
-    visit "/admin"
+    visit '/admin'
 
     click_link('Admin Invoices Index')
-    expect(current_path).to eq("/admin/invoices")
+    expect(current_path).to eq('/admin/invoices')
+  end
 
+  it 'shows incomplete invoices' do
+    visit '/admin'
+
+    expect(page).to have_content('Incomplete Invoices:')
   end
 end
