d5ecfa479425bfae1042e0266d24af078db69d00
Zachary P <zachary.s.prince@outlook.com>
2022-06-05 13:31:49 -0400

    Add update method for admin merchants

--- app/controllers/admin_merchants_controller.rb
+++ app/controllers/admin_merchants_controller.rb
@@ -10,4 +10,16 @@ class AdminMerchantsController < ApplicationController
   def edit
     @merchant = Merchant.find(params[:id])
   end
+
+  def update
+    merch = Merchant.find(merch_params[:id])
+    merch.update(merch_params)
+    redirect_to "/admin/merchants/#{merch.id}"
+  end
+
+  private
+
+  def merch_params
+    params.permit(:id, :name)
+  end
 end
