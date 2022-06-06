c7bffefd879d033d108798d68e5e8e54253a9d7e
Jim Riddle <jimriddle0@gmail.com>
2022-06-06 14:56:38 -0500

    wrote test for new relationship

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Customer do
     describe "relationships" do
-        it { should have_many :invoices } 
+        it { should have_many :invoices }
+        it { should have_many(:transactions).through(:invoices) } 
     end
-    
+
 end
