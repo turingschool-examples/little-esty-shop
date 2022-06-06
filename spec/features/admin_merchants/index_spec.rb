6b18512e7d47240e67802008aa8b68b9ffb5f678
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-06-05 19:22:11 -0600

    Refactor test data in admin merchants index page

--- spec/features/admin_merchants/index_spec.rb
+++ spec/features/admin_merchants/index_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
-# Admin Merchants Index
-#
-# As an admin,
-# When I visit the admin merchants index (/admin/merchants)
-# Then I see the name of each merchant in the system
-
 RSpec.describe "Admin Merchants Index Page ", type: :feature do
-  # let!(:merchant1) { create(:merchant, status: 0) }
-  # let!(:merchant2) { create(:merchant, status: 1) }
-  # let!(:merchant3) { create(:merchant, status: 0) }
-  # let!(:merchant4) { create(:merchant, status: 1) }
-  # let!(:merchants) { create_list(:merchant, 2) }
-
   let!(:merchants)  { create_list(:merchant, 6, status: 1) }
   let!(:merchants2) { create_list(:merchant, 2, status: 0)}
   let!(:customer) { create(:customer) }
@@ -62,51 +50,33 @@ RSpec.describe "Admin Merchants Index Page ", type: :feature do
     it "has a button to disable or enable each merchant and updates status" do
       visit '/admin/merchants'
 
-      within '#enabledMerchants' do
-        expect(page).to have_button('Disable')
-        expect(page).to have_content(merchant2.name)
-        expect(page).to have_content(merchant4.name)
-
-        expect(page).to_not have_content(merchant1.name)
-        expect(page).to_not have_content(merchant3.name)
-        expect(page).to_not have_button('Enable')
-      end
-      within '#disabledMerchants' do
-        expect(page).to have_button('Enable')
-        expect(page).to have_content(merchant1.name)
-        expect(page).to have_content(merchant3.name)
-
-        expect(page).to_not have_content(merchant2.name)
-        expect(page).to_not have_content(merchant4.name)
-        expect(page).to_not have_button('Disable')
-      end
-      expect(merchant2.status).to eq("enabled")
-      within "#enabled-#{merchant2.id}" do
+      expect(merchants[0].status).to eq("enabled")
+      within "#enabled-#{merchants[0].id}" do
         click_button "Disable"
       end
 
       expect(current_path).to eq("/admin/merchants")
-      merchant2.reload
-      expect(merchant2.status).to eq("disabled")
+      merchants[0].reload
+      expect(merchants[0].status).to eq("disabled")
       within '#enabledMerchants' do
-        expect(page).to_not have_content(merchant2.name)
+        expect(page).to_not have_content(merchants[0].name)
       end
       within '#disabledMerchants' do
-        expect(page).to have_content(merchant2.name)
+        expect(page).to have_content(merchants[0].name)
       end
-      expect(merchant3.status).to eq("disabled")
-      within "#disabled-#{merchant3.id}" do
+      expect(merchants2[1].status).to eq("disabled")
+      within "#disabled-#{merchants2[1].id}" do
         click_button "Enable"
       end
 
       expect(current_path).to eq("/admin/merchants")
-      merchant3.reload
-      expect(merchant3.status).to eq("enabled")
+      merchants2[1].reload
+      expect(merchants2[1].status).to eq("enabled")
       within '#enabledMerchants' do
-        expect(page).to have_content(merchant3.name)
+        expect(page).to have_content(merchants2[1].name)
       end
       within '#disabledMerchants' do
-        expect(page).to_not have_content(merchant3.name)
+        expect(page).to_not have_content(merchants2[1].name)
       end
     end
 
@@ -117,16 +87,16 @@ RSpec.describe "Admin Merchants Index Page ", type: :feature do
 
       within "#disabledMerchants" do
         expect(page).to have_button('Enable')
-        expect(page).to have_content(merchant1.name)
-        expect(page).to have_content(merchant3.name)
-        expect(page).to_not have_content(merchant2.name)
+        expect(page).to have_content(merchants2[0].name)
+        expect(page).to have_content(merchants2[1].name)
+        expect(page).to_not have_content(merchants[0].name)
       end
 
       within "#enabledMerchants" do
         expect(page).to have_button('Disable')
-        expect(page).to have_content(merchant2.name)
-        expect(page).to have_content(merchant4.name)
-        expect(page).to_not have_content(merchant3.name)
+        expect(page).to have_content(merchants[0].name)
+        expect(page).to have_content(merchants[1].name)
+        expect(page).to_not have_content(merchants2[0].name)
       end
     end
   end
