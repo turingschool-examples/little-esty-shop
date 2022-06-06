2d3aaf16e60c1dc4253bba4b9ba18ebb98f6a6f5
Nick Jacobs <nickjacobsatc@gmail.com>
2022-06-06 18:53:05 -0500

    added relationship

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,7 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  has_many :transactions, through: :invoice
 
   validates_presence_of :quantity
   validates_presence_of :unit_price
