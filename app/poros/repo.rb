65d347c4466ac7110763081789f0564a7a069609
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-06-07 07:54:21 -0600

    Add files for API coverage and tested it on admin index - passing

--- app/poros/repo.rb
+++
@@ -1,7 +0,0 @@
-class Repo
-  attr_reader :name
-
-  def initialize(data)
-    @name = data[:name]
-  end
-end
--- spec/poros/repository_spec.rb
+++ spec/poros/repository_spec.rb
@@ -1,12 +1,12 @@
 # poro stands for plain old ruby object
 require 'rails_helper'
 
-RSpec.describe Repo do 
+RSpec.describe Repository do 
   it 'exists and has attributes' do 
     data = { name: 'Ruby Rhods Rails Renegades Test Repo' } # will probably add to this
-    repo = Repo.new(data)
+    repo = Repository.new(data)
 
-    expect(repo).to be_an_instance_of(Repo)
+    expect(repo).to be_an_instance_of(Repository)
     expect(repo.name).to eq(data[:name])
   end
 end
