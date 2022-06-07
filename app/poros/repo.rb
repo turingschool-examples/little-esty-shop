7a334e1aa45efc4a0cb4dc326aa6bb7d37521f12
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-06-06 19:47:05 -0600

    Pass poros test

---
+++ app/poros/repo.rb
@@ -0,0 +1,7 @@
+class Repo
+  attr_reader :name
+
+  def initialize(data)
+    @name = data[:name]
+  end
+end
--- spec/poros/repo_spec.rb
+++ spec/poros/repo_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 RSpec.describe Repo do 
   it 'exists and has attributes' do 
     data = { name: 'Ruby Rhods Rails Renegades Test Repo' } # will probably add to this
-    repo = Repo.new
+    repo = Repo.new(data)
 
-    expect(repo).to be_an_instance_of(Repository)
+    expect(repo).to be_an_instance_of(Repo)
     expect(repo.name).to eq(data[:name])
   end
 end
