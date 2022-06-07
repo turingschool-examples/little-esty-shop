a08ef6b0f6285d45166e810e5eb706126734075b
Wesley Garcia <wes@alltradestech.com>
2022-06-06 17:51:51 -0700

    Add sad path coverage for New Merchants and Updated Merchants

--- app/controllers/admin_merchants_controller.rb
+++ app/controllers/admin_merchants_controller.rb
@@ -17,24 +17,28 @@ class AdminMerchantsController < ApplicationController
   end
 
   def create
-    Merchant.create!(merchant_params)
-    redirect_to merchants_path
+    merchant = Merchant.new(merchant_params)
+    if merchant.save
+      redirect_to merchants_path, notice: 'Merchant Created Successfully'
+    else
+      redirect_to merchants_path, notice: 'A Required Field Was Missing; Merchant Not Created'
+    end
   end
 
   def update
     merchant = Merchant.find(params[:id])
     if params[:update_status_to] == "disabled"
       merchant.disabled!
-
       redirect_to "/admin/merchants"
     elsif params[:update_status_to] == "enabled"
       merchant.enabled!
       redirect_to "/admin/merchants"
-    else
-      merchant = Merchant.find(params[:id])
-      merchant.update(merchant_params)
+    elsif merchant.update(merchant_params)
       redirect_to "/admin/merchants/#{merchant.id}"
       flash[:notice] = 'Merchant Successfully Updated'
+    else
+      redirect_to "/admin/merchants/#{merchant.id}"
+      flash[:notice] = 'A Required Field Was Missing; Merchant Not Updated'
     end
   end
 
