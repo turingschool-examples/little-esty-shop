71c059548702eaea2d1029152605b05b7efd486a
Zachary Hazelwood <zhaze001@gmail.com>
2022-06-07 11:09:45 -0600

    Feat: GithubService can pull response data

--- Gemfile
+++ Gemfile
@@ -36,6 +36,11 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'faraday'
+gem 'factory_bot_rails'
+gem 'faker'
+gem 'standardrb'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'rspec-rails'
@@ -45,6 +50,10 @@ group :development, :test do
   gem 'orderly'
   gem 'pry'
   gem 'simplecov'
+  gem 'json'
+  gem 'httparty'
+  gem 'vcr'
+  gem 'webmock'
 end
 
 group :development do
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -33,7 +33,7 @@ class Merchant < ApplicationRecord
   def self.disabled
     where(status: 0)
   end
-    
+
   def top_five_favorite_customers
     customers.joins(invoices: :transactions)
     .where(transactions: { result: true })
@@ -41,5 +41,14 @@ class Merchant < ApplicationRecord
     .group('customers.id')
     .order(transaction_count: :desc)
     .limit(5)
+  end
+
+
+  def top_5_merchants_by_revenue
+    joins(:invoices, [:transactions, :invoice_items]).
+    select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue').
+    group(:id).
+    order(revenue: :desc).
+    limit(5)
   end
 end
