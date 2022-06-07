f39a8a4de49668882c667d81ca38796ba1d84bc7
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-07 16:44:08 -0600

    Ref: merchant items resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
 
   get "/merchants/:merchant_id/dashboard", to: 'merchants#show'
 
-  get "/merchants/:merchant_id/items", to: 'merchant_items#index'
-  patch "/merchants/:merchant_id/items", to: 'merchant_items#update'
-  get "/merchants/:merchant_id/items/new", to: 'merchant_items#new'
-  post "/merchants/:merchant_id/items", to: 'merchant_items#create'
+  resources :merchants do
+    resources :items, controller: :merchant_items
+  end
+  # get "/merchants/:merchant_id/items", to: 'merchant_items#index'
+  # patch "/merchants/:merchant_id/items", to: 'merchant_items#update'
+  # get "/merchants/:merchant_id/items/new", to: 'merchant_items#new'
+  # post "/merchants/:merchant_id/items", to: 'merchant_items#create'
 
   get "/merchants/:merchant_id/items/:item_id", to: 'merchant_items#show'
   get "/merchants/:merchant_id/items/:item_id/edit", to: 'merchant_items#edit'
