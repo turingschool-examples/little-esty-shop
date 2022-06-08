300a74bfa2b13c5a193a38e12c04a7b64372653f
Chris Kjolhede <ckjolhede@gmail.com>
2022-06-07 18:35:50 -0600

    ADD .DS_Store to gitignore. Write TEST for item best day item index

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -124,6 +124,7 @@ RSpec.describe Item, type: :model do
       @invoice_itemZ = @invoiceZ.invoice_items.create!(quantity: 1, unit_price: 100, item_id: @itemZ.id, status: "shipped")
       @invoice_itemX = @invoiceX.invoice_items.create!(quantity: 1, unit_price: 1200, item_id: @itemX.id, status: "shipped")
       visit "merchants/#{@merchant1.id}/items"
+      
       within("#top5items") do
         expect("itemA").to appear_before("itemB")
         expect("itemB").to appear_before("itemD")
@@ -131,6 +132,54 @@ RSpec.describe Item, type: :model do
         expect("itemC").to appear_before("itemF")
         expect(page).to_not have_content("itemZ") #6th highest total revenue 
         expect(page).to_not have_content("itemX") #transactions result not success
+      end
+    end
+
+    it "shows the top sales date for each of the top 5 items" do
+
+      @merchant1 = Merchant.create!(name: 'Merchant1')
+      @customer1 = Customer.create!(first_name: "Cuss", last_name: "Tomer")
+      @itemA = @merchant1.items.create!(name: 'itemA', description: 'Description1', unit_price: 222, status: 0)
+      @itemB = @merchant1.items.create!(name: 'itemB', description: 'Descriptions', unit_price: 222,status: 0)
+      @itemD = @merchant1.items.create!(name: 'itemD', description: 'Descriptive', unit_price: 222, status: 0)
+      @itemC = @merchant1.items.create!(name: 'itemC', description: 'Descriptionless', unit_price: 222, status: 0)
+      @itemF = @merchant1.items.create!(name: 'itemF', description: 'Descriptionulous', unit_price: 222, status: 0)
+
+      @invoiceA = @customer1.invoices.create!(status: "Completed", created_at: "2012-03-15 18:00:00 UTC")
+      @invoiceB = @customer1.invoices.create!(status: "Completed", created_at: "2012-03-16 18:00:00 UTC")
+      @invoiceD = @customer1.invoices.create!(status: "Completed", created_at: "2012-03-17 18:00:00 UTC")
+      @invoiceC = @customer1.invoices.create!(status: "Completed", created_at: "2012-03-18 18:00:00 UTC")
+      @invoiceF = @customer1.invoices.create!(status: "Completed", created_at: "2012-03-19 18:00:00 UTC")
+      @invoiceBB = @customer1.invoices.create!(status: "Completed", created_at: "2012-03-15 18:00:00 UTC")
+      @invoiceAA = @customer1.invoices.create!(status: "Completed", created_at: "2012-03-16 18:00:00 UTC")
+      @invoiceCC = @customer1.invoices.create!(status: "Completed", created_at: "2012-03-17 18:00:00 UTC")
+      @invoiceFF = @customer1.invoices.create!(status: "Completed", created_at: "2012-03-18 18:00:00 UTC")
+      @invoiceDD = @customer1.invoices.create!(status: "Completed", created_at: "2012-03-18 18:00:00 UTC")
+
+      @transaction1 = @invoiceA.transactions.create!(credit_card_number: "1234567891234567", credit_card_expiration_date: "1234", result: "success")
+      @transaction2 = @invoiceB.transactions.create!(credit_card_number: "1234567891234567", credit_card_expiration_date: "1234", result: "success" )
+      @transaction3 = @invoiceD.transactions.create!(credit_card_number: "1234567891234567", credit_card_expiration_date: "1234", result: "success" )
+      @transaction4 = @invoiceC.transactions.create!(credit_card_number: "1234567891234567", credit_card_expiration_date: "1234", result: "success" )
+      @transaction5 = @invoiceF.transactions.create!(credit_card_number: "1234567891234567", credit_card_expiration_date: "1234", result: "success" )
+      @transaction6= @invoiceAA.transactions.create!(credit_card_number: "12345678912134567", credit_card_expiration_date: "1234", result: "success" )
+      @transaction7= @invoiceBB.transactions.create!(credit_card_number: "12345678912134567", credit_card_expiration_date: "1234", result: "success" )
+      @transaction8= @invoiceDD.transactions.create!(credit_card_number: "12345678912134567", credit_card_expiration_date: "1234", result: "success" )
+      @transaction9= @invoiceCC.transactions.create!(credit_card_number: "12345678912134567", credit_card_expiration_date: "1234", result: "success" )
+      @transaction10 = @invoiceFF.transactions.create!(credit_card_number: "12345678912134567", credit_card_expiration_date: "1234", result: "success" )
+      @invoice_itemA = @invoiceA.invoice_items.create!(quantity: 13, unit_price: 41, item_id: @itemA.id, status: "shipped")
+      @invoice_itemB = @invoiceB.invoice_items.create!(quantity: 11, unit_price: 33, item_id: @itemB.id, status: "shipped")
+      @invoice_itemD = @invoiceD.invoice_items.create!(quantity: 7, unit_price: 5, item_id: @itemD.id, status: "shipped")
+      @invoice_itemC = @invoiceC.invoice_items.create!(quantity: 5, unit_price: 7, item_id: @itemC.id, status: "shipped")
+      @invoice_itemF = @invoiceF.invoice_items.create!(quantity: 3, unit_price: 13, item_id: @itemF.id, status: "shipped")
+      @invoice_itemBB= @invoiceBB.invoice_items.create!(quantity: 17, unit_price: 33, item_id: @itemB.id, status: "shipped")
+      @invoice_itemDD= @invoiceDD.invoice_items.create!(quantity: 11, unit_price: 5, item_id: @itemD.id, status: "shipped")
+      @invoice_itemCC= @invoiceCC.invoice_items.create!(quantity: 23, unit_price: 7, item_id: @itemC.id, status: "shipped")
+      @invoice_itemFF= @invoiceFF.invoice_items.create!(quantity: 17, unit_price: 13, item_id: @itemF.id, status: "shipped")
+      @invoice_itemAA= @invoiceAA.invoice_items.create!(quantity: 19, unit_price: 41, item_id: @itemA.id, status: "shipped")
+      visit "/merchants/#{@merchant1.id}/items"
+
+      within("top_day-#{@itemA.id}") do
+        expect(page).to have_content("Top selling date for itemA was 2012-03-06")
       end
     end
   end
