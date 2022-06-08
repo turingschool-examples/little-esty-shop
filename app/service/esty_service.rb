374383ffc050828ddd692f3aa046d37eeba385e4
Andrew King <dev.andrewking@gmail.com>
2022-06-07 23:11:20 -0500

    add contributors url

--- app/service/esty_service.rb
+++ app/service/esty_service.rb
@@ -4,6 +4,10 @@ class EstyService
     get_url("https://api.github.com/repos/ColinReinhart/little-esty-shop")
   end
 
+  def repo_usernames
+    get_url("https://api.github.com/repos/ColinReinhart/little-esty-shop/contributors")
+  end
+
   def get_url(url)
     response = HTTParty.get(url)
     JSON.parse(response.body, symbolize_names: true)
