655bd7dc169906943bd3e5a062efe81487b3bd37
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-08 14:26:49 -0600

    Ref: relationship test for new relationships in merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,7 +3,6 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
-
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
 
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Merchant do
     it { should have_many :items}
     it { should have_many(:invoice_items).through(:items)}
     it { should have_many(:invoices).through(:invoice_items)}
+    it { should have_many(:customers).through(:invoices)}
+    it { should have_many(:transactions).through(:invoices)}
+  
   end
 
   describe 'validations' do
