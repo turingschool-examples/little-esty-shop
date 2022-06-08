584a67abc4da514cd111e3a3637b4208cce743e4
Wesley Garcia <wes@alltradestech.com>
2022-06-07 17:17:25 -0700

    Clear vcr errors and minor changes to tests

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe GithubService do 
+RSpec.describe GithubService do
   #stories live here
-  describe 'enpoint consumption' do  # endpoint is anything after the dot.com 
+  describe 'endpoint consumption' do  # endpoint is anything after the dot.com
     it 'retrieves repo data from endpoint', :vcr do # tells it we are making a fetch call and records it in the cassette
-      json = GithubService.get_repo_data # specifying route
+      githubservice_instance = GithubService.new
+      json = githubservice_instance.repo # specifying route
       expect(json).to have_key(:name) #comes from api github browser - changes strings to symbols in the base server
     end
 
