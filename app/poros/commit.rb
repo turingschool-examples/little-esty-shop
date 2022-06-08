486d317ea52ef0b98168b280c7d903eca18b3bae
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-08 16:20:35 -0700

    Fix Commit in footer layout.

--- app/poros/commit.rb
+++ app/poros/commit.rb
@@ -4,19 +4,13 @@ class Commit
 
   def initialize(commits)
     @commits = commits
+    @commit_count_hash = Hash.new(0)
   end
 
   def count(contributor)
-    @usernames.each do |user|
-      @commit_count_hash[user] += 1
+    @commits.each do |commit|
+      @commit_count_hash[commit[:author][:login]] += 1
     end
     @commit_count_hash[contributor]
   end
 end
-  #
-  # def count(contributor)
-  #   @usernames.each do |user|
-  #     @commit_count_hash[user] += 1
-  #   end
-  #   @commit_count_hash[contributor]
-  # end
--- spec/poros/commit_spec.rb
+++ spec/poros/commit_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Commit do
   it "exists and has attributes" do
-    data = "commit": {"author": {"login": "Eric",}
+    data = {"author": {"login": "Eric"}}
     commit = Commit.new(data)
     expect(commit).to be_an_instance_of(Commit)
     expect(commit.count).to eq(1)
