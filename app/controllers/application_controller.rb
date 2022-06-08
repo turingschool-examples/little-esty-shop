093279ab6c452ba46a1c4bf825a92196d128378f
Colin Reinhart <colinbreinhartgmail.com>
2022-06-07 21:31:27 -0600

    API Commits Complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 require './app/facade/repo_search'
 class ApplicationController < ActionController::Base
-  
-  # before_action :repo_name
+
+  # before_action :repo_name,
+  #               :number_of_commits
 
   def welcome
 
   end
 
   private
-  
+
   def error_message(errors)
     errors.full_messages.join(', ')
   end
@@ -16,5 +17,10 @@ class ApplicationController < ActionController::Base
   # def repo_name
   #   repo_search = RepoSearch.new
   #   @repo = repo_search.repo_information
+  # end
+
+  # def number_of_commits
+  #   commit_search = CommitSearch.new
+  #   @commits = commit_search.commit_information
   # end
 end
--- app/service/esty_service.rb
+++ app/service/esty_service.rb
@@ -1,7 +1,12 @@
-class EstyService  
+require 'httparty'
+class EstyService
 
   def repo
     get_url("https://api.github.com/repos/ColinReinhart/little-esty-shop")
+  end
+
+  def commits(url)
+    get_url(url)
   end
 
   def get_url(url)
