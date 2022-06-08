a65106a985153946b30298a423ae8ef9153503ef
Zachary P <zachary.s.prince@outlook.com>
2022-06-08 10:21:33 -0400

    Remove debugger entry point

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -10,14 +10,10 @@ class MerchantInvoicesController < ApplicationController
   end
 
   def update
-    # require "pry"; binding.pry
     merch = Merchant.find(params[:id])
     invoice = Invoice.find(params[:invoice_id])
     invoice_item = InvoiceItem.find(params[:invoice_item_id])
     invoice_item.update(status: params[:status])
     redirect_to "/merchants/#{merch.id}/invoices/#{invoice.id}"
-
-    # require "pry"; binding.pry
   end
-
 end
