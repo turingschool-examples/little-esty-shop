3021c2a5630aa15ca4b3006cbca76d63fdeb984b
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-06-08 17:24:44 -0400

    Troubleshooting/refactoring from merge conflicts

---
+++ spec/poros/contributor_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Contributor do
+  it "exists and has attributes" do
+    data = { login: 'Fake Repo' }
+    contributor = Contributor.new(data)
+
+    expect(contributor).to be_an_instance_of(Contributor)
+    expect(contributor.login).to eq(data[:login])
+  end
+end
--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -2,34 +2,24 @@
 require 'rails_helper'
 
 RSpec.describe GithubService do
-  # describe "API repo endpoint" do
-  #   it 'gets repo data from Github endpoint', :vcr do
-  #     json = GithubService.get_repo_data
-  #     expect(json).to have_key(:name)
-  #   end
-  # end
-  #
-  # describe "API contributors endpoint" do
-  #   it 'gets contributor data from Github endpoint', :vcr do
-  #     json = GithubService.get_contributor_data
-  #     expect(json).to have_key(:login)
-  #   end
-  # end
- end
+  describe "API repo endpoint" do
+    it 'gets repo data from Github endpoint', :vcr do
+      json = GithubService.get_repo_data
+      expect(json).to have_key(:name)
+    end
+  end
 
-# require 'rails_helper'
-#
-# RSpec.describe GithubService do
-#   describe "API repo endpoint" do
-#     it 'gets repo data from Github endpoint', :vcr do
-#       json = GithubService.get_repo_data
-#       expect(json).to have_key(:name)
-#     end
-#
-#     it 'gets pull count from Github endpoint', :vcr do
-#       json = GithubService.get_pull_data
-#       require "pry"; binding.pry
-#       expect(json).to be_an(Array)
-#     end
-#   end
-# end
+  describe "API contributors endpoint" do
+    it 'gets contributor data from Github endpoint', :vcr do
+      json = GithubService.get_contributor_data
+      expect(json[0]).to have_key(:login)
+    end
+  end
+
+  describe "API pull request endpoint" do
+    it 'gets pull count from Github endpoint', :vcr do
+      json = GithubService.get_pull_data
+      expect(json).to be_an(Array)
+    end
+  end
+end
