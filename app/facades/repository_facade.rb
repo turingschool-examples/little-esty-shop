c2d6befe48b5355fbabdc0111017ee1b459ce388
Sage Skaff <skaffsage@gmail.com>
2022-06-07 18:03:28 -0700

    Add merged or error and create merges methods

--- app/facades/repository_facade.rb
+++ app/facades/repository_facade.rb
@@ -10,7 +10,7 @@ class RepositoryFacade
   end
 
   def self.contributor_or_error # this is the ternary operator - one liner for a condition
-    json = service.repo
+    json = service.contributor
     json[:message].nil? ? create_contributors : json # if json message is nil we're going to return the instance of the repo, otherwise we are  going to return the error message(json) - will call this in the view this whole thing is a conditional for the rate limit
   end
 
@@ -19,8 +19,19 @@ class RepositoryFacade
       Contributor.new(data)
     end
     json.map do |contributor|
-      contributor if [98674727, 98676136, 98354482, 99838997].include?(contributor.id)
+      contributor if [98_674_727, 98_676_136, 98_354_482, 99_838_997].include?(contributor.id)
     end.compact
+  end
+
+  def self.merged_or_error
+    json = service.merge
+    json[:message].nil? ? create_merges : json
+  end
+
+  def self.create_merges
+    json = service.merge.map do |data|
+      Merge.new(data)
+    end
   end
 
   def self.service
