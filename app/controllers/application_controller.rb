b82ffda1fa2fddd974a4fd297f7b474beda4a443
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-09 09:25:09 -0600

    Revert "API Verification"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 require './app/facade/repo_search'
 class ApplicationController < ActionController::Base
 
-  before_action :repo_name,
-                :number_of_commits,
-                :usernames,
-                :pr_count,
-                :avatar
+
+  # before_action :repo_name,
+  #               :number_of_commits
+
+  
+
+  # before_action :repo_name
+  # before_action :usernames
+  # before_action :pr_count
+  # before_action :avatar
+
+
   def welcome
 
   end
@@ -16,29 +23,35 @@ class ApplicationController < ActionController::Base
     errors.full_messages.join(', ')
   end
 
-  def repo_name
-    repo_search = RepoSearch.new
-    @repo = repo_search.repo_information
-  end
+  # def repo_name
+  #   repo_search = RepoSearch.new
+  #   @repo = repo_search.repo_information
+  # end
 
-  def number_of_commits
-    commit_search = CommitSearch.new
-    @commits = commit_search.commit_information
-  end
 
-  def pr_count
-    prc = EstyService.new
-    @count = prc.prs.count
-  end
+  # def number_of_commits
+  #   commit_search = CommitSearch.new
+  #   @commits = commit_search.commit_information
 
-  def usernames
-    username_search = UserSearch.new
-    @usernames = username_search.repo_usernames
-  end
+  # def pr_count
+  #   prc = EstyService.new
+  #   @count = prc.prs.count
 
-  def avatar
-    avatar_search = AvatarSearch.new
-    @avatar_urls = avatar_search.repo_avatars
-  end
+  # end
 
+  # def usernames
+  #   username_search = UserSearch.new
+  #   @usernames = username_search.repo_usernames
+  # end
+
+  # def avatar
+  #   avatar_search = AvatarSearch.new
+  #   @avatar_urls = avatar_search.repo_avatars
+  # end
+
+
+  #add this to the layout to get the avatars to display
+  # <% @avatar_urls.each do |url| %>
+  #  <img src=<%= url %>>
+  # <% end %>
 end
--- spec/features/api_spec.rb
+++ spec/features/api_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe 'tests for API service methods' do
   end
 
 
-  it "can display the avatars for all the team members on every page" do
+  xit "can display the avatars for all the team members on every page" do
     visit '/admin/invoices'
-
+    
     expect(page).to have_css("img[src='https://avatars.githubusercontent.com/u/98759023?v=4']")
     expect(page).to have_css("img[src='https://avatars.githubusercontent.com/u/60715457?v=4']")
     expect(page).to have_css("img[src='https://avatars.githubusercontent.com/u/95403666?v=4']")
     expect(page).to have_css("img[src='https://avatars.githubusercontent.com/u/98788282?v=4']")
     expect(page).to have_css("img[src='https://avatars.githubusercontent.com/u/97638081?v=4']")
   end
-
-  it 'can display all contributor usernames' do
+  
+  xit 'can display all contributor usernames' do
     visit '/admin/merchants'
     expect(page).to have_content("devAndrewK")
     expect(page).to have_content("tjhaines-cap")
@@ -26,14 +26,14 @@ RSpec.describe 'tests for API service methods' do
     expect(page).to have_content("ColinReinhart")
   end
 
-  it "displays the repo name" do
+  xit "displays the repo name" do
     visit "admin/merchants"
 
     expect(page).to have_content("Repo: little-esty-shop")
 
   end
 
-  it "displays each users amount of commits" do
+  xit "displays each users amount of commits" do
     visit '/'
 
     expect(page).to have_content("devAndrewK has _ commits")
