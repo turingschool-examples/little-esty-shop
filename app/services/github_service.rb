62ba49c2595e8af349dc7b9e006785a75b1fdd7f
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-06-09 11:39:40 -0600

    Comment out github service

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,24 +1,24 @@
-class GithubService < BaseService
-
-  def self.get_repo_data
-    response = conn('https://api.github.com').get('/repos/ruezheng/little-esty-shop')
-    get_json(response)
-  end
-
-
-  def self.get_contributor_data
-    response = conn('https://api.github.com').get('/repos/ruezheng/little-esty-shop/contributors')
-    get_json(response)
-  end
-
-  def self.get_pull_data
-    response = conn('https://api.github.com').get('/repos/ruezheng/little-esty-shop/pulls?state=closed&per_page=100')
-    get_json(response)
-  end
-
-  def self.get_commit_data
-  response = conn('https://api.github.com').get('/repos/ruezheng/little-esty-shop/commits?state=closed&per_page=100')
-  get_json(response)
-end
-
-end
+# class GithubService < BaseService
+#
+#   def self.get_repo_data
+#     response = conn('https://api.github.com').get('/repos/ruezheng/little-esty-shop')
+#     get_json(response)
+#   end
+#
+#
+#   def self.get_contributor_data
+#     response = conn('https://api.github.com').get('/repos/ruezheng/little-esty-shop/contributors')
+#     get_json(response)
+#   end
+#
+#   def self.get_pull_data
+#     response = conn('https://api.github.com').get('/repos/ruezheng/little-esty-shop/pulls?state=closed&per_page=100')
+#     get_json(response)
+#   end
+#
+#   def self.get_commit_data
+#   response = conn('https://api.github.com').get('/repos/ruezheng/little-esty-shop/commits?state=closed&per_page=100')
+#   get_json(response)
+# end
+#
+# end
