f5b3443ee0c59192bc8237719a9d036ceaad0c22
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-06-11 00:18:55 -0600

    Feat: add delete button next to each discount

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -2,7 +2,7 @@
 # name all services find_...
 
 class DiscountsController < ApplicationController
-  before_action :find_merchant, only: [:index, :show, :new, :create]
+  before_action :find_merchant, only: [:index, :show, :new, :create, :destroy]
 
   def index
     @discounts = Discount.all
@@ -22,7 +22,7 @@ class DiscountsController < ApplicationController
   end
 
   def destroy
-    @discount = Discount.find(params[:id])
+    Discount.find_by(merchant_id: params[:merchant_id], id: params[:id]).destroy
     redirect_to merchant_discounts_path(@merchant)
   end
 
--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -53,15 +53,6 @@ RSpec.describe "merchant's bulk discounts index" do
     expect(current_path).to eq(new_merchant_discount_path(merchant1))
   end
 
-  # Merchant Bulk Discount Delete
-  #
-  # As a merchant
-  # When I visit my bulk discounts index
-  # Then next to each bulk discount I see a link to delete it
-  # When I click this link
-  # Then I am redirected back to the bulk discounts index page
-  # And I no longer see the discount listed
-
   it "has a delete button next to each discount", :vcr do
     visit "/merchants/#{merchant1.id}/discounts"
 
@@ -69,10 +60,10 @@ RSpec.describe "merchant's bulk discounts index" do
       expect(page).to have_content('20% Discount')
       expect(page).to have_content('Quantity: 10')
       click_button "Remove Discount"
-      expect(page).to_not have_content('20% Discount')
-      expect(page).to_not have_content('Quantity: 10')
     end
 
     expect(current_path).to eq("/merchants/#{merchant1.id}/discounts")
+    expect(page).to_not have_content('20% Discount')
+    expect(page).to_not have_content('Quantity: 10')
   end
 end
