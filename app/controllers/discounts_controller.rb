12f362da3528a25dc339d55d16b712fa1cef8d3e
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-06-10 21:07:00 -0600

    feat: add new and create actions to discounts controller

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,5 +1,8 @@
+# name all facades get_...
+# name all services find_...
+
 class DiscountsController < ApplicationController
-  before_action :find_merchant, only: [:index, :show]
+  before_action :find_merchant, only: [:index, :show, :new, :create]
 
   def index
     @discounts = Discount.all
@@ -12,7 +15,14 @@ class DiscountsController < ApplicationController
 
   def new
   end
-end
 
-# name all facades get_...
-# name all services find_...
+  def create
+    @merchant.items.create!(discount_params)
+    redirect_to merchant_discounts_path(@merchant)
+  end
+
+  private
+  def discount_params
+    params.permit(:percentage, :quantity_threshold)
+  end
+end
