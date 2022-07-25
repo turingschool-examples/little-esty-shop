3b488bed2f12bfb5e9c9eb587d1dc14c63f83189
Tyler Caudill <teecaud@hotmail.com>
2022-07-25 17:54:36 -0500

    feat transactions rake task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_25_224239) do
+ActiveRecord::Schema.define(version: 2022_07_25_225059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 2022_07_25_224239) do
 
   create_table "merchants", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "credit_card_number"
+    t.integer "credit_card_expiration_date"
+    t.string "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
