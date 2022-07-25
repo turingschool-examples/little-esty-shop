abc9b5c18260957a7b400907294a7d6b77d3729d
Thomas Turner <thomasturner482@yahoo.com>
2022-07-25 18:49:26 -0500

    add relationships and tests for customer, invoice_item, incoice

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
-  validates_presence_of :first_name
-  validates_presence_of :last_name
+  validates_presence_of :first_name, presence: true
+  validates_presence_of :last_name, presence: true
+  
   has_many :invoices, dependent: :destroy
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,8 @@
 class InvoiceItem < ApplicationRecord
+  validates_presence_of :quantity, presence: true, numericality: true
+  validates_presence_of :unit_price, presence: true, numericality: true
+  validates_presence_of :status, presence: true, numericality: true
   
+  belongs_to :item
+  belongs_to :invoice
 end
