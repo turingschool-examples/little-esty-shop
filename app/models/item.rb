77e90c4ec168e6fe03924f85579bb22eed293296
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-25 17:10:13 -0600

    add item table with validation tests

---
+++ app/models/item.rb
@@ -0,0 +1,7 @@
+class Item < ApplicationRecord
+    validates_presence_of :name
+    validates_presence_of :description
+    validates_presence_of :unit_price
+    validates_presence_of :created_at
+    validates_presence_of :updated_at
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_25_225853) do
+ActiveRecord::Schema.define(version: 2022_07_25_230641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2022_07_25_225853) do
 
   create_table "invoices", force: :cascade do |t|
     t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
