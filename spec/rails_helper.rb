2c9301b3b0ca05945e6acf6d9b171e4ea613b280
Tyler Caudill <teecaud@hotmail.com>
2022-07-25 17:58:14 -0500

    feat additional gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,10 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+
+# open coverage/index.html# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
+require 'simplecov'
+SimpleCov.start
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -61,4 +64,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
