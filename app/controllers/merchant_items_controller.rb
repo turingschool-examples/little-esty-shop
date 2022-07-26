036c1c74ab821b9c4c025782c00c97b3ab8c8246
Blake Saylor <blakesaylor@gmail.com>
2022-07-26 15:47:04 -0600

    Add show method

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -2,4 +2,8 @@ class MerchantItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    @item = Item.find(params[:item_id])
+  end
 end
