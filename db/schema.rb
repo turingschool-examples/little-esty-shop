43e8c15269e5fba8448b0793062fa3cce04c9895
Sunny Moore <moore.sunny@gmail.com>
2022-07-25 19:59:56 -0700

    Add One to many relationship for items/merchant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_024358) do
+ActiveRecord::Schema.define(version: 2022_07_26_025411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 2022_07_26_024358) do
     t.string "name"
     t.string "description"
     t.integer "unit_price"
+    t.bigint "merchant_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
   create_table "merchants", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema.define(version: 2022_07_26_024358) do
     t.datetime "updated_at"
   end
 
+  add_foreign_key "items", "merchants"
 end
