7d528ae0179d971e55cdab7aa6b86f09c32d427e
Blake Saylor <blakesaylor@gmail.com>
2022-07-26 10:36:32 -0600

    Remove numericality test for result

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
     validates_presence_of :credit_card_number
-    validates :result, presence: true, numericality: true
+    validates :result, presence: true
 
     belongs_to :invoice
 end
