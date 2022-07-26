834f946eeba8673e2ad98c83b36b840e50a58a03
Eli Sachs <easachs13@gmail.com>
2022-07-25 22:04:43 -0600

    Create merchant dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/merchants/:id/dashboard", to: "merchants#show"
+  get "/merchants/:id/items", to: "items#index"
+  get "/merchants/:id/invoices", to: "invoices#index"
 end
