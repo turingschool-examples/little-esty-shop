10e2277b28e7c2bc7032ad20dcb272b113744d39
Sage Skaff <skaffsage@gmail.com>
2022-07-26 12:04:30 -0700

    Update relationships

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,10 @@
 class Invoice < ApplicationRecord
-  enum status: {cancelled: 0, in_progress: 1, completed: 2}
+  enum status: { cancelled: 0, in_progress: 1, completed: 2 }
 
+  has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
 
   belongs_to :customer
 end
+
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Invoice do
   describe 'relationships' do
     it { should belong_to :customer }
     it { should have_many :invoice_items }
+    it { should have_many :transactions }
     it { should have_many(:items).through(:invoice_items) }
   end
 end
+
