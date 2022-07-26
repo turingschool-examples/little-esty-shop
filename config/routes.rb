981452860123cef8cd276ae759766c6f507f2f31
Blake Saylor <blakesaylor@gmail.com>
2022-07-26 15:46:24 -0600

    Add route for merchant item show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/merchants/:merchant_id/items', to: 'merchant_items#index'
+  get '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#show'
 end
