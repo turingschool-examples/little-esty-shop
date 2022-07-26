6e98924b6836116525fec5d475dc2303f831a9f1
Bryce Simonds <bsimonds14@gmail.com>
2022-07-26 16:39:44 -0600

    Test: has name of the merchant

---
+++ spec/features/merchants/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Show Dashboard' do 
+    it 'has the name of the merchant' do 
+        merchant_1 = Merchant.create!(name: 'Spongebob The Merchant')
+        merchant_2 = Merchant.create!(name: 'Jon Doe')
+        
+        visit "/merchants/#{merchant_1.id}/dashboard"
+
+        expect(page).to have_content('Spongebob The Merchant')
+        expect(page).to_not have_content('Jon Doe')
+    end
+end 
