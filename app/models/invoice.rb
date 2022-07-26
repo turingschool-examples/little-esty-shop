6b273e2937ed3a25e8feaa65899baf20da5df4d5
philmarcu <philmark18@gmail.com>
2022-07-26 10:42:02 -0600

    Feat- add models for csvs

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,8 @@
 class Invoice < ApplicationRecord
+  validates_presence_of :status
+
+  belongs_to :customer
+  has_many :transactions, dependent: :destroy
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,2 +1,8 @@
 class InvoiceItem < ApplicationRecord
+  validates_presence_of :quantity
+  validates_presence_of :unit_price
+  validates_presence_of :status
+
+  belongs_to :item
+  belongs_to :invoice
 end
