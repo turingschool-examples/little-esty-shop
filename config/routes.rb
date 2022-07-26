227b081617fd912df388df631e79acee2ba22897
Blake Saylor <blakesaylor@gmail.com>
2022-07-26 15:24:10 -0600

    Add route for merchant items index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/merchants/:merchant_id/items', to: 'merchant_items#index'
 end
