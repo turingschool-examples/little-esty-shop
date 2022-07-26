c96cb07a171904cc8dccec4c98d9b7929e9e6b92
Sage Skaff <skaffsage@gmail.com>
2022-07-26 11:41:41 -0700

    Add #items_ready_to_ship

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,12 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
+
+  def items_ready_to_ship
+    invoice_items.joins(:invoice).where.not(status: 2).order('invoices.created_at')
+  end
 end
+
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -19,12 +19,12 @@ RSpec.describe Merchant do
       invoice1 = customer1.invoices.create!(status: 1)
       invoice2 = customer2.invoices.create!(status: 1)
 
-      invoice_item1 = InvoiceItem.create!(invoice: invoice1, item: item3)
-      invoice_item2 = InvoiceItem.create!(invoice: invoice2, item: item1)
-      invoice_item3 = InvoiceItem.create!(invoice: invoice2, item: item2)
-      invoice_item4 = InvoiceItem.create!(invoice: invoice1, item: item2)
+      invoice_item1 = InvoiceItem.create!(invoice: invoice1, item: item3, quantity: 1, unit_price: 6, status: 1)
+      invoice_item2 = InvoiceItem.create!(invoice: invoice2, item: item1, quantity: 1, unit_price: 20, status: 1)
+      invoice_item3 = InvoiceItem.create!(invoice: invoice2, item: item2, quantity: 1, unit_price: 10, status: 2)
+      invoice_item4 = InvoiceItem.create!(invoice: invoice1, item: item2, quantity: 1, unit_price: 10, status: 1)
 
-      expect(merchant.items_ready_to_ship).to eq([invoice_item1, invoice_item4, invoice_item2, invoice_item3])
+      expect(merchant.items_ready_to_ship).to eq([invoice_item1, invoice_item4, invoice_item2])
     end
   end
 end
