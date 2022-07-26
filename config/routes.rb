bb519cff76e5b7e9395de20a2fd9f2e9464549df
Michael Koulouvaris <mikekoul@gmail.com>
2022-07-26 17:28:51 -0600

    Add merchant invoice index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :merchants do
+    get '/invoices', to: 'merchant_invoices#index'
+    get '/invoices/:invoice_id', to: 'merchant_invoices#show'
+  end
 end
