047aacf0d568d9495713c503c5707fc3ae6ba851
Bryan Shears <brshears@gmail.com>
2022-07-26 11:43:24 -0600

    Change status in invoice & invoice_items table to an integer

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,4 @@
 class Invoice < ApplicationRecord
-    
+    enum status: {in_progress: 0, cancelled: 1, completed: 2}
+    # default: :in_progess
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,4 @@
 class InvoiceItem < ApplicationRecord
-
+  enum status: { pending: 0, shipped: 1, packaged: 2}
+  # default: :pending
 end
