6130b4718ea2d10a4ce4db467196b2ce9271de93
Nick Jones <nickjones9605@gmail.com>
2022-07-26 15:08:45 -0600

    Adds test for incomplete invoices. Adds html to admin index.html.erb for incomplete invoices. Test passes

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'admin index' do
         expect(current_path).to eq('/admin/merchants')
     end 
 
-     it 'has a link to a merchant index' do 
+    it 'has a link to a merchant index' do 
         visit '/admin'
 
         expect(page).to have_content("Invoices")
@@ -28,10 +28,14 @@ RSpec.describe 'admin index' do
         expect(current_path).to eq('/admin/invoices')
     end 
 
-      it 'see the names of top 5 customers' do 
+    it 'see the names of top 5 customers' do 
         visit '/admin'
- save_and_open_page
         expect(page).to have_content("Top 5 Customers")
-       
+    
+    end 
+
+    it 'sees a section for incomplete invoices' do 
+        visit '/admin'
+        expect(page).to have_content("Incomplete Invoices")
     end 
 end 
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Invoice, type: :model do
     it { should validate_presence_of :status }
     it { should validate_presence_of :created_at }
     it { should validate_presence_of :updated_at }
-    it { should define_enum_for(:status).with_values([:completed, :in_progress, :cancelled]) }
+    it { should define_enum_for(:status).with_values([:completed, "in progress", :cancelled]) }
   end
 
   describe 'relationships' do
