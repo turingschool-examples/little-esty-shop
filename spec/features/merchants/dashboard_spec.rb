375d653964fa1b85b19d269243856d20fceca173
Ethan Nguyen <etnguyen16@gmail.com>
2022-07-26 15:35:39 -0600

    add link to Items

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe "merchant dashboard", type: :feature do
     @merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
     @merchant_2 = Merchant.create!(name: "Klein, Rempel and Jones", created_at: Time.now, updated_at: Time.now)
 
+    visit "/merchants/#{@merchant_1.id}/dashboard"
   end
 
   it 'shows the merchants name' do
-    visit "/merchants/#{@merchant_1.id}/dashboard"
     within('#merchant-details') do
       expect(page).to have_content('Schroeder-Jerde')
       expect(page).to_not have_content('Klein, Rempel and Jones')
@@ -16,9 +16,9 @@ RSpec.describe "merchant dashboard", type: :feature do
   end
 
   it "has links to merchant items index and merchant invoices index" do
-    expect(page).to have_link("My Items", href: "/merchants/#{@merchant_1.id}/items")
-    expect(page).to have_link("My Invoices", href: "/merchants/#{@merchant_1.id}/invoices")
-    expect(page).to_not have_link("My Items", href: "/merchants/#{@merchant_2.id}/items")
-    expect(page).to_not have_link("My Invoices", href: "/merchants/#{@merchant_2.id}/invoices")
+    expect(page).to have_link("Items", href: "/merchants/#{@merchant_1.id}/items")
+    expect(page).to have_link("Invoices", href: "/merchants/#{@merchant_1.id}/invoices")
+    expect(page).to_not have_link("Items", href: "/merchants/#{@merchant_2.id}/items")
+    expect(page).to_not have_link("Invoices", href: "/merchants/#{@merchant_2.id}/invoices")
   end
 end
