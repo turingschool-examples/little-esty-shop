9e55a07117f401dc1593cb8c4ea6b6289e41eff8
Bryan Shears <brshears@gmail.com>
2022-07-27 14:50:46 -0600

    Add routes for merchant_dashboard_index & invoice

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :merchants, only: [:index] do
     resources :dashboard, only: [:index]
-
+    resources :items, only: [:index]
+    resources :invoices, only: [:index]
   end
 end
