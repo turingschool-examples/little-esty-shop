3aa62fc141ddae5a602bf0ee8b3e9edb48ea67e4
Gauri Joshi <gauri728@gmail.com>
2022-07-27 12:20:13 -0700

    items ready to ship

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -13,5 +13,8 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it { should belong_to :invoice }
     it { should belong_to :item }
+    it { should have_many(:merchants).through(:item) }
+    it { should have_many(:customers).through(:merchants) }
+    it { should have_many(:transactions).through(:invoice) }
   end
 end
