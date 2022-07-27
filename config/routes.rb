4760e6230eca1a08e22e79fd6f57202a2538d378
philmarcu <philmark18@gmail.com>
2022-07-26 20:54:26 -0600

    Feat- add resource routes for merchant_items index & show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :merchants do
+    resources :items, only: [:index, :show]
+  end
 end
