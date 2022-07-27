c35a494139cec690bda63511629ab3e901a50e8e
mkbonini <mkbonini@gmail.com>
2022-07-27 15:48:21 -0600

    bug fix from rebase

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -4,7 +4,7 @@ class MerchantInvoicesController < ApplicationController
     end
 
     def show
-        @invoice = Invoice.find(params[:invoice_id])
+        @invoice = Invoice.find(params[:id])
         @merchant = Merchant.find(params[:merchant_id])
     end
 end
