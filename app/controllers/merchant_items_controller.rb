8198034ec00224686799366ff31b909e2a4cd753
Thomas Turner <thomasturner482@yahoo.com>
2022-07-27 17:49:03 -0500

    add controllers for user stories 9 and 10

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -18,11 +18,15 @@ class MerchantItemsController < ApplicationController
     item = Item.find(params[:id])
     item.update(item_params)
     flash[:notice] = "Item successfully updated!"
-    redirect_to "/merchants/#{item.merchant_id}/items/#{item.id}"
+    if params[:status].nil?
+      redirect_to "/merchants/#{item.merchant_id}/items/#{item.id}"
+    else
+      redirect_to "/merchants/#{item.merchant_id}/items"
+    end
   end
 
   private
   def item_params
-    params.permit(:name, :description, :unit_price)
+    params.permit(:name, :description, :unit_price, :status)
   end
 end
