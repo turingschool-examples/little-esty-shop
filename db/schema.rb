2323ecd103d5e31f4205be56a42bd05e8b530991
Alex Boyd <alexanderleeboyd@gmail.com>
2022-07-27 14:07:19 -0400

    progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_042453) do
+ActiveRecord::Schema.define(version: 2022_07_27_161627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_07_26_042453) do
     t.bigint "merchant_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "status", default: 0
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe 'merchant items index' do
     merch2 = Merchant.create!(name: 'Molly Fine Arts')
 
     item1 = Item.create!(name: 'Shoe', description: 'Just one shoe', unit_price:500, merchant_id: merch1.id)
-    item1 = Item.create!(name: 'Sock', description: 'A sock', unit_price:200, merchant_id: merch1.id)
-    item1 = Item.create!(name: 'Jerky', description: 'Alligator jerky', unit_price:1500, merchant_id: merch2.id)
+    item2 = Item.create!(name: 'Sock', description: 'A sock', unit_price:200, merchant_id: merch1.id)
+    item3 = Item.create!(name: 'Jerky', description: 'Alligator jerky', unit_price:1500, merchant_id: merch2.id)
 
     visit "/merchants/#{merch1.id}/items"
     expect(current_path).to eq("/merchants/#{merch1.id}/items")
@@ -17,4 +17,38 @@ RSpec.describe 'merchant items index' do
       expect(page).to_not have_content('Jerky')
     end
   end
+
+  it 'has a button next to each item name to disable or enable that item. When clicked, user is redirected to item index and sees item status has changed' do
+    merch1 = Merchant.create!(name: 'Jolly Roger Imports')
+    merch2 = Merchant.create!(name: 'Molly Fine Arts')
+
+    item1 = Item.create!(name: 'Shoe', description: 'Just one shoe', unit_price:500, merchant_id: merch1.id, status: 1)
+    item2 = Item.create!(name: 'Sock', description: 'A sock', unit_price:200, merchant_id: merch1.id)
+    item3 = Item.create!(name: 'Jerky', description: 'Alligator jerky', unit_price:1500, merchant_id: merch1.id)
+
+    visit "/merchants/#{merch1.id}/items"
+
+    save_and_open_page
+
+    within "#enabled-items" do
+      expect(page).to have_content("Shoe")
+    end
+
+    within "#disabled-items" do
+      expect(page).to have_content("Sock")
+      expect(page).to have_content("Jerky")
+    end
+  end
+
 end
+
+#instead of item.status in the view, need that to be the enable/disable button
+
+# Merchant Item Disable/Enable
+
+# As a merchant
+# When I visit my items index page
+# Next to each item name I see a button to disable or enable that item.
+# When I click this button
+# Then I am redirected back to the items index
+# And I see that the items status has changed
