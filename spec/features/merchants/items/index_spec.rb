e877488607faa8fa2a86380fce8ef425728d3fa3
Anna Marie Sterling <sterling.am@gmail.com>
2022-07-27 10:09:01 -0600

    Added dashboard route/merchant items in progress

---
+++ spec/features/merchants/items/index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'the merchant items index' do
+  it 'is a list of all of the items that belong to a merchant' do
+    merchant1 = Merchant.create!(name: 'Fake Merchant')
+    merchant2 = Merchant.create!(name: 'Another Fake Merchant')
+    merchant3 = Merchant.create!(name: 'Faux Merchant')
+
+    item1 = Item.create!(name: 'Crap', description: 'Because you buy stuff for no reason', unit_price: 9999, merchant_id: merchant1.id)
+    item2 = Item.create!(name: 'Junk', description: 'You have room', unit_price: 10999, merchant_id: merchant1.id)
+    item3 = Item.create!(name: 'BS', description: 'Filling the void in your life', unit_price: 11999, merchant_id: merchant1.id)
+
+    visit "/merchants/#{merchant1.id}/items"
+
+    expect(page).to have_content('Name: Crap')
+    expect(page).to have_content('Description: Because you buy stuff for no reason')
+    expect(page).to have_content('Price: $99.99')
+    expect(page).to have_content('Name: Junk')
+    expect(page).to have_content('Description: You have room')
+    expect(page).to have_content('Price: $109.99')
+    expect(page).to have_content('Name: BS')
+    expect(page).to have_content('Description: Filling the void in your life')
+    expect(page).to have_content('Price: $119.99')
+    expect(page).to_not have_content('Name: Another Fake Merchant')
+    expect(page).to_not have_content('Name: Faux Merchant')
+  end
+end
