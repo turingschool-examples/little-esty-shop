df982d4346c41091f472228f3f457c436678de6e
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-27 14:18:44 -0600

    feat: add enable and disable columns and buttons to admin merchant index with feature test

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -28,4 +28,27 @@ RSpec.describe 'admin merchant index' do
         expect(page).to have_content("#{merchant_1.name}")
     end 
 
+    it 'has two sections - enabled and disabled - with a button next to each merchant based on category' do 
+        merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now, status: "enabled")
+        merchant_2 = Merchant.create!(name: "Bobs Cranes", created_at: Time.now, updated_at: Time.now)
+        merchant_3 = Merchant.create!(name: "Joe-Schmo Railroads", created_at: Time.now, updated_at: Time.now)
+        merchant_4 = Merchant.create!(name: "Allison Bugaboo", created_at: Time.now, updated_at: Time.now)
+        merchant_5 = Merchant.create!(name: "Castanza George", created_at: Time.now, updated_at: Time.now, status: "enabled")
+
+        visit '/admin/merchants'
+
+        within (".disabled_merchants") do 
+            expect(page).to have_button("Enable")
+            expect(page).to have_content("Bobs Cranes")
+            expect(page).to have_content("Joe-Schmo Railroads")
+            expect(page).to have_content("Allison Bugaboo")
+        end 
+        
+          within (".enabled_merchants") do 
+            expect(page).to have_button("Disable")
+            expect(page).to have_content("Schroeder-Jerde")
+            expect(page).to have_content("Castanza George")
+        end 
+    end 
+
 end 
