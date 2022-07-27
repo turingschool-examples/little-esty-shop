d7db606f90b6a01178aa044c826dbc2ec854b51d
Michael Koulouvaris <mikekoul@gmail.com>
2022-07-27 11:15:46 -0600

    Add item names to merchant invoice show page

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -25,5 +25,39 @@ RSpec.describe "merchants invoice show page" do
       expect(page).to have_content(invoice_1.created_at.strftime("%A, %B %e, %Y"))
       expect(page).to have_content("David Smith")
     end
+    
+    it 'shows all item names' do
+      merchant_1 = Merchant.create!(name: "Bobs Loggers")
+
+      item_1 = Item.create!(name: "Log", description: "Wood, maple", unit_price: 500, merchant_id: merchant_1.id )
+      item_2 = Item.create!(name: "Saw", description: "Metal, sharp", unit_price: 700, merchant_id: merchant_1.id )
+      item_3 = Item.create!(name: "Bench", description: "Cedar bench", unit_price: 900, merchant_id: merchant_1.id )
+
+      customer_1 = Customer.create!(first_name: "David", last_name: "Smith")
+
+      invoice_1 = Invoice.create!(status: 0, customer_id: customer_1.id)
+
+      invoice_item_1 = InvoiceItem.create!(quantity: 4, unit_price: 800, status: 0, item_id: item_1.id, invoice_id: invoice_1.id)
+      invoice_item_2 = InvoiceItem.create!(quantity: 2, unit_price: 1400, status: 0, item_id: item_2.id, invoice_id: invoice_1.id)
+      invoice_item_3 = InvoiceItem.create!(quantity: 3, unit_price: 500, status: 0, item_id: item_3.id, invoice_id: invoice_1.id)
+
+      visit "/merchants/#{merchant_1.id}/invoices/#{invoice_1.id}"
+      save_and_open_page
+
+      within "#items-#{item_1.id}" do 
+        expect(page).to have_content("Log")
+        expect(page).to_not have_content("Saw")
+      end
+
+      within "#items-#{item_2.id}" do 
+        expect(page).to have_content("Saw")
+        expect(page).to_not have_content("Log")
+      end
+
+      within "#items-#{item_3.id}" do
+        expect(page).to have_content("Bench")
+        expect(page).to_not have_content("Log")
+      end
+    end
   end
 end
