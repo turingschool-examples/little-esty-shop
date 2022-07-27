e021a0a0dc5246ff4a9c32a346416ce2573386f2
philmarcu <philmark18@gmail.com>
2022-07-27 10:25:38 -0600

    save changes

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -1,6 +1,5 @@
-class ItemsController < ApplicationController
+class MerchantItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @merchant.items
   end
 end
