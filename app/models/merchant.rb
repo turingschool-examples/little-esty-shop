e2e9a02bae2cd4cbbc9608afb9438718a07a23a6
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-27 17:58:30 -0600

    feat: add top five merchants top earning day method with feature test

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -11,4 +11,8 @@ class Merchant < ApplicationRecord
         select('merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue').joins(:invoice_items, invoices: :transactions).group('merchants.id').where(transactions: { result: 'success' }).order('revenue desc').limit(5)
         # select('merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue').joins(:invoice_items, invoices: :transactions).group('merchants.id').where(transactions: { result: 'success' }).where(merchants: { status: 'enabled'}).order('revenue desc').limit(5)
     end 
+
+    def top_earning_day
+        invoices.joins(:invoice_items).joins(:transactions).select('invoices.created_at AS date, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue').group('date').order('revenue desc, date desc').limit(1)
+    end 
 end 
