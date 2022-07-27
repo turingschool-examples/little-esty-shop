16f5a813d07d6d3b45e91962799eb76a2718a32b
mkbonini <mkbonini@gmail.com>
2022-07-26 18:35:17 -0600

    feat: merchant_invoices index links to invoice show

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -2,4 +2,8 @@ class MerchantInvoicesController < ApplicationController
     def index
         @merchant = Merchant.find(params[:id])
     end
+
+    def show
+        @invoice = Invoice.find(params[:invoice_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/merchants/:id/invoices', to: 'merchant_invoices#index'
+  get '/merchants/:merchant_id/invoices/:invoice_id', to: 'merchant_invoices#show'
 end
