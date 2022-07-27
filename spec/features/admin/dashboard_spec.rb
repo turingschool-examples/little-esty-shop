f305a79e0925c4b24ff3d686dd3a507df0cfc048
Sunny Moore <moore.sunny@gmail.com>
2022-07-27 16:04:29 -0700

    Test admin page has a header with Admin Dashboard

---
+++ spec/features/admin/dashboard_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Dashboard page' do
+  it 'has a header showing Admin Dashboard' do
+    visit admin_path
+
+    within('#admin-header') do
+      expect(page).to have_content("Admin Dashboard")
+    end
+  end
+  it 'has a link to /admin/merchants' do
+    visit admin_path
+
+    within('#admin-links') do
+      expect(page).to have_link('Merchants')
+    end
+  end
+end
