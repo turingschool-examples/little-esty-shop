2d31acdbf269763248085dedc1033ec2cf9a9401
Thiago Silveira <thiago1992@yahoo.com>
2022-07-27 17:00:30 -0600

    Feat: Favorite customers of a merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,5 +8,16 @@ class Merchant < ApplicationRecord
   def items_ready_to_ship
     invoice_items.joins(:invoice).where.not(status: 2).order('invoices.created_at')
   end
+
+  def favorite_customers
+    # binding.pry
+    customers.joins(invoices: :transactions)
+             .where(transactions: { result: 1 })
+             .select('customers.*, count(transactions.result) as transaction_total')
+             .group(:id)
+             .order(transaction_total: :desc)
+             .distinct
+             .limit(5)      
+  end
 end
 
