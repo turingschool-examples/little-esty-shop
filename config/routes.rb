5b6bd2bac66165a2f89f24f5746f426cf25b6c9b
philmarcu <philmark18@gmail.com>
2022-07-27 10:31:33 -0600

    Feat- refactor items controller into merchant_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :merchants do
-    resources :items, only: [:index, :show]
-  end
+ get "/merchants/:id/items", to: "merchant_items#index"
 end
