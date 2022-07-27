690588e1d1c2bda231e8e79f707a52710184fb33
Gauri Joshi <gauri728@gmail.com>
2022-07-27 11:15:37 -0700

    updates model relationships for merchants, invoice items, invoices, customers, transactions

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,12 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
+  has_many :invoice_items, through: :items 
+  has_many :invoices, through: :invoice_items 
+  has_many :customers, through: :invoices 
+  has_many :transactions, through: :invoices 
+
+  def items_ready_to_ship 
+    ### Active Record Method to get the items based on the invoice item status == pending ### HALP
+  end 
 end
