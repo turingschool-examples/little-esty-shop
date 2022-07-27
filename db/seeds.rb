440f7dfd18ec6e33e71d3bf847bea729b233ccd0
Jared Hardinger <jaredhardinger@gmail.com>
2022-07-27 00:10:35 -0400

    Add flash message to the app layout html file and Add a few seeds to the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@merchant1 = Merchant.create!(name: 'Stevie Plunder')
+@merchant2 = Merchant.create!(name: 'Dave Einstein')
+@hammer = @merchant1.items.create!(name: 'hammer', description: 'it is hammer time', unit_price: 2500)                
+@candlestick = @merchant1.items.create!(name: 'candlestick', description: 'put a candle on it...or beat someone with it', unit_price: 2000)                
+@bat = @merchant2.items.create!(name: 'bat', description: 'Hit a baseball with it', unit_price: 4500) 
