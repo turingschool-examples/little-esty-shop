037bc16e721ba81f0cdc831ac570d1d3b6b92eb4
Blake Saylor <blakesaylor@gmail.com>
2022-07-27 10:52:36 -0600

    Add edit and update methods, private item_params

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -6,4 +6,23 @@ class MerchantItemsController < ApplicationController
   def show
     @item = Item.find(params[:item_id])
   end
+
+  def edit
+    @item = Item.find(params[:item_id])
+  end
+
+  # Will need to update to handle edge case of improper unit_price entry
+  # at a later date
+  # e.g. Typing in as float (3.50) instead of integer (350)
+  def update
+    item = Item.find(params[:item_id])
+    item.update(item_params)
+    flash[:notice] = "Item successfully updated!"
+    redirect_to "/merchants/#{item.merchant_id}/items/#{item.id}"
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :unit_price)
+  end
 end
