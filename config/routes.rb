08a29f017c5dd12cd50027f32b9cabc3990a5ce1
Alex Boyd <alexanderleeboyd@gmail.com>
2022-07-27 00:21:10 -0400

    Make merchant/item/show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/merchants/:merchant_id/dashboard', to: 'merchants#show'
   get '/merchants/:merchant_id/items', to: 'merchant_items#index'
   get '/merchants/:merchant_id/invoices', to: 'merchant_invoices#index'
+
+  get '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#show'
 end
