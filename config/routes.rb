58aba8e5be686d29bcfdd846f88b19116e0314d1
Bryce Simonds <bsimonds14@gmail.com>
2022-07-27 14:25:25 -0600

    Refactored: deleted comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'application#welcome'
 
-#feat/merchant_invoice_index
+
   resources :merchants do
     get '/invoices', to: 'merchant_invoices#index'
     get '/invoices/:invoice_id', to: 'merchant_invoices#show'
