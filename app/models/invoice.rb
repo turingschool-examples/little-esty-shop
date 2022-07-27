1cf0fbe167ab16953f38ff13185f7370c15e93c0
Gauri Joshi <gauri728@gmail.com>
2022-07-27 11:29:54 -0700

    updates invoice relatonships

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,6 +4,7 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+  has_many :merchants, through: :items 
 
   enum status: { 'in progress' => 0, 'cancelled' => 1, 'completed' => 2 }
 end
