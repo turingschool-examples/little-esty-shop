103792032259bd8bad15946be512b7683f94e641
Sage Skaff <skaffsage@gmail.com>
2022-07-27 10:35:27 -0700

    Refactor routes, update controller actions

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,10 +1,10 @@
 class MerchantsController < ApplicationController
   def new
-    @merchant = Merchant.find(params[:merchant_id])
+    @merchant = Merchant.find(params[:id])
   end
 
   def show
-    @merchant = Merchant.find(params[:merchant_id])
+    @merchant = Merchant.find(params[:id])
   end
-
 end
+
