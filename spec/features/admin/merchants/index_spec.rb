53937c23e974c9672ae387f7a8cf26ad015b5b1a
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-27 12:14:36 -0600

    feat: add merchant link to show page with name and feature test

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -17,4 +17,15 @@ RSpec.describe 'admin merchant index' do
         expect(page).to have_content("#{merchant_5.name}")
     end 
 
+     it 'has an admin merchant show page when you click on the merchant name' do 
+        merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+        
+        visit '/admin/merchants'
+
+        click_on("#{merchant_1.name}")
+
+        expect(current_path).to eq("/admin/merchants/#{merchant_1.id}")
+        expect(page).to have_content("#{merchant_1.name}")
+    end 
+
 end 
