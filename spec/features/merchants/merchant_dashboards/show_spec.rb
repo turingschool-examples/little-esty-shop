1ee91a39896c1dfc65e8c7264e39194256ecbf1c
Michael Koulouvaris <mikekoul@gmail.com>
2022-07-27 18:47:24 -0600

    Add link to merchant item index page test passed

--- spec/features/merchants/merchant_dashboards/show_spec.rb
+++ spec/features/merchants/merchant_dashboards/show_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe 'Merchant Show Dashboard' do
         expect(page).to have_content('Spongebob The Merchant')
         expect(page).to_not have_content('Jon Doe')
     end
+
+    it 'on the merchant dashboard I see a link to my items index page' do 
+        merchant_1 = Merchant.create!(name: 'Spongebob The Merchant')
+        merchant_2 = Merchant.create!(name: 'Jon Doe')
+        
+        visit "/merchants/#{merchant_1.id}/dashboard"
+
+        expect(page).to have_link('Spongebob The Merchant Item Index')
+        expect(page).to_not have_content('Jon Doe Item Index')
+    end
 end 
