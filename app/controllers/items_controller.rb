922780899a408ed077fcfd257b07246c64ba0196
Bryan Shears <brshears@gmail.com>
2022-07-28 10:41:44 -0600

    Add welcome page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,12 @@
 class ItemsController < ApplicationController
+
   def index
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items
+  end
+
+  def show
+    @item = Item.find(params[:id])
     @merchant = Merchant.find(params[:merchant_id])
   end
 end
