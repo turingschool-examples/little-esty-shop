43582c2866045499a2a63c84c6cc6ae1f6dfee19
Eli Sachs <easachs13@gmail.com>
2022-07-27 21:15:59 -0600

    Create five_most_popular_items specs

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -80,5 +80,51 @@ RSpec.describe Merchant, type: :model do
       expect(merchant.ready_to_ship.first.item).to eq(item_1)
       expect(merchant.ready_to_ship.last.invoice).to eq(invoice_5)
     end
+
+    it 'has five_most_popular_items' do
+      customer_1 = Customer.create!(first_name: "A", last_name: "A")
+      # customer_2 = Customer.create!(first_name: "B", last_name: "B")
+
+      invoice_1 = Invoice.create!(status: "completed", customer_id: customer_1.id)
+      invoice_2 = Invoice.create!(status: "completed", customer_id: customer_1.id)
+      invoice_3 = Invoice.create!(status: "completed", customer_id: customer_1.id)
+      invoice_4 = Invoice.create!(status: "completed", customer_id: customer_1.id)
+      invoice_5 = Invoice.create!(status: "completed", customer_id: customer_1.id)
+      invoice_6 = Invoice.create!(status: "completed", customer_id: customer_1.id) #failed
+
+      transaction_1 = Transaction.create!(result: "success", credit_card_number: "0000111122223333", invoice_id: invoice_1.id)
+      transaction_2 = Transaction.create!(result: "success", credit_card_number: "0000111122223333", invoice_id: invoice_2.id)
+      transaction_3 = Transaction.create!(result: "success", credit_card_number: "0000111122223333", invoice_id: invoice_3.id)
+      transaction_4 = Transaction.create!(result: "success", credit_card_number: "0000111122223333", invoice_id: invoice_4.id)
+      transaction_5 = Transaction.create!(result: "success", credit_card_number: "0000111122223333", invoice_id: invoice_5.id)
+      transaction_6 = Transaction.create!(result: "failed", credit_card_number: "0000111122223333", invoice_id: invoice_6.id)
+      transaction_7 = Transaction.create!(result: "failed", credit_card_number: "0000111122223333", invoice_id: invoice_6.id) #both failed
+      # transaction_8 = Transaction.create!(result: "failed", credit_card_number: "0000111122223333", invoice_id: invoice_1.id)
+      # transaction_9 = Transaction.create!(result: "success", credit_card_number: "0000111122223333", invoice_id: invoice_1.id)
+      # transaction_10 = Transaction.create!(result: "success", credit_card_number: "0000111122223333", invoice_id: invoice_2.id)
+        
+      merchant = Merchant.create!(name: "Wizards Chest")
+
+      item_1 = Item.create!(name: "A", description: "A", unit_price: 400, merchant_id: merchant.id)
+      item_2 = Item.create!(name: "B", description: "B", unit_price: 200, merchant_id: merchant.id)
+      item_3 = Item.create!(name: "C", description: "C", unit_price: 500, merchant_id: merchant.id)
+      item_4 = Item.create!(name: "D", description: "D", unit_price: 100, merchant_id: merchant.id)
+      item_5 = Item.create!(name: "E", description: "E", unit_price: 300, merchant_id: merchant.id)
+      item_6 = Item.create!(name: "F", description: "F", unit_price: 600, merchant_id: merchant.id) #absent
+
+      # invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, status: "shipped", quantity: 10, unit_price: 100)
+      invoice_item_2 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, status: "shipped", quantity: 20, unit_price: 100) #2000
+      # invoice_item_3 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_2.id, status: "shipped", quantity: 5, unit_price: 200)
+      # invoice_item_4 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_2.id, status: "shipped", quantity: 5, unit_price: 200)
+      invoice_item_5 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_2.id, status: "shipped", quantity: 15, unit_price: 200) #3000
+      invoice_item_6 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_3.id, status: "shipped", quantity: 5, unit_price: 300) #1500
+      # invoice_item_7 = InvoiceItem.create!(item_id: item_4.id, invoice_id: invoice_4.id, status: "shipped", quantity: 5, unit_price: 300)
+      invoice_item_8 = InvoiceItem.create!(item_id: item_4.id, invoice_id: invoice_4.id, status: "shipped", quantity: 10, unit_price: 400) #4000
+      invoice_item_9 = InvoiceItem.create!(item_id: item_5.id, invoice_id: invoice_5.id, status: "shipped", quantity: 5, unit_price: 500) #2500
+      invoice_item_10 = InvoiceItem.create!(item_id: item_6.id, invoice_id: invoice_6.id, status: "shipped", quantity: 6, unit_price: 600) #3600 failed
+
+      # require 'pry'; binding.pry
+      expect(merchant.five_most_popular_items).to eq([item_4, item_2, item_5, item_1, item_3])
+    end
   end
 end
