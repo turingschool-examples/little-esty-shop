faacabff6c2e69cfd3e9d8b7ba87b48a07fd228f
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-28 14:59:12 -0400

    update path helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
 
-  def find_merchant
-    @merchant = Merchant.find(params[:merchant_id])
-  end
-
   def find_item
     @item = Item.find(params[:id])
   end
 
   def find_invoice
     @invoice = Invoice.find(params[:id])
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   private
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,11 +8,11 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+   
   end
 
   def update
