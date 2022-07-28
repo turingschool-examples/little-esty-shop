cc9a2be9e99d90e6c76ef9c4dd65bcacfeb5f26d
Michael Koulouvaris <mikekoul@gmail.com>
2022-07-27 18:59:37 -0600

    test/ Click on link and sent to the merchant invoice index page, test passed

--- spec/features/merchants/merchant_dashboards/show_spec.rb
+++ spec/features/merchants/merchant_dashboards/show_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'Merchant Show Dashboard' do
         expect(page).to_not have_content('Jon Doe Item Index')
     end
 
-    it 'on the merchant dashboard I see a link to my items index page' do 
+    it 'on the merchant dashboard I can click on the link and be sent to items index page' do 
         merchant_1 = Merchant.create!(name: 'Spongebob The Merchant')
         merchant_2 = Merchant.create!(name: 'Jon Doe')
         
@@ -40,5 +40,16 @@ RSpec.describe 'Merchant Show Dashboard' do
 
         expect(page).to have_link('Spongebob The Merchant Invoice Index')
         expect(page).to_not have_content('Jon Doe Invoice Index')
+    end
+
+    it 'on the merchant dashboard I can click on the link and be sent to invoices index page' do 
+        merchant_1 = Merchant.create!(name: 'Spongebob The Merchant')
+        merchant_2 = Merchant.create!(name: 'Jon Doe')
+        
+        visit "/merchants/#{merchant_1.id}/dashboard"
+
+        click_link ('Spongebob The Merchant Invoice Index')
+
+        expect(current_path).to eq("/merchants/#{merchant_1.id}/invoices")
     end
 end 
