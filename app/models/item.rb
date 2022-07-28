9e4816c2c04cd027b36a7010a0c7e5b80feb951f
Sage Skaff <skaffsage@gmail.com>
2022-07-27 19:11:34 -0700

    Update relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,12 @@
 class Item < ApplicationRecord
-  enum status: {Disabled: 0, Enabled: 1}
+  enum status: { Disabled: 0, Enabled: 1 }
 
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
-end
+  has_many :transactions, through: :invoices
 
+  def total_revenue
+    joins(:invoices).where(invoice: { status: 2 }).sum('quantity * unit_price')
+  end
+end
