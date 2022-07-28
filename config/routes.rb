21ee49444398e27e21abee8e0867d9ee4494bb0b
Gauri Joshi <gauri728@gmail.com>
2022-07-28 14:03:10 -0700

    add a GET route for merchants/id/invoices/id to invoices show page, and merchants/id/invoices to merchants show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 resources :merchants, only:[:show] do
+  # resources :invoices, only:[:index, :show]
   resources :dashboard, only:[:index]
   end
 
 get '/', to: 'application#welcome'
 
 get '/merchants/:id/items', to: 'merchants#show'
+get '/merchants/:id/invoices/:id', to: 'invoices#show'
+get '/merchants/:id/invoices', to: 'merchants#show'
 end
