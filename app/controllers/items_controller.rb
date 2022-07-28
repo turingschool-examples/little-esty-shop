f8157711ffb2ab296622a7ffb9ded42ab0a61657
Shirley DeCesari <shirley.decesari@gmail.com>
2022-07-28 16:15:18 -0700

    Feat: Add merchant item update funcationality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])    
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+    @item.update(item_params)
+    redirect_to merchant_item_path(@merchant, @item)   
+    flash.notice = "Item has been successfully update"
+  end
+
+  private 
+  def item_params
+    params.permit(:name, :description, :unit_price)    
+  end
 end
 
