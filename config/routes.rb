afba01e9faa40c6dec9a6e1fd9f5c68e4937e0ef
Blake Saylor <blakesaylor@gmail.com>
2022-07-29 09:15:40 -0600

    Add routing for Admin Invoices Index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   namespace :admin do 
     resources :merchants, only: [:index]
+    resources :invoices, only: [:index]
   end
 
   resources :merchants, only: [:index]  do
