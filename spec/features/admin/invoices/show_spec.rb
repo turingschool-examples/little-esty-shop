7bb5b7e50320725cd9fc56bb863876cd4a5deade
Blake Saylor <blakesaylor@gmail.com>
2022-07-29 10:56:40 -0600

    Add test for User Story 34

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -38,32 +38,32 @@ RSpec.describe 'Admin Invoices Show Page' do
     @invoice_3.id = 40
     @invoice_3.save!
 
-    InvoiceItem.create!(quantity: rand(1..10), 
-                        unit_price: rand(100..10000), 
-                        status: 'shipped', 
-                        item_id: @item_1.id, 
-                        invoice_id: @invoice_1.id)
+    @invoice_item_1 = InvoiceItem.create!(quantity: rand(1..10), 
+                                          unit_price: 5000, 
+                                          status: 'shipped', 
+                                          item_id: @item_1.id, 
+                                          invoice_id: @invoice_1.id)
 
-    InvoiceItem.create!(quantity: rand(1..10), 
-                        unit_price: rand(100..10000), 
-                        status: 'shipped', 
-                        item_id: @item_2.id, 
-                        invoice_id: @invoice_1.id)
+    @invoice_item_2 = InvoiceItem.create!(quantity: rand(1..10), 
+                                          unit_price: 10000, 
+                                          status: 'shipped', 
+                                          item_id: @item_2.id, 
+                                          invoice_id: @invoice_1.id)
 
-    InvoiceItem.create!(quantity: rand(1..10), 
-                        unit_price: rand(100..10000), 
-                        status: 'shipped', 
-                        item_id: @item_1.id, 
-                        invoice_id: @invoice_2.id)
+    @invoice_item_3 = InvoiceItem.create!(quantity: rand(1..10), 
+                                          unit_price: 15000, 
+                                          status: 'shipped', 
+                                          item_id: @item_1.id, 
+                                          invoice_id: @invoice_2.id)
 
-    InvoiceItem.create!(quantity: rand(1..10), 
-                        unit_price: rand(100..10000), 
-                        status: 'shipped', 
-                        item_id: @item_3.id, 
-                        invoice_id: @invoice_3.id)
+    @invoice_item_4 = InvoiceItem.create!(quantity: rand(1..10), 
+                                          unit_price: 25000, 
+                                          status: 'shipped', 
+                                          item_id: @item_3.id, 
+                                          invoice_id: @invoice_3.id)
   end
   
-  # User Story 32
+  # User Story 33
   # Admin Invoice Show Page
 
   # As an admin,
@@ -81,6 +81,39 @@ RSpec.describe 'Admin Invoices Show Page' do
     expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %d, %Y"))
     expect(page).to have_content(@invoice_1.customer.first_name.titlecase)
     expect(page).to have_content(@invoice_1.customer.last_name.titlecase)
+  end
+
+  # User Story 34
+  # Admin Invoice Show Page: Invoice Item Information
+
+  # As an admin
+  # When I visit an admin invoice show page
+  # Then I see all of the items on the invoice including:
+
+  # Item name
+  # The quantity of the item ordered
+  # The price the Item sold for
+  # The Invoice Item status 
+  it 'shows information for all of the invoice items on an invoice' do
+    visit "/admin/invoices/#{@invoice_1.id}"
+
+    within '#invoice-item-details' do
+      within "#invoice-item-#{@invoice_item_1.id}" do
+        expect(page).to have_content(@invoice_item_1.item.name)
+        expect(page).to have_content(@invoice_item_1.quantity)
+        expect(page).to have_content('$50.00')
+        expect(page).to have_content(@invoice_item_1.status.titlecase)
+      end
+
+      within "#invoice-item-#{@invoice_item_2.id}" do
+        expect(page).to have_content(@invoice_item_2.item.name)
+        expect(page).to have_content(@invoice_item_2.quantity)
+        expect(page).to have_content('$100.00')
+        expect(page).to have_content(@invoice_item_2.status.titlecase)
+      end
+
+      expect(page).to_not have_content(@invoice_item_4.item.name)
+    end
   end
 end
 
