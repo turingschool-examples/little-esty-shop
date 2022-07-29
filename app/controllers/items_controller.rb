a9049897debc5dd96d56f30ec30e1551137bcc23
Ethan Nguyen <etnguyen16@gmail.com>
2022-07-29 15:16:07 -0600

    add edit and show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,27 +4,36 @@ class ItemsController < ApplicationController
     @items = @merchant.items
   end
 
-  def update
-        item = Item.find(params[:id])
-        if item.update(item_params)
+  def show
+    @item = Item.find(params[:id])
+    # @merchant = Merchant.find(params[:merchant_id])
+  end
 
-          if params[:status].present?
-            redirect_to merchant_items_path(item.merchant)
-            flash[:success] = "Item successfully updated!"
-          else
-            redirect_to merchant_item_path(item.merchant, item)
-            flash[:success] = "Item successfully updated!"
-          end
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+      item = Item.find(params[:id])
+      if item.update(item_params)
+
+        if params[:status].present?
+           redirect_to merchant_items_path(item.merchant)
+           flash[:success] = "Item successfully updated!"
+        else
+          redirect_to merchant_item_path(item.merchant, item)
+          flash[:success] = "Item successfully updated!"
+        end
 
         else
-            redirect_to edit_merchant_item_path(item.merchant, item)
-            flash[:alert] = "Error: #{error_message(item.errors)}"
+          redirect_to edit_merchant_item_path(item.merchant, item)
+          flash[:alert] = "Error: #{error_message(item.errors)}"
         end
-    end
+  end
 
   private
   def item_params
-    params.permit(:name, :description, :unit_price, :status,)
+    params.permit(:name, :description, :unit_price, :status)
   end
 end
 
