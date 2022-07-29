ce144af1d37fc919db08093fbf9bc16ec7848d09
Sunny Moore <moore.sunny@gmail.com>
2022-07-29 09:01:27 -0700

    Add test of customer model top_customers method

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,5 +3,13 @@ require 'rails_helper'
 RSpec.describe Customer do
   describe 'relationships' do
     it { should have_many :invoices }
+    it { should have_many(:transactions).through(:invoices) }
+  end
+  describe 'instance methods' do
+    it 'top_customers returns the 5 customers with most transactions descending' do
+      top_cust = Customer.top_customers
+      expect(top_cust[0].transaction_total).to be >= (top_cust[1].transaction_total)
+      expect(top_cust.size).to eq(5)
+    end
   end
 end
