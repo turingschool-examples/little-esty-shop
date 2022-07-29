303ba766e6765b510b3f1e6e02f639870a846447
Anna Marie Sterling <sterling.am@gmail.com>
2022-07-28 20:09:20 -0600

    Edge case favorite customers method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,16 +7,15 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   def top_5_customers
-    Customer
-    .joins(:invoices, :transactions)
+    customers
+    .joins(:transactions)
     .group("customers.id")
+    .where(transactions: {result: "success"}, invoices: {status: "completed"})
     .select("customers.*, count(transactions.id) as no_of_transactions")
-    .where(invoices: {status: :completed}, transactions: {result: :success})
-    .order("no_of_transactions desc, customers.last_name").limit(5)
+    .order("no_of_transactions desc").limit(5)
   end
 
   def ready_to_ship
-    # require 'pry'; binding.pry 
     Item.joins(:invoices)
     .select("items.name, invoice_items.id as inv_item, invoices.id as inv_id, invoices.created_at as invoice_date")
     .where(invoice_items: {status: :packaged }, items: {merchant_id: id})
