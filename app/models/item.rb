7de5ead50217b820950801fe02c69d68a20d371d
Blake Saylor <blakesaylor@gmail.com>
2022-07-29 12:26:14 -0600

    validate presence of status

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
     validates_presence_of :name
     validates_presence_of :description
+    validates_presence_of :status
     validates :unit_price, presence: true, numericality: true
 
     belongs_to :merchant
