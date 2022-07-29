9b2850bfa028e25f86ab5cfad0b7608caee2de03
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-07-29 14:16:03 -0500

    test: add test for new Merchant creation with default status of disabled

--- spec/features/admin/merchants/new_spec.rb
+++ spec/features/admin/merchants/new_spec.rb
@@ -6,14 +6,25 @@ RSpec.describe 'form for new Merchant' do
     # And I see the merchant I just created displayed
     # And I see my merchant was created with a default status of disabled.
     it 'can create a new Merchant' do
-        merchant_name = Faker::Name.unique.name
+        Faker::UniqueGenerator.clear 
+        merchant_1 = Merchant.create!(name: Faker::Name.unique.name, status: 1)
+        merchant_2 = Merchant.create!(name: Faker::Name.unique.name, status: 1)
+        
+        merchant_3_name = Faker::Name.unique.name
         visit new_admin_merchant_path
 
-        fill_in('New Merchant Name', with: merchant_name)
+        fill_in('New Merchant Name', with: merchant_3_name)
         click_on 'Create New Merchant' 
 
         expect(current_path).to eq '/admin/merchants'
-
         
+        within('#enabled') do 
+            expect(page).to have_content merchant_1.name
+            expect(page).to have_content merchant_2.name
+        end
+
+        within('#disabled') do 
+            expect(page).to have_content merchant_3_name
+        end
     end
 end
