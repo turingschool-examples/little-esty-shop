6cda861a16260ceb328ceb5da2b501dd6b190a69
Eli Sachs <easachs13@gmail.com>
2022-07-28 23:42:34 -0600

    Create item most sales date/best day method

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,15 @@ class Item < ApplicationRecord
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
 
-
   enum status: { "enabled": 0, "disabled": 1 }
+
+  def most_sales_date
+    invoices
+    .joins(:transactions, :invoice_items)
+    .where('transactions.result = ?', 'success')
+    .select('invoices.created_at, sum(invoice_items.quantity * invoice_items.unit_price) as revenue')
+    .group('invoices.created_at')
+    .order('revenue desc, invoices.created_at desc')
+    .first.created_at.strftime("%m/%d/%y")
+  end
 end
