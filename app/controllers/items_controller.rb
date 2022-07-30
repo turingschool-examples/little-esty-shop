a2a9fa818e2b76eee1432d8a9e638749ba06050f
Parker Thomson <pdthomson1239@gmail.com>
2022-07-30 17:47:39 -0400

    small change

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,20 +13,20 @@ class ItemsController < ApplicationController
   end
 
   def update
-      item = Item.find(params[:id])
-      if params[:status] == 'enabled'
-          item.enabled!
-          redirect_to request.referrer
-          flash.notice = "#{item.name} has been enabled"
-      elsif params[:status] == 'disabled'
-          item.disabled!
-          redirect_to request.referrer
-          flash.notice = "#{item.name} has been disabled"
-      else
-          item.update(item_params)
-          redirect_to "/merchants/#{item.merchant_id}/items/#{item.id}"
-          flash.notice = "#{item.name} has been successfully updated"
-      end
+    item = Item.find(params[:id])
+    if params[:status] == 'enabled'
+        item.enabled!
+        redirect_to request.referrer
+        flash.notice = "#{item.name} has been enabled"
+    elsif params[:status] == 'disabled'
+        item.disabled!
+        redirect_to request.referrer
+        flash.notice = "#{item.name} has been disabled"
+    else
+        item.update(item_params)
+        redirect_to "/merchants/#{item.merchant_id}/items/#{item.id}"
+        flash.notice = "#{item.name} has been successfully updated"
+    end
   end
 
   def new
