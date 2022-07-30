b00ace5bd16ca57505fa341b377f0833ff8d640a
Bryce Simonds <bsimonds14@gmail.com>
2022-07-30 14:09:59 -0600

    Refactor: formatted the top day method to be more readable

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,7 +23,11 @@ class Item < ApplicationRecord
   end
 
   def top_day
-    invoices.select('invoices.created_at, sum(invoice_items.quantity * invoice_items.unit_price) as revenue').group('invoices.created_at').order('revenue desc').first.created_at
+    invoices.select('invoices.created_at, sum(invoice_items.quantity * invoice_items.unit_price) as revenue')
+    .group('invoices.created_at')
+    .order('revenue desc')
+    .first
+    .created_at
   end
 end
 
