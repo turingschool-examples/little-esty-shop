5034579d19221f4d510061b17ec4d2a2f853d25f
Ethan Nguyen <etnguyen16@gmail.com>
2022-07-29 21:17:24 -0600

    add create item test

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  
+
   resources :merchants, only: [:show] do
     resources :items, controller: 'items'
     resources :items, controller: :merchant_items
     resources :invoices, controller: 'invoices'
     resources :dashboard, only: [:index]
   end
+  
+  
+
 
 
 
