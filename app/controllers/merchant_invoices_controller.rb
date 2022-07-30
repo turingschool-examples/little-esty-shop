3e5642c1a462ec0e3e488482dbe8749b3a7b6bdb
Tyler Caudill <teecaud@hotmail.com>
2022-07-30 17:29:38 -0500

    feat show page for merchant invoices

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -1,5 +1,9 @@
-class MerchantInvoicesController < ApplicationController 
-   def index 
+class MerchantInvoicesController < ApplicationController
+   def index
       @merchant = Merchant.find(params[:merchant_id])
+   end
+
+   def show
+     @invoice = Invoice.find(params[:id])
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ resources :merchants, only:[:show] do
 
 get '/merchants/:id/items', to: 'merchants#show'
 get '/merchants/:id/invoices', to: 'merchants#show'
-get '/merchants/:id/invoices/:id', to: 'invoices#show'
+get '/merchants/:id/invoices/:id', to: 'merchant_invoices#show'
 get 'merchants/:id/items/:id', to: 'merchant_items#show'
 end
