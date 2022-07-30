26a7c576f57079ade112e6192cabd9f5fd67af79
Anna Marie Sterling <sterling.am@gmail.com>
2022-07-30 12:33:51 -0600

    Update model relation tests

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -12,5 +12,9 @@ RSpec.describe Transaction, type: :model do
 
   describe 'relationships' do
     it { should belong_to :invoice }
+    it { should have_many(:customers).through(:invoice) }
+    it { should have_many(:invoice_items).through(:invoice) }
+    it { should have_many(:items).through(:invoice_items) }
+    it { should have_many(:merchants).through(:items) }
   end
 end
