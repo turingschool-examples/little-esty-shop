3a6f76a9e1d7ebb900ef94fedf21968c87deda2c
mkbonini <mkbonini@gmail.com>
2022-07-30 12:36:51 -0600

    test: display purchase count on page

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -52,4 +52,29 @@ RSpec.describe 'Admin Dashboard' do
             expect(page).to_not have_content("#{Customer.first.first_name} #{Customer.first.last_name}")
         end
     end
+
+    it 'displays the transaction count of top 5 customers' do
+        8.times do
+            Customer.create!(first_name: Faker::Name.unique.first_name, last_name: Faker::Name.unique.last_name)
+        end
+        Customer.all.each.with_index do |customer, index|
+            index.times do 
+                customer.invoices.create!(status: Faker::Number.between(from: 0, to: 2) )
+            end
+        end
+        Invoice.all.each do |invoice|
+            invoice.transactions.create!(result: "success", credit_card_number: Faker::Business.credit_card_number, credit_card_expiration_date: Faker::Business.credit_card_expiry_date)
+        end
+        20.times do
+            Transaction.create!(result: "failed",invoice_id: Faker::Number.between(from: Invoice.first.id, to: Invoice.last.id), credit_card_number: Faker::Business.credit_card_number, credit_card_expiration_date: Faker::Business.credit_card_expiry_date)
+        end
+        customers = Customer.top_5_by_transaction
+
+        visit '/admin'
+
+        within "#top-customers" do
+            expect(page).to have_content("1. #{customers.first.first_name} #{customers.first.last_name} - 8 purchases")
+            expect(page).to have_content("2. #{customers.second.first_name} #{customers.second.last_name} - 7 purchases")
+        end
+    end
 end
