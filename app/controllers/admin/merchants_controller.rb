a749a5b92b7a8c6dbb4cca830aff0f4228c76adf
Michael Koulouvaris <mikekoul@gmail.com>
2022-07-30 14:37:19 -0600

    feat/add success and error messages when a form is success or if a form update fails

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -16,8 +16,11 @@ class Admin::MerchantsController < ApplicationController
     merchant = Merchant.find(params[:id])
     if merchant.update(merchant_params)
       redirect_to "/admin/merchants/#{merchant.id}"
+      flash.clear
+      flash[:success] = "Update to #{merchant.name} was successful!"
     else
       redirect_to "/admin/merchants/#{merchant.id}/edit"
+      flash[:alert] = "Error: Field Cannot Be Blank"
     end
   end
 
--- spec/features/admin/merchants/edit_spec.rb
+++ spec/features/admin/merchants/edit_spec.rb
@@ -25,5 +25,29 @@ RSpec.describe 'admin merchant update page' do
       expect(current_path).to eq("/admin/merchants/#{merchant_1.id}")
       expect(page).to have_content("Spongebob The Bestest Merchant")
     end
+
+    it 'displays a update was successful flash message on the admin merchant show page' do
+      merchant_1 = Merchant.create!(name: 'Spongebob The Merchant')
+
+      visit "admin/merchants/#{merchant_1.id}/edit"
+
+      fill_in "Merchant Name", with: "Spongebob The Bestest Merchant"
+      click_button "Submit"
+
+      expect(current_path).to eq("/admin/merchants/#{merchant_1.id}")
+      expect(page).to have_content("Update to Spongebob The Bestest Merchant was successful!")
+    end
+
+    it 'displays an error message if field is left blank' do
+      merchant_1 = Merchant.create!(name: 'Spongebob The Merchant')
+
+      visit "admin/merchants/#{merchant_1.id}/edit"
+
+      fill_in "Merchant Name", with:" "
+      click_button "Submit"
+
+      expect(current_path).to eq("/admin/merchants/#{merchant_1.id}/edit")
+      expect(page).to have_content("Error: Field Cannot Be Blank")
+    end
   end
 end
