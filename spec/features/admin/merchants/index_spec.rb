f4ba1c7821851d133c51d6b15e651fed89360ade
Bryan Shears <brshears@gmail.com>
2022-07-30 23:57:24 -0600

    Add features test for display all merchants in admin

---
+++ spec/features/admin/merchants/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'admin merchants index page' do
+  before :each do
+    @merchant_1 = Merchant.create!(name: "Al Capone", created_at: Time.now, updated_at: Time.now)
+    @merchant_2 = Merchant.create!(name: "Lucky Luciano", created_at: Time.now, updated_at: Time.now)
+    @merchant_3 = Merchant.create!(name: "George Remus", created_at: Time.now, updated_at: Time.now)
+  end
+
+  it 'displays all of the merchants' do
+    visit '/admin/merchants'
+
+    expect(page).to have_content(@merchant_1.name)
+    expect(page).to have_content(@merchant_2.name)
+    expect(page).to have_content(@merchant_3.name)
+
+    # click_link "#{@merchant_1.name}"
+    # expect(current_path).to eq("/admin/merchants/#{@merchant_1.id}")
+  end
+
+
+
+
+end
