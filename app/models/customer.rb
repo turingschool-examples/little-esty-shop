a2d67e0f87fa47844d89d28b7a0c92c3ace0e9da
Ben Silverstein <benjsilverstein@comcast.net>
2022-07-31 15:36:34 -0600

    Write top customer method for Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
   validates_presence_of :last_name
 
   has_many :invoices
+
+  def self.top_five_customers
+    select("customers.*, count(transactions.id) as transactions_count")
+    .joins(invoices: :transactions)
+    .where(transactions: {result: 0}, invoices:{status: 2})
+    .group("customers.id")
+    .order(transactions_count: :desc)
+    .limit(5)
+  end
 end
