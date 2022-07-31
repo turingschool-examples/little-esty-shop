de3bcaf80bf48f33f0acac87a93218b3bf65b513
Gauri Joshi <gauri728@gmail.com>
2022-07-30 23:09:09 -0700

    creates initial admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'application#welcome'
 
+  resources :admin, only:[:index]
   resources :merchants, only:[:show] do
     resources :dashboard, only:[:index]
   # resources :invoices, only:[:index, :show]
@@ -10,4 +11,5 @@ Rails.application.routes.draw do
   get '/merchants/:id/invoices', to: 'merchants#show'
   get '/merchants/:id/invoices/:id', to: 'merchant_invoices#show'
   get 'merchants/:id/items/:id', to: 'merchant_items#show'
+  
 end
