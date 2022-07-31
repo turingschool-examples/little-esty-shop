b49411cb1ac372841359c6b27ab862fa77bfdcfe
Parker Thomson <pdthomson1239@gmail.com>
2022-07-31 16:52:11 -0400

    test/feature to create a new merchant, routes, controller updated

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,14 @@ Rails.application.routes.draw do
   get '/merchants/:merchant_id/invoices/:id', to: 'merchant_invoices#show'
   post '/merchants/:merchant_id/invoices/:id', to: 'merchant_invoices#update'
   get '/merchants/:merchant_id/dashboard', to: 'merchants#show'
-  
+
   get '/admin/invoices', to: 'admin#index'
   get '/admin/invoices/:id', to: 'admin#show'
 
   get '/admin/merchants', to: 'admin#index'
-  get 'admin/merchants/:merchant_id', to: 'admin#show_2'
-  get 'admin/merchants/:merchant_id/edit', to: 'admin#edit'
-  patch 'admin/merchants/:merchant_id/update', to: 'admin#update'
+  get '/admin/merchants/new', to: 'admin#new'
+  get '/admin/merchants/:merchant_id', to: 'admin#show_2'
+  get '/admin/merchants/:merchant_id/edit', to: 'admin#edit'
+  patch '/admin/merchants/:merchant_id/update', to: 'admin#update'
+  post '/admin/merchants', to: 'admin#create'
 end
