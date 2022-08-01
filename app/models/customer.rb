2c5cc30c2fe07d22f8a97fb56c3f9825f0ea0db7
Gauri Joshi <gauri728@gmail.com>
2022-08-01 11:08:59 -0700

    define class method for top 5 customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,13 @@ class Customer < ApplicationRecord
   has_many :items, through: :invoice_items 
   has_many :merchants, through: :items 
   has_many :discounts, through: :merchants 
+
+  def self.top_five 
+    joins(invoices: :transactions)
+    .where(transactions: {result: :success})
+    .group(:id)
+    .select("customers.*, count(transactions) as trans_count") ##when i DIDN'T use an alias here this didn't pass for some reason? -GJ
+    .order("trans_count desc")
+    .limit(5)
+  end
 end
