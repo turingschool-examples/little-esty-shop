34626fd98bbc5cf5fe227e8c24cb50b395bdc023
philmarcu <philmark18@gmail.com>
2022-08-01 11:02:50 -0600

    Feat: add other relationships to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,9 @@ class Customer < ApplicationRecord
   validates_presence_of :last_name
 
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
 
   def self.top_five_customers
     select("customers.*, count(transactions.id) as transactions_count")
@@ -11,5 +14,14 @@ class Customer < ApplicationRecord
     .group("customers.id")
     .order(transactions_count: :desc)
     .limit(5)
+  end
+
+  def top_5_merchants
+    merchants.joins(:transactions)
+             .where(transactions: {result: 0})
+             .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue')
+             .group('merchants.id')
+             .order(revenue: :desc)
+             .limit(5)
   end
 end
