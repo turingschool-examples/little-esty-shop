c8a1d73983c80a68d6dd877d229d59b3b26ea161
Sunny Moore <moore.sunny@gmail.com>
2022-08-01 14:45:09 -0700

    Add logic for buttons to update method

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -8,4 +8,21 @@ class Admin::MerchantsController < ApplicationController
     @merchant = Merchant.find(params[:id])
   end
 
+  def update
+    @merchant = Merchant.find(params[:id])
+
+    if params[:commit] == "Disable"
+      @merchant.update(status: params[:status])
+      redirect_to admin_merchants_path
+    elsif params[:commit] == "Enable"
+      @merchant.update(status: params[:status])
+      redirect_to admin_merchants_path
+    else
+      redirect_to admin_merchants_path, notice: "ERROR PROCESSING REQUEST!"
+    end
+  end
+private
+  def merchant__params
+    params.permit(:status)
+  end
 end
