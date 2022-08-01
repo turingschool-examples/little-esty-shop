643f201b2b0080ffeae32adefa6983b13a0381e9
Bryan Shears <brshears@gmail.com>
2022-08-01 17:03:03 -0600

    Add feature tests for admin merchant update

---
+++ spec/features/admin/merchants/edit_spec.rb
@@ -0,0 +1,19 @@
+  require 'rails_helper'
+
+RSpec.describe 'admin merchant update page' do
+
+  it 'allows you to update a merchant and return to the show page' do
+    merchant_1 = Merchant.create!(name: "Al Capone", created_at: Time.now, updated_at: Time.now)
+
+    visit "/admin/merchants/#{merchant_1.id}/edit"
+
+      fill_in "Name", with: "Al Capone"
+      within "#edit_merchant_name" do
+      expect(page).to have_field("Name", with: "Al Capone")
+      save_and_open_page
+      click_on 'Update Merchant'
+      expect(current_path).to eq("/admin/merchants/#{merchant_1.id}")
+    end
+  end
+
+end
