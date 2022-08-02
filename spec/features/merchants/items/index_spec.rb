0f3cbe614de959cae1fc51b7337b76ff9d626291
Shirley DeCesari <shirley.decesari@gmail.com>
2022-08-02 13:46:36 -0700

    Test: Add top 5 merchant items by revenue

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -191,5 +191,83 @@ RSpec.describe 'Merchant Items Index Page' do
       end
     end
   end
+
+  describe 'Most Popular Items' do 
+    it "can see the top 5 most popular items ranked by revenue" do 
+      merchant1 = Merchant.create!(name: "Josey Wales")
+      merchant1 = Merchant.create!(name: "Britches Eckles")
+
+      customer1 = Customer.create!(first_name: "Pipper", last_name: "Pots")
+
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 1)
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 1)
+
+      transaction1 = Transaction.create!(credit_card_number: 948756, result: 1, invoice_id: invoice1.id)
+      transaction2 = Transaction.create!(credit_card_number: 287502, result: 1, invoice_id: invoice1.id)
+
+      item1 = Item.create!(name: "Camera", description: "electronic", unit_price: 500, merchant_id: merchant1.id)
+      item2 = Item.create!(name: "Bone", description: "pet treats", unit_price: 200, merchant_id: merchant1.id)
+      item3 = Item.create!(name: "Kong", description: "pet toys", unit_price: 100, merchant_id: merchant1.id)
+      item4 = Item.create!(name: "Collar", description: "pet collar", unit_price: 300, merchant_id: merchant1.id)
+      item5 = Item.create!(name: "Leash", description: "pet leash", unit_price: 400, merchant_id: merchant1.id)
+      item6 = Item.create!(name: "Kibble", description: "pet food", unit_price: 600, merchant_id: merchant1.id)
+
+      invoice_items1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item1.id, quantity: 1, unit_price: 500, status: 0) #500
+      invoice_items2 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item2.id, quantity: 2, unit_price: 200, status: 0) #400
+      invoice_items3 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item3.id, quantity: 3, unit_price: 100, status: 1) #300
+      invoice_items4 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item4.id, quantity: 2, unit_price: 100, status: 1) #600
+      invoice_items5 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item5.id, quantity: 2, unit_price: 400, status: 2) #800
+      invoice_items6 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item6.id, quantity: 2, unit_price: 600, status: 2) #1200
+
+      visit merchant_items_path(merchant1) 
+
+      within("#top_5") do 
+        expect(item6.name).to appear_before(item5.name)
+        expect(item5.name).to appear_before(item1.name)
+        expect(item1.name).to appear_before(item2.name)
+        expect(item2.name).to appear_before(item3.name)
+        expect(page).to_not have_content(item4.name)
+      end 
+    end
+
+    it "can link top 5 items to the item show page" do 
+      merchant1 = Merchant.create!(name: "Josey Wales")
+      merchant1 = Merchant.create!(name: "Britches Eckles")
+
+      customer1 = Customer.create!(first_name: "Pipper", last_name: "Pots")
+
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 1)
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 1)
+
+      transaction1 = Transaction.create!(credit_card_number: 948756, result: 1, invoice_id: invoice1.id)
+      transaction2 = Transaction.create!(credit_card_number: 287502, result: 1, invoice_id: invoice1.id)
+
+      item1 = Item.create!(name: "Camera", description: "electronic", unit_price: 500, merchant_id: merchant1.id)
+      item2 = Item.create!(name: "Bone", description: "pet treats", unit_price: 200, merchant_id: merchant1.id)
+      item3 = Item.create!(name: "Kong", description: "pet toys", unit_price: 100, merchant_id: merchant1.id)
+      item4 = Item.create!(name: "Collar", description: "pet collar", unit_price: 300, merchant_id: merchant1.id)
+      item5 = Item.create!(name: "Leash", description: "pet leash", unit_price: 400, merchant_id: merchant1.id)
+      item6 = Item.create!(name: "Kibble", description: "pet food", unit_price: 600, merchant_id: merchant1.id)
+
+      invoice_items1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item1.id, quantity: 1, unit_price: 500, status: 0) #500
+      invoice_items2 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item2.id, quantity: 2, unit_price: 200, status: 0) #400
+      invoice_items3 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item3.id, quantity: 3, unit_price: 100, status: 1) #300
+      invoice_items4 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item4.id, quantity: 2, unit_price: 100, status: 1) #600
+      invoice_items5 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item5.id, quantity: 2, unit_price: 400, status: 2) #800
+      invoice_items6 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item6.id, quantity: 2, unit_price: 600, status: 2) #1200
+
+      visit merchant_items_path(merchant1) 
+      within("#top_5") do 
+        expect(page).to have_link(item5.name)
+        expect(page).to have_link(item1.name)
+        expect(page).to have_link(item2.name)
+        expect(page).to have_link(item3.name)
+        
+        click_link "#{item1.name}"
+        
+        expect(current_path).to eq(merchant_item_path(merchant1, item1))
+      end 
+    end
+  end 
 end
 
