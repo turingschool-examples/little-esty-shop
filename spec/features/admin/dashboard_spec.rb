e99737e373a89cb9308a810c9b5c30e72cb609a2
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-08-01 21:37:07 -0700

    test/links to the admin merchants and invoices indexes

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Admin Dashboard', type: :feature do 
 
-# Admin Dashboard 
-# As an admin,
-# When I visit the admin dashboard (/admin)
-# Then I see a header indicating that I am on the admin dashboard
-
  it "has a header indicating this is the admin dashboard" do 
-  
+
   visit '/admin'
 
   expect(page).to have_content('Admin Dashboard')
+ end
+
+ it "has links to the admin merchants and invoices indexes" do 
+
+  visit '/admin'
+
+  expect(page).to have_link('Admin Merchants Index')
+  expect(page).to have_link('Admin Invoices Index')
  end
 end
