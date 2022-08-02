c14744aceb2b4d2361a036dca20e13e823b33d69
Shirley DeCesari <shirley.decesari@gmail.com>
2022-08-02 13:49:02 -0700

    Feat: Add top 5 merchant items by revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,8 +8,18 @@ class Merchant < ApplicationRecord
 
     def merchant_invoices
       invoices.distinct
+    end
 
     def ready_to_ship
       invoice_items.joins(:invoice).where.not("invoice_items.status = ?", 2).order('invoices.created_at')
     end
+
+    def top_5_items
+      items.joins(:invoice_items, invoices: :transactions)
+      .where(transactions: {result: 1})
+      .select('items.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue')
+      .group(:id)
+      .order(revenue: :desc)
+      .limit(5)   
+  end
 end
