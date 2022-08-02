4f6eaa1b6e10e74862c5afc15c1ae4a81b352aaa
mkbonini <mkbonini@gmail.com>
2022-08-02 15:57:28 -0600

    refactor code to match group

--- app/facades/repo_search.rb
+++ app/facades/repo_search.rb
@@ -4,6 +4,10 @@ class RepoSearch
       Repo.new(@_repo_information ||= service.repo)
   end
 
+  def commit_count(username)
+    service.commits(username).count
+  end
+
   def service
     GithubService.new
   end
--- app/poros/git_information.rb
+++
@@ -1,9 +0,0 @@
- class GitInformation
-    def service
-        GithubService.new
-    end
-
-    def commit_count(username)
-        service.commits(username).count
-    end
- end
