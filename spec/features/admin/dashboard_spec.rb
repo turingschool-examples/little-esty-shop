235c1c4e0ee6da5a0278fcb3be030b3676589daa
Jared Hardinger <jaredhardinger@gmail.com>
2022-08-03 16:13:40 -0400

    Add test for Admin Dashboard Invoices sorted by least recent

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -106,7 +106,6 @@ RSpec.describe 'Admin Dashboard', type: :feature do
     expect('Fasey').to appear_before('Pach')
     expect('Pach').to appear_before('Tarker')
     expect('Tarker').to_not appear_before('Gesley')
-    save_and_open_page
   end
 
  it "can list incomplete invoices and link to the invoice's admin show page" do 
@@ -154,19 +153,64 @@ RSpec.describe 'Admin Dashboard', type: :feature do
 
       expect(page).to_not have_content("#{invoice7.id}")
       expect(page).to_not have_content("#{invoice8.id}")
-    
+      save_and_open_page
       expect(page).to have_link("#{invoice2.id}")
       click_on("#{invoice2.id}")
       expect(current_path).to eq("/admin/invoices/#{invoice2.id}")
     end
   end
 
-# Admin Dashboard Invoices sorted by least recent
+  it 'in the Incomplete Invoices section, next to each invoice id i see the date that the invoice
+  was created and I see the date formatted like Monday, July 18, 2019. And I see that the list
+  is ordered from oldest to newest' do
+    merchant1 = Merchant.create!(name: "Poke Retirement homes", status: "enabled")
+    merchant2 = Merchant.create!(name: "Rendolyn Guizs poke stops", status: "enabled")
+    merchant3 = Merchant.create!(name: "Dhirley Secasrio's knits and bits", status: "enabled")
 
-# As an admin,
-# When I visit the admin dashboard
-# In the section for "Incomplete Invoices",
-# Next to each invoice id I see the date that the invoice was created
-# And I see the date formatted like "Monday, July 18, 2019"
-# And I see that the list is ordered from oldest to newest
+    item1 = Item.create!(name: "Pikachu pics", description: 'Cute pics with pikachu', unit_price: 1000, merchant_id: merchant1.id)
+    item2 = Item.create!(name: "Pokemon stuffy", description: 'Pikachu stuffed toy', unit_price: 2000, merchant_id: merchant1.id)
+    item3 = Item.create!(name: "Junk", description: 'junk you should want', unit_price: 500, merchant_id: merchant2.id)
+    item4 = Item.create!(name: "Pencil", description: 'Writes things down', unit_price: 100, merchant_id: merchant2.id)
+    item5 = Item.create!(name: "Chair", description: 'To sit on', unit_price: 7500, merchant_id: merchant2.id)
+    item6 = Item.create!(name: "Blanket", description: 'Keeps you warm', unit_price: 2500, merchant_id: merchant3.id)
+    item7 = Item.create!(name: "shoe", description: 'leather', unit_price: 3500, merchant_id: merchant3.id)
+
+    customer1 = Customer.create!(first_name: "Parker", last_name: "Thomson")
+    invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id, created_at: "2022-07-01 20:00:00 UTC -05:00")
+    invoice2 = Invoice.create!(status: "completed", customer_id: customer1.id, created_at: "2022-07-02 20:00:00 UTC -05:00")
+    invoice3 = Invoice.create!(status: "completed", customer_id: customer1.id, created_at: "2022-06-01 20:00:00 UTC -05:00")
+    invoice4 = Invoice.create!(status: "completed", customer_id: customer1.id, created_at: "2022-05-01 20:00:00 UTC -05:00")
+    invoice5 = Invoice.create!(status: "completed", customer_id: customer1.id, created_at: "2022-05-18 20:00:00 UTC -05:00")
+    invoice6 = Invoice.create!(status: "completed", customer_id: customer1.id, created_at: "2022-07-22 20:00:00 UTC -05:00")
+    invoice7 = Invoice.create!(status: "completed", customer_id: customer1.id, created_at: "2022-06-11 20:00:00 UTC -05:00")
+    invoice8 = Invoice.create!(status: "completed", customer_id: customer1.id, created_at: "2022-07-03 20:00:00 UTC -05:00")
+
+    invoice_item1 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "packaged", item_id: item1.id, invoice_id: invoice1.id)
+    invoice_item2 = InvoiceItem.create!(quantity: 1, unit_price: item2.unit_price, status: "packaged", item_id: item2.id, invoice_id: invoice2.id)
+    invoice_item3 = InvoiceItem.create!(quantity: 1, unit_price: item3.unit_price, status: "pending", item_id: item3.id, invoice_id: invoice3.id)
+    invoice_item4 = InvoiceItem.create!(quantity: 1, unit_price: item4.unit_price, status: "pending", item_id: item4.id, invoice_id: invoice4.id)
+    invoice_item5 = InvoiceItem.create!(quantity: 1, unit_price: item5.unit_price, status: "pending", item_id: item5.id, invoice_id: invoice5.id)
+    invoice_item6 = InvoiceItem.create!(quantity: 1, unit_price: item6.unit_price, status: "packaged", item_id: item6.id, invoice_id: invoice6.id)
+    invoice_item7 = InvoiceItem.create!(quantity: 1, unit_price: item7.unit_price, status: "shipped", item_id: item7.id, invoice_id: invoice7.id)
+    invoice_item8 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item7.id, invoice_id: invoice8.id)
+      
+    visit '/admin'
+
+    within '#incomplete_invoices' do
+      expect(page).to have_content("#{invoice1.id} Date invoice created: Friday, July 01, 2022")
+      expect(page).to have_content("#{invoice2.id} Date invoice created: Saturday, July 02, 2022")
+      expect(page).to have_content("#{invoice3.id} Date invoice created: Wednesday, June 01, 2022")
+      expect(page).to have_content("#{invoice4.id} Date invoice created: Sunday, May 01, 2022")
+      expect(page).to have_content("#{invoice5.id} Date invoice created: Wednesday, May 18, 2022")
+      expect(page).to have_content("#{invoice6.id} Date invoice created: Friday, July 22, 2022")
+
+      expect(page).to_not have_content("#{invoice7.id}")
+      expect(page).to_not have_content("#{invoice8.id}")
+      expect("#{invoice4.id}").to appear_before("#{invoice5.id}")
+      expect("#{invoice5.id}").to appear_before("#{invoice3.id}")
+      expect("#{invoice3.id}").to appear_before("#{invoice1.id}")
+      expect("#{invoice1.id}").to appear_before("#{invoice2.id}")
+      expect("#{invoice2.id}").to appear_before("#{invoice6.id}")
+    end
+  end
 end
