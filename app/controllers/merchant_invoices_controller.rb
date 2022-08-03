d83aa7f0b1885ce73106cbb6975139061a8daadf
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-02 18:29:03 -0600

    Add styling to admin invoices show page and merchant invoices show page

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -5,6 +5,7 @@ class MerchantInvoicesController < ApplicationController
   end
 
   def show
+    @merchant = Merchant.find(params[:merchant_id])
     @invoice = Invoice.find(params[:invoice_id])
   end
 
