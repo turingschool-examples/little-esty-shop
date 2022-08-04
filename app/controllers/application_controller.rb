ae4fa5862d0bd3b69e71679e451585be12212aa4
philmarcu <philmark18@gmail.com>
2022-08-04 14:20:25 -0600

    Refactor- final refactor for group project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :before_anything_else
+  # before_action :before_anything_else
 
-  def before_anything_else
-    @contributors = GithubFacade.contributors
-    @pull_requests = GithubFacade.pull_requests
-    @repo_name = GithubFacade.repo_name
-  end
+  # def before_anything_else
+  #   @contributors = GithubFacade.contributors
+  #   @pull_requests = GithubFacade.pull_requests
+  #   @repo_name = GithubFacade.repo_name
+  # end
 
   def index
   end
