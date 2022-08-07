8f2fab74e856cd588adde3b75ab3923b25705705
Sunny Moore <moore.sunny@gmail.com>
2022-08-07 12:58:45 -0700

    Test bulk disount show page

---
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Bulk Discount Show Page' do
+  it 'shows the attributes for a bulk_discount' do
+    merch1 = Merchant.create!(name: 'Jolly Roger Imports')
+    discount1 = BulkDiscount.create!(merchant_id: merch1.id, percentage: 20, quantity_threshold: 10)
+
+    visit merchant_bulk_discounts_path(merch1.id)
+    click_on('View Details')
+    expect(current_path).to eq(merchant_bulk_discount_path(merch1.id, discount1.id))
+    expect(page).to have_content('Discount: 20%')
+    expect(page).to have_content('Quantity Threshold: 10 Items')
+  end
+end
