0cc1bf69e450a23ca62c7b3cb0fae50ea7f54be4
Sunny Moore <moore.sunny@gmail.com>
2022-08-07 12:44:55 -0700

    Add new to bulk discounts

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -11,4 +11,21 @@ class MerchantBulkDiscountsController < ApplicationController
   def new
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def create
+    discount = BulkDiscount.new(discount_params)
+    
+    if discount.save
+      redirect_to merchant_bulk_discounts_path(params[:merchant_id])
+    else
+      redirect_to new_merchant_bulk_discount_path(params[:merchant_id])
+      # binding.pry
+      flash[:alert] = "Error: #{error_message(discount.errors)}"
+    end
+  end
+
+  private
+  def discount_params
+    params.permit(:merchant_id, :percentage, :quantity_threshold)
+  end
 end
