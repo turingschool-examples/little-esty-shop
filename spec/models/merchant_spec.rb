1533a9dc064b23cba3158a245f2c8aac3d37c2d8
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-13 14:18:07 -0700

    Add relationship tests to model spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do 
   describe 'validations' do
-    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'relationships' do
+    it { should have_many(:items) }
+    it { should have_many(:invoice_items).through(:items) }
+    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:transactions).through(:invoices) }
+    it { should have_many(:customers).through(:invoices) }
   end
 end
