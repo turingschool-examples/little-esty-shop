bc6d55a2185ad8559c8d1563fce24523c3024ad2
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-13 15:21:35 -0600

    tests written for relationships of everything except for customer & transactions

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,8 @@
 class Invoice < ApplicationRecord
   enum status: [:cancelled, :completed, :in_progress]
+  
+  belongs_to :customer
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 end
---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+RSpec.describe Invoice, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:customer) }
+    it { should have_many :invoice_items }
+    it { should have_many :transactions }
+    it { should have_many(:items).through(:invoice_items) }
+  end
+
+  describe 'validations' do
+    #should we put validations here?
+  end
+
+  before(:each) do
+    # @instance_var = Something.create!(input)
+  end
+
+  describe 'class methods' do
+    describe '#search' do
+      it 'returns partial matches' do
+       #method goes here
+      end
+    end
+  end
+
+  describe 'instance methods' do
+    describe '#method_name' do
+     it 'description of method' do
+      #expect statement here
+     end
+    end
+  end
+
+end
