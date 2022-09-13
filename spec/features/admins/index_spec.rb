1b82e7c44fdebd066d35866e4b74615ce10c84e9
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-13 16:14:22 -0600

    test: added tests for user story 2, merchant and invoice links

--- spec/features/admins/index_spec.rb
+++ spec/features/admins/index_spec.rb
@@ -8,6 +8,24 @@ RSpec.describe "Admin Dashboard" do
 
         expect(page).to have_content("Admin Dashboard")
       end
+
+      it "will show a link to the admin merchants index (/admin/merchants)and invoices index (/admin/invoices)" do
+        visit admins_path
+        expect(page).to have_link("Merchants Index")
+        expect(page).to have_link("Invoice Index")
+      end
+
+      it "will have working links to the merchants" do
+        visit admins_path
+        click_on "Merchants Index"
+        expect(current_path).to be("/admins/merchants")
+      end
+
+      it "will have working links to the invoice indexs" do
+        visit admins_path
+        click_on "Invoice Index"
+        expect(current_path).to be("/admins/invoices")
+      end
     end
   end
 end
