ed9e2db0c0fcf67b7735151fbcb9893ec00f7af5
Mary Ballantyne <mballantyne777@gmail.com>
2022-09-13 17:19:40 -0600

    finish creating csv rake file
    co-authored-by: Ryan ryan.chris.smith@gmail.com
    co-authored-by: Taryn taryn.orlemann@gmail.com
    co-authored-by: Shawn shawnl93@hotmail.com

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_12_213734) do
+ActiveRecord::Schema.define(version: 2022_09_13_230254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2022_09_12_213734) do
 
   create_table "transactions", force: :cascade do |t|
     t.bigint "invoice_id"
-    t.integer "credit_card_number"
+    t.string "credit_card_number"
     t.string "credit_card_expiration_date"
     t.boolean "result"
     t.datetime "created_at", null: false
