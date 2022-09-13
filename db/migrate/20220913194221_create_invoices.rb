a941b0a9f95cfdec5e6545691ee0fc9db704107b
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-09-13 14:09:00 -0600

    add all of the database tables/schemas for the csv files. migrations complete.

---
+++ db/migrate/20220913194221_create_invoices.rb
@@ -0,0 +1,9 @@
+class CreateInvoices < ActiveRecord::Migration[5.2]
+  def change
+    create_table :invoices do |t|
+      t.references :customer, foreign_key: true
+      t.integer :status
+      t.timestamps
+    end
+  end
+end
