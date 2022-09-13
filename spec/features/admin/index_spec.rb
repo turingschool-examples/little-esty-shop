8a43846857a16010b5d57e98c153e0bbd1b05562
Jake Kim <imjakekim@gmail.com>
2022-09-13 16:43:14 -0700

    created incomplete invoices test

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin Dashboard Page', type: :feature do
+    let!(:joey) { Customer.create!(first_name: "Joey", last_name: "Ondricka")}
+
+    let!(:invoice1) { joey.invoices.create!(status: 2) }
+    let!(:invoice2) { joey.invoices.create!(status: 1) }
+    let!(:invoice3) { joey.invoices.create!(status: 0) }
+    let!(:invoice4) { joey.invoices.create!(status: 2) }
+
     describe 'Header' do
         it 'indicates admin is on the dashboard' do
             visit("/admin")
             expect(page).to have_content("Admin Dashboard")
+        end
+    end
+
+    describe 'Incomplete Invoices' do
+        it 'has a section for incomplete invoices' do
+            visit("/admin")
+            expect(page).to have_content("Invoice ##{invoice1.id}")
+            expect(page).to have_content("Invoice ##{invoice4.id}")
         end
     end
 end
