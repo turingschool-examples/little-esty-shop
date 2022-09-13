3f606436b0f8a7229166fbc2bd33f86531c0a40c
A.J. Krumholz <krumholz.alan@gmail.com>
2022-09-13 18:04:10 -0400

    add feat: merchant items show displays item attributes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,9 @@
 class ItemsController < ApplicationController
-  
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 end
