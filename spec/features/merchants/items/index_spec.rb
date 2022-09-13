aa728e80b53546e7b79ac3c26899352aa0e5eb50
Candace Eckels <candace.eckels@gmail.com>
2022-09-13 16:43:00 -0600

    added the merchant-items-grouped-by-status test to spec file

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -23,4 +23,25 @@ RSpec.describe 'Merchants Items' do
       end
     end
   end
+
+
+  describe 'Items can be grouped by status' do
+    it 'items are in enabled_section that match their status' do
+      merch1 = Merchant.create!(name: Faker::Movies::VForVendetta.character)
+      merch2 = Merchant.create!(name: Faker::Movies::VForVendetta.character)
+
+      item1 = Item.create!(name: Faker::Games::Minecraft.item, description: Faker::Games::Minecraft.block, unit_price:Faker::Number.number(digits: 5), merchant_id: merch1.id)
+      item2 = Item.create!(name: Faker::Games::Minecraft.item, description: Faker::Games::Minecraft.block, unit_price:Faker::Number.number(digits: 5), merchant_id: merch1.id)
+      item3 = Item.create!(name: Faker::Games::Minecraft.item, description: Faker::Games::Minecraft.block, unit_price:Faker::Number.number(digits: 5), merchant_id: merch1.id)
+      item4 = Item.create!(name: Faker::Games::Minecraft.item, description: Faker::Games::Minecraft.block, unit_price:Faker::Number.number(digits: 5), merchant_id: merch1.id)
+      item5 = Item.create!(name: Faker::Games::Minecraft.item, description: Faker::Games::Minecraft.block, unit_price:Faker::Number.number(digits: 5), merchant_id: merch1.id)
+
+      visit merchant_items_path(merch1)
+
+      # within '.items' do
+      #   within '.enabled-items' do
+      #   end
+      # end
+    end
+  end
 end
