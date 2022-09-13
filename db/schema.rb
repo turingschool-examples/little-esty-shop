cfb513a41e230935194886d37aae550ef02d3c0e
A.J. Krumholz <krumholz.alan@gmail.com>
2022-09-12 21:11:57 -0400

    merge csv_load test branches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_13_010736) do
+ActiveRecord::Schema.define(version: 2022_09_12_234218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
