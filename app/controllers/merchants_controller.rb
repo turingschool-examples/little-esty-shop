9576145e0d76bc32e4b0caf486191b236fd8f609
Andrew Mullins <mullinsand15@gmail.com>
2022-09-12 19:49:50 -0600

    add dashboard view and test is passing

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -2,4 +2,10 @@ class MerchantsController < ApplicationController
   def dashboard
     @merchant = Merchant.find(params[:id])
   end
+
+  private
+
+  def merchant_params
+    params.permit(:name)
+  end
 end
