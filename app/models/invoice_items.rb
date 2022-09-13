e7099c9713220f2d9df4e8659f3a6add8b76e524
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-12 22:28:44 -0600

    feat: added all model relations and testing, all tests break

---
+++ app/models/invoice_items.rb
@@ -0,0 +1,4 @@
+class InvoiceItems < ApplicationRecord
+  belongs_to :item
+  belongs_to :invoice
+end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Invoice, type: :model do
   describe 'relationships' do
     it { should belong_to(:customer) }
+    it { should have_many(:transactions) }
     it { should have_many(:invoice_items) }
     it { should have_many(:items).through(:invoice_items) }
   end
