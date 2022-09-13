202bd34fe5af8705645529835979b3a26123ba5e
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-13 16:39:46 -0600

    Finish database setup

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
   validates_presence_of :invoice_id
   validates_presence_of :credit_card_number
-  validates_presence_of :credit_card_expiration_date
+  validates_presence_of :credit_card_expiration_date, allow_blank: true
   validates_presence_of :result
   validates_numericality_of :invoice_id
   validates_numericality_of :credit_card_number
