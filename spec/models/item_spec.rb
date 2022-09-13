8385bc788d7aa8374f5c2217126af77e330de834
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-12 22:13:58 -0600

    feat: completed item model and test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to(:merchant) }
-    it { should have_many :invoice_items }
+    it { should have_many(:invoice_items) }
+    it { should have_many(:invoices).through(:invoice_items) }
   end
 end
