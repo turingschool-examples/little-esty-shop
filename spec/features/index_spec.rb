3b474600ac34714c0ae0b6a5dc9344f57b8ec476
Sid Mann <sjmann2@outlook.com>
2022-09-13 20:31:03 -0500

    Add header for admin dashboard

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'the admin dashboard' do
-  
+  describe 'When I visit the admin dashboard' do
+    describe 'I see a header indicating I am on the admin dashboard' do
+      it 'has a header on the admin dashboard' do
+        visit admin_index_path
+
+        expect(page).to have_content("Welcome to the Admin Dashboard")
+      end
+    end
+  end
 end
+# As an admin,
+# When I visit the admin dashboard (/admin)
+# Then I see a header indicating that I am on the admin dashboard
