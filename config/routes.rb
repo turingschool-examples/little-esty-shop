2e7259d465e8c1cf3eb78188b7c997414db0844a
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-09-13 21:27:05 -0600

    feat: add index methods to items/invoices controllers and passing feature tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get("/merchants/:id/dashboard",   to: "merchants#show")
+  get "/merchants/:id/dashboard",   to: "merchants#show"
+  get "/merchants/:id/items", to: "items#index"
+  get "/merchants/:id/invoices", to: "invoices#index"
 end
