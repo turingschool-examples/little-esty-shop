528d10dbf77a7d70d826aff45d6bcbe0f808fe72
Madeline Mauser <madeline.mauser@gmail.com>
2022-09-14 16:53:09 -0600

    Add item update form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,20 @@ class ItemsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to merchant_item_path(params[:merchant_id], params[:id])
+      flash[:notice] = 'Item edited successfully!'
+    else
+      redirect_to edit_merchant_item_path(params[:merchant_id], params[:id])
+      flash[:alert] = "Error: #{@item.errors.full_messages.to_sentence}"
+    end
+  end
+
   private
 
+  def item_params
+    params.permit(:name, :description, :unit_price)
+  end
 end
