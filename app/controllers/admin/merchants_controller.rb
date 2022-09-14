c5113c58532cff515cd1eacb2e3be9c12563e150
Riley McCullough <rileybmccullough@gmail.com>
2022-09-13 20:56:20 -0600

    Create admin merchant routes, and create admin merchant controller

---
+++ app/controllers/admin/merchants_controller.rb
@@ -0,0 +1,8 @@
+class Admin::MerchantsController < ApplicationController
+
+  def
+
+
+
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   resources :invoices
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+   resources :invoices, only: [:index, :show, :update]
+   resources :imerchants, only: [:index]
+ end
+
+
 end
