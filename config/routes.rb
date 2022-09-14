cca5df96e51fcdcb200bb3f34ed4f9498730e058
Kevin Ta <kvnta@yahoo.com>
2022-09-13 23:39:28 -0600

    Test: merchant dashboard links

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :merchants
   resources :invoices
   resources :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  get '/merchants/:id/dashboard', to: 'merchants#dashboard'
+  
 end
