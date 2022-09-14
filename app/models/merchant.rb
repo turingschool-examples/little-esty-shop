1db4db06013a0a610ac5bc46ec923af8e8c12986
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-14 15:24:32 -0600

    top five items method working in models

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,15 @@ class Merchant < ApplicationRecord
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+
+  def top_five_items
+    items.joins(:transactions)
+    .where(invoices: {status: 2}, transactions: {result: 'success'})
+    .select("items.id, items.name, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+    .group(:id)
+    .order("revenue desc")
+    .limit(5)
+  end
+
+
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Merchant, type: :model do 
+RSpec.describe Merchant, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
@@ -11,6 +11,75 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:invoices).through(:invoice_items) }
     it { should have_many(:transactions).through(:invoices) }
     it { should have_many(:customers).through(:invoices) }
+  end
+
+  describe 'class methods' do
+    it '#top_five_merchants' do
+      merchant_1 = Merchant.create!(name: "Bread Pitt")
+
+      item_1 = Item.create!(name: "Sourdough", description: "leavened, wild yeast", unit_price: 400, merchant_id: merchant_1.id )
+      item_2 = Item.create!(name: "Baguette", description: "Soft, french", unit_price: 900, merchant_id: merchant_1.id )
+      item_3 = Item.create!(name: "Brioche", description: "Rich, rolled", unit_price: 1100, merchant_id: merchant_1.id )
+      item_4 = Item.create!(name: "Ciabatta", description: "Italy's answer to the baguette", unit_price: 1800, merchant_id: merchant_1.id )
+      item_5 = Item.create!(name: "Rye Bread", description: "Too many jokes to be had", unit_price: 500, merchant_id: merchant_1.id )
+
+      customer_1 = Customer.create!(first_name: "Meat", last_name: "Loaf")
+      customer_2 = Customer.create!(first_name: "Shannon", last_name: "Dougherty")
+      customer_3 = Customer.create!(first_name: "Puff", last_name: "Daddy")
+      customer_4 = Customer.create!(first_name: "Walter", last_name: "Wheat")
+      customer_5 = Customer.create!(first_name: "David", last_name: "Dowie")
+      customer_6 = Customer.create!(first_name: "Clint", last_name: "Yeastwood")
+
+      invoice_1 = Invoice.create!(status: 2, customer_id: customer_1.id)
+      invoice_2 = Invoice.create!(status: 2, customer_id: customer_2.id)
+      invoice_3 = Invoice.create!(status: 2, customer_id: customer_3.id)
+      invoice_4 = Invoice.create!(status: 2, customer_id: customer_4.id)
+      invoice_5 = Invoice.create!(status: 2, customer_id: customer_5.id)
+      invoice_6 = Invoice.create!(status: 0, customer_id: customer_6.id)
+
+      transaction_1 = Transaction.create!(result: 0, invoice_id: invoice_1.id, credit_card_number: "255448968")
+      transaction_2 = Transaction.create!(result: 0, invoice_id: invoice_1.id, credit_card_number: "255448968")
+      transaction_3 = Transaction.create!(result: 0, invoice_id: invoice_1.id, credit_card_number: "255448968")
+      transaction_4 = Transaction.create!(result: 0, invoice_id: invoice_1.id, credit_card_number: "255448968")
+      transaction_5 = Transaction.create!(result: 0, invoice_id: invoice_1.id, credit_card_number: "255448968")
+      transaction_6 = Transaction.create!(result: 0, invoice_id: invoice_1.id, credit_card_number: "255448968")
+
+      transaction_7 = Transaction.create!(result: 0, invoice_id: invoice_2.id, credit_card_number: "355646888")
+      transaction_8 = Transaction.create!(result: 0, invoice_id: invoice_2.id, credit_card_number: "355646888")
+      transaction_9 = Transaction.create!(result: 0, invoice_id: invoice_2.id, credit_card_number: "355646888")
+      transaction_10 = Transaction.create!(result: 0, invoice_id: invoice_2.id, credit_card_number: "355646888")
+      transaction_11 = Transaction.create!(result: 0, invoice_id: invoice_2.id, credit_card_number: "355646888")
+
+      transaction_12 = Transaction.create!(result: 0, invoice_id: invoice_3.id, credit_card_number: "888889999")
+      transaction_13 = Transaction.create!(result: 0, invoice_id: invoice_3.id, credit_card_number: "888889999")
+      transaction_14 = Transaction.create!(result: 0, invoice_id: invoice_3.id, credit_card_number: "888889999")
+      transaction_15 = Transaction.create!(result: 0, invoice_id: invoice_3.id, credit_card_number: "888889999")
+
+      transaction_16 = Transaction.create!(result: 0, invoice_id: invoice_4.id, credit_card_number: "123456789")
+      transaction_17 = Transaction.create!(result: 0, invoice_id: invoice_4.id, credit_card_number: "123456789")
+      transaction_18 = Transaction.create!(result: 0, invoice_id: invoice_4.id, credit_card_number: "123456789")
+
+      transaction_19 = Transaction.create!(result: 0, invoice_id: invoice_5.id, credit_card_number: "987654321")
+      transaction_20 = Transaction.create!(result: 0, invoice_id: invoice_5.id, credit_card_number: "987654321")
+
+      transaction_21 = Transaction.create!(result: 0, invoice_id: invoice_6.id, credit_card_number: "654498711")
+      transaction_22 = Transaction.create!(result: 1, invoice_id: invoice_6.id, credit_card_number: "654498711")
+
+      invoice_item_1 = InvoiceItem.create!(quantity: 4, unit_price: 850, status: 2, item_id: item_1.id, invoice_id: invoice_1.id)
+      invoice_item_2 = InvoiceItem.create!(quantity: 2, unit_price: 1300, status: 2, item_id: item_2.id, invoice_id: invoice_2.id)
+      invoice_item_3 = InvoiceItem.create!(quantity: 3, unit_price: 999, status: 2, item_id: item_3.id, invoice_id: invoice_3.id)
+      invoice_item_4 = InvoiceItem.create!(quantity: 2, unit_price: 1200, status: 2, item_id: item_4.id, invoice_id: invoice_4.id)
+      invoice_item_5 = InvoiceItem.create!(quantity: 3, unit_price: 500, status: 2, item_id: item_5.id, invoice_id: invoice_5.id)
+      invoice_item_6 = InvoiceItem.create!(quantity: 3, unit_price: 444, status: 2, item_id: item_5.id, invoice_id: invoice_6.id)
+
+      expect(merchant_1.top_five_items).to eq([item_1, item_2, item_3, item_4, item_5])
+    end
+  end
+
+  describe 'instance methods' do
+    it '#top_five_items' do
+
+    end
   end
 end
 
