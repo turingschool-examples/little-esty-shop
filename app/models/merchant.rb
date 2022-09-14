3a36e45c461924c6f98034214e37862b0b825700
A.J. Krumholz <krumholz.alan@gmail.com>
2022-09-14 09:21:19 -0400

    add feat: merchant items index has enabled/disabled sections with buttons

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,3 +2,16 @@ class Merchant < ApplicationRecord
   has_many :items
   validates :name, presence: true
 end
+
+#though methods are supposed to be public by default, for some reason these are private unless specified public. not sure why that is.
+public
+def enabled_items
+  items.where(enabled: true)
+end
+
+def disabled_items
+  items.where(enabled: false)
+end
+
+
+
--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -4,18 +4,18 @@ RSpec.describe 'Merchant Items Index' do
   let!(:carly) { Merchant.create!(name: "Carly Simon's Candy Silo")}
   let!(:bmv) { Merchant.create!(name: "Bavarian Motor Velocycles")}
 
-  let!(:licorice) { carly.items.create!(name: "Licorice Funnels", description: "Some stuff", unit_price: 1200) }
-  let!(:peanut) { carly.items.create!(name: "Peanut Bronzinos", description: "Some stuff", unit_price: 1500) }
-  let!(:choco_waffle) { carly.items.create!(name: "Chocolate Waffles Florentine", description: "Some stuff", unit_price: 900) }
-  let!(:hummus) { carly.items.create!(name: "Hummus Snocones", description: "Some stuff", unit_price: 1200) }
+  let!(:licorice) { carly.items.create!(name: "Licorice Funnels", description: "Some stuff", unit_price: 1200, enabled: true) }
+  let!(:peanut) { carly.items.create!(name: "Peanut Bronzinos", description: "Some stuff", unit_price: 1500, enabled: true) }
+  let!(:choco_waffle) { carly.items.create!(name: "Chocolate Waffles Florentine", description: "Some stuff", unit_price: 900, enabled: false) }
+  let!(:hummus) { carly.items.create!(name: "Hummus Snocones", description: "Some stuff", unit_price: 1200, enabled: false) }
 
-  let!(:skooter) { bmv.items.create!(name: "Hollenskooter", description: "Some stuff", unit_price: 12000) }
-  let!(:rider) { bmv.items.create!(name: "Hosenpfloofer", description: "Some stuff", unit_price: 220000) }
+  let!(:skooter) { bmv.items.create!(name: "Hollenskooter", description: "Some stuff", unit_price: 12000, enabled: true) }
+  let!(:rider) { bmv.items.create!(name: "Hosenpfloofer", description: "Some stuff", unit_price: 220000, enabled: true) }
 
   describe 'merchant items index page' do
     it 'lists the names of all items' do
       visit merchant_items_path(carly)
-
+      save_and_open_page
       expect(page).to have_content(licorice.name)
       expect(page).to have_content(peanut.name)
       expect(page).to have_content(choco_waffle.name)
@@ -29,15 +29,75 @@ RSpec.describe 'Merchant Items Index' do
       expect(page).to_not have_content(rider.name)
     end
 
-    # commented out because merchant items show tests this exactly. up to interpretation which spec it belongs in!
-    # it 'items link to merchant item show page' do
-    #   visit merchant_items_path(carly)
+    it 'has sections for enabled and disabled items' do
+      visit merchant_items_path(carly)
 
-    #   click_on licorice.name
+      within("#enabled_items") do
+        expect(page).to have_content(licorice.name)
+        expect(page).to_not have_content(hummus.name)
+      end
 
-    #   expect(current_path).to eq(merchant_item_path(carly, licorice))
-    # end
+      within("#disabled_items") do
+        expect(page).to have_content(hummus.name)
+        expect(page).to_not have_content(licorice.name)
+      end
+    end
+    
+    describe 'for each enabled item' do
+      it 'has a button to disable item' do
+        visit merchant_items_path(carly)
 
+        within("#enabled_items") do
+          expect(page).to have_content(licorice.name)
+        end
+  
+        within("#disabled_items") do
+          expect(page).to_not have_content(licorice.name)
+        end
+
+        within("#item-#{licorice.id}") do
+          click_on "Disable"
+        end
+
+        expect(current_path).to eq(merchant_items_path(carly))
+
+        within("#enabled_items") do
+          expect(page).to_not have_content(licorice.name)
+        end
+  
+        within("#disabled_items") do
+          expect(page).to have_content(licorice.name)
+        end
+      end
+    end
+
+    describe 'for each disabled item' do
+      it 'has a button to enable item' do
+        visit merchant_items_path(carly)
+
+        within("#disabled_items") do
+          expect(page).to have_content(hummus.name)
+        end
+  
+        within("#enabled_items") do
+          expect(page).to_not have_content(hummus.name)
+        end
+
+        within("#item-#{hummus.id}") do
+          click_on "Enable"
+        end
+
+        expect(current_path).to eq(merchant_items_path(carly))
+
+        within("#disabled_items") do
+          expect(page).to_not have_content(hummus.name)
+        end
+  
+        within("#enabled_items") do
+          expect(page).to have_content(hummus.name)
+        end
+      end
+    end
   end
 end
 
