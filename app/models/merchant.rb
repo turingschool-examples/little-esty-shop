7afc133aa2e52f013e56d694e672f79d7d05deb5
Candace Eckels <candace.eckels@gmail.com>
2022-09-14 12:00:11 -0600

    Model tests complete for top 5

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,16 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   has_many :items
+
+  def items_sorted_by_revenue
+    items.
+    joins(:invoice_items).
+    select('items.*, sum(invoice_items.quantity *invoice_items.unit_price) as revenue').
+    group('items.id').
+    order(revenue: :desc)
+  end
+
+  def top_five_items
+    items_sorted_by_revenue.limit(5)
+  end
 end
