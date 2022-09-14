b9bf4de7f28890291110d4fcc73f51d75865a96e
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-14 12:56:54 -0600

    rspec written for story 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/admin", to: "admin#index"
+  get "/admin/merchants", to: 'admin_merchants#index'
+
 end
--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe 'admin dashboard' do
       end
     end
   end
+
+  describe 'US2' do
+    describe 'admin index page' do
+      it 'has a link to the admin merchants index' do
+        visit "/admin"
+
+        click_link "Admin Merchants Index"
+
+        expect(current_path).to eq("/admin/merchants")
+      end
+
+      it 'has a link to the admin invoices index'
+    end
+  end
+
+
 end
