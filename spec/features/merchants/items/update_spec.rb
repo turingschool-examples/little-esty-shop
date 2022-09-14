bf2abaa95087ffa42de9ed438bc302ed0299bb22
Madeline Mauser <madeline.mauser@gmail.com>
2022-09-14 17:15:20 -0600

    Fix errors relating to route helpers

--- spec/features/merchants/items/update_spec.rb
+++ spec/features/merchants/items/update_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'merchant item edit page' do
   end
   describe 'page content' do
     it 'shows a form to edit the item' do
-      visit "/merchants/#{@merchant.id}/items/#{@item.id}/edit"
+      visit edit_merchant_item_path(@merchant, @item)
 
       expect(find('form')).to have_content('Name')
       expect(find('form')).to have_content('Description')
@@ -23,12 +23,12 @@ RSpec.describe 'merchant item edit page' do
   end
   context 'valid data was submitted' do
     it 'updates the item when the form is submitted and redirects to the item show page' do
-      visit edit_merchant_item_path(@merchant.id, @item.id)
+      visit edit_merchant_item_path(@merchant, @item)
 
       fill_in 'Name', with: 'Rocket Ball'
       click_button 'Save'
 
-      expect(page).to have_current_path(merchant_item_path(@merchant.id, @item.id))
+      expect(page).to have_current_path(merchant_item_path(@merchant, @item))
       expect(page).to have_content('Rocket Ball')
       expect(page).to_not have_content('Super Ball')
       expect(page).to have_content('Item edited successfully!')
@@ -36,12 +36,12 @@ RSpec.describe 'merchant item edit page' do
   end
   context 'invalid data was submitted' do
     it 'redirects to the edit page and displays an error message' do
-      visit edit_merchant_item_path(@merchant.id, @item.id)
+      visit edit_merchant_item_path(@merchant, @item)
 
       fill_in 'Name', with: ''
       click_button 'Save'
 
-      expect(page).to have_current_path(edit_merchant_item_path(@merchant.id, @item.id))
+      expect(page).to have_current_path(edit_merchant_item_path(@merchant, @item))
       expect(page).to have_content("Error: Name can't be blank")
     end
   end
