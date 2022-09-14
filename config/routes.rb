5b22bc39725ce2abce8a68a0c8b70ad2030f5054
Thomas Musselman <tmusselman@gmail.com>
2022-09-14 14:18:20 -0600

    created basic routes folder for organization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/merchants/:id/dashboard', to: 'merchants#dashboard'
+
+  resources :merchants do
+    resources :items
+    resources :invoices
+  end
+
+  namespace :admin do
+    resources :items
+    resources :invoices
+    resources :merchants
+  end
 end
