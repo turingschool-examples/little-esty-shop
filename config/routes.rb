234dc0e510dfb6619f27023dd70db184d29a45b3
Sid Mann <sjmann2@outlook.com>
2022-09-13 20:19:13 -0500

    Add admin routes, spec, and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :admin, only: [:index] do
+    resources :merchants, only: [:index]
+    resources :invoices, only: [:index]
+  end
 end
