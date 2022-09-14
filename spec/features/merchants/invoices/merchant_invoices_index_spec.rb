11a77527af7185492511891ca956510029549841
Thomas Musselman <tmusselman@gmail.com>
2022-09-14 17:06:38 -0600

    test: merchant invoices index page feature

---
+++ spec/features/merchants/invoices/merchant_invoices_index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Invoices Index Page', type: :feature do
+
+  before(:each) do
+    @merchant_1 = create(:merchant)
+    @item_1 = create(:item, merchant: @merchant_1)
+    @item_2 = create(:item, merchant: @merchant_1)
+
+    @merchant_2 = create(:merchant)
+    @item_3 = create(:item, merchant: @merchant_2)
+    @item_4 = create(:item, merchant: @merchant_2)
+
+    @invoice_1 = create(:invoice)
+    @invoice_2 = create(:invoice)
+    @invoice_3 = create(:invoice)
+    @invoice_4 = create(:invoice)
+
+    @invoice_1.items << [@item_1, @item_2, @item_3]
+    @invoice_2.items << [@item_3, @item_4]
+    @invoice_3.items << [@item_1, @item_4]
+    @invoice_4.items << @item_2
+  end
+
+  it 'lists the invoices that contain an item sold by the merchant' do
+    visit merchant_invoices_path(@merchant_1.id)
+    expect(page).to have_content("My Invoices")
+    expect(page).to have_content("Invoice ##{@invoice_1.id}")
+    expect(page).to have_content("Invoice ##{@invoice_3.id}")
+    expect(page).to have_content("Invoice ##{@invoice_4.id}")
+  end
+
+end
