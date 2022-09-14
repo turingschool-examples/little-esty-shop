91513c73b683d6d052f5e6aee6717f735c58560f
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-13 20:32:33 -0600

    correcting admin routing to be a namespace vs a resource request for correct rout set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :admin do
+  namespace :admin do
+    get '/', to: 'dashboard#index'
     resources :merchants
     resources :invoices
   end
