ce68e0f3cb80886e52a2a773800cd99c040441dc
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-14 13:06:54 -0600

    story 2 complete with routing through to index.html pages

---
+++ app/controllers/admin_invoices_controller.rb
@@ -0,0 +1,5 @@
+class AdminInvoicesController < ApplicationController
+  def index 
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/admin", to: "admin#index"
   get "/admin/merchants", to: 'admin_merchants#index'
+  get "/admin/invoices", to: 'admin_invoices#index'
 
 end
