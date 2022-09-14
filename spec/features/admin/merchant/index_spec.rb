944b8144fcc640e9d049b839e4e2caa185f86113
Andrew Mullins <mullinsand15@gmail.com>
2022-09-14 11:16:56 -0600

    add test for index admin merchant links

--- spec/features/admin/merchant/index_spec.rb
+++ spec/features/admin/merchant/index_spec.rb
@@ -20,5 +20,26 @@ RSpec.describe 'Admin Merchants Index' do
         end
       end
     end
+
+    it 'has each merchant name is a link to the admin merchant show page' do
+      merch1 = Merchant.create!(name: Faker::Movies::VForVendetta.character)
+      merch2 = Merchant.create!(name: Faker::Movies::VForVendetta.character)
+      merch3 = Merchant.create!(name: Faker::Movies::VForVendetta.character)
+      merch4 = Merchant.create!(name: Faker::Movies::VForVendetta.character)
+      merch5 = Merchant.create!(name: Faker::Movies::VForVendetta.character)
+
+      all_merchants = [merch1, merch2, merch3, merch4, merch5]
+      visit "/admin/merchants"
+
+      within '#merchants' do
+        all_merchants.each do |merchant|
+          within "#merchant_#{merchant.id}" do
+            click_link("#{merchant.name}")
+            expect(page).to have_current_path("/admin/merchants/#{merchant.id}")
+            visit "/admin/merchants"
+          end
+        end
+      end
+    end
   end
 end
