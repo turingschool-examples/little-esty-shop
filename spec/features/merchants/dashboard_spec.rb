c25c7340e050195b1bca40aa34b1d6b807fdff92
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-14 11:41:44 -0600

    Fix: Change Merchant to My

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -1,31 +1,39 @@
 require 'rails_helper'
 
+# As a merchant,
+# When I visit my merchant dashboard
+# Then I see the names of the top 5 customers
+# who have conducted the largest number of successful transactions with my merchant
+# And next to each customer name I see the number of successful transactions they have
+# conducted with my merchant
+
 RSpec.describe "merchant dashboard" do
   describe 'As a merchant, When I visit my merchant dashboard' do
     it 'I see the name of my merchant' do
-      bob = Merchant.create!(name: "Bob's Soaps")
-      sue = Merchant.create!(name: "Sue's Pastries")
-      
-      visit merchant_dashboards_path(bob)
+      visit merchant_dashboards_path(Merchant.first)
 
-      expect(page).to have_content(bob.name)
-      expect(page).to_not have_content(sue.name)
+      expect(page).to have_content(Merchant.first.name)
+      expect(page).to_not have_content(Merchant.last.name)
     end
 
     it 'I see link to my merchant items index' do
-      bob = Merchant.create!(name: "Bob's Soaps")
-      
-      visit merchant_dashboards_path(bob)
+      visit merchant_dashboards_path(Merchant.first)
 
-      expect(page).to have_link("Merchant Items")
+      expect(page).to have_link("My Items")
     end
 
     it 'I see link to my merchant invoices index' do
-      bob = Merchant.create!(name: "Bob's Soaps")
+      visit merchant_dashboards_path(Merchant.first)
 
-      visit merchant_dashboards_path(bob)
+      expect(page).to have_link("My Invoices")
+    end
 
-      expect(page).to have_link("Merchant Invoices")
+    xit 'I see the names of the top 5 customers who have conducted the largest number of successful transactions with my merchant' do
+      visit merchant_dashboards_path(Merchant.first)
+    end
+
+    xit 'next to each customer name I see the number of successful transactions they have conducted with my merchant' do
+      visit merchant_dashboards_path(Merchant.first)
     end
   end
 end
