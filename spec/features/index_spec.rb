200ccddae66e29bd8904762087277899c3dcbfc9
Sid Mann <sjmann2@outlook.com>
2022-09-14 13:20:17 -0500

    Add tests for links and top 5 customers

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -9,5 +9,40 @@ RSpec.describe 'the admin dashboard' do
         expect(page).to have_content("Welcome to the Admin Dashboard")
       end
     end
+
+    describe 'the top 5 customers who have conducted largest number of succesful transactions' do
+      xit 'lists the top 5 customers and number of successful transactions they have' do
+        visit admin_index_path
+        
+        expect(page).to have_content("Top 5 Customers")
+      end
+    end
+
+    describe 'I see a link to the admin merchants index and the admin invoices index' do
+      it 'links to the admin merchants and admin invoices index' do
+        visit admin_index_path
+
+        expect(page).to have_link('Admin merchant page')
+        expect(page).to have_link('Admin invoices page')
+      end
+    end
   end
 end
+
+# Admin Dashboard Links
+
+# As an admin,
+# When I visit the admin dashboard (/admin)
+# Then I see a link to the admin merchants index (/admin/merchants)
+# And I see a link to the admin invoices index (/admin/invoices)
+
+
+
+# As an admin,
+# When I visit the admin dashboard
+# Then I see the names of the top 5 customers
+# who have conducted the largest number of successful transactions
+# And next to each customer name I see the number of successful transactions they have
+# conducted
+
+Customer.distinct.select("customers.*").joins(invoices: :transactions).order('transactions.count').where("transactions.result" => 1).limit(5)
