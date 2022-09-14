e3682690e6019c6d3e2ed20c98b7f2a5c56c7d09
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-13 20:30:21 -0600

    corrected tests and spec location/name for correct routing

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -4,28 +4,28 @@ RSpec.describe "Admin Dashboard" do
   describe "As an admin" do
     describe "I visit the admin dashboard" do
       it "I see a header indicating that I am on the admin dashboard" do
-        visit admin_index_path
+        visit admin_path
 
         expect(page).to have_content("Admin Dashboard")
       end
 
       it "will show a link to the admin merchants index (/admin/merchants)and invoices index (/admin/invoices)" do
-        visit admin_index_path
+        visit admin_path
         
         expect(page).to have_link("Merchant Index")
         expect(page).to have_link("Invoice Index")
       end
 
       it "will have working links to the merchants" do
-        visit admin_index_path
+        visit admin_path
         click_on "Merchant Index"
-        expect(current_path).to eq("/admin/merchants")
+        expect(current_path).to eq(admin_merchants_path)
       end
 
       it "will have working links to the invoice indexs" do
-        visit admin_index_path
+        visit admin_path
         click_on "Invoice Index"
-        expect(current_path).to eq("/admin/invoices")
+        expect(current_path).to eq(admin_invoice_path)
       end
     end
   end
