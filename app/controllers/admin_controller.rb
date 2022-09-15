42c36ed04f68397c28ef7454d7ccd686829f5dca
Lucas Colwell <colwellc37@gmail.com>
2022-09-15 11:23:19 -0600

    Update controller for admin invoice story

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,6 @@
 class AdminController < ApplicationController
   def index
+    @invoices = Invoice.all
     @top_five_customers = Customer.five_top_customers
     @not_shipped_invoices = Invoice.not_shipped_invoices
   end
