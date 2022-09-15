a5adfbbf8633c4dcfe48cb9d5fb831e64d4f3926
Mary Ballantyne <mballantyne777@gmail.com>
2022-09-15 12:24:52 -0600

    added link to incomplete invoice id

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/admin", to: "admin#index"
   get "/admin/merchants", to: 'admin_merchants#index'
   get "/admin/invoices", to: 'admin_invoices#index'
+  get "/admin/invoices/:id", to: 'admin_invoices#show'
   get "/admin/merchants/:id", to: 'admin_merchants#show'
 
 end
