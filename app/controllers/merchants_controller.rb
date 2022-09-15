d10f2fff4736749ede2744705d5f3c360e7e26d4
Erik Riggs <riggs.erik@gmail.com>
2022-09-14 21:16:46 -0600

    test/feat: Add test for and view for 5 top cust merch

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -2,5 +2,8 @@ class MerchantsController < ApplicationController
 
   def show
     @merchant = Merchant.find(params[:id])
+
+    @best_customers = Customer.top_5_cust_by_merch(@merchant.id)
+    
   end
 end
