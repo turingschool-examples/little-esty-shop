fdb8a4d2112219af9caa0b44a7b051f331df61f6
Mary Ballantyne <mballantyne777@gmail.com>
2022-09-15 11:23:23 -0600

    add incomplete invoices method and spec to invoice items

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,18 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-    describe 'validations' do
-        it { should validate_presence_of(:quantity) }
-        it { should validate_presence_of(:unit_price) }
-        it { should validate_presence_of(:status) }
-    end
+  before :each do
+    @merchant_1 = Merchant.create!(name: "Bread Pitt")
 
-    describe 'relationships' do
-        it { should belong_to(:item) }
-        it { should belong_to(:invoice) }
-        it { should have_many(:merchants).through(:item) }
-        it { should have_many(:transactions).through(:invoice) }
-        it { should have_many(:customers).through(:invoice) }
-    end
+    @customer_1 = Customer.create!(first_name: "Meat", last_name: "Loaf")
+    @customer_2 = Customer.create!(first_name: "Shannon", last_name: "Dougherty")
+    @customer_3 = Customer.create!(first_name: "Puff", last_name: "Daddy")
 
+    @invoice_1 = Invoice.create!(status: 2, customer_id: @customer_1.id)
+    @invoice_2 = Invoice.create!(status: 2, customer_id: @customer_2.id)
+    @invoice_3 = Invoice.create!(status: 2, customer_id: @customer_3.id)
+
+    @item_1 = Item.create!(name: "Sourdough", description: "leavened, wild yeast", unit_price: 400, merchant_id: @merchant_1.id)
+    @item_2 = Item.create!(name: "Baguette", description: "Soft, french", unit_price: 900, merchant_id: @merchant_1.id)
+    @item_3 = Item.create!(name: "Brioche", description: "Rich, rolled", unit_price: 1100, merchant_id: @merchant_1.id)
+
+    @invoice_item_1 = InvoiceItem.create!(quantity: 4, unit_price: 850, status: 0, item_id: @item_1.id, invoice_id: @invoice_1.id)
+    @invoice_item_2 = InvoiceItem.create!(quantity: 2, unit_price: 1300, status: 2, item_id: @item_2.id, invoice_id: @invoice_2.id)
+    @invoice_item_3 = InvoiceItem.create!(quantity: 3, unit_price: 999, status: 2, item_id: @item_3.id, invoice_id: @invoice_3.id)
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:quantity) }
+    it { should validate_presence_of(:unit_price) }
+    it { should validate_presence_of(:status) }
+  end
+
+  describe 'relationships' do
+    it { should belong_to(:item) }
+    it { should belong_to(:invoice) }
+    it { should have_many(:merchants).through(:item) }
+    it { should have_many(:transactions).through(:invoice) }
+    it { should have_many(:customers).through(:invoice) }
+  end
+
+  describe 'Incomplete Invoices' do
+    it 'displays the incomplete_invoices id number' do
+      expect(InvoiceItem.incomplete_invoices).to eq([@invoice_1.id])
+    end
+  end
 end
