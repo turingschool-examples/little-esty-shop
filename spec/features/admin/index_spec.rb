c0a069e1717229dfd708a73af06e3a009690d88f
Jake Kim <imjakekim@gmail.com>
2022-09-14 19:06:18 -0700

    testing for date format

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Admin Dashboard Page', type: :feature do
     let!(:joey) { Customer.create!(first_name: "Joey", last_name: "Ondricka")}
 
-    let!(:invoice1) { joey.invoices.create!(status: 2) }
-    let!(:invoice2) { joey.invoices.create!(status: 1) }
-    let!(:invoice3) { joey.invoices.create!(status: 0) }
-    let!(:invoice4) { joey.invoices.create!(status: 2) }
-    # let!(:item1) { invoice1.items.create!(status: 2) }
-    # let!(:item2) { invoice4.items.create!(status: 2) }
+    let!(:invoice1) { joey.invoices.create!(status: 2, created_at: "2021-09-1 08:44:13 UTC") }
+    let!(:invoice2) { joey.invoices.create!(status: 1, created_at: "2022-08-2 08:44:13 UTC") }
+    let!(:invoice3) { joey.invoices.create!(status: 0, created_at: "2022-09-5 08:44:13 UTC") }
+    let!(:invoice4) { joey.invoices.create!(status: 2, created_at: "2022-09-8 08:44:13 UTC") }
+
     describe 'Header' do
         it 'indicates admin is on the dashboard' do
             visit admin_index_path
@@ -18,6 +17,18 @@ RSpec.describe 'Admin Dashboard Page', type: :feature do
 
     describe 'Incomplete Invoices' do
         it 'has a section for incomplete invoices' do
+            visit admin_index_path
+            within("#incomplete_invoices") do
+                expect(page).to have_content("Invoice ##{invoice1.id}")
+                expect(page).to have_content("Invoice ##{invoice4.id}")
+                expect(page).to_not have_content("Invoice ##{invoice2.id}")
+
+                click_on invoice1.id
+                expect(current_path).to eq("/admin/invoices/#{invoice1.id}")
+            end
+        end
+
+        it 'shows the invoice date' do
             visit admin_index_path
             within("#incomplete_invoices") do
                 expect(page).to have_content("Invoice ##{invoice1.id}")
