8530b0088c32020ab17702ec8fed8de3ca65175b
Candace Eckels <candace.eckels@gmail.com>
2022-09-15 12:29:09 -0600

    Working on model method for best day

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -53,6 +53,7 @@ RSpec.describe Item, type: :model do
   end
 
   describe "instance methods" do
+
     describe "#current_price" do
       it 'should divide the unit_price by 100' do
         merch1 = Merchant.create!(name: Faker::Movies::VForVendetta.character)
@@ -60,6 +61,14 @@ RSpec.describe Item, type: :model do
         item1 = Item.create!(name: Faker::Games::Minecraft.item, description: Faker::Games::Minecraft.block, unit_price: 1550, merchant_id: merch1.id)
 
         expect(item1.current_price).to eq 15.50
+      end
+    end
+
+    describe "#best_day" do
+
+      before
+      it 'shows the best day that an item sold' do
+        
       end
     end
   end
