aadb9182e8b466e1bc517d7f01e67078fd7d236f
Andrew Mullins <mullinsand15@gmail.com>
2022-09-14 20:08:24 -0600

    add migration to add status column to merchant table

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,6 @@
 class Merchant < ApplicationRecord
+  enum status: ["Enabled", "Disabled"]
   validates_presence_of :name
-
+  validates_presence_of :status, inclusion: ["Enabled", "Disabled"]
   has_many :items
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Merchant, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :status }
+    it { should define_enum_for(:status).with_values(["Enabled", "Disabled"])}
   end
 
   describe 'relationships' do
