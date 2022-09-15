cd9a5ff04abc30a7882af54f2b2a557135b0a08b
Thomas Musselman <tmusselman@gmail.com>
2022-09-14 22:19:56 -0600

    feat: merchants index show page and styling

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -6,6 +6,7 @@ class MerchantItemsController < ApplicationController
   end
 
   def show
+    @merchant = Merchant.find(params[:merchant_id])
     @item = Item.find(params[:id])
     @selling_price = @item.price_convert
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/merchants/:id/dashboard', to: 'merchants#dashboard'
+  get '/merchants/:id/dashboard', to: 'merchants#dashboard', as: :merchant_dashboard
 
   resources :merchants, except: [:index, :new, :edit, :update, :destroy] do
     resources :items, controller: :merchant_items, only: [:index, :show]
