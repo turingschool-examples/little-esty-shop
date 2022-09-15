d80a50ba032a045e9c550a5e58e0e53cf0852591
Andre Pedro <andrerpedro170@gmail.com>
2022-09-14 20:21:30 -0600

    stopping for the night, need to build update method

--- spec/features/admin/merchants/edit_spec.rb
+++ spec/features/admin/merchants/edit_spec.rb
@@ -9,5 +9,10 @@ RSpec.describe "Merchants Edit Page" do
     visit "admin/merchants/#{merchant2.id}/edit"
 
     expect(page).to have_content("Darnelles Daysies")
+
+    fill_in :name, with: "Roses Roses"
+    click_on "Update merchant"
+
+    expect(page).to have_content("#{merchant2.name} Show Page")
   end
-end 
+end
