a10768df1670cedac5019974b4bb888945d5ee77
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-15 04:13:45 -0600

    refactor unecessary bracket

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,7 +8,7 @@ class Customer < ApplicationRecord
   has_many :merchants, through: :items
 
   def self.top_five_customers
-    joins(invoices: [:transactions])
+    joins(invoices: :transactions)
     .group(:id).where(transactions: {result: 0})
     .select('customers.*, count(result) as success_count')
     .order(success_count: :desc)
