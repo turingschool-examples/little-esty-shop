458b03fbb3f5b054c05e32d1d8d93b183b890220
Lucas Colwell <colwellc37@gmail.com>
2022-09-15 11:22:47 -0600

    Test: admin invoice index test

---
+++ spec/features/admin/invoices/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'As an admin,' do
+  describe 'When I visit the admin Invoices index ("/admin/invoices")' do
+    it 'Then I see a list of all Invoice ids in the system' do
+      visit admin_invoices_path
+
+      # save_and_open_page
+
+      expect(page).to have_content("Invoice #1")
+      expect(page).to have_content("Invoice #2")
+      expect(page).to have_content("Invoice #3")
+      expect(page).to have_content("Invoice #4")
+    end
+
+    it 'Each id links to the admin invoice show page' do
+      expect(page).to have_link("Invoice #1")
+      expect(page).to have_link("Invoice #2")
+      expect(page).to have_link("Invoice #3")
+      expect(page).to have_link("Invoice #4")
+    end
+  end
+end
