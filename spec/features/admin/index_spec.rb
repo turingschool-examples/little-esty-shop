e553b20a728ea95f5bce5cc967b4080f7a2b78af
Jake Kim <imjakekim@gmail.com>
2022-09-14 19:13:26 -0700

    formatted time on view

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Admin Dashboard Page', type: :feature do
     let!(:joey) { Customer.create!(first_name: "Joey", last_name: "Ondricka")}
 
-    let!(:invoice1) { joey.invoices.create!(status: 2, created_at: "2021-09-1 08:44:13 UTC") }
-    let!(:invoice2) { joey.invoices.create!(status: 1, created_at: "2022-08-2 08:44:13 UTC") }
-    let!(:invoice3) { joey.invoices.create!(status: 0, created_at: "2022-09-5 08:44:13 UTC") }
-    let!(:invoice4) { joey.invoices.create!(status: 2, created_at: "2022-09-8 08:44:13 UTC") }
+    let!(:invoice1) { joey.invoices.create!(status: 2, created_at: "2021-09-1 08:44:13 PST") }
+    let!(:invoice2) { joey.invoices.create!(status: 1, created_at: "2022-08-2 08:44:13 PST") }
+    let!(:invoice3) { joey.invoices.create!(status: 0, created_at: "2022-09-5 08:44:13 PST") }
+    let!(:invoice4) { joey.invoices.create!(status: 2, created_at: "2022-09-8 08:44:13 PST") }
 
     describe 'Header' do
         it 'indicates admin is on the dashboard' do
@@ -34,9 +34,7 @@ RSpec.describe 'Admin Dashboard Page', type: :feature do
                 expect(page).to have_content("Invoice ##{invoice1.id}")
                 expect(page).to have_content("Invoice ##{invoice4.id}")
                 expect(page).to_not have_content("Invoice ##{invoice2.id}")
-
-                click_on invoice1.id
-                expect(current_path).to eq("/admin/invoices/#{invoice1.id}")
+                save_and_open_page
             end
         end
 
