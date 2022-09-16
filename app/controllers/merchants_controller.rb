472756f311e59d3bd475cffbeb29c342da7cd61c
Ken Lenhart <omnipaid@gmail.com>
2022-09-16 01:01:10 -0700

    Refactor merchant routes in routes.rb, refactor merchant paths in merchants/dashboard_spec.rb

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,8 +1,9 @@
 class MerchantsController < ApplicationController
 
   def show
-    @merchant = Merchant.find(params[:id])
-
+    
+    @merchant = Merchant.find(params[:merchant_id])
+    
     @best_customers = Customer.top_5_cust_by_merch(@merchant.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/welcome', to: 'welcome#index'
+  get '/', to: 'welcome#index'
 
   namespace :admin do
     get '/', to: 'dashboard#index'
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
     resources :invoices
   end
 
-  get '/merchants/:id/dashboard', to: 'merchants#show'
-
+  resources :merchants, only: [:show] do
+    get '/dashboard', to: 'merchants#show'
+    resources :invoices, only: [:show]
+  end
 
 end
