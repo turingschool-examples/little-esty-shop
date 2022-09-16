a5e5c0859335afbadd35615e796110aac787e6b6
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-15 22:21:01 -0600

    test: added test for user story 24, adding merchants to admin merchant page

---
+++ spec/features/admin/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Admin Merchants" do
+    describe "As an admin" do
+        describe "I visit the admin merchants index (/admin/merchants)" do
+            before :each do
+                @merchant_1 = Merchant.create!(name: "Johns Tools")
+                @merchant_2 = Merchant.create!(name: "Hannas Hammocks")
+                @merchant_3 = Merchant.create!(name: "Pretty Plumbing")
+            end
+        
+        # As an admin,
+        # When I visit the admin merchants index (/admin/merchants)
+        # Then I see the name of each merchant in the system
+            it 'can see the name of each merchant in the system' do
+                visit admin_merchants_path
+                expect(page).to have_content(@merchant_1.name)
+                expect(page).to have_content(@merchant_2.name)
+                expect(page).to have_content(@merchant_3.name)
+
+            end
+        end
+    end
+end
