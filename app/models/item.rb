2797e9b4fa3576c232f5ab90f445b1e7239476b2
A.J. Krumholz <krumholz.alan@gmail.com>
2022-09-15 20:58:12 -0400

    add best_sales_date instance method to item.rb, returns a datetime of day with best revenue for a given item

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,8 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def best_sales_date
+    self.invoices.select(:created_at, "invoice_items.unit_price*quantity as revenue").order("revenue desc", "invoices.created_at desc").first.created_at
+  end
 end
