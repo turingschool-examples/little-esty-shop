f754bfd36c4d5993dad364eda7b49a0b98bcea41
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-16 12:48:16 -0600

    tests for update merchant form on merchant edit page

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -20,12 +20,26 @@ RSpec.describe "Admin Show" do
         expect(page).to have_link("Update")
       end
 
-      it "When I click the link, I am taken to a page to edit this merchant"
+      it "When I click the link, I am taken to a page to edit this merchant" do
+        visit admin_merchant_path(@merchant_1)
+
+        click_link"Update"
+
+        expect(current_path).to eq(edit_admin_merchant_path(@merchant_1))
+      end
       
-      it "I see a form filled in with the existing merchant attribute information"
+      it "I see a form filled in with the existing merchant attribute information" do 
+        visit edit_admin_merchant_path(@merchant_1)
+        
+        expect(page).to have_field("Name", {with: "#{@merchant_1.name}"})
+      end
 
-      it "When I update the information in the form and I click ‘submit’, I am redirected back to the merchant's admin show page where I see the updated information and I see a flash message stating that the information has been successfully updated"
-
+      it "When I update the information in the form and I click ‘submit’, I am redirected back to the merchant's admin show page where I see the updated information and I see a flash message stating that the information has been successfully updated" do
+        visit edit_admin_merchant_path(@merchant_1)
+        save_and_open_page
+        fill_in "Name",	with: "Billy Johns Tools"
+        click_button"Update Merchant"
+      end
     end
   end
 end
