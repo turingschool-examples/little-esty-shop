bd60a3f7377d4145c9a0767ebeef051121c878fa
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-16 17:35:34 -0600

    test written for #incomplete_invoices

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,26 +7,25 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:items).through(:invoice_items) }
   end
 
-  describe 'validations' do
-    #should we put validations here?
-  end
+  let!(:alaina) { Customer.create!(first_name: "Alaina", last_name: "Kneiling")}
+  let!(:eddie) { Customer.create!(first_name: "Eddie", last_name: "Young")}
+  let!(:leah) { Customer.create!(first_name: "Leah", last_name: "Anderson")}
+  let!(:polina) { Customer.create!(first_name: "Polina", last_name: "Eisenberg")}
 
-  before(:each) do
-    # @instance_var = Something.create!(input)
-  end
+  let!(:alaina_invoice2) { alaina.invoices.create!(status: "in_progress")}
+  let!(:eddie_invoice1) { eddie.invoices.create!(status: "completed", created_at: "2000-01-30 14:54:09")}
+  let!(:eddie_invoice2) { eddie.invoices.create!(status: "completed")}
+  let!(:polina_invoice1) { polina.invoices.create!(status: "completed")}
+  let!(:polina_invoice2) { polina.invoices.create!(status: "cancelled")}
+  let!(:leah_invoice1) { leah.invoices.create!(status: "cancelled")}
+  let!(:leah_invoice2) { leah.invoices.create!(status: "in_progress")}
 
-  describe 'class methods' do
-    describe '#search' do
-      it 'returns partial matches' do
-       #method goes here
-      end
-    end
-  end
 
   describe 'instance methods' do
-    describe '#format_' do
-     it 'description of method' do #make a method to format date and time
-      #expect statement here
+    describe '#incomplete_invoices' do
+     it 'can return the invoices that have a status of in progress' do 
+
+      expect(Invoice.incomplete_invoices).to eq([alaina_invoice2, leah_invoice2])
      end
     end
   end
