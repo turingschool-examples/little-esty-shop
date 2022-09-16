5e4641dccf648fe30120670d38e5eeb3336c7c7d
Shawn Lee <Shawnl93@hotmail.com>
2022-09-15 22:53:44 -0600

    fix: method name and fixed id to dynamic

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -74,7 +74,7 @@ RSpec.describe Merchant, type: :model do
     end
 
     xit "top 5 customers" do
-      expect(Customer.top_five_customers).to eq([@customer_1, @customer_2, @customer_3, @customer_4, @customer_5])
+      expect(Customer.top_five_customers_by_merchant).to eq([@customer_1, @customer_2, @customer_3, @customer_4, @customer_5])
     end
   end
 end
