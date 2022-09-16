05ca96e4bf985e2795f31bdd230bdfb74f644fa9
Gavin Carew <gjcarew@gmail.com>
2022-09-16 09:47:15 -0700

    Add tests for user story 21 - top 5 customers

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,26 +4,21 @@ FactoryBot.define do
     last_name  { Faker::Name.last_name }
   end
 
-  factory :invoice do
-    status {"completed"}
-    customer
-  end
-
   # factory :invoice do
   #   status {"completed"}
   #   customer_id {}
   # end
   #
-  # factory :invoice do
-  #   status {["in progress", "completed", "cancelled"].sample}
-  #   customer
-  # end
+  factory :invoice do
+    status {["in progress", "completed", "cancelled"].sample}
+    customer
+  end
 
   factory :transaction do
-    invoice_id {Faker::Number.number(digits:3)}
-    credit_card_number {Faker::Number.number(digits: 10)}
-    credit_card_expiration_date {Faker::Number.number(digits: 4)}
-    result {"success"}
+    invoice_id { Faker::Number.number(digits: 3) }
+    credit_card_number { Faker::Number.number(digits: 10) }
+    credit_card_expiration_date { Faker::Number.number(digits: 4) }
+    result { [:success, :failure].sample }
   end
 
 end
--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -26,46 +26,38 @@ RSpec.describe 'admin dashboard page' do
 
   describe 'dashboard statistics' do
     before :each do
-
-      # create_list(:invoice, 6)
-      # @customer1 = create(:customer)
-      # create_list(:invoice, 2, customer_id: @customer1.id)
-
-      # FactoryBot.create_list(:invoice, 6)
-      # @customer1 = FactoryBot.create(:customer, )
-      # FactoryBot.create_list(:invoice, 2, customer_id: @customer1.id)
-
       @customer1 = create(:customer)
       @customer2 = create(:customer)
       @customer3 = create(:customer)
-      @customer4 = FactoryBot.create(:customer)
-      @customer5 = FactoryBot.create(:customer)
-      @customer6 = FactoryBot.create(:customer)
-      @invoice1 = create_list(:invoice, 1, customer_id: @customer1.id)
-      @invoice2 = create_list(:invoice, 2, customer_id: @customer2.id)
-      @invoice3 = create_list(:invoice, 3, customer_id: @customer3.id)
-      @invoice4 = FactoryBot.create_list(:invoice, 4, customer_id: @customer4.id)
-      @invoice5 = FactoryBot.create_list(:invoice, 5, customer_id: @customer5.id)
-      @invoice6 = FactoryBot.create_list(:invoice, 6, customer_id: @customer6.id)
-      @invoices = [@invoice1, @invoice2, @invoice3, @invoice4, @invoice5, @invoice6].flatten
-
-        @invoices.each do |invoice|
-          require "pry"; binding.pry
-          create(:transaction, invoice_id: invoice.id)
-        end
-
+      @customer4 = create(:customer)
+      @customer5 = create(:customer)
+      @customer6 = create(:customer)
+      @invoice_list_1 = create_list(:invoice, 1, customer_id: @customer1.id)
+      @failed_invoices_1 = create_list(:invoice, 3, customer_id: @customer1.id)
+      @invoice_list_2 = create_list(:invoice, 2, customer_id: @customer2.id)
+      @invoice_list_3 = create_list(:invoice, 3, customer_id: @customer3.id)
+      @invoice_list_4 = create_list(:invoice, 4, customer_id: @customer4.id)
+      @invoice_list_5 = create_list(:invoice, 5, customer_id: @customer5.id)
+      @invoice_list_6 = create_list(:invoice, 6, customer_id: @customer6.id)
     end
 
     it 'can return list of top 5 customers in desending order of completed invoices' do
+      invoices = [@invoice_list_1, @invoice_list_2, @invoice_list_3, @invoice_list_4, @invoice_list_5, @invoice_list_6].flatten
+
+      invoices.each do |invoice|
+        create(:transaction, invoice_id: invoice.id, result: :success)
+      end
+      @failed_invoices.each do |invoice|
+        create(:transaction, invoice_id: invoice.id, result: :failure)
+      end
+
       visit admin_path
-require "pry"; binding.pry
-      expect(page).to_not have_content("#{customer1.name}")
-
+      expect(page).to_not have_content(@customer1.name)
+      expect(all('.customer')[0].text).to eq(@customer6.name)
+      expect(all('.customer')[1].text).to eq(@customer5.name)
+      expect(all('.customer')[2].text).to eq(@customer4.name)
+      expect(all('.customer')[3].text).to eq(@customer3.name)
+      expect(all('.customer')[4].text).to eq(@customer2.name)
     end
-
-
-
-
   end
-
 end
