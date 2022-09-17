c1869b5bcf3e5520f1d2d9aa076af6aefcea67bb
Thomas Musselman <tmusselman@gmail.com>
2022-09-16 18:22:54 -0600

    initial setup

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,10 @@
 class Customer < ApplicationRecord
 
   has_many :invoices
+  has_many :transactions, through: :invoices
+
+  def count_successful_transactions
+    transactions.where
+  end
 
 end 
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,9 +2,21 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices, through: :items
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
 
   def distinct_invoices
     invoices.distinct
+  end
+
+  def top_five_customers
+    customers
+      .joins(:transactions)
+      .select(customers.*)
+      .where('transactions.result = 0')
+      .count('transactions.id')
+      .group(:customer_id)
+      .order('count_transactions_id desc')
   end
 
 end
