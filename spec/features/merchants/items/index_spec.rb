46fc1b4ce5fe0e2f80635e1c3ed9eec9f9b92af1
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-09-17 15:34:21 -0600

    rework tests to include transactions and calcuable revenue. add test and #top_five_revenue to merchant model. rework tests for oredering and links. add @top_five to index page

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -8,15 +8,41 @@ RSpec.describe 'Merchant Items Index Page: ' do
     @item2 = create(:item, merchant: @merch1)
 
     @merch2 = create(:merchant)
-    @item3 = create(:item, merchant: @merch2)
-    @item4 = create(:item, merchant: @merch2)
-    @item5 = create(:item, merchant: @merch2)
-    
-    @invoice1 = create(:invoice, status: :in_progress)
-    @inv_item1 = create(:invoice_item, invoice: @invoice1, item: @item1, status: :packaged)
-    @inv_item2 = create(:invoice_item, invoice: @invoice1, item: @item2, status: :packaged)
-    @inv_item3 = create(:invoice_item, invoice: @invoice1, item: @item3)
+    @item3 = create(:item, merchant: @merch2, unit_price: 500)
+    @item4 = create(:item, merchant: @merch2, unit_price: 500)
+    @item5 = create(:item, merchant: @merch2, unit_price: 500)
+    @item6 = create(:item, merchant: @merch2, unit_price: 500)
+    @item7 = create(:item, merchant: @merch2, unit_price: 500)
+    @item8 = create(:item, merchant: @merch2, unit_price: 500)
+    @item9 = create(:item, merchant: @merch2, unit_price: 500)
 
+    @invoice1 = create(:invoice, status: :completed)
+    @invoice2 = create(:invoice, status: :completed)
+    @invoice3 = create(:invoice, status: :completed)
+    @invoice4 = create(:invoice, status: :completed)
+    @invoice5 = create(:invoice, status: :completed)
+
+    @inv_item1 = create(:invoice_item, invoice: @invoice1, item: @item3, quantity: 10, unit_price: 100, status: :packaged)
+    @inv_item2 = create(:invoice_item, invoice: @invoice2, item: @item4, quantity: 11, unit_price: 100, status: :packaged)
+    @inv_item3 = create(:invoice_item, invoice: @invoice3, item: @item5, quantity: 12, unit_price: 100, status: :packaged)
+    @inv_item4 = create(:invoice_item, invoice: @invoice4, item: @item6, quantity: 13, unit_price: 100, status: :packaged)
+    @inv_item5 = create(:invoice_item, invoice: @invoice5, item: @item7, quantity: 14, unit_price: 100, status: :packaged)
+    @inv_item6 = create(:invoice_item, invoice: @invoice1, item: @item8, quantity: 15, unit_price: 100, status: :packaged)
+    @inv_item7 = create(:invoice_item, invoice: @invoice2, item: @item9, quantity: 16, unit_price: 100, status: :packaged)
+    @inv_item8 = create(:invoice_item, invoice: @invoice3, item: @item3, quantity: 10, unit_price: 100, status: :packaged)
+    @inv_item9 = create(:invoice_item, invoice: @invoice4, item: @item4, quantity: 11, unit_price: 100, status: :packaged)
+    @inv_item10 = create(:invoice_item, invoice: @invoice5, item: @item5, quantity: 12, unit_price: 100, status: :packaged)
+
+    @tranaction1 = create(:transaction, invoice_id: @invoice1.id, result: :success)
+    @tranaction2 = create(:transaction, invoice_id: @invoice2.id, result: :failed)
+    @tranaction3 = create(:transaction, invoice_id: @invoice3.id, result: :success)
+    @tranaction4 = create(:transaction, invoice_id: @invoice4.id, result: :success)
+    @tranaction5 = create(:transaction, invoice_id: @invoice5.id, result: :success)
+    @tranaction6 = create(:transaction, invoice_id: @invoice1.id, result: :success)
+    @tranaction7 = create(:transaction, invoice_id: @invoice2.id, result: :failed)
+    @tranaction8 = create(:transaction, invoice_id: @invoice3.id, result: :failed)
+    @tranaction9 = create(:transaction, invoice_id: @invoice4.id, result: :failed)
+    @tranaction10 = create(:transaction, invoice_id: @invoice5.id, result: :failed)
   end
 
   describe 'As a Merchant' do
@@ -153,18 +179,33 @@ RSpec.describe 'Merchant Items Index Page: ' do
         end
 
         it 'has links to item show page' do
-          visit merchant_items_path(@merch1.id)
+          visit merchant_items_path(@merch2.id)
 
           within("#top_items") do
-            expect(page).to have_link("Top Items")
-            expect(page).to have_link("Top Items")
-            expect(page).to have_link("Top Items")
-            expect(page).to have_link("Top Items")
-            expect(page).to have_link("Top Items")
+            expect(page).to have_link(@item3.name)
+            expect(page).to_not have_link(@item4.name)
+            expect(page).to have_link(@item5.name)
+            expect(page).to have_link(@item6.name)
+            expect(page).to have_link(@item7.name)
+            expect(page).to have_link(@item8.name)
+            expect(page).to_not have_link(@item9.name)
           end
         end
+
+        it 'links are ordered by top revenue' do
+
+          visit merchant_items_path(@merch2.id)
+
+          within("#top_items") do
+            expect(@item3.name).to appear_before(@item8.name)
+            expect(@item8.name).to appear_before(@item5.name)
+            expect(@item5.name).to appear_before(@item7.name)
+            expect(@item7.name).to appear_before(@item6.name)
+          end
+        end 
+
         it 'when link is clicked, taken to show page' do
-          visit merchant_items_path(@merch1.id)
+          visit merchant_items_path(@merch2.id)
 
           within("#top_items") do
             click_link "link"
@@ -174,7 +215,7 @@ RSpec.describe 'Merchant Items Index Page: ' do
         end
 
         it 'total revenue for item is displayed' do
-          visit merchant_items_path(@merch1.id)
+          visit merchant_items_path(@merch2.id)
 
           within("#top_items") do
             expect(page).to have_link("revnue")
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,38 +2,83 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   describe 'relationships' do
-
     it {should have_many(:items)}
+  end
 
-    describe 'instance variables' do
+  before(:each) do
+    @merchant_1 = create(:merchant)
+    @item_1 = create(:item, merchant: @merchant_1)
+    @item_2 = create(:item, merchant: @merchant_1)
 
-      before(:each) do
-        @merchant_1 = create(:merchant)
-        @item_1 = create(:item, merchant: @merchant_1)
-        @item_2 = create(:item, merchant: @merchant_1)
+    @merchant_2 = create(:merchant)
+    @item_3 = create(:item, merchant: @merchant_2)
+    @item_4 = create(:item, merchant: @merchant_2)
 
-        @merchant_2 = create(:merchant)
-        @item_3 = create(:item, merchant: @merchant_2)
-        @item_4 = create(:item, merchant: @merchant_2)
+    @invoice_1 = create(:invoice)
+    @invoice_2 = create(:invoice)
+    @invoice_3 = create(:invoice)
+    @invoice_4 = create(:invoice)
 
-        @invoice_1 = create(:invoice)
-        @invoice_2 = create(:invoice)
-        @invoice_3 = create(:invoice)
-        @invoice_4 = create(:invoice)
+    @invoice_1.items << [@item_1, @item_2, @item_3]
+    @invoice_2.items << [@item_3, @item_4]
+    @invoice_3.items << [@item_1, @item_4]
+    @invoice_4.items << @item_2
+  end
 
-        @invoice_1.items << [@item_1, @item_2, @item_3]
-        @invoice_2.items << [@item_3, @item_4]
-        @invoice_3.items << [@item_1, @item_4]
-        @invoice_4.items << @item_2
-      end
+  describe '.invoices' do
+    it 'returns a list of invoices which contain an item from the merchant' do
+      expect(@merchant_1.distinct_invoices).to match_array([@invoice_1, @invoice_3, @invoice_4])
+      expect(@merchant_2.distinct_invoices).to match_array([@invoice_1, @invoice_2, @invoice_3])
+    end
+  end
 
-      describe '.invoices' do
-        it 'returns a list of invoices which contain an item from the merchant' do
-          expect(@merchant_1.distinct_invoices).to match_array([@invoice_1, @invoice_3, @invoice_4])
-          expect(@merchant_2.distinct_invoices).to match_array([@invoice_1, @invoice_2, @invoice_3])
-        end
-      end
+  describe 'test instance variables for .top_five_revenue' do
+    before(:each) do
+      @merch1 = create(:merchant)
+      @item1 = create(:item, merchant: @merch1, unit_price: 5700)
+      @item2 = create(:item, merchant: @merch1)
+  
+      @merch2 = create(:merchant)
+      @item3 = create(:item, merchant: @merch2, unit_price: 500)
+      @item4 = create(:item, merchant: @merch2, unit_price: 500)
+      @item5 = create(:item, merchant: @merch2, unit_price: 500)
+      @item6 = create(:item, merchant: @merch2, unit_price: 500)
+      @item7 = create(:item, merchant: @merch2, unit_price: 500)
+      @item8 = create(:item, merchant: @merch2, unit_price: 500)
+      @item9 = create(:item, merchant: @merch2, unit_price: 500)
+  
+      @invoice1 = create(:invoice, status: :completed)
+      @invoice2 = create(:invoice, status: :completed)
+      @invoice3 = create(:invoice, status: :completed)
+      @invoice4 = create(:invoice, status: :completed)
+      @invoice5 = create(:invoice, status: :completed)
+  
+      @inv_item1 = create(:invoice_item, invoice: @invoice1, item: @item3, quantity: 10, unit_price: 100, status: :packaged)
+      @inv_item2 = create(:invoice_item, invoice: @invoice2, item: @item4, quantity: 11, unit_price: 100, status: :packaged)
+      @inv_item3 = create(:invoice_item, invoice: @invoice3, item: @item5, quantity: 12, unit_price: 100, status: :packaged)
+      @inv_item4 = create(:invoice_item, invoice: @invoice4, item: @item6, quantity: 13, unit_price: 100, status: :packaged)
+      @inv_item5 = create(:invoice_item, invoice: @invoice5, item: @item7, quantity: 14, unit_price: 100, status: :packaged)
+      @inv_item6 = create(:invoice_item, invoice: @invoice1, item: @item8, quantity: 15, unit_price: 100, status: :packaged)
+      @inv_item7 = create(:invoice_item, invoice: @invoice2, item: @item9, quantity: 16, unit_price: 100, status: :packaged)
+      @inv_item8 = create(:invoice_item, invoice: @invoice3, item: @item3, quantity: 10, unit_price: 100, status: :packaged)
+      @inv_item9 = create(:invoice_item, invoice: @invoice4, item: @item4, quantity: 11, unit_price: 100, status: :packaged)
+      @inv_item10 = create(:invoice_item, invoice: @invoice5, item: @item5, quantity: 12, unit_price: 100, status: :packaged)
+  
+      @transaction1 = create(:transaction, invoice_id: @invoice1.id, result: :success)
+      @transaction2 = create(:transaction, invoice_id: @invoice2.id, result: :failed)
+      @transaction3 = create(:transaction, invoice_id: @invoice3.id, result: :success)
+      @transaction4 = create(:transaction, invoice_id: @invoice4.id, result: :success)
+      @transaction5 = create(:transaction, invoice_id: @invoice5.id, result: :success)
+      @transaction6 = create(:transaction, invoice_id: @invoice1.id, result: :success)
+      @transaction7 = create(:transaction, invoice_id: @invoice2.id, result: :failed)
+      @transaction8 = create(:transaction, invoice_id: @invoice3.id, result: :failed)
+      @transaction9 = create(:transaction, invoice_id: @invoice4.id, result: :failed)
+      @transaction10 = create(:transaction, invoice_id: @invoice5.id, result: :failed)
     end
 
+    it '.top_five_revenue' do
+      expect(@merch2.top_five_revenue).to match_array([@item5, @item7, @item3, @item6, @item8])
+      expect(@merch2.top_five_revenue).to eq([@item3, @item8, @item5, @item7, @item6])
+    end
   end
 end
