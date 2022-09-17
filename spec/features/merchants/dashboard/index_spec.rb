df0a2b9f45fc66f541aa2dc5d72a8a5467baedc2
Thomas Musselman <tmusselman@gmail.com>
2022-09-17 17:13:40 -0600

    test: merchant top_five_customers and dashboard index dispalys top five

--- spec/features/merchants/dashboard/index_spec.rb
+++ spec/features/merchants/dashboard/index_spec.rb
@@ -25,4 +25,41 @@ RSpec.describe 'Merchant Dashboard Index' do
     
     expect(current_path).to eq(merchant_invoices_path(@merchant.id))
   end
+
+  it 'lists the merchants top 5 customers' do
+    item = create(:item, merchant: @merchant)
+    customer_1 = create(:customer)
+    customer_2 = create(:customer)
+    customer_3 = create(:customer)
+    customer_4 = create(:customer)
+    customer_5 = create(:customer)
+
+    invoice_1 = create(:invoice, customer: customer_1)
+    invoice_2 = create(:invoice, customer: customer_2)
+    invoice_3 = create(:invoice, customer: customer_3)
+    invoice_4 = create(:invoice, customer: customer_4)
+    invoice_5 = create(:invoice, customer: customer_5)
+
+    invoice_1.items << item
+    invoice_2.items << item
+    invoice_3.items << item
+    invoice_4.items << item
+    invoice_5.items << item
+
+    transaction_1 = create(:transaction, invoice: invoice_1, result: :success)
+    transaction_2 = create(:transaction, invoice: invoice_2, result: :success)
+    transaction_3 = create(:transaction, invoice: invoice_3, result: :success)
+    transaction_4 = create(:transaction, invoice: invoice_4, result: :success)
+    transaction_5 = create(:transaction, invoice: invoice_5, result: :success)
+
+    visit merchant_dashboard_index_path(@merchant.id)
+    
+    within("div#favorite_customers") do
+      expect(page).to have_content("#{@merchant.top_five_customers[0].first_name} #{@merchant.top_five_customers[0].last_name}")
+      expect(page).to have_content("#{@merchant.top_five_customers[1].first_name} #{@merchant.top_five_customers[1].last_name}")
+      expect(page).to have_content("#{@merchant.top_five_customers[2].first_name} #{@merchant.top_five_customers[2].last_name}")
+      expect(page).to have_content("#{@merchant.top_five_customers[3].first_name} #{@merchant.top_five_customers[3].last_name}")
+      expect(page).to have_content("#{@merchant.top_five_customers[4].first_name} #{@merchant.top_five_customers[4].last_name}")
+    end
+  end
 end
