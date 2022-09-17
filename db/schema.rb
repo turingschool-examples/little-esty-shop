26788dec6a69672287cac0f90da7bdbc9a06d8fc
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-17 16:12:12 -0700

    Create migration for item status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_163432) do
+ActiveRecord::Schema.define(version: 2022_09_17_230540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_09_16_163432) do
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
