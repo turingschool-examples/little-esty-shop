c4d673b12679da11e9e7b7fb60c685652d44e146
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-09-17 12:05:42 -0600

    add test for top 5 merchant revenue items and show page item revenue

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Items Index Page: ' do
-  before :each do
+
+  before(:each) do
     @merch1 = create(:merchant)
     @item1 = create(:item, merchant: @merch1, unit_price: 5700)
     @item2 = create(:item, merchant: @merch1)
@@ -10,6 +11,12 @@ RSpec.describe 'Merchant Items Index Page: ' do
     @item3 = create(:item, merchant: @merch2)
     @item4 = create(:item, merchant: @merch2)
     @item5 = create(:item, merchant: @merch2)
+    
+    @invoice1 = create(:invoice, status: :in_progress)
+    @inv_item1 = create(:invoice_item, invoice: @invoice1, item: @item1, status: :packaged)
+    @inv_item2 = create(:invoice_item, invoice: @invoice1, item: @item2, status: :packaged)
+    @inv_item3 = create(:invoice_item, invoice: @invoice1, item: @item3)
+
   end
 
   describe 'As a Merchant' do
@@ -133,8 +140,37 @@ RSpec.describe 'Merchant Items Index Page: ' do
 
       describe 'there is a link to create a new item' do
         it 'the link exists on the page' do
-        visit merchant_items_path(@merch1.id)
-        expect(page).to have_link("New Item")
+          visit merchant_items_path(@merch1.id)
+          expect(page).to have_link("New Item")
+        end
+      end
+
+      describe 'there are names of the top 5 most popular items ranked by revenue' do
+        it 'has a section for top 5' do
+          visit merchant_items_path(@merch1.id)
+
+          expect(page).to have_content("Top Items")
+        end
+
+        it 'has links to item show page' do
+          visit merchant_items_path(@merch1.id)
+
+          within("#top_items") do
+            expect(page).to have_link("Top Items")
+            expect(page).to have_link("Top Items")
+            expect(page).to have_link("Top Items")
+            expect(page).to have_link("Top Items")
+            expect(page).to have_link("Top Items")
+          end
+        end
+        it 'when link is clicked, taken to show page' do
+          visit merchant_items_path(@merch1.id)
+
+          within("#top_items") do
+            click_link "link"
+          end
+
+          expect(current_path).to eq(merchant_item_path(@merch1.id, "item id"))
         end
       end
     end
--- spec/features/merchants/items/show_spec.rb
+++ spec/features/merchants/items/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Item Show Page: ' do
-  before :each do
+  before(:each) do
     @merch1 = create(:merchant)
     @item1 = create(:item, merchant: @merch1, unit_price: 5700)
     @item2 = create(:item, merchant: @merch1)
@@ -10,6 +10,12 @@ RSpec.describe 'Merchant Item Show Page: ' do
     @item3 = create(:item, merchant: @merch2)
     @item4 = create(:item, merchant: @merch2)
     @item5 = create(:item, merchant: @merch2)
+    
+    @invoice1 = create(:invoice, status: :in_progress)
+    @inv_item1 = create(:invoice_item, invoice: @invoice1, item: @item1, status: :packaged)
+    @inv_item2 = create(:invoice_item, invoice: @invoice1, item: @item2, status: :packaged)
+    @inv_item3 = create(:invoice_item, invoice: @invoice1, item: @item3)
+
   end
 
   describe 'As a merchant' do
@@ -40,6 +46,13 @@ RSpec.describe 'Merchant Item Show Page: ' do
         visit merchant_item_path(@merch2.id, @item4.id)
 
         expect(page).to have_link("Update #{@item4.name}")
+      end
+
+      it 'total revenue for item is displayed' do
+        visit merchant_item_path(@merch2.id, @item4.id)
+
+        expect(page).to have_content("Total Revenue")
+        expect(page).to have_content("Iteger of revenue")
       end
     end
   end
