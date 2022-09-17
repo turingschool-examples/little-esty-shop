5fe12a27162c9893b405d07b58ac10f3c7610ade
Jake Kim <imjakekim@gmail.com>
2022-09-17 13:44:17 -0700

    testing admin invoices index

---
+++ spec/features/admin/invoices/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'admin invoice index' do
+
+    let!(:joey) { Customer.create!(first_name: "Joey", last_name: "Ondricka")}
+
+    let!(:invoice1) { joey.invoices.create!(status: :completed) }
+    let!(:invoice2) { joey.invoices.create!(status: :in_progress) }
+    let!(:invoice3) { joey.invoices.create!(status: :cancelled) }
+    let!(:invoice4) { joey.invoices.create!(status: :cancelled) }
+    # let!(:item1) { invoice1.items.create!(status: 2) }
+    # let!(:item2) { invoice4.items.create!(status: 2) }
+    
+    it 'shows all invoices' do
+        visit admin_invoices_path
+        save_and_open_page
+        expect(page).to have_content(invoice1.id)
+        expect(page).to have_content(invoice2.id)
+        expect(page).to have_content(invoice3.id)
+        expect(page).to have_content(invoice4.id)
+    end
+end
