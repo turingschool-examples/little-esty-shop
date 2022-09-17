a834cb0beb5d7fe672101161d78fb90387610eba
Carter Ball <carter.k.ball@gmail.com>
2022-09-17 15:59:07 -0400

    Tets for admin invoices index

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -2,8 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'the Admin Invoices Index' do
   before :each do
+    @invoices = create_list(:invoice, 20)
     visit admin_invoices_path
   end
+
+  it 'has a list of all invoice IDs' do
+    @invoices.each do |inv|
+      expect(page).to have_content(inv.id)
+    end
+  end
+
+  xit 'each id links to the admin invoice show page' do
+    click_link(@invoices[0].id)
+    expect(path).to eq(admin_invoice_path)
+  end
+
 
 
 end
