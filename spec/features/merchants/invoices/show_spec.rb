0f3383badb82e4cc7913a68568b660ef01f2105f
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-09-17 20:21:35 -0600

    test: create tests for merchant invoice show page.

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -2,16 +2,41 @@ require "rails_helper"
 
 
 RSpec.describe "the merchant invoices show"  do
-    it ' ' do
-    #     Merchant Invoice Show Page
+    describe 'I see all information related to that invoice' do
+        it 'can display the invoice id' do
+            merchant1 = Merchant.create!(name: "Bob")
+            customer1 = Customer.create!(first_name: "Jolene", last_name: "Jones")
+            invoice_1 = customer1.invoices.create!(status: 1, created_at: "2021-09-14 09:00:01")
 
-    # As a merchant
-    # When I visit my merchants invoice show page(/merchants/merchant_id/invoices/invoice_id)
-    # Then I see information related to that invoice including:
+            visit "/merchants/#{merchant1.id}/invoices/#{invoice_1.id}"
+            expect(page).to have_content("Invoice: #{invoice_1.id}")
+        end
 
-    # Invoice id
-    # Invoice status
-    # Invoice created_at date in the format "Monday, July 18, 2019"
-    # Customer first and last name
+        it 'can display the invoice status' do
+            merchant1 = Merchant.create!(name: "Bob")
+            customer1 = Customer.create!(first_name: "Jolene", last_name: "Jones")
+            invoice_1 = customer1.invoices.create!(status: 1, created_at: "2021-09-14 09:00:01")
+
+            visit "/merchants/#{merchant1.id}/invoices/#{invoice_1.id}"
+            expect(page).to have_content("Status: #{invoice_1.status}")
+        end
+
+        it 'can display the date format for its creation date' do
+            merchant1 = Merchant.create!(name: "Bob")
+            customer1 = Customer.create!(first_name: "Jolene", last_name: "Jones")
+            invoice_1 = customer1.invoices.create!(status: 1, created_at: "2021-09-14 09:00:01")
+
+            visit "/merchants/#{merchant1.id}/invoices/#{invoice_1.id}"
+            expect(page).to have_content("Created: Sunday, September 18, 2022")
+        end
+
+        it 'can display the customers first and last name' do
+            merchant1 = Merchant.create!(name: "Bob")
+            customer1 = Customer.create!(first_name: "Jolene", last_name: "Jones")
+            invoice_1 = customer1.invoices.create!(status: 1, created_at: "2021-09-14 09:00:01")
+
+            visit "/merchants/#{merchant1.id}/invoices/#{invoice_1.id}"
+            expect(page).to have_content("Customer: #{customer1.first_name} #{customer1.last_name}")
+        end
     end
 end
