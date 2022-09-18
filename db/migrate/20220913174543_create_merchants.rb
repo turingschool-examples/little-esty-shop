553c4d3b62e07ddf5cedd461edd840b909da8882
Riley McCullough <rileybmccullough@gmail.com>
2022-09-18 10:47:43 -0600

    Create: status column for merchants

--- db/migrate/20220913174543_create_merchants.rb
+++ db/migrate/20220913174543_create_merchants.rb
@@ -2,6 +2,7 @@ class CreateMerchants < ActiveRecord::Migration[5.2]
   def change
     create_table :merchants do |t|
       t.string :name
+      t.string :status, :null => false, :default => "Enabled"
 
       t.timestamps
     end
