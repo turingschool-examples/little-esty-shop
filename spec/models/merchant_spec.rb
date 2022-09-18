9f168e733aaf704204ad3a473922b037649d15db
Andre Pedro <andrerpedro170@gmail.com>
2022-09-18 13:43:00 -0600

    fixed the before each block in the merchants_spec test for the model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,27 +5,7 @@ RSpec.describe Merchant, type: :model do
     it {should have_many(:items)}
   end
 
-  describe 'instance variables' do
 
-    # before(:each) do
-    #   @merchant_1 = create(:merchant)
-    #   @item_1 = create(:item, merchant: @merchant_1)
-    #   @item_2 = create(:item, merchant: @merchant_1)
-    #
-    #   @merchant_2 = create(:merchant)
-    #   @item_3 = create(:item, merchant: @merchant_2)
-    #   @item_4 = create(:item, merchant: @merchant_2)
-    #
-    #   @invoice_1 = create(:invoice)
-    #   @invoice_2 = create(:invoice)
-    #   @invoice_3 = create(:invoice)
-    #   @invoice_4 = create(:invoice)
-    #
-    #   @invoice_1.items << [@item_1, @item_2, @item_3]
-    #   @invoice_2.items << [@item_3, @item_4]
-    #   @invoice_3.items << [@item_1, @item_4]
-    #   @invoice_4.items << @item_2
-    # end
 
     describe '.invoices' do
       it 'returns a list of invoices which contain an item from the merchant' do
@@ -298,5 +278,5 @@ describe 'to see if it helps' do
      expect(Merchant.top_five_merchants[3]).to eq(merchant_5)
      expect(Merchant.top_five_merchants.last).to eq(merchant_4)
    end
-   end
+
  end
