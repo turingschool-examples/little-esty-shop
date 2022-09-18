67da3491a4cb6476d5b987627f242bc9bc1e443f
Ken Lenhart <omnipaid@gmail.com>
2022-09-18 13:30:58 -0700

    test/refactor: tests for User Story #18, remove hidden-field from item/new.html.erb, item/edit.html.erb

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe 'Merchant Invoice Show Page' do
     @invoice_1 = create(:invoice)
     @invoice_2 = create(:invoice)
 
-    @invoice_items_1 = create(:invoice_items, invoice_id: @invoice_1.id, item_id: @item_1.id)
-    @invoice_items_2 = create(:invoice_items, invoice_id: @invoice_2.id, item_id: @item_2.id)
+    @invoice_items_1 = create(:invoice_items, invoice_id: @invoice_1.id, item_id: @item_1.id, status: "pending")
+    @invoice_items_2 = create(:invoice_items, invoice_id: @invoice_2.id, item_id: @item_2.id, status: "pending")
   end
 
   # As a merchant
@@ -49,7 +49,7 @@ RSpec.describe 'Merchant Invoice Show Page' do
         expect(page).to have_content(@invoice_2.customer.last_name )
         expect(page).to_not have_content(@invoice_1.id)
         expect(page).to_not have_content(@invoice_1.customer.first_name)
-        expect(page).to_not have_content(@invoice_1.customer.last_name )
+        expect(page).to_not have_content(@invoice_1.customer.last_name)
       end
     end
   end
@@ -75,7 +75,6 @@ RSpec.describe 'Merchant Invoice Show Page' do
         expect(page).to have_content(@invoice_items_1.quantity)
         expect(page).to_not have_content(@item_2.name)
         expect(page).to_not have_content(@item_2.unit_price)
-        expect(page).to_not have_content(@invoice_items_2.quantity)
       end
 
       visit merchant_invoice_path(@merchant_2, @invoice_2)
@@ -87,8 +86,52 @@ RSpec.describe 'Merchant Invoice Show Page' do
         expect(page).to have_content(@invoice_items_2.quantity)
         expect(page).to_not have_content(@item_1.name)
         expect(page).to_not have_content(@item_1.unit_price)
-        expect(page).to_not have_content(@invoice_items_1.quantity)
       end
+    end
+  end
+
+  # As a merchant
+  # When I visit my merchant invoice show page
+  # I see that each invoice item status is a select field
+  # And I see that the invoice item's current status is selected
+  # When I click this select field,
+  # Then I can select a new status for the Item,
+  # And next to the select field I see a button to "Update Item Status"
+  # When I click this button
+  # I am taken back to the merchant invoice show page
+  # And I see that my Item's status has now been updated
+
+  describe 'When I visit my merchant invoice show page' do
+    it 'I see that each invoice item status is a select field' do
+      visit  merchant_invoice_path(@merchant_1, @invoice_1)
+
+      within "#invoice-items-info" do
+        expect(page).to have_select("invoice[status]", selected: "#{@invoice_items_1.status.titleize}")
+      end
+
+      visit  merchant_invoice_path(@merchant_2, @invoice_2)
+
+      within "#invoice-items-info" do
+        expect(page).to have_select("invoice[status]", selected: "#{@invoice_items_2.status.titleize}")
+      end
+    end
+
+    it 'When I click this select field, select a new status, click button I am taken back to merchant invoice show page' do
+      visit  merchant_invoice_path(@merchant_1, @invoice_1)
+
+      select "Shipped", from: "invoice[status]"
+
+      click_button "Update Item Status"
+
+      expect(current_path).to eq( merchant_invoice_path(@merchant_1, @invoice_1))
+
+      visit  merchant_invoice_path(@merchant_2, @invoice_2)
+
+      select "Shipped", from: "invoice[status]"
+
+      click_button "Update Item Status"
+
+      expect(current_path).to eq( merchant_invoice_path(@merchant_2, @invoice_2))
     end
   end
 end
