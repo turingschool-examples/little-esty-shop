9813cefbdeef690941580176bfee2dccdbf7ba9f
Thomas Musselman <tmusselman@gmail.com>
2022-09-18 09:00:25 -0600

    remove variable from top_five_revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,12 +8,12 @@ class Merchant < ApplicationRecord
   end
 
   def top_five_revenue
-    var = items.joins(invoice_items: [{invoice: :transactions}])
-         .select('items.*, invoice_items.item_id, sum(invoice_items.unit_price * invoice_items.quantity) AS item_revenue')
-         .group('invoice_items.item_id, items.id')
-         .where('transactions.result =?', 1)
-         .order(item_revenue: :DESC)
-         .limit(5)
-
+    items
+      .joins(invoice_items: [{invoice: :transactions}])
+      .select('items.*, invoice_items.item_id, sum(invoice_items.unit_price * invoice_items.quantity) AS item_revenue')
+      .group('invoice_items.item_id, items.id')
+      .where('transactions.result =?', 1)
+      .order(item_revenue: :DESC)
+      .limit(5)
   end
 end
