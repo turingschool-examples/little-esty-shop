c573a7ba919e77525a8194450301cb0ecc485ddc
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-18 00:52:26 -0600

    update factory bot file name and remove unused files

--- spec/factories/item.rb
+++
@@ -1,8 +0,0 @@
-# FactoryBot.define do
-#   factory :item, class: Item do
-#     name        { SecureRandom.uuid }
-#     description {Faker::Marketing.buzzwords}
-#     unit_price  {Faker::Number.within(range: 500..2000)}
-#     association :merchant, factory: :merchant
-#   end
-# end
