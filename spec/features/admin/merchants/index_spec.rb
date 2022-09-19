e225b872c0aecf7539656fbf5bab57014b8b2670
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-18 22:40:08 -0600

    set up test objects and first veiw test for top 5 merchants

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -111,47 +111,75 @@ RSpec.describe "Admin Merchants" do
         end
       end
 
-    # Admin Merchants: Top 5 Merchants by Revenue
+      describe "Top 5 Merchants by Revenue" do
+        before :each do
+          @merchant_1 = create(:merchant, name: "Merchant_1")
+          @merchant_2 = create(:merchant, name: "Merchant_2")
+          @merchant_3 = create(:merchant, name: "Merchant_3")
+          @merchant_4 = create(:merchant, name: "Merchant_4")
+          @merchant_5 = create(:merchant, name: "Merchant_5")
+          @merchant_6 = create(:merchant, name: "Merchant_6")
 
-    # As an admin,
-    # When I visit the admin merchants index
-    # Then I see the names of the top 5 merchants by total revenue generated
-    # And I see that each merchant name links to the admin merchant show page for that merchant
-    # And I see the total revenue generated next to each merchant name
+          @items_1 = create_list(:item, 10, merchant: @merchant_1)
+          @items_2 = create_list(:item, 10, merchant: @merchant_2)
+          @items_3 = create_list(:item, 10, merchant: @merchant_3)
+          @items_4 = create_list(:item, 10, merchant: @merchant_4)
+          @items_5 = create_list(:item, 10, merchant: @merchant_5)
+          @items_6 = create_list(:item, 10, merchant: @merchant_6)
 
-    # Notes on Revenue Calculation:
+          @invoice_2 = create(:invoice)
+          @invoice_1 = create(:invoice)
+          @invoice_3 = create(:invoice)
+          @invoice_4 = create(:invoice)
 
-    #Completed this Step - 
-      # Only invoices with at least one successful transaction should count towards revenue -- #if successfull == counts *(.any?)
+          @items_1.each { |item| create(:invoice_items, invoice: @invoice_1, item: item, unit_price: 500, quantity: 20) } #10000
+          @items_2.each { |item| create(:invoice_items, invoice: @invoice_2, item: item, unit_price: 100, quantity: 5) } #500
+          @items_3.each { |item| create(:invoice_items, invoice: @invoice_2, item: item, unit_price: 500, quantity: 5) } #2500
+          @items_4.each { |item| create(:invoice_items, invoice: @invoice_3, item: item, unit_price: 200, quantity: 10) } # 2000
+          @items_5.each { |item| create(:invoice_items, invoice: @invoice_3, item: item, unit_price: 500, quantity: 15) } #7500
+          @items_6.each { |item| create(:invoice_items, invoice: @invoice_4, item: item, unit_price: 200, quantity: 5) } #1000
 
-    #Completed this Step -
-      # Revenue for an invoice should be calculated as the sum of the revenue of all invoice items -- invoice items.sum
+          @transaction_1 = create_list(:transaction, 2, invoice: @invoice_1, result: :failed)
+          @transaction_2 = create_list(:transaction, 2, invoice: @invoice_1, result: :failed)
+          @transaction_3 = create_list(:transaction, 2, invoice: @invoice_2, result: :success)
+          @transaction_4 = create_list(:transaction, 2, invoice: @invoice_2, result: :success)
+          @transaction_5 = create_list(:transaction, 2, invoice: @invoice_3, result: :failed)
+          @transaction_6 = create_list(:transaction, 2, invoice: @invoice_3, result: :success)
+          @transaction_7 = create_list(:transaction, 2, invoice: @invoice_4, result: :success)
+          @transaction_8 = create_list(:transaction, 2, invoice: @invoice_4, result: :failed)
+        end
+        
+        it 'I see the names of the top 5 merchants by total revenue generated' do
+          visit admin_merchants_path
+          #orderly based off of top revenue
+          expect(@merchant_5.name).to appear_before(@merchant_2.name)
+          expect(@merchant_5.name).to appear_before(@merchant_3.name)
+          expect(@merchant_5.name).to appear_before(@merchant_4.name)
+          expect(@merchant_5.name).to appear_before(@merchant_6.name)
 
-    #
-      # Revenue for an invoice item should be calculated as the invoice item unit price multiplied by the quantity (do not use the item unit price) -- invoice item unit price * quantity 
+          expect(@merchant_3.name).to appear_before(@merchant_4.name)
+          expect(@merchant_3.name).to appear_before(@merchant_6.name)
+          expect(@merchant_3.name).to appear_before(@merchant_2.name)
 
-        # @merchant_1 = create(:merchant, active_status: :enabled)
-        # @merchant_2 = create(:merchant, active_status: :enabled)
-        # @merchant_3 = create(:merchant)
-        # @merchant_4 = create(:merchant, active_status: :enabled)
-        # @merchant_5 = create(:merchant)
+          expect(@merchant_4.name).to appear_before(@merchant_6.name)
+          expect(@merchant_4.name).to appear_before(@merchant_2.name)
 
-      it 'shows the names of the top 5 merchants by total revenue generated and shows the revenue' do
-        visit admin_merchants_path
-        #orderly based off of top revenue
-        expect(page).to have_content(@merchant_1, @merchant_2, @merchant_3, @merchant_4, @merchant_5) #top5merchants - might have to test individually - and need to organize by top revenue
-      end
+          expect(@merchant_6.name).to appear_before(@merchant_2.name)
 
-      it 'I see that each merchant name links to the admin merchant show page for that merchant' do
-        visit admin_merchants_path
+          expect(page).to_not have_content(merchant_5.name)
+        end
 
-        click_button "#{@merchant_1.name}"
-        expect(current_path).to eq(admin_merchant_path(@merchant_1))
-      end
-      
-      it 'shows the revenue generated by each merchant' do
-        visit admin_merchants_path
-        #use within blocks to find on page
+        it 'I see that each merchant name links to the admin merchant show page for that merchant' do
+          visit admin_merchants_path
+
+          click_button "#{@merchant_1.name}"
+          expect(current_path).to eq(admin_merchant_path(@merchant_1))
+        end
+        
+        it 'I see the total revenue generated next to each merchant name' do
+          visit admin_merchants_path
+          #use within blocks to find on page
+        end
       end
     end
   end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -7,71 +7,63 @@ RSpec.describe Merchant, type: :model do
 
   describe 'Class Methods' do
     before :each do
-      
-
-      @merchant_11 = create(:merchant, name: "merch_11", active_status: :enabled)
-      @merchant_12 = create(:merchant, name: "merch_12")
-      @merchant_13 = create(:merchant, name: "merch_13", active_status: :enabled)
-      @merchant_14 = create(:merchant, name: "merch_14", active_status: :enabled)
-      @merchant_15 = create(:merchant, name: "merch_15", active_status: :enabled)
-      @merchant_16 = create(:merchant, name: "merch_16")
+      @merchant_1 = create(:merchant, name: "merch_11", active_status: :enabled)
+      @merchant_2 = create(:merchant, name: "merch_12")
+      @merchant_3 = create(:merchant, name: "merch_13", active_status: :enabled)
+      @merchant_4 = create(:merchant, name: "merch_14", active_status: :enabled)
+      @merchant_5 = create(:merchant, name: "merch_15", active_status: :enabled)
+      @merchant_6 = create(:merchant, name: "merch_16")
 
       #merchant_11 items
-      @items_11 = create_list(:item, 10, merchant: @merchant_11)
-      @items_12 = create_list(:item, 10, merchant: @merchant_11)
+      @items_1 = create_list(:item, 10, merchant: @merchant_1)
+      @items_2 = create_list(:item, 10, merchant: @merchant_1)
       #merchant_12 items
-      @items_13 = create_list(:item, 10, merchant: @merchant_12)
-      @items_14 = create_list(:item, 10, merchant: @merchant_12)
+      @items_3 = create_list(:item, 10, merchant: @merchant_2)
+      @items_4 = create_list(:item, 10, merchant: @merchant_2)
       #merchant_13 items
-      @items_15 = create_list(:item, 10, merchant: @merchant_13)
-      @items_16 = create_list(:item, 10, merchant: @merchant_13)
+      @items_5 = create_list(:item, 10, merchant: @merchant_3)
+      @items_6 = create_list(:item, 10, merchant: @merchant_3)
       #merchant_14 items
-      @items_17 = create_list(:item, 10, merchant: @merchant_14)
-      @items_18 = create_list(:item, 10, merchant: @merchant_14)
+      @items_7 = create_list(:item, 10, merchant: @merchant_4)
+      @items_8 = create_list(:item, 10, merchant: @merchant_4)
       #merchant_15 items
-      @items_19 = create_list(:item, 10, merchant: @merchant_15)
-      @items_20 = create_list(:item, 10, merchant: @merchant_15)
+      @items_9 = create_list(:item, 10, merchant: @merchant_5)
+      @items_10 = create_list(:item, 10, merchant: @merchant_5)
       #merchant_16 items
-      @items_21 = create_list(:item, 10, merchant: @merchant_16)
-      @items_22 = create_list(:item, 10, merchant: @merchant_16)
+      @items_11 = create_list(:item, 10, merchant: @merchant_6)
+      @items_12 = create_list(:item, 10, merchant: @merchant_6)
 
-      @invoice_11 = create(:invoice)
-      @invoice_12 = create(:invoice)
-      @invoice_13 = create(:invoice)
-      @invoice_14 = create(:invoice)
-      @invoice_15 = create(:invoice)
-      @invoice_16 = create(:invoice)
-      @invoice_17 = create(:invoice)
-      @invoice_18 = create(:invoice)
+      @invoice_2 = create(:invoice)
+      @invoice_1 = create(:invoice)
+      @invoice_3 = create(:invoice)
+      @invoice_4 = create(:invoice)
+      @invoice_5 = create(:invoice)
+      @invoice_6 = create(:invoice)
+      @invoice_7 = create(:invoice)
+      @invoice_8 = create(:invoice)
 
-      @items_11.each { |item| create(:invoice_items, invoice: @invoice_11, item: item, unit_price: 500, quantity: 10) } #50000
-      @items_12.each { |item| create(:invoice_items, invoice: @invoice_12, item: item, unit_price: 100, quantity: 5) } #5000
-      
-      @items_13.each { |item| create(:invoice_items, invoice: @invoice_13, item: item, unit_price: 500, quantity: 5) } #25000
-      @items_14.each { |item| create(:invoice_items, invoice: @invoice_14, item: item, unit_price: 200, quantity: 10) } # 20000
-      
-      @items_15.each { |item| create(:invoice_items, invoice: @invoice_15, item: item, unit_price: 500, quantity: 10) } #50000
-      @items_16.each { |item| create(:invoice_items, invoice: @invoice_16, item: item, unit_price: 200, quantity: 5) } #10000
-      
-      @items_17.each { |item| create(:invoice_items, invoice: @invoice_17, item: item, unit_price: 100, quantity: 10) } #10000
-      @items_18.each { |item| create(:invoice_items, invoice: @invoice_18, item: item, unit_price: 200, quantity: 5) } #10000
-       
-      @items_19.each { |item| create(:invoice_items, invoice: @invoice_13, item: item, unit_price: 500, quantity: 15) } #50000
-      @items_20.each { |item| create(:invoice_items, invoice: @invoice_12, item: item, unit_price: 100, quantity: 5) } #5000
-      
-      @items_21.each { |item| create(:invoice_items, invoice: @invoice_15, item: item, unit_price: 500, quantity: 20) } #50000
-      @items_22.each { |item| create(:invoice_items, invoice: @invoice_16, item: item, unit_price: 200, quantity: 10) } #10000
+      @items_1.each { |item| create(:invoice_items, invoice: @invoice_1, item: item, unit_price: 500, quantity: 10) } #50000
+      @items_2.each { |item| create(:invoice_items, invoice: @invoice_2, item: item, unit_price: 100, quantity: 5) } #5000
+      @items_3.each { |item| create(:invoice_items, invoice: @invoice_3, item: item, unit_price: 500, quantity: 5) } #25000
+      @items_4.each { |item| create(:invoice_items, invoice: @invoice_4, item: item, unit_price: 200, quantity: 10) } # 20000
+      @items_5.each { |item| create(:invoice_items, invoice: @invoice_5, item: item, unit_price: 500, quantity: 10) } #50000
+      @items_6.each { |item| create(:invoice_items, invoice: @invoice_6, item: item, unit_price: 200, quantity: 5) } #10000
+      @items_7.each { |item| create(:invoice_items, invoice: @invoice_7, item: item, unit_price: 100, quantity: 10) } #10000
+      @items_8.each { |item| create(:invoice_items, invoice: @invoice_8, item: item, unit_price: 200, quantity: 5) } #10000
+      @items_9.each { |item| create(:invoice_items, invoice: @invoice_3, item: item, unit_price: 500, quantity: 15) } #50000
+      @items_10.each { |item| create(:invoice_items, invoice: @invoice_2, item: item, unit_price: 100, quantity: 5) } #5000
+      @items_11.each { |item| create(:invoice_items, invoice: @invoice_5, item: item, unit_price: 500, quantity: 20) } #50000
+      @items_12.each { |item| create(:invoice_items, invoice: @invoice_6, item: item, unit_price: 200, quantity: 10) } #10000
 
-      @transaction_11 = create_list(:transaction, 5, invoice: @invoice_11, result: :failed)
-      @transaction_12 = create_list(:transaction, 5, invoice: @invoice_12, result: :failed)
-      @transaction_13 = create_list(:transaction, 5, invoice: @invoice_13, result: :success)
-      @transaction_14 = create_list(:transaction, 5, invoice: @invoice_14, result: :success)
-      @transaction_15 = create_list(:transaction, 5, invoice: @invoice_15, result: :success)
-      @transaction_16 = create_list(:transaction, 5, invoice: @invoice_16, result: :success)
-      @transaction_16 = create_list(:transaction, 5, invoice: @invoice_17, result: :success)
-      @transaction_16 = create_list(:transaction, 5, invoice: @invoice_18, result: :success)
+      @transaction_1 = create_list(:transaction, 5, invoice: @invoice_1, result: :failed)
+      @transaction_2 = create_list(:transaction, 5, invoice: @invoice_2, result: :failed)
+      @transaction_3 = create_list(:transaction, 5, invoice: @invoice_3, result: :success)
+      @transaction_4 = create_list(:transaction, 5, invoice: @invoice_4, result: :success)
+      @transaction_5 = create_list(:transaction, 5, invoice: @invoice_5, result: :success)
+      @transaction_6 = create_list(:transaction, 5, invoice: @invoice_6, result: :success)
+      @transaction_7 = create_list(:transaction, 5, invoice: @invoice_7, result: :success)
+      @transaction_8 = create_list(:transaction, 5, invoice: @invoice_8, result: :success)
     end
-
     it "#active" do
       expect(Merchant.active).to eq([@merchant_11, @merchant_13, @merchant_14, @merchant_15])      
     end
