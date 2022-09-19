90e15bc22ac6b2406dca4ec02fe9d3727caa80d1
Sid Mann <sjmann2@outlook.com>
2022-09-19 13:43:38 -0500

    test/feat top_5_items merchant index progress

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -12,6 +12,7 @@ class Merchant < ApplicationRecord
   def ready_to_ship
     items.select("items.*, invoice_items.status as not_shipped, invoices.created_at as created_at").joins( invoices: :invoice_items).where.not("invoice_items.status = ?", 2)
   end
+
   def self.top_5_revenue
    select('merchants.name, sum(invoice_items.quantity * invoice_items.unit_price) as revenue').joins(:invoice_items).group(:id).order('revenue desc').limit(5)
   end
@@ -19,4 +20,27 @@ class Merchant < ApplicationRecord
   def favorite_customers
     Customer.select("customers.*, count(transactions) as transaction_count").joins(invoices: :transactions).where(transactions: {result: 1}).group(:id).order(transaction_count: :desc).limit(5)
   end
+
+  def top_5_items
+    items
+    .joins(:invoice_items, invoices: :transactions)
+    .where(transactions: {result: 1})
+    .select('items.id, sum(invoice_items.quantity * invoice_items.unit_price) as revenue')
+    .group('items.id')
+    .order('revenue desc')
+    .limit(5)
+  end
 end
+# Merchant Items Index: 5 most popular items
+
+# As a merchant
+# When I visit my items index page
+# Then I see the names of the top 5 most popular items ranked by total revenue generated
+# And I see that each item name links to my merchant item show page for that item
+# And I see the total revenue generated next to each item name
+
+# Notes on Revenue Calculation:
+
+# Only invoices with at least one successful transaction should count towards revenue
+# Revenue for an invoice should be calculated as the sum of the revenue of all invoice items
+# Revenue for an invoice item should be calculated as the invoice item unit price multiplied by the quantity (do not use the item unit price)
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe(Merchant, type: :model) do
     #http://www.chrisrolle.com/en/blog/boolean-attribute-validation
   end
 
-  describe("model method") do
+  describe("instance methods") do
     it("ready to ship ") do
       merchant1 = Merchant.create!(      name: "Bob")
       customer1 = Customer.create!(      first_name: "cx first name",       last_name: "cx last name")
@@ -125,5 +125,73 @@ RSpec.describe(Merchant, type: :model) do
 
       expect(Merchant.top_5_revenue).to eq []
     end
+
+    describe 'instance methods' do
+      describe 'top_5_items' do
+        let!(:merchant_1) {create(:random_merchant)}
+        let!(:merchant_2) {create(:random_merchant)}
+  
+        let!(:item_1) {create(:random_item, merchant_id: merchant_1.id)}
+        let!(:item_2) {create(:random_item, merchant_id: merchant_1.id)}
+        let!(:item_3) {create(:random_item, merchant_id: merchant_1.id)}
+        let!(:item_4) {create(:random_item, merchant_id: merchant_1.id)}
+        let!(:item_5) {create(:random_item, merchant_id: merchant_1.id)}
+        let!(:item_6) {create(:random_item, merchant_id: merchant_1.id)}
+        let!(:item_7) {create(:random_item, merchant_id: merchant_1.id)}
+        let!(:item_8) {create(:random_item, merchant_id: merchant_1.id)}
+        let!(:item_9) {create(:random_item, merchant_id: merchant_1.id)}
+        let!(:item_10) {create(:random_item, merchant_id: merchant_2.id)}
+  
+        let!(:customer_1) {create(:random_customer)}
+        let!(:customer_2) {create(:random_customer)}
+        let!(:customer_3) {create(:random_customer)}
+        let!(:customer_4) {create(:random_customer)}
+      
+        let!(:invoice_1) {Invoice.create!(customer_id: customer_1.id, status: 'completed')}
+        let!(:invoice_2) {Invoice.create!(customer_id: customer_2.id, status: 'completed')}
+        let!(:invoice_3) {Invoice.create!(customer_id: customer_3.id, status: 'cancelled')}
+        let!(:invoice_4) {Invoice.create!(customer_id: customer_4.id, status: 'completed')}
+        let!(:invoice_5) {Invoice.create!(customer_id: customer_4.id, status: 'completed')}
+  
+        let!(:transaction_1) {Transaction.create!(invoice_id: invoice_1.id, credit_card_number: 4654405418249632, credit_card_expiration_date: '', result: 'failed')}
+        let!(:transaction_2) {Transaction.create!(invoice_id: invoice_1.id, credit_card_number: 4654405418249632, credit_card_expiration_date: '', result: 'failed')}
+        let!(:transaction_3) {Transaction.create!(invoice_id: invoice_2.id, credit_card_number: 4654405418249632, credit_card_expiration_date: '', result: 'success')}
+        let!(:transaction_4) {Transaction.create!(invoice_id: invoice_2.id, credit_card_number: 4654405418249632, credit_card_expiration_date: '', result: 'failed')}
+        let!(:transaction_5) {Transaction.create!(invoice_id: invoice_3.id, credit_card_number: 4654405418249632, credit_card_expiration_date: '', result: 'success')}
+        let!(:transaction_6) {Transaction.create!(invoice_id: invoice_3.id, credit_card_number: 4654405418249632, credit_card_expiration_date: '', result: 'success')}
+        let!(:transaction_7) {Transaction.create!(invoice_id: invoice_4.id, credit_card_number: 4654405418249632, credit_card_expiration_date: '', result: 'success')}
+        let!(:transaction_8) {Transaction.create!(invoice_id: invoice_5.id, credit_card_number: 4654405418249632, credit_card_expiration_date: '', result: 'success')}
+  
+        let!(:invoice_item_1) { InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 3, unit_price: 2000, status: 'shipped') } #6000
+        let!(:invoice_item_2) { InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_1.id, quantity: 3, unit_price: 2000, status: 'packaged') }#6000
+        let!(:invoice_item_3) { InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_2.id, quantity: 3, unit_price: 300, status: 'shipped') }#900
+        let!(:invoice_item_4) { InvoiceItem.create!(item_id: item_4.id, invoice_id: invoice_2.id, quantity: 3, unit_price: 400, status: 'pending') }#1200
+        let!(:invoice_item_5) { InvoiceItem.create!(item_id: item_5.id, invoice_id: invoice_4.id, quantity: 3, unit_price: 500, status: 'pending') }#1500
+        let!(:invoice_item_6) { InvoiceItem.create!(item_id: item_6.id, invoice_id: invoice_3.id, quantity: 3, unit_price: 600, status: 'pending') }#1800
+        let!(:invoice_item_7) { InvoiceItem.create!(item_id: item_7.id, invoice_id: invoice_3.id, quantity: 3, unit_price: 700, status: 'pending') }#2100
+        let!(:invoice_item_8) { InvoiceItem.create!(item_id: item_8.id, invoice_id: invoice_4.id, quantity: 3, unit_price: 800, status: 'pending') }#2400
+        let!(:invoice_item_9) { InvoiceItem.create!(item_id: item_9.id, invoice_id: invoice_5.id, quantity: 3, unit_price: 900, status: 'pending') }#2700
+        let!(:invoice_item_10) { InvoiceItem.create!(item_id: item_10.id, invoice_id: invoice_5.id, quantity: 3, unit_price: 1000, status: 'pending') }#3000
+
+        it 'returns the 5 most popular items for a merchant by total revenue' do
+           expect(merchant_1.top_5_items).to eq([item_9.id, item_8.id, item_7.id, item_6.id, item_5.id])
+          #  item_7, item_6, item_9, item_8, item_5
+        end
+      end
+    end
   end
 end
+
+# Merchant Items Index: 5 most popular items
+
+# As a merchant
+# When I visit my items index page
+# Then I see the names of the top 5 most popular items ranked by total revenue generated
+# And I see that each item name links to my merchant item show page for that item
+# And I see the total revenue generated next to each item name
+
+# Notes on Revenue Calculation:
+
+# Only invoices with at least one successful transaction should count towards revenue
+# Revenue for an invoice should be calculated as the sum of the revenue of all invoice items
+# Revenue for an invoice item should be calculated as the invoice item unit price multiplied by the quantity (do not use the item unit price)
