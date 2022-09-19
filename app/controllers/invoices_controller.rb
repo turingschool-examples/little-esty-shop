c21ec8132f21667514083906f611037fbb901318
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-18 21:04:12 -0700

    Add merchant invoice index functionality

---
+++ app/controllers/invoices_controller.rb
@@ -0,0 +1,7 @@
+class InvoicesController < ApplicationController
+
+    def index
+        @invoices = Invoice.all
+        @merchant = Merchant.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   get "/merchant/:id/items/:id", to: 'items#show'
   get "/merchant/:id/items/:id/edit", to: 'items#edit'
   patch "/merchant/:id/items/:id", to: 'items#update'
+
+  get "/merchants/:id/invoices", to: 'invoices#index'
   
   # namespace :admin do
   #   resources :merchants
