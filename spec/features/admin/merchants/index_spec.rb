422ed823e3d3c5f769c932083ec7eab15bfc1df9
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-19 00:32:39 -0600

    set up top 5 merchants veiw

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -120,38 +120,38 @@ RSpec.describe "Admin Merchants" do
           @merchant_5 = create(:merchant, name: "Merchant_5")
           @merchant_6 = create(:merchant, name: "Merchant_6")
 
-          @items_1 = create_list(:item, 10, merchant: @merchant_1)
-          @items_2 = create_list(:item, 10, merchant: @merchant_2)
-          @items_3 = create_list(:item, 10, merchant: @merchant_3)
-          @items_4 = create_list(:item, 10, merchant: @merchant_4)
-          @items_5 = create_list(:item, 10, merchant: @merchant_5)
-          @items_6 = create_list(:item, 10, merchant: @merchant_6)
+          @item_1 = create(:item, merchant: @merchant_1)
+          @item_2 = create(:item, merchant: @merchant_2)
+          @item_3 = create(:item, merchant: @merchant_3)
+          @item_4 = create(:item, merchant: @merchant_4)
+          @item_5 = create(:item, merchant: @merchant_5)
+          @item_6 = create(:item, merchant: @merchant_6)
 
           @invoice_2 = create(:invoice)
           @invoice_1 = create(:invoice)
           @invoice_3 = create(:invoice)
           @invoice_4 = create(:invoice)
 
-          @items_1.each { |item| create(:invoice_items, invoice: @invoice_1, item: item, unit_price: 500, quantity: 20) } #10000
-          @items_2.each { |item| create(:invoice_items, invoice: @invoice_2, item: item, unit_price: 100, quantity: 5) } #500
-          @items_3.each { |item| create(:invoice_items, invoice: @invoice_2, item: item, unit_price: 500, quantity: 5) } #2500
-          @items_4.each { |item| create(:invoice_items, invoice: @invoice_3, item: item, unit_price: 200, quantity: 10) } # 2000
-          @items_5.each { |item| create(:invoice_items, invoice: @invoice_3, item: item, unit_price: 500, quantity: 15) } #7500
-          @items_6.each { |item| create(:invoice_items, invoice: @invoice_4, item: item, unit_price: 200, quantity: 5) } #1000
+          @invoice_items_1 = create(:invoice_items, invoice: @invoice_1, item: @item_1, unit_price: 600, quantity: 1) #600 - fails
+          @invoice_items_2 = create(:invoice_items, invoice: @invoice_2, item: @item_2, unit_price: 400, quantity: 1) #400
+          @invoice_items_3 = create(:invoice_items, invoice: @invoice_2, item: @item_3, unit_price: 400, quantity: 2) #800
+          @invoice_items_4 = create(:invoice_items, invoice: @invoice_3, item: @item_4, unit_price: 300, quantity: 4) #1200
+          @invoice_items_5 = create(:invoice_items, invoice: @invoice_3, item: @item_5, unit_price: 200, quantity: 10) #2000
+          @invoice_items_6 = create(:invoice_items, invoice: @invoice_4, item: @item_6, unit_price: 100, quantity: 5) #500
 
-          @transaction_1 = create_list(:transaction, 2, invoice: @invoice_1, result: :failed)
-          @transaction_2 = create_list(:transaction, 2, invoice: @invoice_1, result: :failed)
-          @transaction_3 = create_list(:transaction, 2, invoice: @invoice_2, result: :success)
-          @transaction_4 = create_list(:transaction, 2, invoice: @invoice_2, result: :success)
-          @transaction_5 = create_list(:transaction, 2, invoice: @invoice_3, result: :failed)
-          @transaction_6 = create_list(:transaction, 2, invoice: @invoice_3, result: :success)
-          @transaction_7 = create_list(:transaction, 2, invoice: @invoice_4, result: :success)
-          @transaction_8 = create_list(:transaction, 2, invoice: @invoice_4, result: :failed)
+          @transaction_1 = create(:transaction, invoice: @invoice_1, result: :failed)
+          @transaction_2 = create(:transaction, invoice: @invoice_1, result: :failed)
+          @transaction_3 = create(:transaction, invoice: @invoice_2, result: :success)
+          @transaction_4 = create(:transaction, invoice: @invoice_2, result: :success)
+          @transaction_5 = create(:transaction, invoice: @invoice_3, result: :failed)
+          @transaction_6 = create(:transaction, invoice: @invoice_3, result: :success)
+          @transaction_7 = create(:transaction, invoice: @invoice_4, result: :success)
+          @transaction_8 = create(:transaction, invoice: @invoice_4, result: :success)
         end
         
         it 'I see the names of the top 5 merchants by total revenue generated' do
           visit admin_merchants_path
-          save_and_open_page
+          
           within("#top-5-merchants") do
             expect(@merchant_5.name).to appear_before(@merchant_2.name)
             expect(@merchant_5.name).to appear_before(@merchant_3.name)
@@ -167,20 +167,41 @@ RSpec.describe "Admin Merchants" do
 
             expect(@merchant_6.name).to appear_before(@merchant_2.name)
 
-            expect(page).to_not have_content(merchant_5.name)
+            expect(page).to_not have_content(@merchant_1.name)
           end
         end
 
         it 'I see that each merchant name links to the admin merchant show page for that merchant' do
           visit admin_merchants_path
 
-          click_button "#{@merchant_1.name}"
-          expect(current_path).to eq(admin_merchant_path(@merchant_1))
+          within("#top-5-merchants") do
+            within("#merchant-#{@merchant_5.id}") do
+              click_link "#{@merchant_5.name}"
+              expect(current_path).to eq(admin_merchant_path(@merchant_5))
+            end
+            visit admin_merchants_path
+            within("#merchant-#{@merchant_2.id}") do
+              click_link "#{@merchant_2.name}"
+              expect(current_path).to eq(admin_merchant_path(@merchant_2))
+            end
+          end
         end
         
         it 'I see the total revenue generated next to each merchant name' do
           visit admin_merchants_path
-          #use within blocks to find on page
+          visit admin_merchants_path
+
+          within("#top-5-merchants") do
+            within("#merchant-#{@merchant_5.id}") do
+              expect(page).to have_content(number_to_currency((@merchant_5.total_revenue/100)))
+              expect(page).to_not have_content(number_to_currency((@merchant_1.total_revenue/100)))
+            end
+            visit admin_merchants_path
+            within("#merchant-#{@merchant_2.id}") do
+              expect(page).to have_content(number_to_currency((@merchant_2.total_revenue/100)))
+              expect(page).to_not have_content(number_to_currency((@merchant_3.total_revenue/100)))
+            end
+          end
         end
       end
     end
