a6b2a3dd9cf68b1fe8258f51423b45972c0edb4e
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-20 16:18:59 -0600

    test: added validation tests for transaction, item, and invoice_item specs

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :quantity}
+    it {should validate_presence_of :unit_price}
+    it {should validate_presence_of :status}
+
+    it { should validate_numericality_of(:quantity) } 
+    it { should validate_numericality_of(:unit_price) } 
+  end
   describe 'relationships' do
     it { should belong_to(:item) }
     it { should belong_to(:invoice) }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :unit_price}
+    it {should validate_presence_of :active_status}
+
+    it { should validate_numericality_of(:unit_price) } 
+  end
   describe 'relationships' do
     it { should belong_to(:merchant) }
     it { should have_many(:invoice_items) }
