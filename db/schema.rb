3508bf4a119eb64ea0070c2cf7518f92513bf934
Riley McCullough <rileybmccullough@gmail.com>
2022-09-19 20:59:41 -0600

    Fix: restore merchant status default to Disabled

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2022_09_15_230831) do
 
   create_table "merchants", force: :cascade do |t|
     t.string "name"
-    t.string "status", default: "Enabled", null: false
+    t.string "status", default: "Disabled", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
