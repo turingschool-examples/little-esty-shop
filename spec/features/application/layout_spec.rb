e9aefb7fe06e565cf8b8b2125c45497f42397830
A.J. Krumholz <krumholz.alan@gmail.com>
2022-09-20 17:36:51 -0400

    remove mock calls extracted to spec_helper

--- spec/features/application/layout_spec.rb
+++ spec/features/application/layout_spec.rb
@@ -4,9 +4,6 @@ RSpec.describe 'application layout' do
   describe 'footer content' do
     describe 'api displays' do
       it 'displays github information' do
-        allow(GitHubFacade).to receive(:user_names).and_return(%w[AlainaKneiling AstridHecht LlamaBack ajkrumholz])
-        allow(GitHubFacade).to receive(:get_pr_total).and_return(96)
-
         visit admin_merchants_path
 
         expect(page).to have_content('ajkrumholz')
