2940526d02ba6a6bef24cabfe71899a9bff20f92
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-09-19 21:18:11 -0600

    test: add refactor of routes in test and test for favorite customers.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -76,58 +76,27 @@ RSpec.describe(Merchant, type: :model) do
       transaction_14 = Transaction.create!(invoice_id: invoice_14.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'failed' )
       transaction_14 = Transaction.create!(invoice_id: invoice_15.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
 
-      expect(merchant1.favorite_customers).to eq([Customer.all[2], Customer.all[1], Customer.all[0], Customer.all[4], Customer.all[3]])
+      expect(merchant1.favorite_customers).to eq([])
     end
 
     it 'total revenue' do
       merchant1 = Merchant.create!(name: "Bob")
+      merchant2 = Merchant.create!(name: "Mark")
+      customer1 = Customer.create!(first_name: "Jolene", last_name: "Jones")
+      customer2 = Customer.create!(first_name: "Jake", last_name: "Jones")
+      invoice_1 = customer1.invoices.create!(status: 1, created_at: "2021-09-14 09:00:01")
+      invoice_2 = customer2.invoices.create!(status: 1, created_at: "2021-09-14 09:00:01")
+      item1 = merchant1.items.create!(name: "item1", description: "this is item1 description", unit_price: 1)
+      item2 = merchant1.items.create!(name: "item2", description: "this is item2 description", unit_price: 2)
+      item3 = merchant1.items.create!(name: "item3", description: "this is item3 description", unit_price: 3)
+      item4 = merchant2.items.create!(name: "item4", description: "this is item4 description", unit_price: 3)
+      invoice_item1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice_1.id, unit_price: item1.unit_price, quantity: 1, status: 0)
+      invoice_item2 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice_1.id, unit_price: item2.unit_price, quantity: 3, status: 0)
+      invoice_item3 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice_1.id, unit_price: item3.unit_price, quantity: 2, status: 0)
+      invoice_item3 = InvoiceItem.create!(item_id: item4.id, invoice_id: invoice_2.id, unit_price: item4.unit_price, quantity: 6, status: 0)
 
-      7.times do
-        Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-      end
-
-      invoice_1 = Invoice.create!(customer_id: Customer.all[0].id, status: 'completed')
-      invoice_8 = Invoice.create!(customer_id: Customer.all[0].id, status: 'completed')
-      invoice_13 = Invoice.create!(customer_id: Customer.all[0].id, status: 'completed')
-
-      invoice_9 = Invoice.create!(customer_id: Customer.all[1].id, status: 'completed')
-      invoice_2 = Invoice.create!(customer_id: Customer.all[1].id, status: 'completed')
-
-      invoice_10 = Invoice.create!(customer_id: Customer.all[2].id, status: 'completed')
-      invoice_3 = Invoice.create!(customer_id: Customer.all[2].id, status: 'completed')
-      invoice_15 = Invoice.create!(customer_id: Customer.all[2].id, status: 'completed')
-
-      invoice_11 = Invoice.create!(customer_id: Customer.all[3].id, status: 'completed')
-      invoice_4 = Invoice.create!(customer_id: Customer.all[3].id, status: 'completed')
-
-      invoice_12 = Invoice.create!(customer_id: Customer.all[4].id, status: 'completed')
-      invoice_5 = Invoice.create!(customer_id: Customer.all[4].id, status: 'completed')
-
-      invoice_6 = Invoice.create!(customer_id: Customer.all[5].id, status: 'completed')
-
-      invoice_7 = Invoice.create!(customer_id: Customer.all[6].id, status: 'completed')
-      invoice_14 = Invoice.create!(customer_id: Customer.all[6].id, status: 'completed')
-
-      transaction_1 = Transaction.create!(invoice_id: invoice_1.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_2 = Transaction.create!(invoice_id: invoice_2.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_3 = Transaction.create!(invoice_id: invoice_3.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_4 = Transaction.create!(invoice_id: invoice_4.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_5 = Transaction.create!(invoice_id: invoice_5.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_6 = Transaction.create!(invoice_id: invoice_6.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_7 = Transaction.create!(invoice_id: invoice_7.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_8 = Transaction.create!(invoice_id: invoice_8.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_9 = Transaction.create!(invoice_id: invoice_9.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_10 = Transaction.create!(invoice_id: invoice_10.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_11 = Transaction.create!(invoice_id: invoice_11.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_12 = Transaction.create!(invoice_id: invoice_12.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-      transaction_13 = Transaction.create!(invoice_id: invoice_13.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'failed' )
-      transaction_14 = Transaction.create!(invoice_id: invoice_14.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'failed' )
-      transaction_14 = Transaction.create!(invoice_id: invoice_15.id, credit_card_number: Faker::Number.number(digits: 16), credit_card_expiration_date: '', result: 'success' )
-
-      expect(invoice_1.total_revenue).to eq(0)
-      expect(invoice_4.total_revenue).to eq(3)
-      expect(invoice_6.total_revenue).to eq(4)
-      expect(invoice_14.total_revenue).to eq(1)
+      expect(invoice_1.total_revenue).to eq(13)
+      expect(invoice_2.total_revenue).to eq(18)
     end
 
     xit 'top_5_revenue' do
