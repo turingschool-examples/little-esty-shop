eeab3e68785e5ca0f4d601e245a1214eae1c30d3
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-19 18:53:18 -0600

    added code that hypothetically should work - good stopping points

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 require_relative '../facades/github_facade'
 
 class ApplicationController < ActionController::Base
-  before_action :user_names
+  before_action :user_names, :get_pr_total
   
   private
 
   def user_names
     @user_names = GitHubFacade.user_names
+  end
+
+  def get_pr_total
+    @pr_total = GitHubFacade.get_pr_total
   end
 end
 
--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -6,6 +6,12 @@ class GitHubFacade
   def self.user_names
     response = GitHubService.request("collaborators")
     parsed = JSON.parse(response.body)
-    parsed.map { |user| user["login"] }.sort
+    # parsed.map { |user| user["login"] }.sort
+  end
+  
+  def self.get_pr_total
+    response = GitHubService.request("pulls")
+    parsed = JSON.parse(response.body)
+    # parsed[0]["number"]
   end
 end
