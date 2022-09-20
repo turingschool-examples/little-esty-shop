8d9fd1f8551005e71183c927add32abc5dbfb4c2
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-20 14:05:38 -0600

    refactor complete fr this time

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,11 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   
-# scope module: 'merchant' do
   resources :merchants, except: [:show], module: 'merchant' do
-    # namespace :merchant do
-      # resources :merchant do
-        resources :invoices, only: [:index, :show]
-        resources :items, except: [:delete]
-        resources :invoice_items, only: [:update]
-      # end
-    end
+    resources :invoices, only: [:index, :show]
+    resources :items, except: [:delete]
+    resources :invoice_items, only: [:update]
+  end
   
   get '/merchants/:merchant_id/dashboard', to: 'merchants#show'
 
