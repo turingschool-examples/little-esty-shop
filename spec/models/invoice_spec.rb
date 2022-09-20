39f7a8e7289d9bf6e1d7bff2eb9e3cbb1066d707
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-20 16:22:05 -0600

    test: added validation tests for invoice, customer, and merchant specs

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-
+  describe "validations" do
+    it {should validate_presence_of :status}
+  end
   describe 'relationships' do
     it { should belong_to(:customer) }
     it { should have_many(:transactions) }
