aec2c32ec3ce9ae4cb87b752dd1b8cd21794744e
Shawn Lee <Shawnl93@hotmail.com>
2022-09-19 20:43:25 -0600

    completed total revenue US

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -41,7 +41,6 @@ RSpec.describe 'Merchant Invoice Show' do
 
     it "total revenue that will be generated from all of items on invoice" do
       visit "/merchants/#{@merchant_1.id}/invoices/#{@invoice_1.id}"
-      save_and_open_page
       expect(page).to have_content("Total Revnue: ")
     end
 end
