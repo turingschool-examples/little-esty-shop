48f065dc80c0170731ce7f1bf44de4c3a20d5fb3
Ken Lenhart <omnipaid@gmail.com>
2022-09-20 22:53:00 -0700

    API limit reached

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,19 +3,19 @@ require 'json'
 
 class GithubService
 
-  def get_repos
+  def self.get_repos
     get_uri('https://api.github.com/repos/Penitent0/little-esty-shop')
   end
 
-  def get_total_pulls
+  def self.get_total_pulls
     get_uri('https://api.github.com/search/issues?q=repo:Penitent0/little-esty-shop%20type:pr%20is:merged')
   end
 
-  def get_contributors
+  def self.get_contributors
     get_uri('https://api.github.com/repos/Penitent0/little-esty-shop/contributors')
   end
 
-  def get_uri(uri)
+  def self.get_uri(uri)
     data = HTTParty.get(uri)
     parsed = JSON.parse(data.body, symbolize_names: true)
   end
