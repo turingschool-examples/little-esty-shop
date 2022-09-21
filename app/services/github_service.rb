f96cbd5cc57d5e0bb6f6b2d1b3d50338a1554460
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-20 22:31:36 -0600

    feat: API names generating with passing test when we dont hit limit on api

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -7,6 +7,10 @@ class GithubService
     get_uri('https://api.github.com/repos/Penitent0/little-esty-shop')
   end
 
+  def self.get_total_pulls
+    get_uri('https://api.github.com/search/issues?q=repo:Penitent0/little-esty-shop%20type:pr%20is:merged')
+  end
+
   def self.get_uri(uri)
     data = HTTParty.get(uri)
     parsed = JSON.parse(data.body, symbolize_names: true)
