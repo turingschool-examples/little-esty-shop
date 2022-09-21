3f0d787000fc7c67660bf346f8c084a5e189d789
Jake Kim <imjakekim@gmail.com>
2022-09-20 23:37:38 -0700

    more playing around

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -4,20 +4,26 @@ class GitHubFacade
 
   def self.user_names
     response = GitHubService.request("collaborators", true)
+
     parsed = JSON.parse(response.body)
+
     parsed.map { |user| user["login"] }.sort
+
   end
   
   def self.get_pr_total
     response = GitHubService.request("pulls", false)
     parsed = JSON.parse(response.body)
     parsed[0]["number"]
-    binding.pry
 
   end
 
   def self.user_commits
     response = GitHubService.request("commits", true)
-
+    parsed = JSON.parse(response.body)
+    hash = Hash.new(0)
+    parsed.each do |commit|
+      hash[commit["author"]["login"]] += 1
+    end
   end
 end 
