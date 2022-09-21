1993c2abe6269e4edff533e36898e1e863b4554a
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-21 00:09:18 -0600

    added number of commits to each rebo member

--- app/poros/contributor.rb
+++ app/poros/contributor.rb
@@ -1,7 +1,8 @@
 class Contributor
-  attr_reader :login
+  attr_reader :login, :commits
 
   def initialize(repo_data)
     @login = repo_data[:login]
+    @commits = repo_data[:contributions]
   end
 end
