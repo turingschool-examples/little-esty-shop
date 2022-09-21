9255c9c892ff8ebff0cc158546e501e7bac74798
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-21 14:46:15 -0600

    comlete adding number of commits for each member

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -4,17 +4,17 @@ require 'json'
 class GithubService
 
   def self.get_repos
-    return {name: 'little-esty-shop - TEST', full_name: 'Penitent0/little-esty-shop'} if Rails.env == 'test'
+    # return {name: 'little-esty-shop - TEST', full_name: 'Penitent0/little-esty-shop'} if Rails.env == 'test'
     get_uri('https://api.github.com/repos/Penitent0/little-esty-shop')
   end
 
   def self.get_total_pulls
-    return {total_count: 999} if Rails.env == 'test'
+    # return {total_count: 999} if Rails.env == 'test'
     get_uri('https://api.github.com/search/issues?q=repo:Penitent0/little-esty-shop%20type:pr%20is:merged')
   end
 
   def self.get_contributors
-    return [{login: 'Ken - TEST'}, {login: 'Erik - TEST'}, {login: 'Sandy - TEST'}, {login: 'Aleisha - TEST'}] if Rails.env == 'test'
+    # return [{login: 'Ken - TEST', contributions: 999}, {login: 'Erik - TEST', contributions: 999}, {login: 'Sandy - TEST', contributions: 999}, {login: 'Aleisha - TEST', contributions: 999}] if Rails.env == 'test'
     get_uri('https://api.github.com/repos/Penitent0/little-esty-shop/contributors')
   end
 
