7079e3b4686e5a729aaa65157390eda41dd88e8f
A.J. Krumholz <krumholz.alan@gmail.com>
2022-09-21 18:55:49 -0400

    abandon all webmocks! use regular mocks instead

--- spec/facades/github_facade_spec.rb
+++ spec/facades/github_facade_spec.rb
@@ -4,14 +4,14 @@ require 'github_facade'
 RSpec.describe GitHubFacade do
   describe 'class methods' do
     describe '::user_names' do
-      xit 'returns an array containing the names of all collaboraters on a github repo' do
-        expect(GitHubFacade.user_names).to eq(%w[ajkrumholz AlainaKneiling AstridHecht LlamaBack])
+      it 'returns an array containing the names of all collaboraters on a github repo' do
+        expect(GitHubFacade.user_names).to eq(%w[Alaina-Noel Astrid-Hecht LlamaBack ajkrumholz])
       end
     end
 
     describe '::repo_name' do
-      xit 'returns the origin repo name as s string' do
-        expect(GitHubFacade.repo_names).to eq('little-esty-shop')
+      it 'returns the origin repo name as s string' do
+        expect(GitHubFacade.repo_name).to eq('little-esty-shop')
       end
     end
     
