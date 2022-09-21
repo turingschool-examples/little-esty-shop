f1c94c22031e9d207eed5d98d90bb2586811485d
Erik Riggs <riggs.erik@gmail.com>
2022-09-20 22:01:12 -0600

    removed save and open page

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -84,7 +84,7 @@ RSpec.describe 'Merchant Dashboard' do
       it 'then I see the name of the merchant' do
 
         visit merchant_dashboard_path(@merchant_1)
-        save_and_open_page
+        
         expect(page).to have_content(@merchant_1.name)
         expect(page).to_not have_content(@merchant_2.name)
 
