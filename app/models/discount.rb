aa37c9c8354c60eddf9a8652a1597177abc0387c
Erik Riggs <riggs.erik@gmail.com>
2022-09-22 14:13:31 -0600

    feat: add validations to discount model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,7 @@
 class Discount < ApplicationRecord
+  validates_numericality_of :bulk_discount, less_than: 1
+  validates_numericality_of :bulk_discount, greater_than: 0
+  validates_numericality_of :item_threshold, greater_than: 0
+  
   belongs_to :merchant
-end 
+end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Discount, type: :model do
+  describe "validations" do
+    it { should validate_numericality_of(:bulk_discount).is_less_than(1)}
+    it { should validate_numericality_of(:bulk_discount).is_greater_than(0)}
+    it { should validate_numericality_of(:item_threshold).is_greater_than(0)}
+  end
   describe 'relationships' do
     it { should belong_to(:merchant) }
   end
