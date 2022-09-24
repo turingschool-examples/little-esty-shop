532cd17ebb57493e8c7cea552bf683c589cb71a1
Ken Lenhart <omnipaid@gmail.com>
2022-09-24 12:45:27 -0700

    Add bulk_discount.rb model, validations, and relationships. Add merchant.rb model relationship and test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-    validates_presence_of :first_name, :last_name
+  validates_presence_of :first_name, :last_name
 
   has_many :invoices
 
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :discount }
+    it { should validate_presence_of :threshold }
+  end
   describe "relationships" do
     it { should belong_to(:merchant) }
   end
