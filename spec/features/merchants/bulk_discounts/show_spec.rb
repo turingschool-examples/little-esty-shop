541e4895876fa3dc83bef9498e2fd3eeaa25a0b0
Ken Lenhart <omnipaid@gmail.com>
2022-09-24 21:28:45 -0700

    Add tests for User Story 5

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -38,5 +38,49 @@ RSpec.describe 'bulk discount show page' do
         expect(page).to_not have_content(@bulk_discount_5.threshold)
       end
     end
-  end 
+  end
+
+  # As a merchant
+  # When I visit my bulk discount show page
+  # Then I see a link to edit the bulk discount
+  # When I click this link
+  # Then I am taken to a new page with a form to edit the discount
+  # And I see that the discounts current attributes are pre-poluated in the form
+  # When I change any/all of the information and click submit
+  # Then I am redirected to the bulk discount's show page
+  # And I see that the discount's attributes have been updated
+  
+  describe 'User Story 5 - When I visit my bulk discount show page' do
+    it 'Then I see a link to edit the bulk discount' do
+      visit  merchant_bulk_discount_path(@merchant_1, @bulk_discount_1)
+
+      within "#edit-bulk-discount" do
+        find_link({text: "Edit Bulk Discount ID #{@bulk_discount_1.id}", href: edit_merchant_bulk_discount_path(@merchant_1, @bulk_discount_1)}).visible?
+      end
+
+      visit  merchant_bulk_discount_path(@merchant_2, @bulk_discount_4)
+      
+      within "#edit-bulk-discount" do
+        find_link({text: "Edit Bulk Discount ID #{@bulk_discount_4.id}", href: edit_merchant_bulk_discount_path(@merchant_2, @bulk_discount_4)}).visible?
+      end
+    end
+
+    it 'When I click this link Then I am taken to a new page with a form to edit the discount' do
+      visit merchant_bulk_discount_path(@merchant_1, @bulk_discount_1)
+
+      click_on "Edit Bulk Discount ID #{@bulk_discount_1.id}"
+
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant_1, @bulk_discount_1))
+      save_and_open_page
+      expect(page).to have_content(bulk_discount[edit])
+    end
+
+    it 'And I see that the discounts current attributes are pre-poluated in the form' do
+      
+    end
+
+    it 'when I make an edit, I am redirected back to the show page and see the updated attributes' do
+      
+    end
+  end
 end
