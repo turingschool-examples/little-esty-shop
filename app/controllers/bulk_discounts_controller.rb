aa4e7c0922fda30f2d15a46f958591f5000e19cd
Ken Lenhart <omnipaid@gmail.com>
2022-09-24 21:09:49 -0700

    Add show action to bulk_discount_controller.rb, add bulk_discount/show.html.erb, complete User Story 4

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -3,6 +3,10 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
   end
 
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
   def new
     @merchant = Merchant.find(params[:merchant_id])
     @bulk_discount = BulkDiscount.new
