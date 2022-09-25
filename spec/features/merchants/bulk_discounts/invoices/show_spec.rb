dbd01d637181e43a8ae5601b1c14f6c4a9bbcc8c
Ken Lenhart <omnipaid@gmail.com>
2022-09-24 22:17:40 -0700

    Add bulk_discounts/invoices/show_spec.rb for User Story 6 tests

---
+++ spec/features/merchants/bulk_discounts/invoices/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant invoice show page bulk discounts' do
+  before :each do
+
+  end
+
+  # As a merchant
+  # When I visit my merchant invoice show page
+  # Then I see the total revenue for my merchant from this invoice (not including discounts)
+  # And I see the total discounted revenue for my merchant from this invoice which includes bulk discounts in the calculation
+
+  describe 'User Story 6 - When I visit my merchant invoice show page' do
+    it 'Then I see the total revenue for my merchant from this invoice (not including discounts)' do
+      visit 
+    end
+
+    it 'And I see the total discounted revenue for my merchant from this invoice which includes bulk discounts in the calculation' do
+
+    end
+  end
+end
