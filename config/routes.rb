fa57f27409d073c76fb7a808afcaa7632b8820fd
Riley McCullough <rileybmccullough@gmail.com>
2022-09-28 00:12:56 -0600

    edit route to redirect homepage

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :invoices
   resources :customers
 
-  get: '/' to: '/items/index'
+  get: '/', to: '/items/index'
 
   resources :merchants do
     resources :items
