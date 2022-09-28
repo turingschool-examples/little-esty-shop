08b3e753802d55581c1cbc9a2d771fce0361055c
Ken Lenhart <omnipaid@gmail.com>
2022-09-27 18:26:19 -0700

    Add callback before_validation and after_destroy to bulk_discount.rb

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,5 +1,20 @@
 class BulkDiscount < ApplicationRecord
   validates_presence_of :discount, :threshold
-
   belongs_to :merchant
+
+  before_validation :check_discount, on: [:create, :update]
+  after_destroy :print_destroy_action
+
+  private
+  
+  def check_discount
+    if self.discount >= 1.0
+      puts "Discount too darn high! Setting Discount to 5 percent."
+      self.discount = 0.05
+    end
+  end
+
+  def print_destroy_action
+    puts "Bulk Discount Destroyed!"
+  end
 end
