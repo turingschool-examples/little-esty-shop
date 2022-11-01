bd25e1a2216e0d7c23afff5c1b6d77b2e12ab482
Madeline Mauser <madeline.mauser@gmail.com>
2022-10-31 21:39:32 -0600

    Add test for merchant items show page

---
+++ spec/features/items/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "Merchant Items Show Page", type: :feature do
+  describe 'on visiting the page' do
+    before :each do
+      @merchant = Merchant.create!(name: 'Gemma Belle')
+      @item = @merchant.items.create!(name: "Wooden Necklace", description: "A necklace with wood beads.", unit_price: 1000)
+    end
+    it 'shows all of the attributes of the item' do
+      visit "/merchants/#{@merchant.id}/items/#{@item.id}"
+
+      expect(page).to have_content("Item: Wooden Necklace")
+      expect(page).to have_content("Description: A necklace with wood beads.")
+      expect(page).to have_content("Unit price: 1000")
+    end
+  end
+end
