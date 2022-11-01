78bc1c698e0643e1d98fd3d7bd2a04217cfdae73
Mostafa Sakr <msakr3@gmail.com>
2022-11-01 17:26:45 -0600

    Resolve merge conflicts of US 3 with latest updates and push.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # get '/merchants/:merchant_id/items/:item_id', to: 'items#show'
   # get 'merchants/:id/items', to: 'merchant_items#index'
 
   resources :merchants, except: [:update] do
     resources :items, except: [:update]
-    get 'merchants/:id/dashboard', to: 'merchants#show'
-end
+  end
 
   #These handrolled routes must be accessed by their full URI, but otherwise work the same as their resource versions
   patch '/merchants/:id', to: 'merchants#update'
   patch '/merchants/:merchant_id/items/:id', to: 'items#update'
 
-  
   get 'merchants/:id/dashboard', to: 'merchants#show'
 end
-
