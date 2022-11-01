3dbcbd0bde43f9bfb0bcfb409f97b8a65b8d5a66
Rich Kaht <adamselene1776@protonmail.com>
2022-11-01 12:36:17 -0600

    Feat: Add migrations

---
+++ db/migrate/20221101174052_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.references :merchant, foreign_key: true
+      t.string :name
+      t.string :description
+      t.integer :unit_price
+      t.integer :status
+      
+      t.timestamps
+    end
+  end
+end
