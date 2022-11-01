6e2eb33309c8736c5c882bd4d8006b5d37529e26
Krisetn Nestler <kristen.nestler@gmail.com>
2022-11-01 14:03:44 -0400

    admin dashboard links created

--- spec/features/items/admin/index_spec.rb
+++ spec/features/items/admin/index_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin Index' do
-  before :each do 
+    before :each do 
+      visit '/admin'
+    end
 
-  end
-  it "has a header" do 
-    visit '/admin'
+    it "has a header" do 
+      expect(page).to have_content("Admin Dashboard")
+    end
 
-    expect(page).to have_content("Admin Dashboard")
-  end
+    it "has links" do 
+      expect(page).to have_link('admin merchants index')
+      expect(page).to have_link('admin invoices index')
+    end
+
+
 end
