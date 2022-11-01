ec7403a4e75adb8fab09036ce6628db5f9a5e642
Naomi Yocum <naomiyocum24@gmail.com>
2022-11-01 11:56:19 -0600

    create current_price method

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def current_price
+    unit_price / 100.0
+  end
 end
