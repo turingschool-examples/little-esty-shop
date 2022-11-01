1e4fb5ef17981777adbf3655983689e60d13acb6
Emily Port <emilylport@gmail.com>
2022-11-01 13:46:04 -0600

    updated relationships in models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,6 @@
 class Invoice < ApplicationRecord
   enum status: { cancelled: 0,  "in progress" => 1, completed: 2}
   belongs_to :customer 
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,4 @@
 class InvoiceItem < ApplicationRecord
-  
+  belongs_to :invoice 
+  belongs_to :item 
 end
