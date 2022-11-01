f6c1f2fb516725c112850079c64ec1ce11997bdc
Madeline Mauser <madeline.mauser@gmail.com>
2022-11-01 13:11:08 -0600

    Refactor merchant and merchant item routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/merchants/:merchant_id/items/:item_id', to: 'items#show'
+  # get '/merchants/:merchant_id/items/:item_id', to: 'items#show'
+  # get 'merchants/:id/items', to: 'merchant_items#index'
 
-  get 'merchants/:id/items', to: 'merchant_items#index'
-
+  resources :merchants do
+    resources :items
+  end
 end
 
