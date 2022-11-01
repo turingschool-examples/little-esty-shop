a82bbc98a07b76e93c5680cb09da871488f48ee7
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-11-01 14:13:09 -0600

    first merchant_dashboard user story

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
+# Merchant Dashboard Links
 
+# As a merchant,
+# When I visit my merchant dashboard
+# Then I see link to my merchant items index (/merchants/merchant_id/items)
+# And I see a link to my merchant invoices index (/merchants/merchant_id/invoices)
 RSpec.describe 'merchant dashboard show page' do
   let!(:nomi) {Merchant.create!(name: "Naomi LLC")}
   let!(:tyty) {Merchant.create!(name: "TyTy's Grub")}
@@ -10,5 +15,21 @@ RSpec.describe 'merchant dashboard show page' do
 
       expect(page).to have_content(nomi.name)
     end
+
+    it 'has a link to merchant items index' do
+      visit merchant_dashboard_index_path(nomi)
+
+      click_on("My Items") 
+      
+      expect(page).to have_current_path("/merchants/#{nomi.id}/items")
+    end
+
+    # it 'has a link to my merchant invoices index' do
+    #   visit merchant_dashboard_index_path(nomi)
+
+    #   click_on("My Invoices") 
+      
+    #   expect(page).to have_current_path("/merchants/#{nomi.id}/invoices")
+    # end
   end
 end
