f4617794fb3650b3175ac492b026a6bf1ad1e592
Naomi Yocum <naomiyocum24@gmail.com>
2022-11-01 13:14:05 -0600

    create edit form for item

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :merchants, only: [:index] do
-    resources :items, only: [:index, :show]
+    resources :items, only: [:index, :show, :edit]
   end
+
+  patch '/merchants/:merchant_id/items/:id', to: 'items#update'
 
   resources :admin, only: [:index]
 end
