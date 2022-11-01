0f4c3a5b7fd6df9f111c07f6b6b09fc23128aed4
Madeline Mauser <madeline.mauser@gmail.com>
2022-11-01 13:12:46 -0600

    Move merchant item index logic into items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
+  def index
+    @merchant = Merchant.find(params[:id])
+    @merchant_items = @merchant.items
+  end
+  
   def show
     @item = Item.find(params[:item_id])
   end
