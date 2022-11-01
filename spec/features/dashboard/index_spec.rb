ec73a14be19eb0b2da1fc9a75e1039123f47bf08
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-11-01 13:35:26 -0600

    changing branches

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -1,17 +1,12 @@
 require 'rails_helper'
-# Merchant Dashboard
 
-# As a merchant,
-# When I visit my merchant dashboard (/merchants/merchant_id/dashboard)
-# Then I see the name of my merchant
 RSpec.describe 'merchant dashboard show page' do
   let!(:nomi) {Merchant.create!(name: "Naomi LLC")}
   let!(:tyty) {Merchant.create!(name: "TyTy's Grub")}
 
   describe 'visit merchant dashboard' do
     it 'shows the name of my merchant' do
-      visit "/merchants/#{nomi.id}/dashboard"
-      # merchant_dashboard_index_path
+      visit merchant_dashboard_index_path(nomi)
 
       expect(page).to have_content(nomi.name)
     end
