8284e59265173f50e1b35834afa71623ac3a3515
Will Wang <willy.j.wang@gmail.com>
2022-11-01 18:12:47 -0500

    Add links to merchant dashboard for user story 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'merchants/:id/items', to: 'merchant_items#index'
+
+  get 'merchants/:id/dashboard', to: 'merchants#show'
 end
