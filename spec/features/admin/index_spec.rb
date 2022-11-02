fa3ac4da6a6f6d1bf7d9122c658e29d7577cdcc7
Annie Pulzone <ajpulzone@gmail.com>
2022-11-02 16:46:43 -0700

    Added test data, tests for Admin Index page

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -3,7 +3,34 @@ require "rails_helper"
 RSpec.describe "Admin Dashboard (Index", type: :feature do
   
   before(:each) do
+    @customer_1 = Customer.create!("Luke", "Harison")
+    @customer_2 = Customer.create!("Angela", "Leizer")
+    @customer_3 = Customer.create!("Matt", "Sorry")
+    @customer_4 = Customer.create!("Drake", "Pointer")
+    @customer_5 = Customer.create!("Fannie", "May")
+    @customer_6 = Customer.create!("Lorelai", "Gillmore")
+    @customer_7 = Customer.create!("Simon", "Garfunkle")
 
+    @invoice_1 = @customer_1.invoice.create!(status: completed)
+    @invoice_2 = @customer_1.invoice.create!(status: completed)
+    @invoice_3 = @customer_1.invoice.create!(status: cancelled)
+    @invoice_4 = @customer_1.invoice.create!(status: in_progress)
+    @invoice_5 = @customer_1.invoice.create!(status: in_progress)
+    @invoice_6 = @customer_2.invoice.create!(status: completed)
+    @invoice_7 = @customer_2.invoice.create!(status: completed)
+    @invoice_8 = @customer_3.invoice.create!(status: completed)
+    @invoice_9 = @customer_3.invoice.create!(status: completed)
+    @invoice_10 = @customer_3.invoice.create!(status: in_progress)
+    @invoice_11 = @customer_3.invoice.create!(status: completed)
+    @invoice_12 = @customer_4.invoice.create!(status: cancelled)
+    @invoice_13 = @customer_4.invoice.create!(status: cancelled)
+    @invoice_14 = @customer_4.invoice.create!(status: in_progress)
+    @invoice_15 = @customer_5.invoice.create!(status: completed)
+    @invoice_16 = @customer_5.invoice.create!(status: completed)
+    @invoice_17 = @customer_6.invoice.create!(status: cancelled)
+    @invoice_18 = @customer_6.invoice.create!(status: in_progress)
+    @invoice_19 = @customer_6.invoice.create!(status: in_progress)
+    @invoice_20 = @customer_7.invoice.create!(status: completed)
   end
 
   it "has a header indicating that the user is on the admin dashboard" do
