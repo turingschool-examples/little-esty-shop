0a7385c08dcda33a4d527bf7b0f17a3c8ae9863a
Amanda Ross <amikaross@gmail.com>
2022-11-02 11:55:15 -0600

    Update merchant model spec for default value of disabled for items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe Merchant, type: :model do
         merchant = Merchant.create!(name: "Practical Magic Shop")
         book = merchant.items.create!(name: "Book of the dead", description: "book of necromamcy spells", unit_price: 4)
         candle = merchant.items.create!(name: "Candle of life", description: "candle that gifts everlasting life", unit_price: 15)
-        potion = merchant.items.create!(name: "Love potion", description: "One serving size of true love potion", unit_price: 10, status: 1)
+        potion = merchant.items.create!(name: "Love potion", description: "One serving size of true love potion", unit_price: 10, status: 0)
 
-        expect(merchant.enabled_items).to eq([book, candle])
+        expect(merchant.enabled_items).to eq([potion])
 
         coffee = merchant.items.create!(name: "Coffee mug", description: "Its a mug", unit_price: 1, status: 0)
 
-        expect(merchant.enabled_items).to eq([book, candle, coffee])
+        expect(merchant.enabled_items).to eq([potion, coffee])
       end
     end
 
@@ -26,13 +26,13 @@ RSpec.describe Merchant, type: :model do
         merchant = Merchant.create!(name: "Practical Magic Shop")
         book = merchant.items.create!(name: "Book of the dead", description: "book of necromamcy spells", unit_price: 4)
         candle = merchant.items.create!(name: "Candle of life", description: "candle that gifts everlasting life", unit_price: 15)
-        potion = merchant.items.create!(name: "Love potion", description: "One serving size of true love potion", unit_price: 10, status: 1)
+        potion = merchant.items.create!(name: "Love potion", description: "One serving size of true love potion", unit_price: 10, status: 0)
 
-        expect(merchant.disabled_items).to eq([potion])
+        expect(merchant.disabled_items).to eq([book, candle])
 
-        coffee = merchant.items.create!(name: "Coffee mug", description: "Its a mug", unit_price: 1, status: 1)
+        coffee = merchant.items.create!(name: "Coffee mug", description: "Its a mug", unit_price: 1)
 
-        expect(merchant.disabled_items).to eq([potion, coffee])
+        expect(merchant.disabled_items).to eq([book, candle, coffee])
       end
     end
   end
