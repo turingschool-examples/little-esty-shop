0f2ec02b0969cad261e9233979eaec62ea5dee36
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-01 22:58:27 -0600

    completed user story with passing tests: Merchant Item Disable/Enable

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,12 @@
 class ItemsController < ApplicationController
   def update
-    require 'pry'; binding.pry
+    @item = Item.find(params[:id])
+    if params[:button] == 'true' && @item.status == 'enabled'
+      @item.status = 'disabled'
+    elsif params[:button] == 'true' && @item.status == 'disabled'
+      @item.status = 'enabled'
+    end
+    @item.save
+    redirect_to "/merchants/#{@item.merchant.id}/items"
   end
 end
