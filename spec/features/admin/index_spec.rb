36505fe6e808860215018b1e011b561c9b2f0525
Annie Pulzone <ajpulzone@gmail.com>
2022-11-02 14:27:17 -0700

    WIP, adding top 5 customers to dashboard

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -7,19 +7,30 @@ RSpec.describe "Admin Dashboard (Index", type: :feature do
   end
 
   it "has a header indicating that the user is on the admin dashboard" do
-
     visit "/admin"
-
     expect(page).to have_content("Admin Dashboard")
   end
 
-  xit "has a link to the admin merchants index" do
-    
+  it "has a link to the admin merchants index" do
     visit "/admin"
+    expect(page).to have_link("Admin Merchants", href: "/admin/merchants")
+  end
+
+  it "has a link to the admin invoices index" do
+    visit "/admin"
+    expect(page).to have_link("Admin Invoices", href: "/admin/invoices")
+  end
+
+  it "has a list of the top 5 customers who have conducted the largest unmber of successful
+    transactions" do
+      visit "/admin"
 
   end
 
-  # xit "has a link to the admin invoices index" do
-  #   visit "/admin"
-  # end
+  it "next to each of the top 5 customers it has the number of successful transactions they
+    have conducted" do
+      visit "/admin"
+
+  end
+
 end
