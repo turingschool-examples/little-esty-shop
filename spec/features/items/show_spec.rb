cb5384dbd7d1a5646a0a7f6b54769256189cf56f
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-11-02 13:46:40 -0400

    test: setting up tests for show page

---
+++ spec/features/items/show_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant items show page' do 
+  describe 'when i visit my merchant items index page' do 
+    before :each do 
+      @klein_rempel = Merchant.create!(name: "Klein, Rempel and Jones")
+      @whb = Merchant.create!(name: "WHB")
+      @dk = Merchant.create!(name: "Dickinson-Klein")
+      @watch = @klein_rempel.items.create!(name: "Watch", description: "Tells time on your wrist", unit_price: 300)
+      @radio = @klein_rempel.items.create!(name: "Radio", description: "Broadcasts radio stations", unit_price: 150)
+      @speaker = @klein_rempel.items.create!(name: "Speakers", description: "Listen to your music LOUD", unit_price: 250)
+
+      @ufo = @dk.items.create!(name: "UFO Board", description: "Out of this world MotherBoard", unit_price: 400)
+      @funnypowder = @dk.items.create!(name: "Funny Brick of Powder", description: "White Powder with Gasoline Smell", unit_price: 5000)
+      @binocular = @dk.items.create!(name: "Binoculars", description: "See everything from afar", unit_price: 300)
+      @tent = @dk.items.create!(name: "Tent", description: "Spend the night under the stars... or under THEM!", unit_price: 500)
+
+      @bike = @whb.items.create!(name: "Bike", description: "Two wheel Huffy bike", unit_price: 99)
+      @trex = @whb.items.create!(name: "T-Rex", description: "Skull of a Dinosaur", unit_price: 100000)
+    end
+
+
+    describe "When I click on the name of an item from the merchant items index page" do
+      describe "Then I am taken to that merchant's item's show page (/merchants/merchant_id/items/item_id)" do 
+        it "displays all of the item's attributes including: Name, Description, Current Selling Price" do
+
+          view ("/merchants/#{@dk.id}/items/#{@funnypowder.id}")
+
+          expect(page).to have_content("#{@dk.name}")
+
+        end
+      end
+    end
+
+
+  end
+end
