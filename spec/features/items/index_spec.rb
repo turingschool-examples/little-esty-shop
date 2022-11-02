ec1d8c42ef376fce83fde312086f0281362aba2e
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-01 23:54:47 -0600

    Tests merchants_items_index successfully

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -3,19 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Merchants' do
   before(:each) do
     @merchant = Merchant.create!(name: 'FooMerchant')
-    @merchant.items.create!(name: 'fooItem')
-    @merchant.items.create!(name: 'barItem')
+    @item1 = @merchant.items.create!(name: 'fooItem')
+    @item2 = @merchant.items.create!(name: 'barItem')
   end
 
-  describe "Items" do
+  describe 'Items' do
     describe '#index' do
       it 'has some behaviour' do
         visit "/merchants/#{@merchant.id}/items"
+        expect(page).to have_content(@item1.name)
+        expect(page).to have_content(@item2.name)
       end
-
     end
-
-
   end
-
 end
