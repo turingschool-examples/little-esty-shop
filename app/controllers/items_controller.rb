b92f7c20c792ff53b750b61af74e2e7eb75e575b
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-11-02 19:55:14 -0400

    test/feat: UPDATE button on item show page complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,16 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    # require 'pry';binding.pry
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to "/merchants/#{@item.merchant.id}/items/#{@item.id}"
   end
 end
--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -1,51 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe 'merchant items show page' do 
-    before :each do 
-      @klein_rempel = Merchant.create!(name: "Klein, Rempel and Jones")
-      @whb = Merchant.create!(name: "WHB")
-      @dk = Merchant.create!(name: "Dickinson-Klein")
+  before :each do 
+    @klein_rempel = Merchant.create!(name: "Klein, Rempel and Jones")
+    @whb = Merchant.create!(name: "WHB")
+    @dk = Merchant.create!(name: "Dickinson-Klein")
+    
+    @watch = @klein_rempel.items.create!(name: "Watch", description: "Tells time on your wrist", unit_price: 300)
+    @radio = @klein_rempel.items.create!(name: "Radio", description: "Broadcasts radio stations", unit_price: 150)
+    @speaker = @klein_rempel.items.create!(name: "Speakers", description: "Listen to your music LOUD", unit_price: 250)
+
+    @ufo = @dk.items.create!(name: "UFO Board", description: "Out of this world MotherBoard", unit_price: 400)
+    @funnypowder = @dk.items.create!(name: "Funny Brick of Powder", description: "White Powder with Gasoline Smell", unit_price: 5000)
+    @binocular = @dk.items.create!(name: "Binoculars", description: "See everything from afar", unit_price: 300)
+    @tent = @dk.items.create!(name: "Tent", description: "Spend the night under the stars... or under THEM!", unit_price: 500)
+
+    @bike = @whb.items.create!(name: "Bike", description: "Two wheel Huffy bike", unit_price: 99)
+    @trex = @whb.items.create!(name: "T-Rex", description: "Skull of a Dinosaur", unit_price: 100000)
+  end
+
+  describe "on a merchant's item's show page (/merchants/merchant_id/items/item_id)" do 
       
-      @watch = @klein_rempel.items.create!(name: "Watch", description: "Tells time on your wrist", unit_price: 300)
-      @radio = @klein_rempel.items.create!(name: "Radio", description: "Broadcasts radio stations", unit_price: 150)
-      @speaker = @klein_rempel.items.create!(name: "Speakers", description: "Listen to your music LOUD", unit_price: 250)
-
-      @ufo = @dk.items.create!(name: "UFO Board", description: "Out of this world MotherBoard", unit_price: 400)
-      @funnypowder = @dk.items.create!(name: "Funny Brick of Powder", description: "White Powder with Gasoline Smell", unit_price: 5000)
-      @binocular = @dk.items.create!(name: "Binoculars", description: "See everything from afar", unit_price: 300)
-      @tent = @dk.items.create!(name: "Tent", description: "Spend the night under the stars... or under THEM!", unit_price: 500)
-
-      @bike = @whb.items.create!(name: "Bike", description: "Two wheel Huffy bike", unit_price: 99)
-      @trex = @whb.items.create!(name: "T-Rex", description: "Skull of a Dinosaur", unit_price: 100000)
+    it "show page is specific to a item in the url" do
+      visit ("/merchants/#{@dk.id}/items/#{@tent.id}")
+      
+      expect(page).to_not have_content("#{@whb.name}")
+      expect(page).to_not have_content("#{@klein_rempel.name}")
+      expect(page).to_not have_content("#{@ufo.name}")
+      expect(page).to_not have_content("#{@watch.name}")
+      expect(page).to_not have_content("#{@funnypowder.description}")
+      expect(page).to_not have_content("#{@bike.description}")
     end
 
-    describe "When I click on the name of an item from the merchant items index page" do
-      describe "Then I am taken to that merchant's item's show page (/merchants/merchant_id/items/item_id)" do 
-        
-        it "show page is specific to a item in the url" do
-          visit ("/merchants/#{@dk.id}/items/#{@funnypowder.id}")
-          
-          expect(page).to_not have_content("#{@whb.name}")
-          expect(page).to_not have_content("#{@klein_rempel.name}")
-          expect(page).to_not have_content("#{@ufo.name}")
-          expect(page).to_not have_content("#{@watch.name}")
-          expect(page).to_not have_content("#{@bike.description}")
-        end
-
-        it "displays all of the item's attributes including: Name, Description, Current Selling Price" do
-          visit ("/merchants/#{@dk.id}/items/#{@funnypowder.id}")
-          
-          expect(page).to have_content("#{@dk.name}")
-          
-          within("#merchant-items-#{@funnypowder.id}") do
-            expect(page).to have_content("#{@funnypowder.name}")
-            expect(page).to have_content("#{@funnypowder.description}")
-            expect(page).to have_content("#{@funnypowder.unit_price}")
-            expect(page).to_not have_content("#{@dk.name}")
-          # save_and_open_page
-          # require 'pry';binding.pry
-          end
-        end
+    it "displays all of the item's attributes including: Name, Description, Current Selling Price" do
+      visit ("/merchants/#{@dk.id}/items/#{@funnypowder.id}")
+      
+      expect(page).to have_content("#{@dk.name}")
+      
+      within("#merchant-items-#{@funnypowder.id}") do
+        expect(page).to have_content("#{@funnypowder.name}")
+        expect(page).to have_content("#{@funnypowder.description}")
+        expect(page).to have_content("#{@funnypowder.unit_price}")
+        expect(page).to_not have_content("#{@dk.name}")
       end
     end
+
+    it "has a link to update the item information" do
+      visit ("/merchants/#{@dk.id}/items/#{@funnypowder.id}")
+
+      expect(current_path).to eq("/merchants/#{@dk.id}/items/#{@funnypowder.id}")
+      
+      click_button "Update #{@funnypowder.name}"
+      expect(current_path).to eq("/merchants/#{@dk.id}/items/#{@funnypowder.id}/edit")
+    end
+  end
+      
 end
