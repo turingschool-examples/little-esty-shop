0b4f04dca3b5f2ed90264965366cdee05a262b3d
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-01 23:55:31 -0600

    Updates application layout and items controller with index view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,8 @@
 class ItemsController < ApplicationController
-  def index; end
+  def index
+    # require 'pry'; binding.pry
+    if params.has_key? 'merchant_id'
+      @merchant = Merchant.find(params[:merchant_id])
+    end
+  end
 end
