5136116111684248b7fcb6ba70a4be33f48ba53a
Kevin Ta <kvnta@yahoo.com>
2022-11-01 18:05:44 -0600

    Save before branch switch

---
+++ db/migrate/20221101183619_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.integer :unit_price
+      t.references :merchant, foreign_key: true
+      t.timestamps
+    end
+  end
+end
