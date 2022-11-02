8f37e2032a4c5a326f8be2e8d281645e636fab4b
Naomi Yocum <naomiyocum24@gmail.com>
2022-11-02 15:05:12 -0600

    change formatting

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,7 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
+  has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  has_many :transactions
   enum status: ["in progress", "cancelled", "completed"]
 end
