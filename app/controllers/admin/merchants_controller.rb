9fa36acd5babcac48bf0824ee60abaab6bc8f789
Shawn Lee <Shawnl93@hotmail.com>
2022-11-02 20:19:53 -0600

    completed Admin Merchant Update

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,4 +6,20 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+    @merchant.update(merchant_params)
+    flash[:notice] = "Successfully Updated: #{@merchant.name}"
+    redirect_to "/admin/merchants/#{@merchant.id}"
+  end
+
+private
+  def merchant_params
+    params.permit(:name)
+  end
 end
