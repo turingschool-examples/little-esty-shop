73689ce673aeed830c613318a2d83edf6dbca741
Naomi Yocum <102825498+naomiyocum@users.noreply.github.com>
2022-11-03 09:36:12 -0600

    Update invoice_item.rb

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,13 +2,12 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
   enum status: [:pending, :packaged, :shipped]
-
-<<<<<<< HEAD
-  def self.incomplete_invoices
-    InvoiceItem.where.not(status:2).pluck(:invoice_id)
-  end
-=======
+  
   validates :quantity, :unit_price, :status, :presence => true
   validates :quantity, :unit_price, :numericality => true
->>>>>>> 31c77b12d8f68ec06b9680a4bbd4c108732e5220
+
+
+  def self.incomplete_invoices
+    InvoiceItem.where.not(status:2).distinct.order(:invoice_id).pluck(:invoice_id)
+  end
 end
