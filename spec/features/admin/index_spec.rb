6e0d439e176e5ef402ad40be2d548ec525d1a30e
Darby Smith <newman.darby2@gmail.com>
2022-11-02 22:56:51 -0400

    Test incomplete invoices on admin dasboard

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -26,19 +26,18 @@ RSpec.describe 'Admin#index' do
   # Then I see the names of the top 5 customers
   # who have conducted the largest number of successful transactions
   # And next to each customer name I see the number of successful transactions they have conducted
-  describe 'On admin dashboard we see top 5 customers' do
-    before :each do
+  before :each do
     @customer_1 = Customer.create!(first_name: "Eli", last_name: "Fuchsman")
     @customer_2 = Customer.create!(first_name: "Bryan", last_name: "Keener")
     @customer_3 = Customer.create!(first_name: "Darby", last_name: "Smith")
     @customer_4 = Customer.create!(first_name: "James", last_name: "White")
     @customer_5 = Customer.create!(first_name: "William", last_name: "Lampke")
     @customer_6 = Customer.create!(first_name: "Abdul", last_name: "Redd")
-
+    
     @merchant = Merchant.create!(name: "Test")
-
+    
     @item_1 = Item.create!(name: "item1", description: "desc1", unit_price: 10, merchant_id: @merchant.id)
-
+    
     @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 1)
     @invoice_2 = Invoice.create!(customer_id: @customer_2.id, status: 1)
     @invoice_3 = Invoice.create!(customer_id: @customer_3.id, status: 1)
@@ -47,13 +46,13 @@ RSpec.describe 'Admin#index' do
     @invoice_6 = Invoice.create!(customer_id: @customer_2.id, status: 1)
     @invoice_7 = Invoice.create!(customer_id: @customer_2.id, status: 1)
     @invoice_8 = Invoice.create!(customer_id: @customer_4.id, status: 1)
-
+    
     @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 1)
-    @ii_2 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 1)
+    @ii_2 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 2)
     @ii_3 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_1.id, quantity: 2, unit_price: 10, status: 1)
     @ii_4 = InvoiceItem.create!(invoice_id: @invoice_4.id, item_id: @item_1.id, quantity: 3, unit_price: 10, status: 1)
-    @ii_5 = InvoiceItem.create!(invoice_id: @invoice_5.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 1)
-
+    @ii_5 = InvoiceItem.create!(invoice_id: @invoice_5.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 0)
+    
     @transaction_1 = Transaction.create!(credit_card_number: "1", result: 0, invoice_id: @invoice_1.id)
     @transaction_2 = Transaction.create!(credit_card_number: "2", result: 0, invoice_id: @invoice_2.id)
     @transaction_3 = Transaction.create!(credit_card_number: "3", result: 0, invoice_id: @invoice_3.id)
@@ -62,22 +61,46 @@ RSpec.describe 'Admin#index' do
     @transaction_6 = Transaction.create!(credit_card_number: "5", result: 0, invoice_id: @invoice_6.id)
     @transaction_7 = Transaction.create!(credit_card_number: "5", result: 0, invoice_id: @invoice_7.id)
     @transaction_8 = Transaction.create!(credit_card_number: "5", result: 0, invoice_id: @invoice_8.id)
-    # binding.pry
-
   end
+  
+  describe 'On admin dashboard we see top 5 customers' do
     it 'displays top 5 customers' do
-     visit "/admin"
+      visit "/admin"
       # save_and_open_page
-     expect(page).to have_content("Eli Fuchsman: 1")
-     expect(page).to have_content("Darby Smith: 1")
-     expect(page).to have_content("William Lampke: 1")
-     expect(page).to have_content("Bryan Keener: 3")
-     expect(page).to have_content("James White: 2")
+      expect(page).to have_content("Eli Fuchsman: 1")
+      expect(page).to have_content("Darby Smith: 1")
+      expect(page).to have_content("William Lampke: 1")
+      expect(page).to have_content("Bryan Keener: 3")
+      expect(page).to have_content("James White: 2")
 
-     expect("Bryan Keener: 3").to appear_before("James White: 2")
-     expect("James White: 2").to appear_before("Eli Fuchsman: 1")
-     expect("Eli Fuchsman: 1").to appear_before("Darby Smith: 1")
-     expect("Darby Smith: 1").to appear_before("William Lampke: 1")
+      expect("Bryan Keener: 3").to appear_before("James White: 2")
+      expect("James White: 2").to appear_before("Eli Fuchsman: 1")
+      expect("Eli Fuchsman: 1").to appear_before("Darby Smith: 1")
+      expect("Darby Smith: 1").to appear_before("William Lampke: 1")
+    end
+  end
+
+  describe 'admib dashboard incomplete invoices' do
+    it 'has a section for incomplete invoices that contains ids of invoices for items that have not been shipped' do
+      visit "/admin"
+      
+      within("#invoice-#{@invoice_1.id}") do
+        expect(page).to have_link(@invoice_1.id)
+      end
+
+      within("#invoice-#{@invoice_3.id}") do
+        expect(page).to have_link(@invoice_3.id)
+      end
+
+      within("#invoice-#{@invoice_4.id}") do
+        expect(page).to have_link(@invoice_4.id)
+      end
+
+      within("#invoice-#{@invoice_5.id}") do
+        expect(page).to have_link(@invoice_5.id)
+      end
+
+      expect(page).to_not have_link(@invoice_2.id)
     end
   end
 end
