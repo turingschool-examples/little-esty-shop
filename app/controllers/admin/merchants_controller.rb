113b80c2e154060764d7634c717e56a3652ac2a4
Kevin Ta <kvnta@yahoo.com>
2022-11-03 17:35:28 -0600

    Set up test, make controller for admin merchant show

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -3,6 +3,10 @@ class Admin::MerchantsController < ApplicationController
     @merchants = Merchant.all
   end
 
+  def show 
+    @merchant = Merchant.find(params[:id])
+  end
+
   def update
     @merchant = Merchant.find(params[:id]) 
     if params[:status]
---
+++ spec/features/admin/merchants/show_spec.rb
@@ -0,0 +1,30 @@
+<h1><center> Admin Merchant Show Page </center> </h1>
+
+
+RSpec.describe 'Admin Merchant show page' do 
+  before (:each) do 
+      @klein_rempel = Merchant.create!(name: "Klein, Rempel and Jones", status: "Disabled")
+      @whb = Merchant.create!(name: "WHB", status: "Enabled")
+      @lisa_frank = Merchant.create!(name: 'Lisa Frank Knockoffs', status: 'Enabled')
+      @burger = Merchant.create!(name: 'Burger King', status: 'Disabled')
+
+      
+  end 
+  
+    it 'As an admin, when I click on the name of a merchant from the admin merchant index page' do
+      visit admin_merchants_path
+
+      expect(page).to have_link("#{@burger.name}")
+      click_link("#{@burger.name}")
+      expect(current_path).to eq(admin_merchants_path(@burger))
+    end 
+    
+    it " After clicking on merchant from index page, I am taken to the merchant admin show page, and I see the name of that merchant" do 
+      visit admin_merchants_path(@burger)
+
+      expect(page).to have_content(@burger.name)
+      expect(page).to have_content(@burger.status)
+    end
+  end
+end 
+
