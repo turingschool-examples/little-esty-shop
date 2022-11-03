4ef13f356fc85b8e61e2afe73a2387444c93c0c3
Naomi Yocum <naomiyocum24@gmail.com>
2022-11-02 20:23:38 -0600

    fix validation, i am sorry

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
   
-  validates :credit_card_number, :credit_card_expiration_date, :result, :presence => true
+  validates :credit_card_number, :result, :presence => true
   validates_numericality_of :credit_card_number
 
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -55,7 +55,6 @@ RSpec.describe Transaction, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:credit_card_number)}
     it {should validate_numericality_of(:credit_card_number)}
-     it {should validate_presence_of(:credit_card_expiration_date)}
     it {should validate_presence_of(:result)}
   end
   
