760415cb63d24ab58349cd78cec0dbfc0e2110ec
Joseph Hilby <joseph.hilby@gmail.com>
2022-11-03 16:40:08 -0500

    Add feature merchant invoice show page and tests

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,10 @@
 class InvoicesController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
+    @invoices = Invoice.invoices_for(@merchant)
+  end
+
+  def show
+
   end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,9 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def self.invoices_for(merchant)
+    invoice_ids = merchant.invoice_items.pluck("invoice_id")
+    Invoice.where(id: invoice_ids)
+  end
 end
