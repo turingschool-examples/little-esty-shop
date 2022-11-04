44bbcae0ca9fe27d087ead2dda70452ab6f74111
Alex M <alexm0717@hotmail.com>
2022-11-03 22:22:19 -0700

    updated merchant controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,6 +1,8 @@
 class Admin::MerchantsController < ApplicationController
   def index
     @merchants = Merchant.all
+    @enabled_merchants = Merchant.enabled_merchants
+    @disabled_merchants = Merchant.disabled_merchants
   end
 
   def show
@@ -13,8 +15,14 @@ class Admin::MerchantsController < ApplicationController
 
   def update
     @merchant = Merchant.find(params[:id])
-    @merchant.update!(    name: params[:name])
-    redirect_to(admin_merchant_path(@merchant.id))
-    flash[:notice] = "#{@merchant.name} has been successfully updated"
+
+    if params[:status]
+      @merchant.update!(      status: params[:status])
+      redirect_to(admin_merchants_path)
+    else
+      @merchant.update!(      name: params[:name])
+      redirect_to(admin_merchant_path(@merchant.id))
+      flash[:notice] = "#{@merchant.name} has been successfully updated"
+    end
   end
 end
