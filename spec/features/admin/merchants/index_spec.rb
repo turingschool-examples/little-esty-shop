7bc99836dc5aa7fba4e9bff6f89d632568c70eb2
Darby Smith <newman.darby2@gmail.com>
2022-11-05 19:20:21 -0400

    Refactor tests for admin index merchants page

---
+++ spec/features/admin/merchants/index_spec.rb
@@ -0,0 +1,71 @@
+require "rails_helper"
+
+RSpec.describe "admin/merchants index page" do
+  before :each do
+    @merchant_1 = Merchant.create!(name: "Marvel")
+    @merchant_2 = Merchant.create!(name: "D.C.")
+    @merchant_3 = Merchant.create!(name: "Darkhorse")
+    @merchant_4 = Merchant.create!(name: "Image")
+  end
+  # US 24 As an admin,
+  # When I visit the admin merchants index (/admin/merchants)
+  # Then I see the name of each merchant in the system
+  describe 'when i visit the merchant index I see a name for each merchant'do
+    it "shows the name of each merchant" do
+      visit "/admin/merchants"
+
+      expect(page).to have_content(@merchant_1.name)
+      expect(page).to have_content(@merchant_2.name)
+      expect(page).to have_content(@merchant_3.name)
+      expect(page).to have_content(@merchant_4.name)
+    end
+
+    it "each name links to an merchants' show page" do
+      visit "/admin/merchants"
+
+      expect(page).to have_link(@merchant_1.name)
+      expect(page).to have_link(@merchant_2.name)
+      expect(page).to have_link(@merchant_3.name)
+      expect(page).to have_link(@merchant_4.name)
+  
+      click_on "#{@merchant_1.name}"
+      expect(current_path).to eq("/admin/merchants/#{@merchant_1.id}")
+    end
+  end
+
+  #US 27 As an admin,
+  # When I visit the admin merchants index
+  # Then next to each merchant name I see a button to disable or enable that merchant.
+  # When I click this button
+  # Then I am redirected back to the admin merchants index
+  # And I see that the merchant's status has changed
+  describe 'as an admin I see a button to update a merchants status' do 
+    it 'has a buttons next to each merchant to enable or disable' do
+      visit "/admin/merchants"
+      
+      within("#merchant-#{@merchant_1.id}") do 
+        expect(page).to have_content(@merchant_1.name)
+        expect(page).to have_button("Enable Merchant")
+        expect(page).to have_button("Disable Merchant")
+      end
+
+      within("#merchant-#{@merchant_1.id}") do 
+        expect(page).to have_content(@merchant_2.name)
+        expect(page).to have_button("Enable Merchant")
+        expect(page).to have_button("Disable Merchant")
+      end
+
+      within("#merchant-#{@merchant_1.id}") do 
+        expect(page).to have_content(@merchant_3.name)
+        expect(page).to have_button("Enable Merchant")
+        expect(page).to have_button("Disable Merchant")
+      end
+
+      within("#merchant-#{@merchant_1.id}") do 
+        expect(page).to have_content(@merchant_4.name)
+        expect(page).to have_button("Enable Merchant")
+        expect(page).to have_button("Disable Merchant")
+      end
+    end
+  end
+end
