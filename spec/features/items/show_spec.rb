bce0341bba6a2cc122e86b735d848652671a1fae
Eli Fuchsman <efuchsman@gmail.com>
2022-11-04 22:01:38 -0600

    Remove commented out user story from show spec

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -1,11 +1,3 @@
-# As a merchant,
-# When I click on the name of an item from the merchant items index page,
-# Then I am taken to that merchant's item's show page (/merchants/merchant_id/items/item_id)
-# And I see all of the item's attributes including:
-
-# Name
-# Description
-# Current Selling Price
 require 'rails_helper'
 
 RSpec.describe "Merchant Items Show Page" do
