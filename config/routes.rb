c64480618854e3b5f980bd4b60005ad38565bd59
Darby Smith <newman.darby2@gmail.com>
2022-11-06 11:03:03 -0500

    Add routes for creating merchant

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do 
-    resources :merchants, only: [:index, :show, :edit, :update] 
+    resources :merchants, only: [:index, :create, :new, :show, :edit, :update] 
   end
 
   # admin
