01e2a44b8ec51981fa4ec1e35aed22b36ec06bc8
bkeener <bkeener7@gmail.com>
2022-11-06 12:37:42 -0700

    Revert has_one association and fix test for invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,15 +3,14 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  has_one :merchant, through: :items
-
+  has_many :merchants, through: :items
   validates_presence_of :status, :customer_id
 
   enum status: { 'in progress' => 0, completed: 1, canceled: 2 }
 
   def self.incomplete_invoices
     joins(:invoice_items)
-    .where('invoice_items.status=0 OR invoice_items.status=1')
-    .order(:created_at)
+      .where('invoice_items.status=0 OR invoice_items.status=1')
+      .order(:created_at)
   end
 end
