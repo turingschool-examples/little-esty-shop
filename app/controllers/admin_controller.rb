8a37e9cf94af1e6df167e5b417ac8460f236cc40
Mostafa Sakr <msakr3@gmail.com>
2022-11-07 16:03:21 -0700

    Add tests and implementation code for incomplete invoices methods and section in admin index page.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,6 @@
 class AdminController < ApplicationController
   def index
     @top_customers = Customer.top_5_by_successful_transactions
+    @incomplete_invoice_items = InvoiceItem.not_shipped
   end
 end
