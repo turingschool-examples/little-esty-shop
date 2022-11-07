652c92614709e3c12fe905c7feffc47f6e96413b
Alex M <alexm0717@hotmail.com>
2022-11-06 21:10:03 -0800

    updated test in admin merchant index spec

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -3,9 +3,89 @@ require "rails_helper"
 
 RSpec.describe("admin merchants index page") do
   before(:each) do
+    feb_third = DateTime.new(2022, 2, 3, 4, 5, 6)
+    march_third = DateTime.new(2022, 3, 3, 6, 2, 3)
+    april_first = DateTime.new(2022, 4, 1, 8, 9, 6)
+
+    ####BREAK
     @merchant1 = Merchant.create!(    name: "Tokyos Tractors")
+    @item1 = @merchant1.items.create!(    name: "A",     description: "Alpha",     unit_price: 1)
+    @item2 = @merchant1.items.create!(    name: "B",     description: "Bravo",     unit_price: 2)
+    @item3 = @merchant1.items.create!(    name: "C",     description: "Charlie",     unit_price: 3)
+    @cx1 = Customer.create!(    first_name: "Tapanga",     last_name: "Toloza")
+    @invoice_1 = @cx1.invoices.create!(    status: 1,     created_at: feb_third)
+    @invoice_2 = @cx1.invoices.create!(    status: 1,     created_at: march_third)
+    InvoiceItem.create!(    invoice: @invoice_1,     item: @item1,     quantity: 1,     unit_price: 1,     status: 2)
+    InvoiceItem.create!(    invoice: @invoice_2,     item: @item2,     quantity: 1,     unit_price: 2,     status: 2)
+    InvoiceItem.create!(    invoice: @invoice_2,     item: @item3,     quantity: 1,     unit_price: 3,     status: 2)
+    @invoice_1.transactions.create!(    credit_card_number: 123456789,     credit_card_expiration_date: "07/2023",     result: "success")
+    @invoice_2.transactions.create!(    credit_card_number: 123456789,     credit_card_expiration_date: "07/2023",     result: "success")
+
+    ###LINE BREAK
     @merchant2 = Merchant.create!(    name: "Oslos Outdoor Market")
+    @item4 = @merchant2.items.create!(    name: "D",     description: "Delta",     unit_price: 4)
+    @item5 = @merchant2.items.create!(    name: "E",     description: "Echo",     unit_price: 5)
+    @item6 = @merchant2.items.create!(    name: "F",     description: "Fox",     unit_price: 6)
+    @cx2 = Customer.create!(    first_name: "Ocsar",     last_name: "Oreily")
+    @invoice_3 = @cx2.invoices.create!(    status: 1,     created_at: april_first)
+    InvoiceItem.create!(    invoice: @invoice_2,     item: @item4,     quantity: 1,     unit_price: 3,     status: 2)
+    InvoiceItem.create!(    invoice: @invoice_2,     item: @item5,     quantity: 1,     unit_price: 4,     status: 2)
+    InvoiceItem.create!(    invoice: @invoice_2,     item: @item6,     quantity: 1,     unit_price: 5,     status: 2)
+    @invoice_3.transactions.create!(    credit_card_number: 123456789,     credit_card_expiration_date: "07/2023",     result: "failed")
+
+      ###LINE BREAK
     @merchant3 = Merchant.create!(    name: "Berlins Building Supply")
+    @item7 = @merchant3.items.create!(    name: "G",     description: "Golf",     unit_price: 7)
+    @item8 = @merchant3.items.create!(    name: "H",     description: "Hotel",     unit_price: 8)
+    @item9 = @merchant3.items.create!(    name: "I",     description: "India",     unit_price: 9)
+    @cx3 = Customer.create!(    first_name: "Bruce",     last_name: "Banner")
+    @invoice_4 = @cx3.invoices.create!(    status: 1,     created_at: feb_third)
+    @invoice_5 = @cx3.invoices.create!(    status: 1,     created_at: feb_third)
+    InvoiceItem.create!(    invoice: @invoice_4,     item: @item7,     quantity: 1,     unit_price: 4,     status: 2)
+    InvoiceItem.create!(    invoice: @invoice_4,     item: @item8,     quantity: 1,     unit_price: 5,     status: 2)
+    InvoiceItem.create!(    invoice: @invoice_5,     item: @item9,     quantity: 1,     unit_price: 6,     status: 2)
+    @invoice_4.transactions.create!(    credit_card_number: 123456789,     credit_card_expiration_date: "07/2023",     result: "failed")
+    @invoice_5.transactions.create!(    credit_card_number: 123456789,     credit_card_expiration_date: "07/2023",     result: "failed")
+
+      ###LINE BREAK
+    @merchant4 = Merchant.create!(    name: "Rios Radios")
+    @item10 = @merchant4.items.create!(    name: "J",     description: "Juliet",     unit_price: 10)
+    @item11 = @merchant4.items.create!(    name: "K",     description: "Kilo",     unit_price: 11)
+    @item12 = @merchant4.items.create!(    name: "L",     description: "lima",     unit_price: 12)
+    @cx4 = Customer.create!(    first_name: "Roy",     last_name: "Rodriguez")
+    invoice_4 = @cx4.invoices.create!(    status: 1,     created_at: feb_third)
+    @invoice_5 = @cx4.invoices.create!(    status: 1,     created_at: march_third)
+    InvoiceItem.create!(    invoice: @invoice_4,     item: @item10,     quantity: 1,     unit_price: 7,     status: 2)
+    InvoiceItem.create!(    invoice: @invoice_5,     item: @item11,     quantity: 1,     unit_price: 8,     status: 2)
+    InvoiceItem.create!(    invoice: @invoice_5,     item: @item12,     quantity: 1,     unit_price: 9,     status: 0)
+    @invoice_4.transactions.create!(    credit_card_number: 123456789,     credit_card_expiration_date: "07/2023",     result: "success")
+    @invoice_5.transactions.create!(    credit_card_number: 123456789,     credit_card_expiration_date: "07/2023",     result: "success")
+
+      ###LINE BREAK
+    @merchant5 = Merchant.create!(    name: "Moscows Mechanics")
+    @item13 = @merchant5.items.create!(    name: "M",     description: "Mike",     unit_price: 13)
+    @item14 = @merchant5.items.create!(    name: "N",     description: "November",     unit_price: 14)
+    @item15 = @merchant5.items.create!(    name: "O",     description: "Oscar",     unit_price: 15)
+    @cx5 = Customer.create!(    first_name: "Mary",     last_name: "Mccall")
+    @invoice_6 = @cx3.invoices.create!(    status: 1,     created_at: march_third)
+    @invoice_7 = @cx3.invoices.create!(    status: 1,     created_at: march_third)
+    InvoiceItem.create!(    invoice: @invoice_6,     item: @item13,     quantity: 1,     unit_price: 10,     status: 0)
+    InvoiceItem.create!(    invoice: @invoice_7,     item: @item14,     quantity: 1,     unit_price: 11,     status: 2)
+    InvoiceItem.create!(    invoice: @invoice_7,     item: @item15,     quantity: 1,     unit_price: 12,     status: 2)
+    @invoice_6.transactions.create!(    credit_card_number: 123456789,     credit_card_expiration_date: "07/2023",     result: "failed")
+    @invoice_7.transactions.create!(    credit_card_number: 123456789,     credit_card_expiration_date: "07/2023",     result: "success")
+
+      ###LINE BREAK
+    @merchant6 = Merchant.create!(    name: "Denver Divers")
+    @item16 = @merchant6.items.create!(    name: "P",     description: "Papa",     unit_price: 16)
+    @item17 = @merchant6.items.create!(    name: "Q",     description: "Quebec",     unit_price: 17)
+    @item18 = @merchant6.items.create!(    name: "R",     description: "Romeo",     unit_price: 18)
+    @cx6 = Customer.create!(    first_name: "David",     last_name: "Dallal")
+    @invoice_8 = @cx6.invoices.create!(    status: 1,     created_at: march_third)
+    InvoiceItem.create!(    invoice: @invoice_8,     item: @item16,     quantity: 2,     unit_price: 16,     status: 2)
+    InvoiceItem.create!(    invoice: @invoice_8,     item: @item17,     quantity: 1,     unit_price: 17,     status: 2)
+    InvoiceItem.create!(    invoice: @invoice_8,     item: @item18,     quantity: 1,     unit_price: 18,     status: 2)
+    @invoice_8.transactions.create!(    credit_card_number: 123456789,     credit_card_expiration_date: "07/2023",     result: "failed")
   end
 
   describe("24.admin merchants index") do
@@ -89,6 +169,17 @@ RSpec.describe("admin merchants index page") do
           click_button("Create New Merchant")
           expect(current_path).to(eq(admin_merchants_path))
         end
+      end
+    end
+  end
+
+#Only invoices with at least one successful transaction should count towards revenue
+#Revenue for an invoice should be calculated as the sum of the revenue of all invoice items
+#Revenue for an invoice item should be calculated as the invoice item unit price multiplied by the quantity (do not use the item unit price)
+  describe("29.I see the names of the top 5 merchants by total revenue generated") do
+    describe("And I see that each merchant name links to the admin merchant show page for that merchant") do
+      it("29.And I see the total revenue generated next to each merchant name") do
+        visit(admin_merchants_path)
       end
     end
   end
