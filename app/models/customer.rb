0cefd81ee48492859a3190f9a28a3cbbe07ae2ed
Mostafa Sakr <msakr3@gmail.com>
2022-11-06 21:54:09 -0700

    Add implementation code for admin top 5 customers model method.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,13 @@ class Customer < ApplicationRecord
   has_many :invoices, dependent: :destroy
   has_many :transactions, through: :invoices
   has_many :merchants, through: :invoices
+
+  def self.top_5_by_successful_transactions
+    joins(:transactions)
+    .where(transactions: {result: 1})
+    .group(:first_name, :last_name)
+    .order('count_id desc')
+    .limit(5)
+    .count('id')
+  end
 end
