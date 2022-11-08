87367c7df1754b44d100c0735031329adc79959e
Joseph Hilby <joseph.hilby@gmail.com>
2022-11-08 12:29:09 -0600

    Fix test formatting

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -76,17 +76,17 @@ RSpec.describe Customer, type: :model do
         expect(Customer.top_five_customers_for(@merchant_1)).to eq([@customer_6, @customer_1, @customer_2, @customer_3, @customer_4])
       end
     end
+  end
 
-    describe "instance methods" do
-      describe '#top_five_total_customers' do
-        it 'returns top 5 customers for all merchants based on successful transaction count' do
-          expect(Customer.top_five_total_customers).to eq([@customer_1, @customer_2, @customer_3, @customer_4, @customer_5])
-          expect(Customer.top_five_total_customers.first.num_transactions).to eq(4)
-          expect(Customer.top_five_total_customers.second.num_transactions).to eq(2)
-          expect(Customer.top_five_total_customers.third.num_transactions).to eq(2)
-          expect(Customer.top_five_total_customers.fourth.num_transactions).to eq(2)
-          expect(Customer.top_five_total_customers.fifth.num_transactions).to eq(2)
-        end
+  describe "instance methods" do
+    describe '#top_five_total_customers' do
+      it 'returns top 5 customers for all merchants based on successful transaction count' do
+        expect(Customer.top_five_total_customers).to eq([@customer_1, @customer_2, @customer_3, @customer_4, @customer_5])
+        expect(Customer.top_five_total_customers.first.num_transactions).to eq(4)
+        expect(Customer.top_five_total_customers.second.num_transactions).to eq(2)
+        expect(Customer.top_five_total_customers.third.num_transactions).to eq(2)
+        expect(Customer.top_five_total_customers.fourth.num_transactions).to eq(2)
+        expect(Customer.top_five_total_customers.fifth.num_transactions).to eq(2)
       end
     end
 
