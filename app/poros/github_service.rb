a0b61435fe921c82e1c9dbb860f9c68edf825638
Emily Port <emilylport@gmail.com>
2022-11-08 16:48:16 -0700

    create pr_info method in github service to find pr data

--- app/poros/github_service.rb
+++ app/poros/github_service.rb
@@ -9,6 +9,10 @@ class GitHubService
     get_url('https://api.github.com/repos/eport01/little-esty-shop/collaborators')
   end
 
+  def pr_info
+    get_url('https://api.github.com/repos/eport01/little-esty-shop/pulls')
+  end
+
   def get_url(url)
     response = HTTParty.get(url, headers: { 'Authorization' => 'Bearer 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' })
     parsed = JSON.parse(response.body, symbolize_names: true)
