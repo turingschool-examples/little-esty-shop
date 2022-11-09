a16fa9212c26fc6b5fffd94a36549437e728087a
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-08 21:57:58 -0700

    finish new_spec test for filling out form and new merchant being added to admin merchant index

--- spec/features/merchants/new_spec.rb
+++ spec/features/merchants/new_spec.rb
@@ -7,14 +7,23 @@ RSpec.describe 'create new merchant page', type: :feature do
         visit new_merchant_path
 
         expect(page).to have_selector(:css, "form")
-        expect(page).to have_field(:name)
+        expect(page).to have_field(:name, "New Merchant Name")
         expect(page).to have_field(:status, visible: false)
         expect(page).to have_button("Submit")
       end
 
       it 'when I fill out the form and click (submit), I am taken back to the admin merchants index page. I can see 
       the merchant I just created displayed on the admin merchants index page, and its default status is disabled' do
+        visit new_merchant_path
 
+        fill_in "New Merchant Name", with: "Susan's Snappy Stitches"
+        click_button "Submit"
+
+        expect(current_path).to eq(admin_merchants_path)
+
+        within "#merchant-disabled" do
+          expect(page).to have_content("Susan's Snappy Stitches")
+        end
       end
     end
   end
