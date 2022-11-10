e716998100ce795977e231feb7042dc92b200635
Mostafa Sakr <msakr3@gmail.com>
2022-11-09 17:59:49 -0700

    Add API info to all pages. Limited by number of pulls which can crash page. Temporarily have the requests disabled in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
+require './lib/git_repo'
+
 class ApplicationController < ActionController::Base
+  before_action :repo_info, :only => [:index, :show, :edit, :new]
+
+  def repo_info
+    # repo_call = GitRepo.new
+    # @repository_name = repo_call.repo_name
+    # @group_usernames = repo_call.usernames
+    # @contributor_commits = repo_call.commits_by_contributors
+    # @pull_requests = repo_call.number_of_pull_requests
+    @group_usernames = []
+    @contributor_commits = []
+  end
 end
--- lib/git_repo.rb
+++ lib/git_repo.rb
@@ -2,6 +2,7 @@ require 'httparty'
 require 'json'
 require 'pry'
 
+
 class GitRepo
 
   def call(url)
@@ -9,28 +10,32 @@ class GitRepo
   end
 
   def repo_name
-    response = call("https://api.github.com/users/kenzjoy/repos")
-    response["name"]
+    response = call("https://api.github.com/repos/kenzjoy/little-esty-shop")
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    parsed[:name]
   end
 
   def usernames
     response = call("https://api.github.com/repos/kenzjoy/little-esty-shop/assignees")
-    response.map do |user|
-      user["login"]
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    parsed.map do |user|
+      user[:login]
     end
   end
 
   def commits_by_contributors
     response = call("https://api.github.com/repos/kenzjoy/little-esty-shop/stats/contributors")
+    parsed = JSON.parse(response.body, symbolize_names: true)
     contributor_commit_totals = {}
-    response.each do |record|
-      contributor_commit_totals[record["author"]["login"]] = record["total"]
+    parsed.each do |record|
+      contributor_commit_totals[record[:author][:login]] = record[:total]
     end
     contributor_commit_totals
   end
 
   def number_of_pull_requests
     response = call("https://api.github.com/repos/kenzjoy/little-esty-shop/pulls?state=all")
-    response.first["number"]
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    parsed.first[:number]
   end
 end
