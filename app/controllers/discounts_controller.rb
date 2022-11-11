84dcf8fcc6bb7532e185705eea062a306d37e08a
Joseph Hilby <joseph.hilby@gmail.com>
2022-11-11 08:49:12 -0600

    Add feature discounts show

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,4 +1,15 @@
 class DiscountsController < ApplicationController
   def index
+    @merchant = Merchant.find(params[:merchant_id])
+    @discounts = @merchant.discounts
+  end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.find(params[:id])
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
