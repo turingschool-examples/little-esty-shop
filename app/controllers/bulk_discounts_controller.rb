bd0a78099464ef73c92ca7850c057fe1c010daef
Darby Smith <newman.darby2@gmail.com>
2022-11-11 15:42:09 -0500

    Feat: Merchant Bulk Discount Create

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,4 +6,25 @@ class BulkDiscountsController < ApplicationController
   def show
     @discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    discount = merchant.bulk_discounts.new(bulk_discount_params)
+    if discount.save
+      redirect_to merchant_bulk_discounts_path(merchant)
+    else
+      redirect_to new_merchant_bulk_discount_path(merchant)
+      flash[:alert] = "Error! Please enter a valid discount."
+    end
+  end
+
+  private
+
+  def bulk_discount_params
+    params.permit(:percentage, :quantity_threshold)
+  end
 end
