7e50559ad3678f1efd3823f90bbe0e3defd7629f
Joseph Hilby <joseph.hilby@gmail.com>
2022-11-12 13:25:56 -0600

    Add tests for invoice_show discount feature

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -66,21 +66,50 @@ RSpec.describe 'On the Merchant Invoices Show Page' do
 
       it 'total revenue for all items on invoice' do
         within "#invoice-stats-#{@customer_1_invoice_1.id}" do
-
           expect(page).to have_content((@merchant_1_item_1.unit_price * @invoice_item_1.quantity) + (@merchant_1_item_2.unit_price * @invoice_item_2.quantity))
         end
       end
 
       describe 'revenue for all items on this invoice after discounts are applyed' do
         it 'if discounts are applied, shows an invoice total revenue with applied discounts' do
-          expect(page).to_not have_content("Revenue After Discount:")
+          within "#item-info-#{@customer_1_invoice_1.id}" do
+            expect(page).to_not have_content("Revenue After Discount:")
+          end
         end
 
         it 'discounts are applied to invoice_items individually, not the total collectivly' do
           create(:discount, merchant: @merchant_1, quantity_threshold: 5, percentage_discount: 50)
           visit "/merchants/#{@merchant_1.id}/invoices/#{@customer_1_invoice_1.id}"
 
-          expect(page).to have_content("Revenue After Discount: $85.00")
+          within "#item-info-#{@customer_1_invoice_1.id}" do
+            expect(page).to have_content("Revenue After Discount: $85.00")
+          end
+        end
+
+        describe 'revenue for each item listed on the invoice' do
+          before(:each) do
+            @discount = create(:discount, merchant: @merchant_1, quantity_threshold: 5, percentage_discount: 50)
+            visit "/merchants/#{@merchant_1.id}/invoices/#{@customer_1_invoice_1.id}"
+          end
+
+          it 'displays discounted price for each item on the invoice' do
+            within "#item-info-#{@customer_1_invoice_1.id}" do
+              expect(page).to have_content("$15.00")
+            end
+          end
+
+          it 'displays a link to the discount show page if a discout is applied' do
+            within "#item-info-#{@customer_1_invoice_1.id}" do
+              expect(page).to have_link("Discount # #{@discount.id}")
+            end
+          end
+
+          it 'when I click on the link I am take to the discount show page' do
+            within "#item-info-#{@customer_1_invoice_1.id}" do
+              click_link("Discount # #{@discount.id}")
+              expect(current_path).to eq(merchant_discount_path(@merchant_1, @discount))
+            end
+          end
         end
 
         it 'if multiple discounts are avaliable to an invoice_item, only the highest amount discount is applied' do
@@ -88,8 +117,10 @@ RSpec.describe 'On the Merchant Invoices Show Page' do
           create(:discount, merchant: @merchant_1, quantity_threshold: 5, percentage_discount: 10)
           create(:discount, merchant: @merchant_2, quantity_threshold: 1, percentage_discount: 99)
           visit "/merchants/#{@merchant_1.id}/invoices/#{@customer_1_invoice_1.id}"
-          save_and_open_page
-          expect(page).to have_content("Revenue After Discount: $85.00")
+
+          within "#item-info-#{@customer_1_invoice_1.id}" do
+            expect(page).to have_content("Revenue After Discount: $85.00")
+          end
         end
       end
 
