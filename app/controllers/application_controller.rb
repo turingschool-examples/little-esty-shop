05fd70a0259f9cc9f3ed4310227ce07d3eca52da
Joseph Hilby <joseph.hilby@gmail.com>
2022-11-11 19:41:56 -0600

    Add webmocs to rails_helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :get_info,   only: [:index, :show, :new, :edit]
 
   def get_info
-    # @repo_name = GithubSearch.new.repo_information
-    # contributors = GithubSearch.new.contributor_names
-    # @contributors = contributors.sort_by {|contributor| contributor.num_commits}.last(6)
-    # @latest_pr = GithubSearch.new.num_pulls
-
-    @repo_name = Repo.new("little-esty-shop")
-    @contributors = [Contributor.new({total: 54, author: {login: "josephhilby"}}), Contributor.new({total: 47, author: {login: "amikaross"}}), Contributor.new({total: 21, author: {login: "AlexMR-93"}}), Contributor.new({total: 13, author: {login: "ashuhleyt"}}) ]
-    @latest_pr = Pull.new({number: 37})
+    @repo_name = GithubSearch.new.repo_information
+    contributors = GithubSearch.new.contributor_names
+    @contributors = contributors.sort_by {|contributor| contributor.num_commits}.last(6)
+    @latest_pr = GithubSearch.new.num_pulls
   end
 end
