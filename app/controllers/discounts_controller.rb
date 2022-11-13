6badfe99f891ac3b869186cc8f1399be4abb61c4
William Lampke <gravitybangers@gmail.com>
2022-11-13 15:14:06 -0500

    userstory 3 and testing completed

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -11,4 +11,9 @@ class DiscountsController < ApplicationController
     Discount.create!(merchant_id: @merchant.id, quantity_threshhold: params[:quantity_threshhold], percentage: params[:percentage])
     redirect_to "/merchants/#{@merchant.id}/discounts"
   end
+  def destroy
+    @merchant = Merchant.find(params[:merchant_id])
+    Discount.destroy(params[:id])
+    redirect_to "/merchants/#{@merchant.id}/discounts"
+  end
 end
