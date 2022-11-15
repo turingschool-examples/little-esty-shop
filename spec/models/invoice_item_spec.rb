99ed5bac3d9c4c9e673706bddd7976fc868923d5
Darby Smith <newman.darby2@gmail.com>
2022-11-14 22:07:25 -0500

    Find discount for an invoice item

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -13,4 +13,40 @@ RSpec.describe InvoiceItem, type: :model do
     it { should validate_presence_of :item_id }
     it { should validate_presence_of :invoice_id }
   end
+
+  before(:each) do
+    @merchant_1 = Merchant.create!(name: 'Marvel', status: 'enabled')
+    @merchant_2 = Merchant.create!(name: 'D.C.', status: 'disabled')
+    
+    @discount_1 = BulkDiscount.create!(percentage: 15, quantity_threshold: 5, merchant_id: @merchant_1.id)
+    @discount_2 = BulkDiscount.create!(percentage: 20, quantity_threshold: 10, merchant_id: @merchant_1.id)
+    @discount_3 = BulkDiscount.create!(percentage: 10, quantity_threshold: 2, merchant_id: @merchant_2.id)
+    
+    @customer1 = Customer.create!(first_name: 'Peter', last_name: 'Parker')
+    @customer2 = Customer.create!(first_name: 'Clark', last_name: 'Kent') 
+    
+    @invoice1 = Invoice.create!(status: 'completed', customer_id: @customer1.id, created_at: Time.parse('21.01.28'))
+    @invoice2 = Invoice.create!(status: 'completed', customer_id: @customer2.id, created_at: Time.parse('22.08.22'))
+    
+    @item1 = Item.create!(name: 'Beanie Babies', description: 'Investments', unit_price: 100, merchant_id: @merchant_1.id)
+    @item2 = Item.create!(name: 'Bat-A-Rangs', description: 'Weapons', unit_price: 500, merchant_id: @merchant_2.id)
+    
+    @invoice_item_1 = InvoiceItem.create!(quantity: 5, unit_price: 500, status: 'packaged', item_id: @item1.id, invoice_id: @invoice1.id)
+    @invoice_item_2 = InvoiceItem.create!(quantity: 1, unit_price: 100, status: 'shipped', item_id: @item1.id, invoice_id: @invoice2.id)
+    
+    @invoice_item_3 = InvoiceItem.create!(quantity: 50, unit_price: 5000, status: 'shipped', item_id: @item2.id, invoice_id: @invoice1.id)
+    @invoice_item_4 = InvoiceItem.create!(quantity: 15, unit_price: 1500, status: 'shipped', item_id: @item2.id, invoice_id: @invoice2.id)
+    
+    @transaction1 = Transaction.create!(credit_card_number: '4801647818676136', credit_card_expiration_date: nil, result: 'failed', invoice_id: @invoice1.id)
+    @transaction2 = Transaction.create!(credit_card_number: '4654405418249632', credit_card_expiration_date: nil, result: 'success', invoice_id: @invoice1.id)
+    @transaction3 = Transaction.create!(credit_card_number: '4800749911485986', credit_card_expiration_date: nil, result: 'success', invoice_id: @invoice2.id)
+  end
+
+  describe 'instance methods' do
+    describe '#find_discount' do
+      it 'returns the bulk discount that applies to the invoice item' do
+        expect(@invoice_item_1.find_discount).to eq(@discount_1)
+      end
+    end
+  end
 end
