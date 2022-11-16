a4e01ba75814d926e2addeb38fd0ad3b2e7ab51b
William Lampke <gravitybangers@gmail.com>
2022-11-16 02:02:41 -0500

    user story 6

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,9 +1,44 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
-  has_many :discounts
+  has_many :merchants, through: :item
+  has_many :discounts, through: :merchants
 
   validates_presence_of :quantity, :unit_price, :status, :item_id, :invoice_id
 
   enum status: { pending: 0, packaged: 1, shipped: 2 }
+  
+
+  def self.total_revenue(merchant)
+    select(:quantity, :unit_price)
+    .joins(:item)
+    .where("merchant_id = #{merchant}")
+    .sum("invoice_items.unit_price * quantity")
+  end
+
+  def self.discounted(merchant_id)
+    highest_discount = Discount.joins('INNER JOIN invoice_items ON discounts.invoice_item_id = invoice_items.id')
+    .where('discounts.merchant_id = ?', merchant_id)
+    .where('invoice_items.quantity >= discounts.quantity_threshhold')
+    .order('discounts.quantity_threshhold DESC')
+    .first
+
+    self.joins(:item, :discounts)
+    .where('items.merchant_id = ? AND discounts.merchant_id = ?', merchant_id, merchant_id) 
+    .where('invoice_items.quantity >= discounts.quantity_threshhold')
+    .where('discounts.quantity_threshhold = ?', highest_discount.quantity_threshhold)
+    .sum('(invoice_items.unit_price * percentage) * quantity')
+
+  end
 end
+
+"
+
+select(:quantity, :unit_price)
+.joins(:item, :discounts).where('invoice_items.quantity >= discounts.quantity_threshhold')
+
+joins(:item, :discounts).select('quantity * invoice_items.unit_price AS price').first.price
+"
+
+
+
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it { should belong_to :invoice }
     it { should belong_to :item }
-    it { should have_many :discounts }
+    it { should have_many(:discounts).through(:merchants) }
   end
 
   describe 'validations' do
@@ -13,5 +13,38 @@ RSpec.describe InvoiceItem, type: :model do
     it { should validate_presence_of :status }
     it { should validate_presence_of :item_id }
     it { should validate_presence_of :invoice_id }
+  end
+  before :each do
+    @merchant1 = Merchant.create!(name: 'Marvel')
+    @merchant2 = Merchant.create!(name: 'Honey Bee', status: 'enabled')
+
+    @customer1 = Customer.create!(first_name: 'Peter', last_name: 'Parker')
+
+    @item1 = Item.create!(name: 'Beanie Babies', description: 'Investments', unit_price: 100,
+                          merchant_id: @merchant1.id)
+    @item2 = Item.create!(name: 'Bat-A-Rangs', description: 'Weapons', unit_price: 100, merchant_id: @merchant1.id)
+    @item3 = Item.create!(name: 'Bat Mask', description: 'Identity Protection', unit_price: 800,
+                          merchant_id: @merchant2.id)
+
+    @invoice1 = Invoice.create!(status: 'completed', customer_id: @customer1.id, created_at: Time.parse('19.07.18'))
+    @invoice2 = Invoice.create!(status: 'completed', customer_id: @customer1.id, created_at: '2010-03-11 01:51:45')
+
+    @ii1 = InvoiceItem.create!(quantity: 5, unit_price: 100, status: 'packaged', item_id: @item1.id, invoice_id: @invoice1.id)
+    @ii2 = InvoiceItem.create!(quantity: 15, unit_price: 100, status: 'packaged', item_id: @item2.id, invoice_id: @invoice1.id)
+    @ii3 = InvoiceItem.create!(quantity: 50, unit_price: 800, status: 'shipped', item_id: @item3.id, invoice_id: @invoice2.id)
+
+    @discount1 = Discount.create!(merchant_id: @merchant1.id, quantity_threshhold: 5, percentage: 0.2, invoice_item_id: @ii1.id)
+    # @discount2 = Discount.create!(merchant_id: @merchant1.id, quantity_threshhold: 15, percentage: 0.4, invoice_item_id: @ii2.id)
+    # @discount2 = Discount.create!(merchant_id: @merchant2.id, quantity_threshhold: 212, percentage: 0.4, invoice_item_id: @ii3.id)
+  end
+  describe 'total_revenue' do
+    it 'joins with merchant and gets total of quantity * unit price' do
+      expect(InvoiceItem.total_revenue(@merchant1.id)).to eq(2000)
+    end
+    describe 'discounted' do
+      it 'totals revenue including discounts' do
+        expect(InvoiceItem.discounted(@merchant1.id)).to eq(400.0)
+      end
+    end
   end
 end
