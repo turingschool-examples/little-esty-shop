c2f0b66d588d7052ad0d6f8341118af04a0412d9
Drew Layton <dlayton66@gmail.com>
2023-01-02 16:45:34 -0700

    Create rake commands

--- db/migrate/20230102223527_create_transactions.rb
+++ db/migrate/20230102223527_create_transactions.rb
@@ -2,7 +2,7 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.string :credit_card_number
-      t.string :credit_card_exp_date
+      t.string :credit_card_expiration_date
       t.integer :result
       t.references :invoice, null: false, foreign_key: true
       t.timestamps
