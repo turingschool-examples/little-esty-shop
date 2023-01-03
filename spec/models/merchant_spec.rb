33e8008f13dd44bad9515a651cecdc1ec0959614
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-01-03 16:15:56 -0700

    Add relationship and validation tests for transactions

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,5 +8,4 @@ RSpec.describe Merchant, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name } 
   end
-
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :invoice }
+  end
+
+  describe 'validations' do
+    # Add length validation to integer?
+    it { should validate_numericality_of(:credit_card_number) }
+  end
 end
