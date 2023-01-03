704cea27efdf7cda9df5428c161fe4a980872eef
Kelsie Porter <krporter22@gmail.com>
2023-01-03 12:17:48 -0700

    Edit status column and add enums to invoice_items

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,3 @@
 class InvoiceItem < ApplicationRecord
-  
+  enum status: {"pending": 0, "packaged": 1, "shipped": 2}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_03_183304) do
+ActiveRecord::Schema.define(version: 2023_01_03_190944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2023_01_03_183304) do
     t.bigint "invoice_id"
     t.integer "quantity"
     t.integer "unit_price"
-    t.string "status"
+    t.integer "status"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
