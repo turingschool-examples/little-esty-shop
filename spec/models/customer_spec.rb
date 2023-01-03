2564fc05eb0eb51ec2f087dbab36abd219aa1275
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-01-02 20:04:29 -0700

    Add tests for Customer model

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+  let!(:customer) { Customer.create!(first_name: "A Name", last_name: "A Last Name") }
+  describe 'the customer model' do
+    it 'exists' do
+      expect(customer).to be_a(Customer)      
+      expect(customer.first_name).to eq("A Name")      
+      expect(customer.last_name).to eq("A Last Name")      
+    end
+  end
+end
