f72cc02f6d1bd985ff0d9043d314c73e798a52aa
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-03 10:26:40 -0800

    Test: admin dashboard hearder

---
+++ spec/features/admin/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'admin index page' do
+  describe 'header' do
+    #     As an admin,
+    # When I visit the admin dashboard (/admin)
+    # Then I see a header indicating that I am on the admin dashboard
+    it 'indicates we are on admin dashboard with header' do
+      visit admin_path
+
+      # add within block
+      expect(page).to have_content("Admin Dashboard")
+    end
+  end
+end
