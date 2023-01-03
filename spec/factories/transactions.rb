4eb993adcd96540856ac58601d1b468af314639c
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-03 08:59:21 -0700

    fix: credit card number length

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :transaction do
-    credit_card_number {Faker::Number.within(range: 400000000000..49999999999)}
+    credit_card_number {Faker::Number.within(range: 4000000000000000..4999999999999999)}
     credit_card_expiration_date {Faker::Date.forward(days: 365)}
     result {[0, 1].sample}
   end
