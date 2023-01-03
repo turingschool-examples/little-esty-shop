d116548b431e34bfac4a3a22b3def6c72c9a60b6
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-03 12:39:00 -0800

    add routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
-  # resources :merchants, only: [:show]
+  resources :merchants, only: [] do
+    resources :items, only: [:index]
+    resources :invoices, only: [:index]
+  end
+
   get '/merchants/:merchant_id/dashboard', to: 'merchants#show'
 end
