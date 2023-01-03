81377b1dc2b2929741e6b51a3a670799aac2445a
Drew Layton <dlayton66@gmail.com>
2023-01-03 14:41:50 -0700

    test: User story 20 nav bar

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -9,5 +9,14 @@ RSpec.describe 'admin index page' do
         expect(page).to have_content('Admin Dashboard')
       end
     end
+
+    it 'has links to admin merchants and admin invoices' do
+      visit admin_index_path
+
+      within('header') do
+        expect(page).to have_link("Merchants", href: admin_merchants_path)
+        expect(page).to have_link("Invoices", href: admin_invoices_path)
+      end
+    end
   end
 end
