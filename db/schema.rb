1cc17a9137a96408ac4fae661c304219708e57fc
Kelsie Porter <krporter22@gmail.com>
2023-01-03 12:07:40 -0700

    Edit status column to enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_03_001115) do
+ActiveRecord::Schema.define(version: 2023_01_03_183304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2023_01_03_001115) do
 
   create_table "invoices", force: :cascade do |t|
     t.bigint "customer_id"
-    t.string "status"
+    t.integer "status"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
