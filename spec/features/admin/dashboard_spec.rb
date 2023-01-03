40cd10b9e7748b961cfb57a5e3bb2682e5452d91
Brady Rohrig <brady.rohrig@gmail.com>
2023-01-03 11:47:33 -0800

    test: wrote test for US 20

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -4,9 +4,13 @@ RSpec.describe 'admin dashboard' do
   it 'has a header for the page name' do
     visit '/admin'
     expect(page).to have_content("Admin Dashboard")
-
   end
 
+  it 'has links to the admin merch and invoices indices' do
+    visit '/admin'
+    expect(page).to have_link 'Merchants Index', href: admin_merchants_path
+    expect(page).to have_link 'Invoices Index', href: admin_invoices_path
+  end
 
 
 
