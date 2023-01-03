b575bb705fe7d3b2c5f92d30a3f5cb000b255686
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-01-03 16:57:25 -0700

    Add validations for Customer model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,11 +4,10 @@ RSpec.describe Customer, type: :model do
   let!(:customer) { Customer.create!(first_name: "A Name", last_name: "A Last Name") }
   describe 'the customer model' do
     it { should have_many :invoices }
-    
-    it 'exists' do
-      expect(customer).to be_a(Customer)      
-      expect(customer.first_name).to eq("A Name")      
-      expect(customer.last_name).to eq("A Last Name")      
-    end
+  end
+  
+  describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
   end
 end
