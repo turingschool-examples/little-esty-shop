280791b988a817a4c29369a8ae807d48c6b8a1e4
Mike Cummins <Cummins.Michael.R@gmail.com>
2023-01-03 14:45:54 -0800

    Complete user story 21

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,7 +3,16 @@ require 'rails_helper'
 RSpec.describe Customer do
   describe 'Relationships' do
     it {should have_many :invoices}
+    it {should have_many :transactions}
     it {should validate_presence_of :first_name}
     it {should validate_presence_of :last_name}
+  end
+
+  it 'Selects the top 5 customers by successful transaction' do
+    expect(Customer.top_5_by_transactions).to eq([Customer.find(12), Customer.find(13), Customer.find(10), Customer.find(7), Customer.find(4)])
+  end
+
+  it 'Counts the number of successful transactions' do
+    expect(Customer.find(12).transactions_count).to eq(9)
   end
 end
