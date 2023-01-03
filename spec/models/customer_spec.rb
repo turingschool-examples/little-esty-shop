c99ca701ddc8b3f66e5997b12fbc9368800ce126
Bryan Flanagan <bflanagan78@yahoo.com>
2023-01-03 07:21:36 -0700

    Add customer and item relationship and associations, tests passing

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   describe 'relationships' do
     it {should have_many(:invoices)}
+    it {should have_many(:transactions).through(:invoices)}
   end
 
   describe 'validations' do
     it {should validate_presence_of(:first_name)}
     it {should validate_presence_of(:last_name)}
-
   end
 end
