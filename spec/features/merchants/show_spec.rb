d832f12a431178c9169d8a43994d3f06de8a93e7
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-03 12:38:43 -0800

    adds test for links

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -13,4 +13,24 @@ RSpec.describe "merchant dashboard" do
     expect(page).to have_content(@merchant_1.name)
     expect(page).to_not have_content(@merchant_2.name)
   end
+
+  it 'will have a link to the merchant item index' do
+    visit "/merchants/#{@merchant_1.id}/dashboard"
+
+    expect(page).to have_link("My Items")
+
+    click_link "My Items"
+
+    expect(current_path).to eq("/merchants/#{@merchant_1.id}/items")
+  end
+
+  it 'will have a link to my merchant invoices index' do
+    visit "/merchants/#{@merchant_1.id}/dashboard"
+
+    expect(page).to have_link("My Invoices")
+
+    click_link "My Invoices"
+
+    expect(current_path).to eq("/merchants/#{@merchant_1.id}/invoices")
+  end
 end
