a07ae11ea1c6ea1054b61a609494b187afe1f574
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-03 15:53:52 -0800

    Rename directories

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -20,11 +20,11 @@ RSpec.describe 'merchant show' do
 
     @invoice4 = Invoice.create!(status: 1, customer_id: @customer2.id)
 
-    @transaction1 = Transaction.create!(credit_card_number: '123456789', credit_card_exp_date: '05/07', invoice_id: @invoice1.id)
-    @transaction2 = Transaction.create!(credit_card_number: '987654321', credit_card_exp_date: '04/07', invoice_id: @invoice2.id)
-    @transaction3 = Transaction.create!(credit_card_number: '543219876', credit_card_exp_date: '03/07', invoice_id: @invoice3.id)
+    @transaction1 = Transaction.create!(credit_card_number: '123456789', credit_card_expiration_date: '05/07', invoice_id: @invoice1.id)
+    @transaction2 = Transaction.create!(credit_card_number: '987654321', credit_card_expiration_date: '04/07', invoice_id: @invoice2.id)
+    @transaction3 = Transaction.create!(credit_card_number: '543219876', credit_card_expiration_date: '03/07', invoice_id: @invoice3.id)
 
-    @transaction4 = Transaction.create!(credit_card_number: '121987654', credit_card_exp_date: '02/07', invoice_id: @invoice4.id)
+    @transaction4 = Transaction.create!(credit_card_number: '121987654', credit_card_expiration_date: '02/07', invoice_id: @invoice4.id)
 
     @ii1 = InvoiceItem.create!(quantity: 5, unit_price: @item1.unit_price, item_id: @item1.id, invoice_id: @invoice1.id)
     @ii2 = InvoiceItem.create!(quantity: 5, unit_price: @item2.unit_price, item_id: @item2.id, invoice_id: @invoice2.id)
