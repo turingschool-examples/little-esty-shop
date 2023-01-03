623ef648dff4f55d30cce2a7f8a62073caf32921
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-03 10:42:15 -0800

    Test: admin dashboard header

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe 'admin index page' do
     # When I visit the admin dashboard (/admin)
     # Then I see a header indicating that I am on the admin dashboard
     it 'indicates we are on admin dashboard with header' do
-      visit admin_path
+      visit admin_index_path
 
       # add within block
+      expect(page).to have_content("Admin Dashboard")
+      visit admin_merchant_index_path
+      expect(page).to have_content("Admin Dashboard")
+      visit admin_invoice_index_path
       expect(page).to have_content("Admin Dashboard")
     end
   end
