7e27cd8c7ab16c61d8c098e927b084f41fe53396
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-01-03 15:57:08 -0700

    Add relationship and validation tests for items and merchants

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  validates_presence_of :name, :description, :unit_price
+  validates_numericality_of :unit_price, only_integer: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :merchant }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :unit_price }
+    it { should validate_numericality_of(:unit_price).only_integer }
+  end
 end
