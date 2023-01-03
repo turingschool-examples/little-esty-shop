df7b5349e5903b70c0bae99c52aaba24fb1e0eaa
Leo Banos Garcia <bsleo625@gmail.com>
2023-01-03 14:18:32 -0700

    Adds spec files for relationships merchants, items, invoice items.

---
+++ spec/models/items_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  describe 'relationships' do
+    it {should belong_to :merchant}
+    it {should have_many :invoice_items}
+  end
+end
