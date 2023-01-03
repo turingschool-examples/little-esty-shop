ec0df605baeaf309b6da1646d1663c2db5405b2d
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-03 13:02:40 -0800

    Test: update test

--- spec/features/merchant/item/index_spec.rb
+++ spec/features/merchant/item/index_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe 'merchant items index page' do
     mariah = Merchant.create!(name: "Mariah Ahmed")
     terry = Merchant.create!(name: "Terry Peeples")
     
-    pen = mariah.Item.create!(name: "pen", description: "writes stuff", unit_price: 33)
-    marker = mariah.Item.create!(name: "marker", description: "writes stuff", unit_price: 23)
-    pencil = mariah.Item.create!(name: "pencil", description: "writes stuff", unit_price: 13)
+    pen = mariah.Items.create!(name: "pen", description: "writes stuff", unit_price: 33)
+    marker = mariah.Items.create!(name: "marker", description: "writes stuff", unit_price: 23)
+    pencil = mariah.Items.create!(name: "pencil", description: "writes stuff", unit_price: 13)
 
-    socks = terry.create!(name: "socks", description: "keeps feet warm", unit_price: 8)
-    shoes = terry.create!(name: "shoes", description: "provides arch support", unit_price: 68)
+    socks = terry.Items.create!(name: "socks", description: "keeps feet warm", unit_price: 8)
+    shoes = terry.Items.create!(name: "shoes", description: "provides arch support", unit_price: 68)
 
     visit merchant_items_index_path(mariah)
 
