754a854c1d3f8b7bb3e218e2540dbcc1bef31516
Ryan Canton <ryancanton999@gmail.com>
2023-01-04 14:24:25 -0700

    Accept incoming changes from remote repo

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -1,6 +1,3 @@
-# As a merchant,
-# When I visit my merchant dashboard (/merchants/merchant_id/dashboard)
-# Then I see the name of my merchant
 require 'rails_helper'
 
 RSpec.describe "The Merchant Dashboard" do
@@ -25,6 +22,12 @@ RSpec.describe "The Merchant Dashboard" do
     visit "/merchants/2/dashboard"
     # save_and_open_page
     expect(page).to have_content("Favorite Customers\nIlene Pfannerstill - 9 Purchases Ramona Reynolds - 8 Purchases Joey Ondricka - 7 Purchases Mariah Toy - 3 Purchases Logan Kris - 3 Purchases")
-
   end
+  # As a merchant
+  # When I visit my merchant dashboard
+  # Then I see a section for "Items Ready to Ship"
+  # In that section I see a list of the names of all of my items that
+  # have been ordered and have not yet been shipped,
+  # And next to each Item I see the id of the invoice that ordered my item
+  # And each invoice id is a link to my merchant's invoice show page
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -7,16 +7,6 @@ RSpec.describe Customer do
     it {should validate_presence_of :first_name}
     it {should validate_presence_of :last_name}
   end
-<<<<<<< HEAD
-=======
-
-
-  describe '#transactions_with_merchant()' do
-    it 'returns the number of successful transactions with a given merchant' do
-      cust1 = Customer.find(1)
-      # require 'pry'; binding.pry
-      expect(cust1.transactions_with_merchant(2)).to eq(5)
-    end
     
   it 'Selects the top 5 customers by successful transaction' do
     expect(Customer.top_5_by_transactions).to eq([Customer.find(12), Customer.find(13), Customer.find(10), Customer.find(7), Customer.find(4)])
@@ -24,7 +14,5 @@ RSpec.describe Customer do
 
   it 'Counts the number of successful transactions' do
     expect(Customer.find(12).transactions_count).to eq(9)
-
   end
->>>>>>> 57fd61a4eb59e1f16ff758251b924710b5d65fd0
 end
