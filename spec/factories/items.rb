e4d7106f6041b7d84f592a59070234947e8a4008
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-04 09:12:34 -0700

    test: show invoice invoice items

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :item do
-    name {Faker::Name.name}
+    name {Faker::Food.dish}
     unit_price {Faker::Number.within(range: 1000..100000)}
     description {Faker::Quote.famous_last_words}
   end
--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -7,11 +7,27 @@ RSpec.describe 'the admin show page' do
     @customer_3 = create(:customer)
     @customer_4 = create(:customer)
     @customer_5 = create(:customer)
+
     @invoice_1 = create(:invoice, customer: @customer_1)
     @invoice_2 = create(:invoice, customer: @customer_1)
     @invoice_3 = create(:invoice, customer: @customer_2)
     @invoice_4 = create(:invoice, customer: @customer_5)
     @invoice_5 = create(:invoice, customer: @customer_5)
+
+    @merchant_1 = create(:merchant)
+    @merchant_2 = create(:merchant)
+    @merchant_3 = create(:merchant)
+    @merchant_4 = create(:merchant)
+
+    @item_1 = create(:item, merchant: @merchant_1)
+    @item_2 = create(:item, merchant: @merchant_1)
+    @item_3 = create(:item, merchant: @merchant_1)
+    @item_4 = create(:item, merchant: @merchant_1)
+
+    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1)
+    @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_1)
+    @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice_1)
+    @invoice_item_4 = create(:invoice_item, item: @item_4, invoice: @invoice_1)
   end
 
   describe 'As an admin, When I visit an admin invoice show page' do
@@ -23,6 +39,16 @@ RSpec.describe 'the admin show page' do
       expect(page).to have_content(@invoice_1.created_at.to_formatted_s(:admin_invoice_date))
       expect(page).to have_content(@invoice_1.customer.first_name)
       expect(page).to have_content(@invoice_1.customer.last_name)
+    end
+
+    it 'shows all of the items on the invoice' do
+      visit admin_invoice_path(@invoice_1)
+
+      expect(page).to have_content(@invoice_1.id)
+      expect(page).to have_content(@item_1.name)
+      expect(page).to have_content(@invoice_item_1.quantity)
+      expect(page).to have_content(@invoice_item_1.unit_price)
+      expect(page).to have_content(@invoice_item_1.status)
     end
   end
 end
