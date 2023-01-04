337d5ea1fdf5af10ee8080062394980c228ea1b3
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-04 14:08:09 -0800

    Test/feat: invoice created at date format

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -40,10 +40,8 @@ RSpec.describe 'Merchant invoice show page' do
     
     expect(page).to have_content(@invoice1.id)
     expect(page).to have_content(@invoice1.status)
-    expect(page).to have_content(@invoice1.created_at)
-    # test date format
+    expect(page).to have_content(@invoice1.created_at.strftime("%A, %B%e, %Y"))
     expect(page).to have_content(@customer1.first_name)
     expect(page).to have_content(@customer1.last_name)
-    
   end
 end
