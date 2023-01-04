e717ea92e0cd7ded9ded3547fd3b878e607fc760
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-03 16:21:01 -0800

    Test: merchant invoices relationship

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Merchant do
   describe "Relationships" do
     it {should have_many :items} 
+    it { should have_many(:invoices).through(:items) }
   end
 end
