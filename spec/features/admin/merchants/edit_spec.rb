dfadc2daf124f749845d4ba2e75f3bed4c0b057e
Samuel Cox <samc1253@gmail.com>
2023-01-04 15:05:08 -0700

    Add change to testing to rever to previous name

--- spec/features/admin/merchants/edit_spec.rb
+++ spec/features/admin/merchants/edit_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Admin Merchant Edit' do
   it 'Shows a form with information pre-filled' do
+    current_name = Merchant.all.first.name
     visit admin_merchant_path(Merchant.all.first)
     click_on('Edit this merchant')
     
@@ -13,5 +14,6 @@ RSpec.describe 'Admin Merchant Edit' do
     expect(current_path).to eq(admin_merchant_path(Merchant.all.first))
     expect(page).to have_content("Jimbob's Carpentry")
     expect(page).to have_content("Merchant name has been changed")
+    Merchant.all.first.update({name: current_name})
   end
 end
