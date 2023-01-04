a0ff245ed61c6d178e3392ae2d15bd342bb80f93
Kyle Ledin <Kyle.Ledin85@gmail.com>
2023-01-04 16:27:21 -0600

    Have the first test passing for story 7

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -2,4 +2,8 @@ class MerchantItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+  
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+  end 
 end
