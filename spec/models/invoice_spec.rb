8f0e61acede342fd4b8f8a9b8c8ad72f1cf3cffa
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-04 09:18:35 -0700

    test: method invoice invoice item

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -14,6 +14,9 @@ RSpec.describe Invoice, type: :model do
     before(:each) do
       @customer_1 = create(:customer)
       @invoice_1 = create(:invoice, customer: @customer_1)
+      @merchant_1 = create(:merchant)
+      @item_1 = create(:item, merchant: @merchant_1)
+      @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1)
     end
 
     describe '#format_date_long' do
@@ -21,6 +24,14 @@ RSpec.describe Invoice, type: :model do
         expected = "Wednesday, January 04, 2023"
         
         expect("2023-01-04 16:29:30 +0000".to_datetime.to_formatted_s(:admin_invoice_date)).to eq(expected)
+      end
+    end
+
+    describe '#invoice_item' do
+      it 'returns the invoice item associated with the item specified' do
+        expect(@invoice_1.invoice_item(@item_1).quantity).to eq(@invoice_item_1.quantity)
+        expect(@invoice_1.invoice_item(@item_1).unit_price).to eq(@invoice_item_1.unit_price)
+        expect(@invoice_1.invoice_item(@item_1).status).to eq(@invoice_item_1.status)
       end
     end
   end
