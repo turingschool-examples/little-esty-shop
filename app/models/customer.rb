d79f957b6beeff93d91a49956bf6c3d5d67a4723
Brady Rohrig <brady.rohrig@gmail.com>
2023-01-03 20:07:46 -0800

    feat: completed customer method to find top 5 by successful transactions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,12 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
 
   validates_presence_of :first_name, :last_name
+
+  def self.top_5_transactions
+    self.joins(:transactions)
+        .group("customers.id")
+        .where(transactions: { result: "success" } )
+        .order("count(transactions.id) desc")
+        .limit(5)
+  end
 end
