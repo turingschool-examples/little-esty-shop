ec52c6be0e86ba82f9794cb8f45d5182d4054c5d
Mike Cummins <Cummins.Michael.R@gmail.com>
2023-01-04 10:57:52 -0800

    Complete user story 25

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe 'Admin Merchants index' do
     expect(page).to have_content(Merchant.all.fifth.name)
     expect(page).to have_content(Merchant.all.last.name)
   end
+
+  it 'Has links to each merchant show page' do
+    visit admin_merchants_path
+
+    click_on(Merchant.all.first.name)
+
+    expect(page).to have_content(Merchant.all.first.name)
+    expect(page).to_not have_content(Merchant.all.last.name)
+    expect(current_path).to eq("/admin/merchants/#{Merchant.all.first.id}")
+  end
 end
