e5159245e503e8c6e4828923a6ad681ae51acf51
William Lampke <gravitybangers@gmail.com>
2023-01-03 20:13:36 -0500

    wip

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,17 @@
 class Merchant < ApplicationRecord 
   has_many :items
-  
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
+
+  def self.top5(id)
+    binding.pry
+    joins(:customers, :transactions)
+    .where("transactions.result = 0 AND merchants.id = #{id}")
+    .select('customers.*, count(transactions) as success_count')
+    .order(success_count: :desc)
+  end
 end
+
+# customers names, where Transaction
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -20,11 +20,11 @@ RSpec.describe 'merchant show' do
 
     @invoice4 = Invoice.create!(status: 1, customer_id: @customer2.id)
 
-    @transaction1 = Transaction.create!(credit_card_number: '123456789', credit_card_exp_date: '05/07', invoice_id: @invoice1.id)
-    @transaction2 = Transaction.create!(credit_card_number: '987654321', credit_card_exp_date: '04/07', invoice_id: @invoice2.id)
-    @transaction3 = Transaction.create!(credit_card_number: '543219876', credit_card_exp_date: '03/07', invoice_id: @invoice3.id)
+    @transaction1 = Transaction.create!(credit_card_number: '123456789', credit_card_expiration_date: '05/07', invoice_id: @invoice1.id)
+    @transaction2 = Transaction.create!(credit_card_number: '987654321', credit_card_expiration_date: '04/07', invoice_id: @invoice2.id)
+    @transaction3 = Transaction.create!(credit_card_number: '543219876', credit_card_expiration_date: '03/07', invoice_id: @invoice3.id)
 
-    @transaction4 = Transaction.create!(credit_card_number: '121987654', credit_card_exp_date: '02/07', invoice_id: @invoice4.id)
+    @transaction4 = Transaction.create!(credit_card_number: '121987654', credit_card_expiration_date: '02/07', invoice_id: @invoice4.id)
 
     @ii1 = InvoiceItem.create!(quantity: 5, unit_price: @item1.unit_price, item_id: @item1.id, invoice_id: @invoice1.id)
     @ii2 = InvoiceItem.create!(quantity: 5, unit_price: @item2.unit_price, item_id: @item2.id, invoice_id: @invoice2.id)
@@ -59,6 +59,15 @@ RSpec.describe 'merchant show' do
       
       expect(page).to have_link("#{@merchant1.name} invoices")
       
+    end
+  end
+  describe 'story 3' do
+    it "When I visit my merchant dashboard
+    Then I see the names of the top 5 customers
+    who have conducted the largest number of successful transactions with my merchant
+    And next to each customer name I see the number of successful transactions they have
+    conducted with my merchant" do
+    visit "merchants/#{@merchant1.id}/dashboards"
     end
   end
 end
