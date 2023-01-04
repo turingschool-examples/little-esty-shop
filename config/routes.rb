291bccca81d18047380e8fc103b89933e2868c35
Anhnhi Tran <anhtran811@gmail.com>
2023-01-04 11:41:37 -0700

    Fixed routing to be correct

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   #merchants
-  # get '/merchants/:id/dashboard', to: 'merchants#show'
-  resources :merchants, only: [:show] do 
-    resources :dashboard, controller: "merchants"
-    resources :items
-    resources :invoices
+  get '/merchants/:id/dashboard', to: 'merchants#show'
+  # resources :merchants, only: [:show] do 
+  #   resources :dashboard, controller: "merchants"
+  #   resources :items
+  #   resources :invoices
+  # end
+
+  resources :merchants do
+    resources :items, only: [:index]
+    resources :invoices, only: [:index]
   end
 end
