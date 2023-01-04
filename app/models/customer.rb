f3f30ba7bed4082308685a4efe1b2ac755a741ca
Matisse Mallette <matissemallette@gmail.com>
2023-01-04 15:35:53 -0700

    Adds a method to find top 5 customers by # of transactions, refactored 'before :each' block

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,11 @@
 class Customer < ApplicationRecord
-
   has_many :invoices, dependent: :destroy
   has_many :transactions, through: :invoices, dependent: :destroy
   has_many :invoice_items, through: :invoices, dependent: :destroy
   has_many :items, through: :invoice_items, dependent: :destroy
   has_many :merchants, through: :items, dependent: :destroy
+
+  def self.top_5_customers_by_successful_transactions
+    joins(:transactions).where(transactions: {result: "success"}).select("customers.*, count(result) as count_result").group(:id).order("count_result desc").limit(5)
+  end
 end
--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -30,56 +30,121 @@ RSpec.describe "Admin Dashboard(Index)" do
 
     @invoice_1 = @customer_1.invoices.create!(status: 'cancelled')
     @invoice_2 = @customer_1.invoices.create!(status: 'cancelled')
+
     @invoice_3 = @customer_2.invoices.create!(status: 'completed')
     @invoice_4 = @customer_2.invoices.create!(status: 'in progress')
-    @invoice_5 = @customer_2.invoices.create!(status: 'cancelled')
+
+    @invoice_5 = @customer_3.invoices.create!(status: 'cancelled')
     @invoice_6 = @customer_3.invoices.create!(status: 'in progress')
-    @invoice_7 = @customer_3.invoices.create!(status: 'in progress')
-    @invoice_8 = @customer_3.invoices.create!(status: 'cancelled')
-    @invoice_9 = @customer_3.invoices.create!(status: 'completed')
-    @invoice_10 = @customer_3.invoices.create!(status: 'completed')
-    @invoice_13 = @customer_3.invoices.create!(status: 'in progress')
-    @invoice_12 = @customer_4.invoices.create!(status: 'completed')
+
+    @invoice_7 = @customer_4.invoices.create!(status: 'in progress')
+    @invoice_8 = @customer_4.invoices.create!(status: 'cancelled')
+
+    @invoice_9 = @customer_5.invoices.create!(status: 'completed')
+    @invoice_10 = @customer_5.invoices.create!(status: 'completed')
+
+    @invoice_11 = @customer_6.invoices.create!(status: 'in progress')
+    @invoice_12 = @customer_6.invoices.create!(status: 'completed')
 
     #do i have to calculate the actual unit_price?
-    InvoiceItem.create!(invoice_id: @invoice_1.id  item_id: @item_1.id, quantity: 5, unit_price: 13635, status: 'packaged')
-    InvoiceItem.create!(invoice_id: @invoice_1.id  item_id: @item_2.id, quantity: 9, unit_price: 23324, status: 'pending')
-    InvoiceItem.create!(invoice_id: @invoice_2.id  item_id: @item_2.id, quantity: 12, unit_price: 34873, status: 'packaged')
-    InvoiceItem.create!(invoice_id: @invoice_2.id  item_id: @item_4.id, quantity: 8, unit_price: 2196, status: 'pending')
-    InvoiceItem.create!(invoice_id: @invoice_2.id  item_id: @item_5.id, quantity: 3, unit_price: 79140, status: 'packaged')
-    InvoiceItem.create!(invoice_id: @invoice_2.id  item_id: @item_1.id, quantity: 9, unit_price: 52100, status: 'shipped')
-    InvoiceItem.create!(invoice_id: @invoice_3.id  item_id: @item_7.id, quantity: 10, unit_price: 66747, status: 'shipped')
-    InvoiceItem.create!(invoice_id: @invoice_3.id  item_id: @item_8.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_1.id,  item_id: @item_1.id, quantity: 5, unit_price: 13635, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 9, unit_price: 23324, status: 'pending')
+    InvoiceItem.create!(invoice_id: @invoice_2.id,  item_id: @item_2.id, quantity: 12, unit_price: 34873, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_2.id,  item_id: @item_4.id, quantity: 8, unit_price: 2196, status: 'pending')
+    InvoiceItem.create!(invoice_id: @invoice_2.id,  item_id: @item_5.id, quantity: 3, unit_price: 79140, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_2.id,  item_id: @item_1.id, quantity: 9, unit_price: 52100, status: 'shipped')
+    InvoiceItem.create!(invoice_id: @invoice_3.id,  item_id: @item_7.id, quantity: 10, unit_price: 66747, status: 'shipped')
+    InvoiceItem.create!(invoice_id: @invoice_3.id,  item_id: @item_8.id, quantity: 9, unit_price: 76941, status: 'packaged')
 
     @transaction_1 = @invoice_1.transactions.create!(credit_card_number: '4654405418249632', credit_card_expiration_date: '04/22/20', result: 'success')
-    @transaction_2 = @invoice_2.transactions.create!(credit_card_number: '4580251236515201', credit_card_expiration_date: '03/22/20', result: 'failed')
-    @transaction_3 = @invoice_3.transactions.create!(credit_card_number: '4354495077693036', credit_card_expiration_date: '09/22/20', result: 'success')
-    @transaction_4 = @invoice_4.transactions.create!(credit_card_number: '4515551623735607', credit_card_expiration_date: '08/22/20', result: 'success')
-    @transaction_5 = @invoice_5.transactions.create!(credit_card_number: '4844518708741275', credit_card_expiration_date: '10/22/20', result: 'success')
-    @transaction_6 = @invoice_6.transactions.create!(credit_card_number: '4203696133194408', credit_card_expiration_date: '02/22/20', result: 'failed')
-    @transaction_7 = @invoice_7.transactions.create!(credit_card_number: '4801647818676136', credit_card_expiration_date: '01/22/20', result: 'failed')
-    @transaction_8 = @invoice_8.transactions.create!(credit_card_number: '4540842003561938', credit_card_expiration_date: '09/22/20', result: 'success')
-    @transaction_9 = @invoice_9.transactions.create!(credit_card_number: '4140149827486249', credit_card_expiration_date: '10/22/20', result: 'failed')
-    @transaction_10 = @invoice10.transactions.create!(credit_card_number: '4923661117104166', credit_card_expiration_date: '08/22/20', result: 'success')
+    @transaction_2 = @invoice_1.transactions.create!(credit_card_number: '4654405418249632', credit_card_expiration_date: '04/22/20', result: 'failed')
+    @transaction_3 = @invoice_1.transactions.create!(credit_card_number: '4654405418249632', credit_card_expiration_date: '04/22/20', result: 'success')
+
+    @transaction_4 = @invoice_2.transactions.create!(credit_card_number: '4580251236515201', credit_card_expiration_date: '03/22/20', result: 'failed')
+    @transaction_5 = @invoice_2.transactions.create!(credit_card_number: '4580251236515201', credit_card_expiration_date: '03/22/20', result: 'failed')
+    @transaction_6 = @invoice_2.transactions.create!(credit_card_number: '4580251236515201', credit_card_expiration_date: '03/22/20', result: 'failed')
+
+    @transaction_7 = @invoice_3.transactions.create!(credit_card_number: '4354495077693036', credit_card_expiration_date: '09/22/20', result: 'failed')
+    @transaction_8 = @invoice_3.transactions.create!(credit_card_number: '4354495077693036', credit_card_expiration_date: '09/22/20', result: 'success')
+    @transaction_9 = @invoice_3.transactions.create!(credit_card_number: '4354495077693036', credit_card_expiration_date: '09/22/20', result: 'success')
+
+    @transaction_10 = @invoice_4.transactions.create!(credit_card_number: '4515551623735607', credit_card_expiration_date: '08/22/20', result: 'success')
+    @transaction_11 = @invoice_4.transactions.create!(credit_card_number: '4515551623735607', credit_card_expiration_date: '08/22/20', result: 'success')
+    @transaction_12 = @invoice_4.transactions.create!(credit_card_number: '4515551623735607', credit_card_expiration_date: '08/22/20', result: 'success')
+
+    @transaction_13 = @invoice_5.transactions.create!(credit_card_number: '4844518708741275', credit_card_expiration_date: '10/22/20', result: 'success')
+    @transaction_14 = @invoice_5.transactions.create!(credit_card_number: '4844518708741275', credit_card_expiration_date: '10/22/20', result: 'success')
+    @transaction_15 = @invoice_5.transactions.create!(credit_card_number: '4844518708741275', credit_card_expiration_date: '10/22/20', result: 'success')
+
+    @transaction_16 = @invoice_6.transactions.create!(credit_card_number: '4203696133194408', credit_card_expiration_date: '02/22/20', result: 'failed')
+    @transaction_17 = @invoice_6.transactions.create!(credit_card_number: '4203696133194408', credit_card_expiration_date: '02/22/20', result: 'failed')
+    @transaction_18 = @invoice_6.transactions.create!(credit_card_number: '4203696133194408', credit_card_expiration_date: '02/22/20', result: 'success')
+
+    @transaction_19 = @invoice_7.transactions.create!(credit_card_number: '4801647818676136', credit_card_expiration_date: '01/22/20', result: 'failed')
+    @transaction_20 = @invoice_7.transactions.create!(credit_card_number: '4801647818676136', credit_card_expiration_date: '01/22/20', result: 'failed')
+    @transaction_21 = @invoice_7.transactions.create!(credit_card_number: '4801647818676136', credit_card_expiration_date: '01/22/20', result: 'failed')
+
+    @transaction_22 = @invoice_8.transactions.create!(credit_card_number: '4540842003561938', credit_card_expiration_date: '09/22/20', result: 'success')
+    @transaction_23 = @invoice_8.transactions.create!(credit_card_number: '4540842003561938', credit_card_expiration_date: '09/22/20', result: 'failed')
+    @transaction_24 = @invoice_8.transactions.create!(credit_card_number: '4540842003561938', credit_card_expiration_date: '09/22/20', result: 'failed')
+
+    @transaction_25 = @invoice_9.transactions.create!(credit_card_number: '4140149827486249', credit_card_expiration_date: '10/22/20', result: 'failed')
+    @transaction_26 = @invoice_9.transactions.create!(credit_card_number: '4140149827486249', credit_card_expiration_date: '10/22/20', result: 'failed')
+    @transaction_27 = @invoice_9.transactions.create!(credit_card_number: '4140149827486249', credit_card_expiration_date: '10/22/20', result: 'failed')
+
+    @transaction_28 = @invoice_10.transactions.create!(credit_card_number: '4923661117104166', credit_card_expiration_date: '08/22/20', result: 'success')
+    @transaction_29 = @invoice_10.transactions.create!(credit_card_number: '4923661117104166', credit_card_expiration_date: '08/22/20', result: 'success')
+    @transaction_30 = @invoice_10.transactions.create!(credit_card_number: '4923661117104166', credit_card_expiration_date: '08/22/20', result: 'success')
+
+    @transaction_31 = @invoice_11.transactions.create!(credit_card_number: '4923661117104166', credit_card_expiration_date: '08/22/20', result: 'success')
+    @transaction_32 = @invoice_11.transactions.create!(credit_card_number: '4923661117104166', credit_card_expiration_date: '08/22/20', result: 'success')
+    @transaction_33 = @invoice_11.transactions.create!(credit_card_number: '4923661117104166', credit_card_expiration_date: '08/22/20', result: 'success')
+
+    @transaction_34 = @invoice_12.transactions.create!(credit_card_number: '4923661117104166', credit_card_expiration_date: '08/22/20', result: 'success')
+    @transaction_35 = @invoice_12.transactions.create!(credit_card_number: '4923661117104166', credit_card_expiration_date: '08/22/20', result: 'success')
+    @transaction_36 = @invoice_12.transactions.create!(credit_card_number: '4923661117104166', credit_card_expiration_date: '08/22/20', result: 'success')
 
   end
+
   describe "visiting admin dashboard" do
-    it 'see a header indicating that user is on admin dashboard'do
-      visit admin_index_path
 
-      expect(page).to have_content("Admin Dashboard")
+    describe "user story 19" do 
+      it 'see a header indicating that user is on admin dashboard'do
+        visit admin_index_path
+
+        expect(page).to have_content("Admin Dashboard")
+      end
+    end 
+
+    describe "user story 20" do 
+      it 'see a link to the admin merchants index' do
+        visit '/admin'
+
+        expect(page).to have_link("Merchants")
+      end
+
+      it 'see a link to the admin invoices index' do
+        visit '/admin'
+
+        expect(page).to have_link("Invoices")
+      end
     end
 
-    it 'see a link to the admin merchants index' do
-      visit '/admin'
+    describe "user story 21" do 
+      it 'displays a list of the top 5 customers who have conducted the largest number of successful transactions' do 
+        visit '/admin'
 
-      expect(page).to have_link("Merchants")
-    end
-
-    it 'see a link to the admin invoices index' do
-      visit '/admin'
-
-      expect(page).to have_link("Invoices")
+        within('#top_customers') do
+          expect("Heber").to appear_before("Cecelia")
+          expect("Cecelia").to appear_before("Mariah")
+          expect("Mariah").to appear_before("Sylvester")
+          expect("Sylvester").to appear_before("Joey")
+          expect("Joey").to_not appear_before("Heber")
+          expect(page).to_not have_content("Leanna")
+        end
+        
+        binding.pry
+      end
     end
   end
 end
