4bcb0261ebe4fd086f4d2d05082a12363841f857
Brady Rohrig <brady.rohrig@gmail.com>
2023-01-03 20:06:47 -0800

    test: added test for top 5 customers by transaction method

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
+
+  before(:all) do
+    Rails.application.load_seed
+  end
+
   describe 'relationships' do
     it {should have_many(:invoices)}
     it {should have_many(:transactions).through(:invoices)}
@@ -9,5 +14,11 @@ RSpec.describe Customer, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:first_name)}
     it {should validate_presence_of(:last_name)}
+  end
+
+  it 'has a method to find the top 5 customers by successful invoices' do
+    result = [Customer.third, Customer.fifth, Customer.second, Customer.fourth, Customer.last]
+    expect(Customer.top_5_transactions).to eq(result)
+
   end
 end
