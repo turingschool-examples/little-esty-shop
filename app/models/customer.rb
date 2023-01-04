5249effe3722c9941d137876c86ecb39020862d5
Anhnhi Tran <anhtran811@gmail.com>
2023-01-04 11:42:17 -0700

    Starts US 3 with objects created

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,4 @@
 class Customer < ApplicationRecord
-
   has_many :invoices, dependent: :destroy
   has_many :transactions, through: :invoices, dependent: :destroy
   has_many :invoice_items, through: :invoices, dependent: :destroy
