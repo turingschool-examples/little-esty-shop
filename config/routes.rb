8fa67bf1c342830ff6d99ccbc08754dcc3946205
Braxton Eddings <beddings81@gmail.com>
2023-01-05 12:31:28 -0600

    Pass test fo rlink to update merchant

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :admin, only: [:index]
 
   namespace :admin do
-    resources :merchants, only: [:index, :show]
+    resources :merchants, only: [:index, :show, :edit]
     resources :invoices, only: [:index, :show]
   end
   
