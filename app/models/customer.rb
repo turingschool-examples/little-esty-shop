f410313a0c97c84b33b689ee4ebd3cc9f3366ca7
Anhnhi Tran <anhtran811@gmail.com>
2023-01-04 21:03:02 -0700

    US 3 merchant top customer model method complete with views passing

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,24 +1,15 @@
 class Customer < ApplicationRecord
   has_many :invoices, dependent: :destroy
   has_many :transactions, through: :invoices, dependent: :destroy
-  # has_many :invoice_items, through: :invoices, dependent: :destroy
-  has_many :items, through: :invoices, dependent: :destroy
-  has_many :merchants, through: :invoices, dependent: :destroy
-  # has_many :merchants, through: :items, dependent: :destroy
+  has_many :invoice_items, through: :invoices, dependent: :destroy
+  has_many :items, through: :invoice_items, dependent: :destroy
+  has_many :merchants, through: :items, dependent: :destroy
 
-  def self.top_customers
-      # joins(:invoices, :transactions)
-      #       .where(transactions: { result: "success"})
-      #       .select(:id, :first_name, :last_name, 'count(result) as num_transactions' )
-      #       .group(:id)
-      #       .order("num_transactions desc")
-      #       .limit(5)
-
-
-      joins(:invoices, :transactions)
-        .where(transactions: { result: "success"})
-        .group(:id).select('customers.*, count(result) as num_transactions')
-        .order(num_transactions: :desc)
-        .limit(5)
+  def self.merchant_top_customers
+    joins(:invoices, :transactions)
+      .where(transactions: { result: "success"})
+      .group(:id).select('customers.*, count(result) as num_transactions')
+      .order(num_transactions: :desc)
+      .limit(5)
   end
 end
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -8,14 +8,6 @@ RSpec.describe 'Merchant Dashboard' do
 
     @item_1 = @merchant_1.items.create!(name: 'Qui Esse', description: 'Nihil autem sit odio inventore deleniti', unit_price: 75107)
     @item_2 = @merchant_1.items.create!(name: 'Autem Minima', description: 'Cumque consequuntur ad', unit_price: 67076)
-    @item_3 = @merchant_1.items.create!(name: 'Ea Voluptatum', description: 'Sunt officia eum qui molestiae', unit_price: 32301)
-    @item_4 = @merchant_1.items.create!(name: 'Nemo Facere', description: 'Sunt eum id eius magni consequuntur delectus veritatis', unit_price: 4291)
-    @item_5 = @merchant_1.items.create!(name: 'Expedita Aliquam', description: 'Voluptate aut labore qui illum tempore eius. Corrupti cum et rerum', unit_price: 68723)
-    @item_6 = @merchant_1.items.create!(name: 'Provident At', description: 'Numquam officiis reprehenderit eum ratione neque tenetur', unit_price: 15925)
-    @item_7 = @merchant_1.items.create!(name: 'Expedita Fuga', description: 'Fuga assumenda occaecati hic dolorem tenetur dolores nisi', unit_price: 31163)
-    @item_8 = @merchant_1.items.create!(name: 'Est Consequuntur', description: 'Reprehenderit est officiis cupiditate quia eos', unit_price: 34355)
-    @item_9 = @merchant_1.items.create!(name: 'Quo Magnam', description: 'Culpa deleniti adipisci voluptates aut. Sed eum quisquam nisi', unit_price: 22582)
-    @item_10 = @merchant_1.items.create!(name: 'Quidem Suscipit', description: 'Reiciendis sed aperiam culpa animi laudantium', unit_price: 34018)
 
     @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Ondricka')
     @customer_2 = Customer.create!(first_name: 'Cecelia', last_name: 'Osinski')
@@ -39,18 +31,38 @@ RSpec.describe 'Merchant Dashboard' do
     @invoice_9 = @customer_3.invoices.create!(status: 'completed')
     @invoice_10 = @customer_6.invoices.create!(status: 'completed')
     @invoice_11 = @customer_5.invoices.create!(status: 'in progress')
-    @invoice_12 = @customer_4.invoices.create!(status: 'completed')
+    @invoice_12 = @customer_3.invoices.create!(status: 'completed')
     @invoice_13 = @customer_4.invoices.create!(status: 'completed')
-    @invoice_14 = @customer_5.invoices.create!(status: 'completed')
+    @invoice_14 = @customer_2.invoices.create!(status: 'completed')
+    @invoice_15 = @customer_3.invoices.create!(status: 'completed')
+    @invoice_16 = @customer_3.invoices.create!(status: 'completed')
+    @invoice_17 = @customer_4.invoices.create!(status: 'completed')
+    @invoice_18 = @customer_4.invoices.create!(status: 'in progress')
+    @invoice_19 = @customer_4.invoices.create!(status: 'in progress')
+    @invoice_20 = @customer_5.invoices.create!(status: 'completed')
+    @invoice_21 = @customer_4.invoices.create!(status: 'in progress')
 
     InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 5, unit_price: 13635, status: 'packaged')
-    InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 9, unit_price: 23324, status: 'pending')
-    InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_2.id, quantity: 12, unit_price: 34873, status: 'packaged')
-    InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_4.id, quantity: 8, unit_price: 2196, status: 'pending')
-    InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_5.id, quantity: 3, unit_price: 79140, status: 'packaged')
-    InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_1.id, quantity: 9, unit_price: 52100, status: 'shipped')
-    InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_7.id, quantity: 10, unit_price: 66747, status: 'shipped')
-    InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_8.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_1.id, quantity: 9, unit_price: 23324, status: 'pending')
+    InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_1.id, quantity: 12, unit_price: 34873, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_4.id, item_id: @item_1.id, quantity: 8, unit_price: 2196, status: 'pending')
+    InvoiceItem.create!(invoice_id: @invoice_5.id, item_id: @item_1.id, quantity: 3, unit_price: 79140, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_6.id, item_id: @item_1.id, quantity: 9, unit_price: 52100, status: 'shipped')
+    InvoiceItem.create!(invoice_id: @invoice_7.id, item_id: @item_1.id, quantity: 10, unit_price: 66747, status: 'shipped')
+    InvoiceItem.create!(invoice_id: @invoice_8.id, item_id: @item_2.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_9.id, item_id: @item_2.id, quantity: 3, unit_price: 79140, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_10.id, item_id: @item_2.id, quantity: 9, unit_price: 52100, status: 'shipped')
+    InvoiceItem.create!(invoice_id: @invoice_11.id, item_id: @item_2.id, quantity: 10, unit_price: 66747, status: 'shipped')
+    InvoiceItem.create!(invoice_id: @invoice_12.id, item_id: @item_2.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_13.id, item_id: @item_2.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_14.id, item_id: @item_2.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_15.id, item_id: @item_2.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_16.id, item_id: @item_2.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_17.id, item_id: @item_2.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_18.id, item_id: @item_2.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_19.id, item_id: @item_2.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_20.id, item_id: @item_2.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_21.id, item_id: @item_2.id, quantity: 9, unit_price: 76941, status: 'packaged')
 
     @transaction_1 = @invoice_1.transactions.create!(credit_card_number: '4654405418249632', credit_card_expiration_date: '04/22/20', result: 'failed')
     @transaction_2 = @invoice_2.transactions.create!(credit_card_number: '4580251236515201', credit_card_expiration_date: '03/22/20', result: 'failed')
@@ -66,8 +78,13 @@ RSpec.describe 'Merchant Dashboard' do
     @transaction_12 = @invoice_12.transactions.create!(credit_card_number: '4017503416578382', credit_card_expiration_date: '08/22/20', result: 'success')
     @transaction_13 = @invoice_13.transactions.create!(credit_card_number: '9856503416578382', credit_card_expiration_date: '08/25/20', result: 'success')
     @transaction_14 = @invoice_14.transactions.create!(credit_card_number: '6565503416578382', credit_card_expiration_date: '08/29/20', result: 'success')
-
-
+    @transaction_15 = @invoice_15.transactions.create!(credit_card_number: '6225503416578382', credit_card_expiration_date: '08/29/20', result: 'success')
+    @transaction_16 = @invoice_16.transactions.create!(credit_card_number: '6965503416578382', credit_card_expiration_date: '08/29/20', result: 'success')
+    @transaction_17 = @invoice_17.transactions.create!(credit_card_number: '6965503416578382', credit_card_expiration_date: '08/29/20', result: 'success')
+    @transaction_18 = @invoice_18.transactions.create!(credit_card_number: '6965503416578382', credit_card_expiration_date: '08/29/20', result: 'success')
+    @transaction_19 = @invoice_19.transactions.create!(credit_card_number: '6965503416578382', credit_card_expiration_date: '08/29/20', result: 'success')
+    @transaction_20 = @invoice_20.transactions.create!(credit_card_number: '6965503416578382', credit_card_expiration_date: '08/29/20', result: 'success')
+    @transaction_21 = @invoice_21.transactions.create!(credit_card_number: '6965503416578382', credit_card_expiration_date: '08/29/20', result: 'success')
   end
   
   # As a merchant,
@@ -138,7 +155,7 @@ RSpec.describe 'Merchant Dashboard' do
       expect(page).to_not have_content(@customer_1.first_name)
       expect(page).to_not have_content(@customer_1.last_name)
     end
-    
+
     it 'displays the number of successful transactions of each customer'
   end
 end
