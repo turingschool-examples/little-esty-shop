c24ca54eba2de3b7eedeba9bf314ccb24eebe01d
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-04 19:10:27 -0800

    Test: merchant packaged items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Merchant do
-  describe "Relationships" do
-    it {should have_many :items} 
+  describe 'Relationships' do
+    it { should have_many :items }
     it { should have_many(:invoices).through(:items) }
+  end
+
+  describe 'instance methods' do
+    it '#packaged_items' do
+      expect(merchant.packaged_items).to eq([item1, item2, item3])
+    end
   end
 end
