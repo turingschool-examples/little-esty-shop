7b29470f51b33636ba5b7b7c68753f7db699d81a
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-04 19:04:49 -0800

    Test: list packaged item names

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -79,10 +79,41 @@ RSpec.describe 'merchant show' do
 
     it 'has section titled Items Ready to Ship' do
       expect(page).to have_css('section#packaged')
-      expect(page).to have_content("Items Ready to Ship")
+      expect(page).to have_content('Items Ready to Ship')
     end
 
-    it 'lists names of all ordered, unshipped items'
+    it 'lists names of all ordered, unshipped items' do
+      @merchant1 = Merchant.create!(name: 'Rays Hand Made Jewlery')
+
+      @item1 = Item.create!(name: 'Chips', description: 'Ring', unit_price: 20, merchant_id: @merchant1.id)
+      @item2 = Item.create!(name: 'darrel', description: 'Bracelet', unit_price: 40, merchant_id: @merchant1.id)
+      @item3 = Item.create!(name: 'don', description: 'Necklace', unit_price: 30, merchant_id: @merchant1.id)
+
+      @customer1 = Customer.create!(first_name: 'Kyle', last_name: 'Ledin')
+      @invoice1 = Invoice.create!(status: 1, customer_id: @customer1.id)
+      @ii1 = InvoiceItem.create!(quantity: 5, unit_price: @item1.unit_price, item_id: @item1.id, invoice_id: @invoice1.id)
+      @ii2 = InvoiceItem.create!(quantity: 5, unit_price: @item2.unit_price, item_id: @item2.id, invoice_id: @invoice1.id)
+      @ii3 = InvoiceItem.create!(quantity: 5, unit_price: @item3.unit_price, item_id: @item3.id, invoice_id: @invoice1.id)
+      # @invoice2 = Invoice.create!(status: 1, customer_id: @customer1.id)
+      
+      # status pending
+      # status packaged
+      # @invoice3 = Invoice.create!(status: 1, customer_id: @customer1.id)
+      # @invoice4 = Invoice.create!(status: 1, customer_id: @customer2.id)
+      # @invoice4 = Invoice.create!(status: 1, customer_id: @customer2.id)
+      # @customer2 = Customer.create!(first_name: 'William', last_name: 'Lampke')
+      # @invoice4 = Invoice.create!(status: 2, customer_id: @customer2.id)
+      # @ii2 = InvoiceItem.create!(quantity: 5, unit_price: @item2.unit_price, item_id: @item2.id, invoice_id: @invoice2.id)
+      # @ii3 = InvoiceItem.create!(quantity: 4, unit_price: @item3.unit_price, item_id: @item3.id, invoice_id: @invoice2.id)
+      # status shipped
+
+      expect(page).to have_content(@item1.name)
+      expect(page).to have_content(@item2.name)
+      expect(page).to have_content(@item3.name)
+
+
+
+    end
     it 'lists invoice id next to item name'
     it 'links to merchants invoice show page from invoice id'
   end
