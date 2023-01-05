9e26dc853cd9a57a29de7661a9052eeae90f04ca
Ryan Canton <ryancanton999@gmail.com>
2023-01-05 10:41:15 -0700

    Refactor: Change path names to use prefixes in merchants/dashboard_spec

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -19,14 +19,14 @@ RSpec.describe "The Merchant Dashboard" do
   end
 
   it 'contains a list of the top five customers based on successful transactions' do
-    visit "/merchants/2/dashboard"
+    visit merchant_dashboard_path(2)
 
     expect(page).to have_content("Favorite Customers\nIlene Pfannerstill - 9 Purchases Ramona Reynolds - 8 Purchases Joey Ondricka - 7 Purchases Mariah Toy - 3 Purchases Logan Kris - 3 Purchases")
   end
 
   describe 'items ready to ship' do
     it 'has a list of items ready to ship including quantity' do
-      visit "/merchants/1/dashboard"
+      visit merchant_dashboard_path(1)
 
       expect(page).to have_content("Items Ready to Ship")
       expect(page).to have_content("Item Quidem Suscipit - Invoice #76 - Wednesday, March 7, 2012 Item Rerum Est - Invoice #3 - Saturday, March 10, 2012 Item Autem Minima - Invoice #38 - Saturday, March 10, 2012 Item Ea Voluptatum - Invoice #38 - Saturday, March 10, 2012 Item Quidem Suscipit - Invoice #2 - Monday, March 12, 2012 Item Ea Voluptatum - Invoice #75 - Monday, March 12, 2012 Item Nemo Facere - Invoice #75 - Monday, March 12, 2012 Item Expedita Aliquam - Invoice #75 - Monday, March 12, 2012 Item Expedita Fuga - Invoice #75 - Monday, March 12, 2012 Item Quo Magnam - Invoice #75 - Monday, March 12, 2012 Item Et Cumque - Invoice #40 - Wednesday, March 14, 2012 Item Voluptatem Sint - Invoice #40 - Wednesday, March 14, 2012 Item Qui Esse - Invoice #1 - Sunday, March 25, 2012 Item Ea Voluptatum - Invoice #1 - Sunday, March 25, 2012 Item Nemo Facere - Invoice #1 - Sunday, March 25, 2012 Item Provident At - Invoice #1 - Sunday, March 25, 2012")
@@ -37,7 +37,7 @@ RSpec.describe "The Merchant Dashboard" do
     end
 
     it 'the list is ordered by oldest to newest and shows date invoice created' do
-      visit "/merchants/1/dashboard"
+      visit merchant_dashboard_path(1)
 
       expect(Item.find(10).name).to appear_before(Item.find(15).name)
       expect(Item.find(15).name).to appear_before(Item.find(2).name)
