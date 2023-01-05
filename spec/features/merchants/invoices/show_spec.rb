1ef9fb1049ae2f33cdf69b6c7c4efa38e32f7662
Bryan Flanagan <bflanagan78@yahoo.com>
2023-01-04 17:33:48 -0700

    feat: add items to merchant invoice show page, complete US 16

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -21,15 +21,16 @@ RSpec.describe 'merchant invoice show' do
     @customer_2 = create(:customer)
 
     @invoice_1 = create(:invoice, customer: @customer_1)
-    @invoice_1.items << @item_1
     @invoice_2 = create(:invoice, customer: @customer_1)
-    @invoice_2.items << @item_2
     @invoice_3 = create(:invoice, customer: @customer_1)
-    @invoice_3.items << @item_3
-    @invoice_3.items << @item_4
     @invoice_4 = create(:invoice, customer: @customer_2)
-    @invoice_4.items << @item_5
-    @invoice_4.items << @item_7
+
+    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1)
+    @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2)
+    @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice_3)
+    @invoice_item_4 = create(:invoice_item, item: @item_4, invoice: @invoice_3) 
+    @invoice_item_5 = create(:invoice_item, item: @item_5, invoice: @invoice_4)
+    @invoice_item_6 = create(:invoice_item, item: @item_7, invoice: @invoice_4)
   end
 
   describe '/merchants/merchant_id/invoices/invoice_id' do
@@ -42,7 +43,16 @@ RSpec.describe 'merchant invoice show' do
       expect(page).to have_content(@invoice_2.customer.first_name)
       expect(page).to have_content(@invoice_2.customer.last_name)
       expect(page).to_not have_content(@invoice_1.id)
-     
+    end
+
+    it 'shows all items on an invoice and their attributes including item name, quantity, sale price, invoice item status' do
+      visit merchant_invoice_path(@merchant_2, @invoice_3)
+    
+      expect(page).to have_content(@invoice_3.id)
+      expect(page).to have_content(@item_3.name)
+      expect(page).to have_content(@invoice_item_3.quantity)
+      expect(page).to have_content(@invoice_item_3.unit_price)
+      expect(page).to_not have_content(@item_1.name)
     end
   end
 end
