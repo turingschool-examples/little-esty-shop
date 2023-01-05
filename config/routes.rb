dd1c947c37b368573fc1763b20b416361f727bd9
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-04 16:42:42 -0800

    Add views and pass tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   
   resources :merchants, only: [] do
     resources :items, only: [:index]
-    resources :invoices, only: [:index]
+    resources :invoices, only: [:index, :show]
   end
   
   resources :admin, only: [:index]
   
   get '/merchants/:merchant_id/dashboard', to: 'merchants#show'
+
 end
