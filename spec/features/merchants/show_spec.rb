e49fdbeef91eace924a628ab57c3ccbbe68d76a8
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-05 10:57:58 -0800

    Test: invoice creation date

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -213,7 +213,28 @@ RSpec.describe 'merchant show' do
       expect(current_path).to eq(invoice_path(invoice1))
     end
 
-    it 'lists invoice creation date next to each item'
+    it 'lists invoice creation date next to each item' do
+      merchant1 = Merchant.create!(name: 'Rays Hand Made Jewlery')
+
+      item1 = Item.create!(name: 'Chips', description: 'Ring', unit_price: 20, merchant_id: merchant1.id)
+      item2 = Item.create!(name: 'darrel', description: 'Bracelet', unit_price: 40, merchant_id: merchant1.id)
+      item3 = Item.create!(name: 'don', description: 'Necklace', unit_price: 30, merchant_id: merchant1.id)
+
+      customer1 = Customer.create!(first_name: 'Kyle', last_name: 'Ledin')
+      invoice1 = Invoice.create!(status: 1, customer_id: customer1.id)
+      ii1 = InvoiceItem.create!(quantity: 5, unit_price: item1.unit_price, item_id: item1.id,
+                                invoice_id: invoice1.id)
+      ii2 = InvoiceItem.create!(quantity: 5, unit_price: item2.unit_price, item_id: item2.id,
+                                invoice_id: invoice1.id)
+      ii3 = InvoiceItem.create!(quantity: 5, unit_price: item3.unit_price, item_id: item3.id,
+                                invoice_id: invoice1.id)
+
+      visit merchant_dashboards_path(merchant1.id)
+
+      within("#invoice#{invoice1.id}-item#{item1.id}") do
+        expect(page).to have_content(invoice1.created_at)
+      end
+    end
     it 'formats date as Weekday, Month DD, YYYY'
     it 'orders list from oldest invoice creation to newest'
   end
