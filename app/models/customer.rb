0f4cff417e1d1ff49525020821bcfa82f9bb2c2a
Drew Layton <dlayton66@gmail.com>
2023-01-04 21:06:48 -0700

    feat/test: Create top_customers in Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,16 @@
 class Customer < ApplicationRecord 
-  has_many :invoices
+  has_many :invoices, dependent: :destroy
+  has_many :transactions, through: :invoices
+
+  def self.successful_transactions
+    joins(:transactions).where("transactions.result = 'success'")
+  end
+
+  def self.order_by_purchases
+    select("customers.*, COUNT(invoices.customer_id) as purchases").group("customers.id").order("purchases desc, customers.id")
+  end
+
+  def self.top_customers
+    successful_transactions.order_by_purchases.first(5)
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,7 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Customer do
+  before :each do
+    FactoryBot.reload
+    @customers = FactoryBot.create_list(:customer_with_invoice, 10)
+    @invoices = Invoice.all
+    FactoryBot.create_list(:transaction, 6, result: 'success', invoice_id: @invoices[0].id)
+    FactoryBot.create_list(:transaction, 5, result: 'success', invoice_id: @invoices[3].id)
+    FactoryBot.create_list(:transaction, 4, result: 'success', invoice_id: @invoices[1].id)
+    FactoryBot.create_list(:transaction, 3, result: 'success', invoice_id: @invoices[4].id)
+    FactoryBot.create_list(:transaction, 2, result: 'success', invoice_id: @invoices[2].id)
+  end
+
   describe "Relationships" do
     it {should have_many :invoices} 
+    it {should have_many(:transactions).through(:invoices)}
+  end
+
+  describe '#successful_transactions' do
+    it 'returns all the successful transactions' do
+      expect(Customer.successful_transactions.count).to eq 20
+    end
+  end
+
+  describe '#order_by_purchases' do
+    it 'orders a customer-transaction join table by number of purchases' do
+      expect(Customer.successful_transactions.order_by_purchases.first.id).to eq @customers[0].id
+      expect(Customer.successful_transactions.order_by_purchases.fifth.id).to eq @customers[2].id
+    end
+  end
+
+  describe '#top_customers' do
+    it 'returns the 5 customers with most purchases' do
+      expect(Customer.top_customers.first.id).to eq @customers[0].id
+      expect(Customer.top_customers.fifth.id).to eq @customers[2].id
+      expect(Customer.top_customers.count).to eq 5
+    end
   end
 end
