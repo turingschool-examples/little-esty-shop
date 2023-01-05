73a8ed2b84364a765ed1d9121d0fc307dc7f771b
Ryan Canton <ryancanton999@gmail.com>
2023-01-05 10:37:25 -0700

    Feat: Add prefix to handrolled route for merchant dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/merchants/:id/dashboard', to: 'merchants/dashboard#show'
+  get '/merchants/:id/dashboard', to: 'merchants/dashboard#show', as: 'merchant_dashboard'
 
   resources :merchants, only: [] do
     scope module: "merchants" do
