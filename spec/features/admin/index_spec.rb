c13655a23a334d363424b7a5560be86cbf4bcc2d
Drew Layton <dlayton66@gmail.com>
2023-01-05 20:33:52 -0700

    test: Incomplete Invoices on Admin Dashboard

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -11,20 +11,18 @@ RSpec.describe 'admin index page' do
     FactoryBot.create_list(:transaction, 4, result: 'success', invoice_id: @invoices[1].id)
     FactoryBot.create_list(:transaction, 3, result: 'success', invoice_id: @invoices[4].id)
     FactoryBot.create_list(:transaction, 2, result: 'success', invoice_id: @invoices[2].id)
+
+    visit admin_index_path
   end
   
   describe 'header' do
     it 'indicates we are on admin dashboard with header' do
-      visit admin_index_path
-
       within('header') do
         expect(page).to have_content('Admin Dashboard')
       end
     end
 
     it 'has links to admin merchants and admin invoices' do
-      visit admin_index_path
-
       within('header') do
         expect(page).to have_link("Merchants", href: admin_merchants_path)
         expect(page).to have_link("Invoices", href: admin_invoices_path)
@@ -34,13 +32,35 @@ RSpec.describe 'admin index page' do
 
   describe 'top customers' do
     it 'lists the top 5 customers by number of successful transactions' do
-      visit admin_index_path
-
       expect(page).to have_content("Top Customers")
+
       within("#top-cust") do
         expect(page).to have_content(Customer.top_customers.first.first_name)
         expect(page).to have_content(Customer.top_customers.first.last_name)
         expect(page).to have_content(Customer.top_customers.first.purchases)
+      end
+    end
+  end
+
+  describe 'incomplete invoices' do
+    # Admin Dashboard Incomplete Invoices
+
+    # As an admin,
+    # When I visit the admin dashboard
+    # Then I see a section for "Incomplete Invoices"
+    # In that section I see a list of the ids of all invoices
+    # That have items that have not yet been shipped
+    # And each invoice id links to that invoice's admin show page
+    
+    it 'shows the incomplete invoices with links to their invoice admin show page' do
+      invoice_1 = Invoice.incomplete.first
+      invoice_2 = Invoice.incomplete.second
+      invoice_3 = Invoice.incomplete.third
+      
+      within("#incomplete-invoices") do
+        expect(page).to have_link(invoice_1.id.to_s, href: admin_invoices_show_path(invoice_1))
+        expect(page).to have_link(invoice_2.id.to_s, href: admin_invoices_show_path(invoice_2))
+        expect(page).to have_link(invoice_3.id.to_s, href: admin_invoices_show_path(invoice_3))
       end
     end
   end
