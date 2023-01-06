b45de8b34d814fb71d9c27a01bb182c4cc2926c0
Braxton Eddings <beddings81@gmail.com>
2023-01-05 19:36:05 -0600

    Pass tests for user story 9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,27 +17,24 @@ class ItemsController < ApplicationController
   def update
     @merchant = Merchant.find(params[:merchant_id])
     @item = @merchant.items.find(params[:id])
+    
     if @item.update(item_params)
-      redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}"
-      flash[:message] = 'Item updated'
+      if params[:status]
+        @item.change_status
+        redirect_to merchant_items_path(@item.merchant)
+      else
+        redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}"
+        flash[:message] = 'Item updated'
+      end
     else
       redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}/edit"
       flash[:error] = 'Update invalid'
     end
   end
-  #   if params[:enabled].present?
-  #     @item.update(item_params)
-  #     redirect_to merchant_items_path(@item.merchant)
-  #   else
-  #     @item.update(item_params)
-  #     flash.notice = "ITEM UPDATED"
-  #     redirect_to merchant_item_path(@item.merchant, @item)
-  #   end
-  # end
 
   private
 
   def item_params
-    params.permit(:name, :description, :unit_price, :status)
+    params.permit(:name, :description, :unit_price)
   end
 end
