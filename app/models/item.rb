f164e8907272b7a3e6122036c7554e4ad61945ae
Anhnhi Tran <anhtran811@gmail.com>
2023-01-06 14:50:11 -0700

    Add validations for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,6 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items, dependent: :destroy
   has_many :customers, through: :invoices, dependent: :destroy
   has_many :transactions, through: :invoices, dependent: :destroy
+
+  validates_presence_of :name, :description, :unit_price
 end
