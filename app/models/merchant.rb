b7b8f809f668c76674f7dd10a7977493ef64f1a5
Drew Layton <dlayton66@gmail.com>
2023-01-05 17:41:34 -0700

    refactor: Make a failed transaction 0 and successful transaction 1

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,7 +8,7 @@ class Merchant < ApplicationRecord
   def self.top5(id)
     Customer
     .joins(:merchants, :transactions)
-    .where("transactions.result = 0 AND merchants.id = #{id}")
+    .where("transactions.result = 1 AND merchants.id = #{id}")
     .group(:id)
     .select('first_name, last_name, count(customers) AS transactions_count')
     .order(transactions_count: :desc)
