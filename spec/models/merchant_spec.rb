3942ed83703137561eba2653f8bee7d8fc824903
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-05 19:07:17 -0800

    Test: merchants pending invoices order by oldest

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -49,6 +49,20 @@ RSpec.describe Merchant do
 
         expect(merchant1.pending_invoices).to eq([])
       end
+
+      it 'orders invoices from oldest to newest' do
+        merchant1 = Merchant.create!(name: 'Rays Hand Made Jewlery')
+        item1 = Item.create!(name: 'Chips', description: 'Ring', unit_price: 20, merchant_id: merchant1.id)
+        customer = Customer.create!(first_name: 'Kyle', last_name: 'Ledin')
+        invoice1 = Invoice.create!(status: 1, customer_id: customer.id)
+        ii1 = InvoiceItem.create!(quantity: 5, unit_price: item1.unit_price, item_id: item1.id, invoice_id: invoice1.id)
+        invoice2 = Invoice.create!(status: 1, customer_id: customer.id, created_at: Time.now-5.days)
+        ii2 = InvoiceItem.create!(quantity: 5, unit_price: item1.unit_price, item_id: item1.id, invoice_id: invoice2.id)
+        invoice3 = Invoice.create!(status: 1, customer_id: customer.id, created_at: Time.now-15.days)
+        ii3 = InvoiceItem.create!(quantity: 5, unit_price: item1.unit_price, item_id: item1.id, invoice_id: invoice3.id)
+
+        expect(merchant1.pending_invoices).to eq([invoicer3, invoice2, invoice1])
+      end
     end
   end
 end
