d0eea89946a1b1dfb7a720d079debb61f12a0b81
Joe King <j.king92@gmail.com>
2023-01-05 19:29:50 -0700

    Add relationship and testing for invoice_items have many transactions

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,7 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
+  has_many :transactions, through: :invoice
   enum status: { pending: 0, packaged: 1, shipped: 2 }
   validates_numericality_of :quantity, :unit_price
 
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe InvoiceItem do
   describe 'Relationships' do
     it {should belong_to :invoice}
     it {should belong_to :item}
+    it {should have_many(:transactions).through(:invoice)}
     it {should validate_numericality_of :quantity}
     it {should validate_numericality_of :unit_price}
   end
