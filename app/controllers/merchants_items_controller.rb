d58d0e67b0873bf6eaf290839ec532476d6280b0
Brady Rohrig <brady.rohrig@gmail.com>
2023-01-06 20:38:26 -0800

    feat: completed functionality for merch_item creation and new page

--- app/controllers/merchants_items_controller.rb
+++ app/controllers/merchants_items_controller.rb
@@ -14,6 +14,18 @@ class MerchantsItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.create!(create_params)
+
+    redirect_to merchant_items_path(@merchant.id)
+  end
+
   def update
     @merchant = Merchant.find(params[:merchant_id])
     @item = Item.find(params[:id])
@@ -32,5 +44,9 @@ class MerchantsItemsController < ApplicationController
 
   def update_params
     params.permit(:name, :description, :unit_price)
+  end
+
+  def create_params
+    params.permit(:name, :description, :unit_price, :status, :merchant_id)
   end
 end
