d56ddb8013ba1445d7c172ecff711b32cb152b0e
Kelsie Porter <krporter22@gmail.com>
2023-01-06 22:15:02 -0700

    Complete US 11

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -122,4 +122,43 @@ RSpec.describe 'Merchant Items Index page' do
       end
     end 
   end
+
+  # As a merchant
+  # When I visit my items index page
+  # I see a link to create a new item.
+  # When I click on the link,
+  # I am taken to a form that allows me to add item information.
+  # When I fill out the form I click ‘Submit’
+  # Then I am taken back to the items index page
+  # And I see the item I just created displayed in the list of items.
+  # And I see my item was created with a default status of disabled.
+  describe 'user story 11' do 
+    it 'displays a link to create a new item' do
+      visit merchant_items_path(@merchant_1.id)
+ 
+      click_link('Create a new item')
+
+      expect(current_path).to eq(new_merchant_item_path(@merchant_1.id))
+
+      fill_in 'Name', with: "Soccer Ball"
+      fill_in 'Description', with: "black and white"
+      fill_in 'Unit price', with: 22555
+      click_button 'Submit'
+
+      expect(current_path).to eq(merchant_items_path(@merchant_1.id))
+      expect(page).to have_content("Soccer Ball")
+      expect(page).to have_content("Status: disabled")
+    end
+    it 'sad path - cannot create a new item if a field is empty' do
+      visit new_merchant_item_path(@merchant_1.id)
+
+      fill_in 'Name', with: ""
+      fill_in 'Description', with: ""
+      fill_in 'Unit price', with: 
+      click_button('Submit')
+
+      expect(page).to have_content('Item was not created, please fill out all of the fields.')
+      expect(page).to have_button('Submit')
+    end
+  end
 end
