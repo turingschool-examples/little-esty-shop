12c427a78d9a8ecb572c11181ee140eed7c2fdb7
Kelsie Porter <krporter22@gmail.com>
2023-01-07 16:06:58 -0700

    Remove duplicate files

--- app/models/item 2.rb
+++
@@ -1,7 +0,0 @@
-class Item < ApplicationRecord
-  belongs_to :merchant
-  has_many :invoice_items, dependent: :destroy
-  has_many :invoices, through: :invoice_items, dependent: :destroy
-  has_many :customers, through: :invoices, dependent: :destroy
-  has_many :transactions, through: :invoices, dependent: :destroy
-end
