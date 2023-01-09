66a0f06042209f18eabc6d23c3c334ebfc8e72af
Joe King <j.king92@gmail.com>
2023-01-09 12:00:20 -0700

    Add transactions relationship to item along with associated test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Item do
   it {should belong_to :merchant}
   it {should have_many :invoice_items}
   it {should have_many(:invoices).through(:invoice_items)}
+  it {should have_many(:transactions).through(:invoices)}
   it {should validate_presence_of :name}
   it {should validate_presence_of :unit_price}
   it {should validate_presence_of :description}
