9914e807139cc5b867c0427719a3e3ade5edc8b1
Samuel Cox <samc1253@gmail.com>
2023-01-09 14:27:11 -0700

    Remove commented out lines

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,5 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
-  # belongs_to :invoice_item, through: :invoice
-  # belongs_to :item, through: :invoice_item
-  # belongs_to :merchant, through: :item
+
   validates_presence_of :credit_card_number, :result
 end
