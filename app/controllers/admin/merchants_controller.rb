46fc06e8f5792099b824c339c32875e396b99f13
Drew Layton <dlayton66@gmail.com>
2023-01-08 17:27:40 -0700

    feat/test: User Story 26: Admin Merchant Edit/Update

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,4 +6,25 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    if merchant.update(merchant_params)
+      redirect_to admin_merchant_path(merchant)
+      flash[:alert] = "Successfully updated"
+    else
+      redirect_to edit_admin_merchant_path(merchant)
+      flash[:alert] = "Update failed"
+    end
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
