eaa2c78b2b011575d765561a531cf3036fdd9c76
Alastair Lloyd <dex8056@aol.com>
2023-01-10 08:44:55 -0700

    add test data to helper methods and require statement added to rails helper to use method in tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+
+  require 'helper_methods'
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
