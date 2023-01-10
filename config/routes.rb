9d3e573e56472f113aee02f53cb49819bcf29c06
Kaylah Rose M <111713452+kaylahrose@users.noreply.github.com>
2023-01-09 17:56:42 -0800

    Revert "Gets app ready for heroku deployment"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+
 
   resources :admin, only: [:index] 
 
@@ -19,5 +19,4 @@ Rails.application.routes.draw do
   end
 
   resources :invoices, only: [:show]
-
 end
