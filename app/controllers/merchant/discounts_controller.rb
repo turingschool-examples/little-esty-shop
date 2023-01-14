f2af254f4b09898f41134c7b7b05d48b9a44a2ac
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-14 12:01:45 -0800

    Add discount show page and tests

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,6 +1,11 @@
-class Merchant::ItemsController < ApplicationController
+class Merchant::DiscountsController < ApplicationController
 
   def index
-    
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.find(params[:id])
   end
 end
