516b1c1a7168f318ee0f5c71f8d363d780acbcd2
Weston Sandfort <sandfortw@gmail.com>
2023-02-22 15:40:19 -0700

    add invoice_items factory

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :invoice_item do
-    
+    item_id {Faker::Number.within(range: 100..9999)}
+    invoice_id {Faker::Number.within(range: 1..999)}
+    quantity {Faker::Number.within(range: 1..99)}
+    unit_price {Faker::Number.within(range: 1000..99999)}
+    status {:pending}
   end
 end
