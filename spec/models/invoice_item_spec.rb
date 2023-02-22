ec73e097a1776cc5653bfd5bb8cd8b9682c4abad
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-02-22 15:55:32 -0800

    Add enum testing to relationships

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it { should belong_to :invoice }
     it { should belong_to :item }
+    it { should define_enum_for(:status).with_values(["packaged", "pending", "shipped"])}
   end
 end
