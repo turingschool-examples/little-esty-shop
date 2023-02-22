b8a8c96a5c3c404654da4dd14479d39ac1069333
Adam Bailey <ab67319@gmail.com>
2023-02-21 19:06:57 -0700

    feat: establish model associations
    Co-authored-by: Dawson Timmons <dawsontimmons@gmail.com>

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  belongs_to :merchants
+
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,4 @@
 class Transaction < ApplicationRecord
+  belongs_to :invoice
 end
+
