318121f2015f95184830bebb3e5085b3b29ca10b
Caleb Thomas <cjthomas00@comcast.net>
2023-02-22 15:46:27 -0700

    create relationships tests.

---
+++ spec/invoice_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Invoice, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:customer) }
+    it { should have_many(:items).through(:invoice_items)}
+  end
+end
