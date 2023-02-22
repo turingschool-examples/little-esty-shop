c3d875e0ebf461f1e1dfaac276115ed3922e1572
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-02-22 15:39:04 -0800

    Feat: create model relationships and tests

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,8 @@
 class Invoice < ApplicationRecord
+  belongs_to :customer
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+  
   enum status: ["cancelled", "in progress", "completed"]
 end
