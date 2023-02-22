07b44d0d438e4d5c53c4a467abb51b76dc847dbb
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-22 15:52:28 -0700

    Test: add test for items and invoices links on merchant dashboard

--- spec/features/merchants/dashboard/index_spec.rb
+++ spec/features/merchants/dashboard/index_spec.rb
@@ -10,5 +10,27 @@ RSpec.describe 'merchant show dashboard index page', type: :feature do
 
       expect(page).to have_content("Brian's Beads")
     end
+
+    it "will have a link that takes me to the 'merchants/merchant_id/items index page" do 
+
+      visit "/merchants/#{merchant1.id}/dashboard"
+
+      expect(page).to have_link("Merchant Items")
+
+      click_link("Merchant Items")
+
+      expect(current_path).to eq("/merchants/#{merchant1.id}/items")
+    end
+
+    it "will have a link that takes me to the 'merchants/merchant_id/invoices index page" do 
+
+      visit "/merchants/#{merchant1.id}/dashboard"
+
+      expect(page).to have_link("Merchant Invoices")
+
+      click_link("Merchant Invoices")
+
+      expect(current_path).to eq("/merchants/#{merchant1.id}/invoices")
+    end 
   end
 end
