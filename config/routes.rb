7e8ab6496d9099b3c2598eb45adb426db7fafb09
Huy Phan <huyphan520@gmail.com>
2023-02-22 15:51:01 -0600

    Created folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,36 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+ #admins
+  #merchants
+  #customers
+  #invoices
+  #transactions
+  #items
+  #invoice_items
+  
+ #merchants
+  #customers
+  #invoices
+  #transactions
+  #items
+  #invoice_items
 
+ #customers
+
+
+
+ #invoices
+
+
+
+ #transactions
+
+
+
+ #items
+
+
+
+ #invoice_items
 end
