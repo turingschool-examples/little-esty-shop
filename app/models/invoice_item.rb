48531fd86f66ff64dff6658676fc92176db30a89
Elle Majors <hellolovely@ellemajors.com>
2023-02-22 15:53:13 -0700

    add: model associations for incoice_item, item, transaction

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,4 @@
 class InvoiceItem < ApplicationRecord
-  
+  belongs_to :item
+  belongs_to :invoice
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
-  
+  has_many :invoice_items
+  has_many :invoices
+  belongs_to :merchant
 end
