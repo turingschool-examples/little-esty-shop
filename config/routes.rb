a5d8f78854d1dbd3ab216b015d6f1e6d257c9d55
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-02-22 14:09:20 -0800

    Creat routes and controllers for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :admin, only: :index
+  
+  namespace :admin do
+    resources :merchant, only: [:index, :show]
+  end
+
 end
