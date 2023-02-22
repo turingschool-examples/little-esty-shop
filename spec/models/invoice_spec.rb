4262a257498bc622cefe5ce77941f1d7b4aa7716
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-02-22 15:55:32 -0800

    Add enum testing to relationships

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe Invoice, type: :model do
     it { should have_many :transactions }
     it { should have_many :invoice_items }
     it { should have_many(:items).through(:invoice_items)}
+    it { should define_enum_for(:status).with_values(["cancelled", "in progress", "completed"])}
   end
 end
