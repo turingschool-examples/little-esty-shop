865a8e176452ce499b2ce375eb6c55dda7ed19f4
Weston Sandfort <sandfortw@gmail.com>
2023-02-23 11:42:44 -0700

    create top 5 customers by transactions method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,11 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
   has_many :invoice_items, through: :invoices
   has_many :items, through: :invoice_items
+
+  def self.top_5_by_transactions
+    Customer.joins(:transactions)
+            .select("customers.*, count(transactions.id)")
+            .group(:id)
+            .limit(5)  
+  end
 end
