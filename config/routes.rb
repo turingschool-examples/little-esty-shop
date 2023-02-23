5d6ed632045d77b622e0894298b7570b614f3b12
Melony Franchini <meltravelz@gmail.com>
2023-02-22 17:22:01 -0700

    Add admin index page (dashboard)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
  #admins
+ get '/admin', to: "admin#index"
+
   #merchants
   #customers
   #invoices
