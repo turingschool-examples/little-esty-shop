5c710549c1e319f663e2829e0ca29dd91a5da674
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-02-22 18:29:09 -0700

    Add us2

--- spec/features/merchants/dashboard_index_spec.rb
+++ spec/features/merchants/dashboard_index_spec.rb
@@ -14,16 +14,15 @@ RSpec.describe "Merchant Dashboard Index" do
       it "Then I see the name of my merchant" do
         expect(page).to have_content("Name: #{merchant1.name}")
         expect(page).to_not have_content(merchant2.name)
-        save_and_open_page
       end
-
+      
       # 2. Merchant Dashboard Links
-      xit "Then I see link to my merchant items index (/merchants/merchant_id/items)" do 
-
+      it "Then I see link to my merchant items index (/merchants/merchant_id/items)" do 
+        expect(page).to have_link("Items")
       end
-
-      xit "Then I see a link to my merchant invoices index (/merchants/merchant_id/invoices)" do 
-
+      
+      it "Then I see a link to my merchant invoices index (/merchants/merchant_id/invoices)" do 
+        expect(page).to have_link("Invoices")
       end
     end
   end
