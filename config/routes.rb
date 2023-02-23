e7824999e4b232fd78895bec2fbc3d3a979fb936
Harrison Ryan <hwryan12@gmail.com>
2023-02-23 17:41:44 -0600

    feature: Add routes for merchants/:id/items and /invoices Co-authored-by: Chris Crane <chrisjcrane@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :merchants do
+    resources :items, only: [:index]
+    resources :invoices, only: [:index]
     resources :dashboard, only: [:index], controller: "merchant_dashboards"
   end
 end
