e3fc941594d19024d979527d879f1b0e5d2d7117
Adam Bailey <ab67319@gmail.com>
2023-02-22 20:05:06 -0700

    test: create feature test for admin page links, user story 20
    Co-authored-by: Harrison Ryan <hwryan12@gmail.com>
    Co-authored-by: Dawson Timmons <dawsontimmons@gmail.com>
    Co-authored-by: Chris Crane <chrisjcrane@gmail.com>

--- spec/features/admins/index_spec.rb
+++ spec/features/admins/index_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "The Admin Index" do
+  before(:each) do
+    visit "/admin"
+  end
+
   describe "User Story 19" do
     it "When an admin the admin dashboard (/admin), they see a header indicating that they are on the admin dashboard" do 
-      visit "/admin"
-
       expect(page).to have_content("Admin Dashboard")
+    end
+  end
+
+  describe "User Story 20" do
+    it "When I visit the admin dashboard (/admin) then I see a link to the admin merchants index (/admin/merchants)
+      and I see a link to the admin invoices index (/admin/invoices)" do
+
+      expect(page).to have_link("Merchants", href: "/admin/merchants")
+      expect(page).to have_link("Invoices", href: "/admin/invoices")
     end
   end
 end
