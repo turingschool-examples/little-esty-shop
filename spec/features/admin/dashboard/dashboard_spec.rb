9f6667836231605ee29681a5dff7a88258c61e0d
Weston Sandfort <sandfortw@gmail.com>
2023-02-22 18:52:48 -0700

    create tests for user story 20

--- spec/features/admin/dashboard/dashboard_spec.rb
+++ spec/features/admin/dashboard/dashboard_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
 describe 'dashboard' do
-  describe '/admin' do
-    
+  describe 'user story 19' do
     it 'should have a header \'admin dashboard\'' do
       visit '/admin'
       within('div#header') do
-        expect(page).to have_content("Admin Dashboard")
+        expect(page).to have_css('h1', text: "Admin Dashboard")
       end
     end
 
+  describe 'user story 20' do
+    it 'should have a link to admin merchants index' do
+      visit '/admin'
+      expect(page).to have_link('/admin/merchants')
+    end
+
+    it 'should have a link to admin invoices index' do
+      visit '/admin'
+      expect(page).to have_link('/admin/invoices')
+    end
+  end 
   end
 end
