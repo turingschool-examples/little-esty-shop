b6376b0ed416daf4eeef614c607c2f911ae0e1c4
Weston Sandfort <sandfortw@gmail.com>
2023-02-22 18:09:50 -0700

    change invoice items and invoice factories to add randomization of status

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
     invoice_id {Faker::Number.within(range: 1..999)}
     quantity {Faker::Number.within(range: 1..99)}
     unit_price {Faker::Number.within(range: 1000..99999)}
-    status { 0 }
+    status { [0, 1, 2].shuffle.first }
   end
 end
--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :invoice do
     customer_id {Faker::Number.within(range: 1..174)}
-    status { 0 }
+    status { [0, 1, 2].shuffle.first }
   end
 end
