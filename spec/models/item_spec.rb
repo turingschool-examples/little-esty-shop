acbf4558b39299071e0319dd203376bd3fbee1ca
David Marino <davidjmarino8@gmail.com>
2023-02-22 19:15:59 -0500

    User Story 14 complete

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   it { should belong_to :merchant }
   it { should have_many :invoice_items }
-  it { should have_many(:items).through(:invoice_items) }
   it { should have_many(:customers).through(:invoices) }
 end
