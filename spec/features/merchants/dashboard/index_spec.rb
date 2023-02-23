dcf2c2c5248859e31f6624523519f2b567b274e0
Chris Crane <chrisjcrane@gmail.com>
2023-02-23 11:45:21 -0700

    Added tests for user story 2 Co-authored-by: Harrison Ryan <hwryan12@gmail.com> Co-authored-by: Dawson Timmons <dawsontimmons@gmail.com>

--- spec/features/merchants/dashboard/index_spec.rb
+++ spec/features/merchants/dashboard/index_spec.rb
@@ -14,15 +14,14 @@ RSpec.describe "Dashboard", type: :feature do
     end
   end
 
-  xdescribe "User story 2" do
+  describe "User story 2" do
     describe "When I visit my merchant dashboard" do
       it "shows a link to my merchant items index (/merchants/merchant_id/items)" do
         expect(page).to have_link("Items", href: "/items")
-        expect(page).to have_link("Merchants", href: "/merchants")
       end
 
       it "show a link to my merchant invoices index (/merchants/merchant_id/invoices)" do
-        
+        expect(page).to have_link("Invoices", href: "/invoices")
       end
     end
   end
