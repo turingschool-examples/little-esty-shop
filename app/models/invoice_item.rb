502dd3fdca7a53c262a48061a05df1f037e5cdcc
Bobby Luly <bobbyluly@gmail.com>
2023-02-23 12:08:24 -0700

    Create reset_pk sequence for all csv load tasks

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,5 +1,5 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
-  enum staus: ["pending","packaged", "shipped"]
+  enum status: ["pending", "packaged", "shipped"]
 end
