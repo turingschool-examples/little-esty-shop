9deb22c13b2476c6244e0b4572a5b9e6f4298a9e
Conner Van Loan <cdvanloan@gmail.com>
2023-02-23 16:45:39 -0700

    Updates top 5 customers method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,7 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
-	has_many :customers, through: :invoices
-  has_many :transactions, through: :invoices
+  has_many :customers, -> { distinct }, through: :invoices  
+  has_many :transactions, -> { distinct }, through: :invoices
 
 end
