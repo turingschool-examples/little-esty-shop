ef11d8f8bb9a799e4147cf9fde87de6e9e9d20b1
Meredith Trevino <meredithtrevino@gmail.com>
2023-02-22 20:42:36 -0700

    adds test for user story 3

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,8 @@ class Merchant < ApplicationRecord
 	validates :name, presence: true
 	enum status: [ "active", "disabled" ]
   has_many :items
+
+  def top_five_customers
+    binding.pry
+  end
 end
--- spec/features/merchants/dashboard/index_spec.rb
+++ spec/features/merchants/dashboard/index_spec.rb
@@ -11,25 +11,49 @@ RSpec.describe 'merchant show dashboard index page', type: :feature do
     let!(:customer5) { Customer.create!(uuid: 5, first_name: "Barbara", last_name: "Hilton")}
     let!(:customer6) { Customer.create!(uuid: 6, first_name: "Bella", last_name: "Thomas")}
 
-    customer1.invoices.create!(uuid: 10, status: 2)
-    customer1.invoices.create!(uuid: 11, status: 2)
+    let!(:invoice1) { customer1.invoices.create!(uuid: 10, status: 2) }
+    let!(:invoice2) { customer1.invoices.create!(uuid: 11, status: 2) }
+    let!(:invoice3) { customer2.invoices.create!(uuid: 12, status: 2) }
+    let!(:invoice4) { customer2.invoices.create!(uuid: 13, status: 2) }
+    let!(:invoice5) { customer3.invoices.create!(uuid: 14, status: 2) }
+    let!(:invoice6) { customer3.invoices.create!(uuid: 15, status: 2) }
+    let!(:invoice7) { customer4.invoices.create!(uuid: 16, status: 2) }
+    let!(:invoice8) { customer5.invoices.create!(uuid: 17, status: 2) }
+    let!(:invoice9) { customer5.invoices.create!(uuid: 18, status: 2) }
+    let!(:invoice10) { customer6.invoices.create!(uuid: 19, status: 2) }
+    let!(:invoice11) { customer6.invoices.create!(uuid: 20, status: 2) }
 
-    customer2.invoices.create!(uuid: 12, status: 2)
-    customer2.invoices.create!(uuid: 13, status: 2)
+    let!(:item1) { merchant1.items.create!(name: "water bottle", description: "24oz metal container for water", unit_price: 8) }    
+    let!(:item2) { merchant1.items.create!(name: "rubber duck", description: "toy for bath", unit_price: 1) }    
+    let!(:item3) { merchant1.items.create!(name: "lamp", description: "12 inch desk lamp", unit_price: 16) }    
+    let!(:item4) { merchant1.items.create!(name: "wireless mouse", description: "wireless computer mouse for mac", unit_price: 40) }    
+    let!(:item5) { merchant1.items.create!(name: "chapstick", description: "coconut flavor chapstick", unit_price: 2) }    
 
-    customer3.invoices.create!(uuid: 14, status: 2)
-    customer3.invoices.create!(uuid: 15, status: 2)
+    before do
+      InvoiceItem.create!(item: item1, invoice: invoice1)
+      InvoiceItem.create!(item: item2, invoice: invoice1)
+      InvoiceItem.create!(item: item1, invoice: invoice2)
+      InvoiceItem.create!(item: item4, invoice: invoice2)
+      InvoiceItem.create!(item: item4, invoice: invoice3)
+      InvoiceItem.create!(item: item3, invoice: invoice3)
+      InvoiceItem.create!(item: item1, invoice: invoice4)
+      InvoiceItem.create!(item: item4, invoice: invoice4)
+      InvoiceItem.create!(item: item1, invoice: invoice5)
+      InvoiceItem.create!(item: item2, invoice: invoice5)
+      InvoiceItem.create!(item: item2, invoice: invoice6)
+      InvoiceItem.create!(item: item3, invoice: invoice6)
 
-    customer4.invoices.create!(uuid: 16, status: 2)
-    customer4.invoices.create!(uuid: 17, status: 2)
-    customer4.invoices.create!(uuid: 18, status: 2)
-    
-    customer5.invoices.create!(uuid: 19, status: 2)
-    customer5.invoices.create!(uuid: 20, status: 2)
+      InvoiceItem.create!(item: item5, invoice: invoice7)
 
-    customer6.invoices.create!(uuid: 21, status: 2)
-    customer6.invoices.create!(uuid: 22, status: 2)
-
+      InvoiceItem.create!(item: item1, invoice: invoice8)
+      InvoiceItem.create!(item: item3, invoice: invoice8)
+      InvoiceItem.create!(item: item2, invoice: invoice9)
+      InvoiceItem.create!(item: item3, invoice: invoice9)
+      InvoiceItem.create!(item: item3, invoice: invoice10)
+      InvoiceItem.create!(item: item4, invoice: invoice10)
+      InvoiceItem.create!(item: item1, invoice: invoice11)
+      InvoiceItem.create!(item: item4, invoice: invoice11)
+    end
     
 
     it 'shows my name(merchant)' do
@@ -62,10 +86,19 @@ RSpec.describe 'merchant show dashboard index page', type: :feature do
     end 
 
     it 'shows the names of the top 5 customers(largest number of successful transactions with merchant)' do
+      visit "/merchants/#{merchant1.id}/dashboard"
 
+      expect(page).to have_content("Top 5 customers with largest transactions")
+      expect(page).to have_content("Britney Spears")
+      expect(page).to have_content("Bob Smith")
+      expect(page).to have_content("Bill Johnson")
+      expect(page).to have_content("Barbara Hilton")
+      expect(page).to have_content("Bella Thomas")
+      expect(page).to_not have_content("Boris Nelson")
     end
 
-    it 'next to the top five customers, I see the number of successful transactions they have conducted with merchant' do
+    xit 'next to the top five customers, I see the number of successful transactions they have conducted with merchant' do
+      visit "/merchants/#{merchant1.id}/dashboard"
 
     end
   end
