36007d926aae30d95ed17c2f8c77471a30876050
Chris Crane <chrisjcrane@gmail.com>
2023-02-23 11:43:14 -0700

    Cleaned up test syntax with before each Co-authored-by: Chris Crane <chrisjcrane@gmail.com> Co-authored-by: Harrison Ryan <hwryan12@gmail.com>

--- spec/features/merchants/dashboard/index_spec.rb
+++ spec/features/merchants/dashboard/index_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Dashboard", type: :feature do
   before(:each) do
-    @merchant = create(:merchant)
+    @merchant = create(:merchant, name: "Trader Bob's")
+    visit "/merchants/#{@merchant.id}/dashboard"
   end
-  # let!(:merchant) { Merchant.create!(name: "Trader Bob's") }
+
   describe "User story 1" do
     it "As a merchant, when I visit my merchant dashboard 
       (/merchants/:merchant_id/dashboard) then I see the name of my merchant" do
-      visit "/merchants/#{merchant.id}/dashboard"
 
       expect(page).to have_content("Trader Bob's")
     end
@@ -17,7 +17,6 @@ RSpec.describe "Dashboard", type: :feature do
   xdescribe "User story 2" do
     describe "When I visit my merchant dashboard" do
       it "shows a link to my merchant items index (/merchants/merchant_id/items)" do
-        visit "/merchants/#{merchant.id}/dashboard"
         expect(page).to have_link("Items", href: "/items")
         expect(page).to have_link("Merchants", href: "/merchants")
       end
