a079e7dd76ac496525e5d09b8abca02408a28d6b
Isaac Alter <i_alter@yahoo.com>
2023-02-22 20:10:17 -0700

    Refactors feat: User Story 32 admin/invoice/show included in routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   resource :admin, only: :index do
     resources :merchants, only: :index, controller: "admin/merchant"
-    resources :invoices, only: :index, controller: "admin/invoice"
+    resources :invoices, only: [:index, :show], controller: "admin/invoice"
   end
 end
