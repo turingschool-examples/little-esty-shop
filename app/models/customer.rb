f6a3a12180bbdda9c6b55850cb9aeaaa304c4cca
Harrison Ryan <hwryan12@gmail.com>
2023-02-23 18:44:36 -0600

    feature: Add customer/model method #top_five_customers and top_five_customers Co-authored-by: Chris Crane <chrisjcrane@gmail.com>

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
   has_many :invoices
+
+  def self.top_five_customers
+    Customer.select("customers.*, count('transactions.id') as total_purchases")
+      .joins(invoices: :transactions)
+      .where(transactions: {result: 0})
+      .group(:id)
+      .order(count: :desc)
+      .limit(5)
+  end
 end
