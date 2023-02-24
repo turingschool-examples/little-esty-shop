192ec060d8a7039ba0a0ccd9511afd4e1c08b913
Tori Enyart <torienyart@gmail.com>
2023-02-23 18:25:16 -0700

    Add section for incomplete invoices w/ links to invoices in admin dashboard page

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,6 @@ class Customer < ApplicationRecord
   has_many :invoices, dependent: :destroy
 	has_many :transactions, through: :invoices
 
-
   def self.top_five_customers
     x = joins(:transactions)
     .select("customers.*, count(DISTINCT transactions.id) as trans_count")
