c2d7256c948391c127ba44aef8e42e114d35e056
Conner Van Loan <cdvanloan@gmail.com>
2023-02-23 21:08:20 -0700

    makes a slightly more robust test for admin merchant show

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -3,14 +3,19 @@ require 'rails_helper'
 RSpec.describe '#index' do
   before :each do
     @merchant1 = Merchant.create!(name: "Carlos Jenkins") 
-
+    @merchant2 = Merchant.create!(name: "Leroy Jenkins") 
     visit "/admin/merchants/#{@merchant1.id}"
   end
 
   describe 'as an admin when I visit the admin merchants show page' do
     it 'I see the names of that merchant' do
-      save_and_open_page
+
       expect(page).to have_content("Carlos Jenkins")
+
+      visit "/admin/merchants/#{@merchant2.id}"
+      expect(page).to have_content("Leroy Jenkins")
+
+
     end
 
   end
