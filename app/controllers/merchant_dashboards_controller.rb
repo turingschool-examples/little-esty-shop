9327bc45dcb40abb1d90c7a306b4d31f48c6dc84
Harrison Ryan <hwryan12@gmail.com>
2023-02-23 18:51:10 -0600

    feature: Add top_five_customers variable to merchant_dashboards_controller Co-authored-by: Chris Crane <chrisjcrane@gmail.com>

--- app/controllers/merchant_dashboards_controller.rb
+++ app/controllers/merchant_dashboards_controller.rb
@@ -1,5 +1,6 @@
 class MerchantDashboardsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
+    @top_five_customers = Customer.top_five_customers
   end
 end
