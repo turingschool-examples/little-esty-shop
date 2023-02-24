c556e12a345ff86f64f8e0b91c4a0d6302c02c3a
Huy Phan <huyphan520@gmail.com>
2023-02-23 18:03:03 -0600

    Finish user story 8 edit

--- spec/features/merchants/items/show_spec.rb
+++ spec/features/merchants/items/show_spec.rb
@@ -16,15 +16,6 @@ RSpec.describe 'show', type: :feature do
       expect(page).to have_content("Unit Price: #{qui.unit_price}")
     end
 
-    # As a merchant,
-    # When I visit the merchant show page of an item
-    # I see a link to update the item information.
-    # When I click the link
-    # Then I am taken to a page to edit this item
-    # And I see a form filled in with the existing item attribute information
-    # When I update the information in the form and I click ‘submit’
-    # Then I am redirected back to the item show page where I see the updated information
-    # And I see a flash message stating that the information has been successfully updated.
     it 'have form to update and edit the item' do
       visit "/merchants/#{schroeder_jerde.id}/items/#{qui.id}"
 
