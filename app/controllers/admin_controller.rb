436b4a044257c072441c7e5c608770bbfedca7dd
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-02-23 18:51:06 -0800

    Feat: complete user story 21 functionality and tests

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
   def index
+    @customers = Customer.top_five_customers
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,8 @@
 class Customer < ApplicationRecord
   has_many :invoices
+  has_many :transactions, through: :invoices
+
+  def self.top_five_customers
+    joins(:transactions).select("customers.*, count(result) as result_count").where(transactions: {result: 'success'}).group(:id).order("count(result) desc").limit(5)
+  end
 end
