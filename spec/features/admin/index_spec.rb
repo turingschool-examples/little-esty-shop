3183330e76c1de837037a0f8ce37432875142a4a
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-02-23 20:50:16 -0800

    Feat: complete user story 22 functionality and tests

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe "As an admin" do
+  let!(:merchant1) {Merchant.create!(uuid: 101, name: "Brian's Beads")}
+
   let!(:customer1) { Customer.create!(uuid: 1, first_name: "Britney", last_name: "Spears")}
   let!(:customer2) { Customer.create!(uuid: 2, first_name: "Bob", last_name: "Smith")}
   let!(:customer3) { Customer.create!(uuid: 3, first_name: "Bill", last_name: "Johnson")}
@@ -24,6 +26,12 @@ describe "As an admin" do
   
   let!(:invoice10) { customer6.invoices.create!(uuid: 10, status: 2) }
   let!(:invoice11) { customer6.invoices.create!(uuid: 11, status: 2) }
+
+  let!(:item1) { merchant1.items.create!(name: "water bottle", description: "24oz metal container for water", unit_price: 8) }    
+  let!(:item2) { merchant1.items.create!(name: "rubber duck", description: "toy for bath", unit_price: 1) }    
+  let!(:item3) { merchant1.items.create!(name: "lamp", description: "12 inch desk lamp", unit_price: 16) }    
+  let!(:item4) { merchant1.items.create!(name: "wireless mouse", description: "wireless computer mouse for mac", unit_price: 40) }    
+  let!(:item5) { merchant1.items.create!(name: "chapstick", description: "coconut flavor chapstick", unit_price: 2) }
   
   let!(:transaction1) { invoice1.transactions.create!(uuid: 1, credit_card_number: 4654405418249632, credit_card_expiration_date: Date.new(2024, 1, 3), result: "success") }
   let!(:transaction2) { invoice1.transactions.create!(uuid: 2, credit_card_number: 4140149827486249, credit_card_expiration_date: Date.new(2024, 1, 3), result: "success") }
@@ -54,6 +62,23 @@ describe "As an admin" do
   
   let!(:transaction18) { invoice11.transactions.create!(uuid: 18, credit_card_number: 4504301557459341, credit_card_expiration_date: Date.new(2024, 1, 3), result: "failed") }
   let!(:transaction19) { invoice11.transactions.create!(uuid: 19, credit_card_number: 4504301557459341, credit_card_expiration_date: Date.new(2024, 1, 3), result: "success") }
+
+  before(:each) do
+    InvoiceItem.create!(invoice_id: invoice1.id, item_id: item1.id, quantity: 3, unit_price: item1.unit_price, status: 0)
+    InvoiceItem.create!(invoice_id: invoice2.id, item_id: item2.id, quantity: 7, unit_price: item2.unit_price, status: 1)
+    InvoiceItem.create!(invoice_id: invoice2.id, item_id: item3.id, quantity: 1, unit_price: item3.unit_price, status: 2)
+    InvoiceItem.create!(invoice_id: invoice3.id, item_id: item3.id, quantity: 6, unit_price: item3.unit_price, status: 2)
+    InvoiceItem.create!(invoice_id: invoice4.id, item_id: item4.id, quantity: 9, unit_price: item4.unit_price, status: 1)
+    InvoiceItem.create!(invoice_id: invoice5.id, item_id: item5.id, quantity: 4, unit_price: item5.unit_price, status: 0)
+    InvoiceItem.create!(invoice_id: invoice5.id, item_id: item4.id, quantity: 7, unit_price: item4.unit_price, status: 0)
+    InvoiceItem.create!(invoice_id: invoice6.id, item_id: item4.id, quantity: 8, unit_price: item4.unit_price, status: 2)
+    InvoiceItem.create!(invoice_id: invoice7.id, item_id: item3.id, quantity: 6, unit_price: item3.unit_price, status: 1)
+    InvoiceItem.create!(invoice_id: invoice8.id, item_id: item2.id, quantity: 3, unit_price: item2.unit_price, status: 0)
+    InvoiceItem.create!(invoice_id: invoice9.id, item_id: item1.id, quantity: 2, unit_price: item1.unit_price, status: 2)
+    InvoiceItem.create!(invoice_id: invoice10.id, item_id: item2.id, quantity: 1, unit_price: item2.unit_price, status: 1)
+    InvoiceItem.create!(invoice_id: invoice10.id, item_id: item5.id, quantity: 3, unit_price: item5.unit_price, status: 0)
+    InvoiceItem.create!(invoice_id: invoice11.id, item_id: item3.id, quantity: 2, unit_price: item3.unit_price, status: 0)
+  end
 
   describe "When I visit the admin dashboard" do
     it "I see a header indicating that I am on the admin dashboard" do
@@ -92,6 +117,30 @@ describe "As an admin" do
         expect(page).to have_content("Name: #{customer6.first_name} #{customer6.last_name}, Successful Transactions: 2")
         expect(page).to have_content("Name: #{customer2.first_name} #{customer2.last_name}, Successful Transactions: 1")
         expect(page).to_not have_content("#{customer4.first_name} #{customer4.last_name}")
+      end
+    end
+
+    it "I see a section for 'Incomplete Invoices' that lists the ids, as links to their admin invoice show pages, of all invoices that have items that have not yet been shipped" do
+      visit "/admin"
+
+      within "#incomplete_invoices" do
+        expect(page).to have_link(invoice1.id, href: admin_invoice_path(invoice1.id))
+        expect(page).to have_link(invoice2.id, href: admin_invoice_path(invoice2.id))
+        expect(page).to have_link(invoice4.id, href: admin_invoice_path(invoice4.id))
+        expect(page).to have_link(invoice5.id, href: admin_invoice_path(invoice5.id))
+        expect(page).to have_link(invoice7.id, href: admin_invoice_path(invoice7.id))
+        expect(page).to have_link(invoice8.id, href: admin_invoice_path(invoice8.id))
+        expect(page).to have_link(invoice10.id, href: admin_invoice_path(invoice10.id))
+        expect(page).to have_link(invoice11.id, href: admin_invoice_path(invoice11.id))
+      end
+    end
+
+    it "has links to each incomplete invoice's admin invoice show page" do
+      visit "/admin"
+
+      within "#incomplete_invoices" do
+        click_link invoice1.id
+        expect(current_path).to eq("/admin/invoices/#{invoice1.id}")
       end
     end
   end
