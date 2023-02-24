ed7918cb26d2a8a9b6efadc8121971d4d8701505
Jade Stewart <jadekstewart3@gmail.com>
2023-02-23 17:15:22 -0700

    Create a for display of items that have been ordered but not yet shipped

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 RSpec.describe Merchant, type: :feature do 
 
   describe 'Merchant Dashboard' do
+    let!(:louise) { Customer.create!(first_name: "Louise", last_name: "Belcher") }
 
+    let!(:invoice1) { Invoice.create!(customer_id: louise.id) } 
+    let!(:invoice2) { Invoice.create!(customer_id: louise.id) } 
+    let!(:invoice3) { Invoice.create!(customer_id: louise.id) } 
+    
     let!(:bob) { Merchant.create!(name: "Bob's Beauties") } 
 
-    let!(:crystal_vase) { bob.items.create!(name: "Crystal Vase", description: "Genuine crystal vase", unit_price: 5000) }
-    let!(:cat_statue) { bob.items.create!(name: "Cat Statue", description: "Vintage cat statue", unit_price: 2500) }
-    let!(:ceramic_planter) { bob.items.create!(name: "Ceramic Planter", description: "Black ceramic planter", unit_price: 2500) }
+    let!(:coochie_copi) { bob.items.create!(name: "Coochie Copi Night Light", description: "Night Light", unit_price: 5000) }
+    let!(:napkin_holder) { bob.items.create!(name: "Napkin Holder", description: "Stainless Steel Napkin Holder", unit_price: 2500) }
+    let!(:window_planter) { bob.items.create!(name: "Window Planter", description: "Plastic Window Planter", unit_price: 2500) }
 
     before (:each) do 
+      InvoiceItem.create!(invoice_id: invoice1.id, item_id: coochie_copi.id, status: 0)
+      InvoiceItem.create!(invoice_id: invoice2.id, item_id: napkin_holder.id, status: 1)
+      InvoiceItem.create!(invoice_id: invoice3.id, item_id: window_planter.id, status: 2)
+      
       visit merchant_dashboard_index_path(bob.id)
     end
 
@@ -30,15 +39,22 @@ RSpec.describe Merchant, type: :feature do
         end
       end
 
-      context 'in the section for Items Ready to Ship' do 
+      # context 'in the section for Items Ready to Ship' do 
  #user 4
         it 'displays a list of names of all items that have been ordered but not shipped' do
+          save_and_open_page
+          within "#ready_items" do 
+            expect(page).to have_content("Items Ready to Ship")
+            expect(page).to have_content("Coochie Copi Night Light")
+            expect(page).to have_content("Napkin Holder")
 
+            expect(page).to_not have_content("Window Planter")
+          end
         end
         it 'displays the id of the invoice that ordered that item'
 
         it 'displays invoice ids as a link to my merchants invoice show page'
-      end
+      # end
     end
   end
 end
