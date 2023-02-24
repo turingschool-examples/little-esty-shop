08ca5bb1148225fcd6e67f9fcf52b2d500da8182
Huy Phan <huyphan520@gmail.com>
2023-02-24 16:57:28 -0600

    merge from main

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,5 +5,13 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
 
-
+  def mech_top_5_successful_customers
+    binding.pry
+    joins(:transactions)
+    .select("customers.*, COUNT(transactions.id) AS mech_transaction_count")
+    .where(transactions: {result: 0})
+    .group(:id)
+    .order("mech_transaction_count DESC")
+    .limit(5)
+  end
 end
