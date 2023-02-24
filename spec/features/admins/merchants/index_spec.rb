6ea0b27f3e6a32c9c33441f4ae9e9151cba3b67d
Dawson Timmons <DawsonTimmons@gmail.com>
2023-02-24 15:15:17 -0700

    test: Add Admin merchant show page
    Co-authored-by: Adam Bailey <ab67319gmail.com>

--- spec/features/admins/merchants/index_spec.rb
+++ spec/features/admins/merchants/index_spec.rb
@@ -1,19 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "The Admin Merchants Index" do
+  before(:each) do
+    @merchant_1 = create(:merchant)
+    @merchant_2 = create(:merchant)
+    @merchant_3 = create(:merchant)
+    @merchant_4 = create(:merchant)
+    
+    visit admin_merchants_path
+  end
   describe "User Story 24" do
     it "When an admin visits the merchants index (/admin/merchants) they see the name of each merchant in the system" do
-      merchant_1 = create(:merchant)
-      merchant_2 = create(:merchant)
-      merchant_3 = create(:merchant)
-      merchant_4 = create(:merchant)
-      
-      visit admin_merchants_path
 
-      expect(page).to have_content(merchant_1.name)
-      expect(page).to have_content(merchant_2.name)
-      expect(page).to have_content(merchant_3.name)
-      expect(page).to have_content(merchant_4.name)
+      expect(page).to have_content(@merchant_1.name)
+      expect(page).to have_content(@merchant_2.name)
+      expect(page).to have_content(@merchant_3.name)
+      expect(page).to have_content(@merchant_4.name)
+    end
+  end
+
+  describe "User Story 25" do
+    it "When an admin visits the merchants index (/admin/merchants) and they click on the merchant's name, they are taken to that merchant's show page And they see the name of that merchant" do
+      click_link(@merchant_1.name)
+
+      expect(current_path).to eq(admin_merchant_path(@merchant_1))
+      expect(page).to have_content(@merchant_1.name)
+      expect(page).to_not have_content(@merchant_2.name)
     end
   end
 end
