995889010f5efda016c55a4e741bfc5707dc228b
Harrison Ryan <hwryan12@gmail.com>
2023-02-23 18:30:30 -0600

    feature: Create test for User Story 3 Co-authored-by: Chris Crane <chrisjcrane@gmail.com>

--- spec/features/merchants/dashboard/index_spec.rb
+++ spec/features/merchants/dashboard/index_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe "Dashboard", type: :feature do
   end
 
   describe "User story 1" do
-    it "As a merchant, when I visit my merchant dashboard 
-      (/merchants/:merchant_id/dashboard) then I see the name of my merchant" do
+    describe "When I visit my merchant dashboard" do
+      it "As a merchant, when I visit my merchant dashboard 
+        (/merchants/:merchant_id/dashboard) then I see the name of my merchant" do
 
-      expect(page).to have_content("Trader Bob's")
+        expect(page).to have_content("Trader Bob's")
+      end
     end
   end
 
@@ -25,6 +27,32 @@ RSpec.describe "Dashboard", type: :feature do
       it "show a link to my merchant invoices index (/merchants/merchant_id/invoices)" do
         
         expect(page).to have_link("Invoices", href: "/merchants/#{@merchant.id}/invoices")
+      end
+    end
+  end
+
+  describe "User story 3" do
+    describe "When I visit my merchant dashboard" do
+      it "I see the names of the top 5 customers who have conducted 
+        the largest number of successful transactions with my merchant" do
+       
+        expect(page).to have_content("customer1_name")
+        expect(page).to have_content("customer2_name")
+        expect(page).to have_content("customer3_name")
+        expect(page).to have_content("customer4_name")
+        expect(page).to have_content("customer5_name")
+        expect(page).to_not have_content("customer6_name")
+      end
+
+      it "next to each customer name I see the number of successful transactions they have
+        conducted with my merchant" do
+        
+        expect(page).to have_content("customer1_transactions")
+        expect(page).to have_content("customer2_transactions")
+        expect(page).to have_content("customer3_transactions")
+        expect(page).to have_content("customer4_transactions")
+        expect(page).to have_content("customer5_transactions")
+        expect(page).to_not have_content("customer6_transactions")
       end
     end
   end
