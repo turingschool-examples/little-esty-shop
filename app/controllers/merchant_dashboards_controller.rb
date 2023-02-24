4c84f4667259c680a1107da7d26ea4e67729afc9
Adam Bailey <ab67319@gmail.com>
2023-02-23 19:36:34 -0700

    feat: create functionality for user story 3, adding top 5 customers by succesful transactions
    Co-authored-by: Harrison Ryan <hwryan12@gmail.com>
    Co-authored-by: Dawson Timmons <dawsontimmons@gmail.com>
    Co-authored-by: Chris Crane <chrisjcrane@gmail.com>

--- app/controllers/merchant_dashboards_controller.rb
+++ app/controllers/merchant_dashboards_controller.rb
@@ -1,6 +1,6 @@
 class MerchantDashboardsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @top_five_customers = Customer.top_five_customers
+    @top_five_merchant_customers = @merchant.top_five_merchant_customers
   end
 end
