028bba931b32c7c756a225cead1621d047e420d6
Bobby Luly <bobbyluly@gmail.com>
2023-02-24 18:35:02 -0700

    Feat: add edit and update actions, flash success message, and item strong params

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -1,11 +1,34 @@
 class Merchants::ItemsController < ApplicationController
   def index
-   @merchant = Merchant.find(params[:merchant_id])
-   @items = @merchant.items
+    @merchant = Merchant.find(params[:merchant_id])
+    @items = @merchant.items
   end
 
   def show
-   @merchant = Merchant.find(params[:merchant_id])
-   @item = @merchant.items.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items.find(params[:id])
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = @merchant.items.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    item = merchant.items.find(params[:id])
+    
+    item.update(item_params)
+    flash[:success] = "Item Successfully Updated!"
+    redirect_to merchant_item_path(merchant.id, item.id)
+    # else
+    #   flash[:notice] = error_message(item.errors)
+		# 	render :edit
+    # end
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
   end
 end
