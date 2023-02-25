59cfae85b5e440d231c1d609211d3452ef6c06e1
Harrison Ryan <hwryan12@gmail.com>
2023-02-25 17:06:33 -0600

    feature: Create methods #enabled and #disabled and pass tests Co-authored-by: Chris Crane <chrisjcrane@gmail.com>

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.enabled
+    where(status: "Enabled")
+  end
+
+  def self.disabled
+    where(status: "Disabled")
+  end
 end
