f8e9e29a599d31087ec495ec2802743dff6c4798
Dawson Timmons <DawsonTimmons@gmail.com>
2023-02-24 18:57:39 -0700

    test: Add admin merchant enable/disable
    Co-authored-by: Adam Bailey <ab67319gmail.com>

--- spec/features/admins/merchants/index_spec.rb
+++ spec/features/admins/merchants/index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "The Admin Merchants Index" do
   before(:each) do
     @merchant_1 = create(:merchant)
-    @merchant_2 = create(:merchant)
+    @merchant_2 = create(:merchant, status: 1)
     @merchant_3 = create(:merchant)
     @merchant_4 = create(:merchant)
     
@@ -26,6 +26,34 @@ RSpec.describe "The Admin Merchants Index" do
       expect(current_path).to eq(admin_merchant_path(@merchant_1))
       expect(page).to have_content(@merchant_1.name)
       expect(page).to_not have_content(@merchant_2.name)
+    end
+  end
+
+  describe "User Story 27" do
+    it "shows a button next to each merchant to disable or enable that merchant. When I click this button, I am redirected back to the admin merchants index, and I see that the merchant's status has changed" do
+
+      within("#Merchant-#{@merchant_1.id}")  {
+        expect(page).to_not have_button("Enable")
+        expect(page).to have_button("Disable")
+        click_button "Disable"
+      }
+ 
+      within("#Merchant-#{@merchant_1.id}")  {
+        expect(page).to have_button("Enable")
+      }
+
+      expect(current_path).to eq(admin_merchants_path)
+
+      within("#Merchant-#{@merchant_2.id}")  {
+        expect(page).to have_button("Enable")
+        expect(page).to_not have_button("Disable")
+
+        click_button "Enable"
+      }
+      
+      within("#Merchant-#{@merchant_2.id}")  {
+        expect(page).to have_button("Disable")
+      }
     end
   end
 end
