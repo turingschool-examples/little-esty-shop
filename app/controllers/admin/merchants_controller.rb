928bdafac898c2a7cdebc106c486bf5271aea933
Adam Bailey <ab67319@gmail.com>
2023-02-24 17:12:24 -0700

    test: create functionality for user story 26, Admin Merchant Update
    Co-authored-by: Dawson Timmons <dawsontimmons@gmail.com>

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,4 +6,27 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+    @merchant.update(merchant_params)
+    
+    if @merchant.save
+      flash[:notice] = "#{@merchant.name} Information has been Updated"
+      redirect_to admin_merchant_path(@merchant)
+    else
+      flash[:notice] = "Unable to Update - Missing Information"
+      redirect_to edit_admin_merchant_path(@merchant)
+    end
+
+  end
+
+  private
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
