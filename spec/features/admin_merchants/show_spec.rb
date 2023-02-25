37e55b4bd6165a9a1a3417227307f0e77704402e
Weston Sandfort <sandfortw@gmail.com>
2023-02-24 20:47:30 -0700

    run rubocop on admin merchants show spec

--- spec/features/admin_merchants/show_spec.rb
+++ spec/features/admin_merchants/show_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'Admin Merchants show page' do
@@ -10,20 +12,17 @@ describe 'Admin Merchants show page' do
       it "I am taken to that merchant's admin show page (/admin/merchants/merchant_id) and I see the name of that merchant" do
         visit admin_merchants_path
 
-        click_link("#{@merchant_1.name}")
-     
+        click_link(@merchant_1.name.to_s)
+
         expect(current_path).to eq(admin_merchant_path(@merchant_1))
-        expect(page).to have_content("#{@merchant_1.name}")
-        expect(page).to_not have_content("#{@merchant_2.name}")
+        expect(page).to have_content(@merchant_1.name.to_s)
+        expect(page).to_not have_content(@merchant_2.name.to_s)
       end
     end
 
     describe 'link to update a merchant' do
-      before do
-        visit admin_merchant_path(@merchant_1.id)
-      end
-
       it 'takes me to the admin merchant edit page' do
+        visit admin_merchant_path(@merchant_1.id)
         click_link('Update Merchant')
         expect(current_path).to eq(edit_admin_merchant_path(@merchant_1.id))
       end
