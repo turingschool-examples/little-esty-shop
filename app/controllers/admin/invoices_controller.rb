8e4d97ab32f8dbc66a43fb4603109e63b0a08c4a
Matt Enyeart <menyeart1@gmail.com>
2023-02-25 12:32:43 -0700

    Create invoice items and attirbute method

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -2,6 +2,7 @@ class Admin::InvoicesController < ApplicationController
 
   def show
     @invoice = Invoice.find(params[:id])
+    @invoice_items_and_attributes = @invoice.items_with_invoice_attributes
   end
 
   def index
