3eb042aaa0f807dc28e6df6cb4a4d7a048d4fb2c
Melony Franchini <meltravelz@gmail.com>
2023-02-25 18:27:24 -0700

    Add within test/div in view

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Admin Invoice Show Page' do
   before(:each) do 
-    @merchant1 = Merchant.create!(name: "Mel's Travels", uuid: 1) 
-    @merchant2 = Merchant.create!(name: "Hady's Beach Shack", uuid: 2) 
+    @merchant1 = Merchant.create!(name: "Mel's Travels") 
+    @merchant2 = Merchant.create!(name: "Hady's Beach Shack") 
 
     @item1 = Item.create!(name: "Salt", description: "it is salty", unit_price: 1200, merchant: @merchant1)
     @item2 = Item.create!(name: "Pepper", description: "it is peppery", unit_price: 1150, merchant: @merchant1)
@@ -24,10 +24,10 @@ RSpec.describe 'Admin Invoice Show Page' do
     InvoiceItem.create!(item: @item3, invoice: @invoice3, quantity: 1, unit_price: 1500)
 
     @invoice4 = Invoice.create!(customer: @customer_1, status: 1) #completed
-    InvoiceItem.create!(item: @item1, invoice: @invoice4, quantity: 1, unit_price: 1000)
+    InvoiceItem.create!(item: @item1, invoice: @invoice4, quantity: 2, unit_price: 1000)
     InvoiceItem.create!(item: @item2, invoice: @invoice4, quantity: 1, unit_price: 1500)
-    InvoiceItem.create!(item: @item5, invoice: @invoice4, quantity: 1, unit_price: 2000)
-    InvoiceItem.create!(item: @item4, invoice: @invoice4, quantity: 1, unit_price: 5000)
+    InvoiceItem.create!(item: @item5, invoice: @invoice4, quantity: 4, unit_price: 2000)
+    InvoiceItem.create!(item: @item6, invoice: @invoice4, quantity: 5, unit_price: 5000)
   end 
 
   describe "as an admin" do 
@@ -41,19 +41,20 @@ RSpec.describe 'Admin Invoice Show Page' do
     # User Story 34
     it "I see the name of all the items on that invoice" do
       visit "/admin/invoices/#{@invoice4.id}"
-      expect(page).to have_content("Items on Invoice")
 
+      expect(page).to have_content("Items on Invoice")
       expect(page).to have_content(@item1.name)
       expect(page).to have_content(@item2.name)
-      expect(page).to have_content(@item4.name)
       expect(page).to have_content(@item5.name)
+      expect(page).to have_content(@item6.name)
     end
 
     # User Story 34
-    xit "I also see the quantity of each item ordered, price sold for, & the invoice item status " do
+    it "I also see the quantity of each item ordered, price sold for, & the invoice item status " do
       visit "/admin/invoices/#{@invoice4.id}"
-      within "#item_info-#{@item1.id}" do
-        expect(page).to have_content(@item1.name)
+      
+      within "#item-#{@item6.id}" do
+        expect(page).to have_content(@item6.name)
       end
     end
   end
