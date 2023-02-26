8e75856a6bb143e64a6216a083a0ead052009c8b
Sam Walker <sgwalker327@gmail.com>
2023-02-25 22:27:05 -0700

    Slightly clean up items controller and action refactor update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,21 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def update
-    # require 'pry'; binding.pry
-    merchant = Merchant.find(params[:merchant_id])
     item = Item.find(params[:item_id])
     item.update(item_params)
-    # require 'pry'; binding.pry
-    redirect_to "/merchants/#{merchant.id}/items/#{item.id}"
+    redirect_to "/merchants/#{item.merchant_id}/items/#{item.id}"
   end
 
   def status_update
-    # merchant = Merchant.find(params[:merchant_id])
     item = Item.find(params[:item_id])
-    # require 'pry'; binding.pry
     item.update!(status: params[:status])
     item.save
-    # require 'pry'; binding.pry
     redirect_to "/merchants/#{item.merchant_id}/items"
   end
 
