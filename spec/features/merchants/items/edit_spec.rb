e264f353302eb13fecd9547b7185cb5c7c8befe9
Chris Crane <chrisjcrane@gmail.com>
2023-02-26 12:40:21 -0700

    Resolving merge issues

--- spec/features/merchants/items/edit_spec.rb
+++ spec/features/merchants/items/edit_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe "Merchant_Items#Edit", type: :feature do
   before(:each) do
     @merchant = create(:merchant, name: "Trader Bob's")
 
-    @customer_1 = create(:customer)
-
     @invoice_1 = create(:invoice, customer_id: @customer_1.id)
 
     @item_1 = create(:item, merchant: @merchant)
@@ -20,14 +18,22 @@ RSpec.describe "Merchant_Items#Edit", type: :feature do
 
   describe "User Story 8" do
     it "shows a form filled in with the existing attribute information" do
-      # save_and_open_page
       expect(page).to have_field("Name", with: @item_1.name)
       expect(page).to have_field("Description", with: @item_1.description)
       expect(page).to have_field("Unit price", with: @item_1.unit_price)
     end
+    
+    it "allows me to update the item info and submit the form" do
+      fill_in :item_name, with: "Updated Name"
+      fill_in :item_description, with: "Updated Description"
+      fill_in :item_unit_price, with: "3"
+      save_and_open_page
+      click_button "Update #{@item_1.name}"
 
-    xit "allows me to update the item info and submit the form" do
-
+      expect(current_path).to eq("/merchants/#{@merchant.id}/items/#{@item_1.id}")
+      expect(page).to have_content("Updated Name")
+      expect(page).to have_content("Updated Description")
+      expect(page).to have_content("3")
     end
   end
 end
