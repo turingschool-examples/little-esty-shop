ad378f6861671dc34ceedc3ebbd2403aeb78f7b2
Meredith Trevino <meredithtrevino@gmail.com>
2023-02-25 19:43:14 -0700

    adds completed user story 16

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -8,5 +8,6 @@ class InvoicesController < ApplicationController
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @invoice = Invoice.find(params[:id])
+    @items_on_invoice = @invoice.items
   end
 end
