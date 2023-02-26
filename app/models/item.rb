d20337ab5fe0ad24d563e3d5ed525ffebbcb6d96
Sam Walker <sgwalker327@gmail.com>
2023-02-25 20:28:21 -0700

    clean models and add enums

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
-  enum :status, { disabled: 0, enabled: 1 }
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
+  
+  enum status: { disabled: 0, enabled: 1 }
 
   def item_invoice_id
     invoice_items.first.invoice_id
