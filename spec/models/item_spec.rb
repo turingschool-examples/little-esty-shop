48281bf0057358f612f8d1e398b89cd8978e9a97
Harrison Ryan <hwryan12@gmail.com>
2023-02-26 20:00:47 -0600

    feature: Create test for item model method #item_best_day Co-authored-by: Chris Crane <chrisjcrane@gmail.com>

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,12 +72,17 @@ RSpec.describe Item, type: :model do
       create(:transaction, invoice_id: @invoice_12.id, result: 0)
 
       @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, unit_price: 900, quantity: 3)
-      @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, unit_price: 900, quantity: 3)
       @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2, unit_price: 800, quantity: 3)
       @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice_3, unit_price: 700, quantity: 3)
       @invoice_item_4 = create(:invoice_item, item: @item_4, invoice: @invoice_4, unit_price: 600, quantity: 3)
       @invoice_item_5 = create(:invoice_item, item: @item_5, invoice: @invoice_5, unit_price: 500, quantity: 3)
       @invoice_item_6 = create(:invoice_item, item: @item_6, invoice: @invoice_6, unit_price: 400, quantity: 3)
+      @invoice_item_7 = create(:invoice_item, item: @item_1, invoice: @invoice_7, unit_price: 900, quantity: 2)
+      @invoice_item_8 = create(:invoice_item, item: @item_2, invoice: @invoice_8, unit_price: 800, quantity: 2)
+      @invoice_item_9 = create(:invoice_item, item: @item_3, invoice: @invoice_9, unit_price: 700, quantity: 2)
+      @invoice_item_10 = create(:invoice_item, item: @item_4, invoice: @invoice_10, unit_price: 600, quantity: 2)
+      @invoice_item_11 = create(:invoice_item, item: @item_5, invoice: @invoice_11, unit_price: 500, quantity: 2)
+      @invoice_item_12 = create(:invoice_item, item: @item_6, invoice: @invoice_12, unit_price: 400, quantity: 2)
     end
 
     describe '::item_best_day' do
