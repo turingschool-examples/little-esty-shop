fb6d644359814b2a0ef122b11dacee862f20201e
Jade Stewart <jadekstewart3@gmail.com>
2023-02-27 11:52:18 -0700

    Add enums with default

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :show, :edit, :update], controller: "merchants/items"
     resources :invoices, only: [:index, :show], controller: "merchants/invoices"
   end
-
+  
   namespace :admin do
     get '/', to: 'dashboard#index'
     resources :merchants, only: [:index, :show]
