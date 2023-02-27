844583bae07c299e01c2ebf90834da5043be29fd
Jade Stewart <jadekstewart3@gmail.com>
2023-02-27 11:53:03 -0700

    Add association realted to user story 3

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,8 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices, through: :items
   has_many :invoice_items, through: :items
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
 
   def merchant_invoices
     invoices.distinct.order(:id)
