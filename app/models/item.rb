99e79ed14718646e4e53c708068e64e22561a764
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-02-27 12:07:29 -0700

    Add: model test and action for creating a new item

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,9 @@ class Item < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :description
   validates_presence_of :unit_price
+
+  def self.create_new_item(item_params)
+    create(item_params)
+  end
+
 end
