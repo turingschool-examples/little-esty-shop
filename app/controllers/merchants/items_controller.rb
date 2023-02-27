c18d1945cca47398a27070b43c6f520ed63a0894
Jade Stewart <jadekstewart3@gmail.com>
2023-02-27 11:54:45 -0700

    Add to update method to allow for enabling and disabling of items

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -17,18 +17,23 @@ class Merchants::ItemsController < ApplicationController
   def update
     merchant = Merchant.find(params[:merchant_id])
     item = merchant.items.find(params[:id])
-    
-    if item.update(item_params)
-      flash[:success] = "Item Successfully Updated!"
-      redirect_to merchant_item_path(merchant.id, item.id)
+
+    if item_params[:status].present?
+      item.update!(item_params)
+      redirect_to merchant_items_path(merchant.id)
     else
-      flash[:notice] = error_message(item.errors)
-      redirect_to edit_merchant_item_path(merchant.id, item.id)
+      if item.update(item_params)
+        flash[:success] = "Item Successfully Updated!"
+        redirect_to merchant_item_path(merchant.id, item.id)
+      else
+        flash[:notice] = error_message(item.errors)
+        redirect_to edit_merchant_item_path(merchant.id, item.id)
+      end
     end
   end
 
   private
   def item_params
-    params.permit(:name, :description, :unit_price, :merchant_id)
+    params.permit(:name, :description, :unit_price, :merchant_id, :status)
   end
 end
