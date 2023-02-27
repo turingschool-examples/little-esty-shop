c9222f1f62e20225fa62ed34ec9c923257307181
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-26 22:09:13 -0700

    Test: add test for displaying a form with fields to create a new merchant

---
+++ spec/features/admin/merchants/new_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin merchant new page' do 
+  describe 'when I visit a admin merchant new page' do 
+    it 'will have a form to create a new merchant' do 
+      new_admin_merchant_path
+
+      expect(page).to have_field('Name')
+    end
+  end
+end
