4e487b0ea311b064bd3d076a17b754d05111440a
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-26 22:15:56 -0700

    Feat: add functionality to display a form on the admin merchants new page

--- spec/features/admin/merchants/new_spec.rb
+++ spec/features/admin/merchants/new_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Admin merchant new page' do 
   describe 'when I visit a admin merchant new page' do 
     it 'will have a form to create a new merchant' do 
-      new_admin_merchant_path
-
-      expect(page).to have_field('Name')
+      visit new_admin_merchant_path
+    
+      expect(page).to have_field('Name:')
+      expect(page).to have_button("Create Merchant")
     end
   end
 end
