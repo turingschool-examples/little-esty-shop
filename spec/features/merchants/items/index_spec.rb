7e9636a135cf51787a651401815e282ce7e755a5
Conner VL <88596340+C-V-L@users.noreply.github.com>
2023-02-26 18:24:28 -0700

    Revert "Revert "Andra/12 user story""

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -115,23 +115,67 @@ RSpec.describe 'Merchant Items Index' do
 
     describe '5 Most Popular Items' do
       before do
-        @merchant1 = Merchant.create(name: "Handmades")
-        @merchant2 = Merchant.create(name: "Candles")
+        @merchant = Merchant.create(name: "Handmades")
 
-        @customer1 = FactoryBot.create(:customer)
-        @customer2 = FactoryBot.create(:customer)
-        @customer3 = FactoryBot.create(:customer)
+        @cust1 = FactoryBot.create(:customer)
+        @cust2 = FactoryBot.create(:customer)
 
-        # @inv1 = @customer1.invoices.create!()
-        
+        @inv1 = @cust1.invoices.create!(status: 1)
+        @inv2 = @cust1.invoices.create!(status: 1)
+        @inv3 = @cust1.invoices.create!(status: 1)
+        @inv4 = @cust2.invoices.create!(status: 1)
+        @inv5 = @cust2.invoices.create!(status: 1)
+        @inv6 = @cust2.invoices.create!(status: 1)
+
+        @trans1 = @inv1.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans1_5 = @inv1.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans2 = @inv2.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans3 = @inv2.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans4 = @inv3.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans5 = @inv3.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans6 = @inv3.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans7 = @inv4.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans8 = @inv4.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans9 = @inv4.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans10 = @inv4.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans11 = @inv5.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans12 = @inv5.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans13 = @inv5.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans14 = @inv5.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans15 = @inv5.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+        @trans16 = @inv6.transactions.create!(credit_card_number: 5555555555555555, credit_card_expiration_date: nil, result: 0)
+
+        @bowl = @merchant.items.create!(name: "Bowl", description: "it's a bowl", unit_price: 350) 
+        @knife = @merchant.items.create!(name: "Knife", description: "it's a knife", unit_price: 300) 
+        @spoon = @merchant.items.create!(name: "Spoon", description: "it's a spoon", unit_price: 275) 
+        @plate = @merchant.items.create!(name: "Plate", description: "it's a plate", unit_price: 250) 
+        @fork = @merchant.items.create!(name: "Fork", description: "it's a fork", unit_price: 100) 
+        @pan = @merchant.items.create!(name: "Pan", description: "it's a pan", unit_price: 250) 
+          
+        InvoiceItem.create!(item_id: @bowl.id, invoice_id: @inv1.id, quantity: 10, unit_price: 350, status: 1)
+        InvoiceItem.create!(item_id: @knife.id, invoice_id: @inv1.id, quantity: 9, unit_price: 300, status: 1)
+        InvoiceItem.create!(item_id: @plate.id, invoice_id: @inv1.id, quantity: 10, unit_price: 201, status: 1)
+        InvoiceItem.create!(item_id: @spoon.id, invoice_id: @inv1.id, quantity: 8, unit_price: 2500, status: 1)
+        InvoiceItem.create!(item_id: @fork.id, invoice_id: @inv5.id, quantity: 1, unit_price: 14, status: 1)
+        InvoiceItem.create!(item_id: @pan.id, invoice_id: @inv6.id, quantity: 6, unit_price: 15, status: 1)
       end
 
       it 'I see the names of the top 5 most popular items ranked by total revenue generated' do
 
         visit "/merchants/#{@carlos.id}/items"
 
-        within "#top-items" do
+        within "div#top-items" do
           expect(page).to have_content("Top Items")
+          expect("Spoon").to appear_before("Bowl")
+          expect("Bowl").to appear_before("Knife")
+          expect("Knife").to appear_before("Plate")
+          expect("Plate").to appear_before("Pan")
+
+          expect("Revenue: 20000").to appear_before("Revenue: 3500")
+          expect("Revenue: 3500").to appear_before("Revenue: 2700")
+          expect("Revenue: 2700").to appear_before("Revenue: 2010")
+          expect("Revenue: 2010").to appear_before("Revenue: 90")
+
         end
       end
     end
