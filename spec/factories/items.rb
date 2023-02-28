f94cd57fa026d40b6deadb38a6822accb98cc529
Matt Enyeart <menyeart1@gmail.com>
2023-02-28 15:08:33 -0700

    Revert item description to sentence to prevent dup descriptions

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
     name {Faker::Lorem.sentence(word_count: 2)}
-    description {Faker::Books::Dune.quote}
+    description {Faker::Lorem.sentence(word_count:17)}
     unit_price {Faker::Number.number(digits: 5)}
     merchant_id {Faker::Number.within(range: 1..100)}
   end
