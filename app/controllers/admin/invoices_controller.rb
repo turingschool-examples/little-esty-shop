e6888c141b6369457611db175d3d1d0151488805
Matt Enyeart <menyeart1@gmail.com>
2023-02-27 17:35:00 -0700

    Add statuses instance variable to admin invoices controller show action

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -4,9 +4,21 @@ class Admin::InvoicesController < ApplicationController
     @invoice = Invoice.find(params[:id])
     @invoice_items_and_attributes = @invoice.items_with_invoice_attributes
     @total_revenue = @invoice.calc_total_revenue.first
+    @statuses = ["cancelled", "in_progress", "completed"]
   end
 
   def index
     @invoices = Invoice.all
+  end
+
+  def update
+    invoice = Invoice.find(params[:id])
+    invoice.update(invoice_params)
+    redirect_to 
+  end
+
+  private
+  def invoice_params
+    params.permit(:status)
   end
 end
