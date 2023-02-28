340be19648526c816a458506f19ab0af8aa8b20c
Dawson Timmons <DawsonTimmons@gmail.com>
2023-02-28 14:33:40 -0700

    test: Add Invoice item information, user story 34
    Co-authored-by: Adam Bailey <ab67319@gmail.com>

--- spec/features/admins/invoices/show_spec.rb
+++ spec/features/admins/invoices/show_spec.rb
@@ -15,4 +15,45 @@ RSpec.describe "Admin Invoice Show Page" do
       expect(page).to have_content("Adam Bailey")
     end
   end
+
+  describe "User Story 34" do
+    it "When I visit an admin invoice show page, then I see all of the items on the invoice including: Item name, The quantity of the item ordered, The price the Item sold for
+    The Invoice Item status" do
+
+      @merchant = create(:merchant, name: "Trader Bob's")
+      
+      @customer = create(:customer, first_name: "Adam", last_name: "Bailey")
+
+      @item_1 = create(:item, name: "Jar", merchant: @merchant)
+      @item_2 = create(:item, name: "La Croix", merchant: @merchant)
+      @item_3 = create(:item, name: "Sunglasses", merchant: @merchant)
+
+      @invoice = create(:invoice, customer: @customer, status: 0, created_at: Date.new(2023,1,1))
+
+      @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice, quantity: 2, unit_price: 299, status: "pending")
+      @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice, quantity: 5, unit_price: 669, status: "packaged")
+      @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice, quantity: 14, unit_price: 2420, status: "shipped")
+
+      visit admin_invoice_path(@invoice)
+
+      within("#invoice_item-#{@invoice_item_1.id}") {
+        expect(page).to have_content("Jar")
+        expect(page).to have_content("2")
+        expect(page).to have_content("$2.99")
+        expect(page).to have_content("pending")
+      }
+      within("#invoice_item-#{@invoice_item_2.id}") {
+        expect(page).to have_content("La Croix")
+        expect(page).to have_content("5")
+        expect(page).to have_content("$6.69")
+        expect(page).to have_content("packaged")
+      }
+      within("#invoice_item-#{@invoice_item_3.id}") {
+        expect(page).to have_content("Sunglasses")
+        expect(page).to have_content("14")
+        expect(page).to have_content("$24.20")
+        expect(page).to have_content("shipped")
+      }
+    end
+  end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
+  describe "instance methods" do 
+    describe "#invoice_items_and_names" do
+      it "returns us all invoice items and item names" do
+        @merchant = create(:merchant, name: "Trader Bob's")
+      
+        @customer = create(:customer, first_name: "Adam", last_name: "Bailey")
 
+        @item_1 = create(:item, name: "Jar", merchant: @merchant)
+        @item_2 = create(:item, name: "La Croix", merchant: @merchant)
+        @item_3 = create(:item, name: "Sunglasses", merchant: @merchant)
+
+        @invoice = create(:invoice, customer: @customer, status: 0, created_at: Date.new(2023,1,1))
+
+        @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice, quantity: 2, unit_price: 299, status: "pending")
+        @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice, quantity: 5, unit_price: 669, status: "packaged")
+        @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice, quantity: 14, unit_price: 2420, status: "shipped")
+        
+        expected_result = @invoice.invoice_items_and_names
+        
+        expect(expected_result).to contain_exactly(@invoice_item_1, @invoice_item_2, @invoice_item_3)
+
+        expect(expected_result[0].name).to eq("Jar")
+        expect(expected_result[1].name).to eq("La Croix")
+        expect(expected_result[2].name).to eq("Sunglasses")
+      end
+    end
+  end
 end
