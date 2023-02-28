f3f059ce932e903822e871d9c6950a3e0a8c4b35
Hady Matar <hadyematar@gmail.com>
2023-02-28 14:22:48 -0700

    Updated to functioning test

--- app/poros/commits/commit.rb
+++ app/poros/commits/commit.rb
@@ -1,8 +1,9 @@
+require 'pry'; binding.pry
 class Commit #this is like the outline of the ruby object- the blueprint. DEFINES THE ATTRIBUTES 
       # attr_reader :commi
 
   def initialize(data)
-
+    @author = data[:commit][:author][:name] 
   end
 
   def total_count 
