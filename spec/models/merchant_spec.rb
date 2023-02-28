9fd8f5690248e9df1ce33394d0c78d2002117331
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-02-27 18:36:06 -0800

    Update models to include all relationship tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Merchant, type: :model do
 
   describe 'relationships' do
     it { should have_many :items }
+    it { should have_many(:invoice_items).through(:items) }
+    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:transactions).through(:invoices) }
+    it { should have_many(:customers).through(:invoices) }
 		it { should define_enum_for(:status).with_values(["active", "disabled"])}
   end
 
