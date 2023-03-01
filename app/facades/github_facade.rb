2602a5981e71ac729bba3e91205d816bdd1009cf
Hady Matar <hadyematar@gmail.com>
2023-03-01 16:43:50 -0700

    Completed commits

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -3,11 +3,10 @@ class GithubFacade
   def self.info_hash
    info = {
     repo_name: get_repo_name,
-    contributors: get_contributors 
-    # commits: get_commits,
+    contributors: get_contributors,  
+    commits: get_commits,
     pr_count: get_pr_count
     }
-
    GithubInfo.new(info)
   end
 
@@ -16,19 +15,22 @@ class GithubFacade
     git_name[:name]
   end
 
-  # def self.get_contributors
-  #   contributors = GithubService.fetch_api("/contributors")
-  #   contributors.map do |contributor|
-  #     contributor[:login]
-  #   end
-  # end
+  def self.get_contributors
+    contributors = GithubService.fetch_api("/contributors")
+    contributors.map do |contributor|
+      contributor[:login]
+    end
+  end
 
-  # def self.get_commits
-  #   commits = GithubService.fetch_api("/commits")
-  #   commits.map do |commit|
-  #     commit
-  #   end
-  # end
+  def self.get_commits
+    commits = GithubService.fetch_api("/stats/contributors")
+    new_hash = {}
+    commits.map do |individual|
+      new_hash[individual[:author][:login]]= individual[:total]
+    end
+    return new_hash
+  end
+
 
   def self.get_pr_count
     all_closed_prs = GithubService.fetch_api("/pulls?state=closed")
--- app/poros/github_info.rb
+++ app/poros/github_info.rb
@@ -1,10 +1,11 @@
 class GithubInfo
-  attr_reader :repo_name, :contributors, :pr_count
+  attr_reader :repo_name, :contributors, :pr_count, :commits, :commits2, :commits3
   
   def initialize(info)
     @repo_name = info[:repo_name]
-    # @contributors = info[:contributors]
-    # @commits = info[:commits]
+    @contributors = info[:contributors]
+    @commits = info[:commits]
     @pr_count = info[:pr_count]
   end
+
 end
