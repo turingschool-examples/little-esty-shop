fb9a8ceb416d935c645a048a91bd6e6e4fa60e17
Tori Enyart <torienyart@gmail.com>
2023-03-01 16:01:24 -0700

    input refreshed auth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  # before_action :github_info
+  # add_flash_types :info, :error
+
+  # def github_info
+  #   @github_info = GithubFacade.info_hash
+  # end
+
+  # private
+
+  # def error_message(errors)
+  #   errors.full_messages.join(", ")
+  # end
 end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,24 +3,24 @@ require 'httparty'
 class GithubService
 
   def usernames
-    get_url("https://api.github.com/repos/torienyart/little-esty-shop/collaborators", headers: {"Authorization" => "Bearer #{ENV['github_token']}"})
+    get_url("https://api.github.com/repos/torienyart/little-esty-shop/collaborators", headers: {"Authorization" => "Bearer #{ENV["github_token"]}"})
   end
 
   def commits(username)
-    get_url("https://api.github.com/repos/torienyart/little-esty-shop/commits?author=#{username}&per_page=100", headers: {"Authorization" => "Bearer #{ENV['github_token']}"})
+    get_url("https://api.github.com/repos/torienyart/little-esty-shop/commits?author=#{username}&per_page=100", headers: {"Authorization" => "Bearer #{ENV["github_token"]}"})
   end
 
 
   def pulls
-    get_url("https://api.github.com/repos/torienyart/little-esty-shop/pulls?state=closed&per_page=100", headers: {"Authorization" => "Bearer #{ENV['github_token']}"})
+    get_url("https://api.github.com/repos/torienyart/little-esty-shop/pulls?state=closed&per_page=100", headers: {"Authorization" => "Bearer #{ENV["github_token"]}"})
   end
 
   def repo
-    get_url("https://api.github.com/repos/torienyart/little-esty-shop", headers: {"Authorization" => "Bearer #{ENV['github_token']}"})
+    get_url("https://api.github.com/repos/torienyart/little-esty-shop", headers: {"Authorization" => "Bearer #{ENV["github_token"]}"})
   end
 
   def collaborators
-    get_url("https://api.github.com/repos/torienyart/little-esty-shop/collaborators", headers: {"Authorization" => "Bearer #{ENV['github_token']}"})
+    get_url("https://api.github.com/repos/torienyart/little-esty-shop/collaborators", headers: {"Authorization" => "Bearer #{ENV["github_token"]}"})
   end
 
   def get_url(url, token) #Make a Get request
