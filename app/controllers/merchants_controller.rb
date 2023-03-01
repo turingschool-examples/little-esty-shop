1ba767cf7cdc947be22c063e0afc63ebe8808a9e
David Marino <davidjmarino8@gmail.com>
2023-03-01 18:17:37 -0500

    refactor

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -5,20 +5,4 @@ class MerchantsController < ApplicationController
     @mech_unshipped_invoice_items = @merchant.unshipped_items
     @mech_top_5_customers = @merchant.mech_top_5_successful_customers
   end
-
-  def create
- 
-    merchant = Merchant.new(merchant_params)
-
-    if merchant.save 
-       redirect_to admin_merchants_path
-    end 
-  end
-
-  def merchant_params
-    params[:merchant][:status].to_i
-    params.require(:merchant).permit(:name, :status)
-   
-  end
-
 end
