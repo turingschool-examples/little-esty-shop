e8d08fd7f69f362273fcb62d8aa0e7a687faf3f8
Meredith Trevino <meredithtrevino@gmail.com>
2023-02-28 22:28:36 -0700

    updates to attempt-- nothing new working

--- app/poros/repo_name_search.rb
+++ app/poros/repo_name_search.rb
@@ -2,7 +2,6 @@ require './app/services/github_service'
 require './app/poros/repo_name'
 
 class RepoNameSearch
-
   def repo_name_information
     service.repo_name.map do |data|
       RepoName.new(data)
