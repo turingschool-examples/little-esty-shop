6a7d6f2dc1bb1f5b772f8f12e20cbd7362b92527
Young Koh <youngheon6@gmail.com>
2023-04-10 17:08:22 -0600

    feat: migrations setup

---
+++ db/migrate/20230410224300_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      
+      t.string :name, null: false
+      t.string :description, null: false
+      t.integer :sell_price, null: false
+      t.timestamp :created_at, null: false
+      t.timestamp :updated_at, null: false
+      t.references :merchant, foreign_key: true, null: false
+    end
+  end
+end
