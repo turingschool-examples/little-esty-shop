614efb325b1b04e1ef70c40997d485cc07ad1435
Logan Wilson <loganwilson777@gmail.com>
2023-04-11 14:57:36 -0600

    mod coded our test data

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   it { should have_many(:invoices) }
+
+  before :each do
+    test_data
+    require 'pry'; binding.pry
+  end
 end
