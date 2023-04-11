dd0a9b77c51d4930d22f8393cce2544935030569
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-11 17:13:31 -0400

    update routes for us1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :merchants do
+    get 'dashboard', to:'merchant/dashboard#show', on: :member
+  end
 end
