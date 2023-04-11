968f8d11d050571d3b370bd78b39287904ceba3e
Logan Wilson <loganwilson777@gmail.com>
2023-04-11 16:24:35 -0600

    Delete unessesary pry

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,9 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   it { should have_many(:invoices) }
-
-  before :each do
-    test_data
-    require 'pry'; binding.pry
-  end
 end
