fc49ca57f5be986d21e8bc754ac6a7e8c83db014
Isaac Thill <ithill22@turing.edu>
2023-04-11 17:23:21 -0600

    remove save_and_open_page

--- spec/features/merchants/items/show_spec.rb
+++ spec/features/merchants/items/show_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'Merchant Items Show Page' do
       expect(page).to_not have_content(@item_9.name)
 
       visit merchant_item_path(@merchant_1, @item_9)
-save_and_open_page
+
       expect(page).to have_content(@item_9.name)
       expect(page).to have_content(@item_9.description)
       expect(page).to have_content(@item_9.unit_price_to_dollars)
