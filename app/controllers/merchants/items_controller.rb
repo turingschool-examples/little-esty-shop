43e9c517f740d8a85532c3356975aa6f34bd63f2
Isaac Thill <ithill22@turing.edu>
2023-04-11 16:59:10 -0600

    test: add test for user story 7

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -2,4 +2,9 @@ class Merchants::ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
+  end
 end
