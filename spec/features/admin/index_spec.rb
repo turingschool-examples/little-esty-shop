707b1e2f5a8b634fc5fc7208fb12c709fdd502ce
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-12 17:20:47 -0400

    test: add test for admin merchants index and admin invoices index links on dashboard (User Story 20)

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -2,11 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Admin Index (Dashboard) Page', type: :feature do
   before(:each) do
-    visit '/admin'
+    visit admin_path
   end
 
   it 'has a header' do
     expect(page).to have_content('Admin Dashboard')
     expect(page).to have_selector('h1')
+  end
+
+  it 'has links to the admin merchants index and admin invoices index' do
+    expect(page).to have_link('Admin Merchants Index')
+    expect(page).to have_link('Admin Invoices Index')
+
+    click_link('Admin Merchants Index')
+    expect(current_path).to eq(admin_merchants_path)
+
+    visit admin_path
+    click_link('Admin Invoices Index')
+    expect(current_path).to eq(admin_invoices_path)
   end
 end
