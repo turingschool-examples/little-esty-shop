d60cdcaf9af810fd14aa826337ec84775ffbf1c7
Boston Lowrey <bostonlowrey@gmail.com>
2023-04-11 21:31:59 -0400

    feat: add enum to some models and dependencies

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,4 @@
 class Merchant < ApplicationRecord
-  has_many :items
+  has_many :items, dependent: :destroy
   has_many :invoices, through: :items
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+  enum result: { failed: 0, success: 1 }
 end
