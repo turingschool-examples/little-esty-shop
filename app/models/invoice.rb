fc4ce5cc142b4cf154b9c50a4d5333b16d355508
Sean Cowans <zedesia@yahoo.com>
2023-04-12 15:10:54 -0600

    feat: Complete user story 5

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,8 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   enum status: { in_progress: 0, completed: 1, cancelled: 2 }
+
+  def custom_date
+    self.created_at.strftime("%A, %B %d, %Y")
+  end
 end
