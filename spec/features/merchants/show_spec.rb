d173bfbd26b455d56ae3c30989049f934eb54e62
Nick Sacco <nicksacco10@gmail.com>
2023-04-11 19:38:16 -0700

    test: update routes in tests

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe 'Merchant Show Dashboard Page', type: :feature do
   describe "As a merchant visiting '/merchants/:id/dashboard'" do
     it 'I see the name of my merchant' do
       visit merchant_dashboard_path(merchant1)
-      # save_and_open_page
       
       expect(page).to have_content('Steve')
     end
@@ -21,13 +20,16 @@ RSpec.describe 'Merchant Show Dashboard Page', type: :feature do
       visit merchant_dashboard_path(merchant1)
       
       expect(page).to have_link('My Items')
+      click_link("My Items")
+      expect(current_path).to eq(merchant_items_path(merchant1))
     end
     
     it 'has a link to merchant invoice index' do
       visit merchant_dashboard_path(merchant1)
       
       expect(page).to have_link('My Invoices')
-      save_and_open_page
+      click_link("My Invoices")
+      expect(current_path).to eq(merchant_invoices_path(merchant1))
     end
   end
 end
