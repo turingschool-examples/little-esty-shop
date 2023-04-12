7c267f684f46d78efeda7c9cc3b9af3ac62e7960
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-12 17:58:12 -0400

    test: set up test for User Story 21

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -21,4 +21,43 @@ RSpec.describe 'Admin Index (Dashboard) Page', type: :feature do
     click_link('Admin Invoices Index')
     expect(current_path).to eq(admin_invoices_path)
   end
+
+  it 'displays the top 5 customers with their names and number of successful transactions' do
+
+
+    within("#customer-#{.id}") do # add customer variable
+      expect(page).to have_content() #customer name 1
+      expect(page).to have_content() #add number of successful transactions
+      expect().to_appear_before()  #name to appear before transactions count
+    end
+
+    within("#customer-#{.id}") do # add customer variable
+      expect(page).to have_content() #customer name 2
+      expect(page).to have_content() #add number of successful transactions
+      expect().to_appear_before()  #name to appear before transactions count
+    end
+
+    within("#customer-#{.id}") do # add customer variable
+      expect(page).to have_content() #customer name 3
+      expect(page).to have_content() #add number of successful transactions
+      expect().to_appear_before()  #name to appear before transactions count
+    end
+
+    within("#customer-#{.id}") do # add customer variable
+      expect(page).to have_content() #customer name 4
+      expect(page).to have_content() #add number of successful transactions
+      expect().to_appear_before()  #name to appear before transactions count
+    end
+
+    within("#customer-#{.id}") do # add customer variable
+      expect(page).to have_content() #customer name 5
+      expect(page).to have_content() #add number of successful transactions
+      expect().to_appear_before()  #name to appear before transactions count
+    end
+  end
 end
+
+Then I see the names of the top 5 customers
+who have conducted the largest number of successful transactions
+And next to each customer name I see the number of successful transactions they have
+conducted
