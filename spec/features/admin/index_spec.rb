ac26b67a591a8c27d44e3edc2879541687a61928
Boston Lowrey <bostonlowrey@gmail.com>
2023-04-12 18:34:09 -0400

    added tests for the new links in the /admin

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -7,8 +7,27 @@ RSpec.describe '/admin', type: :feature do
 
   describe 'when I visit the admin dashboard page' do
     it 'I see a header indicating that I am on the admin dashboard' do
-      save_and_open_page
       expect(page).to have_content('Admin Dashboard')
+    end
+
+    it 'Then I see a link to the admin merchants index (/admin/merchants)' do
+      expect(page).to have_link('Merchants')
+
+      within('div#merchants') do
+        click_link 'Merchants'
+
+        expect(current_path).to eq('/admin/merchants')
+      end
+    end
+
+    it 'Then I see a link to the admin invoices index (/admin/invoices)' do
+      expect(page).to have_link('Invoices')
+
+      within('div#invoices') do
+        click_link 'Invoices'
+
+        expect(current_path).to eq('/admin/invoices')
+      end
     end
   end
 end
