8f3998f2a2b8c7890205b55bc5dc59892ebd95bc
Reilly Miller <reillyjmiller31@gmail.com>
2023-04-12 16:54:37 -0600

    write tests for the class methods

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe Item, type: :model do
     it { should have_many :invoice_items}
     it { should have_many(:invoices).through(:invoice_items)}
   end
+
+  describe "class methods" do
+    before do
+      @merchant_1 = FactoryBot.create(:merchant)
+      @item_1 = FactoryBot.create(:item, merchant: @merchant_1)
+      @item_2 = FactoryBot.create(:item, merchant: @merchant_1)
+      @item_3 = FactoryBot.create(:item, merchant: @merchant_1)
+      @item_4 = FactoryBot.create(:item, merchant: @merchant_1, status: 0)
+      @item_5 = FactoryBot.create(:item, merchant: @merchant_1, status: 0)
+    end
+
+    it "enabled_items" do
+      puts @merchant_1.items.enabled_items.inspect
+      puts @merchant_1.items.inspect
+      expect(@merchant_1.items.enabled_items).to eq([@item_1, @item_2, @item_3])
+    end
+
+    it "disabled_items" do
+      expect(@merchant_1.items.disabled_items).to eq([@item_4, @item_5])
+    end
+  end
 end
