1e38898eda8b5854b029c5e0f7f0a98bd6288f79
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-12 18:50:21 -0400

    wip

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -22,42 +22,56 @@ RSpec.describe 'Admin Index (Dashboard) Page', type: :feature do
     expect(current_path).to eq(admin_invoices_path)
   end
 
-  it 'displays the top 5 customers with their names and number of successful transactions' do
+  describe 'User Story 21' do
+    before(:each) do
+      5.times do
+        customer = create(:customer)
+        2.times do
+          invoice = create(:invoice, customer: customer)
+          create(:transaction, result: true, invoice: invoice)
+        end
+      end
 
+      @most_transactions_customer = Customer.first
+      invoice = @most_transactions_customer.invoices.first
+      create(:transaction, result: true, invoice: invoice)
 
-    within("#customer-#{.id}") do # add customer variable
-      expect(page).to have_content() #customer name 1
-      expect(page).to have_content() #add number of successful transactions
-      expect().to_appear_before()  #name to appear before transactions count
+      @no_invoice_customer = create(:customer)
     end
 
-    within("#customer-#{.id}") do # add customer variable
-      expect(page).to have_content() #customer name 2
-      expect(page).to have_content() #add number of successful transactions
-      expect().to_appear_before()  #name to appear before transactions count
-    end
+    it 'displays the top 5 customers with their names and number of successful transactions' do
+      require 'pry'; binding.pry
+      # within("#customer-#{.id}") do # add customer variable
+      #   expect(page).to have_content() #customer name 1
+      #   expect(page).to have_content() #add number of successful transactions
+      #   expect().to_appear_before()  #name to appear before transactions count
+      # end
 
-    within("#customer-#{.id}") do # add customer variable
-      expect(page).to have_content() #customer name 3
-      expect(page).to have_content() #add number of successful transactions
-      expect().to_appear_before()  #name to appear before transactions count
-    end
+      # within("#customer-#{.id}") do # add customer variable
+      #   expect(page).to have_content() #customer name 2
+      #   expect(page).to have_content() #add number of successful transactions
+      #   expect().to_appear_before()  #name to appear before transactions count
+      # end
 
-    within("#customer-#{.id}") do # add customer variable
-      expect(page).to have_content() #customer name 4
-      expect(page).to have_content() #add number of successful transactions
-      expect().to_appear_before()  #name to appear before transactions count
-    end
+      # within("#customer-#{.id}") do # add customer variable
+      #   expect(page).to have_content() #customer name 3
+      #   expect(page).to have_content() #add number of successful transactions
+      #   expect().to_appear_before()  #name to appear before transactions count
+      # end
 
-    within("#customer-#{.id}") do # add customer variable
-      expect(page).to have_content() #customer name 5
-      expect(page).to have_content() #add number of successful transactions
-      expect().to_appear_before()  #name to appear before transactions count
+      # within("#customer-#{.id}") do # add customer variable
+      #   expect(page).to have_content() #customer name 4
+      #   expect(page).to have_content() #add number of successful transactions
+      #   expect().to_appear_before()  #name to appear before transactions count
+      # end
+
+      # within("#customer-#{.id}") do # add customer variable
+      #   expect(page).to have_content() #customer name 5
+      #   expect(page).to have_content() #add number of successful transactions
+      #   expect().to_appear_before()  #name to appear before transactions count
+        expect(true).to eq(false)
+      # end
     end
   end
 end
 
-Then I see the names of the top 5 customers
-who have conducted the largest number of successful transactions
-And next to each customer name I see the number of successful transactions they have
-conducted
