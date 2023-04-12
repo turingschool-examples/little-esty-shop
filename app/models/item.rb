650fed520ff6107e71b00f8066aef94f71fc4ef3
Reilly Miller <reillyjmiller31@gmail.com>
2023-04-12 16:54:02 -0600

    Wrote two class methods for items enabled and disabled

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
   belongs_to :merchant
   enum status: ["disabled", "enabled"]
+
+  def self.enabled_items
+    Item.where(status: 1)
+  end
+
+  def self.disabled_items
+    Item.where(status: 0)
+  end
 end
