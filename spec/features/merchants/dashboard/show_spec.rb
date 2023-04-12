834cc3ed7f76963b5e93b58a264aeadadb5f57a7
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-12 17:07:41 -0400

    update to complete test coverage for model

--- spec/features/merchants/dashboard/show_spec.rb
+++ spec/features/merchants/dashboard/show_spec.rb
@@ -50,13 +50,14 @@ RSpec.describe 'Merchant Dashboard Show Page' do
   describe 'As a merchant' do
     it 'I see the names of the top 5 customers' do
       visit dashboard_merchant_path(@merchant_1)
-      
+
       within '#top_customers' do
         expect(@customer_6.last_name).to appear_before(@customer_5.last_name)
         expect(@customer_5.last_name).to appear_before(@customer_1.last_name)
         expect(@customer_1.last_name).to appear_before(@customer_2.last_name)
         expect(@customer_2.last_name).to appear_before(@customer_3.last_name)
       end
+
       @invoice_21 = Invoice.create!(customer_id: @customer_5.id, status: 1)
       @invoice_22 = Invoice.create!(customer_id: @customer_5.id, status: 1)
       @invoice_23 = Invoice.create!(customer_id: @customer_5.id, status: 1)
@@ -74,19 +75,21 @@ RSpec.describe 'Merchant Dashboard Show Page' do
       @transaction_25 = Transaction.create!(invoice_id: @invoice_25.id, credit_card_number: 1234567890123456, credit_card_expiration_date: 2020-01-01, result: 1)
 
       visit dashboard_merchant_path(@merchant_1)
-      save_and_open_page
+      
       within '#top_customers' do
         expect(@customer_5.last_name).to appear_before(@customer_6.last_name)
         expect(@customer_6.last_name).to appear_before(@customer_1.last_name)
       end
     end
+
+    it 'I see the number of successful transactions for each customer' do
+      visit dashboard_merchant_path(@merchant_1)
+
+      within '#top_customers' do
+        expect("Customer Six * Number of Transactions: 7").to appear_before("Customer Five * Number of Transactions: 4")
+        expect("Customer Five * Number of Transactions: 4").to appear_before("Customer One * Number of Transactions: 3")
+      end
+    end
     
   end
 end
-
-# As a merchant,
-# When I visit my merchant dashboard
-# Then I see the names of the top 5 customers
-# who have conducted the largest number of successful transactions with my merchant
-# And next to each customer name I see the number of successful transactions they have
-# conducted with my merchant
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -13,7 +13,8 @@ RSpec.describe Invoice, type: :model do
 
     it 'can find top 5 customers' do
       expect(@merchant_1.top_five_customers).to eq([@customer_6, @customer_5, @customer_1, @customer_2, @customer_3])  
-
+      expect(@merchant_1.top_five_customers.first.transaction_count).to eq(7)
+      
       @invoice_21 = Invoice.create!(customer_id: @customer_5.id, status: 1)
       @invoice_22 = Invoice.create!(customer_id: @customer_5.id, status: 1)
       @invoice_23 = Invoice.create!(customer_id: @customer_5.id, status: 1)
@@ -30,6 +31,7 @@ RSpec.describe Invoice, type: :model do
       @transaction_24 = Transaction.create!(invoice_id: @invoice_24.id, credit_card_number: 1234567890123456, credit_card_expiration_date: 2020-01-01, result: 1)
       @transaction_25 = Transaction.create!(invoice_id: @invoice_25.id, credit_card_number: 1234567890123456, credit_card_expiration_date: 2020-01-01, result: 1)
 
+      expect(@merchant_1.top_five_customers.first.transaction_count).to eq(9)
       expect(@merchant_1.top_five_customers).to eq([@customer_5, @customer_6, @customer_1, @customer_2, @customer_3])
     end
   end
