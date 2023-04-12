355ec302d411eb947ceb5bbcfd57ca119703dbb0
Logan Cole <emtlogancole@gmail.com>
2023-04-12 10:36:23 -0500

    test and create links to Merchant and invoices index

--- app/views/admin/index.html.erb
+++
@@ -1,1 +0,0 @@
-<h3>Admin Dashboard</h3>
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/admin', to: 'admin#dashboard'
+  get 'admin/merchants', to: 'admin/merchants#index'
+  get 'admin/invoices', to: 'admin/invoices#index'
+
   resources :admin, only: [:index]
   resources :merchants, only: [] do
     resources :items, only: [:index, :show], controller: 'merchants/items'
