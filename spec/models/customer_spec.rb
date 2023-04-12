7acf713fec3d0a60d7563b6f168ce4ec527ee370
Logan Cole <emtlogancole@gmail.com>
2023-04-12 17:27:21 -0500

    feat: admin dashboard shows top 5 customers

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -20,26 +20,28 @@ RSpec.describe Customer, type: :model do
     @invoice_23 = Invoice.create!(status: "Completed", customer_id: @customer_4.id)
     @invoice_24 = Invoice.create!(status: "Completed", customer_id: @customer_4.id)
     @invoice_25 = Invoice.create!(status: "Completed", customer_id: @customer_5.id)
+    @invoice_26 = Invoice.create!(status: "Completed", customer_id: @customer_5.id)
+    @invoice_27 = Invoice.create!(status: "Completed", customer_id: @customer_4.id)
+    @invoice_28 = Invoice.create!(status: "Completed", customer_id: @customer_3.id)
 
-    @invoice_item_21 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_11.id, quantity: 1, unit_price: 100, status: 0)
-    @invoice_item_22 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_12.id, quantity: 1, unit_price: 100, status: 0)
-    @invoice_item_23 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_13.id, quantity: 1, unit_price: 100, status: 0)
-    @invoice_item_24 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_14.id, quantity: 1, unit_price: 100, status: 0)
-    @invoice_item_25 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_15.id, quantity: 1, unit_price: 100, status: 0)
-
-    @invoice_item_26 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_16.id, quantity: 1, unit_price: 1500, status: 0)
-    @invoice_item_27 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_17.id, quantity: 1, unit_price: 1500, status: 0)
-    @invoice_item_28 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_18.id, quantity: 1, unit_price: 1500, status: 0)
-    @invoice_item_29 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_19.id, quantity: 1, unit_price: 1500, status: 0)
-
-    @invoice_item_30 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_20.id, quantity: 1, unit_price: 50000, status: 0)
-    @invoice_item_31 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_21.id, quantity: 1, unit_price: 50000, status: 0)
-    @invoice_item_32 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_22.id, quantity: 1, unit_price: 50000, status: 0)
-
-    @invoice_item_33 = InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_23.id, quantity: 1, unit_price: 20000, status: 0)
-    @invoice_item_34 = InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_24.id, quantity: 1, unit_price: 20000, status: 0)
-
-    @invoice_item_35 = InvoiceItem.create!(item_id: @item_5.id, invoice_id: @invoice_25.id, quantity: 1, unit_price: 25000, status: 0)
+    @invoice_item_21 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_11.id, quantity: 1, unit_price: 100, status: "shipped")
+    @invoice_item_22 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_12.id, quantity: 1, unit_price: 100, status: "shipped")
+    @invoice_item_23 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_13.id, quantity: 1, unit_price: 100, status: "shipped")
+    @invoice_item_24 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_14.id, quantity: 1, unit_price: 100, status: "shipped")
+    @invoice_item_25 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_15.id, quantity: 1, unit_price: 100, status: "shipped")
+    @invoice_item_26 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_16.id, quantity: 1, unit_price: 1500, status: "shipped")
+    @invoice_item_27 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_17.id, quantity: 1, unit_price: 1500, status: "shipped")
+    @invoice_item_28 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_18.id, quantity: 1, unit_price: 1500, status: "shipped")
+    @invoice_item_29 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_19.id, quantity: 1, unit_price: 1500, status: "shipped")
+    @invoice_item_30 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_20.id, quantity: 1, unit_price: 50000, status: "shipped")
+    @invoice_item_31 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_21.id, quantity: 1, unit_price: 50000, status: "shipped")
+    @invoice_item_32 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_22.id, quantity: 1, unit_price: 50000, status: "shipped")
+    @invoice_item_33 = InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_23.id, quantity: 1, unit_price: 20000, status: "shipped")
+    @invoice_item_34 = InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_24.id, quantity: 1, unit_price: 20000, status: "shipped")
+    @invoice_item_35 = InvoiceItem.create!(item_id: @item_5.id, invoice_id: @invoice_25.id, quantity: 1, unit_price: 25000, status: "shipped")
+    @invoice_item_36 = InvoiceItem.create!(item_id: @item_5.id, invoice_id: @invoice_26.id, quantity: 1, unit_price: 25000, status: "shipped")
+    @invoice_item_37 = InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_27.id, quantity: 1, unit_price: 20000, status: "shipped")
+    @invoice_item_38 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_28.id, quantity: 1, unit_price: 50000, status: "shipped")
 
     @transaction_11 = Transaction.create!(credit_card_number: 1234567890123456, credit_card_expiration_date: "2024-01-01", result: true, invoice: @invoice_11)
     @transaction_12 = Transaction.create!(credit_card_number: 1234567890123456, credit_card_expiration_date: "2024-01-01", result: true, invoice: @invoice_12)
@@ -56,12 +58,14 @@ RSpec.describe Customer, type: :model do
     @transaction_23 = Transaction.create!(credit_card_number: 1234567890123456, credit_card_expiration_date: "2024-01-01", result: true, invoice: @invoice_23)
     @transaction_24 = Transaction.create!(credit_card_number: 1234567890123456, credit_card_expiration_date: "2024-01-01", result: true, invoice: @invoice_24)
     @transaction_25 = Transaction.create!(credit_card_number: 1234567890123456, credit_card_expiration_date: "2024-01-01", result: true, invoice: @invoice_25)
-
+    @transaction_26 = Transaction.create!(credit_card_number: 1234567890123456, credit_card_expiration_date: "2024-01-01", result: true, invoice: @invoice_26)
+    @transaction_27 = Transaction.create!(credit_card_number: 1234567890123456, credit_card_expiration_date: "2024-01-01", result: true, invoice: @invoice_27)
+    @transaction_28 = Transaction.create!(credit_card_number: 1234567890123456, credit_card_expiration_date: "2024-01-01", result: true, invoice: @invoice_28)
   end
   describe "#instance methods" do
     describe "#top_five_customers" do
       it 'can find the top 5 customers by successful transactions' do
-        expect(Customer.top_five_customers).to eq([@customer_1, @customer_2, @customer_3, @customer_4, @customer_10])
+        expect(Customer.top_five_customers).to eq([@customer_1, @customer_2, @customer_3, @customer_4, @customer_5])
       end
     end
   end
