ee4f2034339d18c5a4e76abf26cfc3d829656c4c
Young Koh <youngheon6@gmail.com>
2023-04-12 14:21:18 -0600

    feat: admin routes and controller setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#welcome'
   get '/merchants', to: 'merchants#index'
+
+  get '/admin', to: 'admin#index'
+
+  namespace :admin do
+    resources :merchants, except: [:destroy, :create , :update]
+    resources :invoices, only: [:index, :show]
+  end
 end
