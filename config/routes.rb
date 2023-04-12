3065556a7f1b7be1ae765d0e53862dc0015d8d32
Logan Cole <emtlogancole@gmail.com>
2023-04-12 10:41:05 -0500

    set route prefixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/admin', to: 'admin#dashboard'
-  get 'admin/merchants', to: 'admin/merchants#index'
-  get 'admin/invoices', to: 'admin/invoices#index'
+  get '/admin', to: 'admin#dashboard', as: 'admin_dashboard'
+  get 'admin/merchants', to: 'admin/merchants#index', as: 'admin_merchants'
+  get 'admin/invoices', to: 'admin/invoices#index',  as: 'admin_invoices'
 
   resources :admin, only: [:index]
   resources :merchants, only: [] do
