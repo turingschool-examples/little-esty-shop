3a88856e4507610b54fa67de21d19b974d05bbb7
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-12 17:54:03 -0600

    feat: add items not yet shipped instance to the dashboard controller

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Merchant::DashboardController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
+    @items_not_yet_shipped = @merchant.invoice_items.not_yet_shipped
   end
 end
