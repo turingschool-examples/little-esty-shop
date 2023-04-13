660b786fb94591cb69c0c51696e1b96565a99949
Grace Joh <graceehjoh@gmail.com>
2023-04-13 18:55:49 -0500

    fix: invoice unit price faker number

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :invoice_item do
     sequence(:id)
     quantity { Faker::Number.between(from: 1, to: 5) }
-    unit_price { Faker::Commerce.between(from: 1, to: 1000) }
+    unit_price { Faker::Number.between(from: 1, to: 1000) }
     status { Faker::Number.between(from: 0, to: 2) }
     created_at { Time.zone.now }
     updated_at { Time.zone.now }
