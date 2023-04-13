01e84de8ae23b8510c4116eacf31f1915cc0e038
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-13 09:32:33 -0600

    test: Add tests for enums.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  it { should belong_to(:customer) }
-  it { should have_many(:invoice_items) }
-  it { should have_many(:items).through(:invoice_items) }
-  it { should have_many(:transactions) }
+  describe 'relationships' do
+    it { should belong_to(:customer) }
+    it { should have_many(:invoice_items) }
+    it { should have_many(:items).through(:invoice_items) }
+    it { should have_many(:transactions) } 
+  end
+  
+  describe 'enums' do
+    it do
+      should define_enum_for(:status).
+     with_values(["in progress", "completed", "cancelled"])
+    end
+  end
 end
