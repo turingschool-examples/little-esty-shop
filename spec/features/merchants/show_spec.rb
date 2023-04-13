f51bb9122badcf0686aedfbf206bfc4545fc326d
Crow Rising <miscbyrd@gmail.com>
2023-04-12 21:21:01 -0600

    test: Add tests for merchant invoices index

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -34,6 +34,44 @@ RSpec.describe 'merchant show page' do
        expect(current_path).to eq("/merchants/#{@merch_1.id}/invoices")
     end
 
+    it 'displays the names of top five customers based on successful transactions' do
+      visit "/merchants/#{@merch_1.id}/dashboard"
 
+      expect(page).to have_content(@cust_1.first_name)
+      expect(page).to have_content(@cust_1.last_name)
+      expect(page).to have_content(@cust_2.first_name)
+      expect(page).to have_content(@cust_2.last_name)
+      expect(page).to have_content(@cust_3.first_name)
+      expect(page).to have_content(@cust_3.last_name)
+      expect(page).to have_content(@cust_4.first_name)
+      expect(page).to have_content(@cust_4.last_name)
+      expect(page).to have_content(@cust_5.first_name)
+      expect(page).to have_content(@cust_5.last_name)
+      expect(page).to have_no_content(@cust_6.first_name)
+      expect(page).to have_no_content(@cust_6.last_name)
+
+      expect(@cust_1.last_name).to appear_before(@cust_2.last_name)
+      expect(@cust_2.last_name).to appear_before(@cust_3.last_name)
+      expect(@cust_3.last_name).to appear_before(@cust_4.last_name)
+      expect(@cust_4.last_name).to appear_before(@cust_5.last_name)
+    end
+
+    it 'displays the number of successful transactions next to the customer name' do
+      visit "/merchants/#{@merch_1.id}/dashboard"
+      within("#customer-#{@cust_1.id}") do
+        expect(page).to have_content("6")
+      end
+
+      within("#customer-#{@cust_2.id}") do
+        expect(page).to have_content("5")
+      end
+    end
+
+    it 'does not count failed transactions' do
+      visit "/merchants/#{@merch_1.id}/dashboard"
+      within("#customer-#{@cust_5.id}") do
+        expect(page).to have_content("2")
+      end
+    end
   end
 end
