2e10d8ad06a0fa35f6b61c447fae312d5b7a06d1
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-13 18:14:00 -0400

    test: add tests for sorting invoices by creation date and formatting dates

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe Invoice, type: :model do
   end
 
   describe 'class methods' do
-    describe '.find_incomplete_invoices' do
-      it 'returns all invoices that have items that have not yet shipped' do
+    describe '.find_and_sort_incomplete_invoices' do
+      it 'returns all invoices that have items that have not yet shipped, sorted by creation date (oldest to newest)' do
         merchant = create(:merchant)
         customer = create(:customer)
 
@@ -26,19 +26,25 @@ RSpec.describe Invoice, type: :model do
         item_4 = create(:item, merchant: merchant)
         item_5 = create(:item, merchant: merchant)
         item_6 = create(:item, merchant: merchant)
+        item_7 = create(:item, merchant: merchant)
+        item_8 = create(:item, merchant: merchant)
 
-        @invoice_1 = create(:invoice, customer: customer) # 2 items that have not shipped
-        @invoice_2 = create(:invoice, customer: customer) # 1 item shipped, 1 not shipped
-        @invoice_3 = create(:invoice, customer: customer) # 2 items that have shipped
+        @invoice_1 = create(:invoice, created_at: '2023-03-26 09:54:09 UTC', customer: customer) # newest creation date
+        @invoice_2 = create(:invoice, created_at: '2023-03-24 09:54:09 UTC', customer: customer) # oldest creation date
+        @invoice_3 = create(:invoice, created_at: '2023-03-25 09:54:09 UTC', customer: customer) # middle creation date
+        @invoice_4 = create(:invoice, created_at: '2023-03-26 09:54:09 UTC', customer: customer) # 2 shipped items - expect to be excluded
+
 
         @invoice_item_1 = create(:invoice_item, status: "Pending", item: item_1, invoice: @invoice_1)
         @invoice_item_2 = create(:invoice_item, status: "Packaged", item: item_2, invoice: @invoice_1)
         @invoice_item_3 = create(:invoice_item, status: "Pending", item: item_3, invoice: @invoice_2)
         @invoice_item_4 = create(:invoice_item, status: "Shipped", item: item_4, invoice: @invoice_2)
-        @invoice_item_5 = create(:invoice_item, status: "Shipped", item: item_5, invoice: @invoice_3)
+        @invoice_item_5 = create(:invoice_item, status: "Packaged", item: item_5, invoice: @invoice_3)
         @invoice_item_6 = create(:invoice_item, status: "Shipped", item: item_6, invoice: @invoice_3)
+        @invoice_item_7 = create(:invoice_item, status: "Shipped", item: item_7, invoice: @invoice_4)
+        @invoice_item_8 = create(:invoice_item, status: "Shipped", item: item_8, invoice: @invoice_4)
 
-        expect(Invoice.find_incomplete_invoices).to eq([@invoice_1, @invoice_2])
+        expect(Invoice.find_and_sort_incomplete_invoices).to eq([@invoice_2, @invoice_3, @invoice_1])
       end
     end
   end
@@ -47,6 +53,15 @@ RSpec.describe Invoice, type: :model do
     describe 'customer_name' do
       it 'returns the name of the customer' do
         expect(@invoice_1.customer_name).to eq(@customer_1.first_name + " " + @customer_1.last_name)
+      end
+    end
+
+    describe '#format_creation_date' do
+      it 'returns the formatted creation date of the given invoice' do
+        customer = create(:customer)
+        invoice = create(:invoice, created_at: '2023-03-26 09:54:09 UTC', customer: customer)
+
+        expect(invoice.format_creation_date).to eq("Sunday, March 26, 2023")
       end
     end
   end
