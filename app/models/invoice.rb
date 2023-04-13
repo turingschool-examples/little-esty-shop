18185cf469191bc4ec33338ba816f2d372bb0345
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-04-13 10:57:52 -0500

    Fix: add missing distinct to association

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,7 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :transactions, dependent: :destroy
   has_many :invoice_items, dependent: :destroy
-  has_many :items, through: :invoice_items
+  has_many :items, -> { distinct }, through: :invoice_items
   has_many :merchants, -> { distinct }, through: :items
 
   validates :status, presence: true
