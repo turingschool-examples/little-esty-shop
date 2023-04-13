97cdc83f3e10b50a7567c6507f3aba192eb0fe1e
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-13 19:30:25 -0400

    fix capybara issue with dashboard show, create US17 tests

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:items).through(:invoice_items)}
     it { should have_many :transactions }
   end
+
+  before(:each) do
+    test_data
+  end
+
+  describe '#total_revenue' do
+    it 'can calculate the total revenue of an invoice' do
+      expect(@invoice_1.total_revenue).to eq(2600)
+      expect(@invoice_2.total_revenue).to eq(2700)
+    end
+  end
 end
