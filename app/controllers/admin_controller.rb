517371ca8f3013a34e7e08f79740ec4edee03f3a
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-13 18:15:41 -0400

    feat: implement invoice display order and add formatted creation dates to pages (user story 23)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,6 @@ class AdminController < ApplicationController
 
   def index
     @customers = Customer.top_5_by_transactions
-    @invoices = Invoice.find_incomplete_invoices
+    @invoices = Invoice.find_and_sort_incomplete_invoices
   end
 end
