e21b95230aa4dff98eaf10ed2abc9ee39773b240
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-13 11:31:48 -0600

    fix: remove some code that was not needed

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,8 +2,6 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
-  # has_many :customers, through: :invoices
-  # has_many :transactions, through: :invoices
 
   def top_five_customers
     Customer.select('customers.*, COUNT(transactions.id) as transaction_count')
@@ -13,12 +11,4 @@ class Merchant < ApplicationRecord
            .order('transaction_count DESC')
            .limit(5)
   end
-
-  # def top_five_customers
-  #   customers.select('customers.*, COUNT(transactions.id) as transaction_count')
-  #            .where("transactions.result = ?", 1)
-  #            .group('customers.id')
-  #            .order('transaction_count DESC')
-  #            .limit(5)
-  # end
 end
