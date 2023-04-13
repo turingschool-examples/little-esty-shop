bd1c7e8a955d6a56a9fb5b8579aedd1b1a019ec0
Isaac Thill <ithill22@turing.edu>
2023-04-12 23:38:59 -0600

    remove item

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -25,13 +25,18 @@ RSpec.describe 'Merchant Items Index Page' do
 
     it 'each item name has a button to enable or disable that item' do
       visit merchant_items_path(@merchant_1)
-
-      within "#enabled-item-#{@item_1.id}" do
-        expect(page).to_not have_button('Enable')
-        expect(page).to have_button('Disable')
-        
-        click_button('Disable')
+require 'pry'; binding.pry
+      within '#enabled-items-list' do
+        expect(page).to have_content(@item_1.name)
+        expect(page).to have_content(@item_9.name)
       end
+
+        within "#enabled-item-#{@item_1.id}" do
+          expect(page).to_not have_button('Enable')
+          expect(page).to have_button('Disable')
+          
+          click_button('Disable')
+        end
 
       expect(current_path).to eq(merchant_items_path(@merchant_1))
       
