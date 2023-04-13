b94af781bda5cfce90b56927e3b530cdb0007cc0
margdaly <margaret.daly@gmail.com>
2023-04-12 19:37:58 -0600

    feat: Add merchant/invoice show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get '/merchants/:id/dashboard', to: 'merchants#show'
 
   get '/merchants/:id/invoices', to: 'merchants/invoices#index'
+  get '/merchants/:id/invoices/:id', to: 'merchants/invoices#show'
   
   resources :merchants do
+    resources :items, only: [:index]
+    resources :invoices, only: [:index, :show]
   end
 end
