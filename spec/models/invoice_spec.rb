0a0888ec5a27066d92496c0123fd096a9d60b286
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-13 00:58:49 -0400

    test: add test for class method to find incomplete invoices to invoice model

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,4 +12,33 @@ RSpec.describe Invoice, type: :model do
     it { should validate_presence_of(:status)}
     it { should define_enum_for(:status)}
   end
+
+  describe 'class methods' do
+    describe '.find_incomplete_invoices' do
+      it 'returns all invoices that have items that have not yet shipped' do
+        merchant = create(:merchant)
+        customer = create(:customer)
+
+        item_1 = create(:item, merchant: merchant)
+        item_2 = create(:item, merchant: merchant)
+        item_3 = create(:item, merchant: merchant)
+        item_4 = create(:item, merchant: merchant)
+        item_5 = create(:item, merchant: merchant)
+        item_6 = create(:item, merchant: merchant)
+
+        @invoice_1 = create(:invoice, customer: customer) # 2 items that have not shipped
+        @invoice_2 = create(:invoice, customer: customer) # 1 item shipped, 1 not shipped
+        @invoice_3 = create(:invoice, customer: customer) # 2 items that have shipped
+
+        @invoice_item_1 = create(:invoice_item, status: "Pending", item: item_1, invoice: @invoice_1)
+        @invoice_item_2 = create(:invoice_item, status: "Packaged", item: item_2, invoice: @invoice_1)
+        @invoice_item_2 = create(:invoice_item, status: "Pending", item: item_3, invoice: @invoice_2)
+        @invoice_item_2 = create(:invoice_item, status: "Shipped", item: item_4, invoice: @invoice_2)
+        @invoice_item_2 = create(:invoice_item, status: "Shipped", item: item_5, invoice: @invoice_3)
+        @invoice_item_2 = create(:invoice_item, status: "Shipped", item: item_6, invoice: @invoice_3)
+
+        expect(Invoice.find_incomplete_invoices).to eq([@invoice_1, @invoice_2])
+      end
+    end
+  end
 end
