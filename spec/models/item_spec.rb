496a03587a8c4f9d52c035bd9fb739baee382641
Isaac Thill <ithill22@turing.edu>
2023-04-12 23:45:22 -0600

    test: add additional method testing

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,14 +15,17 @@ RSpec.describe Item, type: :model do
   describe 'enums' do
     it do
       should define_enum_for(:status).
-     with_values(["enabled", "disabled"])
+      with_values(["enabled", "disabled"])
     end
   end
 
   describe 'instance methods' do
     it '#unit_price_to_dollars' do
+      expect(@item_1.unit_price).to eq(100)
       expect(@item_1.unit_price_to_dollars).to eq('1.00')
+      expect(@item_2.unit_price).to eq(15000)
       expect(@item_2.unit_price_to_dollars).to eq('150.00')
+      expect(@item_3.unit_price).to eq(50000)
       expect(@item_3.unit_price_to_dollars).to eq('500.00')
     end
   end
