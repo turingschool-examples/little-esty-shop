808132fbb7871a5522581f0221b6f3e5548812b1
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-13 19:30:52 -0400

    create total_revenue instance method for invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,8 @@ class Invoice < ApplicationRecord
   belongs_to :customer
 
   enum status: ["cancelled", "in progress", "completed"]
+  
+  def total_revenue
+    invoice_items.sum('quantity * unit_price')
+  end
 end
