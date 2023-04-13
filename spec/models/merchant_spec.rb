2d06ad49822d5bcbd8d923d3cc4dc10ae29d40d1
Isaac Thill <ithill22@turing.edu>
2023-04-13 10:00:26 -0600

    test: add test for enable/disabled items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,4 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   it { should have_many(:items) }
+  it { should have_many(:invoices).through(:items)}
+  it { should validate_presence_of(:name)}
+
+  before :each do
+    test_data
+  end
+
+  describe 'instance methods' do
+    it '#enabled_items' do
+      expect(@merchant_1.enabled_items).to eq([@item_1, @item_9])
+      expect(@merchant_1.enabled_items).to_not eq([@item_2, @item_3])
+      expect(@merchant_2.enabled_items).to eq([@item_2, @item_10])
+      expect(@merchant_2.enabled_items).to_not eq([@item_1, @item_3])
+      @item_1.update(status: 1)
+      expect(@merchant_1.enabled_items).to eq([@item_9])
+      expect(@merchant_1.enabled_items).to_not eq([@item_1, @item_9])
+    end
+
+    it '#disabled_items' do
+      expect(@merchant_1.disabled_items).to eq([])
+      expect(@merchant_2.disabled_items).to eq([])
+      @item_1.update(status: 1)
+      @item_2.update(status: 1)
+      expect(@merchant_1.disabled_items).to eq([@item_1])
+      expect(@merchant_1.disabled_items).to_not eq([@item_9])
+    end
+  end
 end
