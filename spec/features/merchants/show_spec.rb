d3ab3baf661ebc0fb9b336900d518189fbc859bc
Crow Rising <miscbyrd@gmail.com>
2023-04-13 12:03:25 -0600

    wip: User story 3

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -1,13 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'merchant show page' do
-  before(:each) do
-    dummy_data
-  end
-
   describe 'as a merchant when I visit my dashboard' do
     it 'displays the name of merchant' do
       visit "/merchants/#{@merch_1.id}/dashboard"
+      
       expect(page).to have_content(@merch_1.name)
       expect(page).to have_no_content(@merch_2.name)
     end
