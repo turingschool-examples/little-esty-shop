ca9f125b63495ab617054975579175362a8029f0
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-13 09:48:14 -0600

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_11_004514) do
+ActiveRecord::Schema.define(version: 2023_04_12_233931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2023_04_11_004514) do
   create_table "invoice_items", force: :cascade do |t|
     t.integer "quantity"
     t.integer "unit_price"
-    t.integer "status"
+    t.integer "status", default: 0
     t.bigint "invoice_id"
     t.bigint "item_id"
     t.datetime "created_at", null: false
@@ -35,10 +35,10 @@ ActiveRecord::Schema.define(version: 2023_04_11_004514) do
   end
 
   create_table "invoices", force: :cascade do |t|
-    t.string "status"
     t.bigint "customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
   end
 
