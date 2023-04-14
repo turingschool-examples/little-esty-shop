178dec760b50ef4e167b4368f2a7513d8cdd16da
Crow Rising <miscbyrd@gmail.com>
2023-04-14 16:15:41 -0600

    test: Adjust data for passing test

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'merchant show page' do
   describe 'as a merchant when I visit my dashboard' do
+    before(:each) do
+      @merch_1 = create(:merchant)
+      @merch_2 = create(:merchant)
+    end
     it 'displays the name of merchant' do
       visit "/merchants/#{@merch_1.id}/dashboard"
       expect(page).to have_content(@merch_1.name)
@@ -27,7 +31,12 @@ RSpec.describe 'merchant show page' do
       end
        expect(current_path).to eq("/merchants/#{@merch_1.id}/invoices")
     end
-
+  end
+    
+  describe 'as merchant when I visit the favorite_customer section of the dashboard' do
+    before(:each) do
+      us_3_test_data
+    end
     it 'displays the names of top five customers based on successful transactions' do
       visit "/merchants/#{@merch_1.id}/dashboard"
 
@@ -44,7 +53,7 @@ RSpec.describe 'merchant show page' do
       expect(page).to have_no_content(@cust_1.first_name)
       expect(page).to have_no_content(@cust_1.last_name)
 
-      expect(@cust_1.last_name).to appear_before(@cust_2.last_name)
+      expect(@cust_6.last_name).to appear_before(@cust_2.last_name)
       expect(@cust_2.last_name).to appear_before(@cust_3.last_name)
       expect(@cust_3.last_name).to appear_before(@cust_4.last_name)
       expect(@cust_4.last_name).to appear_before(@cust_5.last_name)
@@ -52,6 +61,7 @@ RSpec.describe 'merchant show page' do
 
     it 'displays the number of successful transactions next to the customer name' do
       visit "/merchants/#{@merch_1.id}/dashboard"
+
       within("#customer-#{@cust_6.id}") do
         expect(page).to have_content("6")
       end
