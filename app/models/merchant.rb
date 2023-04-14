db037fa4cafcb070e55a5e8d77197b5d14382edc
Crow Rising <miscbyrd@gmail.com>
2023-04-13 21:44:01 -0600

    perf: Add realationships to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,17 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, through: :items
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
+
+  def favorite_customer
+    #Customer.joins(:items).where("items.merchant_id = #{self.id}").distinct
+  end
 end
+# def top_5_customers
+# customers.joins(:transactions)
+# .where(transactions: {result: 'success'})
+# .select("customers.*, count(DISTINCT transactions.id) as transaction_count")
+# .group("customers.id")
+# .order("transaction_count desc").limit(5)
+# end
