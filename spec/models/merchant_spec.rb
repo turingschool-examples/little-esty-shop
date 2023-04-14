79363c83fe482079d075da22044d2f01c9d84315
Crow Rising <miscbyrd@gmail.com>
2023-04-13 21:44:33 -0600

    test: Add realtionship tests to merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,5 +4,15 @@ RSpec.describe Merchant do
   describe 'relationships' do
     it { should have_many(:items).dependent(:destroy) }
     it { should have_many(:invoices).through(:items) }
+    it { should have_many(:customers).through(:invoices) }
+    it { should have_many(:transactions).through(:invoices) }
+  end
+
+  describe '#instance methods' do
+    describe '#favorite_customers' do
+      it 'returns an array of top five customers with most purchases' do
+        expect(@merch_1.favorite_customer).to eq(2)
+      end
+    end
   end
 end
