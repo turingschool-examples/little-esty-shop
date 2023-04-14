4552631e40c82b4b79448aed0848ed6d69e3e194
Sean Cowans <zedesia@yahoo.com>
2023-04-14 12:45:35 -0600

    feat: Complete user story 21

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
   def index
+    @customers = Customer.top_customers
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,12 @@
 class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
+
+  def self.top_customers
+    joins(:transactions)
+    .where(transactions: {result: 'success'})
+    .select("customers.*, count(DISTINCT transactions.id) as transaction_count")
+    .group("customers.id")
+    .order("transaction_count desc").limit(5)
+  end
 end
