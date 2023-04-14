4461d4afecd6af2061283ef1825bdb6d94819418
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-14 14:18:53 -0600

    Feat: update routes, and merchant invoices controller to finish user story 4

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -2,4 +2,10 @@ class Merchant::InvoicesController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoice = Invoice.find(params[:id])
+# require 'pry'; binding.pry
+  end
 end
