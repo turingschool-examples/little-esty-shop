8bd64818af13a407d163f7306cbd858585f64b25
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-04-14 17:12:45 -0600

    #top_five class method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
   has_many :invoices
+  has_many :transactions, through: :invoices
+
+  def self.top_five
+    Customer.joins(:transactions)
+      .select("CONCAT(customers.first_name, ' ',  customers.last_name) AS full_name, customers.id, COUNT(transactions.id) AS transaction_count")
+      .group("customers.id, full_name")
+      .order(count: :desc)
+      .limit(5)
+  end
 end
