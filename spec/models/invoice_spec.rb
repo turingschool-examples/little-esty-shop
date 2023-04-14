c7a2ba2a439a3d61900996473f5c3babad1f64a7
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-14 16:23:48 -0600

    feat: Add created_at_formatted and customer_full_name method.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -24,5 +24,19 @@ RSpec.describe Invoice, type: :model do
         expect(@invoice_4.total_revenue).to eq(3300000)
       end
     end
+
+    describe 'created_at_formatted' do
+      it "formats the created at attribute" do 
+        @customer_1 = Customer.create!(first_name: "John", last_name: "Doe")
+        invoice_1 = Invoice.create!(status: "cancelled", customer: @customer_1, created_at: '2023-14-04 20:54:10 UTC' )
+        expect(invoice_1.created_at_formatted).to eq("Friday, April 14, 2023")
+      end
+    end
+
+    describe 'customer_full_name' do
+      it 'returns customers first and last name' do
+        expect(@invoice_8.customer_full_name).to eq("Sally Williams")
+      end
+    end
   end
 end
