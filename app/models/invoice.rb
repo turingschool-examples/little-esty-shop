257503725fc63cf360411b11033cad17ecaea5be
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-14 16:24:51 -0600

    feat: Add created_at_formatted and customer_full_name method.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -9,4 +9,12 @@ class Invoice < ApplicationRecord
   def total_revenue
     invoice_items.sum("quantity * unit_price")
   end
+
+  def created_at_formatted
+    created_at.strftime("%A, %B %d, %Y")
+  end
+
+  def customer_full_name
+    "#{customer.first_name} #{customer.last_name}"
+  end
 end
