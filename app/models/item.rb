c85fbb47493db1c6cf4dd9e1dcdb8809711b548e
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-04-13 19:32:34 -0600

    add #top_five_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
   belongs_to :merchant
   enum status: ["disabled", "enabled"]
 
@@ -10,5 +11,9 @@ class Item < ApplicationRecord
 
   def self.disabled_items
     Item.where(status: 0)
+  end
+
+  def self.top_five_items
+    Item.select("items.*, sum(invoice_items.quantity * invoice_items.unit_price) AS product").joins(:transactions).where("transactions.result = 1").group("items.id").order(product: :desc).limit(5)
   end
 end
