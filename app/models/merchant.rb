de5163cd9579260d3f18e38aad7cb750514e48fc
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-13 20:34:38 -0600

    Feat: add items not shipped

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -10,4 +10,8 @@ class Merchant < ApplicationRecord
   def top_five_customers
     customers.joins(:transactions).where(transactions: {result: 'success'}).select("customers.*, CONCAT(first_name,' ',last_name) as name, count(DISTINCT transactions.id) as transactions_count").group("customers.id").order("transactions_count desc").limit(5)
   end
+
+  def items_not_shipped
+    invoice_items.select('invoice_items.*').where(status: [0, 1]).joins(:invoice).order('invoices.created_at')
+  end
 end
