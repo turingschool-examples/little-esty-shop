39f1725adf752995a31756b7656d87e834e3b60f
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-14 19:56:14 -0400

    feat: add enable or disable buttons and actions for each merchant on admin merchant page

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,7 +3,18 @@ class MerchantsController < ApplicationController
     @merchants = Merchant.all
   end
 
+  def update
+    merchant = Merchant.find(params[:id])
+    merchant.update(merchant_params)
+    redirect_to admin_merchants_path
+  end
+
   def show
     @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  private
+  def merchant_params
+    params.require(:merchant).permit(:name, :is_enabled)
   end
 end
