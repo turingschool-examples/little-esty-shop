e8d2af36d4af4073e7ff21a73529dcd4eef1f47f
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-15 12:07:33 -0600

    test: create test for user story 32 - admin invoice show page

---
+++ spec/features/admin/invoices/show_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Invoice Show Page' do
+  before (:each) do
+    test_data
+  end
+
+  describe 'User Story 32' do 
+    it 'can visit the admin invoice show page' do
+      visit admin_invoice_path(@invoice_1)
+    end
+
+    it 'I see information related to that invoice(invoice id, status, invoice created_at, and customer name)' do
+      visit admin_invoice_path(@invoice_1)
+
+      within("#invoice_details") do
+        expect(page).to have_content(@invoice_1.id)
+        expect(page).to have_content(@invoice_1.status)
+        expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %d, %Y"))
+        expect(page).to have_content(@customer_1.first_name)
+        expect(page).to have_content(@customer_1.last_name)
+      end 
+
+      visit admin_invoice_path(@invoice_2)
+
+      within("#invoice_details") do
+        expect(page).to have_content(@invoice_2.id)
+        expect(page).to have_content(@invoice_2.status)
+        expect(page).to have_content(@invoice_2.created_at.strftime("%A, %B %d, %Y"))
+        expect(page).to have_content(@customer_1.first_name)
+        expect(page).to have_content(@customer_1.last_name)
+      end
+    end
+  end
+end
