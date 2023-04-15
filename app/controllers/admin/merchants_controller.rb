7a1fd6fa00de48951ad32152c134d591c39a4be3
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-04-15 13:04:18 -0600

    add controller actions for edit and update

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,4 +6,19 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+    @merchant.update(admin_merchant_params)
+    redirect_to admin_merchant_path(@merchant)
+  end
+
+  private
+  def admin_merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
