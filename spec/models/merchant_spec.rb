800b3269cbc4c818473fac2779a6e7376c7a0e0a
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-16 18:42:55 -0400

    test: add tests for highest revenue date instance method on model class

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -45,5 +45,42 @@ RSpec.describe Merchant, type: :model do
         expect(merchant.enabled_status).to eq("Disabled")
       end
     end
+
+    describe "#highest_revenue_date" do
+      it 'returns the date of the invoice with the greatest revenue for the given merchant' do
+        customer = create(:customer)
+        merchant = create(:merchant)
+
+        invoice_1 = create(:invoice, created_at: '2023-01-01 20:54:10 UTC', customer: customer) # has only successful transactions
+        invoice_2 = create(:invoice, created_at: '2023-01-02 20:54:10 UTC', customer: customer) # has 2 successful transactions and 1 un-successful transaction
+        invoice_3 = create(:invoice, created_at: '2023-01-03 20:54:10 UTC', customer: customer) # has no successful transactions
+
+        item_1 = create(:item, merchant: merchant)
+        item_2 = create(:item, merchant: merchant)
+        item_3 = create(:item, merchant: merchant)
+
+        transaction_1 = create(:transaction, result: true, invoice: invoice_1)
+        transaction_2 = create(:transaction, result: true, invoice: invoice_1)
+        transaction_3 = create(:transaction, result: true, invoice: invoice_1)
+        transaction_4 = create(:transaction, result: true, invoice: invoice_2)
+        transaction_5 = create(:transaction, result: true, invoice: invoice_2)
+        transaction_6 = create(:transaction, result: false, invoice: invoice_2)
+        transaction_7 = create(:transaction, result: false, invoice: invoice_3)
+        transaction_8 = create(:transaction, result: false, invoice: invoice_3)
+        transaction_9 = create(:transaction, result: false, invoice: invoice_3)
+
+        invoice_item_1 = create(:invoice_item, invoice_id: invoice_1.id, item_id: item_1.id, quantity: 1, unit_price: 100)
+        invoice_item_2= create(:invoice_item, invoice_id: invoice_1.id, item_id: item_2.id, quantity: 100, unit_price: 10)
+        invoice_item_3 = create(:invoice_item, invoice_id: invoice_1.id, item_id: item_3.id, quantity: 5, unit_price: 50)
+        invoice_item_4 = create(:invoice_item, invoice_id: invoice_2.id, item_id: item_1.id, quantity: 0, unit_price: 0)
+        invoice_item_5= create(:invoice_item, invoice_id: invoice_2.id, item_id: item_2.id, quantity: 6, unit_price: 6)
+        invoice_item_6 = create(:invoice_item, invoice_id: invoice_2.id, item_id: item_3.id, quantity: 5, unit_price: 4)
+        invoice_item_7 = create(:invoice_item, invoice_id: invoice_3.id, item_id: item_1.id, quantity: 2, unit_price: 100)
+        invoice_item_8= create(:invoice_item, invoice_id: invoice_3.id, item_id: item_2.id, quantity: 100, unit_price: 10)
+        invoice_item_9 = create(:invoice_item, invoice_id: invoice_3.id, item_id: item_3.id, quantity: 5, unit_price: 50)
+
+        expect(merchant.highest_revenue_date).to eq(invoice_1.created_at)
+      end
+    end
   end
 end
