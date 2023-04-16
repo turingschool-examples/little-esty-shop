761106daad1a4c550e13750072d2a70432d11b94
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-16 14:35:45 -0600

    test: create tests for top_five_merchants and total revenue methods

--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -128,6 +128,64 @@ RSpec.describe 'Admin Dashboard Index Page' do
       end
     end
   end
+
+  describe 'User Story 30' do 
+    before (:each) do
+      test_data
+      @transaction_21 = @invoice_1.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_22 = @invoice_2.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_23 = @invoice_3.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_24 = @invoice_4.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_25 = @invoice_5.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_26 = @invoice_6.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_27 = @invoice_7.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_28 = @invoice_8.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_29 = @invoice_9.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_30 = @invoice_10.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_31 = @invoice_11.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_32 = @invoice_12.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_33 = @invoice_13.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_34 = @invoice_14.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_35 = @invoice_15.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_36 = @invoice_16.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_37 = @invoice_17.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_38 = @invoice_18.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_39 = @invoice_19.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_40 = @invoice_20.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @invoice_item_53 = @invoice_13.invoice_items.create!(item: @item_26, quantity: 1, unit_price: 2200, status: 1)
+      @invoice_item_54 = @invoice_14.invoice_items.create!(item: @item_26, quantity: 1, unit_price: 2200, status: 1)
+      @invoice_item_55 = @invoice_15.invoice_items.create!(item: @item_26, quantity: 1, unit_price: 2200, status: 1)
+      @invoice_item_56 = @invoice_16.invoice_items.create!(item: @item_30, quantity: 1, unit_price: 2300, status: 1)
+      @invoice_item_57 = @invoice_17.invoice_items.create!(item: @item_30, quantity: 1, unit_price: 2300, status: 1)
+      @invoice_item_58 = @invoice_18.invoice_items.create!(item: @item_30, quantity: 1, unit_price: 2300, status: 1)
+      @invoice_item_59 = @invoice_19.invoice_items.create!(item: @item_35, quantity: 1, unit_price: 2400, status: 1)
+      @invoice_item_60 = @invoice_20.invoice_items.create!(item: @item_35, quantity: 1, unit_price: 2400, status: 1)
+    end
+
+    it 'I see the names of the top 5 merchants by total revenue generated' do
+      visit admin_dashboard_path
+
+      within("#top_five_merchants") do
+        expect("#{@merchant_3.name}").to appear_before("#{@merchant_1.name}")
+        expect("#{@merchant_1.name}").to appear_before("#{@merchant_5.name}")
+        expect("#{@merchant_5.name}").to appear_before("#{@merchant_2.name}")
+        expect("#{@merchant_2.name}").to appear_before("#{@merchant_4.name}")
+        expect(page).to_not have_content(@merchant_6.name)
+      end
+    end
+
+    it 'I see the total revenue generated next to each merchant name' do
+      visit admin_dashboard_path
+      
+      within("#top_five_merchants") do
+        expect(page).to have_content(@merchant_3.name)
+        expect(page).to have_content(@merchant_3.total_revenue)
+        expect(page).to have_content(@merchant_1.name)
+        expect(page).to have_content(@merchant_1.total_revenue)
+        expect("#{@merchant_3.total_revenue}").to appear_before("#{@merchant_1.total_revenue}")
+      end
+    end
+  end
 end
 
 
