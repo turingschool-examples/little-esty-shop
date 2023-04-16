8e8fbb8649ef0a1b42f738eb524ec89db325ea58
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-16 14:36:51 -0600

    feat: create instance variable of top 5 merchants

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Admin::DashboardController < ApplicationController
     @merchants = Merchant.all
     @top_five_customers = Customer.top_five
     @incomplete_invoices = Invoice.incomplete_invoices
+    @top_five_merchants = Merchant.top_five_merchants
   end
 
 end
