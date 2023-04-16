5cfc420231387f51247aca3754a55d8ae35ed841
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-16 12:26:14 -0600

    feat: Add update action to merchants invoice items controller.

---
+++ app/controllers/merchants/invoice_items_controller.rb
@@ -0,0 +1,21 @@
+class Merchants::InvoiceItemsController < ApplicationController
+  def update
+    merchant = Merchant.find(params[:invoice_item][:merchant_id])
+    invoice = Invoice.find(params[:invoice_item][:invoice_id])
+    invoice_item = InvoiceItem.find(params[:id])
+    invoice_item.update(invoice_items_params)
+    if (params[:invoice_item][:status] == "packaged")
+      invoice_item.update(status: "packaged")
+    elsif (params[:invoice_item][:status] == "shipped")
+      invoice_item.update(status: "shipped")
+    else (params[:invoice_item][:status] == "pending")
+      invoice_item.update(status: "pending")
+    end
+    redirect_to merchant_invoice_path(merchant, invoice)
+  end
+
+  private
+  def invoice_items_params
+    params.require(:invoice_item).permit(:status)
+  end
+end
