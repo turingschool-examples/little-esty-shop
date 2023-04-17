e1a960a8d381d1be10dba94a77aa57ba52cecf16
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-16 21:01:13 -0400

    test + feat: add format_revenue method to applicationhelper and add tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def format_date(date)
     date.strftime("%-m/%-d/%y")
   end
+
+  def format_revenue(amount)
+    "$#{amount.fdiv(100).round()}"
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe ApplicationHelper do
       expect(format_date(Date.parse('2023-01-01 20:54:10 UTC'))).to eq("1/1/23")
     end
   end
+
+  describe 'format_revenue' do
+    it 'formats an amount of cents as dollars and decimals with a dollar sign' do
+      expect(format_revenue(123456)).to eq("$1235")
+    end
+  end
 end
