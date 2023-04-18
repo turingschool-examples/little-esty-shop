4e90530ef549a6f6a470ccac27a8230d1b468a2f
Reilly Miller <reillyjmiller31@gmail.com>
2023-04-17 18:46:13 -0600

    write feature tests for top day, move tests around

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -36,14 +36,18 @@ RSpec.describe "admin/merchants index", type: :feature do
 
     it "links to show pages are functional" do
       visit admin_merchants_path
+      within "#disabled_merchants" do
 
-      click_link(@merchant_1.name)
-      expect(current_path).to eq(admin_merchant_path(@merchant_1))
+        click_link(@merchant_1.name)
+        expect(current_path).to eq(admin_merchant_path(@merchant_1))
+      end
 
       visit admin_merchants_path
+      within "#disabled_merchants" do
 
-      click_link(@merchant_2.name)
-      expect(current_path).to eq(admin_merchant_path(@merchant_2))
+        click_link(@merchant_2.name)
+        expect(current_path).to eq(admin_merchant_path(@merchant_2))
+      end
     end
 
     it "has buttons to enable/disable merchants" do
@@ -110,6 +114,86 @@ RSpec.describe "admin/merchants index", type: :feature do
       expect(page).to have_link("Add New Merchant", href: new_admin_merchant_path)
       click_link("Add New Merchant")
       expect(current_path).to eq(new_admin_merchant_path)
+    end
+  end
+  describe 'User Story 30' do 
+    before (:each) do
+      test_data
+      @transaction_21 = @invoice_1.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_22 = @invoice_2.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_23 = @invoice_3.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_24 = @invoice_4.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_25 = @invoice_5.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_26 = @invoice_6.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_27 = @invoice_7.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_28 = @invoice_8.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_29 = @invoice_9.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_30 = @invoice_10.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_31 = @invoice_11.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_32 = @invoice_12.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_33 = @invoice_13.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_34 = @invoice_14.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_35 = @invoice_15.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_36 = @invoice_16.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_37 = @invoice_17.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_38 = @invoice_18.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_39 = @invoice_19.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @transaction_40 = @invoice_20.transactions.create!(credit_card_number: 1, credit_card_expiration_date: 1, result: 1)
+      @invoice_item_53 = @invoice_13.invoice_items.create!(item: @item_26, quantity: 1, unit_price: 2200, status: 1)
+      @invoice_item_54 = @invoice_14.invoice_items.create!(item: @item_26, quantity: 1, unit_price: 2200, status: 1)
+      @invoice_item_55 = @invoice_15.invoice_items.create!(item: @item_26, quantity: 1, unit_price: 2200, status: 1)
+      @invoice_item_56 = @invoice_16.invoice_items.create!(item: @item_30, quantity: 1, unit_price: 2300, status: 1)
+      @invoice_item_57 = @invoice_17.invoice_items.create!(item: @item_30, quantity: 1, unit_price: 2300, status: 1)
+      @invoice_item_58 = @invoice_18.invoice_items.create!(item: @item_30, quantity: 1, unit_price: 2300, status: 1)
+      @invoice_item_59 = @invoice_19.invoice_items.create!(item: @item_35, quantity: 1, unit_price: 2400, status: 1)
+      @invoice_item_60 = @invoice_20.invoice_items.create!(item: @item_35, quantity: 1, unit_price: 2400, status: 1)
+    end
+
+    it 'I see the names of the top 5 merchants by total revenue generated' do
+      visit admin_merchants_path
+
+      within("#top_five_merchants") do
+        expect("#{@merchant_3.name}").to appear_before("#{@merchant_1.name}")
+        expect("#{@merchant_1.name}").to appear_before("#{@merchant_5.name}")
+        expect("#{@merchant_5.name}").to appear_before("#{@merchant_2.name}")
+        expect("#{@merchant_2.name}").to appear_before("#{@merchant_4.name}")
+        expect(page).to_not have_content(@merchant_6.name)
+      end
+    end
+
+    it 'I see the total revenue generated next to each merchant name' do
+      visit admin_merchants_path
+      
+      within("#top_five_merchants") do
+        expect(page).to have_content(@merchant_3.name)
+        expect(page).to have_content(@merchant_3.total_revenue)
+        expect(page).to have_content(@merchant_1.name)
+        expect(page).to have_content(@merchant_1.total_revenue)
+        expect("#{@merchant_3.total_revenue}").to appear_before("#{@merchant_1.total_revenue}")
+      end
+    end
+    
+    it "shows the top merchants best day" do
+      @invoice_item_61 = @invoice_4.invoice_items.create!(item: @item_35, quantity: 1000, unit_price: 2400, status: 1)
+      @invoice_1.update(created_at: "06/04/2023")
+      @invoice_item_1.update(quantity: 1000)
+      @invoice_8.update(created_at: "16/04/2023")
+      @invoice_item_28.update(quantity: 1000)
+      @invoice_12.update(created_at: "09/04/2023")
+      @invoice_item_32.update(quantity: 1000)
+      @invoice_13.update(created_at: "31/03/2023")
+      @invoice_item_53.update(quantity: 10000)
+      @invoice_4.update(created_at: "27/03/2023")
+
+      visit admin_merchants_path
+      within "#top_five_merchants" do
+
+        expect(page).to have_content("Top day for #{@merchant_3.name} was 04/09/2023")
+        expect(page).to have_content("Top day for #{@merchant_1.name} was 04/06/2023")
+        expect(page).to have_content("Top day for #{@merchant_5.name} was 03/27/2023")
+        expect(page).to have_content("Top day for #{@merchant_2.name} was 04/16/2023")
+        expect(page).to have_content("Top day for #{@merchant_4.name} was 03/31/2023")
+      end
     end
   end
 end
