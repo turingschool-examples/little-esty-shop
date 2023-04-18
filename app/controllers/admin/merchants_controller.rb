b1abf8aa94453e2bf0e9a84f0f9c5f5399b5ab7b
Nickolas Marquez <nickomarquez7@gmail.com>
2023-04-18 12:18:00 -0600

    Feat: create updatre and index

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -3,9 +3,30 @@ class Admin::MerchantsController < ApplicationController
     @merchants = Merchant.all
   end
 
-  def show
-    require 'pry';binding.pry
-    
+  def show    
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+    if @merchant.update(merchant_params)
+     
+      redirect_to "/admin/merchants/#{@merchant.id}"
+      flash[:notice] = "Successfully Updated"
+    else
+      redirect_to "/admin/merchants/#{@merchant.id}/edit"
+      flash[:notice] = "Field cannot be blank"
+    end
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:id, :name)
+  end
+
 end
