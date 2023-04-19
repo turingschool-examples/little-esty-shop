54074e9e879f4ab1dd62d637abbd6822dd03cd6c
margdaly <margaret.daly@gmail.com>
2023-04-19 14:02:03 -0600

    Set logo in application_controller

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,12 +2,10 @@ class InvoicesController < ApplicationController
   def index
     @invoices = Invoice.all
     @merchant = Merchant.find(params[:merchant_id])
-    @logo_photo = PhotoBuilder.logo_img
   end
 
   def show
     @invoice = Invoice.find(params[:id])
     @merchant = Merchant.find(params[:merchant_id])
-    @logo_photo = PhotoBuilder.logo_img
   end
 end
