69c77ef789740d9b864ae190829cb7ea3cb7da47
Logan Cole <emtlogancole@gmail.com>
2023-04-19 18:59:47 -0500

    test: admin/invoices#show page

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -7,6 +7,26 @@ RSpec.describe "admin/invoices#show" do
 
 
   it 'has invoice ID' do
-    visit admin_invoices_show
+    visit admin_invoice(@invoice_1)
+
+    expect(page).to have_content(@invoice_1.id)
+  end
+
+  xit 'has status' do
+    visit admin_invoice(@invoice_1)
+
+    expect(page).to have_content(@invoice_1.status)
+  end
+
+  xit 'has created at formatted' do
+    visit admin_invoice(@invoice_1)
+
+    expect(page).to have_content(@invoice_1.created_at_formatted)
+  end
+
+  xit 'has customer first and last name' do
+    visit admin_invoice(@invoice_1)
+
+    expect(page).to have_content(@invoice_1.id)
   end
 end
