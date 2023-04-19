e2fed6a953b89a367fff8de8b23d932b23606100
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-04-19 05:29:26 -0500

    Test: add test for logo likes

--- spec/features/application/welcome_spec.rb
+++ spec/features/application/welcome_spec.rb
@@ -7,10 +7,12 @@ RSpec.describe 'Welcome Page', type: :feature do
     @invoice_1 = create(:invoice, customer_id: @customer_1.id,status: 'In Progress')
     @item_1 = create(:item, merchant_id: @merchant_1.id)
     @invoice_item_1 = create(:invoice_item, invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 50, unit_price: 50)
+
+    visit '/'
   end
+
   describe "Unsplash API: App Logo" do
     it "displays the logo on every page" do
-      visit '/'
       expect(page).to have_selector("#logo_img")
       visit merchant_dashboard_path(@merchant_1.id)
       expect(page).to have_selector("#logo_img")
@@ -28,7 +30,10 @@ RSpec.describe 'Welcome Page', type: :feature do
       expect(page).to have_selector("#logo_img")
       visit admin_invoices_path
       expect(page).to have_selector("#logo_img")
+    end
 
+    it 'displays the logo likes on every page' do
+      expect(page).to have_content("Likes: 111")
     end
   end
 end
