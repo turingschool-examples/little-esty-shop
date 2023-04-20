519ecee90bd8da87d4d9d661e1386f7dacf74abb
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-19 21:44:57 -0600

    Users story 36.

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -5,6 +5,17 @@ class Admin::InvoicesController < ApplicationController
 
   def show
     @invoice = Invoice.find(params[:id])
-    @invoice_created = @invoice.created_at_formatted
+  end
+
+  def update
+    # binding.pry
+    invoice = Invoice.find(params[:id])
+    invoice.update(invoice_params)
+    redirect_to admin_invoice_path(invoice)
+  end
+
+  private
+  def invoice_params
+    params.permit(:status)
   end
 end
