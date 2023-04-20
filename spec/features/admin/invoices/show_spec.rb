9a4412a19a0630111654cdd68363a18315c67eeb
Logan Cole <emtlogancole@gmail.com>
2023-04-19 19:35:13 -0500

    test admin/invoices show page

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -29,4 +29,40 @@ RSpec.describe "admin/invoices#show" do
 
     expect(page).to have_content("#{@invoice_1.customer.first_name} #{@invoice_1.customer.last_name}")
   end
+
+  it 'lists item names' do
+    visit admin_invoice_path(@invoice_1)
+    
+    expect(page).to have_content("#{@item_1.name}")
+    expect(page).to have_content("#{@item_10.name}")
+  end
+
+  xit 'lists quantity' do
+    visit admin_invoice_path(@invoice_1)
+    
+    #probably a within here
+    expect(page).to have_content("Quantity: #{@invoice_item_1.quantity}")
+    expect(page).to have_content("Quantity: #{@invoice_item_10.quantity}")
+  end
+
+  xit 'shows price sold for' do
+    visit admin_invoice_path(@invoice_1)
+    
+    expect(page).to have_content("Quantity: #{@invoice_item_1.unit_price}")
+    expect(page).to have_content("Quantity: #{@invoice_item_10.unit_price}")
+  end
+
+  xit 'shows invoice_item status' do
+    visit admin_invoice_path(@invoice_1)
+    
+    expect(page).to have_content("Quantity: #{@invoice_item_1.status}")
+    expect(page).to have_content("Quantity: #{@invoice_item_10.status}")
+  end
+
+  xit 'shows the total revenue' do
+    visit admin_invoice_path(@invoice_1)
+    
+    expect(page).to have_content("Quantity: #{@invoice_item_1.total_revenue}")
+    expect(page).to have_content("Quantity: #{@invoice_item_10.total_revenue}")
+  end
 end
